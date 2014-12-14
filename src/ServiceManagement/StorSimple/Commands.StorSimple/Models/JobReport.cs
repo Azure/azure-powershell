52d58e3b43884271c7adbea6b3c37f5a3d51892f
madhurta <madhur.tanwani@microsoft.com>
2014-12-14 20:13:12 +0530

    Build fix

--- src/ServiceManagement/StorSimple/Commands.StorSimple/Models/JobReport.cs
+++ src/ServiceManagement/StorSimple/Commands.StorSimple/Models/JobReport.cs
@@ -1,29 +1,25 @@
 ﻿using Microsoft.WindowsAzure.Management.StorSimple.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Microsoft.WindowsAzure.Commands.StorSimple.Models
 {
     public class JobReport
     {
         public string JobId { get; set; }
-        public JobResult JobResult { get; set; }
-        public JobStatus JobStatus { get; set; }
+        public AsyncTaskResult JobResult { get; set; }
+        public AsyncTaskStatus JobStatus { get; set; }
         public string ErrorCode { get; set; }
         public string ErrorMessage { get; set; }
-        public IList<JobStep> JobSteps { get; set; }
+        public IList<TaskStep> JobSteps { get; set; }
 
-        public JobReport(JobStatusInfo jobStatusInfo)
+        public JobReport(TaskStatusInfo jobStatusInfo)
         {
-            this.JobId = jobStatusInfo.JobId;
+            this.JobId = jobStatusInfo.TaskId;
             this.JobResult = jobStatusInfo.Result;
             this.JobStatus = jobStatusInfo.Status;
             this.ErrorCode = jobStatusInfo.Error.Code;
             this.ErrorMessage = jobStatusInfo.Error.Message;
-            this.JobSteps = jobStatusInfo.JobSteps;
+            this.JobSteps = jobStatusInfo.TaskSteps;
         }
     }
 }
