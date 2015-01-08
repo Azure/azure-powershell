304d39661fa49eca203a14c0cf3fc0ed8f6467bc
Esha Parmar <esparmar@microsoft.com>
2015-01-08 15:29:34 -0800

    initial code changes for dsc extension status cmdlet

---
+++ src/ServiceManagement/Compute/Commands.ServiceManagement/IaaS/Extensions/DSC/VirtualMachineDscExtensionStatusContext.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Microsoft.WindowsAzure.Commands.ServiceManagement.IaaS.Extensions
+{
+    public class VirtualMachineDscExtensionStatusContext
+    {
+        public string ServiceName { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public int StatusCode { get; set; }
+        public string StatusMessage { get; set; }
+        public string[] DscConfigurationLog { get; set; }
+        public string TimestampUtc { get; set; }
+    }
+}
