8bb370fe70f0d86a0c407158bec753ae0e203722
stankovski <denis.stankovski@gmail.com>
2015-02-11 11:46:21 -0800

    Added test infrastructure for profile cmdlets

--- src/Common/Commands.Profile/Profile/NewAzureProfile.cs
+++ src/Common/Commands.Profile/Profile/NewAzureProfile.cs
@@ -12,6 +12,9 @@
 // limitations under the License.
 // ----------------------------------------------------------------------------------
 
+using System;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Azure.Common.Authentication;
 using Microsoft.Azure.Common.Authentication.Models;
 using Microsoft.WindowsAzure.Commands.Utilities.Common;
 using System.Management.Automation;
@@ -27,6 +30,7 @@ namespace Microsoft.WindowsAzure.Commands.Profile
     {
         private const string CertificateParameterSet = "Certificate";
         private const string CredentialsParameterSet = "Credentials";
+        private const string AccessTokenParameterSet = "Token";
         private const string FileParameterSet = "File";
 
         [Parameter(Mandatory = false, Position = 0, ValueFromPipelineByPropertyName = true, ParameterSetName = CertificateParameterSet)]
@@ -42,20 +46,65 @@ namespace Microsoft.WindowsAzure.Commands.Profile
         public string StorageAccount { get; set; }
         
         [Parameter(Mandatory = true, Position = 3, ValueFromPipelineByPropertyName = true, ParameterSetName = CertificateParameterSet)]
-        public string Certificate { get; set; }
+        public X509Certificate2 Certificate { get; set; }
 
         [Parameter(Mandatory = true, Position = 3, ValueFromPipelineByPropertyName = true, ParameterSetName = CredentialsParameterSet)]
         public PSCredential Credential { get; set; }
-        
-        [Parameter(Mandatory = true, Position = 4, ValueFromPipelineByPropertyName = true, ParameterSetName = CertificateParameterSet)]
+
+        [Parameter(Mandatory = true, Position = 4, ValueFromPipelineByPropertyName = true, ParameterSetName = CredentialsParameterSet)]
         public string Tenant { get; set; }
+
+        [Parameter(Mandatory = true, Position = 3, ValueFromPipelineByPropertyName = true, ParameterSetName = AccessTokenParameterSet)]
+        public string AccessToken { get; set; }
+
+        [Parameter(Mandatory = true, Position = 4, ValueFromPipelineByPropertyName = true, ParameterSetName = AccessTokenParameterSet)]
+        public string AccountId { get; set; }
 
         [Parameter(Mandatory = true, Position = 0, ValueFromPipelineByPropertyName = true, ParameterSetName = FileParameterSet)]
         public string Path { get; set; }
+
+        [Parameter(Mandatory = false)]
+        public SwitchParameter PassThru { get; set; }
         
         [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
         public override void ExecuteCmdlet()
         {
+            AzureProfile azureProfile = new AzureProfile();
+            ProfileClient profileClient = new ProfileClient(azureProfile);
+            if (Environment == null)
+            {
+                Environment = AzureEnvironment.PublicEnvironments["AzureCloud"];
+            }
+
+            switch (ParameterSetName)
+            {
+                case CertificateParameterSet:
+                    profileClient.InitializeProfile(Environment, new Guid(SubscriptionId), Certificate, 
+                        StorageAccount);
+                    break;
+                case CredentialsParameterSet:
+                    AzureAccount userAccount = new AzureAccount
+                    {
+                        Id = Credential.UserName
+                    };
+                    profileClient.InitializeProfile(Environment, new Guid(SubscriptionId), userAccount, 
+                        Credential.Password, StorageAccount);
+                    break;
+                case AccessTokenParameterSet:
+                    AzureAccount tokenAccount = new AzureAccount
+                    {
+                        Id = AccountId,
+                        Type = AzureAccount.AccountType.User
+                    };
+                    profileClient.InitializeProfile(Environment, new Guid(SubscriptionId), tokenAccount, 
+                        Credential.Password, StorageAccount);
+                    break;
+            }
+
+            if (PassThru)
+            {
+                WriteObject(azureProfile);
+            }
         }
     }
 }
