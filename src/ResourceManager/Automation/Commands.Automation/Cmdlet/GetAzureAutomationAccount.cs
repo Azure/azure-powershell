34044726b3a4ce5b6cb73b134ff2cba13306690e
safeermohammed <safeerm@microsoft.com>
2015-03-10 14:33:11 -0700

    ARM project file and Automation Account cmdlets

---
+++ src/ResourceManager/Automation/Commands.Automation/Cmdlet/GetAzureAutomationAccount.cs
@@ -0,0 +1,61 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Security.Permissions;
+using Microsoft.Azure.Commands.Automation.Common;
+using Microsoft.Azure.Commands.Automation.Model;
+using Microsoft.WindowsAzure.Commands.Utilities.Common;
+
+namespace Microsoft.Azure.Commands.Automation.Cmdlet
+{
+    /// <summary>
+    /// Gets azure automation accounts, filterd by automation account name and location.
+    /// </summary>
+    [Cmdlet(VerbsCommon.Get, "AzureAutomationAccount")]
+    [OutputType(typeof(AutomationAccount))]
+    public class GetAzureAutomationAccount : AzureAutomationBaseCmdlet
+    {
+        /// <summary>
+        /// Gets or sets the automation account name.
+        /// </summary>
+        [Parameter(Position = 1, Mandatory = false, ValueFromPipelineByPropertyName = true, HelpMessage = "The automation account name.")]
+        [Alias("AutomationAccountName")]
+        [ValidateNotNullOrEmpty]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Execute this cmdlet.
+        /// </summary>
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        public override void ExecuteCmdlet()
+        {
+            IEnumerable<AutomationAccount> ret = null;
+            if (this.ParameterSetName == AutomationCmdletParameterSets.ByAutomationAccountName)
+            {
+                ret = new List<AutomationAccount> 
+                { 
+                   this.AutomationClient.GetAutomationAccount(this.ResourceGroupName, this.Name)
+                };
+            }
+            else
+            {
+                ret = this.AutomationClient.ListAutomationAccounts(this.ResourceGroupName);
+            }
+
+            this.GenerateCmdletOutput(ret);
+        }
+    }
+}
---
+++ src/ResourceManager/Automation/Commands.Automation/Model/AutomationAccount.cs
@@ -0,0 +1,78 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Commands.Automation.Common;
+
+namespace Microsoft.Azure.Commands.Automation.Model
+{
+    using AutomationManagement = Microsoft.Azure.Management.Automation;
+
+    /// <summary>
+    /// The automation account.
+    /// </summary>
+    public class AutomationAccount
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AutomationAccount"/> class.
+        /// </summary>
+        /// <param name="resrouceGroupName">
+        /// The resource group name.
+        /// </param>
+        /// <param name="automationAccount">
+        /// The automation account.
+        /// </param>
+        public AutomationAccount(string resrouceGroupName, AutomationManagement.Models.AutomationAccount automationAccount)
+        {
+            Requires.Argument("ResrouceGroupName", resrouceGroupName).NotNull();
+            Requires.Argument("AutomationAccount", automationAccount).NotNull();
+
+            this.RerourceGroupName = resrouceGroupName;
+            this.AutomationAccountName = automationAccount.Name;
+            this.Location = automationAccount.Location;
+            this.Plan = automationAccount.Properties.Sku.Family;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AutomationAccount"/> class.
+        /// </summary>
+        public AutomationAccount()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the resource group name.
+        /// </summary>
+        public string RerourceGroupName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the automation account name.
+        /// </summary>
+        public string AutomationAccountName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the location.
+        /// </summary>
+        public string Location { get; set; }
+
+        /// <summary>
+        /// Gets or sets the state.
+        /// </summary>
+        public string State { get; set; }
+
+        /// <summary>
+        /// Gets or sets the plan.
+        /// </summary>
+        public string Plan { get; set; }
+    }
+}
