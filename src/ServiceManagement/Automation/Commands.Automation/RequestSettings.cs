4a1731c5fff48ac8eafa7a1a06194e8e76188c1f
safeermohammed <safeerm@microsoft.com>
2015-03-13 15:32:33 -0700

    client Request Id for Runbook cmdlets

---
+++ src/ServiceManagement/Automation/Commands.Automation/RequestSettings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Commands.Automation.Common;
+using Microsoft.WindowsAzure.Management.Automation;
+
+namespace Microsoft.Azure.Commands.Automation
+{
+    public class RequestSettings : IDisposable
+    {
+        private readonly AutomationManagementClient client;
+
+        public RequestSettings(IAutomationManagementClient automationClient)
+        {
+            client = ((AutomationManagementClient)automationClient);
+            client.HttpClient.DefaultRequestHeaders.Remove(Constants.ClientRequestIdHeaderName);
+            client.HttpClient.DefaultRequestHeaders.Add(Constants.ClientRequestIdHeaderName, Guid.NewGuid().ToString());
+        }
+        
+        public void Dispose()
+        {
+            client.HttpClient.DefaultRequestHeaders.Remove(Constants.ClientRequestIdHeaderName);
+        }
+    }
+}
