e4c41240aaa8aac8abc02790fbc5902e4fa617ce
huangpf <phuang@microsoft.com>
2015-03-23 23:02:14 -0700

    update

--- src/Common/Commands.ScenarioTests.Common/Common.ps1
+++ src/Common/Commands.ScenarioTests.Common/Common.ps1
@@ -307,7 +307,7 @@ function Wait-Seconds
 {
     param([int] $timeout)
     
-    [Microsoft.Azure.Test.TestUtilities]::Wait($timeout * 1000)
+    [Microsoft.WindowsAzure.Testing.TestUtilities]::Wait($timeout * 1000)
 }
 
 
