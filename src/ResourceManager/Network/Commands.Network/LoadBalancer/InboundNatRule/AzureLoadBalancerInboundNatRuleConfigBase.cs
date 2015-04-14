b2aa6b27da426025018f7054e859ed4351836303
deepakswifty <rajendran.deepak@outlook.com>
2015-04-14 14:52:55 -0700

    Rename NetworkResourceProvider project to Network

---
+++ src/ResourceManager/Network/Commands.Network/LoadBalancer/InboundNatRule/AzureLoadBalancerInboundNatRuleConfigBase.cs
@@ -0,0 +1,97 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Management.Automation;
+using Microsoft.Azure.Commands.Network.Models;
+using MNM = Microsoft.Azure.Management.Network.Models;
+
+namespace Microsoft.Azure.Commands.Network
+{
+    public class AzureLoadBalancerInboundNatRuleConfigBase : NetworkBaseCmdlet
+    {
+        [Parameter(
+            Mandatory = false,
+            HelpMessage = "The name of the Inbound NAT rule")]
+        [ValidateNotNullOrEmpty]
+        public virtual string Name { get; set; }
+
+        [Parameter(
+            ParameterSetName = "SetByResourceId",
+            HelpMessage = "IDs of the FrontendIpConfigurations")]
+        [ValidateNotNullOrEmpty]
+        public List<string> FrontendIPConfigurationId { get; set; }
+
+        [Parameter(
+            ParameterSetName = "SetByResourceId",
+            HelpMessage = "IPConfig ID of NetworkInterface")]
+        [ValidateNotNullOrEmpty]
+        public string BackendIpConfigurationId { get; set; }
+
+        [Parameter(
+             ParameterSetName = "SetByResource",
+             HelpMessage = "The list of frontend Ip config")]
+        [ValidateNotNullOrEmpty]
+        public List<PSFrontendIpConfiguration> FrontendIpConfiguration { get; set; }
+
+        [Parameter(
+            ParameterSetName = "SetByResource",
+            HelpMessage = "IPConfig of NetworkInterface")]
+        [ValidateNotNullOrEmpty]
+        public PSNetworkInterfaceIpConfiguration BackendIpConfiguration { get; set; }
+        
+        [Parameter(
+            Mandatory = false,
+            HelpMessage = "The transport protocol for the external endpoint.")]
+        [ValidateSet(MNM.TransportProtocol.Tcp, MNM.TransportProtocol.Udp, IgnoreCase = true)]
+        public string Protocol { get; set; }
+
+        [Parameter(
+            Mandatory = false,
+            HelpMessage = "The frontend port")]
+        public int FrontendPort { get; set; }
+
+        [Parameter(
+            Mandatory = false,
+            HelpMessage = "The frontend port")]
+        public int BackendPort { get; set; }
+
+        [Parameter(
+            Mandatory = false,
+            HelpMessage = "IdleTimeoutInMinutes")]
+        public int IdleTimeoutInMinutes { get; set; }
+
+        [Parameter(
+            Mandatory = false,
+            HelpMessage = "EnableFloatingIP")]
+        public SwitchParameter EnableFloatingIP { get; set; }
+
+        public override void ExecuteCmdlet()
+        {
+            base.ExecuteCmdlet();
+
+            if (string.Equals(ParameterSetName, Microsoft.Azure.Commands.Network.Properties.Resources.SetByResource))
+            {
+                this.BackendIpConfigurationId = this.BackendIpConfiguration.Id;
+                
+                this.FrontendIPConfigurationId = new List<string>();
+
+                foreach (var frontendIpConfiguration in this.FrontendIpConfiguration)
+                {
+                    this.FrontendIPConfigurationId.Add(frontendIpConfiguration.Id);
+                }
+            }
+        }
+    }
+}
---
+++ src/ResourceManager/Network/Commands.Network/LoadBalancer/InboundNatRule/SetAzureLoadBalancerInboundNatRuleConfigCommand.cs
@@ -0,0 +1,73 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using Microsoft.Azure.Commands.Network.Models;
+using MNM = Microsoft.Azure.Management.Network.Models;
+
+namespace Microsoft.Azure.Commands.Network
+{
+    [Cmdlet(VerbsCommon.Set, "AzureLoadBalancerInboundNatRuleConfig"), OutputType(typeof(PSFrontendIpConfiguration))]
+    public class SetAzureLoadBalancerInboundNatRuleConfigCommand : AzureLoadBalancerInboundNatRuleConfigBase
+    {
+        [Parameter(
+            Mandatory = true,
+            HelpMessage = "The name of the Inbound NAT rule")]
+        [ValidateNotNullOrEmpty]
+        public override string Name { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            ValueFromPipeline = true,
+            HelpMessage = "The load balancer")]
+        public PSLoadBalancer LoadBalancer { get; set; }
+
+        public override void ExecuteCmdlet()
+        {
+            base.ExecuteCmdlet();
+
+            var inboundNatRule = this.LoadBalancer.InboundNatRules.SingleOrDefault(resource => string.Equals(resource.Name, this.Name, System.StringComparison.CurrentCultureIgnoreCase));
+
+            if (inboundNatRule == null)
+            {
+                throw new ArgumentException("InboundNatRule with the specified name does not exist");
+            }
+
+            inboundNatRule.Name = this.Name;
+            inboundNatRule.Protocol = this.Protocol;
+            inboundNatRule.FrontendPort = this.FrontendPort;
+            inboundNatRule.BackendPort = this.BackendPort;
+            if (this.IdleTimeoutInMinutes > 0)
+            {
+                inboundNatRule.IdleTimeoutInMinutes = this.IdleTimeoutInMinutes;
+            }
+            inboundNatRule.EnableFloatingIP = this.EnableFloatingIP.IsPresent;
+            inboundNatRule.BackendIPConfiguration = new PSResourceId();
+            inboundNatRule.BackendIPConfiguration.Id = this.BackendIpConfigurationId;
+            inboundNatRule.FrontendIPConfigurations = new List<PSResourceId>();
+
+            foreach (var frontendIPConfigurationId in this.FrontendIPConfigurationId)
+            {
+                var resourceId = new PSResourceId();
+                resourceId.Id = frontendIPConfigurationId;
+                inboundNatRule.FrontendIPConfigurations.Add(resourceId);
+            }
+
+            WriteObject(this.LoadBalancer);
+        }
+    }
+}
