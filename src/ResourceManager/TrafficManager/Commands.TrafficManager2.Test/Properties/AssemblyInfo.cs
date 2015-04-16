b10643ca13721c7c2340b3dfba98de13ebdc223d
t-alguer <t-alguer@microsoft.com>
2015-04-17 00:50:47 +0100

    Addressing last comments

--- src/ResourceManager/TrafficManager/Commands.TrafficManager2.Test/Properties/AssemblyInfo.cs
+++ src/ResourceManager/TrafficManager/Commands.TrafficManager2.Test/Properties/AssemblyInfo.cs
@@ -18,10 +18,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Microsoft.Azure.Commands.TrafficManager.Test")]
-[assembly: AssemblyCompany(Microsoft.WindowsAzure.Commands.Common.AzurePowerShell.AssemblyCompany)]
-[assembly: AssemblyProduct(Microsoft.WindowsAzure.Commands.Common.AzurePowerShell.AssemblyProduct)]
-[assembly: AssemblyCopyright("Copyright (c) 2014")]
+[assembly: AssemblyTitle("Microsoft.Azure.Commands.Network.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Microsoft.Azure.Commands.TrafficManager.Test")]
+[assembly: AssemblyCopyright(Microsoft.WindowsAzure.Commands.Common.AzurePowerShell.AssemblyCopyright)]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
