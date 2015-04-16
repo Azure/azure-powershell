2f175d970a9edd59928815b103b1cb8d29debedb
t-alguer <t-alguer@microsoft.com>
2015-04-17 00:57:43 +0100

    Ups

--- src/ResourceManager/TrafficManager/Commands.TrafficManager2.Test/Properties/AssemblyInfo.cs
+++ src/ResourceManager/TrafficManager/Commands.TrafficManager2.Test/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Microsoft.Azure.Commands.Network.Test")]
+[assembly: AssemblyTitle("Microsoft.Azure.Commands.TrafficManager.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
