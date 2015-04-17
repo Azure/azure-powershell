f5ca88a7b007482a7ca36c2bfa9e4f2942b36afd
Ed Munoz <edmunoz@microsoft.com>
2015-04-16 17:02:15 -0700

    Add IPForwarding scenario test for NIC

--- src/ServiceManagement/Network/Commands.Network.Test/ScenarioTests/IPForwarding/IPForwardingTests.ps1
+++ src/ServiceManagement/Network/Commands.Network.Test/ScenarioTests/IPForwarding/IPForwardingTests.ps1
@@ -43,3 +43,33 @@ function Test-SetIPForwardingOnVMAndUpdateVM
     $ipForwarding = Get-AzureVM $name | Get-AzureIPForwarding
     Assert-AreEqual "Enabled" $ipForwarding
 }
+
+<#
+.SYNOPSIS
+Executes Get and Update-AzureVM does not clear IPForwarding on NIC.
+#>
+
+function Test-SetIPForwardingOnNICAndUpdateVM
+{
+    # Setup
+    Set-AzureVNetConfig ($(Get-Location).Path +  "\TestData\SimpleNetworkConfiguration.xml")
+    $name = getAssetName
+    $image = get-azurevmimage | Where-Object {$_.OS -eq 'Windows'} | Select-Object -First 1 -ExpandProperty ImageName
+
+    # Test
+    New-AzureVMConfig -ImageName $image -Name $name -InstanceSize Small |
+    Add-AzureProvisioningConfig -Windows -AdminUsername azuretest -Password "Pa@!!w0rd" |
+    Set-AzureSubnet -SubnetNames $SubnetName |
+    New-AzureVM -VNetName $VirtualNetworkName -ServiceName $name -Location $Location
+
+    Get-AzureVM $name | Set-AzureIPForwarding -Enable
+
+    # update VM
+    Get-AzureVM $name |
+    Add-AzureEndpoint -Name "http" -Protocol tcp -LocalPort 80 -PublicPort 80 |
+    Update-AzureVM
+
+    # Assert
+    $ipForwarding = Get-AzureVM $name | Get-AzureIPForwarding
+    Assert-AreEqual "Enabled" $ipForwarding
+}
