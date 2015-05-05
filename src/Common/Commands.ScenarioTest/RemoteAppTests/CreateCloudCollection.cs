cff97c66b4fc9c16497b9ae82196c50ea284e4e7
Bob Landau <robertla@microsoft.com>
2015-05-05 11:38:03 -0700

    Adding Checkin Tests to Azure RemoteApp

--- src/Common/Commands.ScenarioTest/RemoteAppTests/CreateCloudCollection.cs
+++ src/Common/Commands.ScenarioTest/RemoteAppTests/CreateCloudCollection.cs
@@ -2,35 +2,17 @@
 using Microsoft.Azure.Test;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Management.Automation;
 using System.IO;
 using System.Linq;
+using System.Management.Automation;
 using Xunit;
 
-using Microsoft.Azure.Management.RemoteApp;
 
 namespace Microsoft.WindowsAzure.Commands.ScenarioTest.RemoteAppTests
 {
     
     public class CreateCloudCollection
     {
-        private EnvironmentSetupHelper helper = null;
-
-        protected void SetupManagementClients()
-        {
-            StorageManagementClient managedCacheClient = GetManagedCacheClient();
-	    helper = new EnvironmentSetupHelper();
-            helper.SetupManagementClients(managedCacheClient);
-            helper.SetupSomeOfManagementClients();
-        }
-
-
-        protected void SetupAzureEnvironment()
-        {
-            EnvironmentSetupHelper helper = new EnvironmentSetupHelper();
-            helper.SetupEnvironment(AzureModule.AzureServiceManagement);
-        }
-
         protected Collection<T> RunPowerShellTest<T>(params string[] scripts)
         {
             using (UndoContext context = UndoContext.Current)
@@ -59,9 +41,9 @@ namespace Microsoft.WindowsAzure.Commands.ScenarioTest.RemoteAppTests
 
         [Fact]
         [Trait(Category.AcceptanceType, Category.CheckIn)]
-        public void CheckinTest()
+        public void TestRemoteAppEndToEnd()
         {
-            RunPowerShellTest<string>("CheckinTest");
+            RunPowerShellTest<string>("TestRemoteAppEndToEnd");
         }
     }
 }
