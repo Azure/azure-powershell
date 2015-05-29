fc61f75323e90d5c9875b60d706e9766ebac32f6
dragonfly91 <anudeeb@microsoft.com>
2015-05-29 12:04:11 +0530

    Renamed InstanceId to ContainerId due to conflicts with top level objects.

--- src/ResourceManager/AzureBackup/Commands.AzureBackup/Models/AzureBackupContainer.cs
+++ src/ResourceManager/AzureBackup/Commands.AzureBackup/Models/AzureBackupContainer.cs
@@ -43,7 +43,7 @@ namespace Microsoft.Azure.Commands.AzureBackup.Cmdlets
         /// <summary>
         /// Id of the Azure Backup Container
         /// </summary>
-        public string InstanceId { get; set; }
+        public string ContainerId { get; set; }
 
         /// <summary>
         /// Name of the Azure Backup container
