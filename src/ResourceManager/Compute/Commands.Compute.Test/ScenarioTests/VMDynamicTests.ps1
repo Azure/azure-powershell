d15435525b835343f32bfe9c34fe57ede64cf4b7
huangpf <phuang@microsoft.com>
2015-06-01 16:34:05 -0700

    Address CR and not use canonical form for location strings

--- src/ResourceManager/Compute/Commands.Compute.Test/ScenarioTests/VMDynamicTests.ps1
+++ src/ResourceManager/Compute/Commands.Compute.Test/ScenarioTests/VMDynamicTests.ps1
@@ -16,11 +16,6 @@ function get_all_vm_locations
 {
     $locations = Get-AzureLocation | where { $_.Name -like 'Microsoft.Compute/virtualMachines' } | select -ExpandProperty Locations;
 
-    for ($i = 0; $i -lt $locations.Count; $i++)
-    {
-        $locations[$i] = ($locations[$i] -Replace ' ', '').ToLower();
-    }
-
     return $locations;
 }
 
