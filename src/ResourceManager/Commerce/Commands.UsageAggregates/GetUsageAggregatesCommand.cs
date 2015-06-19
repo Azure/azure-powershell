db579db6484c9c0e9b95b67efa0401024d1fa20f
Matt Westphal <matt.westphal@microsoft.com>
2015-06-19 16:48:29 -0700

    updated help file correctly for Get-UsageAggregates

--- src/ResourceManager/Commerce/Commands.UsageAggregates/GetUsageAggregatesCommand.cs
+++ src/ResourceManager/Commerce/Commands.UsageAggregates/GetUsageAggregatesCommand.cs
@@ -28,25 +28,25 @@ namespace Microsoft.Azure.Commands.UsageAggregates
     public class GetUsageAggregatesCommand : AzurePSCmdlet
     {
         private UsageAggregationManagementClient _theClient;
-
+        private AggregationGranularity _aggregationGranularity = AggregationGranularity.Daily;
+        private bool _showDetails = true;
+     
         [Parameter(Mandatory = true, HelpMessage = "The start of the time range to retrieve data for.")]
         public DateTime ReportedStartTime { get; set; }
 
         [Parameter(Mandatory = true, HelpMessage = "The end of the time range to retrieve data for.")]
         public DateTime ReportedEndTime { get; set; }
 
-        private AggregationGranularity aggregationGranularity = AggregationGranularity.Daily;
         [Parameter(Mandatory = false, HelpMessage = "Value is either daily (default) or hourly to tell the API how to return the results grouped by day or hour.")]
         public AggregationGranularity AggregationGranularity { 
-            get { return aggregationGranularity; } 
-            set { aggregationGranularity = value;}
+            get { return _aggregationGranularity; } 
+            set { _aggregationGranularity = value;}
         }
 
-        private bool showDetails = true;
         [Parameter(Mandatory = false, HelpMessage = "When set to true (default), the aggregates are broken down into the instance metadata which is more granular.")]
         public bool ShowDetails {
-            get { return showDetails; }
-            set { showDetails = value; }
+            get { return _showDetails; }
+            set { _showDetails = value; }
         }
 
         [Parameter(Mandatory = false, HelpMessage = "Retrieved from previous calls, this is the bookmark used for progress when the responses are paged.")]
