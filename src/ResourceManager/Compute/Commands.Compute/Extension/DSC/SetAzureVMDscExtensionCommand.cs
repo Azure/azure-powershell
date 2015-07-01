d99623c5411ed425d2bc2c9bc71c2ff5c3a38f4b
Esha Parmar <eshaparmar@gmail.com>
2015-07-01 10:46:29 -0700

    Set-AzureVMDscExtension ARM cmdlet

---
+++ src/ResourceManager/Compute/Commands.Compute/Extension/DSC/SetAzureVMDscExtensionCommand.cs
@@ -0,0 +1,395 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.IO;
+using System.Management.Automation;
+using System.Text.RegularExpressions;
+using AutoMapper;
+using Microsoft.Azure.Commands.Compute.Common;
+using Microsoft.Azure.Commands.Compute.Models;
+using Microsoft.Azure.Management.Compute;
+using Microsoft.Azure.Management.Compute.Models;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Microsoft.Azure.Commands.Compute.Extension.DSC
+{
+    [Cmdlet(
+        VerbsCommon.Set,
+        ProfileNouns.VirtualMachineDscExtension,
+        SupportsShouldProcess = true,
+        DefaultParameterSetName = AzureBlobDscExtensionParamSet)]
+    [OutputType(typeof(PSComputeLongRunningOperation))]
+    class SetAzureVMDscExtensionCommand : VirtualMachineDscExtensionBaseCmdlet
+    {
+        protected const string AzureBlobDscExtensionParamSet = "AzureBlobDscExtension";
+
+        [Parameter(
+           Mandatory = true,
+           Position = 2,
+           ValueFromPipelineByPropertyName = true,
+           HelpMessage = "Name of the resource group where virtual machine resource exist.")]
+        [ValidateNotNullOrEmpty]
+        public string ResourceGroupName { get; set; }
+
+        [Alias("ResourceName")]
+        [Parameter(
+            Mandatory = true,
+            Position = 3,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "Name of the virtual machine where dsc extension handler would be installed.")]
+        [ValidateNotNullOrEmpty]
+        public string VMName { get; set; }
+
+        [Alias("ExtensionName")]
+        [Parameter(
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "Name of the DSC Extension handler. It will default to 'DSC' when it is not provided.")]
+        [ValidateNotNullOrEmpty]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The name of the configuration archive that was previously uploaded by 
+        /// Publish-AzureVMDSCConfiguration. This parameter must specify only the name 
+        /// of the file, without any path.
+        /// A null value or empty string indicate that the VM extension should install DSC,
+        /// but not start any configuration
+        /// </summary>
+        [Parameter(
+            Mandatory = true, 
+            Position = 1, 
+            ValueFromPipelineByPropertyName = true,
+            ParameterSetName = AzureBlobDscExtensionParamSet,
+            HelpMessage = "The name of the configuration file that was previously uploaded by Publish-AzureVMDSCConfiguration")]
+        [AllowEmptyString]
+        [AllowNull]
+        public string ConfigurationArchiveBlob { get; set; }
+
+        /// <summary>
+        /// The Azure Storage Account name used to upload the configuration script to the container specified by ContainerName. 
+        /// </summary>
+        [Parameter(
+            Mandatory = true,
+            Position = 4,
+            ValueFromPipelineByPropertyName = true,
+            ParameterSetName = AzureBlobDscExtensionParamSet,
+            HelpMessage = "The Azure Storage Account name used to download the ConfigurationArchiveBlob")]
+        [ValidateNotNullOrEmpty]
+        public String StorageAccountName { get; set; }
+
+        /// <summary>
+        /// The DNS endpoint suffix for all storage services, e.g. "core.windows.net".
+        /// </summary>
+        [Parameter(
+            ValueFromPipelineByPropertyName = true,
+            ParameterSetName = AzureBlobDscExtensionParamSet,
+            HelpMessage = "The Storage Endpoint Suffix.")]
+        [ValidateNotNullOrEmpty]
+        public string StorageEndpointSuffix { get; set; }
+
+        /// <summary>
+        /// Name of the Azure Storage Container where the configuration script is located.
+        /// </summary>
+        [Parameter(
+            ValueFromPipelineByPropertyName = true,
+            ParameterSetName = AzureBlobDscExtensionParamSet,
+            HelpMessage = "Name of the Azure Storage Container where the configuration script is located")]
+        [ValidateNotNullOrEmpty]
+        public string ContainerName { get; set; }
+
+        /// <summary>
+        /// Name of the configuration that will be invoked by the DSC Extension. The value of this parameter should be the name of one of the configurations 
+        /// contained within the file specified by ConfigurationArchiveBlob.
+        /// 
+        /// If omitted, this parameter will default to the name of the file given by the ConfigurationArchiveBlob parameter, excluding any extension, for example if 
+        /// ConfigurationArchiveBlob is "SalesWebSite.ps1", the default value for ConfigurationName will be "SalesWebSite".
+        /// </summary>
+        [Parameter(
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "Name of the configuration that will be invoked by the DSC Extension")]
+        [ValidateNotNullOrEmpty]
+        public string ConfigurationName { get; set; }
+
+        /// <summary>
+        /// A hashtable specifying the arguments to the ConfigurationFunction. The keys 
+        /// correspond to the parameter names and the values to the parameter values.
+        /// </summary>
+        [Parameter(
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "A hashtable specifying the arguments to the ConfigurationFunction")]
+        [ValidateNotNullOrEmpty]
+        public Hashtable ConfigurationArgument { get; set; }
+
+        /// <summary>
+        /// Path to a .psd1 file that specifies the data for the Configuration. This 
+        /// file must contain a hashtable with the items described in 
+        /// http://technet.microsoft.com/en-us/library/dn249925.aspx.
+        /// </summary>
+        [Parameter(
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "Path to a .psd1 file that specifies the data for the Configuration")]
+        [ValidateNotNullOrEmpty]
+        public string ConfigurationData { get; set; }
+
+        /// <summary>
+        /// The specific version of the DSC extension that Set-AzureVMDSCExtension will 
+        /// apply the settings to. 
+        /// </summary>
+        [Alias("HandlerVersion", "Version")]
+        [Parameter(
+            Mandatory = true,
+            Position = 5,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The version of the DSC extension that Set-AzureVMDSCExtension will apply the settings to. " +
+                          "Allowed format N.N")]
+        [ValidateNotNullOrEmpty]
+        public string Version { get; set; }
+
+        /// <summary>
+        /// By default Set-AzureVMDscExtension will not overwrite any existing blobs. Use -Force to overwrite them.
+        /// </summary>
+        [Parameter(
+            HelpMessage = "Use this parameter to overwrite any existing blobs")]
+        public SwitchParameter Force { get; set; }
+
+        [Parameter(
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "Location of the resource.")]
+        [ValidateNotNullOrEmpty]
+        public string Location { get; set; }
+
+        //Private Variables
+
+        private const string VersionRegexExpr = @"^(([0-9])\.)([0-9])$";
+
+        /// <summary>
+        /// Credentials used to access Azure Storage
+        /// </summary>
+        private StorageCredentials _storageCredentials;
+
+        public override void ExecuteCmdlet()
+        {
+            base.ExecuteCmdlet();
+
+            ValidateParameters();
+
+            CreateConfiguration();
+        }
+
+        //Private Methods
+        private void ValidateParameters()
+        {
+            if (string.IsNullOrEmpty(ConfigurationArchiveBlob))
+            {
+                if (ConfigurationName != null || ConfigurationArgument != null
+                    || ConfigurationData != null)
+                {
+                    ThrowInvalidArgumentError(Properties.Resources.AzureVMDscNullArchiveNoConfiguragionParameters);
+                }
+                if (ContainerName != null || StorageEndpointSuffix != null)
+                {
+                    ThrowInvalidArgumentError(Properties.Resources.AzureVMDscNullArchiveNoStorageParameters);
+                }
+            }
+            else
+            {
+                if (string.Compare(
+                    Path.GetFileName(ConfigurationArchiveBlob),
+                    ConfigurationArchiveBlob,
+                    StringComparison.InvariantCultureIgnoreCase) != 0)
+                {
+                    ThrowInvalidArgumentError(Properties.Resources.AzureVMDscConfigurationDataFileShouldNotIncludePath);
+                }
+
+                if (ConfigurationData != null)
+                {
+                    ConfigurationData = GetUnresolvedProviderPathFromPSPath(ConfigurationData);
+
+                    if (!File.Exists(ConfigurationData))
+                    {
+                        ThrowInvalidArgumentError(
+                            Properties.Resources.AzureVMDscCannotFindConfigurationDataFile,
+                            ConfigurationData);
+                    }
+                    if (string.Compare(
+                        Path.GetExtension(ConfigurationData),
+                        ".psd1",
+                        StringComparison.InvariantCultureIgnoreCase) != 0)
+                    {
+                        ThrowInvalidArgumentError(Properties.Resources.AzureVMDscInvalidConfigurationDataFile);
+                    }
+                }
+
+                _storageCredentials = GetStorageCredentials(ResourceGroupName, StorageAccountName);
+
+                if (ConfigurationName == null)
+                {
+                    ConfigurationName = Path.GetFileNameWithoutExtension(ConfigurationArchiveBlob);
+                }
+
+                if (ContainerName == null)
+                {
+                    ContainerName = DefaultContainerName;
+                }
+
+                if (Regex.Match(Version, VersionRegexExpr).Success)
+                {
+                    ThrowInvalidArgumentError(Properties.Resources.AzureVMDscExtensionInvalidVersion);
+                }
+            }
+        }
+
+        private void CreateConfiguration()
+        {
+            var publicSettings = new DscExtensionPublicSettings();
+            var privateSettings = new DscExtensionPrivateSettings();
+            
+            if (!string.IsNullOrEmpty(ConfigurationArchiveBlob))
+            {
+                ConfigurationUris configurationUris = UploadConfigurationDataToBlob();
+
+                publicSettings.SasToken = configurationUris.SasToken;
+                publicSettings.ModulesUrl = configurationUris.ModulesUrl;
+                publicSettings.ConfigurationFunction = string.Format(
+                    CultureInfo.InvariantCulture,
+                    "{0}\\{1}",
+                    Path.GetFileNameWithoutExtension(ConfigurationArchiveBlob),
+                    ConfigurationName);
+                Tuple<DscExtensionPublicSettings.Property[], Hashtable> settings =
+                    DscSettingsSerializer.SeparatePrivateItems(ConfigurationArgument);
+                publicSettings.Properties = settings.Item1;
+                privateSettings.Items = settings.Item2;
+
+                privateSettings.DataBlobUri = configurationUris.DataBlobUri;
+            }
+
+            var parameters = new VirtualMachineExtension
+            {
+                Location = Location,
+                Name = Name ?? ExtensionName,
+                Type = VirtualMachineExtensionType,
+                Publisher = ExtensionNamespace,
+                ExtensionType = ExtensionName,
+                TypeHandlerVersion = Version ?? "",
+                // Define the public and private property bags that will be passed to the extension.
+                Settings = DscSettingsSerializer.SerializePublicSettings(publicSettings),
+                //PrivateConfuguration contains sensitive data in a plain text
+                ProtectedSettings = DscSettingsSerializer.SerializePrivateSettings(privateSettings),
+            };
+
+            var op = VirtualMachineExtensionClient.CreateOrUpdate(
+                ResourceGroupName,
+                VMName,
+                parameters);
+
+            var result = Mapper.Map<PSComputeLongRunningOperation>(op);
+            WriteObject(result);
+        }
+
+        /// <summary>
+        /// Uploading configuration data to blob storage.
+        /// </summary>
+        /// <returns>ConfigurationUris collection that represent artifacts of uploading</returns>
+        private ConfigurationUris UploadConfigurationDataToBlob()
+        {
+            //
+            // Get a reference to the container in blob storage
+            //
+            var storageAccount = string.IsNullOrEmpty(StorageEndpointSuffix)
+                                     ? new CloudStorageAccount(_storageCredentials, true)
+                                     : new CloudStorageAccount(
+                                           _storageCredentials,
+                                           StorageEndpointSuffix,
+                                           true);
+
+            var blobClient = storageAccount.CreateCloudBlobClient();
+
+            var containerReference = blobClient.GetContainerReference(ContainerName);
+
+            //
+            // Get a reference to the configuration blob and create a SAS token to access it
+            //
+            var blobAccessPolicy = new SharedAccessBlobPolicy
+            {
+                SharedAccessExpiryTime =
+                    DateTime.UtcNow.AddHours(1),
+                Permissions = SharedAccessBlobPermissions.Read | SharedAccessBlobPermissions.Delete
+            };
+
+            var configurationBlobName = ConfigurationArchiveBlob;
+            var configurationBlobReference = containerReference.GetBlockBlobReference(configurationBlobName);
+            var configurationBlobSasToken = configurationBlobReference.GetSharedAccessSignature(blobAccessPolicy);
+
+            //
+            // Upload the configuration data to blob storage and get a SAS token
+            //
+            string configurationDataBlobUri = null;
+
+            if (ConfigurationData != null)
+            {
+                ConfirmAction(
+                    true,
+                    string.Empty,
+                    string.Format(
+                        CultureInfo.CurrentUICulture,
+                        Properties.Resources.AzureVMDscUploadToBlobStorageAction,
+                        ConfigurationData),
+                    configurationBlobReference.Uri.AbsoluteUri,
+                    () =>
+                    {
+                        var guid = Guid.NewGuid();
+                        // there may be multiple VMs using the same configuration
+
+                        var configurationDataBlobName = string.Format(
+                            CultureInfo.InvariantCulture,
+                            "{0}-{1}.psd1",
+                            ConfigurationName,
+                            guid);
+
+                        var configurationDataBlobReference =
+                            containerReference.GetBlockBlobReference(configurationDataBlobName);
+
+                        if (!Force && configurationDataBlobReference.Exists())
+                        {
+                            ThrowTerminatingError(
+                                new ErrorRecord(
+                                    new UnauthorizedAccessException(
+                                        string.Format(
+                                            CultureInfo.CurrentUICulture,
+                                            Properties.Resources.AzureVMDscStorageBlobAlreadyExists,
+                                            configurationDataBlobName)),
+                                    "StorageBlobAlreadyExists",
+                                    ErrorCategory.PermissionDenied,
+                                    null));
+                        }
+
+                        configurationDataBlobReference.UploadFromFile(ConfigurationData, FileMode.Open);
+
+                        var configurationDataBlobSasToken =
+                            configurationDataBlobReference.GetSharedAccessSignature(blobAccessPolicy);
+
+                        configurationDataBlobUri =
+                            configurationDataBlobReference.StorageUri.PrimaryUri.AbsoluteUri
+                            + configurationDataBlobSasToken;
+                    });
+            }
+            return new ConfigurationUris
+            {
+                ModulesUrl = configurationBlobReference.StorageUri.PrimaryUri.AbsoluteUri,
+                SasToken = configurationBlobSasToken,
+                DataBlobUri = configurationDataBlobUri
+            };
+        }
+
+        /// <summary>
+        /// Class represent info about uploaded Configuration.
+        /// </summary>
+        private class ConfigurationUris
+        {
+            public string SasToken { get; set; }
+            public string DataBlobUri { get; set; }
+            public string ModulesUrl { get; set; }
+        }
+    }
+}
--- src/ResourceManager/Compute/Commands.Compute/Extension/DSC/VirtualMachineDscExtensionBaseCmdlet.cs
+++ src/ResourceManager/Compute/Commands.Compute/Extension/DSC/VirtualMachineDscExtensionBaseCmdlet.cs
@@ -38,7 +38,7 @@ namespace Microsoft.Azure.Commands.Compute.Extension.DSC
             set { _storageClientWrapper = new StorageManagementClientWrapper(value); }
         }
 
-        public StorageCredentials GetStorageCredentials(String resourceGroupName, String storageAccountName)
+        protected StorageCredentials GetStorageCredentials(String resourceGroupName, String storageAccountName)
         {
             StorageCredentials credentials = null;
 
@@ -72,55 +72,8 @@ namespace Microsoft.Azure.Commands.Compute.Extension.DSC
             return credentials;
         }
 
-        /*
-         * Old Code: TODO: remove this soon
-        public StorageCredentials GetStorageCredentials(String resourceGroupName, AzureStorageContext storageContext)
-        {
-            StorageCredentials credentials = null;
-
-            if (storageContext != null)
-            {
-                credentials = storageContext.StorageAccount.Credentials;
-            }
-            else
-            {
-                var storageAccountName = Profile.Context.Subscription.GetProperty(AzureSubscription.Property.StorageAccount);
-                
-                if (!string.IsNullOrEmpty(storageAccountName) && StorageClient.StorageAccounts != null)
-                {
-
-                    var keys = StorageClient.StorageAccounts.ListKeys(resourceGroupName, storageAccountName);
-
-                    if (keys != null && keys.StorageAccountKeys != null)
-                    {
-                        var storageAccountKey = string.IsNullOrEmpty(keys.StorageAccountKeys.Key1) ? keys.StorageAccountKeys.Key2 : keys.StorageAccountKeys.Key1;
-
-                        credentials = new StorageCredentials(storageAccountName, storageAccountKey);
-                    }
-                }
-            }
-
-            if (credentials == null)
-            {
-                ThrowTerminatingError(
-                    new ErrorRecord(
-                        new UnauthorizedAccessException(Properties.Resources.AzureVMDscDefaultStorageCredentialsNotFound),
-                        "CredentialsNotFound",
-                        ErrorCategory.PermissionDenied,
-                        null));
-            }
-
-            if (string.IsNullOrEmpty(credentials.AccountName))
-            {
-                ThrowInvalidArgumentError(Properties.Resources.AzureVMDscStorageContextMustIncludeAccountName);
-            }
-
-            return credentials;
-        }
-        */
-
         //TODO: Move this to a common Exception class for DSC
-        public void ThrowInvalidArgumentError(string format, params object[] args)
+        protected void ThrowInvalidArgumentError(string format, params object[] args)
         {
             ThrowTerminatingError(
                 new ErrorRecord(
