aac87f947f9e26745a4d211d0d585369155e9e1f
Avijit Gupta <avgupt@microsoft.com>
2015-07-05 20:25:21 -0700

    Reserved IP mock added

---
+++ src/ServiceManagement/Network/Commands.Network.Test/ReservedIP/NewAzureReservedIPTest.cs
@@ -0,0 +1,222 @@
+﻿using System.Net;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.WindowsAzure.Commands.Common.Test.Mocks;
+using Microsoft.WindowsAzure.Commands.ScenarioTest;
+using Microsoft.WindowsAzure.Commands.ServiceManagement.IaaS;
+using Microsoft.WindowsAzure.Commands.ServiceManagement.Network.Test.TestInterfaces;
+using Microsoft.WindowsAzure.Commands.Utilities.Common;
+using Microsoft.WindowsAzure.Management.Compute;
+using Microsoft.WindowsAzure.Management.Network;
+using Moq;
+using Microsoft.WindowsAzure.Management.Compute.Models;
+using Microsoft.WindowsAzure.Management.Network.Models;
+using Microsoft.WindowsAzure.Management;
+using Microsoft.WindowsAzure.Management.Storage;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace Microsoft.WindowsAzure.Commands.ServiceManagement.Network.Test.ReservedIP
+{
+    public class NewAzureReservedIPTest
+    {
+        private MockCommandRuntime mockCommandRuntime;
+
+        public const string ServiceName = "ServiceName";
+        public const string DeploymentName = "DeploymentName";
+        public const string ReservedIPName = "ReservedIPName";
+        public const string ReservedIPLocation = "West US";
+        public const string ReservedIPLabel = "SomeLabel";
+        public const string VipName = "VipName";
+        private NewAzureReservedIPCmdlet cmdlet;
+        private NetworkClient client;
+        private Mock<NetworkManagementClient> networkingClientMock;
+        private Mock<ComputeManagementClient> computeClientMock;
+        private Mock<ManagementClient> managementClientMock;
+        private Mock<StorageManagementClient> storageClientMock;
+        private Mock<ServiceManagementBaseCmdlet> svcmgmt;
+        private IClientProvider testClientProvider;
+
+
+        public NewAzureReservedIPTest()
+        {
+            this.networkingClientMock = new Mock<NetworkManagementClient>();
+            this.computeClientMock = new Mock<ComputeManagementClient>();
+            this.managementClientMock = new Mock<ManagementClient>();
+            this.storageClientMock = new Mock<StorageManagementClient>();
+            this.mockCommandRuntime = new MockCommandRuntime();
+            
+            testClientProvider = new TestClientProvider(this.managementClientMock.Object,
+                this.computeClientMock.Object, this.storageClientMock.Object, this.networkingClientMock.Object);
+
+            this.computeClientMock
+                .Setup(c => c.Deployments.GetBySlotAsync(ServiceName, DeploymentSlot.Production, It.IsAny<CancellationToken>()))
+                .Returns(Task.Factory.StartNew(() => new DeploymentGetResponse()
+                {
+                    Name = DeploymentName
+                }));
+
+            // Reserve IP simple
+            this.networkingClientMock
+                .Setup(c => c.ReservedIPs.CreateAsync(
+                    It.Is<NetworkReservedIPCreateParameters>(
+                        p =>
+                            string.Equals(p.Name, ReservedIPName) && string.IsNullOrEmpty(p.ServiceName) &&
+                            string.IsNullOrEmpty(p.VirtualIPName) && string.IsNullOrEmpty(p.DeploymentName)),
+                    It.IsAny<CancellationToken>()))
+                .Returns(Task.Factory.StartNew(() => new OperationStatusResponse()
+                {
+                    Status = OperationStatus.Succeeded
+                }));
+
+            // Reserve in use IP single vip
+            this.networkingClientMock
+                .Setup(c => c.ReservedIPs.CreateAsync(
+                    It.Is<NetworkReservedIPCreateParameters>(
+                        p =>
+                            string.Equals(p.Name, ReservedIPName) && string.Equals(p.ServiceName, ServiceName) &&
+                            string.IsNullOrEmpty(p.VirtualIPName) && string.Equals(p.DeploymentName, DeploymentName)),
+                    It.IsAny<CancellationToken>()))
+                .Returns(Task.Factory.StartNew(() => new OperationStatusResponse()
+                {
+                    Status = OperationStatus.Succeeded
+                }));
+
+            // Reserve in use IP named vip
+            this.networkingClientMock
+                .Setup(c => c.ReservedIPs.CreateAsync(
+                    It.Is<NetworkReservedIPCreateParameters>(
+                        p =>
+                            string.Equals(p.Name, ReservedIPName) && string.Equals(p.ServiceName, ServiceName) &&
+                            string.Equals(p.VirtualIPName, VipName) && string.Equals(p.DeploymentName, DeploymentName)),
+                    It.IsAny<CancellationToken>()))
+                .Returns(Task.Factory.StartNew(() => new OperationStatusResponse()
+                {
+                    Status = OperationStatus.Succeeded
+                }));
+        }
+
+        [Fact]
+        [Trait(Category.Service, Category.Network)]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void NewAzureReservedIPSimple()
+        {
+            cmdlet = new NewAzureReservedIPCmdlet(testClientProvider)
+            {
+                ReservedIPName = ReservedIPName,
+                Location = "WestUS",
+                Label = ReservedIPLabel,
+                CommandRuntime = mockCommandRuntime,
+            };
+
+            cmdlet.SetParameterSet(NewAzureReservedIPCmdlet.ReserveNewIPParamSet);
+
+            // Action
+            cmdlet.ExecuteCmdlet();
+
+            // Assert
+            computeClientMock.Verify(
+                c => c.Deployments.GetBySlotAsync(
+                    ServiceName,
+                    DeploymentSlot.Production,
+                    It.IsAny<CancellationToken>()),
+                Times.Never);
+
+            networkingClientMock.Verify(
+                c => c.ReservedIPs.CreateAsync(
+                    It.Is<NetworkReservedIPCreateParameters>(
+                        p =>
+                            string.Equals(p.Name, ReservedIPName) && string.IsNullOrEmpty(p.ServiceName) &&
+                            string.IsNullOrEmpty(p.VirtualIPName) && string.IsNullOrEmpty(p.DeploymentName)),
+                    It.IsAny<CancellationToken>()),
+                Times.Once());
+
+            Assert.Equal(1, mockCommandRuntime.OutputPipeline.Count);
+            Assert.Equal("Succeeded", ((ManagementOperationContext)mockCommandRuntime.OutputPipeline[0]).OperationStatus);
+        }
+
+        [Fact]
+        [Trait(Category.Service, Category.Network)]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void ReservedExistingIPSingleVip()
+        {
+            cmdlet = new NewAzureReservedIPCmdlet(testClientProvider)
+            {
+                ReservedIPName = ReservedIPName,
+                Location = "WestUS",
+                ServiceName = ServiceName,
+                Label = ReservedIPLabel,
+                CommandRuntime = mockCommandRuntime,
+            };
+
+            cmdlet.SetParameterSet(NewAzureReservedIPCmdlet.ReserveInUseIPParamSet);
+
+            // Action
+            cmdlet.ExecuteCmdlet();
+
+            // Assert
+            computeClientMock.Verify(
+                c => c.Deployments.GetBySlotAsync(
+                    ServiceName,
+                    DeploymentSlot.Production,
+                    It.IsAny<CancellationToken>()),
+                Times.Once);
+
+            networkingClientMock.Verify(
+                c => c.ReservedIPs.CreateAsync(
+                    It.Is<NetworkReservedIPCreateParameters>(
+                        p =>
+                            string.Equals(p.Name, ReservedIPName) && string.Equals(p.ServiceName, ServiceName) &&
+                            string.IsNullOrEmpty(p.VirtualIPName) && string.Equals(p.DeploymentName, DeploymentName)),
+                    It.IsAny<CancellationToken>()),
+                Times.Once());
+
+            Assert.Equal(1, mockCommandRuntime.OutputPipeline.Count);
+            Assert.Equal("Succeeded", ((ManagementOperationContext)mockCommandRuntime.OutputPipeline[0]).OperationStatus);
+        }
+
+        [Fact]
+        [Trait(Category.Service, Category.Network)]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void ReserveExistingIPNamedVip()
+        {
+            cmdlet = new NewAzureReservedIPCmdlet(testClientProvider)
+            {
+                ReservedIPName = ReservedIPName,
+                Location = "WestUS",
+                ServiceName = ServiceName,
+                Label = ReservedIPLabel,
+                VirtualIPName = VipName,
+                CommandRuntime = mockCommandRuntime,
+            };
+
+            cmdlet.SetParameterSet(NewAzureReservedIPCmdlet.ReserveInUseIPParamSet);
+
+            // Action
+            cmdlet.ExecuteCmdlet();
+
+            // Assert
+            computeClientMock.Verify(
+                c => c.Deployments.GetBySlotAsync(
+                    ServiceName,
+                    DeploymentSlot.Production,
+                    It.IsAny<CancellationToken>()),
+                Times.Once);
+
+            networkingClientMock.Verify(
+                c => c.ReservedIPs.CreateAsync(
+                    It.Is<NetworkReservedIPCreateParameters>(
+                        p =>
+                            string.Equals(p.Name, ReservedIPName) && string.Equals(p.ServiceName, ServiceName) &&
+                            string.Equals(p.VirtualIPName, VipName) && string.Equals(p.DeploymentName, DeploymentName)),
+                    It.IsAny<CancellationToken>()),
+                Times.Once());
+
+            Assert.Equal(1, mockCommandRuntime.OutputPipeline.Count);
+            Assert.Equal("Succeeded", ((ManagementOperationContext)mockCommandRuntime.OutputPipeline[0]).OperationStatus);
+        }
+    }
+}
---
+++ src/ServiceManagement/Services/Commands.Utilities/Common/ClientProvider.cs
@@ -0,0 +1,57 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Common.Authentication.Models;
+using Microsoft.WindowsAzure.Management;
+using Microsoft.WindowsAzure.Management.Compute;
+using Microsoft.WindowsAzure.Management.Network;
+using Microsoft.WindowsAzure.Management.Storage;
+using Microsoft.Azure.Common.Authentication;
+
+namespace Microsoft.WindowsAzure.Commands.Utilities.Common
+{
+    public class ClientProvider : IClientProvider
+    {
+        private readonly AzureProfile profile;
+
+        public ClientProvider(AzureProfile azureProfile)
+        {
+            this.profile = azureProfile;
+        }
+
+        ManagementClient IClientProvider.CreateClient()
+        {
+            return AzureSession.ClientFactory.CreateClient<ManagementClient>(this.profile.Context,
+                AzureEnvironment.Endpoint.ServiceManagement);
+        }
+
+        ComputeManagementClient IClientProvider.CreateComputeClient()
+        {
+            return AzureSession.ClientFactory.CreateClient<ComputeManagementClient>(this.profile,
+                this.profile.Context.Subscription, AzureEnvironment.Endpoint.ServiceManagement);
+        }
+
+        StorageManagementClient IClientProvider.CreateStorageClient()
+        {
+            return AzureSession.ClientFactory.CreateClient<StorageManagementClient>(this.profile,
+                this.profile.Context.Subscription, AzureEnvironment.Endpoint.ServiceManagement);
+        }
+
+        NetworkManagementClient IClientProvider.CreateNetworkClient()
+        {
+            return AzureSession.ClientFactory.CreateClient<NetworkManagementClient>(this.profile,
+                this.profile.Context.Subscription, AzureEnvironment.Endpoint.ServiceManagement);
+        }
+    }
+}
