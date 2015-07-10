df363d96a6bc2326c351c9a562e231d2a389c39d
t-alguer <alguerra@microsoft.com>
2015-07-10 14:36:05 +0100

    Disable

--- src/ResourceManager/TrafficManager/Commands.TrafficManager2/Profile/DisableAzureTrafficManagerProfile.cs
+++ src/ResourceManager/TrafficManager/Commands.TrafficManager2/Profile/DisableAzureTrafficManagerProfile.cs
@@ -20,7 +20,7 @@ using ProjectResources = Microsoft.Azure.Commands.TrafficManager.Properties.Reso
 
 namespace Microsoft.Azure.Commands.TrafficManager
 {
-    [Cmdlet(VerbsLifecycle.Enable, "AzureTrafficManagerProfile"), OutputType(typeof(bool))]
+    [Cmdlet(VerbsLifecycle.Disable, "AzureTrafficManagerProfile"), OutputType(typeof(bool))]
     public class DisableAzureTrafficManagerProfile : TrafficManagerBaseCmdlet
     {
         [Parameter(Mandatory = true, HelpMessage = "The name of the profile.", ParameterSetName = "Fields")]
