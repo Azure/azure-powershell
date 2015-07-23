f0d10d440371206b6db950628044d38c2aab99ba
Esha Parmar <eshaparmar@gmail.com>
2015-07-23 11:32:59 -0700

    Removed commands.servicemanagement.extensions.test project and moved DSC tests to commands.servicemanagement.test

---
+++ src/ServiceManagement/Compute/Commands.ServiceManagement.Test/Resources/DSC/FourthCoffee.ps1
@@ -0,0 +1,49 @@
+﻿Configuration WebSite 
+{ 
+    Import-DscResource -Module xWebAdministration     
+        
+        # Install the IIS role  
+        WindowsFeature IIS  
+        {  
+            Ensure          = 'Present'  
+            Name            = 'Web-Server'  
+        }  
+  
+        # Install the ASP .NET 4.5 role  
+        WindowsFeature AspNet45  
+        {  
+            Ensure          = 'Present'  
+            Name            = 'Web-Asp-Net45'  
+        }  
+  
+        # Stop the default website  
+        xWebsite DefaultSite   
+        {  
+            Ensure          = 'Present'  
+            Name            = 'Default Web Site'  
+            State           = 'Stopped'  
+            PhysicalPath    = 'C:\inetpub\wwwroot'  
+            DependsOn       = '[WindowsFeature]IIS'  
+        }  
+  
+        # Copy the website content  
+        File WebContent  
+        {  
+            Ensure          = 'Present'  
+            SourcePath      = 'C:\Program Files\WindowsPowerShell\Modules\BakeryWebsite' 
+            DestinationPath = 'C:\inetpub\FourthCoffee' 
+            Recurse         = $true  
+            Type            = 'Directory'  
+            DependsOn       = '[WindowsFeature]AspNet45'  
+        }   
+
+        # Create a new website  
+        xWebsite BakeryWebSite   
+        {  
+            Ensure          = 'Present'  
+            Name            = 'FourthCoffee' 
+            State           = 'Started'  
+            PhysicalPath    = 'C:\inetpub\FourthCoffee'  
+            DependsOn       = '[File]WebContent'  
+        } 
+} 
