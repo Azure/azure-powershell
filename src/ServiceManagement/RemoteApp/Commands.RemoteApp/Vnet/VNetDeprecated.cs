8e57dd8a800ddc613da437e3e86833828cb9e024
Bob Landau <robertla@microsoft.com>
2015-08-06 13:42:02 -0700

    Deprecating VNets

---
+++ src/ServiceManagement/RemoteApp/Commands.RemoteApp/Vnet/VNetDeprecated.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.WindowsAzure.Management.RemoteApp;
+using Microsoft.WindowsAzure.Management.RemoteApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Reflection;
+
+namespace Microsoft.WindowsAzure.Management.RemoteApp.Cmdlets
+{
+    public class VNetDeprecated : RdsCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            string message = String.Format("This cmdlet {0} has been deprecated. See x {1}",
+                this.GetType().Name, "https://azure.microsoft.com/documentation/articles/remoteapp-migratevnet/");
+            throw new RemoteAppServiceException(message, ErrorCategory.InvalidOperation);
+        }
+    }
+}
