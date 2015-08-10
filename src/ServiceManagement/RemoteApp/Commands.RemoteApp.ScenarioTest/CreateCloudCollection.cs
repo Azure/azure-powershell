edbc5c8ea2d3e1f148dfad1770da039f49c72706
Bob Landau <robertla@microsoft.com>
2015-08-10 06:25:37 -0700

    Updating code based off of the review

--- src/ServiceManagement/RemoteApp/Commands.RemoteApp.ScenarioTest/CreateCloudCollection.cs
+++ src/ServiceManagement/RemoteApp/Commands.RemoteApp.ScenarioTest/CreateCloudCollection.cs
@@ -1,4 +1,19 @@
-﻿using Microsoft.Azure.Common.Authentication;
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+
+using Microsoft.Azure.Common.Authentication;
 using Microsoft.Azure.Test;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -23,8 +38,7 @@ namespace Microsoft.WindowsAzure.Commands.ScenarioTest.RemoteAppTests
                 Collection<T> result = new Collection<T>();
                 EnvironmentSetupHelper helper = new EnvironmentSetupHelper();
 
-                modules = Directory.GetFiles(@"ServiceManagement\Azure\RemoteApp", "*.ps1").ToList();
-
+                modules = Directory.GetFiles(@"..\..\Scripts", "*.ps1").ToList();
                 helper.SetupSomeOfManagementClients();
                 helper.SetupEnvironment(AzureModule.AzureServiceManagement);
                 helper.SetupModules(AzureModule.AzureServiceManagement, modules.ToArray());
@@ -43,7 +57,7 @@ namespace Microsoft.WindowsAzure.Commands.ScenarioTest.RemoteAppTests
         [Trait(Category.AcceptanceType, Category.CheckIn)]
         public void TestRemoteAppEndToEnd()
         {
-            System.Environment.SetEnvironmentVariable("rdfeNameSpace", "rdst15");
+            System.Environment.SetEnvironmentVariable("rdfeNameSpace", "rdsr8");
             RunPowerShellTest<string>("TestRemoteAppEndToEnd");
         }
     }
