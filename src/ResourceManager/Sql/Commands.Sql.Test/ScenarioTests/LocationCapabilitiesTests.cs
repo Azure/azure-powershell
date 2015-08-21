124fdd35621ca05578f1924e20f6e1ef7408801f
Adam Kromm <adamkromm@outlook.com>
2015-08-21 11:16:13 -0700

    Fixing test name.

--- src/ResourceManager/Sql/Commands.Sql.Test/ScenarioTests/LocationCapabilitiesTests.cs
+++ src/ResourceManager/Sql/Commands.Sql.Test/ScenarioTests/LocationCapabilitiesTests.cs
@@ -24,7 +24,7 @@ namespace Microsoft.Azure.Commands.Sql.Test.ScenarioTests
         [Trait(Category.AcceptanceType, Category.CheckIn)]
         public void TestLocationCapabilities()
         {
-            RunPowerShellTest("Test-LocationCapabilities");
+            RunPowerShellTest("Test-Capabilities");
         }
     }
 }
