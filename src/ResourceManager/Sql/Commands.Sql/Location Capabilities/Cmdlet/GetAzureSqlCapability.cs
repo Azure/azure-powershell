6d75b5d3fda987a51d2c91132b3322b9bdd225de
Adam Kromm <adamkromm@outlook.com>
2015-08-24 16:02:18 -0700

    Adding custom output for Capabilities cmdlet
    When filtering the results of the Capabilites cmdlet, custom output is provided.

--- src/ResourceManager/Sql/Commands.Sql/Location Capabilities/Cmdlet/GetAzureSqlCapability.cs
+++ src/ResourceManager/Sql/Commands.Sql/Location Capabilities/Cmdlet/GetAzureSqlCapability.cs
@@ -17,6 +17,7 @@ using Microsoft.Azure.Commands.Sql.Location_Capabilities.Model;
 using Microsoft.Azure.Commands.Sql.Location_Capabilities.Services;
 using Microsoft.WindowsAzure.Commands.Utilities.Common;
 using System.Linq;
+using System.Text;
 
 namespace Microsoft.Azure.Commands.Sql.Location_Capabilities.Cmdlet
 {
@@ -90,12 +91,14 @@ namespace Microsoft.Azure.Commands.Sql.Location_Capabilities.Cmdlet
         {
             AzureSqlCapabilitiesAdapter adapter = new AzureSqlCapabilitiesAdapter(Profile, Profile.Context.Subscription);
             LocationCapabilityModel model = adapter.GetLocationCapabilities(LocationName);
+            int depth = 0;
 
             switch(ParameterSetName)
             {
                 case _default:
                     {
                         FilterByDefaults(model);
+                        depth = 3;
                     }
                     break;
                 case _filtered:
@@ -103,20 +106,129 @@ namespace Microsoft.Azure.Commands.Sql.Location_Capabilities.Cmdlet
                         if (this.MyInvocation.BoundParameters.ContainsKey("ServerVersionName"))
                         {
                             FilterByServerVersion(model);
+                            depth = 1;
                         }
                         if (this.MyInvocation.BoundParameters.ContainsKey("EditionName"))
                         {
                             FilterByEditionName(model);
+                            depth = 2;
                         }
                         if (this.MyInvocation.BoundParameters.ContainsKey("ServiceObjectiveName"))
                         {
                             FilterByServiceObjectiveName(model);
+                            depth = 3;
                         }
                     }
                     break;
             }
 
-            this.WriteObject(model);
+            if(depth > 0)
+            {
+                model.ExpandedDetails = CreateExpandedDetails(model, depth);
+            }
+
+            this.WriteObject(model, true);
+        }
+
+        /// <summary>
+        /// Given a <see cref="LocationCapabilityModel"/> constructs a formatted string of its expanded details to the desired depth.
+        /// </summary>
+        /// <param name="model">The model details</param>
+        /// <param name="depth">The depth to expand to</param>
+        /// <returns>The formatted string containing the model details</returns>
+        private string CreateExpandedDetails(LocationCapabilityModel model, int depth)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            foreach(var version in model.SupportedServerVersions)
+            {
+                string versionInfo = GetVersionInformation(version);
+
+                if(depth > 1)
+                {
+                    ExpandEdition(depth, builder, version, versionInfo);
+                }
+                else
+                {
+                    builder.AppendLine(versionInfo);
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        /// <summary>
+        /// Formats all the supported editions in <paramref name="version"/> as strings prefixed with <paramref name="prefix"/>
+        /// and appends them to the <paramref name="builder"/>
+        /// </summary>
+        /// <param name="depth">How deep to expand the information</param>
+        /// <param name="builder">The string builder to append the information</param>
+        /// <param name="version">The version object to expand and format</param>
+        /// <param name="prefix">The prefix to apply to the information strings</param>
+        private void ExpandEdition(int depth, StringBuilder builder, ServerVersionCapabilityModel version, string prefix)
+        {
+            foreach (var edition in version.SupportedEditions)
+            {
+                string editionInfo = GetEditionInformation(prefix, edition);
+
+                if (depth > 2)
+                {
+                    ExpandServiceObjective(builder, edition, editionInfo);
+                }
+                else
+                {
+                    builder.AppendLine(editionInfo);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Formats all the supported service objectives in <paramref name="edition"/> as strings prefixed with <paramref name="prefix"/>
+        /// and appends them to the <paramref name="builder"/>
+        /// </summary>
+        /// <param name="builder">The string building to add the formatted string to</param>
+        /// <param name="edition">The edition containing the supported service objectives</param>
+        /// <param name="prefix">The prefix for the formatted string</param>
+        private void ExpandServiceObjective(StringBuilder builder, EditionCapabilityModel edition, string prefix)
+        {
+            foreach (var slo in edition.SupportedServiceObjectives)
+            {
+                string serviceObjectiveInfo = GetServiceObjectiveInformation(prefix, slo);
+
+                builder.AppendLine(serviceObjectiveInfo);
+            }
+        }
+
+        /// <summary>
+        /// Gets the string formatting of the server version object
+        /// </summary>
+        /// <param name="version">The server version information to format as a string</param>
+        /// <returns>The formatted string containing the server version information</returns>
+        private string GetVersionInformation(ServerVersionCapabilityModel version)
+        {
+            return string.Format("Version: {0} ({1})", version.ServerVersionName, version.Status);
+        }
+
+        /// <summary>
+        /// Gets the string formatting of the edition object
+        /// </summary>
+        /// <param name="baseString">The prefix before the edition information</param>
+        /// <param name="edition">The edition information to format and append to the end of the baseString</param>
+        /// <returns>The formatted string containing the edition information</returns>
+        private string GetEditionInformation(string baseString, EditionCapabilityModel edition)
+        {
+            return string.Format("{0} -> Edition: {1} ({2})", baseString, edition.EditionName, edition.Status);
+        }
+
+        /// <summary>
+        /// Gets the string formatting of the service objective object
+        /// </summary>
+        /// <param name="baseString">The prefix before the service objective information</param>
+        /// <param name="objective">The service objective information to append</param>
+        /// <returns>The formatted string containing the service objective information</returns>
+        private string GetServiceObjectiveInformation(string baseString, ServiceObjectiveCapabilityModel objective)
+        {
+            return string.Format("{0} -> Service Objective: {1} ({2})", baseString, objective.ServiceObjectiveName, objective.Status);
         }
 
         /// <summary>
