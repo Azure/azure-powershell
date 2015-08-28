c128d414feaa2e49ddfa3190db9ef23a2ad16a15
Hyonho Lee <Hyonho.Lee@microsoft.com>
2015-08-28 01:12:03 -0700

    Adding Add-AzureVhd cmdlet for ARM

--- src/ResourceManager/Compute/Commands.Compute/Common/ConstantStringTypes.cs
+++ src/ResourceManager/Compute/Commands.Compute/Common/ConstantStringTypes.cs
@@ -97,5 +97,7 @@ namespace Microsoft.Azure.Commands.Compute.Common
         //DSC
         public const string VirtualMachineDscExtension = "AzureVMDscExtension";
         public const string VirtualMachineDscConfiguration = "AzureVMDscConfiguration";
+
+        public const string Vhd = "AzureVhd";
     }
 }
