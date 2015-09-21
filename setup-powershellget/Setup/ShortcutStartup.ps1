a4f609d59e41d90f58dc143f0b1e54381d1b3175
stankovski <denis.stankovski@gmail.com>
2015-09-21 11:51:56 -0700

    Addressed code review feedback

--- setup-powershellget/Setup/ShortcutStartup.ps1
+++ setup-powershellget/Setup/ShortcutStartup.ps1
@@ -11,28 +11,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ----------------------------------------------------------------------------------
+[CmdletBinding()]
+Param(
+[Parameter(Mandatory=$False, HelpMessage="Use Install parameter to install Azure modules from PowerShell Gallery.")]
+[switch]$Install
+)
 
-function Get-ScriptDirectory
-{
-    $Invocation = (Get-Variable MyInvocation -Scope 1).Value
-    Split-Path $Invocation.MyCommand.Path
-}
-
-$modulePath = Join-Path $(Split-Path (Get-ScriptDirectory)) "Azure.psd1"
-Import-Module $modulePath
 cd c:\
 $welcomeMessage = @"
 For a list of all Azure cmdlets type 'help azure'.
-For a list of Windows Azure Pack cmdlets type 'Get-Command *wapack*'.
-For Node.js cmdlets type 'help node-dev'.
-For PHP cmdlets type 'help php-dev'.
+For a list of Azure Pack cmdlets type 'Get-Command *wapack*'.
 "@
 Write-Output $welcomeMessage
 
-Set-ExecutionPolicy -Scope Process Undefined -Force
-if ($(Get-ExecutionPolicy) -eq "Restricted")
-{
-    Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned -Force
-}
-
-$VerbosePreference="Continue"
+$VerbosePreference = "Continue"
