8806c2eccd2394b7373d5d5072cbcfe9aa0b2753
Esha Parmar <eshaparmar@gmail.com>
2015-09-23 13:53:13 -0700

    Moving changes for ** ARM E2E scenario tests

---
+++ src/ResourceManager/Compute/Commands.Compute.Test/ScenarioTests/DscExtensionTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.WindowsAzure.Commands.ScenarioTest;
+using Xunit;
+
+namespace Microsoft.Azure.Commands.Compute.Test.ScenarioTests
+{
+    public class DscExtensionTests
+    {
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestGetAzureRmVMDscExtension()
+        {
+            ComputeTestController.NewInstance.RunPsTest("Test-GetAzureRmVMDscExtension");
+        }
+    }
+}
