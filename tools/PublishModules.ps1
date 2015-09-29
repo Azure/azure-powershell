b8977c14f50ab1a4a9a9e451df8cef68fed4681f
yugangw-msft <yugangw@microsoft.com>
2015-09-29 15:47:14 -0700

    publish AzureRM.Profile when building local module

--- tools/PublishModules.ps1
+++ tools/PublishModules.ps1
@@ -41,7 +41,7 @@ if ([string]::IsNullOrEmpty($scope))
     $scope = 'All'  
 }
 
-Write-Host "Publishing $scope package(s)" 
+Write-Host "Publishing $scope package(and its dependencies)" 
 
 $packageFolder = "$PSScriptRoot\..\src\Package"
 
@@ -54,9 +54,9 @@ if ($repo -ne $null) {
 }
 
 $resourceManagerRootFolder = "$packageFolder\$buildConfig\ResourceManager\AzureResourceManager"
-
-if ($scope -eq 'All') {
-    # Publish AzureRM.Profile first which is the common dependency
+$publishToLocal = test-path $repositoryLocation
+if (($scope -eq 'All') -or $publishToLocal ) {
+    # If we publish 'All' or to local folder, publish AzureRM.Profile first, becasue it is the common dependency
     Write-Host "Publishing profile module"
     Publish-Module -Path "$resourceManagerRootFolder\AzureRM.Profile" -NuGetApiKey $apiKey -Repository $repoName
     Write-Host "Published profile module"
