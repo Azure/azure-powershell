43e30af9236da1c6ae4252c4feaccfa71f4b9253
Abdelrahman Elogeel <elogeel@outlook.com>
2015-09-30 16:23:27 -0700

    Fixes for PublishedModules
    - Remove fake error thrown upon publishing AzureRM module
    - Include AzureRM module in All scope

--- tools/PublishModules.ps1
+++ tools/PublishModules.ps1
@@ -76,7 +76,7 @@ if (($scope -eq 'All') -or ($scope -eq 'AzureStorage')) {
     Publish-Module -Path $modulePath -NuGetApiKey $apiKey -Repository $repoName
 } 
 
-if ($scope -eq 'AzureRM') {
+if (($scope -eq 'All') -or ($scope -eq 'AzureRM')) {
     # Publish AzureRM module    
     $modulePath = "$PSScriptRoot\AzureRM"
     Write-Host "Publishing AzureRM module from $modulePath"
@@ -96,7 +96,7 @@ if ($scope -eq 'All') {
             Write-Host "Published $module module"
         }
     }
-} else {
+} elseif ($scope -ne 'AzureRM') {
     $modulePath = Join-Path $resourceManagerRootFolder "AzureRM.$scope"
     if (Test-Path $modulePath) {
         Write-Host "Publishing $scope module from $modulePath"
