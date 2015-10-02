f9bdd0cc9b582755ad4b41be4845267367223d14
Jianghao Lu <jianghaolu@users.noreply.github.com>
2015-10-02 11:11:03 -0700

    Update Common authentication from 1.3.0 to 1.3.2 in tests

--- src/Common/Commands.ScenarioTests.Common/Mocks/MockCertificateAuthenticationFactory.cs
+++ src/Common/Commands.ScenarioTests.Common/Mocks/MockCertificateAuthenticationFactory.cs
@@ -79,5 +79,10 @@ namespace Microsoft.WindowsAzure.Commands.Common.Test.Mocks
         {
             throw new System.NotImplementedException();
         }
+
+        public SubscriptionCloudCredentials GetSubscriptionCloudCredentials(AzureContext context, AzureEnvironment.Endpoint targetEndpoint)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
