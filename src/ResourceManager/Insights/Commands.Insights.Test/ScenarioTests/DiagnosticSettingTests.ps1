4255c69ab0e5414fe82548a50e89716ccba725eb
Vinicius <vcanaa@gmail.com>
2015-10-22 11:09:12 -0700

    Bad merge fix

--- src/ResourceManager/Insights/Commands.Insights.Test/ScenarioTests/DiagnosticSettingTests.ps1
+++ src/ResourceManager/Insights/Commands.Insights.Test/ScenarioTests/DiagnosticSettingTests.ps1
@@ -53,14 +53,14 @@ function Test-SetDiagnosticSetting
 
 		Assert-AreEqual $actual.StorageAccountId "/subscriptions/1a66ce04-b633-4a0b-b2bc-a912ec8986a6/resourceGroups/montest/providers/Microsoft.Storage/storageAccounts/montest3470"
 		Assert-AreEqual montest3470 $actual.StorageAccountName
-		Assert-AreEqual 1 $actual.Metrics.Count
-		Assert-AreEqual $true $actual.Metrics[0].Enabled
-		Assert-AreEqual "PT1M" $actual.Metrics[0].Timegrain
-		Assert-AreEqual 2 $actual.Logs.Count
-		Assert-AreEqual $true $actual.Logs[0].Enabled
-		Assert-AreEqual "TestLog1" $actual.Logs[0].Category
-		Assert-AreEqual $true $actual.Logs[1].Enabled
-		Assert-AreEqual "TestLog2" $actual.Logs[1].Category
+		Assert-AreEqual 1           $actual.Metrics.Count
+		Assert-AreEqual $true       $actual.Metrics[0].Enabled
+		Assert-AreEqual "PT1M"      $actual.Metrics[0].Timegrain
+		Assert-AreEqual 2           $actual.Logs.Count
+		Assert-AreEqual $true       $actual.Logs[0].Enabled
+		Assert-AreEqual "TestLog1"  $actual.Logs[0].Category
+		Assert-AreEqual $true       $actual.Logs[1].Enabled
+		Assert-AreEqual "TestLog2"  $actual.Logs[1].Category
     }
     finally
     {
