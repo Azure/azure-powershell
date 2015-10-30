7d02de61bf1c687f87116d2f5228895a2ffcdf21
Sudhakara Reddy Evuri <suredd@microsoft.com>
2015-10-30 12:29:49 -0700

    Remove GetAzureDiskEncryptionExtension cmdlet

--- src/ResourceManager/Compute/Commands.Compute/Common/ConstantStringTypes.cs
+++ src/ResourceManager/Compute/Commands.Compute/Common/ConstantStringTypes.cs
@@ -117,6 +117,6 @@ namespace Microsoft.Azure.Commands.Compute.Common
 
         //AzureDiskEncryption
         public const string AzureDiskEncryptionExtension = "AzureRmVMDiskEncryptionExtension";
-        public const string AzureDiskEncryptionStatus = "AzureRmVmDiskEncryptionStatus";
+        public const string AzureDiskEncryptionStatus = "AzureRmVMDiskEncryptionStatus";
     }
 }
