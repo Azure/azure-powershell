ac641f7b35cb0516dc365bd7d7aa7bc92d8d31dd
emmazhu <emmazhu@microsoft.com>
2015-11-03 14:11:52 +0800

    Roll back some project which doesn't need to upgrade to xscl6.0

--- tools/Build.ps1
+++ tools/Build.ps1
@@ -15,4 +15,4 @@
 $scriptFolder = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 . ($scriptFolder + '.\SetupEnv.ps1')
 
-msbuild "$env:AzurePSRoot\build.proj" /t:Full
+msbuild "$env:AzurePSRoot\build.proj" /t:Build
