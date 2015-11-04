85c69b0265b1bd80f9bb9af009eca6b64c1e0421
Balu Kambala <baluk@microsoft.com>
2015-11-04 13:48:54 -0800

    Fixed the cmdlet to use ARM template v2.6

--- src/ResourceManager/Automation/Commands.Automation/Common/Constants.cs
+++ src/ResourceManager/Automation/Commands.Automation/Common/Constants.cs
@@ -60,12 +60,12 @@ namespace Microsoft.Azure.Commands.Automation.Common
         public const int PsCommandValueDepth = 10;
 
         // The template file is a json
-        public const string TemplateFile = @"https://eus2oaasibizamarketprod1.blob.core.windows.net/automationdscpreview/azuredeploy.json";
+        public const string TemplateFile = @"https://eus2oaasibizamarketprod1.blob.core.windows.net/automationdscpreview/azuredeployV2.json";
 
         // The metaconfig file
-        public const string ModulesUrl = @"https://eus2oaasibizamarketprod1.blob.core.windows.net/automationdscpreview/RegistrationMetaConfig.zip";
+        public const string ModulesUrl = @"https://eus2oaasibizamarketprod1.blob.core.windows.net/automationdscpreview/RegistrationMetaconfigV2.zip";
 
-        public const string ConfigurationFunction = @"RegistrationMetaConfig.ps1\RegistrationMetaConfig";
+        public const string ConfigurationFunction = @"RegistrationMetaconfigV2.ps1\\RegistrationMetaconfigV2";
 
 
         public static class RunbookType
