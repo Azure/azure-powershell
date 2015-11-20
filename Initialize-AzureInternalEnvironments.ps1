5c614cb6af5c8de6c98ff48a2664347643191b27
Venkata Ramanamurthy Chandaka <vrmurthy@microsoft.com>
2015-11-20 09:26:02 -0800

    Script changes to fix environment initialization

--- Initialize-AzureInternalEnvironments.ps1
+++ Initialize-AzureInternalEnvironments.ps1
@@ -26,28 +26,4 @@ if (-not (Has-Environment $dogfood))
     -GalleryEndpoint 'https://df.gallery.azure-test.net/' `
     -GraphEndpoint 'https://graph.ppe.windows.net/'
 }
-if (-not (Has-Environment $current))
-{
-  Add-AzureRmEnvironment -Name $current `
-    -PublishSettingsFileUrl 'https://auxcurrent.windows.azure-test.net/publishsettings/index' `
-    -ServiceEndpoint 'https://management.rdfetest.dnsdemo4.com/' `
-    -ManagementPortalUrl 'https://auxcurrent.windows.azure-test.net/' `
-    -ActiveDirectoryEndpoint 'https://login.windows-ppe.net/' `
-    -ActiveDirectoryServiceEndpointResourceId 'https://management.core.windows.net/' `
-    -ResourceManagerEndpoint 'https://api-current.resources.windows-int.net/' `
-    -GalleryEndpoint 'https://current.gallery.azure-test.net/' `
-    -GraphEndpoint 'https://graph.ppe.windows.net/'
-}
-if (-not (Has-Environment $next))
-{
-  Add-AzureRmEnvironment -Name $next `
-    -PublishSettingsFileUrl 'https://auxnext.windows.azure-test.net/publishsettings/index' `
-    -ServiceEndpoint 'https://managementnext.rdfetest.dnsdemo4.com/' `
-    -ManagementPortalUrl 'https://auxnext.windows.azure-test.net/' `
-    -ActiveDirectoryEndpoint 'https://login.windows-ppe.net/' `
-    -ActiveDirectoryServiceEndpointResourceId 'https://management.core.windows.net/' `
-    -ResourceManagerEndpoint 'https://api-next.resources.windows-int.net/' `
-    -GalleryEndpoint 'https://next.gallery.azure-test.net/' `
-    -GraphEndpoint 'https://graph.ppe.windows.net/'
-}
 #####################
--- tools/Init2.ps1
+++ tools/Init2.ps1
@@ -17,14 +17,16 @@
 	.\Initialize-AzureInternalEnvironments.ps1
 	
 	write-host "Logging in with Azure environment.." -ForegroundColor Cyan
-	$env = Get-AzureRmEnvironment 
+	$env = Get-AzureRmEnvironment -Name dogfood
+	
 	$secpasswd = ConvertTo-SecureString "Pa`$`$w0rd" -AsPlainText -Force
 	$mycreds = New-Object System.Management.Automation.PSCredential ("admin@aad296.ccsctp.net", $secpasswd)
 	
 	#$secpasswd = ConvertTo-SecureString "Pass1Word" -AsPlainText -Force
 	#$mycreds = New-Object System.Management.Automation.PSCredential ("admin@OneDFAdminExpRunner2DMSUB01.ccsctp.net", $secpasswd)
 
+
 	#$secpasswd = ConvertTo-SecureString "P0rsche911" -AsPlainText -Force
 	#$mycreds = New-Object System.Management.Automation.PSCredential ("admin@uxmdmonly.ccsctp.net", $secpasswd)
 	
-	Login-AzureRmAccount -environment $env[2] -credential $mycreds
+	Login-AzureRmAccount -environment $env -credential $mycreds
