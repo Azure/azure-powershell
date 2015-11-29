7aa74931b0016bd17ba3208992b10c199c3c9ace
Hovsep Mkrtchyan <hovsepm@microsoft.com>
2015-11-29 15:50:44 -0800

    Fixed issues with token id that does not match incoming account id

--- src/ResourceManager/Common/Commands.ScenarioTests.ResourceManager.Common/Mocks/MockClientFactory.cs
+++ src/ResourceManager/Common/Commands.ScenarioTests.ResourceManager.Common/Mocks/MockClientFactory.cs
@@ -189,7 +189,7 @@ namespace Microsoft.WindowsAzure.Commands.Common.Test.Mocks
 
         /// <summary>
         /// This class exists to allow adding an additional reference to the httpClient to prevent the client 
-        /// from being disposed.  Should not be used execpt in this mocked context.
+        /// from being disposed.  Should not be used except in this mocked context.
         /// </summary>
         class PassThroughDelegatingHandler : DelegatingHandler
         {
