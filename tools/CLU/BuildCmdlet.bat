4108f1f5970c4152f8073c22c21bc93043349350
yugangw-msft <yugangw@microsoft.com>
2015-12-10 15:53:36 -0800

    prepare osx/linux bits which can xcopy and execute

--- tools/CLU/BuildCmdlet.bat
+++ tools/CLU/BuildCmdlet.bat
@@ -11,6 +11,7 @@ if not "%1"=="" (
     %root%\drop\clurun\win7-x64\clurun.exe --install
     %root%\drop\clurun\win7-x64\clurun.exe --install Microsoft.Azure.Commands.Profile
     %root%\drop\clurun\win7-x64\clurun.exe --install Microsoft.Azure.Commands.Resources
+    %root%\drop\clurun\win7-x64\clurun.exe --install Microsoft.Azure.Commands.Resources.Cmdlets
     %root%\drop\clurun\win7-x64\clurun.exe --install Microsoft.Azure.Commands.Websites
     %root%\drop\clurun\win7-x64\clurun.exe --install Microsoft.Azure.Commands.Management.Storage
 )
