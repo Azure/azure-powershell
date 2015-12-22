25c7a48bb50c34329ca213aa4f771ddaf310efb1
Pooneh <pomortaz@microsoft.com>
2015-12-22 09:27:41 -0800

    Fixing PowerShell in Fairfax by removing dependency to AD cmdlets: AD cmdlets are broken in Fairfax and there is no immediate promise to fix them. This change is to remove the dependency to AD cmdlets and Graph API in Key Vault PowerShell Cmdlets in Fairfax.

--- src/ResourceManager/KeyVault/Commands.KeyVault/Models/VaultManagementClient.cs
+++ src/ResourceManager/KeyVault/Commands.KeyVault/Models/VaultManagementClient.cs
@@ -67,9 +67,7 @@ namespace Microsoft.Azure.Commands.KeyVault.Models
                 throw new ArgumentNullException("parameters.SkuFamilyName");
             if (parameters.TenantId == null || parameters.TenantId == Guid.Empty)
                 throw new ArgumentException("parameters.TenantId");
-            if (parameters.ObjectId == null || parameters.ObjectId == Guid.Empty)
-                throw new ArgumentException("parameters.ObjectId");
-            
+
             var response = this.KeyVaultManagementClient.Vaults.CreateOrUpdate(
                 resourceGroupName: parameters.ResourceGroupName,
                 vaultName: parameters.VaultName,
@@ -90,19 +88,9 @@ namespace Microsoft.Azure.Commands.KeyVault.Models
                         EnabledForDiskEncryption = parameters.EnabledForDiskEncryption,
                         TenantId = parameters.TenantId,
                         VaultUri = "",
-                        AccessPolicies = new []
-                        {
-                            new AccessPolicyEntry
-                            {
-                                TenantId = parameters.TenantId,
-                                ObjectId = parameters.ObjectId,
-                                PermissionsToKeys = parameters.PermissionsToKeys,                                    
-                                PermissionsToSecrets = parameters.PermissionsToSecrets
-                            }
-                        }
+                        AccessPolicies = (parameters.AccessPolicy != null) ? new[] { parameters.AccessPolicy } : new AccessPolicyEntry[] { }
                     }
-                }
-                );
+                });
 
             return new PSVault(response.Vault, adClient);
         }
