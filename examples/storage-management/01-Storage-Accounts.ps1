ea8f84dd12faeb1e07306dddc7561685a52b72bc
emmazhu <emmazhu@microsoft.com>
2016-01-06 13:46:35 +0800

    Fix issue of the aliases of new storage account and set storage account don't work. Add new scenario test for storage management cmdlets.

---
+++ examples/storage-management/01-Storage-Accounts.ps1
@@ -0,0 +1,33 @@
+﻿Param(
+  [string]$rgname,
+  [string]$resourceGroupLocation
+)
+Write-Host "=== Managing Storage Accounts Resources in Azure ==="
+
+$stoname = 'sto' + $rgname;
+$stotype = 'Standard_GRS';
+$loc = 'West US';
+
+New-AzureRmResourceGroup -Name $rgname -Location $resourceGroupLocation;
+
+Write-Host "1. Create a new storage account"
+New-AzureRmStorageAccount -ResourceGroupName $rgname -Name $stoname -Location $loc -Type $stotype;
+
+Write-Host "2. Get info of a storage account"
+$stos = Get-AzureRmStorageAccount -ResourceGroupName $rgname;
+
+Write-Host "3. Update storage account type"
+$stotype = 'Standard_RAGRS';
+Set-AzureRmStorageAccount -ResourceGroupName $rgname -Name $stoname -Type $stotype;
+
+Write-Host "4. Get account key of a storage account"
+$stokeys=Get-AzureRmStorageAccountKey -ResourceGroupName $rgname -Name $stoname;
+
+Write-Host "5. Renew key1 of a storage account"
+New-AzureRmStorageAccountKey -ResourceGroupName $rgname -Name $stoname -KeyName key1;
+
+Write-Host "6. Renew key2 of a storage account"        
+New-AzureRmStorageAccountKey -ResourceGroupName $rgname -Name $stoname -KeyName key2;
+
+Write-Host "7. Remove storage account"
+Remove-AzureRmStorageAccount -ResourceGroupName $rgname -Name $stoname;
