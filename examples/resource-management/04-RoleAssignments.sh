d2f184be5b8e1d2bdc5ba8b486e1884e21d028fc
markcowl <markcowl@microsoft.com>
2016-01-15 18:56:25 -0800

    Updatign tests for new resource cmdlet names

--- examples/resource-management/04-RoleAssignments.sh
+++ examples/resource-management/04-RoleAssignments.sh
@@ -3,25 +3,25 @@ set -e
 printf "\n=== Managing Role Assignments in Azure ===\n"
 
 printf "\n1. Creating a new resource group: %s and location: %s.\n" "$groupName" "$location"
-az resourcemanager group create --name "$groupName" --location "$location"
+az resource group create --name "$groupName" --location "$location"
 
 printf "\n2. Creating a new Role Assignment.\n"
 export MSYS_NO_PATHCONV=1
-users=$(az activedirectory users ls)
+users=$(az ad users ls)
 userId=$(echo $users | cat | jq '.[0].Id' -s --raw-output)
 echo "UserID: $userId"
-roleDefinitions=$(az networksecurityrole definition ls)
+roleDefinitions=$(az resource role definition ls)
 roleDefinitionId=$(echo $roleDefinitions | cat | jq '.[0].Id' -s --raw-output)
 echo "RoleDefinitionId: $roleDefinitionId"
 subsciptions=$(az subscription ls)
 subscriptionId=$(echo $subsciptions | cat | jq '.[0].SubscriptionId' -s --raw-output)
 scope="/subscriptions/$subscriptionId/resourceGroups/$groupName"
 echo "Scope: $scope"
-az networksecurityrole assignment create --ObjectId "$userId" --RoleDefinitionId "$roleDefinitionId" --Scope "$scope"
+az resource role assignment create --ObjectId "$userId" --RoleDefinitionId "$roleDefinitionId" --Scope "$scope"
 
 printf "\n3. Delete last created Role Assignment.\n"
-assignments=$(az networksecurityrole assignment ls)
+assignments=$(az resource role assignment ls)
 assignmentId=$(echo $assignments | cat | jq '.[-1:][0].ObjectId' -s --raw-output)
 echo "Deleting assignment: $assignmentId"
-az networksecurityrole assignment rm --ObjectId "$assignmentId" --Scope "$scope" --RoleDefinitionId "$roleDefinitionId" -f
+az resource role assignment rm --ObjectId "$assignmentId" --Scope "$scope" --RoleDefinitionId "$roleDefinitionId" -f
 export MSYS_NO_PATHCONV=
