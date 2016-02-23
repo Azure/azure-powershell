bdb8ff5f310c9532285870f9beb7f1ad03069ad9
Sebastian Max Dusch <sedusch@microsoft.com>
2016-02-23 19:58:45 +0100

    add license header, remove unused usings

--- src/ResourceManager/Compute/Commands.Compute/Extension/AEM/AEMHelper.cs
+++ src/ResourceManager/Compute/Commands.Compute/Extension/AEM/AEMHelper.cs
@@ -1,4 +1,18 @@
-﻿using Microsoft.Azure.Commands.Common.Authentication.Models;
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Commands.Common.Authentication.Models;
 using Microsoft.Azure.Commands.Compute.StorageServices;
 using Microsoft.Azure.Management.Compute.Models;
 using Microsoft.Azure.Management.Storage;
@@ -17,10 +31,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Management.Automation;
 using System.Management.Automation.Host;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Microsoft.Azure.Commands.Compute.Extension.AEM
 {
