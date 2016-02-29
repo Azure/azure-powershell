d18875c04e8efb9556842083ad8faff4f871b64e
clamchowder <lamchester@gmail.com>
2016-02-29 12:03:43 -0800

    Add BgpSettings

---
+++ src/ServiceManagement/Network/Commands.Network/Gateway/Model/BgpSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.WindowsAzure.Commands.ServiceManagement.Network
+{
+    public class BgpSettings
+    {
+        public uint Asn { get; set; }
+
+        public string BgpPeeringAddress { get; set; }
+
+        public int PeerWeight { get; set; }
+    }
+}
