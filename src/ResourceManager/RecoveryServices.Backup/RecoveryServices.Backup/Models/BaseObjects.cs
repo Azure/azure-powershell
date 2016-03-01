56410d2f1ec2a4fa9213ad3bf4b73259beae1700
dragonfly91 <anudeeb@microsoft.com>
2016-03-01 10:20:45 +0530

    [RSBackup] Adjusting namespace depth

--- src/ResourceManager/RecoveryServices.Backup/RecoveryServices.Backup/Models/BaseObjects.cs
+++ src/ResourceManager/RecoveryServices.Backup/RecoveryServices.Backup/Models/BaseObjects.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Microsoft.Azure.Commands.RecoveryServices.Backup.Cmdlets.ProviderModel.Models
+namespace Microsoft.Azure.Commands.RecoveryServices.Backup.Cmdlets.Models
 {
     public class BaseObjects
     {
