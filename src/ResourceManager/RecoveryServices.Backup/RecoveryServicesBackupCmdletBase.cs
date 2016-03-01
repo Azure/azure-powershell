d0fcc9645412407e187eeaed6d2c7c8a48b5691d
dragonfly91 <anudeeb@microsoft.com>
2016-03-01 11:37:46 +0530

    1. Added cmdlet base 2. Restructured directory 3. Added license header

---
+++ src/ResourceManager/RecoveryServices.Backup/RecoveryServicesBackupCmdletBase.cs
@@ -0,0 +1,107 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Hyak.Common;
+using Microsoft.Azure.Commands.ResourceManager.Common;
+using RecoveryServices.Backup.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecoveryServices.Backup
+{
+    /// <summary>
+    /// Base class for Recovery Services Backup cmdlets
+    /// </summary>
+    public abstract class RecoveryServicesBackupCmdletBase : AzureRMCmdlet
+    {
+        protected void InitializeAzureBackupCmdlet(string resourceGroupName, string resourceName)
+        {
+
+        }
+
+        protected void ExecutionBlock(Action action)
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (Exception exception)
+            {
+                HandleException(exception);
+            }
+        }
+
+        /// <summary>
+        /// Handles set of exceptions thrown by client
+        /// </summary>
+        /// <param name="ex"></param>
+        private void HandleException(Exception exception)
+        {
+            if (exception is AggregateException && ((AggregateException)exception).InnerExceptions != null
+                && ((AggregateException)exception).InnerExceptions.Count != 0)
+            {
+                WriteDebug(Resources.AggregateException);
+                foreach (var innerEx in ((AggregateException)exception).InnerExceptions)
+                {
+                    HandleException(innerEx);
+                }
+            }
+            else
+            {
+                Exception targetEx = exception;
+                string targetErrorId = String.Empty;
+                ErrorCategory targetErrorCategory = ErrorCategory.NotSpecified;
+
+                if (exception is CloudException)
+                {
+                    var cloudEx = exception as CloudException;
+                    if (cloudEx.Response != null && cloudEx.Response.StatusCode == HttpStatusCode.NotFound)
+                    {
+                        WriteDebug(String.Format(Resources.CloudExceptionCodeNotFound, cloudEx.Response.StatusCode));
+
+                        targetEx = new Exception(Resources.ResourceNotFoundMessage);
+                        targetErrorCategory = ErrorCategory.InvalidArgument;
+                    }
+                    else if (cloudEx.Error != null)
+                    {
+                        WriteDebug(String.Format(Resources.CloudException, cloudEx.Error.Code, cloudEx.Error.Message));
+
+                        targetErrorId = cloudEx.Error.Code;
+                        targetErrorCategory = ErrorCategory.InvalidOperation;
+                    }
+                }
+                else if (exception is WebException)
+                {
+                    var webEx = exception as WebException;
+                    WriteDebug(string.Format(Resources.WebException, webEx.Response, webEx.Status));
+
+                    targetErrorCategory = ErrorCategory.ConnectionError;
+                }
+                else if (exception is ArgumentException || exception is ArgumentNullException)
+                {
+                    WriteDebug(string.Format(Resources.ArgumentException));
+                    targetErrorCategory = ErrorCategory.InvalidArgument;
+                }
+
+                var errorRecord = new ErrorRecord(targetEx, targetErrorId, targetErrorCategory, null);
+                WriteError(errorRecord);
+            }
+        }
+    }
+}
