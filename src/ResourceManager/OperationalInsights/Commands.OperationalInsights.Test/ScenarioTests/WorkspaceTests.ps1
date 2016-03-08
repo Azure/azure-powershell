62d6960b0c6584a2c3220c32aebbd3850cd21eb4
Christina Funk <christinamfunk@gmail.com>
2016-03-08 14:53:15 -0800

    Update workspace test for listing IPs

--- src/ResourceManager/OperationalInsights/Commands.OperationalInsights.Test/ScenarioTests/WorkspaceTests.ps1
+++ src/ResourceManager/OperationalInsights/Commands.OperationalInsights.Test/ScenarioTests/WorkspaceTests.ps1
@@ -193,6 +193,8 @@ function Test-WorkspaceEnableDisableListIntelligencePacks
 
 	# List to verify that the IP's have been disabled
 	$ipList = Get-AzureRmOperationalInsightsIntelligencePacks -ResourceGroupName $rgname -WorkspaceName $wsname
+	Assert-NotNull $ipList
+	Assert-NotEqual 0 $ipList.Count
 	Foreach ($ip in $ipList)
 	{
 		if ($ip.Name -eq "LogManagement")
