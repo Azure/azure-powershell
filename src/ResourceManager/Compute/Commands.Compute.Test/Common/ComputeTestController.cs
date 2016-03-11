80a0dbe6fbd4d5d72f843c2396209cfd188c1645
vivsriaus <visriniv@microsoft.com>
2016-03-10 18:21:37 -0800

    Undo

--- src/ResourceManager/Compute/Commands.Compute.Test/Common/ComputeTestController.cs
+++ src/ResourceManager/Compute/Commands.Compute.Test/Common/ComputeTestController.cs
@@ -83,7 +83,6 @@ namespace Microsoft.Azure.Commands.Compute.Test.ScenarioTests
             d.Add("Microsoft.Resources", null);
             d.Add("Microsoft.Features", null);
             d.Add("Microsoft.Authorization", null);
-            d.Add("Microsoft.Compute", null);
             var providersToIgnore = new Dictionary<string, string>();
             providersToIgnore.Add("Microsoft.Azure.Management.Resources.ResourceManagementClient", "2016-02-01");
             HttpMockServer.Matcher = new PermissiveRecordMatcherWithApiExclusion(true, d, providersToIgnore);
