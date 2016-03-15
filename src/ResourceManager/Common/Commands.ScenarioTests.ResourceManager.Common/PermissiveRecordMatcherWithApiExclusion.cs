6370ac514bcebf3fee9a71ba437ea86967662909
Vivek Srinivasan <visriniv@microsoft.com>
2016-03-14 22:28:42 -0700

    Fix

--- src/ResourceManager/Common/Commands.ScenarioTests.ResourceManager.Common/PermissiveRecordMatcherWithApiExclusion.cs
+++ src/ResourceManager/Common/Commands.ScenarioTests.ResourceManager.Common/PermissiveRecordMatcherWithApiExclusion.cs
@@ -112,8 +112,8 @@ namespace Microsoft.WindowsAzure.Commands.ScenarioTest
                 !requestUri.StartsWith("/pools", StringComparison.InvariantCultureIgnoreCase) &&
                 !requestUri.StartsWith("/jobs", StringComparison.InvariantCultureIgnoreCase) &&
                 !requestUri.StartsWith("/jobschedules", StringComparison.InvariantCultureIgnoreCase) &&
-                !requestUri.StartsWith("/applications", StringComparison.InvariantCultureIgnoreCase) &&
-                !requestUri.Contains("servicePrincipals"))
+                !requestUri.Contains("/applications?") &&
+                !requestUri.Contains("/servicePrincipals?"))
             {
                 version = String.Empty;
                 return true;
