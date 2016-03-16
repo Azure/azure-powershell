53dcccb48a8b6a0080c95b03862b4a715badcfe3
markcowl <markcowl@microsoft.com>
2016-03-15 23:00:12 -0700

    Update msi for storage changes

--- src/Common/Commands.Common.Storage/IStorageService.cs
+++ src/Common/Commands.Common.Storage/IStorageService.cs
@@ -14,10 +14,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.WindowsAzure.Storage.Auth;
 
 namespace Microsoft.WindowsAzure.Commands.Common.Storage
 {
