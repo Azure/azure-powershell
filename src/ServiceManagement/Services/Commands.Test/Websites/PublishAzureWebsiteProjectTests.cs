74fb71621c0ad805552f2a2d6ef1db376ed82edb
Ethan Crawford <Ethan.Crawford@microsoft.com>
2016-03-22 12:58:03 -0700

    Initialize hashtable after declaration to fix build break

--- src/ServiceManagement/Services/Commands.Test/Websites/PublishAzureWebsiteProjectTests.cs
+++ src/ServiceManagement/Services/Commands.Test/Websites/PublishAzureWebsiteProjectTests.cs
@@ -13,175 +13,176 @@ using Xunit;
 
 namespace Microsoft.WindowsAzure.Commands.Test.Websites
 {
-    public class PublishAzureWebsiteProjectTests : WebsitesTestBase
-    {
-        [Fact]
-        [Trait(Category.AcceptanceType, Category.CheckIn)]
-        public void PublishFromPackageWithoutSetParameters()
-        {
-            const string websiteName = "test-site";
-            const string package = "test-package";
-            string slot = null;
-            var connectionStrings = new Hashtable { ["DefaultConnection"] = "test-connection-string" };
+	public class PublishAzureWebsiteProjectTests : WebsitesTestBase
+	{
+		[Fact]
+		[Trait(Category.AcceptanceType, Category.CheckIn)]
+		public void PublishFromPackageWithoutSetParameters()
+		{
+			const string websiteName = "test-site";
+			const string package = "test-package";
+			string slot = null;
+			var connectionStrings = new Hashtable();
+			connectionStrings["DefaultConnection"] = "test-connection-string";
 
-            var publishProfile = new WebSiteGetPublishProfileResponse.PublishProfile
-            {
-                UserName = "test-user-name",
-                UserPassword = "test-password",
-                PublishUrl = "test-publish-url"
-            };
+			var publishProfile = new WebSiteGetPublishProfileResponse.PublishProfile
+			{
+				UserName = "test-user-name",
+				UserPassword = "test-password",
+				PublishUrl = "test-publish-url"
+			};
 
-            var published = false;
+			var published = false;
 
-            var clientMock = new Mock<IWebsitesClient>();
+			var clientMock = new Mock<IWebsitesClient>();
 
-            clientMock.Setup(c => c.GetWebDeployPublishProfile(websiteName, slot)).Returns(publishProfile);
-            clientMock.Setup(c => c.PublishWebProject(websiteName, slot, package, null, connectionStrings, false, false))
-                .Callback((string n, string s, string p, string spf, Hashtable cs, bool skipAppData, bool doNotDelete) =>
-                {
-                    Assert.Equal(websiteName, n);
-                    Assert.Equal(slot, s);
-                    Assert.Equal(package, p);
-                    Assert.Equal(connectionStrings, cs);
-                    Assert.False(skipAppData);
-                    Assert.False(doNotDelete);
-                    published = true;
-                });
+			clientMock.Setup(c => c.GetWebDeployPublishProfile(websiteName, slot)).Returns(publishProfile);
+			clientMock.Setup(c => c.PublishWebProject(websiteName, slot, package, null, connectionStrings, false, false))
+				.Callback((string n, string s, string p, string spf, Hashtable cs, bool skipAppData, bool doNotDelete) =>
+				{
+					Assert.Equal(websiteName, n);
+					Assert.Equal(slot, s);
+					Assert.Equal(package, p);
+					Assert.Equal(connectionStrings, cs);
+					Assert.False(skipAppData);
+					Assert.False(doNotDelete);
+					published = true;
+				});
 
-            var powerShellMock = new Mock<ICommandRuntime>();
+			var powerShellMock = new Mock<ICommandRuntime>();
 
-            var command = new PublishAzureWebsiteProject
-            {
-                CommandRuntime = powerShellMock.Object,
-                Name = websiteName,
-                Package = package,
-                ConnectionString = connectionStrings,
-                WebsitesClient = clientMock.Object,
-                SetParametersFile = null
-            };
+			var command = new PublishAzureWebsiteProject
+			{
+				CommandRuntime = powerShellMock.Object,
+				Name = websiteName,
+				Package = package,
+				ConnectionString = connectionStrings,
+				WebsitesClient = clientMock.Object,
+				SetParametersFile = null
+			};
 
-            command.ExecuteCmdlet();
+			command.ExecuteCmdlet();
 
-            powerShellMock.Verify(f => f.WriteVerbose(string.Format("[Complete] Publishing package {0}", package)), Times.Once());
-            Assert.True(published);
-        }
+			powerShellMock.Verify(f => f.WriteVerbose(string.Format("[Complete] Publishing package {0}", package)), Times.Once());
+			Assert.True(published);
+		}
 
-        [Fact]
-        [Trait(Category.AcceptanceType, Category.CheckIn)]
-        public void PublishFromPackage()
-        {
-            var websiteName = "test-site";
-            string slot = null;
-            var package = "test-package";
-            var connectionStrings = new Hashtable();
-            connectionStrings["DefaultConnection"] = "test-connection-string";
-            string setParametersFile = "testfile.xml";
+		[Fact]
+		[Trait(Category.AcceptanceType, Category.CheckIn)]
+		public void PublishFromPackage()
+		{
+			var websiteName = "test-site";
+			string slot = null;
+			var package = "test-package";
+			var connectionStrings = new Hashtable();
+			connectionStrings["DefaultConnection"] = "test-connection-string";
+			string setParametersFile = "testfile.xml";
 
-            var publishProfile = new WebSiteGetPublishProfileResponse.PublishProfile()
-            {
-                UserName = "test-user-name",
-                UserPassword = "test-password",
-                PublishUrl = "test-publlish-url"
-            };
+			var publishProfile = new WebSiteGetPublishProfileResponse.PublishProfile()
+			{
+				UserName = "test-user-name",
+				UserPassword = "test-password",
+				PublishUrl = "test-publlish-url"
+			};
 
-            var published = false;
+			var published = false;
 
-            Mock<IWebsitesClient> clientMock = new Mock<IWebsitesClient>();
+			Mock<IWebsitesClient> clientMock = new Mock<IWebsitesClient>();
 
-            clientMock.Setup(c => c.GetWebDeployPublishProfile(websiteName, slot)).Returns(publishProfile);
-            clientMock.Setup(c => c.PublishWebProject(websiteName, slot, package, setParametersFile, connectionStrings, false, false))
-                .Callback((string n, string s, string p, string spf, Hashtable cs, bool skipAppData, bool doNotDelete) =>
-                {
-                    Assert.Equal(websiteName, n);
-                    Assert.Equal(slot, s);
-                    Assert.Equal(package, p);
-                    Assert.Equal(setParametersFile, spf);
-                    Assert.Equal(connectionStrings, cs);
-                    Assert.False(skipAppData);
-                    Assert.False(doNotDelete);
-                    published = true;
-                });
+			clientMock.Setup(c => c.GetWebDeployPublishProfile(websiteName, slot)).Returns(publishProfile);
+			clientMock.Setup(c => c.PublishWebProject(websiteName, slot, package, setParametersFile, connectionStrings, false, false))
+				.Callback((string n, string s, string p, string spf, Hashtable cs, bool skipAppData, bool doNotDelete) =>
+				{
+					Assert.Equal(websiteName, n);
+					Assert.Equal(slot, s);
+					Assert.Equal(package, p);
+					Assert.Equal(setParametersFile, spf);
+					Assert.Equal(connectionStrings, cs);
+					Assert.False(skipAppData);
+					Assert.False(doNotDelete);
+					published = true;
+				});
 
-            Mock<ICommandRuntime> powerShellMock = new Mock<ICommandRuntime>();
+			Mock<ICommandRuntime> powerShellMock = new Mock<ICommandRuntime>();
 
-            var command = new PublishAzureWebsiteProject()
-            {
-                CommandRuntime = powerShellMock.Object,
-                Name = websiteName,
-                Package = package,
-                ConnectionString = connectionStrings,
-                WebsitesClient = clientMock.Object,
-                SetParametersFile = setParametersFile
-            };
+			var command = new PublishAzureWebsiteProject()
+			{
+				CommandRuntime = powerShellMock.Object,
+				Name = websiteName,
+				Package = package,
+				ConnectionString = connectionStrings,
+				WebsitesClient = clientMock.Object,
+				SetParametersFile = setParametersFile
+			};
 
-            command.ExecuteCmdlet();
+			command.ExecuteCmdlet();
 
-            powerShellMock.Verify(f => f.WriteVerbose(string.Format("[Complete] Publishing package {0}", package)), Times.Once());
-            Assert.True(published);
-        }
+			powerShellMock.Verify(f => f.WriteVerbose(string.Format("[Complete] Publishing package {0}", package)), Times.Once());
+			Assert.True(published);
+		}
 
-        [Fact]
-        [Trait(Category.AcceptanceType, Category.CheckIn)]
-        public void PublishFromProjectFile()
-        {
-            var websiteName = "test-site";
-            string slot = null;
-            var projectFile = string.Format(@"{0}\Resources\MyWebApplication\WebApplication4.csproj", AppDomain.CurrentDomain.BaseDirectory);
-            var configuration = "Debug";
-            var logFile = string.Format(@"{0}\build.log", AppDomain.CurrentDomain.BaseDirectory);
-            var connectionStrings = new Hashtable();
-            connectionStrings["DefaultConnection"] = "test-connection-string";
-            string setParametersFile = "testfile.xml";
+		[Fact]
+		[Trait(Category.AcceptanceType, Category.CheckIn)]
+		public void PublishFromProjectFile()
+		{
+			var websiteName = "test-site";
+			string slot = null;
+			var projectFile = string.Format(@"{0}\Resources\MyWebApplication\WebApplication4.csproj", AppDomain.CurrentDomain.BaseDirectory);
+			var configuration = "Debug";
+			var logFile = string.Format(@"{0}\build.log", AppDomain.CurrentDomain.BaseDirectory);
+			var connectionStrings = new Hashtable();
+			connectionStrings["DefaultConnection"] = "test-connection-string";
+			string setParametersFile = "testfile.xml";
 
-            using (FileSystemHelper files = new FileSystemHelper(this))
-            {
-                string originalDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            }
+			using (FileSystemHelper files = new FileSystemHelper(this))
+			{
+				string originalDirectory = AppDomain.CurrentDomain.BaseDirectory;
+			}
 
-            var publishProfile = new WebSiteGetPublishProfileResponse.PublishProfile()
-            {
-                UserName = "test-user-name",
-                UserPassword = "test-password",
-                PublishUrl = "test-publlish-url"
-            };
-            var package = "test-package.zip";
+			var publishProfile = new WebSiteGetPublishProfileResponse.PublishProfile()
+			{
+				UserName = "test-user-name",
+				UserPassword = "test-password",
+				PublishUrl = "test-publlish-url"
+			};
+			var package = "test-package.zip";
 
-            var published = false;
+			var published = false;
 
-            Mock<IWebsitesClient> clientMock = new Mock<IWebsitesClient>();
+			Mock<IWebsitesClient> clientMock = new Mock<IWebsitesClient>();
 
-            clientMock.Setup(c => c.GetWebDeployPublishProfile(websiteName, slot)).Returns(publishProfile);
-            clientMock.Setup(c => c.BuildWebProject(projectFile, configuration, logFile)).Returns(package);
-            clientMock.Setup(c => c.PublishWebProject(websiteName, slot, package, setParametersFile, connectionStrings, false, false))
-                .Callback((string n, string s, string p, string spf, Hashtable cs, bool skipAppData, bool doNotDelete) =>
-                {
-                    Assert.Equal(websiteName, n);
-                    Assert.Equal(slot, s);
-                    Assert.Equal(package, p);
-                    Assert.Equal(setParametersFile, spf);
-                    Assert.Equal(connectionStrings, cs);
-                    Assert.False(skipAppData);
-                    Assert.False(doNotDelete);
-                    published = true;
-                });
+			clientMock.Setup(c => c.GetWebDeployPublishProfile(websiteName, slot)).Returns(publishProfile);
+			clientMock.Setup(c => c.BuildWebProject(projectFile, configuration, logFile)).Returns(package);
+			clientMock.Setup(c => c.PublishWebProject(websiteName, slot, package, setParametersFile, connectionStrings, false, false))
+				.Callback((string n, string s, string p, string spf, Hashtable cs, bool skipAppData, bool doNotDelete) =>
+				{
+					Assert.Equal(websiteName, n);
+					Assert.Equal(slot, s);
+					Assert.Equal(package, p);
+					Assert.Equal(setParametersFile, spf);
+					Assert.Equal(connectionStrings, cs);
+					Assert.False(skipAppData);
+					Assert.False(doNotDelete);
+					published = true;
+				});
 
-            Mock<ICommandRuntime> powerShellMock = new Mock<ICommandRuntime>();
-            TestMockSupport.TestExecutionFolder = AppDomain.CurrentDomain.BaseDirectory;
-            var command = new PublishAzureWebsiteProject()
-            {
-                CommandRuntime = powerShellMock.Object,
-                WebsitesClient = clientMock.Object,
-                Name = websiteName,
-                ProjectFile = projectFile,
-                Configuration = configuration,
-                ConnectionString = connectionStrings,
-                SetParametersFile = setParametersFile
-            };
+			Mock<ICommandRuntime> powerShellMock = new Mock<ICommandRuntime>();
+			TestMockSupport.TestExecutionFolder = AppDomain.CurrentDomain.BaseDirectory;
+			var command = new PublishAzureWebsiteProject()
+			{
+				CommandRuntime = powerShellMock.Object,
+				WebsitesClient = clientMock.Object,
+				Name = websiteName,
+				ProjectFile = projectFile,
+				Configuration = configuration,
+				ConnectionString = connectionStrings,
+				SetParametersFile = setParametersFile
+			};
 
-            command.ExecuteCmdlet();
+			command.ExecuteCmdlet();
 
-            powerShellMock.Verify(f => f.WriteVerbose(string.Format("[Complete] Publishing package {0}", package)), Times.Once());
-            Assert.True(published);
-        }
-    }
+			powerShellMock.Verify(f => f.WriteVerbose(string.Format("[Complete] Publishing package {0}", package)), Times.Once());
+			Assert.True(published);
+		}
+	}
 }
