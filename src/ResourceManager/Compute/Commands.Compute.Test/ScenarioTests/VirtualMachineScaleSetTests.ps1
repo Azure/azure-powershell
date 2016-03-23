1380783609a55b435bcec9fd48c456c0b5babbe2
Hyonho Lee <hylee@microsoft.com>
2016-03-22 17:20:37 -0700

    Arrange test script file.

--- src/ResourceManager/Compute/Commands.Compute.Test/ScenarioTests/VirtualMachineScaleSetTests.ps1
+++ src/ResourceManager/Compute/Commands.Compute.Test/ScenarioTests/VirtualMachineScaleSetTests.ps1
@@ -20,38 +20,35 @@ PS C:\> Get-Command *VMSS* | ft Name,Version,ModuleName
 
 Name                                            Version ModuleName
 ----                                            ------- ----------
-Add-AzureRmVmssAdditionalUnattendContent        1.2.3   AzureRM.Compute
-Add-AzureRmVmssExtension                        1.2.3   AzureRM.Compute
-Add-AzureRmVMSshPublicKey                       1.2.3   AzureRM.Compute
-Add-AzureRmVmssListener                         1.2.3   AzureRM.Compute
-Add-AzureRmVmssNetworkInterfaceConfiguration    1.2.3   AzureRM.Compute
-Add-AzureRmVmssPublicKey                        1.2.3   AzureRM.Compute
-Add-AzureRmVmssSecret                           1.2.3   AzureRM.Compute
-Get-AzureRmVmss                                 1.2.3   AzureRM.Compute
-Get-AzureRmVmssSkusList                         1.2.3   AzureRM.Compute
-Get-AzureRmVmssVM                               1.2.3   AzureRM.Compute
-New-AzureRmVmss                                 1.2.3   AzureRM.Compute
-New-AzureRmVmssConfig                           1.2.3   AzureRM.Compute
-New-AzureRmVmssIpConfigurationConfig            1.2.3   AzureRM.Compute
-New-AzureRmVmssVaultCertificateConfig           1.2.3   AzureRM.Compute
-Remove-AzureRmVmss                              1.2.3   AzureRM.Compute
-Remove-AzureRmVmssAdditionalUnattendContent     1.2.3   AzureRM.Compute
-Remove-AzureRmVmssExtension                     1.2.3   AzureRM.Compute
-Remove-AzureRmVmssInstances                     1.2.3   AzureRM.Compute
-Remove-AzureRmVmssListener                      1.2.3   AzureRM.Compute
-Remove-AzureRmVmssNetworkInterfaceConfiguration 1.2.3   AzureRM.Compute
-Remove-AzureRmVmssPublicKey                     1.2.3   AzureRM.Compute
-Remove-AzureRmVmssSecret                        1.2.3   AzureRM.Compute
-Remove-AzureRmVmssVM                            1.2.3   AzureRM.Compute
-Restart-AzureRmVmss                             1.2.3   AzureRM.Compute
-Restart-AzureRmVmssVM                           1.2.3   AzureRM.Compute
-Set-AzureRmVmssOsProfile                        1.2.3   AzureRM.Compute
-Set-AzureRmVmssStorageProfile                   1.2.3   AzureRM.Compute
-Start-AzureRmVmss                               1.2.3   AzureRM.Compute
-Start-AzureRmVmssVM                             1.2.3   AzureRM.Compute
-Stop-AzureRmVmss                                1.2.3   AzureRM.Compute
-Stop-AzureRmVmssVM                              1.2.3   AzureRM.Compute
-Update-AzureRmVmssInstances                     1.2.3   AzureRM.Compute
+Add-AzureRmVmssAdditionalUnattendContent           1.2.4      AzureRM.Compute
+Add-AzureRmVmssExtension                           1.2.4      AzureRM.Compute
+Add-AzureRmVMSshPublicKey                          1.2.4      AzureRM.Compute
+Add-AzureRmVmssNetworkInterfaceConfiguration       1.2.4      AzureRM.Compute
+Add-AzureRmVmssSecret                              1.2.4      AzureRM.Compute
+Add-AzureRmVmssSshPublicKey                        1.2.4      AzureRM.Compute
+Add-AzureRmVmssWinRMListener                       1.2.4      AzureRM.Compute
+Get-AzureRmVmss                                    1.2.4      AzureRM.Compute
+Get-AzureRmVmssSku                                 1.2.4      AzureRM.Compute
+Get-AzureRmVmssVM                                  1.2.4      AzureRM.Compute
+New-AzureRmVmss                                    1.2.4      AzureRM.Compute
+New-AzureRmVmssConfig                              1.2.4      AzureRM.Compute
+New-AzureRmVmssIpConfigurationConfig               1.2.4      AzureRM.Compute
+New-AzureRmVmssVaultCertificateConfig              1.2.4      AzureRM.Compute
+Remove-AzureRmVmss                                 1.2.4      AzureRM.Compute
+Remove-AzureRmVmssAdditionalUnattendContent        1.2.4      AzureRM.Compute
+Remove-AzureRmVmssExtension                        1.2.4      AzureRM.Compute
+Remove-AzureRmVmssNetworkInterfaceConfiguration    1.2.4      AzureRM.Compute
+Remove-AzureRmVmssSecret                           1.2.4      AzureRM.Compute
+Remove-AzureRmVmssSshPublicKey                     1.2.4      AzureRM.Compute
+Remove-AzureRmVmssWinRMListener                    1.2.4      AzureRM.Compute
+Restart-AzureRmVmss                                1.2.4      AzureRM.Compute
+Set-AzureRmVmss                                    1.2.4      AzureRM.Compute
+Set-AzureRmVmssOsProfile                           1.2.4      AzureRM.Compute
+Set-AzureRmVmssStorageProfile                      1.2.4      AzureRM.Compute
+Set-AzureRmVmssVM                                  1.2.4      AzureRM.Compute
+Start-AzureRmVmss                                  1.2.4      AzureRM.Compute
+Stop-AzureRmVmss                                   1.2.4      AzureRM.Compute
+Update-AzureRmVmss                                 1.2.4      AzureRM.Compute
 #>
 function Test-VirtualMachineScaleSet
 {
@@ -64,12 +61,11 @@ function Test-VirtualMachineScaleSet
         $loc = 'westus';
         New-AzureRMResourceGroup -Name $rgname -Location $loc -Force;
 
-		# SRP
+        # SRP
         $stoname = 'sto' + $rgname;
         $stotype = 'Standard_GRS';
         New-AzureRMStorageAccount -ResourceGroupName $rgname -Name $stoname -Location $loc -Type $stotype;
         $stoaccount = Get-AzureRMStorageAccount -ResourceGroupName $rgname -Name $stoname;
-
 
         # NRP
         $subnet = New-AzureRMVirtualNetworkSubnetConfig -Name ('subnet' + $rgname) -AddressPrefix "10.0.0.0/24";
@@ -78,51 +74,46 @@ function Test-VirtualMachineScaleSet
         $subnetId = $vnet.Subnets[0].Id;
         $pubip = New-AzureRMPublicIpAddress -Force -Name ('pubip' + $rgname) -ResourceGroupName $rgname -Location $loc -AllocationMethod Dynamic -DomainNameLabel ('pubip' + $rgname);
         $pubip = Get-AzureRMPublicIpAddress -Name ('pubip' + $rgname) -ResourceGroupName $rgname;
-        #$pubipId = $pubip.Id;
-        #$nic = New-AzureRMNetworkInterface -Force -Name ('nic' + $rgname) -ResourceGroupName $rgname -Location $loc -SubnetId $subnetId -PublicIpAddressId $pubip.Id;
-        #$nic = Get-AzureRMNetworkInterface -Name ('nic' + $rgname) -ResourceGroupName $rgname;
-        #$nicId = $nic.Id;
 
-		# Create LoadBalancer
-		$frontendName = Get-ResourceName
-		$backendAddressPoolName = Get-ResourceName
-		$probeName = Get-ResourceName
-		$inboundNatPoolName = Get-ResourceName
-		$lbruleName = Get-ResourceName
-		$lbName = Get-ResourceName
+        # Create LoadBalancer
+        $frontendName = Get-ResourceName
+        $backendAddressPoolName = Get-ResourceName
+        $probeName = Get-ResourceName
+        $inboundNatPoolName = Get-ResourceName
+        $lbruleName = Get-ResourceName
+        $lbName = Get-ResourceName
 
-		$frontend = New-AzureRmLoadBalancerFrontendIpConfig -Name $frontendName -PublicIpAddress $pubip
+        $frontend = New-AzureRmLoadBalancerFrontendIpConfig -Name $frontendName -PublicIpAddress $pubip
         $backendAddressPool = New-AzureRmLoadBalancerBackendAddressPoolConfig -Name $backendAddressPoolName
         $probe = New-AzureRmLoadBalancerProbeConfig -Name $probeName -RequestPath healthcheck.aspx -Protocol http -Port 80 -IntervalInSeconds 15 -ProbeCount 2
-		$inboundNatPool = New-AzureRmLoadBalancerInboundNatPoolConfig -Name $inboundNatPoolName -FrontendIPConfigurationId $frontend.Id -Protocol Tcp -FrontendPortRangeStart 3360 -FrontendPortRangeEnd 3362 -BackendPort 3370 
-        $lbrule = New-AzureRmLoadBalancerRuleConfig -Name $lbruleName -FrontendIPConfiguration $frontend -BackendAddressPool $backendAddressPool -Probe $probe -Protocol Tcp -FrontendPort 80 -BackendPort 80 -IdleTimeoutInMinutes 15 -EnableFloatingIP -LoadDistribution SourceIP
-        $actualLb = New-AzureRmLoadBalancer -Name $lbName -ResourceGroupName $rgname -Location $loc -FrontendIpConfiguration $frontend -BackendAddressPool $backendAddressPool -Probe $probe -LoadBalancingRule $lbrule -InboundNatPool $inboundNatPool
+        $inboundNatPool = New-AzureRmLoadBalancerInboundNatPoolConfig -Name $inboundNatPoolName -FrontendIPConfigurationId `
+            $frontend.Id -Protocol Tcp -FrontendPortRangeStart 3360 -FrontendPortRangeEnd 3362 -BackendPort 3370;
+        $lbrule = New-AzureRmLoadBalancerRuleConfig -Name $lbruleName `
+            -FrontendIPConfiguration $frontend -BackendAddressPool $backendAddressPool `
+            -Probe $probe -Protocol Tcp -FrontendPort 80 -BackendPort 80 `
+            -IdleTimeoutInMinutes 15 -EnableFloatingIP -LoadDistribution SourceIP;
+        $actualLb = New-AzureRmLoadBalancer -Name $lbName -ResourceGroupName $rgname -Location $loc `
+            -FrontendIpConfiguration $frontend -BackendAddressPool $backendAddressPool `
+            -Probe $probe -LoadBalancingRule $lbrule -InboundNatPool $inboundNatPool;
         $expectedLb = Get-AzureRmLoadBalancer -Name $lbName -ResourceGroupName $rgname
 
-		# Verification
+        # Verification
         Assert-AreEqual $expectedLb.ResourceGroupName $actualLb.ResourceGroupName
         Assert-AreEqual $expectedLb.Name $actualLb.Name
         Assert-AreEqual $expectedLb.Location $actualLb.Location
         Assert-AreEqual "Succeeded" $expectedLb.ProvisioningState
         Assert-NotNull $expectedLb.ResourceGuid
         Assert-AreEqual 1 @($expectedLb.FrontendIPConfigurations).Count
-        
-		Assert-AreEqual $frontendName $expectedLb.FrontendIPConfigurations[0].Name
+        Assert-AreEqual $frontendName $expectedLb.FrontendIPConfigurations[0].Name
         Assert-AreEqual $pubip.Id $expectedLb.FrontendIPConfigurations[0].PublicIpAddress.Id
         Assert-Null $expectedLb.FrontendIPConfigurations[0].PrivateIpAddress
-
         Assert-AreEqual $backendAddressPoolName $expectedLb.BackendAddressPools[0].Name
-
         Assert-AreEqual $probeName $expectedLb.Probes[0].Name
         Assert-AreEqual $probe.RequestPath $expectedLb.Probes[0].RequestPath
-
         Assert-AreEqual $expectedLb.FrontendIPConfigurations[0].Id $expectedLb.InboundNatPools[0].FrontendIPConfiguration.Id
         Assert-AreEqual $lbruleName $expectedLb.LoadBalancingRules[0].Name
         Assert-AreEqual $expectedLb.FrontendIPConfigurations[0].Id $expectedLb.LoadBalancingRules[0].FrontendIPConfiguration.Id
         Assert-AreEqual $expectedLb.BackendAddressPools[0].Id $expectedLb.LoadBalancingRules[0].BackendAddressPool.Id
-
-
-
         
         # New VMSS Parameters
         $vmssName = 'vmss' + $rgname;
@@ -135,29 +126,14 @@ function Test-VirtualMachineScaleSet
         $vhdContainer = "https://" + $stoname + ".blob.core.windows.net/" + $vmssName;
 
         $aucComponentName="Microsoft-Windows-Shell-Setup";
-		$aucComponentName="MicrosoftWindowsShellSetup";
+        $aucComponentName="MicrosoftWindowsShellSetup";
         $aucPassName ="oobeSystem";
         $aucSetting = "AutoLogon";
         $aucContent = "<UserAccounts><AdministratorPassword><Value>password</Value><PlainText>true</PlainText></AdministratorPassword></UserAccounts>";
-
-
-
-		#$frontendipconfig = New-Object Microsoft.Azure.Management.Compute.Models.IPConfigurationLoadBalancerInboundNATPool;
-		#Write-Verbose ("Inbound: " + $expectedLb.InboundNatPools[0].Id);
-		#$frontendipconfig.ReferenceUri = $expectedLb.InboundNatPools[0].Id;
-		#$ipCfg.LoadBalancerInboundNatPools = New-Object System.Collections.Generic.List``1[Microsoft.Azure.Management.Compute.Models.IPConfigurationLoadBalancerInboundNATPool];		
-		#$ipCfg.LoadBalancerInboundNatPools.Add($frontendipconfig);
-
-		#$backendipconfig = New-Object Microsoft.Azure.Management.Compute.Models.IPConfigurationLoadBalancerAddressPool;
-		#Write-Verbose ("Back: " + $expectedLb.BackendAddressPools[0].Id);
-		#$backendipconfig.ReferenceUri = $expectedLb.BackendAddressPools[0].Id;
-		#$ipCfg.LoadBalancerBackendAddressPools = New-Object System.Collections.Generic.List``1[Microsoft.Azure.Management.Compute.Models.IPConfigurationLoadBalancerAddressPool];
-		#$ipCfg.LoadBalancerBackendAddressPools.Add($backendipconfig);		
-
-
-        $ipCfg = New-AzureRmVmssIPConfigurationConfig -Name 'test' -LoadBalancerInboundNatPoolsId $expectedLb.InboundNatPools[0].Id -LoadBalancerBackendAddressPoolsId $expectedLb.BackendAddressPools[0].Id -SubnetId $subnetId;
-
-
+        $ipCfg = New-AzureRmVmssIPConfigurationConfig -Name 'test' `
+            -LoadBalancerInboundNatPoolsId $expectedLb.InboundNatPools[0].Id `
+            -LoadBalancerBackendAddressPoolsId $expectedLb.BackendAddressPools[0].Id `
+            -SubnetId $subnetId;
         $vmss = New-AzureRmVmssConfig -Location $loc -SkuCapacity 2 -SkuName 'Standard_A0' -UpgradePolicyMode 'automatic' -NetworkInterfaceConfiguration $netCfg `
             | Add-AzureRmVmssNetworkInterfaceConfiguration -Name 'test' -Primary $true -IPConfiguration $ipCfg `
             | Set-AzureRmVmssOSProfile -ComputerNamePrefix 'test' -AdminUsername $adminUsername -AdminPassword $adminPassword `
@@ -238,37 +214,33 @@ function Test-VirtualMachineScaleSet
         # Stop/Start/Restart Operation
         for ($i = 0; $i -lt 2; $i++)
         {
-			$argList = New-AzureComputeArgumentList -MethodName VirtualMachineScaleSetVMsPowerOff;
-			$argList[0].Value = $rgname;
-			$argList[1].Value = $vmssName;
-			$argList[2].Value = $i.ToString();
-			$args = ($argList | select -ExpandProperty Value);
-			Invoke-AzureComputeMethod -MethodName VirtualMachineScaleSetVMPowerOff -ArgumentList $args;
+            $argList = New-AzureComputeArgumentList -MethodName VirtualMachineScaleSetVMsPowerOff;
+            $argList[0].Value = $rgname;
+            $argList[1].Value = $vmssName;
+            $argList[2].Value = $i.ToString();
+            $args = ($argList | select -ExpandProperty Value);
+            Invoke-AzureComputeMethod -MethodName VirtualMachineScaleSetVMPowerOff -ArgumentList $args;
 
-			$argList = New-AzureComputeArgumentList -MethodName VirtualMachineScaleSetVMsDeallocate;
-			$argList[0].Value = $rgname;
-			$argList[1].Value = $vmssName;
-			$argList[2].Value = $i.ToString();;
-			$args = ($argList | select -ExpandProperty Value);
-			Invoke-AzureComputeMethod -MethodName VirtualMachineScaleSetVMDeallocate -ArgumentList $args;
+            $argList = New-AzureComputeArgumentList -MethodName VirtualMachineScaleSetVMsDeallocate;
+            $argList[0].Value = $rgname;
+            $argList[1].Value = $vmssName;
+            $argList[2].Value = $i.ToString();;
+            $args = ($argList | select -ExpandProperty Value);
+            Invoke-AzureComputeMethod -MethodName VirtualMachineScaleSetVMDeallocate -ArgumentList $args;
 
-			$argList = New-AzureComputeArgumentList -MethodName VirtualMachineScaleSetVMsStart;
-			$argList[0].Value = $rgname;
-			$argList[1].Value = $vmssName;
-			$argList[2].Value = $i.ToString();;
-			$args = ($argList | select -ExpandProperty Value);
-			Invoke-AzureComputeMethod -MethodName VirtualMachineScaleSetVMStart -ArgumentList $args;
+            $argList = New-AzureComputeArgumentList -MethodName VirtualMachineScaleSetVMsStart;
+            $argList[0].Value = $rgname;
+            $argList[1].Value = $vmssName;
+            $argList[2].Value = $i.ToString();;
+            $args = ($argList | select -ExpandProperty Value);
+            Invoke-AzureComputeMethod -MethodName VirtualMachineScaleSetVMStart -ArgumentList $args;
 
-			$argList = New-AzureComputeArgumentList -MethodName VirtualMachineScaleSetVMsRestart;
-			$argList[0].Value = $rgname;
-			$argList[1].Value = $vmssName;
-			$argList[2].Value = $i.ToString();;
-			$args = ($argList | select -ExpandProperty Value);
-			Invoke-AzureComputeMethod -MethodName VirtualMachineScaleSetVMRestart -ArgumentList $args;
-            #$st = Stop-AzureRmVmssVM -StayProvision -ResourceGroupName $rgname -VMScaleSetName $vmssName -InstanceId $i;
-            #$st = Stop-AzureRmVmssVM -ResourceGroupName $rgname -VMScaleSetName $vmssName -InstanceId $i;
-            #$st = Start-AzureRmVmssVM -ResourceGroupName $rgname -VMScaleSetName $vmssName -InstanceId $i;
-            #$st = Restart-AzureRmVmssVM -ResourceGroupName $rgname -VMScaleSetName $vmssName -InstanceId $i;
+            $argList = New-AzureComputeArgumentList -MethodName VirtualMachineScaleSetVMsRestart;
+            $argList[0].Value = $rgname;
+            $argList[1].Value = $vmssName;
+            $argList[2].Value = $i.ToString();;
+            $args = ($argList | select -ExpandProperty Value);
+            Invoke-AzureComputeMethod -MethodName VirtualMachineScaleSetVMRestart -ArgumentList $args;
         }
 
         $st = Stop-AzureRmVmss -StayProvision -ResourceGroupName $rgname -VMScaleSetName $vmssName;
@@ -290,27 +262,27 @@ function Test-VirtualMachineScaleSet
         # Remove
         $st = Remove-AzureRmVmss -ResourceGroupName $rgname -VMScaleSetName $vmssName -InstanceId 1;
 
-		$argList = New-AzureComputeArgumentList -MethodName VirtualMachineScaleSetVMsDelete;
-	    $argList[0].Value = $rgname;
-		$argList[1].Value = $vmssName;
-		$i = 0;
-		$argList[2].Value = $i.ToString();
-		$args = ($argList | select -ExpandProperty Value);
+        $argList = New-AzureComputeArgumentList -MethodName VirtualMachineScaleSetVMsDelete;
+        $argList[0].Value = $rgname;
+        $argList[1].Value = $vmssName;
+        $i = 0;
+        $argList[2].Value = $i.ToString();
+        $args = ($argList | select -ExpandProperty Value);
 
-		try
-		{
-			Invoke-AzureComputeMethod -MethodName VirtualMachineScaleSetVMDelete -ArgumentList $args;
-		}
-		catch
-		{
-			$actualMessage = $_.Exception.Message;
+        try
+        {
+            Invoke-AzureComputeMethod -MethodName VirtualMachineScaleSetVMDelete -ArgumentList $args;
+        }
+        catch
+        {
+            $actualMessage = $_.Exception.Message;
             Write-Output ("Caught exception: '$actualMessage'");
-			if (-not $actualMessage.Contains("BadRequest"))
-			{
-				throw "Expected exception does not contain expected text 'BadRequest', the actual message is '$actualMessage'";
-			}
-		}
-		
+            if (-not $actualMessage.Contains("BadRequest"))
+            {
+                throw "Expected exception does not contain expected text 'BadRequest', the actual message is '$actualMessage'";
+            }
+        }
+
         $st = Remove-AzureRmVmss -ResourceGroupName $rgname -VMScaleSetName $vmssName;
     }
     finally
