5c774504f65e9495873572530ea74fd966b329ee
Hovsep Mkrtchyan <hovsepm@microsoft.com>
2016-04-04 11:52:58 -0700

    Bumping up Arm Compute and ASM modules.

--- src/Common/Commands.Common.Authentication.Test/Properties/AssemblyInfo.cs
+++ src/Common/Commands.Common.Authentication.Test/Properties/AssemblyInfo.cs
@@ -28,6 +28,6 @@ using Xunit;
 
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyFileVersion("1.3.0")]
+[assembly: AssemblyVersion("1.3.1")]
+[assembly: AssemblyFileVersion("1.3.1")]
 [assembly: CollectionBehavior(DisableTestParallelization = true)]
