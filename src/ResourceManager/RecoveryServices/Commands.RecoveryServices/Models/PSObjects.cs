ca483e58ecbf7d40e44cf6c40fac8a910f1de411
vishak-ms <vishak@microsoft.com>
2016-04-12 18:06:26 +0530

    Revert "Get/Set AzureRmRecoveryServicesBackupProperties"
    This reverts commit 7471ce80f4fb122ed7c202e469a95c5b8276dc31.

--- src/ResourceManager/RecoveryServices/Commands.RecoveryServices/Models/PSObjects.cs
+++ src/ResourceManager/RecoveryServices/Commands.RecoveryServices/Models/PSObjects.cs
@@ -135,23 +135,11 @@ namespace Microsoft.Azure.Commands.RecoveryServices
     public class ASRVaultProperties
     {
         #region Properties
-
+        
         /// <summary>
         /// Gets or sets Provisioning State.
         /// </summary>
         public string ProvisioningState { get; set; }
-
-        #endregion
-    }
-
-    public class ASRVaultBackupProperties
-    {
-        #region Properties
-
-        /// <summary>
-        /// Gets or sets BackupStorageRedundancy type.
-        /// </summary>
-        public string BackupStorageRedundancy { get; set; }
 
         #endregion
     }
