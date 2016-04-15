e44c657e7dd6db4a1fe3e748d17749b59d401243
Tudor Didilescu <tudor.didilescu@live.com>
2016-04-15 14:57:23 -0700

    Pushing in current format of services cmdlets + helper code

---
+++ src/ResourceManager/MachineLearning/Commands.MLWebServices/Cmdlets/GetAzureMLWebService.cs
@@ -0,0 +1,79 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Management.Automation;
+using System.Threading.Tasks;
+using Microsoft.Azure.Commands.ResourceManager.Cmdlets.Components;
+using Microsoft.Azure.Management.MachineLearning.WebServices.Models;
+
+namespace Microsoft.Azure.Commands.MachineLearning
+{
+    [Cmdlet(VerbsCommon.Get, WebServicesCmdletBase.CommandletSuffix)]
+    public class GetAzureMLWebService : WebServicesCmdletBase
+    {
+        private const string GetMlWebServiceByNameGroupParameterSet = "Get an Azure ML web service resource details by name and group.";
+
+        private const string GetMlWebServicesByGroupParameterSet = "Get the paginated list of Azure ML web service resources within a resource group.";
+
+        private const string GetMlWebServicesInSubscriptionParameterSet = "Get the paginated list of Azure ML web service resources within the current subscription.";
+
+        [Parameter(ParameterSetName = GetAzureMLWebService.GetMlWebServiceByNameGroupParameterSet, Mandatory = true, HelpMessage = "The name of the resource group for the Azure ML web service.")]
+        [Parameter(ParameterSetName = GetAzureMLWebService.GetMlWebServicesByGroupParameterSet, Mandatory = true, HelpMessage = "The name of the resource group for the Azure ML web services.")]
+        [ValidateNotNullOrEmpty]
+        public string ResourceGroupName { get; set; }
+
+        [Parameter(ParameterSetName = GetAzureMLWebService.GetMlWebServiceByNameGroupParameterSet, Mandatory = true, HelpMessage = "The name of the web service.")]
+        [ValidateNotNullOrEmpty]
+        public string Name { get; set; }
+
+        [Parameter(ParameterSetName = GetAzureMLWebService.GetMlWebServicesByGroupParameterSet, Mandatory = true, HelpMessage = "When specified, ensures that the query is run against a paginated list of services instead of a resource.")]
+        [Parameter(ParameterSetName = GetAzureMLWebService.GetMlWebServicesInSubscriptionParameterSet, Mandatory = true, HelpMessage = "When specified, ensures that the query is run against a paginated list of services instead of a resource.")]
+        public SwitchParameter IsCollection { get; set; }
+
+        protected override void RunCmdlet()
+        {
+            // If this is a simple get web service by name operation, resolve it as such
+            if (string.Equals(this.ParameterSetName, GetAzureMLWebService.GetMlWebServiceByNameGroupParameterSet, StringComparison.OrdinalIgnoreCase))
+            {
+                WebService service = this.WebServicesClient.GetAzureMlWebService(this.SubscriptionId, this.ResourceGroupName, this.Name);
+                this.WriteObject(service);
+            }
+            else
+            {
+                // This is a collection of web services get call, so determine which flavor it is
+                Func<Task<ResponseWithContinuation<WebService[]>>> getFirstServicesPageCall = () => this.WebServicesClient.GetAzureMlWebServicesBySubscriptionAsync(this.SubscriptionId, null, this.CancellationToken);
+                Func<string, Task<ResponseWithContinuation<WebService[]>>> getNextPageCall = nextLink => this.WebServicesClient.GetAzureMlWebServicesBySubscriptionAsync(this.SubscriptionId, nextLink, this.CancellationToken);
+                if (this.ResourceGroupName != null)
+                {
+                    // This is a call for resource retrieval within a resource group
+                    getFirstServicesPageCall = () => this.WebServicesClient.GetAzureMlWebServicesBySubscriptionAndGroupAsync(this.SubscriptionId, this.ResourceGroupName, null, this.CancellationToken);
+                    getNextPageCall = nextLink => this.WebServicesClient.GetAzureMlWebServicesBySubscriptionAndGroupAsync(this.SubscriptionId, this.ResourceGroupName, nextLink, this.CancellationToken);
+                }
+
+                PaginatedResponseHelper.ForEach<WebService>(
+                    getFirstPage: getFirstServicesPageCall,
+                    getNextPage: getNextPageCall,
+                    cancellationToken: this.CancellationToken,
+                    action: webServices =>
+                    {
+                        foreach (var service in webServices)
+                        {
+                            this.WriteObject(service, true);
+                        }
+                    });
+            }
+        }
+    }
+}
---
+++ src/ResourceManager/MachineLearning/Commands.MLWebServices/Cmdlets/RemoveAzureMLWebService.cs
@@ -0,0 +1,69 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Management.Automation;
+using Microsoft.Azure.Commands.MachineLearning.WebServices.Utilities;
+using Microsoft.Azure.Management.MachineLearning.WebServices.Models;
+using Microsoft.WindowsAzure.Commands.Common;
+
+namespace Microsoft.Azure.Commands.MachineLearning
+{
+    [Cmdlet(VerbsCommon.Remove, WebServicesCmdletBase.CommandletSuffix)]
+    public class RemoveAzureMLWebService : WebServicesCmdletBase
+    {
+        protected const string RemoveByNameGroupParameterSet = "Remove an Azure ML web service resouce by name and resource group.";
+        protected const string RemoveByObjectParameterSet = "Remove an Azure ML web service specified as an object.";
+
+        [Parameter(ParameterSetName = RemoveAzureMLWebService.RemoveByNameGroupParameterSet, Position = 0, Mandatory = true, HelpMessage = "The name of the resource group for the Azure ML web service.")]
+        [ValidateNotNullOrEmpty]
+        public string ResourceGroupName { get; set; }
+
+        [Parameter(ParameterSetName = RemoveAzureMLWebService.RemoveByNameGroupParameterSet, Position = 1, Mandatory = true, HelpMessage = "The name of the web service.")]
+        [ValidateNotNullOrEmpty]
+        public string Name { get; set; }
+
+        [Parameter(ParameterSetName = RemoveByObjectParameterSet, Position = 0, Mandatory = true, HelpMessage = "The machine learning web service object.", ValueFromPipeline = true)]
+        [ValidateNotNullOrEmpty]
+        public WebService MlWebService { get; set; }
+
+        [Parameter(Mandatory = false, HelpMessage = "Do not ask for confirmation.")]
+        public SwitchParameter Force { get; set; }
+
+        protected override void RunCmdlet()
+        {
+            if (string.Equals(this.ParameterSetName, RemoveAzureMLWebService.RemoveByObjectParameterSet, StringComparison.OrdinalIgnoreCase))
+            {
+                string subscriptionId, resourceGroup, webServiceName;
+                if (!CmdletHelpers.TryParseMlWebServiceMetadataFromResourceId(this.MlWebService.Id, out subscriptionId, out resourceGroup, out webServiceName))
+                {
+                    throw new ValidationMetadataException(WebServices.Resources.InvalidWebServiceIdOnObject);
+                }
+
+                this.ResourceGroupName = resourceGroup;
+                this.Name = webServiceName;
+            }
+
+            this.ConfirmAction(
+                force: this.Force.IsPresent,
+                actionMessage: WebServices.Resources.RemoveMlServiceWarning.FormatInvariant(this.Name),
+                processMessage: @"Deleting machine learning web service..",
+                target: this.Name,
+                action: () =>
+                {
+                    this.WebServicesClient.DeleteAzureMlWebService(this.SubscriptionId, this.ResourceGroupName, this.Name);
+                });
+        }
+    }
+}
