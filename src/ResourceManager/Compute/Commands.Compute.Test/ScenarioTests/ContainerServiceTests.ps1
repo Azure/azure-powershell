84e7420c5c90d7ba45ef05b1b96bf3d82d7d8dcd
Hyonho Lee <hylee@microsoft.com>
2016-04-21 16:31:32 -0700

    Add ContainerService tests

---
+++ src/ResourceManager/Compute/Commands.Compute.Test/ScenarioTests/ContainerServiceTests.ps1
@@ -0,0 +1,210 @@
+﻿# ----------------------------------------------------------------------------------
+#
+# Copyright Microsoft Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------------
+
+<#
+.SYNOPSIS
+Test Container Service
+
+PS C:\> Get-Command *VMSS* | ft Name,Version,ModuleName
+
+Name                                            Version ModuleName
+----                                            ------- ----------
+
+#>
+
+<#
+.SYNOPSIS
+Test Virtual Machine Scale Set
+#>
+function Test-ContainerService
+{
+    # Setup
+    $rgname = Get-ComputeTestResourceName
+
+    try
+    {
+        # Common
+        $loc = 'australiasoutheast';
+        New-AzureRMResourceGroup -Name $rgname -Location $loc -Force;
+
+
+		$csName = 'cs' + $rgname;
+		$masterDnsPrefixName = 'master' + $rgname;
+		$agentPoolDnsPrefixName = 'ap' + $rgname;
+		$agentPoolProfileName = 'AgentPool1';
+		$vmSize = 'Standard_A1';
+
+		$orchestratorType = 'DCOS';
+		$adminUserName = 'acsLinuxAdmin';
+		$sshPublicKey =
+            "MIIDszCCApugAwIBAgIJALBV9YJCF/tAMA0GCSqGSIb3DQEBBQUAMEUxCzAJBgNV" +
+            "BAYTAkFVMRMwEQYDVQQIEwpTb21lLVN0YXRlMSEwHwYDVQQKExhJbnRlcm5ldCBX" +
+            "aWRnaXRzIFB0eSBMdGQwHhcNMTUwMzIyMjI1NDQ5WhcNMTYwMzIxMjI1NDQ5WjBF" +
+            "MQswCQYDVQQGEwJBVTETMBEGA1UECBMKU29tZS1TdGF0ZTEhMB8GA1UEChMYSW50" +
+            "ZXJuZXQgV2lkZ2l0cyBQdHkgTHRkMIIBIDANBgkqhkiG9w0BAQEFAAOCAQ0AMIIB" +
+            "CAKCAQEAxDC+OfmB+tQ+P1MLmuuW2hJLdcK8m4DLgAk5l8bQDNBcVezt+bt/ZFMs" +
+            "CHBhfTZG9O9yqMn8IRUh7/7jfQm6DmXCgtxj/uFiwT+F3out5uWvMV9SjFYvu9kJ" +
+            "NXiDC2u3l4lHV8eHde6SbKiZB9Jji9FYQV4YiWrBa91j9I3hZzbTL0UCiJ+1PPoL" +
+            "Rx/T1s9KT5Wn8m/z2EDrHWpetYu45OA7nzyIFOyQup5oWadWNnpk6HkCGutl9t9b" +
+            "cLdjXjXPm9wcy1yxIB3Dj/Y8Hnulr80GJlUtUboDm8TExGc4YaPJxdn0u5igo5gZ" +
+            "c6qnqH/BMd1nsyICx6AZnKBXBycoSQIBI6OBpzCBpDAdBgNVHQ4EFgQUzWhrCCDs" +
+            "ClANCGlKZ64rHp2BDn0wdQYDVR0jBG4wbIAUzWhrCCDsClANCGlKZ64rHp2BDn2h" +
+            "SaRHMEUxCzAJBgNVBAYTAkFVMRMwEQYDVQQIEwpTb21lLVN0YXRlMSEwHwYDVQQK" +
+            "ExhJbnRlcm5ldCBXaWRnaXRzIFB0eSBMdGSCCQCwVfWCQhf7QDAMBgNVHRMEBTAD" +
+            "AQH/MA0GCSqGSIb3DQEBBQUAA4IBAQCUaJnX0aBzwBkbJrBS5YvkZnNKLdc4oHgC" +
+            "/Nsr/9pwXzFYYXkdqpTw2nygH0C0WuPVVrG3Y3EGx/UIGDtLbwMvZJhQN9mZH3oX" +
+            "+c3HGqBnXGuDRrtsfsK1ywAofx9amZfKNk/04/Rt3POdbyD1/AOADw2zMokbIapX" +
+            "+nMDUtD/Tew9+0qU9+dcFMrFE1N4utlrFHlrLFbiCA/eSegP6gOeu9mqZv7UHIz2" +
+            "oe6IQTw7zJF7xuBIzTYwjOCM197GKW7xc4GU4JZIN+faZ7njl/fxfUNdlqvgZUUn" +
+            "kfdrzU3PZPl0w9NuncgEje/PZ+YtZvIsnH7MLSPeIGNQwW6V2kc8";
+
+		#$container = New-AzureRmContainerServiceConfig -Location $loc `
+		#    -OrchestratorProfileOrchestratorType $orchestratorType `
+		#    -MasterProfileDnsPrefix $masterDnsPrefixName `
+		#    -AdminUsername $adminUserName `
+		#    -Ssh $sshPublicKey;
+
+		#$container = Add-AzureRmContainerServiceAgentPoolProfile -ContainerService $container `
+		#    -Name $agentPoolProfileName `
+		#    -VmSize $vmSize `
+		#    -DnsPrefix $agentPoolDnsPrefixName;
+
+		#$st = New-AzureRmContainerService -ResourceGroupName $rgname -ContainerServiceName $csName -ContainerService $container;
+
+
+		$container = New-AzureRmContainerServiceConfig -Location $loc -OrchestratorProfileOrchestratorType $orchestratorType `
+		    -MasterProfileDnsPrefix $masterDnsPrefixName -AdminUsername $adminUserName -Ssh $sshPublicKey `
+		| Add-AzureRmContainerServiceAgentPoolProfile -Name $agentPoolProfileName -VmSize $vmSize -DnsPrefix $agentPoolDnsPrefixName `
+		| New-AzureRmContainerService -ResourceGroupName $rgname -ContainerServiceName $csName;
+
+
+		$cs = Get-AzureRmContainerService -ResourceGroupName $rgname -ContainerServiceName $csName;
+
+		$cslist = Get-AzureRmContainerService -ResourceGroupName $rgname;
+
+		$st = Remove-AzureRmContainerService -ResourceGroupName $rgname -ContainerServiceName $csName;
+    }
+    finally
+    {
+        # Cleanup
+        Clean-ResourceGroup $rgname
+    }
+}
+
+<#
+.SYNOPSIS
+Test Virtual Machine Scale Set
+#>
+function Test-ContainerServiceUpdate
+{
+    # Setup
+    $rgname = Get-ComputeTestResourceName
+
+    try
+    {
+        # Common
+        $loc = 'australiasoutheast';
+        New-AzureRMResourceGroup -Name $rgname -Location $loc -Force;
+
+
+		$csName = 'cs' + $rgname;
+		$masterDnsPrefixName = 'master' + $rgname;
+		$agentPoolDnsPrefixName = 'ap' + $rgname;
+		$agentPoolProfileName = 'AgentPool1';
+		$vmSize = 'Standard_A1';
+
+		$orchestratorType = 'DCOS';
+		$adminUserName = 'acsLinuxAdmin';
+		$sshPublicKey =
+            "MIIDszCCApugAwIBAgIJALBV9YJCF/tAMA0GCSqGSIb3DQEBBQUAMEUxCzAJBgNV" +
+            "BAYTAkFVMRMwEQYDVQQIEwpTb21lLVN0YXRlMSEwHwYDVQQKExhJbnRlcm5ldCBX" +
+            "aWRnaXRzIFB0eSBMdGQwHhcNMTUwMzIyMjI1NDQ5WhcNMTYwMzIxMjI1NDQ5WjBF" +
+            "MQswCQYDVQQGEwJBVTETMBEGA1UECBMKU29tZS1TdGF0ZTEhMB8GA1UEChMYSW50" +
+            "ZXJuZXQgV2lkZ2l0cyBQdHkgTHRkMIIBIDANBgkqhkiG9w0BAQEFAAOCAQ0AMIIB" +
+            "CAKCAQEAxDC+OfmB+tQ+P1MLmuuW2hJLdcK8m4DLgAk5l8bQDNBcVezt+bt/ZFMs" +
+            "CHBhfTZG9O9yqMn8IRUh7/7jfQm6DmXCgtxj/uFiwT+F3out5uWvMV9SjFYvu9kJ" +
+            "NXiDC2u3l4lHV8eHde6SbKiZB9Jji9FYQV4YiWrBa91j9I3hZzbTL0UCiJ+1PPoL" +
+            "Rx/T1s9KT5Wn8m/z2EDrHWpetYu45OA7nzyIFOyQup5oWadWNnpk6HkCGutl9t9b" +
+            "cLdjXjXPm9wcy1yxIB3Dj/Y8Hnulr80GJlUtUboDm8TExGc4YaPJxdn0u5igo5gZ" +
+            "c6qnqH/BMd1nsyICx6AZnKBXBycoSQIBI6OBpzCBpDAdBgNVHQ4EFgQUzWhrCCDs" +
+            "ClANCGlKZ64rHp2BDn0wdQYDVR0jBG4wbIAUzWhrCCDsClANCGlKZ64rHp2BDn2h" +
+            "SaRHMEUxCzAJBgNVBAYTAkFVMRMwEQYDVQQIEwpTb21lLVN0YXRlMSEwHwYDVQQK" +
+            "ExhJbnRlcm5ldCBXaWRnaXRzIFB0eSBMdGSCCQCwVfWCQhf7QDAMBgNVHRMEBTAD" +
+            "AQH/MA0GCSqGSIb3DQEBBQUAA4IBAQCUaJnX0aBzwBkbJrBS5YvkZnNKLdc4oHgC" +
+            "/Nsr/9pwXzFYYXkdqpTw2nygH0C0WuPVVrG3Y3EGx/UIGDtLbwMvZJhQN9mZH3oX" +
+            "+c3HGqBnXGuDRrtsfsK1ywAofx9amZfKNk/04/Rt3POdbyD1/AOADw2zMokbIapX" +
+            "+nMDUtD/Tew9+0qU9+dcFMrFE1N4utlrFHlrLFbiCA/eSegP6gOeu9mqZv7UHIz2" +
+            "oe6IQTw7zJF7xuBIzTYwjOCM197GKW7xc4GU4JZIN+faZ7njl/fxfUNdlqvgZUUn" +
+            "kfdrzU3PZPl0w9NuncgEje/PZ+YtZvIsnH7MLSPeIGNQwW6V2kc8";
+
+		#$container = New-AzureRmContainerServiceConfig -Location $loc `
+		#    -OrchestratorProfileOrchestratorType $orchestratorType `
+		#    -MasterProfileDnsPrefix $masterDnsPrefixName `
+		#    -AdminUsername $adminUserName `
+		#    -Ssh $sshPublicKey;
+
+		#$container = Add-AzureRmContainerServiceAgentPoolProfile -ContainerService $container `
+		#    -Name $agentPoolProfileName `
+		#    -VmSize $vmSize `
+		#    -DnsPrefix $agentPoolDnsPrefixName;
+
+		#$st = New-AzureRmContainerService -ResourceGroupName $rgname -ContainerServiceName $csName -ContainerService $container;
+
+
+		$container = New-AzureRmContainerServiceConfig -Location $loc `
+		    -OrchestratorProfileOrchestratorType $orchestratorType `
+		    -MasterProfileDnsPrefix $masterDnsPrefixName `
+		    -MasterProfileCount 1 `
+		    -AdminUsername $adminUserName `
+		    -Ssh $sshPublicKey `
+		| Add-AzureRmContainerServiceAgentPoolProfile -Name $agentPoolProfileName `
+		    -VmSize $vmSize `
+		    -DnsPrefix $agentPoolDnsPrefixName `
+		    -Count 1 `
+		| New-AzureRmContainerService -ResourceGroupName $rgname -ContainerServiceName $csName;
+
+
+
+
+		Get-AzureRmContainerService -ResourceGroupName $rgname -ContainerServiceName $csName `
+		| Remove-AzureRmContainerServiceAgentPoolProfile -Name $agentPoolProfileName `
+		| Add-AzureRmContainerServiceAgentPoolProfile -Name $agentPoolProfileName `
+		    -VmSize $vmSize `
+		    -DnsPrefix $agentPoolDnsPrefixName `
+		    -Count 2 `
+		| Update-AzureRmContainerService -ResourceGroupName $rgname -ContainerServiceName $csName;
+
+		#$cs = Get-AzureRmContainerService -ResourceGroupName $rgname -ContainerServiceName $csName;
+
+		#$cs = Remove-AzureRmContainerServiceAgentPoolProfile -Name $agentPoolProfileName -ContainerService $cs;
+
+		#$cs = Add-AzureRmContainerServiceAgentPoolProfile -Name $agentPoolProfileName `
+		#    -VmSize $vmSize `
+		#    -DnsPrefix $agentPoolDnsPrefixName `
+		#    -Count 2 `
+		#    -ContainerService $cs;
+
+		#$st = Update-AzureRmContainerService -ResourceGroupName $rgname -ContainerServiceName $csName -ContainerService $cs;
+
+		$cs = Get-AzureRmContainerService -ResourceGroupName $rgname -ContainerServiceName $csName;
+		
+		$st = Remove-AzureRmContainerService -ResourceGroupName $rgname -ContainerServiceName $csName;
+    }
+    finally
+    {
+        # Cleanup
+        Clean-ResourceGroup $rgname
+    }
+}
+
