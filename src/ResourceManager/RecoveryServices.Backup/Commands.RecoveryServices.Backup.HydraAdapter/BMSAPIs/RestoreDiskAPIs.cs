cbb5bda0b331946988e296c9093c2e94a5b34494
mkherani <mkherani@microsoft.com>
2016-04-27 01:43:18 +0530

    Fixing PR comment

--- src/ResourceManager/RecoveryServices.Backup/Commands.RecoveryServices.Backup.HydraAdapter/BMSAPIs/RestoreDiskAPIs.cs
+++ src/ResourceManager/RecoveryServices.Backup/Commands.RecoveryServices.Backup.HydraAdapter/BMSAPIs/RestoreDiskAPIs.cs
@@ -12,7 +12,7 @@ namespace Microsoft.Azure.Commands.RecoveryServices.Backup.Cmdlets.HydraAdapterN
     public partial class HydraAdapter
     {
         /// <summary>
-        /// 
+        /// Restore the given Recovery point of protectedItem to given storage account
         /// </summary>
         /// <param name="resourceGroupName"></param>
         /// <param name="resourceName"></param>
