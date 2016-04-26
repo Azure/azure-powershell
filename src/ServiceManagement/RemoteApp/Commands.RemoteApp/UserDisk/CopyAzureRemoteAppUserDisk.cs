6b81effe899ad4d33e26fbacb68f7e8f67d7c7eb
Lei He <hele@microsoft.com>
2016-04-26 16:48:19 -0700

    Provide tenant admin support for deleting and migrating UVHD

---
+++ src/ServiceManagement/RemoteApp/Commands.RemoteApp/UserDisk/CopyAzureRemoteAppUserDisk.cs
@@ -0,0 +1,55 @@
+﻿//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Management.RemoteApp;
+using Microsoft.WindowsAzure.Management.RemoteApp.Models;
+using System.Management.Automation;
+
+namespace Microsoft.WindowsAzure.Management.RemoteApp.Cmdlets
+{
+    [Cmdlet(VerbsCommon.Copy, "AzureRemoteAppUserDisk", SupportsShouldProcess = false)]
+    public class CopyAzureRemoteAppUserDisk : RdsCmdlet
+    {
+        [Parameter(Mandatory = true,
+            Position = 0,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "RemoteApp source collection name")]
+        public string SrcCollectionName { get; set; }
+
+        [Parameter(Mandatory = true,
+            Position = 1,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "RemoteApp destination collection name")]
+        public string DstCollectionName { get; set; }
+
+        [Parameter(Mandatory = true,
+            Position = 2,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "RemoteApp user upn")]
+        public string UserUpn { get; set; }
+
+        [Parameter(Mandatory = true,
+            Position = 3,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "overwrite the existing user disk if it exsits")]
+        public bool OverwriteExistingUserDisk { get; set; }
+
+        public override void ExecuteCmdlet()
+        {
+            AzureOperationResponse response = null;
+
+            response = CallClient(() => Client.UserDisks.Copy(SrcCollectionName, DstCollectionName, UserUpn, OverwriteExistingUserDisk), Client.UserDisks);
+        }
+    }
+}
