a894cb298892babb2b19f18e937eda4111a2e786
Hyonho Lee <hylee@microsoft.com>
2016-04-27 11:27:39 -0700

    Regenerate with updated auto-generator which supports Table and Custom formatting

--- src/ResourceManager/Compute/Commands.Compute/Generated/ContainerService/ContainerServiceDeleteMethod.cs
+++ src/ResourceManager/Compute/Commands.Compute/Generated/ContainerService/ContainerServiceDeleteMethod.cs
@@ -24,9 +24,11 @@ using Microsoft.Azure.Commands.Compute.Automation.Models;
 using Microsoft.Azure.Management.Compute;
 using Microsoft.Azure.Management.Compute.Models;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Management.Automation;
+using System.Reflection;
 
 namespace Microsoft.Azure.Commands.Compute.Automation
 {
