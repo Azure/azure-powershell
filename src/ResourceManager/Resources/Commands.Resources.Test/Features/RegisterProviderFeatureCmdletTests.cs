f14896feea7706e8c76c47b396340878703b52a0
vivsriaus <visriniv@microsoft.com>
2016-04-28 17:39:30 -0700

    Changes to feature cmdlet tests

--- src/ResourceManager/Resources/Commands.Resources.Test/Features/RegisterProviderFeatureCmdletTests.cs
+++ src/ResourceManager/Resources/Commands.Resources.Test/Features/RegisterProviderFeatureCmdletTests.cs
@@ -19,9 +19,12 @@ namespace Microsoft.Azure.Commands.Resources.Test
     using Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkModels;
     using Microsoft.Azure.Management.ResourceManager;
     using Microsoft.Azure.Management.ResourceManager.Models;
+    using Microsoft.Rest.Azure;
+    using Microsoft.WindowsAzure.Commands.Common.Test.Mocks;
     using Microsoft.WindowsAzure.Commands.ScenarioTest;
     using Moq;
     using System;
+    using System.Collections.Generic;
     using System.Management.Automation;
     using System.Net;
     using System.Threading;
@@ -34,110 +37,116 @@ namespace Microsoft.Azure.Commands.Resources.Test
     /// </summary>
     public class RegisterAzureProviderFeatureCmdletTests : RMTestBase
     {
-        ///// <summary>
-        ///// An instance of the cmdlet
-        ///// </summary>
-        //private readonly RegisterAzureProviderFeatureCmdlet cmdlet;
+        /// <summary>
+        /// An instance of the cmdlet
+        /// </summary>
+        private readonly RegisterAzureProviderFeatureCmdlet cmdlet;
 
-        ///// <summary>
-        ///// A mock of the client
-        ///// </summary>
-        //private readonly Mock<IFeatures> featureOperationsMock;
+        /// <summary>
+        /// A mock of the client
+        /// </summary>
+        private readonly Mock<IFeaturesOperations> featureOperationsMock;
 
-        ///// <summary>
-        ///// A mock of the command runtime
-        ///// </summary>
-        //private readonly Mock<ICommandRuntime> commandRuntimeMock;
+        /// <summary>
+        /// A mock of the command runtime
+        /// </summary>
+        private readonly Mock<ICommandRuntime> commandRuntimeMock;
+        private MockCommandRuntime mockRuntime;
 
-        ///// <summary>
-        ///// Initializes a new instance of the <see cref="GetAzureProviderFeatureCmdletTests"/> class.
-        ///// </summary>
-        //public RegisterAzureProviderFeatureCmdletTests()
-        //{
-        //    this.featureOperationsMock = new Mock<IFeatures>();
-        //    var featureClient = new Mock<IFeatureClient>();
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetAzureProviderFeatureCmdletTests"/> class.
+        /// </summary>
+        public RegisterAzureProviderFeatureCmdletTests()
+        {
+            this.featureOperationsMock = new Mock<IFeaturesOperations>();
+            var featureClient = new Mock<IFeatureClient>();
 
-        //    featureClient
-        //        .SetupGet(client => client.Features)
-        //        .Returns(() => this.featureOperationsMock.Object);
+            featureClient
+                .SetupGet(client => client.Features)
+                .Returns(() => this.featureOperationsMock.Object);
 
-        //    this.commandRuntimeMock = new Mock<ICommandRuntime>();
+            this.commandRuntimeMock = new Mock<ICommandRuntime>();
 
-        //    this.commandRuntimeMock
-        //      .Setup(m => m.ShouldProcess(It.IsAny<string>(), It.IsAny<string>()))
-        //      .Returns(() => true);
+            this.commandRuntimeMock
+              .Setup(m => m.ShouldProcess(It.IsAny<string>(), It.IsAny<string>()))
+              .Returns(() => true);
 
-        //    this.cmdlet = new RegisterAzureProviderFeatureCmdlet()
-        //    {
-        //        CommandRuntime = commandRuntimeMock.Object,
-        //        ProviderFeatureClient = new ProviderFeatureClient
-        //        {
-        //            FeaturesManagementClient = featureClient.Object
-        //        }
-        //    };
-        //}
+            this.cmdlet = new RegisterAzureProviderFeatureCmdlet()
+            {
+                //CommandRuntime = commandRuntimeMock.Object,
+                ProviderFeatureClient = new ProviderFeatureClient
+                {
+                    FeaturesManagementClient = featureClient.Object
+                }
+            };
+            PSCmdletExtensions.SetCommandRuntimeMock(cmdlet, commandRuntimeMock.Object);
+            mockRuntime = new MockCommandRuntime();
+            commandRuntimeMock.Setup(f => f.Host).Returns(mockRuntime.Host);
+        }
 
-        ///// <summary>
-        ///// Validates all Register-AzureRmResourceProvider scenarios
-        ///// </summary>
-        //[Fact]
-        //[Trait(Category.AcceptanceType, Category.CheckIn)]
-        //public void RegisterResourceProviderFeatureTests()
-        //{
-        //    const string ProviderName = "Providers.Test";
-        //    const string FeatureName = "Feature1";
+        /// <summary>
+        /// Validates all Register-AzureRmResourceProvider scenarios
+        /// </summary>
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void RegisterResourceProviderFeatureTests()
+        {
+            const string ProviderName = "Providers.Test";
+            const string FeatureName = "Feature1";
 
-        //    var registeredFeature = new FeatureResponse
-        //    {
-        //        Id = "featureId1",
-        //        Name = ProviderName + "/" + FeatureName,
-        //        Properties = new FeatureProperties
-        //        {
-        //            State = ProviderFeatureClient.RegisteredStateName,
-        //        },
-        //        RequestId = "requestId",
-        //        StatusCode = HttpStatusCode.OK,
-        //        Type = "Microsoft.Features/feature"
-        //    };
+            var registeredFeature = new FeatureResult
+            {
+                Id = "featureId1",
+                Name = ProviderName + "/" + FeatureName,
+                Properties = new FeatureProperties
+                {
+                    State = ProviderFeatureClient.RegisteredStateName,
+                },
+                Type = "Microsoft.Features/feature"
+            };
 
-        //    this.featureOperationsMock
-        //        .Setup(client => client.RegisterAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<CancellationToken>()))
-        //        .Callback((string providerName, string featureName, CancellationToken ignored) =>
-        //        {
-        //            Assert.Equal(ProviderName, providerName, StringComparer.InvariantCultureIgnoreCase);
-        //            Assert.Equal(FeatureName, featureName, StringComparer.InvariantCultureIgnoreCase);
-        //        })
-        //        .Returns(() => Task.FromResult(registeredFeature));
+            this.featureOperationsMock
+                .Setup(client => client.RegisterWithHttpMessagesAsync(It.IsAny<string>(), It.IsAny<string>(), null, It.IsAny<CancellationToken>()))
+                .Callback((string providerName, string featureName, Dictionary<string, List<string>> customHeaders, CancellationToken ignored) =>
+                {
+                    Assert.Equal(ProviderName, providerName, StringComparer.OrdinalIgnoreCase);
+                    Assert.Equal(FeatureName, featureName, StringComparer.OrdinalIgnoreCase);
+                })
+                .Returns(() => Task.FromResult(new AzureOperationResponse<FeatureResult>()
+                {
+                    Body = registeredFeature
+                }));
 
-        //    this.cmdlet.Force = true;
-        //    this.cmdlet.ProviderNamespace = ProviderName;
-        //    this.cmdlet.FeatureName = FeatureName;
+            this.cmdlet.Force = true;
+            this.cmdlet.ProviderNamespace = ProviderName;
+            this.cmdlet.FeatureName = FeatureName;
 
-        //    this.commandRuntimeMock
-        //        .Setup(m => m.WriteObject(It.IsAny<object>()))
-        //        .Callback((object obj) =>
-        //        {
-        //            Assert.IsType<PSProviderFeature>(obj);
-        //            var feature = (PSProviderFeature)obj;
-        //            Assert.Equal(ProviderName, feature.ProviderName, StringComparer.InvariantCultureIgnoreCase);
-        //            Assert.Equal(FeatureName, feature.FeatureName, StringComparer.InvariantCultureIgnoreCase);
-        //        });
+            this.commandRuntimeMock
+                .Setup(m => m.WriteObject(It.IsAny<object>()))
+                .Callback((object obj) =>
+                {
+                    Assert.IsType<PSProviderFeature>(obj);
+                    var feature = (PSProviderFeature)obj;
+                    Assert.Equal(ProviderName, feature.ProviderName, StringComparer.OrdinalIgnoreCase);
+                    Assert.Equal(FeatureName, feature.FeatureName, StringComparer.OrdinalIgnoreCase);
+                });
 
-        //    this.cmdlet.ExecuteCmdlet();
+            this.cmdlet.ExecuteCmdlet();
 
-        //    this.VerifyCallPatternAndReset(succeeded: true);
-        //}
+            this.VerifyCallPatternAndReset(succeeded: true);
+        }
 
-        ///// <summary>
-        ///// Verifies the right call patterns are made
-        ///// </summary>
-        //private void VerifyCallPatternAndReset(bool succeeded)
-        //{
-        //    this.featureOperationsMock.Verify(f => f.RegisterAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<CancellationToken>()), Times.Once());
-        //    this.commandRuntimeMock.Verify(f => f.WriteObject(It.IsAny<object>()), succeeded ? Times.Once() : Times.Never());
+        /// <summary>
+        /// Verifies the right call patterns are made
+        /// </summary>
+        private void VerifyCallPatternAndReset(bool succeeded)
+        {
+            this.featureOperationsMock.Verify(f => f.RegisterWithHttpMessagesAsync(It.IsAny<string>(),
+                It.IsAny<string>(), null, It.IsAny<CancellationToken>()), Times.Once());
+            this.commandRuntimeMock.Verify(f => f.WriteObject(It.IsAny<object>()), succeeded ? Times.Once() : Times.Never());
 
-        //    this.featureOperationsMock.ResetCalls();
-        //    this.commandRuntimeMock.ResetCalls();
-        //}
+            this.featureOperationsMock.ResetCalls();
+            this.commandRuntimeMock.ResetCalls();
+        }
     }
 }
