4f93758ce66e4755c79e032be3d0bb72a1c36902
Brian <l.brian.hoang@gmail.com>
2016-05-14 05:04:01 -0700

    Fixed to match previous branch

--- src/ResourceManager/AzureBatch/Commands.Batch/Models/BatchClient.Accounts.cs
+++ src/ResourceManager/AzureBatch/Commands.Batch/Models/BatchClient.Accounts.cs
@@ -21,7 +21,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Threading.Tasks;
 using Microsoft.Azure.Batch;
 using Microsoft.Rest.Azure;
 using CloudException = Hyak.Common.CloudException;
