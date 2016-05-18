bb06294c8ca640a3d335075cc82b4e1d12b7bbf0
Iouri Simernitski <iouris@microsoft.com>
2016-05-18 16:23:24 -0700

    Add DevTestLabs

---
+++ src/ResourceManager/DevTestLabs/Commands.DevTestLabs/Commands/Set/SetAzureRmDtlAutoShutdownPolicy.cs
@@ -0,0 +1,61 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Commands.DevTestLabs.Models;
+using Microsoft.Azure.Management.DevTestLabs;
+using Microsoft.Azure.Management.DevTestLabs.Models;
+using System;
+using System.Management.Automation;
+
+namespace Microsoft.Azure.Commands.DevTestLabs
+{
+    [Cmdlet(VerbsCommon.Set, "AzureRmDtlAutoShutdownPolicy", HelpUri = Constants.DevTestLabsHelpUri, DefaultParameterSetName = ParameterSetEnable)]
+    [OutputType(typeof(Schedule))]
+    public class SetAzureRmDtlAutoShutdownPolicy : DtlPolicyCmdletBase
+    {
+        #region Input Parameter Definitions
+
+        /// <summary>
+        /// Time of day for shutting down the virtual machine.
+        /// </summary>
+        [Parameter(Mandatory = true,
+            Position = 4,
+            HelpMessage = "Time of day for shutting down the virtual machine.")]
+        [ValidateNotNullOrEmpty]
+        public DateTime Time { get; set; }
+
+        #endregion Input Parameter Definitions
+
+        public override void ExecuteCmdlet()
+        {
+            var schedule = DataServiceClient.Schedule.CreateOrUpdateResource(
+                ResourceGroupName,
+                LabName,
+                WellKnownPolicyNames.LabVmsShutdown,
+                new Schedule
+                {
+                    TimeZoneId = TimeZoneInfo.Local.Id,
+                    TaskType = TaskType.LabVmsShutdownTask,
+                    DailyRecurrence = new DayDetails
+                    {
+                        Time = Time.ToString("HHmm")
+                    },
+                    Status = Enable ? PolicyStatus.Enabled : PolicyStatus.Disabled,
+                }
+                );
+
+            WriteObject(schedule.DuckType<ScheduleDisplay>());
+        }
+    }
+}
---
+++ src/ResourceManager/DevTestLabs/Commands.DevTestLabs/Models/ScheduleDisplay.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Azure.Management.DevTestLabs.Models;
+using System;
+using System.Linq;
+
+namespace Microsoft.Azure.Commands.DevTestLabs.Models
+{
+    internal class ScheduleDisplay : Schedule
+    {
+        internal static string ToLocalizedTime(string militaryTime)
+        {
+            if (militaryTime == null || militaryTime.Length != 4)
+            {
+                return militaryTime;
+            }
+
+            ushort hours;
+
+            if (!ushort.TryParse(militaryTime.Substring(0, 2), out hours))
+            {
+                return militaryTime;
+            }
+
+            ushort minutes;
+
+            if (!ushort.TryParse(militaryTime.Substring(2), out minutes))
+            {
+                return militaryTime;
+            }
+
+            if (hours > 23 || minutes > 59)
+            {
+                return militaryTime;
+            }
+
+            return new DateTime(2000, 1, 1, hours, minutes, 0, 0).ToShortTimeString();
+        }
+
+        public new WeekDetailsDisplay WeeklyRecurrence
+        {
+            get
+            {
+                return base.WeeklyRecurrence.DuckType<WeekDetailsDisplay>();
+            }
+            set
+            {
+                base.WeeklyRecurrence = value.DuckType<WeekDetails>();
+            }
+        }
+
+        public new HourDetailsDisplay HourlyRecurrence
+        {
+            get
+            {
+                return base.HourlyRecurrence.DuckType<HourDetailsDisplay>();
+            }
+            set
+            {
+                base.HourlyRecurrence = value.DuckType<HourDetails>();
+            }
+        }
+
+        public new DayDetailsDisplay DailyRecurrence
+        {
+            get
+            {
+                return base.DailyRecurrence.DuckType<DayDetailsDisplay>();
+            }
+            set
+            {
+                base.DailyRecurrence = value.DuckType<DayDetails>();
+            }
+        }
+
+        #region nested classes
+
+        public class WeekDetailsDisplay : WeekDetails
+        {
+            public override string ToString()
+            {
+                if (Weekdays == null || Weekdays.Count == 0)
+                {
+                    return String.Format("{{ Time: {0} }}", ToLocalizedTime(Time));
+                }
+
+                return String.Format("{{ Time: {0}, Weekdays: {1} }}", ToLocalizedTime(Time), string.Join(", ", Weekdays.Select(i => i.ToString())));
+            }
+        }
+
+        public class DayDetailsDisplay : DayDetails
+        {
+            public override string ToString()
+            {
+                return String.Format("{{ Time: {0} }}", ToLocalizedTime(Time));
+            }
+        }
+
+        public class HourDetailsDisplay : HourDetails
+        {
+            public override string ToString()
+            {
+                return String.Format("{{ Minute: {0} }}", Minute);
+            }
+        }
+
+        #endregion nested classes
+    }
+}
