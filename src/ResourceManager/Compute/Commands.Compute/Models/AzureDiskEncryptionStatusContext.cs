8a656bda82d1344138b504ea9c55f3a4118b57c3
Kamran Khan <kakhan@microsoft.com>
2016-05-25 11:15:05 -0700

    Remove {Os,Data}VolumeEncryptionSettingsText

--- src/ResourceManager/Compute/Commands.Compute/Models/AzureDiskEncryptionStatusContext.cs
+++ src/ResourceManager/Compute/Commands.Compute/Models/AzureDiskEncryptionStatusContext.cs
@@ -15,29 +15,12 @@ namespace Microsoft.Azure.Commands.Compute.Models
         public EncryptionStatus OsVolumeEncrypted { get; set; }
         public DiskEncryptionSettings OsVolumeEncryptionSettings { get; set; }
         public EncryptionStatus DataVolumesEncrypted { get; set; }
-        [JsonIgnore]
-        public string OsVolumeEncryptionSettingsText
-        {
-            get { return JsonConvert.SerializeObject(OsVolumeEncryptionSettings, Formatting.Indented); }
-        }
     }
     class AzureDiskEncryptionStatusLinuxContext
     {
         public EncryptionStatus OsVolumeEncrypted { get; set; }
         public DiskEncryptionSettings OsVolumeEncryptionSettings { get; set; }
         public DiskEncryptionSettings DataVolumeEncryptionSettings { get; set; }
-
-        [JsonIgnore]
-        public string OsVolumeEncryptionSettingsText
-        {
-            get { return JsonConvert.SerializeObject(OsVolumeEncryptionSettings, Formatting.Indented); }
-        }
-		
-        [JsonIgnore]
-        public string DataVolumeEncryptionSettingsText
-        {
-            get { return JsonConvert.SerializeObject(DataVolumeEncryptionSettings, Formatting.Indented); }
-        }
         public EncryptionStatus DataVolumesEncrypted { get; set; }
     }
 }
