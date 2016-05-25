03a14f3015e8362150130c741b0b6c9cc96e4943
dihan <dihan@microsoft.com>
2016-05-25 16:53:31 -0700

    rename files

--- src/ResourceManager/Network/Commands.Network/ExpressRouteCircuit/Stats/GetAzureExpressRouteCircuitRouteTableSummaryCommand.cs
+++ src/ResourceManager/Network/Commands.Network/ExpressRouteCircuit/Stats/GetAzureExpressRouteCircuitRouteTableSummaryCommand.cs
@@ -1,19 +1,22 @@
-﻿// ----------------------------------------------------------------------------------
-//
-// Copyright Microsoft Corporation
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// ----------------------------------------------------------------------------------
+﻿// 		
+//		
+// Copyright Microsoft Corporation		
+// Licensed under the Apache License, Version 2.0 (the "License");		
+// you may not use this file except in compliance with the License.		
+// You may obtain a copy of the License at		
+// http://www.apache.org/licenses/LICENSE2.0		
+// Unless required by applicable law or agreed to in writing, software		
+// distributed under the License is distributed on an "AS IS" BASIS,		
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.		
+// See the License for the specific language governing permissions and		
+// limitations under the License.		
+// 		
 
+using System.Collections;
 using System.Collections.Generic;
 using System.Management.Automation;
+using AutoMapper;
+using Microsoft.Azure.Commands.Tags.Model;
 using Microsoft.Azure.Management.Network;
 using Microsoft.Azure.Commands.Network.Models;
 using MNM = Microsoft.Azure.Management.Network.Models;
@@ -21,12 +24,13 @@ using MNM = Microsoft.Azure.Management.Network.Models;
 namespace Microsoft.Azure.Commands.Network
 {
     using System;
+    using System.Collections;
     using System.Linq;
 
     using AutoMapper;
 
-    [Cmdlet(VerbsCommon.Get, "AzureExpressRouteCircuitRouteTableSummary"), OutputType(typeof(PSExpressRouteCircuitRoutesTableSummary))]
-    public class GetAzureExpressRouteCircuitRouteTableSummaryCommand : NetworkBaseCmdlet
+    [Cmdlet(VerbsCommon.Get, "AzureRmExpressRouteCircuitRouteTableSummary"), OutputType(typeof(PSExpressRouteCircuitRoutesTableSummary))]
+    public class GetAzureRmExpressRouteCircuitRouteTableSummaryCommand : NetworkBaseCmdlet
     {
         [Alias("ResourceName")]
         [Parameter(
@@ -64,7 +68,7 @@ namespace Microsoft.Azure.Commands.Network
             Mandatory = true,
             HelpMessage = "The DevicePath, can be either Primary or Secondary")]
         [ValidateNotNullOrEmpty]
-        public string DevicePath { get; set; }	
+        public string DevicePath { get; set; }
 
         public override void ExecuteCmdlet()
         {
@@ -80,7 +84,8 @@ namespace Microsoft.Azure.Commands.Network
                     psARPs.Add(psARP);
                 }
                 WriteObject(psARPs, true);
-            }		
+            }
         }
     }
+
 }
