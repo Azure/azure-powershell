966111ccba7e2cb51d56345174eb77ae3eefdf2c
dragonfly91 <anudeeb@microsoft.com>
2016-06-10 11:29:21 +0530

    [RS Backup] Moving Trigger Backup API from ProtectableItemAPI.cs to ItemAPIs.cs

--- src/ResourceManager/RecoveryServices.Backup/Commands.RecoveryServices.Backup.ServiceClientAdapter/BMSAPIs/ItemAPIs.cs
+++ src/ResourceManager/RecoveryServices.Backup/Commands.RecoveryServices.Backup.ServiceClientAdapter/BMSAPIs/ItemAPIs.cs
@@ -120,5 +120,30 @@ namespace Microsoft.Azure.Commands.RecoveryServices.Backup.Cmdlets.ServiceClient
                                      BmsAdapter.GetCustomRequestHeaders(),
                                      BmsAdapter.CmdletCancellationToken).Result;
         }
+
+        /// <summary>
+        /// Triggers backup on the specified item
+        /// </summary>
+        /// <param name="containerName">Name of the container which this item belongs to</param>
+        /// <param name="itemName">Name of the item</param>
+        /// <returns>Job created by this operation</returns>
+        public BaseRecoveryServicesJobResponse TriggerBackup(string containerName, string itemName, DateTime? expiryDateTimeUtc)
+        {
+            TriggerBackupRequest triggerBackupRequest = new TriggerBackupRequest();
+            triggerBackupRequest.Item = new BackupRequestResource();
+            IaaSVMBackupRequest iaasVmBackupRequest = new IaaSVMBackupRequest();
+            iaasVmBackupRequest.RecoveryPointExpiryTimeInUTC = expiryDateTimeUtc;
+            triggerBackupRequest.Item.Properties = iaasVmBackupRequest;
+
+            return BmsAdapter.Client.Backups.TriggerBackupAsync(
+                BmsAdapter.GetResourceGroupName(),
+                BmsAdapter.GetResourceName(),
+                BmsAdapter.GetCustomRequestHeaders(),
+                ServiceClientAdapter.AzureFabricName,
+                containerName,
+                itemName,
+                triggerBackupRequest,
+                BmsAdapter.CmdletCancellationToken).Result;
+        }
     }
 }
