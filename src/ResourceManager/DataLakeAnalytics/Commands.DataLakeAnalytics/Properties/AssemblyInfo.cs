76220052ac549ad52c93e47f714ce770683382d0
Hovsep Mkrtchyan <hovsepm@microsoft.com>
2016-07-05 15:29:49 -0700

    Updated package and assembly versions for release 1.6.0

--- src/ResourceManager/DataLakeAnalytics/Commands.DataLakeAnalytics/Properties/AssemblyInfo.cs
+++ src/ResourceManager/DataLakeAnalytics/Commands.DataLakeAnalytics/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 
 
-[assembly: AssemblyVersion("1.1.2")]
-[assembly: AssemblyFileVersion("1.1.2")]
+[assembly: AssemblyVersion("1.1.3")]
+[assembly: AssemblyFileVersion("1.1.3")]
