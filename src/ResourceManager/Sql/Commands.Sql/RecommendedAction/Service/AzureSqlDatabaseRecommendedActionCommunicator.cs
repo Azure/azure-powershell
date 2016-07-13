a42470633f4c0b78c82adc04bb8f56f6347e2685
Anil Kumar Yelam <anilyela@microsoft.com>
2016-07-13 17:24:53 +0200

    Added cmdlets and tests for Recommended Actions

---
+++ src/ResourceManager/Sql/Commands.Sql/RecommendedAction/Service/AzureSqlDatabaseRecommendedActionCommunicator.cs
@@ -0,0 +1,69 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Commands.Common.Authentication.Models;
+using Microsoft.Azure.Management.Sql;
+using Microsoft.Azure.Management.Sql.Models;
+using System.Collections.Generic;
+
+namespace Microsoft.Azure.Commands.Sql.RecommendedAction.Service
+{
+    /// <summary>
+    /// This class is responsible for all the REST communication with the database recommended action REST endpoints
+    /// </summary>
+    public class AzureSqlDatabaseRecommendedActionCommunicator : AzureSqlRecommendedActionCommunicatorBase
+    {
+        /// <summary>
+        /// Creates a communicator for Azure Sql Database Recommended Actions
+        /// </summary>
+        public AzureSqlDatabaseRecommendedActionCommunicator(AzureContext context)
+            : base(context)
+        {
+        }
+
+        /// <summary>
+        /// Gets the Azure Sql Database Recommended Action
+        /// </summary>
+        public Management.Sql.Models.RecommendedAction Get(string resourceGroupName, string serverName, string databaseName, string advisorName, string recommendedActionName, string clientRequestId)
+        {
+            return GetCurrentSqlClient(clientRequestId).DatabaseRecommendedActions.Get(resourceGroupName, serverName, databaseName, advisorName, recommendedActionName).RecommendedAction;
+        }
+
+        /// <summary>
+        /// Lists Azure Sql Database Recommended Actions
+        /// </summary> 
+        public IList<Management.Sql.Models.RecommendedAction> List(string resourceGroupName, string serverName, string databaseName, string advisorName, string clientRequestId)
+        {
+            return GetCurrentSqlClient(clientRequestId).DatabaseRecommendedActions.List(resourceGroupName, serverName, databaseName, advisorName).RecommendedActions;
+        }
+
+        /// <summary>
+        /// Update Recommended Action State
+        /// </summary>
+        public Management.Sql.Models.RecommendedAction UpdateState(string resourceGroupName, string serverName, string databaseName, string advisorName, string recommendedActionName, string newStateValue, string clientRequestId)
+        {
+            return GetCurrentSqlClient(clientRequestId).DatabaseRecommendedActions.Update(resourceGroupName, serverName, databaseName, advisorName, recommendedActionName,
+                    new RecommendedActionUpdateParameters
+                    {
+                        Properties = new RecommendedActionUpdateProperties()
+                        {
+                            State = new RecommendedActionUpdateStateInfo()
+                            {
+                                CurrentValue = newStateValue
+                            }
+                        }
+                    }).RecommendedAction;
+        }
+    }
+}
---
+++ src/ResourceManager/Sql/Commands.Sql/RecommendedAction/Service/AzureSqlServerRecommendedActionAdapter.cs
@@ -0,0 +1,85 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Commands.Common.Authentication.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Azure.Commands.Sql.RecommendedAction.Model;
+using Microsoft.Azure.Commands.Sql.Services;
+
+namespace Microsoft.Azure.Commands.Sql.RecommendedAction.Service
+{
+    /// <summary>
+    /// Adapter for Server Recommended Action operations
+    /// </summary>
+    public class AzureSqlServerRecommendedActionAdapter
+    {
+        /// <summary>
+        /// Gets or sets the Communicator which has all the needed management clients
+        /// </summary>
+        private AzureSqlServerRecommendedActionCommunicator Communicator { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Azure profile
+        /// </summary>
+        public AzureContext Context { get; set; }
+
+        /// <summary>
+        /// Constructs adapter
+        /// </summary>
+        public AzureSqlServerRecommendedActionAdapter(AzureContext context)
+        {
+            Context = context;
+            Communicator = new AzureSqlServerRecommendedActionCommunicator(Context);
+        }
+
+        /// <summary>
+        /// Gets an Azure Sql Server Recommended Action by name.
+        /// </summary>
+        /// <param name="resourceGroupName">The name of the resource group</param>
+        /// <param name="serverName">The name of the Azure Sql Server</param>
+        /// <param name="advisorName">The name of the Azure Sql Server Advisor</param>
+        /// <param name="RecommendedActionName">The name of the Azure Sql Server Recommended Action</param>
+        /// <returns>The Azure Sql Server RecommendedAction object</returns>
+        internal AzureSqlServerRecommendedActionModel GetServerRecommendedAction(string resourceGroupName, string serverName, string advisorName, string RecommendedActionName)
+        {
+            var response = Communicator.Get(resourceGroupName, serverName, advisorName, RecommendedActionName, Util.GenerateTracingId());
+            return new AzureSqlServerRecommendedActionModel(resourceGroupName, serverName, advisorName, response);
+        }
+
+        /// <summary>
+        /// Gets a list of Azure Sql Server Recommended Actions.
+        /// </summary>
+        /// <param name="resourceGroupName">The name of the resource group</param>
+        /// <param name="serverName">The name of the Azure Sql Server</param>
+        /// <param name="advisorName">The name of the Azure Sql Server Advisor</param>
+        /// <returns>A list of Server Recommended Action objects</returns>
+        internal ICollection<AzureSqlServerRecommendedActionModel> ListServerRecommendedActions(string resourceGroupName, string serverName, string advisorName)
+        {
+            var response = Communicator.List(resourceGroupName, serverName, advisorName, Util.GenerateTracingId());
+            return response.Select(adv => new AzureSqlServerRecommendedActionModel(resourceGroupName, serverName, advisorName, adv)).ToList();
+        }
+
+        /// <summary>
+        /// Updates Auto Execute Status of Azure Sql Server Recommended Action.
+        /// </summary>
+        /// <param name="model">The input parameters for the update operation</param>
+        /// <returns>The upserted Azure Sql Server Recommended Action</returns>
+        internal AzureSqlServerRecommendedActionModel UpdateState(AzureSqlServerRecommendedActionModel model)
+        {
+            var response = Communicator.UpdateState(model.ResourceGroupName, model.ServerName, model.AdvisorName, model.RecommendedActionName, model.State.CurrentValue, Util.GenerateTracingId());
+            return new AzureSqlServerRecommendedActionModel(model.ResourceGroupName, model.ServerName, model.AdvisorName, response);
+        }
+    }
+}
