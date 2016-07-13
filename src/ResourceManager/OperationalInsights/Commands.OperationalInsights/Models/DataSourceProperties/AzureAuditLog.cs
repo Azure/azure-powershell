24cee4d362b3a1a3cf69ccd0cdb17c4d5c0d7ecc
haitch <haitch@outlook.com>
2016-07-13 00:08:15 -0700

    reference to newer version, also add dataModel

---
+++ src/ResourceManager/OperationalInsights/Commands.OperationalInsights/Models/DataSourceProperties/AzureAuditLog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Commands.OperationalInsights.Models
+{
+    public class AzureAuditLog: PSDataSourcePropertiesBase
+    {
+        public string LinkedResourceId { get; set; }
+    }
+}
---
+++ src/ResourceManager/OperationalInsights/Commands.OperationalInsights/Models/PSDataSource.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.OperationalInsights.Models;
+
+namespace Microsoft.Azure.Commands.OperationalInsights.Models
+{
+    public class PSDataSourcePropertiesBase { }
+
+    public class PSDataSource
+    {
+        public string Name { get; set; }
+
+        public string ResourceId { get; set; }
+
+        public string ResourceGroupName { get; set; }
+
+        public string WorkspaceName { get; set; }
+
+        public string Kind { get; set; }
+
+        public PSDataSourcePropertiesBase Properties { get; set; }
+
+        public PSDataSource()
+        {
+        }
+
+        public PSDataSource(DataSource storageInsight, string resourceGroupName, string workspaceName)
+        {
+            if (storageInsight == null)
+            {
+                throw new ArgumentNullException("storageInsight");
+            }
+
+            this.ResourceGroupName = resourceGroupName;
+            this.WorkspaceName = workspaceName;
+            this.Name = storageInsight.Name;
+            this.ResourceId = storageInsight.Id;
+            this.Kind = storageInsight.Kind;
+
+        }
+    }
+}
