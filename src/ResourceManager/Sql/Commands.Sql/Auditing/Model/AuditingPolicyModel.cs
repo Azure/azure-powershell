ad90f4273e6ed2d5b8f5c21c9d29cff12301fd7f
Yoav Rubin <yoavrubin@hotmail.com>
2016-07-24 15:00:17 +0300

    Adding sql blob auditing

--- src/ResourceManager/Sql/Commands.Sql/Auditing/Model/AuditingPolicyModel.cs
+++ src/ResourceManager/Sql/Commands.Sql/Auditing/Model/AuditingPolicyModel.cs
@@ -20,11 +20,6 @@ namespace Microsoft.Azure.Commands.Sql.Auditing.Model
     public enum StorageKeyKind { Primary, Secondary };
 
     /// <summary>
-    /// The possible audit event types
-    /// </summary> 
-    public enum AuditEventType { PlainSQL_Success, PlainSQL_Failure, ParameterizedSQL_Success, ParameterizedSQL_Failure, StoredProcedure_Success, StoredProcedure_Failure, Login_Success, Login_Failure, TransactionManagement_Success, TransactionManagement_Failure };
-
-    /// <summary>
     /// The possible states in which an auditing policy may be in
     /// </summary>
     public enum AuditStateType { Enabled, Disabled, New };
@@ -32,7 +27,7 @@ namespace Microsoft.Azure.Commands.Sql.Auditing.Model
     /// <summary>
     /// The base class that defines the core properties of an auditing policy
     /// </summary>
-    public abstract class BaseAuditingPolicyModel
+    public abstract class AuditingPolicyModel
     {
         /// <summary>
         /// Gets or sets the resource group
@@ -50,16 +45,6 @@ namespace Microsoft.Azure.Commands.Sql.Auditing.Model
         public string StorageAccountName { get; set; }
 
         /// <summary>
-        /// Gets or sets the storage key type
-        /// </summary>
-        public StorageKeyKind StorageKeyType { get; set; }
-
-        /// <summary>
-        /// Gets or sets the audit event types
-        /// </summary>
-        public AuditEventType[] EventType { get; set; }
-
-        /// <summary>
         /// Gets or sets the audit state
         /// </summary>
         public AuditStateType AuditState { get; set; }
@@ -68,15 +53,5 @@ namespace Microsoft.Azure.Commands.Sql.Auditing.Model
         /// Gets or sets the retention days
         /// </summary>
         public uint? RetentionInDays { get; internal set; }
-
-        /// <summary>
-        /// Gets or sets the audit logs table name 
-        /// </summary>
-        public string TableIdentifier { get; internal set; }
-
-        /// <summary>
-        /// Gets or sets the full name of audit logs table 
-        /// </summary>
-        public string FullAuditLogsTableName { get; internal set; }
     }
 }
