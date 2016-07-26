48b7f02f1c1bba818a331fda5661f551cee69a6f
DeepakRajendranMsft <derajen@microsoft.com>
2016-07-25 22:58:21 -0700

    Minor fixes and enable tests (#2662)

--- src/ResourceManager/Network/Commands.Network/Models/PSNetworkInterface.cs
+++ src/ResourceManager/Network/Commands.Network/Models/PSNetworkInterface.cs
@@ -29,7 +29,7 @@ namespace Microsoft.Azure.Commands.Network.Models
 
         public bool? Primary { get; set; }
 
-        public bool EnableIPForwarding { get; set; }
+        public bool? EnableIPForwarding { get; set; }
 
         public PSNetworkSecurityGroup NetworkSecurityGroup { get; set; }
 
