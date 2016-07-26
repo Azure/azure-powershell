9287f925d37446d3e4879695be2b912a36033b3e
Sumanan Jeyabalasingam <sumananj@microsoft.com>
2016-07-25 17:32:17 -0700

    Add Scheduler ResourceManager Powershell.
     This change add following cmdlets for RM Powershell:
    
     1)  Disable-AzureRmSchedulerJobCollection
     2)  Enable-AzureRmSchedulerJobCollection
     3)  Get-AzureRmSchedulerJobCollection
     4)  New-AzureRmSchedulerJobCollection
     5)  Remove-AzureRmSchedulerJobCollection
     6)  Set-AzureRmSchedulerJobCollection
     7)  Get-AzureRmSchedulerJob
     8)  Get-AzureRmSchedulerJobHistory
     9)  New-AzureRmSchedulerHttpJob
     10) New-AzureRmSchedulerServiceBusQueueJob
     11) New-AzureRmSchedulerServiceBusTopicJob
     12) New-AzureSchedulerStorageQueueJob
     13) Remove-AzureSchedulerJob
     14) Set-AzureRmSchedulerHttpJob
     15) Set-AzureRmSchedulerServiceBusQueueJob
     16) Set-AzureRmSchedulerServiceBusTopicJob
     17) Set-AzureSchedulerStorageQueueJob
    
     Missing from this Change:
     Support for Advance schedule and Retry policy. Will be part of next release
    
    Fix.

---
+++ src/ResourceManager/Scheduler/Commands.Scheduler/Utilities/SchedulerClient/SchedulerClient.cs
@@ -0,0 +1,83 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+namespace Microsoft.Azure.Commands.Scheduler.Utilities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Management.Resources.Models;
+    using Microsoft.Azure.Commands.Common.Authentication;
+    using Microsoft.Azure.Commands.Common.Authentication.Models;
+    using Microsoft.Azure.Management.Resources;
+    using Microsoft.Azure.Management.Scheduler;
+
+    /// <summary>
+    /// Scheduler client class.
+    /// </summary>
+    public partial class SchedulerClient
+    {
+        /// <summary>
+        /// Azure resource managment client.
+        /// </summary>
+        private ResourceManagementClient _resourceManagementClient;
+
+        /// <summary>
+        /// Scheduler available regions.
+        /// </summary>
+        private IList<string> _availableRegions;
+
+        /// <summary>
+        /// Gets or sets Scheduler client instance.
+        /// </summary>
+        public ISchedulerManagementClient SchedulerManagementClient { get; set; }
+
+        /// <summary>
+        /// Gets available regions for Scheduler.
+        /// </summary>
+        public IList<string> AvailableRegions
+        {
+            get
+            {
+                return _availableRegions;
+            }
+        }
+
+        /// <summary>
+        /// Initializes new instance of the <see cref="SchedulerClient"/> class.
+        /// </summary>
+        /// <param name="context">The Azure context reference.</param>
+        public SchedulerClient(AzureContext context)
+        {
+            this.SchedulerManagementClient = AzureSession.ClientFactory.CreateArmClient<SchedulerManagementClient>(context, AzureEnvironment.Endpoint.ResourceManager);
+            this._resourceManagementClient = AzureSession.ClientFactory.CreateClient<ResourceManagementClient>(context, AzureEnvironment.Endpoint.ResourceManager);
+
+            ProviderResourceType providerResourceType= _resourceManagementClient.Providers.Get(Constants.ProviderNamespace).Provider.ResourceTypes.First((resourceType) => resourceType.Name.Equals(Constants.ResourceType, StringComparison.InvariantCultureIgnoreCase));
+
+            _availableRegions = providerResourceType != null ? providerResourceType.Locations : null;
+
+            this.SchedulerManagementClient.SubscriptionId = context.Subscription.Id.ToString();
+        }
+
+        /// <summary>
+        /// Check whether resource group exists.
+        /// </summary>
+        /// <param name="resourceGroupName">Name of the resource group.</param>
+        /// <returns>true if resource group exists, false otherwise</returns>
+        internal bool DoesResourceGroupExists(string resourceGroupName)
+        {
+            return this._resourceManagementClient.ResourceGroups.CheckExistence(resourceGroupName).Exists;
+        }
+    }
+}
