e7bdd7be4841d377b64f87b29df0b0d7769a151d
Lei Yu <yulei05@gmail.com>
2016-07-29 14:54:28 -0700

    Add PowerShell commands support for Azure Media Services. (#2577)
    * Add PowerShell commands support for RPV2.
    - CRUD operations of media service account.
    - List and regenerate the keys of media service account.
    - Synchronize storage account keys for a storage account associated with the media service account.
    
    ScenarioTests:
    Done
    
    UT:
    Done (Playback mode of ScenarioTests)
    
    Validation of sync storage account keys:
    1. upload an asset to a media service account.
    2. regenerate two keys of storage account and the above asset cannot be deleted because of different storage account keys.
    3. run the sync cmdlet and after a while the above asset can be deleted.
    
    * Addressed the feedback received from the swagger design spec.
    - Name conventions and flattened Json format.
    
    * Remove the generated files from the swagger and consume them as a nuget package.
    
    * Upgrade the active directory lib to 2.28.
    
    * - Fix hardcoded location.
    - Protect the write cmdlets with ShouldProcess.
    
    * Add request comfirmations for write cmdlets.
    
    * Update the help documentation with more details.
    
    * - Consume the media management client library 0.1.0.
    - Change the assembly version to 0.1.0.
    
    * - Protect non-descructive actions with ShouldProcess only.
    - Update the aliases of the AccountName property.
    - Update the tools/AzureRm/AzureRm.psd1.
    - Update the help doc.
    
    * Fix build error.
    
    * upgrade the client runtime library.
    
    * Remove the reference Commands.Tags in cmdlet code and redo the test.
    
    * Update the installer.
    
    * Fix tags null exception.
    
    * Clean the debug information in tests and re-run the test.
    
    * - Remove unused reference.
    - Add cmdlet New-AzureRmMediaServiceStorageConfig.
    - Add resource file.
    - Upgrade the AzureRm.profile to 1.0.11.
    - Update the tools/AzureRm.psd1.
    - Update and re-run the test.
    - Update the installer.
    
    * Put the text into resouces.
    
    * Remove extra leading tab in AzureRm.psd1.

---
+++ src/ResourceManager/Media/Commands.Media/MediaService/NewAzureRmMediaService.cs
@@ -0,0 +1,175 @@
+﻿//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Net;
+using Microsoft.Azure.Commands.Media.Common;
+using Microsoft.Azure.Commands.Media.Models;
+using Microsoft.Azure.Management.Media;
+using Microsoft.Azure.Management.Media.Models;
+using RestMediaService = Microsoft.Azure.Management.Media.Models.MediaService;
+
+namespace Microsoft.Azure.Commands.Media.MediaService
+{
+    /// <summary>
+    /// Create a media service.
+    /// </summary>
+    [Cmdlet(VerbsCommon.New, MediaServiceNounStr, SupportsShouldProcess = true), OutputType(typeof(PSMediaService))]
+    public class NewAzureRmMediaService : AzureMediaServiceCmdletBase
+    {
+        protected const string PrimaryStorageAccountParamSet = "StorageAccountIdParamSet";
+        protected const string StorageAccountsParamSet = "StorageAccountsParamSet";
+        private const string NewMediaServiceWhatIfMessage = "New a MediaService";
+
+        [Parameter(
+            Position = 0,
+            Mandatory = true,
+            ParameterSetName = PrimaryStorageAccountParamSet,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The resource group name.")]
+        [Parameter(
+            Position = 0,
+            Mandatory = true,
+            ParameterSetName = StorageAccountsParamSet,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The resource group name.")]
+
+        [ValidateNotNullOrEmpty]
+        public string ResourceGroupName { get; set; }
+
+        [Parameter(
+            Position = 1,
+            Mandatory = true,
+            ParameterSetName = PrimaryStorageAccountParamSet,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The media service account name.")]
+        [Parameter(
+            Position = 1,
+            Mandatory = true,
+            ParameterSetName = StorageAccountsParamSet,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The media service account name.")]
+        [ValidateNotNullOrEmpty]
+        [ValidateLength(MediaServiceAccountNameMinLength, MediaServiceAccountNameMaxLength)]
+        [ValidatePattern(MediaServiceAccountNamePattern)]
+        [Alias("Name", "ResourceName")]
+        public string AccountName { get; set; }
+
+        [Parameter(
+            Position = 2,
+            Mandatory = true,
+            ParameterSetName = PrimaryStorageAccountParamSet,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The location.")]
+        [Parameter(
+            Position = 2,
+            Mandatory = true,
+            ParameterSetName = StorageAccountsParamSet,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The location.")]
+        [ValidateNotNullOrEmpty]
+        public string Location { get; set; }
+
+        [Parameter(
+            Position = 3,
+            Mandatory = true,
+            ParameterSetName = PrimaryStorageAccountParamSet,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The primary storage account assosiated with the media account.")]
+        [Alias("Id")]
+        [ValidateNotNullOrEmpty]
+        public string StorageAccountId { get; set; }
+
+        [Parameter(
+            Position = 3,
+            Mandatory = true,
+            ParameterSetName = StorageAccountsParamSet,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The storage accounts assosiated with the media account.")]
+        [ValidateNotNullOrEmpty]
+        public PSStorageAccount[] StorageAccounts { get; set; }
+
+        [Parameter(
+            Mandatory = false, 
+            HelpMessage = "The tags associated with the media service account.")]
+        [ValidateNotNull]
+        public Hashtable Tags { get; set; }
+
+        public override void ExecuteCmdlet()
+        {
+            if (ShouldProcess(AccountName, NewMediaServiceWhatIfMessage))
+            {
+                try
+                {
+                    var mediaService = MediaServicesManagementClient.MediaService.Get(ResourceGroupName, AccountName);
+                    if (mediaService != null)
+                    {
+                        throw new ArgumentException(string.Format(Properties.Resource.InvalidMediaServiceAccount,
+                            AccountName,
+                            SubscrptionName,
+                            ResourceGroupName));
+                    }
+                }
+                catch (ApiErrorException exception)
+                {
+                    if (exception.Response != null && exception.Response.StatusCode.Equals(HttpStatusCode.NotFound))
+                    {
+                        var restMediaService = new RestMediaService(
+                            Location,
+                            Tags.ToDictionaryTags(),
+                            null,
+                            null,
+                            MediaServiceType);
+
+                        switch (ParameterSetName)
+                        {
+                            case PrimaryStorageAccountParamSet:
+                                restMediaService.StorageAccounts = new List<StorageAccount>
+                            {
+                                new StorageAccount
+                                {
+                                    Id = StorageAccountId,
+                                    IsPrimary = true
+                                }
+                            };
+                                break;
+
+                            case StorageAccountsParamSet:
+                                if (StorageAccounts.Count(x => x.IsPrimary.HasValue && x.IsPrimary.Value) != 1)
+                                {
+                                    throw new ArgumentException(Properties.Resource.OnlyOnePrimaryStorageAccountAllowed);
+                                }
+
+                                restMediaService.StorageAccounts = StorageAccounts.Select(x => x.ToStorageAccount()).ToList();
+                                break;
+
+                            default:
+                                throw new ArgumentException("Bad ParameterSet Name");
+                        }
+
+                        var mediaServiceCreated = MediaServicesManagementClient.MediaService.Create(ResourceGroupName, AccountName, restMediaService);
+                        WriteObject(mediaServiceCreated.ToPSMediaService(), true);
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+        }
+    }
+}
