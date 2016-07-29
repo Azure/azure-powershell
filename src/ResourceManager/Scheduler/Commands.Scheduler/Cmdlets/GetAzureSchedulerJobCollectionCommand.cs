8514b73c38210ba0a216d4de0ad6e47898ed382b
Sumanan Jeyabalasingam <sumananj@microsoft.com>
2016-07-29 15:59:32 -0700

    Add Scheduler ResourceManager Powershell.
             This change add following cmdlets for RM Powershell:
    
             1) Disable-AzureRmSchedulerJobCollection
             2)  Enable-AzureRmSchedulerJobCollection
             3)  Get-AzureRmSchedulerJobCollection
             4)  New-AzureRmSchedulerJobCollection
             5)  Remove-AzureRmSchedulerJobCollection
             6)  Set-AzureRmSchedulerJobCollection
             7)  Get-AzureRmSchedulerJob
             8)  Get-AzureRmSchedulerJobHistory
             9)  New-AzureRmSchedulerHttpJob
             10) New-AzureRmSchedulerServiceBusQueueJob
             11) New-AzureRmSchedulerServiceBusTopicJob
             12) New-AzureSchedulerStorageQueueJob
             13) Remove-AzureSchedulerJob
             14) Set-AzureRmSchedulerHttpJob
             15) Set-AzureRmSchedulerServiceBusQueueJob
             16) Set-AzureRmSchedulerServiceBusTopicJob
             17) Set-AzureSchedulerStorageQueueJob
    
             Missing from this Change:
             Support for Advance schedule and Retry policy.

---
+++ src/ResourceManager/Scheduler/Commands.Scheduler/Cmdlets/GetAzureSchedulerJobCollectionCommand.cs
@@ -0,0 +1,50 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+namespace Microsoft.Azure.Commands.Scheduler.Cmdlets
+{
+    using System.Management.Automation;
+    using Microsoft.Azure.Commands.Scheduler.Properties;
+    using Microsoft.Azure.Commands.Scheduler.Utilities;
+    using Microsoft.Azure.Management.Scheduler.Models;
+    using Microsoft.Azure.Commands.Scheduler.Models;
+
+    /// <summary>
+    /// Gets job collection info.
+    /// </summary>
+    [Cmdlet(VerbsCommon.Get, "AzureRmSchedulerJobCollection"), OutputType(typeof(object))]
+    public class GetAzureSchedulerJobCollectionCommand : SchedulerBaseCmdlet
+    {
+        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, HelpMessage = "The targeted resource group for job collection.")]
+        [ValidateNotNullOrEmpty]
+        public string ResourceGroupName { get; set; }
+
+        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, HelpMessage = "The name of the job collection.")]
+        [Alias("Name", "ResourceName")]
+        [ValidateNotNullOrEmpty]
+        public string JobCollectionName { get; set; }
+
+        /// <summary>
+        /// Executes the command.
+        /// </summary>
+        public override void ExecuteCmdlet()
+        {
+            base.ExecuteCmdlet();
+
+            this.WriteObject(
+                this.SchedulerClient.ListJobCollectionPS(this.ResourceGroupName, this.JobCollectionName),
+                enumerateCollection: true);
+        }
+    }
+}
