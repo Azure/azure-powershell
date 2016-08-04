24c4b9ee73c202ba5a5a5198c6cfc5a3369e4b08
Corina <corina.aj@gmail.com>
2016-08-04 10:54:51 -0700

    Added comments to DnsZone
    Signed-off-by: Corina <corina.aj@gmail.com>

--- src/ResourceManager/Dns/Commands.Dns/Models/DnsZone.cs
+++ src/ResourceManager/Dns/Commands.Dns/Models/DnsZone.cs
@@ -51,6 +51,9 @@ namespace Microsoft.Azure.Commands.Dns
         /// Gets or sets the number of records for this zone
         /// </summary>
         public long? NumberOfRecordSets { get; set; }
+        /// <summary>
+        ///Gets or sets the max number of records for this zone 
+        /// </summary>
         public long? MaxNumberOfRecordSets { get; set; }
     }
 }
