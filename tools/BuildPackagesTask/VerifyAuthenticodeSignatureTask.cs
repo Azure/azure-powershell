5a8a1bbca85167f9409e92a09f25a6b844b69dd8
shahabhijeet <abhimswork@outlook.com>
2016-08-25 10:11:15 -0700

    Changes made to staticanalysis tool to add serverity 1 from 2 for violating rule where cmdLet has a Process Verb and does not support "shouldProcess". Add test infrastructure to test cmdlets against static analysis tool, adding tests for Signature verifier. Remove existing console logger, incorporate log4net logger in Analysis Logger, Added overload to IStaticAnalysis for adding filter delegates during analysis. updating build.proj to restore staticanalysis project packages to the right location. Long term we should remove local nuget.config file and use a common package restore location, adding restore packages properties to true in VS for staticanalysis and staticanalysis test project

---
+++ tools/BuildPackagesTask/VerifyAuthenticodeSignatureTask.cs
@@ -0,0 +1,62 @@
+﻿namespace Microsoft.Azure.Build.Tasks
+{
+    using Microsoft.Build.Framework;
+    using Microsoft.Build.Utilities;
+    using System;
+    using System.IO;
+    using System.Security.Cryptography.X509Certificates;
+
+    public class VerifyAuthenticodeSignatureTask : Task
+    {
+        /// <summary>
+        /// Gets or sets the assembly on which authenticode signature verification is performed.
+        /// </summary>
+        [Required]
+        public ITaskItem Assembly { get; set; }
+
+        [Output]
+        public bool AuthTaskErrorsDetected { get; private set; }
+
+        public override bool Execute()
+        {
+            string assemblyPath = Assembly.ItemSpec;
+            string assemblyName = Path.GetFileName(assemblyPath);            
+            AuthTaskErrorsDetected = false;
+            try
+            {
+                Log.LogMessage("Verifying Authenticode Signature for '{0}' - '{1}'", assemblyName, assemblyPath);
+                X509Certificate cert = X509Certificate.CreateFromSignedFile(assemblyPath);
+                if ((assemblyName.ToLower().StartsWith("microsoft")) || (assemblyName.ToLower().StartsWith("system")))
+                {
+                    if (cert.Issuer.ToLower().Contains("microsoft"))
+                    {
+                        // Verify if the certificate by which it was signed is not expired
+                        DateTime notAfter = DateTime.Parse(cert.GetExpirationDateString());
+                        if (notAfter < DateTime.Today)
+                            throw new System.Security.Cryptography.CryptographicException("'{0}' has been signed with an expired certificate", assemblyPath);
+
+                        Log.LogMessage("Verified: '{0}' has been signed using valid cerificate issued by Microsoft", assemblyName);
+                    }
+                    else
+                    {
+                        throw new System.Security.Cryptography.CryptographicUnexpectedOperationException(string.Format("'{0}' - '{1}' assembly is not signed using Microsoft certificate", assemblyName, assemblyPath));
+                    }
+                }
+
+                return true;
+            }
+            catch(System.Security.Cryptography.CryptographicException)
+            {
+                Log.LogError(string.Format("'{0}' is not authenticode signed..... exiting build", assemblyPath));
+                AuthTaskErrorsDetected = true;
+                return false;
+            }
+            catch(Exception ex)
+            {
+                Log.LogErrorFromException(ex);
+                AuthTaskErrorsDetected = true;
+                return false;
+            }
+        }
+    }
+}
