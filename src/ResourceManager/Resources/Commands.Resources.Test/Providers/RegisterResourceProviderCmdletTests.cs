afb3f0cbe1796dc32c0510b877fdabc2558c22ab
Tian Ouyang <tiano@microsoft.com>
2016-08-27 17:01:28 -0700

    Update ResourceManager SDK to 1.1.5-preview version.

--- src/ResourceManager/Resources/Commands.Resources.Test/Providers/RegisterResourceProviderCmdletTests.cs
+++ src/ResourceManager/Resources/Commands.Resources.Test/Providers/RegisterResourceProviderCmdletTests.cs
@@ -120,7 +120,7 @@ namespace Microsoft.Azure.Commands.Resources.Test
                 }));
 
             this.providerOperationsMock
-              .Setup(f => f.GetWithHttpMessagesAsync(It.IsAny<string>(), null, It.IsAny<CancellationToken>()))
+              .Setup(f => f.GetWithHttpMessagesAsync(It.IsAny<string>(), null, null, It.IsAny<CancellationToken>()))
               .Returns(() => Task.FromResult( new AzureOperationResponse<Provider>() {
                    Body = provider
               }));
