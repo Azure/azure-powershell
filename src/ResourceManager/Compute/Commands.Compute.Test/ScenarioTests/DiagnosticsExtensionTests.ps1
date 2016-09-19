11c7910c78938d1364f319a36ad41e0dc616f65d
zhencui <zhencui@microsoft.com>
2016-09-19 15:04:26 -0700

    Implement Add/Remove Cmdlets for VMSS diagnostics extension, add related test case and scenario records

--- src/ResourceManager/Compute/Commands.Compute.Test/ScenarioTests/DiagnosticsExtensionTests.ps1
+++ src/ResourceManager/Compute/Commands.Compute.Test/ScenarioTests/DiagnosticsExtensionTests.ps1
@@ -184,3 +184,113 @@ function Test-DiagnosticsExtensionSupportJsonConfig
         Clean-ResourceGroup $rgname
     }
 }
+
+<#
+.SYNOPSIS
+Test the basic usage of the Add/Remove VMSS diagnostics extension command
+#>
+function Test-VmssDiagnosticsExtension
+{
+    $rgname = Get-ComputeTestResourceName
+    $loc = Get-ComputeVmssLocation
+
+    try
+    {
+        # Common
+        New-AzureRMResourceGroup -Name $rgname -Location $loc -Force;
+
+        # Create VMSS
+
+        # SRP
+        $stoname = 'sto' + $rgname;
+        $stotype = 'Standard_GRS';
+        New-AzureRMStorageAccount -ResourceGroupName $rgname -Name $stoname -Location $loc -Type $stotype;
+        $stoaccount = Get-AzureRMStorageAccount -ResourceGroupName $rgname -Name $stoname;
+
+        # NRP
+        $subnet = New-AzureRMVirtualNetworkSubnetConfig -Name ('subnet' + $rgname) -AddressPrefix "10.0.0.0/24";
+        $vnet = New-AzureRMVirtualNetwork -Force -Name ('vnet' + $rgname) -ResourceGroupName $rgname -Location $loc -AddressPrefix "10.0.0.0/16" -Subnet $subnet;
+        $vnet = Get-AzureRMVirtualNetwork -Name ('vnet' + $rgname) -ResourceGroupName $rgname;
+        $subnetId = $vnet.Subnets[0].Id;
+
+        # New VMSS Parameters
+        $vmssName = 'vmss' + $rgname;
+        $vmssType = 'Microsoft.Compute/virtualMachineScaleSets';
+
+        $adminUsername = 'Foo12';
+        $adminPassword = "BaR@123" + $rgname;
+
+        $imgRef = Get-DefaultCRPImage -loc $loc;
+        $vhdContainer = "https://" + $stoname + ".blob.core.windows.net/" + $vmssName;
+
+        $extname = 'diagextest';
+        $diagExtPublisher = 'Microsoft.Azure.Diagnostics';
+        $diagExtType = 'IaaSDiagnostics';
+
+        # This storage name will be used in command line directly when set diagnostics extension
+        $storagename = 'definedinconfigstorage';
+        $storagetype = 'Standard_GRS';
+        New-AzureRmStorageAccount -ResourceGroupName $rgname -Name $storagename -Location $loc -Type $storagetype;
+
+        $ipCfg = New-AzureRmVmssIPConfig -Name 'test' -SubnetId $subnetId;
+        $vmss = New-AzureRmVmssConfig -Location $loc -SkuCapacity 2 -SkuName 'Standard_A0' -UpgradePolicyMode 'automatic' -NetworkInterfaceConfiguration $netCfg `
+            | Add-AzureRmVmssNetworkInterfaceConfiguration -Name 'test' -Primary $true -IPConfiguration $ipCfg `
+            | Set-AzureRmVmssOSProfile -ComputerNamePrefix 'test' -AdminUsername $adminUsername -AdminPassword $adminPassword `
+            | Set-AzureRmVmssStorageProfile -Name 'test' -OsDiskCreateOption 'FromImage' -OsDiskCaching 'None' `
+            -ImageReferenceOffer $imgRef.Offer -ImageReferenceSku $imgRef.Skus -ImageReferenceVersion $imgRef.Version `
+            -ImageReferencePublisher $imgRef.PublisherName -VhdContainer $vhdContainer;
+
+        # Full parameter test
+        $version = '1.5';
+        $publicSettingFilePath = "$TestOutputRoot\ConfigFiles\DiagnosticsExtensionPublicConfig.json";
+        $privateSettingFilePath = "$TestOutputRoot\ConfigFiles\DiagnosticsExtensionPrivateConfig.json";
+        $vmss = Add-AzureRmVmssDiagnosticsExtension -VirtualMachineScaleSet $vmss -Name $extname -SettingFilePath $publicSettingFilePath `
+            -ProtectedSettingFilePath $privateSettingFilePath -TypeHandlerVersion $version -AutoUpgradeMinorVersion $false;
+
+        $vmssDiagExtensions = $vmss.VirtualMachineProfile.ExtensionProfile.Extensions | Where-Object {$_.Publisher -eq $diagExtPublisher -and $_.Type -eq $diagExtType};
+        Assert-AreEqual 1 $vmssDiagExtensions.Count;
+        $vmssDiagExtension = $vmssDiagExtensions | Select-Object -first 1;
+
+        Assert-AreEqual $extname $vmssDiagExtension.Name;
+        Assert-AreEqual $version $vmssDiagExtension.TypeHandlerVersion;
+        Assert-AreEqual $false $vmssDiagExtension.AutoUpgradeMinorVersion;
+
+        # Storage account key should be filled up by the cmdlet.
+        $storageAccountKey = $vmssDiagExtension.ProtectedSettings['storageAccountKey'];
+        Assert-NotNull $storageAccountKey;
+        Assert-AreNotEqual '' $storageAccountKey;
+
+        # Remove without specifying extension name, diagnostic extension is expected to be removed.
+        $vmss = Remove-AzureRmVmssDiagnosticsExtension -VirtualMachineScaleSet $vmss;
+        $vmssDiagExtensions = $vmss.VirtualMachineProfile.ExtensionProfile.Extensions | Where-Object {$_.Publisher -eq $diagExtPublisher -and $_.Type -eq $diagExtType};
+
+        Assert-Null $vmssDiagExtensions;
+
+        $vmss = $vmss | Add-AzureRmVmssDiagnosticsExtension -Name $extname -SettingFilePath $publicSettingFilePath `
+            | New-AzureRmVmss -ResourceGroupName $rgname -Name $vmssName;
+
+        $vmss = Get-AzureRmVmss -ResourceGroupName $rgname -VMScaleSetName $vmssName;
+
+        $vmssDiagExtensions = $vmss.VirtualMachineProfile.ExtensionProfile.Extensions | Where-Object {$_.Publisher -eq $diagExtPublisher -and $_.Type -eq $diagExtType};
+        Assert-AreEqual 1 $vmssDiagExtensions.Count;
+
+        $vmssDiagExtension = $vmssDiagExtensions | Select-Object -first 1;
+        Assert-AreEqual $extname $vmssDiagExtension.Name;
+        
+        $settings = $vmssDiagExtension.Settings;
+        Assert-AreEqual $storagename $settings.storageAccount.Value;
+
+        Remove-AzureRmVmssDiagnosticsExtension -VirtualMachineScaleSet $vmss -Name $extname;
+        Update-AzureRmVmss -ResourceGroupName $rgname -Name $vmssName -VirtualMachineScaleSet $vmss;
+
+        $vmss = Get-AzureRmVmss -ResourceGroupName $rgname -VMScaleSetName $vmssName;
+        $vmssDiagExtensions = $vmss.VirtualMachineProfile.ExtensionProfile.Extensions | Where-Object {$_.Publisher -eq $diagExtPublisher -and $_.Type -eq $diagExtType};
+
+        Assert-Null $vmssDiagExtensions;
+    }
+    finally
+    {
+        # Cleanup
+        Clean-ResourceGroup $rgname
+    }
+}
