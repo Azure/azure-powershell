0aeab6cf3e8c97324c23ac4145375edbc712c6a8
smithab <smithab@microsoft.com>
2016-10-04 10:34:14 -0700

    updated the test

--- src/ResourceManager/NotificationHubs/Commands.NotificationHubs.Test/ScenarioTests/NotificationHubServiceTests.ps1
+++ src/ResourceManager/NotificationHubs/Commands.NotificationHubs.Test/ScenarioTests/NotificationHubServiceTests.ps1
@@ -180,6 +180,7 @@ function Test-CRUDNamespace
 
     Write-Debug " Get the updated namespace "
     $getUpdatedNamespace = Get-AzureRmNotificationHubsNamespace -ResourceGroup $secondResourceGroup -Namespace $namespaceName2
+	#uncomment once the playback mode doesnt fail on this 
     #Assert-AreEqual $updatedNamespace.Tags.Count $getUpdatedNamespace.Tags.Count
 
     Write-Debug " Delete namespaces"
