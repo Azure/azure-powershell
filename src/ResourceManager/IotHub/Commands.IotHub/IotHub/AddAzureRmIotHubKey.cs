14308c4ecb1fb458d16e4e4dbeaad74edb80e9b6
rkmanda <rmanda@microsoft.com>
2016-10-07 15:48:22 -0700

    Initial commit

---
+++ src/ResourceManager/IotHub/Commands.IotHub/IotHub/AddAzureRmIotHubKey.cs
@@ -0,0 +1,100 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+namespace Microsoft.Azure.Commands.Management.IotHub
+{
+    using System.Collections.Generic;
+    using System.Management.Automation;
+    using System.Linq;
+    using Microsoft.Azure.Commands.Management.IotHub.Common;
+    using Microsoft.Azure.Commands.Management.IotHub.Models;
+    using Microsoft.Azure.Management.IotHub;
+    using Microsoft.Azure.Management.IotHub.Models;
+
+    [Cmdlet(VerbsCommon.Add, "AzureRmIotHubKey")]
+    [OutputType(typeof(PSSharedAccessSignatureAuthorizationRule), typeof(List<PSSharedAccessSignatureAuthorizationRule>))]
+    public class AddAzureRmIotHubKey : IotHubBaseCmdlet
+    {
+
+        [Parameter(
+            Position = 0,
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "Resource Group Name")]
+        [ValidateNotNullOrEmpty]
+        public string ResourceGroupName { get; set; }
+
+        [Parameter(
+            Position = 1,
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "Name")]
+        [ValidateNotNullOrEmpty]
+        public string Name { get; set; }
+
+        [Parameter(
+            Position = 2,
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "Name")]
+        [ValidateNotNullOrEmpty]
+        public string KeyName { get; set; }
+
+        [Parameter(
+            Position = 3,
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "PrimaryKey")]
+        [ValidateNotNullOrEmpty]
+        public string PrimaryKey { get; set; }
+
+        [Parameter(
+            Position = 4,
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "SecondaryKey")]
+        [ValidateNotNullOrEmpty]
+        public string SecondaryKey { get; set; }
+
+        [Parameter(
+            Position = 5,
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "Rights")]
+        [ValidateNotNullOrEmpty]
+        public PSAccessRights Rights { get; set; }
+
+        public override void ExecuteCmdlet()
+        {
+            var psAuthRule = new PSSharedAccessSignatureAuthorizationRule()
+            {
+                KeyName = this.KeyName,
+                PrimaryKey = this.PrimaryKey,
+                SecondaryKey = this.SecondaryKey,
+                Rights = this.Rights
+            };
+
+            var authRule = IotHubUtils.ToSharedAccessSignatureAuthorizationRule(psAuthRule);
+
+            IotHubDescription iothubDesc = this.IotHubClient.IotHubResource.Get(this.ResourceGroupName, this.Name);
+            IList<SharedAccessSignatureAuthorizationRule> authRules = (List<SharedAccessSignatureAuthorizationRule>) this.IotHubClient.IotHubResource.ListKeys(this.ResourceGroupName, this.Name).ToList();
+            authRules.Add(authRule);
+            iothubDesc.Properties.AuthorizationPolicies = authRules;
+
+            this.IotHubClient.IotHubResource.CreateOrUpdate(this.ResourceGroupName, this.Name, iothubDesc);
+            IEnumerable<SharedAccessSignatureAuthorizationRule> updatedAuthRules = this.IotHubClient.IotHubResource.ListKeys(this.ResourceGroupName, this.Name);
+            this.WriteObject(IotHubUtils.ToPSSharedAccessSignatureAuthorizationRules(updatedAuthRules), true);
+        }
+    }
+}
---
+++ src/ResourceManager/IotHub/Commands.IotHub/ResourceManagerStartup.ps1
@@ -0,0 +1,276 @@
+# ----------------------------------------------------------------------------------
+#
+# Copyright Microsoft Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------------
+
+# Authorization script commandlet that builds on top of existing Insights comandlets. 
+# This commandlet gets all events for the "Microsoft.Authorization" resource provider by calling the "Get-AzureRmResourceProviderLog" commandlet
+
+function Get-AzureRmAuthorizationChangeLog { 
+<#
+
+.SYNOPSIS
+
+Gets access change history for the selected subscription for the specified time range i.e. role assignments that were added or removed, including classic administrators (co-administrators and service administrators).
+Maximum duration that can be queried is 15 days (going back up to past 90 days).
+
+
+.DESCRIPTION
+
+The Get-AzureRmAuthorizationChangeLog produces a report of who granted (or revoked) what role to whom at what scope within the subscription for the specified time range. 
+
+The command queries all role assignment events from the Insights resource provider of Azure Resource Manager. Specifying the time range is optional. If both StartTime and EndTime parameters are not specified, the default query interval is the past 1 hour. Maximum duration that can be queried is 15 days (going back up to past 90 days).
+
+
+.PARAMETER StartTime 
+
+Start time of the query. Optional.
+
+
+.PARAMETER EndTime 
+
+End time of the query. Optional
+
+
+.EXAMPLE 
+
+Get-AzureRmAuthorizationChangeLog
+
+Gets the access change logs for the past hour.
+
+
+.EXAMPLE   
+
+Get-AzureRmAuthorizationChangeLog -StartTime "09/20/2015 15:00" -EndTime "09/24/2015 15:00"
+
+Gets all access change logs between the specified dates
+
+Timestamp        : 2015-09-23 21:52:41Z
+Caller           : admin@rbacCliTest.onmicrosoft.com
+Action           : Revoked
+PrincipalId      : 54401967-8c4e-474a-9fbb-a42073f1783c
+PrincipalName    : testUser
+PrincipalType    : User
+Scope            : /subscriptions/9004a9fd-d58e-48dc-aeb2-4a4aec58606f/resourceGroups/TestRG/providers/Microsoft.Network/virtualNetworks/testresource
+ScopeName        : testresource
+ScopeType        : Resource
+RoleDefinitionId : /subscriptions/9004a9fd-d58e-48dc-aeb2-4a4aec58606f/providers/Microsoft.Authorization/roleDefinitions/b24988ac-6180-42a0-ab88-20f7382dd24c
+RoleName         : Contributor
+
+
+.EXAMPLE 
+
+Get-AzureRmAuthorizationChangeLog  -StartTime ([DateTime]::Now - [TimeSpan]::FromDays(5)) -EndTime ([DateTime]::Now) | FT Caller, Action, RoleName, PrincipalName, ScopeType
+
+Gets access change logs for the past 5 days and format the output
+
+Caller                  Action                  RoleName                PrincipalName           ScopeType
+------                  ------                  --------                -------------           ---------
+admin@contoso.com       Revoked                 Contributor             User1                   Subscription
+admin@contoso.com       Granted                 Reader                  User1                   Resource Group
+admin@contoso.com       Revoked                 Contributor             Group1                  Resource
+
+.LINK
+
+New-AzureRmRoleAssignment
+
+.LINK
+
+Get-AzureRmRoleAssignment
+
+.LINK
+
+Remove-AzureRmRoleAssignment
+
+#>
+
+    [CmdletBinding()] 
+    param(  
+        [parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true, HelpMessage = "The start time. Optional
+             If both StartTime and EndTime are not provided, defaults to querying for the past 1 hour. Maximum allowed difference in StartTime and EndTime is 15 days")] 
+        [DateTime] $StartTime,
+
+        [parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true, HelpMessage = "The end time. Optional. 
+            If both StartTime and EndTime are not provided, defaults to querying for the past 1 hour. Maximum allowed difference in StartTime and EndTime is 15 days")] 
+        [DateTime] $EndTime
+    ) 
+    PROCESS { 
+         # Get all events for the "Microsoft.Authorization" provider by calling the Insights commandlet
+         $events = Get-AzureRmLog -ResourceProvider "Microsoft.Authorization" -DetailedOutput -StartTime $StartTime -EndTime $EndTime
+             
+         $startEvents = @{}
+         $endEvents = @{}
+         $offlineEvents = @()
+
+         # StartEvents and EndEvents will contain matching pairs of logs for when role assignments (and definitions) were created or deleted. 
+         # i.e. A PUT on roleassignments will have a Start-End event combination and a DELETE on roleassignments will have another Start-End event combination
+         $startEvents = $events | ? { $_.httpRequest -and $_.Status -ieq "Started" }
+         $events | ? { $_.httpRequest -and $_.Status -ne "Started" } | % { $endEvents[$_.OperationId] = $_ }
+         # This filters non-RBAC events like classic administrator write or delete
+         $events | ? { $_.httpRequest -eq $null } | % { $offlineEvents += $_ } 
+
+         $output = @()
+
+         # Get all role definitions once from the service and cache to use for all 'startevents'
+         $azureRoleDefinitionCache = @{}
+         Get-AzureRmRoleDefinition | % { $azureRoleDefinitionCache[$_.Id] = $_ }
+
+         $principalDetailsCache = @{}
+
+         # Process StartEvents
+         # Find matching EndEvents that succeeded and relating to role assignments only
+         $startEvents | ? { $endEvents.ContainsKey($_.OperationId) `
+             -and $endEvents[$_.OperationId] -ne $null `
+             -and $endevents[$_.OperationId].OperationName.StartsWith("Microsoft.Authorization/roleAssignments", [System.StringComparison]::OrdinalIgnoreCase)  `
+             -and $endEvents[$_.OperationId].Status -ieq "Succeeded"} |  % {
+       
+         $endEvent = $endEvents[$_.OperationId];
+        
+         # Create the output structure
+         $out = "" | select Timestamp, Caller, Action, PrincipalId, PrincipalName, PrincipalType, Scope, ScopeName, ScopeType, RoleDefinitionId, RoleName
+				 
+         $out.Timestamp = Get-Date -Date $endEvent.EventTimestamp -Format u
+         $out.Caller = $_.Caller
+         if ($_.HttpRequest.Method -ieq "PUT") {
+            $out.Action = "Granted"
+            if ($_.Properties.Content.ContainsKey("requestbody")) {
+                $messageBody = ConvertFrom-Json $_.Properties.Content["requestbody"]
+            }
+             
+          $out.Scope =  $_.Authorization.Scope
+        } 
+        elseif ($_.HttpRequest.Method -ieq "DELETE") {
+            $out.Action = "Revoked"
+            if ($endEvent.Properties.Content.ContainsKey("responseBody")) {
+                $messageBody = ConvertFrom-Json $endEvent.Properties.Content["responseBody"]
+            }
+        }
+
+        if ($messageBody) {
+            # Process principal details
+            $out.PrincipalId = $messageBody.properties.principalId
+            if ($out.PrincipalId -ne $null) { 
+				# Get principal details by querying Graph. Cache principal details and read from cache if present
+				$principalId = $out.PrincipalId 
+                
+				if($principalDetailsCache.ContainsKey($principalId)) {
+					# Found in cache
+                    $principalDetails = $principalDetailsCache[$principalId]
+                } else { # not in cache
+		            $principalDetails = "" | select Name, Type
+                    $user = Get-AzureRmADUser -ObjectId $principalId
+                    if ($user) {
+                        $principalDetails.Name = $user.DisplayName
+                        $principalDetails.Type = "User"    
+                    } else {
+                        $group = Get-AzureRmADGroup -ObjectId $principalId
+                        if ($group) {
+                            $principalDetails.Name = $group.DisplayName
+                            $principalDetails.Type = "Group"        
+                        } else {
+                            $servicePrincipal = Get-AzureRmADServicePrincipal -objectId $principalId
+                            if ($servicePrincipal) {
+                                $principalDetails.Name = $servicePrincipal.DisplayName
+                                $principalDetails.Type = "Service Principal"                        
+                            }
+                        }
+                    }              
+					# add principal details to cache
+                    $principalDetailsCache.Add($principalId, $principalDetails);
+	            }
+
+                $out.PrincipalName = $principalDetails.Name
+                $out.PrincipalType = $principalDetails.Type
+            }
+
+			# Process scope details
+            if ([string]::IsNullOrEmpty($out.Scope)) { $out.Scope = $messageBody.properties.Scope }
+            if ($out.Scope -ne $null) {
+				# Remove the authorization provider details from the scope, if present
+			    if ($out.Scope.ToLower().Contains("/providers/microsoft.authorization")) {
+					$index = $out.Scope.ToLower().IndexOf("/providers/microsoft.authorization") 
+					$out.Scope = $out.Scope.Substring(0, $index) 
+				}
+
+              	$scope = $out.Scope 
+				$resourceDetails = "" | select Name, Type
+                $scopeParts = $scope.Split('/', [System.StringSplitOptions]::RemoveEmptyEntries)
+                $len = $scopeParts.Length
+
+                if ($len -gt 0 -and $len -le 2 -and $scope.ToLower().Contains("subscriptions"))	{
+                    $resourceDetails.Type = "Subscription"
+                    $resourceDetails.Name  = $scopeParts[1]
+                } elseif ($len -gt 0 -and $len -le 4 -and $scope.ToLower().Contains("resourcegroups")) {
+                    $resourceDetails.Type = "Resource Group"
+                    $resourceDetails.Name  = $scopeParts[3]
+                    } elseif ($len -ge 6 -and $scope.ToLower().Contains("providers")) {
+                        $resourceDetails.Type = "Resource"
+                        $resourceDetails.Name  = $scopeParts[$len -1]
+                        }
+                
+				$out.ScopeName = $resourceDetails.Name
+                $out.ScopeType = $resourceDetails.Type
+            }
+
+			# Process Role definition details
+            $out.RoleDefinitionId = $messageBody.properties.roleDefinitionId
+			
+            if ($out.RoleDefinitionId -ne $null) {
+								
+				#Extract roleDefinitionId Guid value from the fully qualified id string.
+				$roleDefinitionIdGuid= $out.RoleDefinitionId.Substring($out.RoleDefinitionId.LastIndexOf("/")+1)
+
+                if ($azureRoleDefinitionCache[$roleDefinitionIdGuid]) {
+                    $out.RoleName = $azureRoleDefinitionCache[$roleDefinitionIdGuid].Name
+                } else {
+                    $out.RoleName = ""
+                }
+            }
+        }
+        $output += $out
+    } # start event processing complete
+
+    # Filter classic admins events
+    $offlineEvents | % {
+        if($_.Status -ne $null -and $_.Status -ieq "Succeeded" -and $_.OperationName -ne $null -and $_.operationName.StartsWith("Microsoft.Authorization/ClassicAdministrators", [System.StringComparison]::OrdinalIgnoreCase)) {
+            
+            $out = "" | select Timestamp, Caller, Action, PrincipalId, PrincipalName, PrincipalType, Scope, ScopeName, ScopeType, RoleDefinitionId, RoleName
+            $out.Timestamp = Get-Date -Date $_.EventTimestamp -Format u
+            $out.Caller = "Subscription Admin"
+
+            if($_.operationName -ieq "Microsoft.Authorization/ClassicAdministrators/write"){
+                $out.Action = "Granted"
+            } 
+            elseif($_.operationName -ieq "Microsoft.Authorization/ClassicAdministrators/delete"){
+                $out.Action = "Revoked"
+            }
+
+            $out.RoleDefinitionId = $null
+            $out.PrincipalId = $null
+            $out.PrincipalType = "User"
+            $out.Scope = "/subscriptions/" + $_.SubscriptionId
+            $out.ScopeType = "Subscription"
+            $out.ScopeName = $_.SubscriptionId
+                                
+            if($_.Properties -ne $null){
+                $out.PrincipalName = $_.Properties.Content["adminEmail"]
+                $out.RoleName = "Classic " + $_.Properties.Content["adminType"]
+            }
+                     
+            $output += $out
+        }
+    } # end offline events
+
+    $output | Sort Timestamp
+} 
+} # End commandlet
+ 
