35e3e55629b72dbd1c836d25b275fe22f09112d5
shahabhijeet <abhimswork@outlook.com>
2016-10-14 15:14:06 -0700

    Added parameterset to Set-TestFramework function, added function to pretty print connection string, removed commented code and irrelevant functions

--- tools/Repo-Tasks.psm1
+++ tools/Repo-Tasks.psm1
@@ -4,7 +4,6 @@ $userPsFileDir = [string]::Empty
 
 [string]$envVariableName="TEST_CSM_ORGID_AUTHENTICATION"
 
-
 [CmdletBinding]
 Function Set-TestEnvironment
 {
@@ -15,43 +14,57 @@ In order to successfully run a test, you will need SubscriptionId, TenantId
 This cmdlet will only prompt you for Subscription and Tenant information, rest all other parameters are optional
 
 #>
+    [CmdletBinding(DefaultParameterSetName='UserIdParamSet')]
     param(
-        [parameter(Mandatory=$true, Position=0, HelpMessage='SubscriptionId you would like to use')]
-        [ValidateNotNullOrEmpty()]
-        [string]$SubscriptionId,
-
-        [parameter(Mandatory=$true, Position=1, HelpMessage='AADTenant/TenantId you would like to use:')]
-        [ValidateNotNullOrEmpty()]
-        [string]$TenantId,
-
-        [parameter(Mandatory=$false, Position=2, HelpMessage='UserId (OrgId) you would like to use')]
+        [Parameter(ParameterSetName='UserIdParamSet', Mandatory=$true, HelpMessage = "UserId (OrgId) you would like to use")]
         [ValidateNotNullOrEmpty()]
         [string]$UserId,
 
-        [parameter(Mandatory=$false, Position=3, HelpMessage='ServicePrincipal/ClientId you would like to use')]
+        [Parameter(ParameterSetName='UserIdParamSet', Mandatory=$true, HelpMessage = "UserId (OrgId) you would like to use")]
+        [ValidateNotNullOrEmpty()]
+        [string]$Password,
+
+        [Parameter(ParameterSetName='SpnParamSet', Mandatory=$true, HelpMessage='ServicePrincipal/ClientId you would like to use')]   
         [ValidateNotNullOrEmpty()]
         [string]$ServicePrincipal,
 
-        [parameter(Mandatory=$false, Position=4, HelpMessage='ServicePrincipal Secret/Client Secret you would like to use')]
+        [Parameter(ParameterSetName='SpnParamSet', Mandatory=$true, HelpMessage='ServicePrincipal Secret/ClientId Secret you would like to use')]
         [ValidateNotNullOrEmpty()]
         [string]$ServicePrincipalSecret,
 
-        [parameter(Mandatory=$false, Position=5, HelpMessage='RecordMode you would like to set')]    
-        [string]$RecordMode = "Playback",
+        [Parameter(ParameterSetName='SpnParamSet', Mandatory=$true)]
+        [Parameter(ParameterSetName='UserIdParamSet', Mandatory=$true, HelpMessage = "SubscriptionId you would like to use")]
+        [ValidateNotNullOrEmpty()]
+        [string]$SubscriptionId,
 
-        [parameter(Mandatory=$false, Position=6, HelpMessage='Target Environment to use {Prod (default) | Dogfood | Next | Current')]
-        [string]$TargetEnvironment = "Prod"
+        [Parameter(ParameterSetName='SpnParamSet', Mandatory=$true, HelpMessage='AADTenant/TenantId you would like to use')]
+        [ValidateNotNullOrEmpty()]
+        [string]$TenantId,
+
+        [ValidateSet("Playback", "Record", "None")]
+        [string]$RecordMode='Playback',
+
+        [ValidateSet("Prod", "Dogfood", "Current", "Next")]
+        [string]$TargetEnvironment='Prod'
     )
 
-  [PSCredential] 
+    [string]$uris="https://management.azure.com/;AADAuthEndpoint=https://login.windows.net/;GraphUri=https://graph.windows.net/"
 
-    [string]$uris="BaseUri=https://management.azure.com/;AADAuthEndpoint=https://login.windows.net/;GraphUri=https://graph.windows.net/"
-
-    $formattedConnStr = [string]::Format("SubscriptionId={0};AADTenant={1};HttpRecorderMode={2};Environment={3}", $SubscriptionId, $TenantId, $RecordMode, $TargetEnvironment)
+    $formattedConnStr = [string]::Format("SubscriptionId={0};HttpRecorderMode={1};Environment={2}", $SubscriptionId, $RecordMode, $TargetEnvironment)
 
     if([string]::IsNullOrEmpty($UserId) -eq $false)
     {
         $formattedConnStr = [string]::Format([string]::Concat($formattedConnStr, ";UserId={0}"), $UserId)
+    }
+
+    if([string]::IsNullOrEmpty($Password) -eq $false)
+    {
+        $formattedConnStr = [string]::Format([string]::Concat($formattedConnStr, ";Password={0}"), $Password)
+    }
+
+    if([string]::IsNullOrEmpty($TenantId) -eq $false)
+    {
+        $formattedConnStr = [string]::Format([string]::Concat($formattedConnStr, ";AADTenant={0}"), $TenantId)
     }
 
     if([string]::IsNullOrEmpty($ServicePrincipal) -eq $false)
@@ -63,100 +76,85 @@ This cmdlet will only prompt you for Subscription and Tenant information, rest a
     {
         $formattedConnStr = [string]::Format([string]::Concat($formattedConnStr, ";ServicePrincipalSecret={0}"), $ServicePrincipalSecret)
     }
+    
+    $formattedConnStr = [string]::Format([string]::Concat($formattedConnStr, ";BaseUri={0}"), $uris)
 
-    $formattedConnStr = [string]::Concat($formattedConnStr, ";", $uris)
+    Write-Host "Below connection string is ready to be set"
+    Print-ConnectionString $UserId $Password $SubscriptionId $TenantId $ServicePrincipal $ServicePrincipalSecret $RecordMode $TargetEnvironment $uris
 
     #Set connection string to Environment variable
     $env:TEST_CSM_ORGID_AUTHENTICATION=$formattedConnStr
     Write-Host ""
 
     # Retrieve the environment variable
-    Write-Host "Setting up below connection string. Start Visual Studio by typing devenv" -ForegroundColor Green
+    Write-Host ""
+    Write-Host "Below connection string was set. Start Visual Studio by typing devenv" -ForegroundColor Green
     [Environment]::GetEnvironmentVariable($envVariableName)
     Write-Host ""
     
     Write-Host "If your needs demand you to set connection string differently, for all the supported Key/Value pairs in connection string"
-    Write-Host "Please visit https://github.com/Azure/azure-powershell/blob/dev/documentation/Using-Azure-TestFramework.md"
+    Write-Host "Please visit https://github.com/Azure/azure-powershell/blob/dev/documentation/Using-Azure-TestFramework.md" -ForegroundColor Yellow
 }
 
-
-
-<#
-
-[CmdletBinding]
-Function Set-TestEnvironment
+Function Print-ConnectionString([string]$uid, [string]$pwd, [string]$subId, [string]$aadTenant, [string]$spn, [string]$spnSecret, [string]$recordMode, [string]$targetEnvironment, [string]$uris)
 {
-<#
-.SYNOPSIS
-This cmdlet helps you to setup Test Environment for running tests
-In order to successfully run a test, you will need SubscriptionId, TenantId
-This cmdlet will only prompt you for Subscription and Tenant information, rest all other parameters are optional
 
-
-    param(
-        [parameter(Mandatory=$true, Position=0, HelpMessage='SubscriptionId you would like to use')]
-        [ValidateNotNullOrEmpty()]
-        [string]$SubscriptionId,
-
-        [parameter(Mandatory=$true, Position=1, HelpMessage='AADTenant/TenantId you would like to use:')]
-        [ValidateNotNullOrEmpty()]
-        [string]$TenantId,
-
-        [parameter(Mandatory=$false, Position=2, HelpMessage='UserId (OrgId) you would like to use')]
-        [ValidateNotNullOrEmpty()]
-        [string]$UserId,
-
-        [parameter(Mandatory=$false, Position=3, HelpMessage='ServicePrincipal/ClientId you would like to use')]
-        [ValidateNotNullOrEmpty()]
-        [string]$ServicePrincipal,
-
-        [parameter(Mandatory=$false, Position=4, HelpMessage='ServicePrincipal Secret/Client Secret you would like to use')]
-        [ValidateNotNullOrEmpty()]
-        [string]$ServicePrincipalSecret,
-
-        [parameter(Mandatory=$false, Position=5, HelpMessage='RecordMode you would like to set')]    
-        [string]$RecordMode = "Playback",
-
-        [parameter(Mandatory=$false, Position=6, HelpMessage='Target Environment to use {Prod (default) | Dogfood | Next | Current')]
-        [string]$TargetEnvironment = "Prod"
-    )
-
-    [string]$uris="BaseUri=https://management.azure.com/;AADAuthEndpoint=https://login.windows.net/;GraphUri=https://graph.windows.net/"
-
-    $formattedConnStr = [string]::Format("SubscriptionId={0};AADTenant={1};HttpRecorderMode={2};Environment={3}", $SubscriptionId, $TenantId, $RecordMode, $TargetEnvironment)
-
-    if([string]::IsNullOrEmpty($UserId) -eq $false)
+    if([string]::IsNullOrEmpty($uid) -eq $false)
     {
-        $formattedConnStr = [string]::Format([string]::Concat($formattedConnStr, ";UserId={0}"), $UserId)
+        Write-Host "UserId=" -ForegroundColor Green -NoNewline
+        Write-Host $uid";" -NoNewline 
     }
 
-    if([string]::IsNullOrEmpty($ServicePrincipal) -eq $false)
+    if([string]::IsNullOrEmpty($pwd) -eq $false)
     {
-        $formattedConnStr = [string]::Format([string]::Concat($formattedConnStr, ";ServicePrincipal={0}"), $ServicePrincipal)
+        Write-Host "Password=" -ForegroundColor Green -NoNewline
+        Write-Host $pwd";" -NoNewline 
     }
 
-    if([string]::IsNullOrEmpty($ServicePrincipalSecret) -eq $false)
+    if([string]::IsNullOrEmpty($subId) -eq $false)
     {
-        $formattedConnStr = [string]::Format([string]::Concat($formattedConnStr, ";ServicePrincipalSecret={0}"), $ServicePrincipalSecret)
+        Write-Host "SubscriptionId=" -ForegroundColor Green -NoNewline
+        Write-Host $subId";" -NoNewline 
     }
 
-    $formattedConnStr = [string]::Concat($formattedConnStr, ";", $uris)
+    if([string]::IsNullOrEmpty($aadTenant) -eq $false)
+    {
+        Write-Host "AADTenant=" -ForegroundColor Green -NoNewline
+        Write-Host $aadTenant";" -NoNewline
+    }
 
-    #Set connection string to Environment variable
-    $env:TEST_CSM_ORGID_AUTHENTICATION=$formattedConnStr
+    if([string]::IsNullOrEmpty($spn) -eq $false)
+    {
+        Write-Host "ServicePrincipal=" -ForegroundColor Green -NoNewline
+        Write-Host $spn";" -NoNewline
+    }
+
+    if([string]::IsNullOrEmpty($spnSecret) -eq $false)
+    {
+        Write-Host "ServicePrincipalSecret=" -ForegroundColor Green -NoNewline
+        Write-Host $spnSecret";" -NoNewline
+    }
+
+    if([string]::IsNullOrEmpty($recordMode) -eq $false)
+    {
+        Write-Host "HttpRecorderMode=" -ForegroundColor Green -NoNewline
+        Write-Host $recordMode";" -NoNewline
+    }
+
+    if([string]::IsNullOrEmpty($targetEnvironment) -eq $false)
+    {
+        Write-Host "Environment=" -ForegroundColor Green -NoNewline
+        Write-Host $targetEnvironment";" -NoNewline
+    }
+
+    if([string]::IsNullOrEmpty($uris) -eq $false)
+    {
+        Write-Host "BaseUri=" -ForegroundColor Green -NoNewline
+        Write-Host $uris";" -NoNewline
+    }
+
     Write-Host ""
-
-    # Retrieve the environment variable
-    Write-Host "Setting up below connection string. Start Visual Studio by typing devenv" -ForegroundColor Green
-    [Environment]::GetEnvironmentVariable($envVariableName)
-    Write-Host ""
-    
-    Write-Host "If your needs demand you to set connection string differently, for all the supported Key/Value pairs in connection string"
-    Write-Host "Please visit https://github.com/Azure/azure-powershell/blob/dev/documentation/Using-Azure-TestFramework.md"
 }
-#>
-
-
 
 [CmdletBinding]
 Function Get-BuildScopes
@@ -244,11 +242,9 @@ Function Init()
     }
 }
 
-
 export-modulemember -Function Set-TestEnvironment
 export-modulemember -Function Get-BuildScopes
 export-modulemember -Function Start-Build
 export-modulemember -Function Invoke-CheckinTests
 
-#Initialize
 Init
