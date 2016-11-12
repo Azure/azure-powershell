0d04f719603f798523ea024656dabc781cf09974
Ilshat Ahatov <ilahat@microsoft.com>
2016-11-11 18:39:13 -0800

    Revert "Search code base for any deprecation warning and remove it"
    This reverts commit ebc473c3a2c9546c16516d124331a15cb2f931d3.

--- src/ResourceManager/Resources/Commands.ResourceManager/Cmdlets/Implementation/Resource/SetAzureResourceCmdlet.cs
+++ src/ResourceManager/Resources/Commands.ResourceManager/Cmdlets/Implementation/Resource/SetAzureResourceCmdlet.cs
@@ -82,6 +82,11 @@ namespace Microsoft.Azure.Commands.ResourceManager.Cmdlets.Implementation
         {
             base.OnProcessRecord();
 
+            if (!string.IsNullOrEmpty(this.ODataQuery))
+            {
+                this.WriteWarning("The ODataQuery parameter is being deprecated in Set-AzureRmResource cmdlet and will be removed in a future release.");
+            }
+
             var resourceId = this.GetResourceId();
             this.ConfirmAction(
                 this.Force,
