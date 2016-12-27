e4ae6c0fa1123002750f4e7931f604350adcbb13
aliasgar16 <aliasgar.batterywala@msystechnologies.com>
2016-12-27 13:11:12 +0000

    Fixed minor bugs.
    Signed-off-by: aliasgar16 <aliasgar.batterywala@msystechnologies.com>

--- src/ServiceManagement/Compute/Commands.ServiceManagement/IaaS/Extensions/Chef/VirtualMachineChefExtensionCmdletBase.cs
+++ src/ServiceManagement/Compute/Commands.ServiceManagement/IaaS/Extensions/Chef/VirtualMachineChefExtensionCmdletBase.cs
@@ -21,7 +21,7 @@ namespace Microsoft.WindowsAzure.Commands.ServiceManagement.IaaS.Extensions
         protected const string ExtensionDefaultPublisher = "Chef.Bootstrap.WindowsAzure";
         protected const string ExtensionDefaultName = "ChefClient";
         protected const string LinuxExtensionName = "LinuxChefClient";
-        protected const string PrivateConfigurationTemplate = "{{\"validation_key\":\"{0}\"}}";
+        protected const string PrivateConfigurationTemplate = "validation_key";
         protected const string ClientRbTemplate = "client_rb";
         protected const string BootstrapVersionTemplate = "bootstrap_version";
         protected const string BootStrapOptionsTemplate = "bootstrap_options";
