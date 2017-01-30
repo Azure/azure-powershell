637456780480a11af126bfb66075c46387a02ea9
shahabhijeet <abhimswork@outlook.com>
2017-01-30 00:45:45 -0800

    Adding test for checking if contents of msi are signed including .msi.

--- setup/PowerShellSetup.Test/MsiLayoutTests.cs
+++ setup/PowerShellSetup.Test/MsiLayoutTests.cs
@@ -4,6 +4,16 @@
     //using Microsoft.WindowsAzure.Commands.ScenarioTest;
     using Xunit;
     using Microsoft.Azure.Management.Resources;
+    using System.Diagnostics;
+    using System;
+    using System.Reflection;
+    using System.IO;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Management.Automation;
+    using System.Threading.Tasks;
+    using Microsoft.WindowsAzure.Commands.ScenarioTest;
+
     //using RestTestFramework = Microsoft.Rest.ClientRuntime.Azure.TestFramework;
 
     /// <summary>
@@ -19,34 +29,117 @@
     /// </summary>
     public class MsiLayoutTests
     {
+        string procOutput = string.Empty;
+        string procErr = string.Empty;
+
         public MsiLayoutTests()
         {
-
+            procOutput = string.Empty;
+            procErr = string.Empty;
         }
 
         [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
         public void VerifyMsiExecExists()
         {
+            ProcessStartInfo psi = GetInitializedPSI();
+            psi.Arguments = "/qr /x:1234";
 
+            ExecuteShellCmd(psi, out procOutput, out procErr);
+            Assert.NotEmpty(procOutput);
         }
 
         [Fact]
-        //[Trait(Category.AcceptanceType, Category.CheckIn)]
-        public void VerifyFilesAreSigned(Xunit.Abstractions.ITestOutputHelper output)
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void VerifyFilesAreSigned()
         {
+            var testLoc = new Uri(Assembly.GetExecutingAssembly().CodeBase);
+            var codebasePath = Uri.UnescapeDataString(testLoc.AbsolutePath);
+            var dirPath = Path.GetDirectoryName(codebasePath);
+            string msiFullPath = Path.Combine(dirPath, "AzurePowerShell.msi");
+            string msiContentsDirPath = Path.Combine(dirPath, "msiContents");
+            Assert.True(File.Exists(msiFullPath));
+
+            ProcessStartInfo psi = GetInitializedPSI();
             
+            psi.Arguments = string.Format("/a {0} /qn TargetDir={1}", msiFullPath, msiContentsDirPath);
+            ExecuteShellCmd(psi, out procOutput, out procErr);
+            Assert.True(string.IsNullOrEmpty(procErr));
+
+            IEnumerable<string>msiFiles = Directory.EnumerateFiles(msiContentsDirPath, "*", SearchOption.AllDirectories);
+            Assert.NotNull(msiFiles);
+            List<string> msiFileList = msiFiles.ToList<string>();
+            Assert.True(msiFileList.Count > 0);
+            List<string> unsignedFiles = GetUnsignedFiles(msiFileList);
+
+            Assert.True(unsignedFiles.Count == 0);
         }
 
-        [Fact]
-        //[Trait(Category.AcceptanceType, Category.CheckIn)]
-        public void CreateVM_Test1()
+        private void ExecuteShellCmd(ProcessStartInfo psi, out string procOutput, out string procErr)
         {
-            //CSMTestEnvironmentFactory csmTestFactory = new CSMTestEnvironmentFactory();
-            //ResourceManagementClient ResourceMgmtClient = TestBase.GetServiceClient<ResourceManagementClient>(csmTestFactory);
+            procOutput = string.Empty;
+            procErr = string.Empty;
+            Process proc = Process.Start(psi);
+            
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                procOutput = proc.StandardOutput.ReadToEnd();
+                procOutput = procOutput.Replace("\0", string.Empty);
+            }
 
-            //Test-PSScript is the name of a function in your Powershell script that you would like to execute
-            //ServiceTestController.NewInstance.RunPsTest("Test-PSScript"); 
-            Assert.Equal<bool>(true, true);
+            while (!proc.StandardError.EndOfStream)
+            {
+                procErr = proc.StandardError.ReadToEnd();
+                procErr = procErr.Replace("\0", string.Empty);
+            }
+
+            proc.WaitForExit(5000);
+        }
+
+        private ProcessStartInfo GetInitializedPSI()
+        {
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.RedirectStandardError = true;
+            psi.RedirectStandardInput = true;
+            psi.RedirectStandardOutput = true;
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+            psi.FileName = "Msiexec.exe";
+            return psi;
+        }
+
+        private List<string> GetUnsignedFiles(List<string> signedFiles)
+        {
+            List<string> unsignedFiles = new List<string>();
+
+            Parallel.ForEach<string>(signedFiles, (providedFilePath) =>
+            {
+                bool isSigned = true;
+                string fileName = Path.GetFileName(providedFilePath);
+                string calculatedFullPath = Path.GetFullPath(providedFilePath);
+
+                if (File.Exists(calculatedFullPath))
+                {
+                    using (PowerShell ps = PowerShell.Create())
+                    {
+                        ps.AddCommand("Get-AuthenticodeSignature", true);
+                        ps.AddParameter("FilePath", calculatedFullPath);
+                        var cmdLetResults = ps.Invoke();
+
+                        foreach (PSObject result in cmdLetResults)
+                        {
+                            Signature s = (Signature)result.BaseObject;
+                            isSigned = s.Status.Equals(SignatureStatus.Valid);
+                            if (isSigned == false)
+                            {
+                                unsignedFiles.Add(providedFilePath);
+                            }
+                        }
+                    }
+                }
+            });
+
+            return unsignedFiles;
         }
     }
 }
