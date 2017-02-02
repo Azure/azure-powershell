4432396939fb32ee0f04b964a8cac1c5a9f1cf08
zhencui <zhencui@microsoft.com>
2017-02-02 14:10:16 -0800

    Update VM/VMSS diagnostics extensions to support WAD 1.8

--- src/ResourceManager/Compute/Commands.Compute.Test/ScenarioTests/DiagnosticsExtensionTests.cs
+++ src/ResourceManager/Compute/Commands.Compute.Test/ScenarioTests/DiagnosticsExtensionTests.cs
@@ -12,7 +12,9 @@
 // limitations under the License.
 // ----------------------------------------------------------------------------------
 
+using Microsoft.Azure.Commands.Compute.Common;
 using Microsoft.WindowsAzure.Commands.ScenarioTest;
+using System;
 using Xunit;
 
 namespace Microsoft.Azure.Commands.Compute.Test.ScenarioTests
@@ -57,6 +59,78 @@ namespace Microsoft.Azure.Commands.Compute.Test.ScenarioTests
         public void TestVmssDiagnosticsExtension()
         {
             ComputeTestController.NewInstance.RunPsTest("Test-VmssDiagnosticsExtension");
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestDiagnosticsConfigBuilderMismatchAccountNames()
+        {
+            string pubJsonConfigPath = @"ConfigFiles\DiagnosticsExtensionPublicConfigWithStorageType.json";
+            string privJsonConfigPath = @"ConfigFiles\DiagnosticsExtensionprivateConfigWithWrongName.json";
+
+            var exception = Record.Exception(() => DiagnosticsHelper.GetConfigurationsFromFiles(pubJsonConfigPath, privJsonConfigPath, "a-resouce-id", null, null));
+            Assert.IsType(typeof(ArgumentException), exception);
+
+
+            string[] configs = new[] {
+                @"ConfigFiles\DiagnosticsExtensionConfigWithWrongName.json",
+                @"ConfigFiles\DiagnosticsExtensionConfigWithWrongName.xml"
+            };
+
+            foreach (var configPath in configs)
+            {
+                exception = Record.Exception(() => DiagnosticsHelper.GetPrivateDiagnosticsConfiguration(configPath, "wrong-name", "a-key", "an-endpoint"));
+                Assert.IsType(typeof(ArgumentException), exception);
+            }
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestDiagnosticsConfigBuilderWithSasToken()
+        {
+            string pubJsonConfigPath = @"ConfigFiles\DiagnosticsExtensionPublicConfigWithStorageType.json";
+            string privJsonConfigPath = @"ConfigFiles\DiagnosticsExtensionprivateConfigWithSasToken.json";
+
+            string sasTokenValue = "This-is-a-sas-token";
+            var result = DiagnosticsHelper.GetConfigurationsFromFiles(pubJsonConfigPath, privJsonConfigPath, "a-resource-id", null, null);
+            Assert.Equal<string>(sasTokenValue, result.Item2["storageAccountSasToken"] as string);
+
+            string[] configs = new[] {
+                @"ConfigFiles\DiagnosticsExtensionConfigWithSasToken.json",
+                @"ConfigFiles\DiagnosticsExtensionConfigWithSasToken.xml"
+            };
+
+            foreach (var configPath in configs)
+            {
+                var privateSettings = DiagnosticsHelper.GetPrivateDiagnosticsConfiguration(configPath, "[StorageAccountName]", "a-key", "an-endpoint");
+                Assert.Null(privateSettings["storageAccountKey"]);
+                Assert.NotNull(privateSettings["storageAccountEndPoint"]);
+                Assert.Equal<string>(sasTokenValue, privateSettings["storageAccountSasToken"] as string);
+            }
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestDiagnosticsConfigBuilderWithStorageType()
+        {
+            string pubJsonConfigPath = @"ConfigFiles\DiagnosticsExtensionPublicConfigWithStorageType.json";
+            string privJsonConfigPath = @"ConfigFiles\DiagnosticsExtensionprivateConfigWithSasToken.json";
+
+            string storageTypeValue = "TableAndBlob";
+
+            var result = DiagnosticsHelper.GetConfigurationsFromFiles(pubJsonConfigPath, privJsonConfigPath, "a-resource-id", null, null);
+            Assert.Equal<string>(storageTypeValue, result.Item1["StorageType"] as string);
+
+            string[] configs = new[] {
+                @"ConfigFiles\DiagnosticsExtensionConfigWithStorageType.json",
+                @"ConfigFiles\DiagnosticsExtensionConfigWithStorageType.xml"
+            };
+
+            foreach (var configPath in configs)
+            {
+                var publicSettings = DiagnosticsHelper.GetPublicDiagnosticsConfigurationFromFile(configPath, "[StorageAccountName]", "dummy", null);
+                Assert.Equal<string>(storageTypeValue, publicSettings["StorageType"] as string);
+            }
         }
     }
 }
