a7fe00fbcdd8142f806c0433407d7992551ba64e
Chester Lam <chelam@microsoft.com>
2017-02-27 11:38:48 -0800

    BGP route APIs

---
+++ src/ResourceManager/Network/Commands.Network/VirtualNetworkGateway/GetAzureVirtualNetworkGatewayBgpPeerStatus.cs
@@ -0,0 +1,60 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using AutoMapper;
+using Microsoft.Azure.Commands.Network.Models;
+using Microsoft.Azure.Management.Network;
+using System.Collections.Generic;
+using System.Management.Automation;
+
+namespace Microsoft.Azure.Commands.Network
+{
+    [Cmdlet(VerbsCommon.Get, "AzureRmVirtualNetworkGatewayBgpPeerStatus"), OutputType(typeof(PSBgpPeerStatus))]
+    public class GetAzureVirtualNetworkGatewayBgpPeerStatus : VirtualNetworkGatewayBaseCmdlet
+    {
+        [Alias("ResourceName")]
+        [Parameter(
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "Virtual network gateway name")]
+        [ValidateNotNullOrEmpty]
+        public virtual string Name { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "Virtual network gateway resource group's name")]
+        [ValidateNotNullOrEmpty]
+        public virtual string ResourceGroupName { get; set; }
+
+        [Parameter(
+            Mandatory = false,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "IP of the peer to retrieve status for")]
+        public virtual string Peer { get; set; }
+
+        public override void Execute()
+        {
+            base.Execute();
+
+            List<PSBgpPeerStatus> peerStatuses = new List<PSBgpPeerStatus>();
+            foreach (var peerStatus in this.VirtualNetworkGatewayClient.GetBgpPeerStatus(this.ResourceGroupName, this.Name, this.Peer).Value)
+            {
+                peerStatuses.Add(Mapper.Map<PSBgpPeerStatus>(peerStatus));
+            }
+
+            WriteObject(peerStatuses, true);
+        }
+    }
+}
