7a4f2768022de4a45224d1cc1b3324da46544daf
jobatzil <jobatzil@microsoft.com>
2017-03-06 22:10:21 -0800

    #ConnectionDraining changed WriteObject Method that is used in get cmdlet for connection draining and WAF.

--- src/ResourceManager/Network/Commands.Network/ApplicationGateway/ConnectionDraining/GetAzureApplicationGatewayConnectionDrainingCommand.cs
+++ src/ResourceManager/Network/Commands.Network/ApplicationGateway/ConnectionDraining/GetAzureApplicationGatewayConnectionDrainingCommand.cs
@@ -30,7 +30,7 @@ namespace Microsoft.Azure.Commands.Network
         public override void ExecuteCmdlet()
         {
             base.ExecuteCmdlet();
-            WriteObject(this.BackendHttpSettings.ConnectionDraining, true);
+            WriteObject(this.BackendHttpSettings.ConnectionDraining);
         }
     }
 }
