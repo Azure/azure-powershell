dea3b282b54f7afae75857ea8ca14bbf87b80c98
markcowl <markcowl@microsoft.com>
2017-03-28 15:25:24 -0700

    Adding abstractions classes for authentication types

---
+++ src/Common/Commands.Common.Authentication.Abstractions/TokenStore.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commands.Common.Authentication.Abstractions
+{
+    public class TokenStore : TokenCache
+    {
+        public override IEnumerable<TokenCacheItem> ReadItems()
+        {
+            return base.ReadItems();
+        }
+
+        public override void Clear()
+        {
+            base.Clear();
+        }
+
+        public override void DeleteItem(TokenCacheItem item)
+        {
+            base.DeleteItem(item);
+        }
+
+        public void HandleBeforeAccess(TokenCacheNotificationArgs args)
+        {
+
+        }
+
+        public void HandleAfterAccess(TokenCacheNotificationArgs args)
+        {
+
+        }
+    }
+}
