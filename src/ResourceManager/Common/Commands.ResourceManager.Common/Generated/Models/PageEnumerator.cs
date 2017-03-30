2bc4790fb5093d966c850a2f6735419a7778330d
cormacpayne <corm@microsoft.com>
2017-03-30 12:17:59 -0700

    Clean up MoveNext method

--- src/ResourceManager/Common/Commands.ResourceManager.Common/Generated/Models/PageEnumerator.cs
+++ src/ResourceManager/Common/Commands.ResourceManager.Common/Generated/Models/PageEnumerator.cs
@@ -56,7 +56,6 @@ namespace Microsoft.Azure.Internal.Subscriptions.Models
 
         public bool MoveNext()
         {
-            retry:
             if (_currentEnumerator.MoveNext())
             {
                 return true;
@@ -70,7 +69,7 @@ namespace Microsoft.Azure.Internal.Subscriptions.Models
             IPage<Subscription> tempPage = _client.Subscriptions.ListNext(_nextPageLink);
             _currentEnumerator = tempPage.GetEnumerator();
             _nextPageLink = tempPage.NextPageLink;
-            goto retry;
+            return this.MoveNext();
         }
 
         public void Reset()
