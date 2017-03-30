dffaba7f7fdcda6a56ac411124863448c442b48e
jobatzil <jobatzil@microsoft.com>
2017-03-30 11:51:37 -0700

    #WAF Readded ShouldProcess to new/set-WAF cmdlet for now to avoid breaking changes.

--- src/ResourceManager/Network/Commands.Network/ApplicationGateway/FirewallConfiguration/SetAzureApplicationGatewayWebApplicationFirewallConfigurationCommand.cs
+++ src/ResourceManager/Network/Commands.Network/ApplicationGateway/FirewallConfiguration/SetAzureApplicationGatewayWebApplicationFirewallConfigurationCommand.cs
@@ -17,7 +17,7 @@ using System.Management.Automation;
 
 namespace Microsoft.Azure.Commands.Network
 {
-    [Cmdlet(VerbsCommon.Set, "AzureRmApplicationGatewayWebApplicationFirewallConfiguration"),
+    [Cmdlet(VerbsCommon.Set, "AzureRmApplicationGatewayWebApplicationFirewallConfiguration", SupportsShouldProcess = true),
         OutputType(typeof(PSApplicationGateway))]
     public class SetAzureApplicationGatewayWebApplicationFirewallConfigurationCommand : AzureApplicationGatewayWebApplicationFirewallConfigurationBase
     {
@@ -29,20 +29,23 @@ namespace Microsoft.Azure.Commands.Network
 
         public override void ExecuteCmdlet()
         {
-            base.ExecuteCmdlet();
-
-            if(this.ApplicationGateway.WebApplicationFirewallConfiguration == null)
+            if (ShouldProcess("AzureApplicationGatewayWebApplicationFirewallConfiguration", Microsoft.Azure.Commands.Network.Properties.Resources.OverwritingResourceMessage))
             {
-                this.ApplicationGateway.WebApplicationFirewallConfiguration = new PSApplicationGatewayWebApplicationFirewallConfiguration();
+                base.ExecuteCmdlet();
+
+                if (this.ApplicationGateway.WebApplicationFirewallConfiguration == null)
+                {
+                    this.ApplicationGateway.WebApplicationFirewallConfiguration = new PSApplicationGatewayWebApplicationFirewallConfiguration();
+                }
+
+                this.ApplicationGateway.WebApplicationFirewallConfiguration.Enabled = this.Enabled;
+                this.ApplicationGateway.WebApplicationFirewallConfiguration.FirewallMode = this.FirewallMode;
+                this.ApplicationGateway.WebApplicationFirewallConfiguration.RuleSetType = this.RuleSetType;
+                this.ApplicationGateway.WebApplicationFirewallConfiguration.RuleSetVersion = this.RuleSetVersion;
+                this.ApplicationGateway.WebApplicationFirewallConfiguration.DisabledRuleGroups = this.DisabledRuleGroups;
+
+                WriteObject(this.ApplicationGateway);
             }
-
-            this.ApplicationGateway.WebApplicationFirewallConfiguration.Enabled = this.Enabled;
-            this.ApplicationGateway.WebApplicationFirewallConfiguration.FirewallMode = this.FirewallMode;
-            this.ApplicationGateway.WebApplicationFirewallConfiguration.RuleSetType = this.RuleSetType;
-            this.ApplicationGateway.WebApplicationFirewallConfiguration.RuleSetVersion = this.RuleSetVersion;
-            this.ApplicationGateway.WebApplicationFirewallConfiguration.DisabledRuleGroups = this.DisabledRuleGroups;
-
-            WriteObject(this.ApplicationGateway);
         }
     }
 }
