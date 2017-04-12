5f043f7e52b473602948a9fbc53d1ce551ea6883
Dragos Avadanei <dragosav>
2017-04-12 12:34:05 -0700

    adding support for backing up and restoring of KeyVault secrets
    revising KeyVault backup/restore cmdlets:
        adding support for pipelining
        allowing overwriting of dest file
        using session data store for file operations

--- src/ResourceManager/KeyVault/Commands.KeyVault/Properties/Resources.Designer.cs
+++ src/ResourceManager/KeyVault/Commands.KeyVault/Properties/Resources.Designer.cs
@@ -260,6 +260,24 @@ namespace Microsoft.Azure.Commands.KeyVault.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Overwrite File ?.
+        /// </summary>
+        internal static string FileOverwriteCaption {
+            get {
+                return ResourceManager.GetString("FileOverwriteCaption", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Overwrite existing file at &apos;{0}&apos; ?.
+        /// </summary>
+        internal static string FileOverwriteMessage {
+            get {
+                return ResourceManager.GetString("FileOverwriteMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to BYOK key can not be imported as software key.
         /// </summary>
         internal static string ImportByokAsSoftkeyError {
