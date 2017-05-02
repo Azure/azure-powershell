b9fb260e1d5ed319ca77a236b22a1e05965564f6
Yang Wang <luanshixia@gmail.com>
2017-05-02 16:51:00 -0700

    Reverted a temporary change

--- src/ResourceManager/Resources/Commands.ResourceManager/Cmdlets/SdkClient/ResourceManagerSdkClient.cs
+++ src/ResourceManager/Resources/Commands.ResourceManager/Cmdlets/SdkClient/ResourceManagerSdkClient.cs
@@ -97,7 +97,6 @@ namespace Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkClient
         public ResourceManagerSdkClient(
             IResourceManagementClient resourceManagementClient)
         {
-            resourceManagementClient.BaseUri = new Uri("https://centraluseuap.management.azure.com");
             this.ResourceManagementClient = resourceManagementClient;
         }
 
