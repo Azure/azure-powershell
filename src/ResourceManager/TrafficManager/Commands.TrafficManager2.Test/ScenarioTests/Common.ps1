d15ab75ae1ea2b358a22f9b94c01730b33e5089e
vladca <vladca@microsoft.com>
2017-05-23 18:29:33 +0100

    Re-record tests with cleanup enhancements

--- src/ResourceManager/TrafficManager/Commands.TrafficManager2.Test/ScenarioTests/Common.ps1
+++ src/ResourceManager/TrafficManager/Commands.TrafficManager2.Test/ScenarioTests/Common.ps1
@@ -101,7 +101,5 @@ Cleans the created resource groups
 #>
 function TestCleanup-RemoveResourceGroup($rgname)
 {
-    if ([Microsoft.Azure.Test.HttpRecorder.HttpMockServer]::Mode -ne [Microsoft.Azure.Test.HttpRecorder.HttpRecorderMode]::Playback) {
-        Remove-AzureRmResourceGroup -Name $rgname -Force
-    }
+    Remove-AzureRmResourceGroup -Name $rgname -Force
 }
