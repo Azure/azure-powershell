018cab676be16c021ddbd87779b6b27841788835
Evgeny Agafonchikov <evgeny.agafonchikov@akvelon.com>
2017-06-20 14:54:27 +0300

    Adding Virtual Network Usage cmdlet

---
+++ src/ResourceManager/Network/Commands.Network/Models/PSVirtualNetworkUsage.cs
@@ -0,0 +1,11 @@
+﻿namespace Microsoft.Azure.Commands.Network.Models
+{
+    public partial class PSVirtualNetworkUsage
+    {
+        public string Id;
+        public long CurrentValue;
+        public long Limit;
+        public string Unit;
+        public PSUsageName Name;
+    }
+}
