9ef90808e6d9baef080fe364c0f23aa68f7a9d60
Balu Kambala <baluk@microsoft.com>
2017-07-18 21:49:41 -0700

    Made changes to logic such that customer can terminate cmdlet while cmdlet trying to get more data

--- src/ResourceManager/Automation/Commands.Automation/Common/IAutomationClient.cs
+++ src/ResourceManager/Automation/Commands.Automation/Common/IAutomationClient.cs
@@ -45,9 +45,9 @@ namespace Microsoft.Azure.Commands.Automation.Common
 
         CompilationJob GetCompilationJob(string resourceGroupName, string automationAccountName, Guid id);
 
-        IEnumerable<CompilationJob> ListCompilationJobsByConfigurationName(string resourceGroupName, string automationAccountName, string configurationName, DateTimeOffset? startTime, DateTimeOffset? endTime, string jobStatus);
-
-        IEnumerable<CompilationJob> ListCompilationJobs(string resourceGroupName, string automationAccountName, DateTimeOffset? startTime, DateTimeOffset? endTime, string jobStatus);
+        IEnumerable<CompilationJob> ListCompilationJobsByConfigurationName(string resourceGroupName, string automationAccountName, string configurationName, DateTimeOffset? startTime, DateTimeOffset? endTime, string jobStatus, ref string nextLink);
+                                    
+        IEnumerable<CompilationJob> ListCompilationJobs(string resourceGroupName, string automationAccountName, DateTimeOffset? startTime, DateTimeOffset? endTime, string jobStatus, ref string nextLink);
 
         CompilationJob StartCompilationJob(string resourceGroupName, string automationAccountName, string configurationName, IDictionary parameters, IDictionary configurationData);
 
@@ -57,9 +57,9 @@ namespace Microsoft.Azure.Commands.Automation.Common
         #region NodeConfiguration
         NodeConfiguration GetNodeConfiguration(string resourceGroupName, string automationAccountName, string nodeConfigurationName, string rollupStatus);
 
-        IEnumerable<NodeConfiguration> ListNodeConfigurationsByConfigurationName(string resourceGroupName, string automationAccountName, string configurationName, string rollupStatus);
+        IEnumerable<NodeConfiguration> ListNodeConfigurationsByConfigurationName(string resourceGroupName, string automationAccountName, string configurationName, string rollupStatus, ref string nextLink);
 
-        IEnumerable<NodeConfiguration> ListNodeConfigurations(string resourceGroupName, string automationAccountName, string rollupStatus);
+        IEnumerable<NodeConfiguration> ListNodeConfigurations(string resourceGroupName, string automationAccountName, string rollupStatus, ref string nextLink);
 
         NodeConfiguration CreateNodeConfiguration(string resourceGroupName, string automationAccountName, string sourcePath, string nodeConfiguraionName, bool overWrite);
 
@@ -68,7 +68,7 @@ namespace Microsoft.Azure.Commands.Automation.Common
 
         #region Configurations
 
-        IEnumerable<DscConfiguration> ListDscConfigurations(string resourceGroupName, string automationAccountName);
+        IEnumerable<DscConfiguration> ListDscConfigurations(string resourceGroupName, string automationAccountName, ref string nextLink);
 
         DscConfiguration GetConfiguration(string resourceGroupName, string automationAccountName, string configurationName);
 
@@ -94,16 +94,19 @@ namespace Microsoft.Azure.Commands.Automation.Common
 
         DscNode GetDscNodeById(string resourceGroupName, string automationAccountName, Guid nodeId);
 
-        IEnumerable<DscNode> ListDscNodes(string resourceGroupName, string automationAccountName, string status);
+        IEnumerable<DscNode> ListDscNodes(string resourceGroupName, string automationAccountName, string status, ref string nextLink);
 
-        IEnumerable<DscNode> ListDscNodesByName(string resourceGroupName, string automationAccountName, string nodeName, string status);
-        IEnumerable<DscNode> ListDscNodesByNodeConfiguration(string resourceGroupName, string automationAccountName, string nodeConfigurationName, string status);
+        IEnumerable<DscNode> ListDscNodesByName(string resourceGroupName, string automationAccountName, string nodeName, string status, ref string nextLink);
+
+        IEnumerable<DscNode> ListDscNodesByNodeConfiguration(string resourceGroupName, string automationAccountName, string nodeConfigurationName, string status, ref string nextLink);
 
         IEnumerable<DscNode> ListDscNodesByConfiguration(
             string resourceGroupName,
             string automationAccountName,
             string configurationName,
-            string status);
+            string status,
+            ref string nextLink);
+
         DscNode SetDscNodeById(string resourceGroupName, string automationAccountName, Guid nodeId, string nodeConfigurationName);
 
         void DeleteDscNode(string resourceGroupName, string automationAccountName, Guid nodeId);
@@ -130,7 +133,7 @@ namespace Microsoft.Azure.Commands.Automation.Common
 
         DscNodeReport GetLatestDscNodeReport(string resourceGroupName, string automationAccountName, Guid nodeId);
 
-        IEnumerable<DscNodeReport> ListDscNodeReports(string resourceGroupName, string automationAccountName, Guid nodeId, DateTimeOffset? startTime, DateTimeOffset? endTime);
+        IEnumerable<DscNodeReport> ListDscNodeReports(string resourceGroupName, string automationAccountName, Guid nodeId, DateTimeOffset? startTime, DateTimeOffset? endTime, ref string nextLink);
 
         DscNodeReport GetDscNodeReportByReportId(string resourceGroupName, string automationAccountName, Guid nodeId, Guid reportId);
 
