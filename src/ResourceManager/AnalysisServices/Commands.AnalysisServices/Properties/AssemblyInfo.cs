0238ae35a14fe5bb718ef2c07b6c2e7dc78e0c0b
Tai-Yi Wu <taiwu@microsoft.com>
2017-07-21 18:25:13 -0700

    bump major version

--- src/ResourceManager/AnalysisServices/Commands.AnalysisServices/Properties/AssemblyInfo.cs
+++ src/ResourceManager/AnalysisServices/Commands.AnalysisServices/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 
 
-[assembly: AssemblyVersion("0.5.0")]
-[assembly: AssemblyFileVersion("0.5.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
