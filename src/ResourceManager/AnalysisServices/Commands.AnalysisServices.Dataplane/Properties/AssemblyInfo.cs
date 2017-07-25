34d3eea9ade6845e7e5264d1c9c39bdb8975de6f
Tai-Yi Wu <taiwu@microsoft.com>
2017-07-25 14:18:46 -0700

    Revert version bump and add exception for new operation

--- src/ResourceManager/AnalysisServices/Commands.AnalysisServices.Dataplane/Properties/AssemblyInfo.cs
+++ src/ResourceManager/AnalysisServices/Commands.AnalysisServices.Dataplane/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("0.4.2")]
+[assembly: AssemblyFileVersion("0.4.2")]
