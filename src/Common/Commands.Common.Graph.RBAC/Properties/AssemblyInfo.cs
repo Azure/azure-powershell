7768d25d7c7442c3b4d30638f788485732961fe1
cormacpayne <corm@microsoft.com>
2017-07-28 13:54:49 -0700

    Resolve review feedback

--- src/Common/Commands.Common.Graph.RBAC/Properties/AssemblyInfo.cs
+++ src/Common/Commands.Common.Graph.RBAC/Properties/AssemblyInfo.cs
@@ -1,4 +1,18 @@
-﻿using System.Reflection;
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,8 +20,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Commands.Common.Graph.RBAC")]
-[assembly: AssemblyDescription("Graph.RBAC library generated from AutoRest - API Version 1.6")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyDescription("Graph.RBAC library generated from AutoRest")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
 [assembly: AssemblyProduct("Microsoft Azure PowerShell")]
 [assembly: AssemblyCopyright("Copyright © Microsoft")]
 
