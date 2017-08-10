17c52fd46ef8f9dd7956dbbf817770a74c3483ab
Michael Falk <mifal@microsoft.com>
2017-08-09 17:21:38 -0700

    Add new sync response data contract

---
+++ src/ResourceManager/AnalysisServices/Commands.AnalysisServices.Dataplane/Models/ScaleOutServerDatabaseSyncResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Microsoft.Azure.Commands.AnalysisServices.Dataplane.Models
+{
+    public sealed class ScaleOutServerDatabaseSyncResult
+    {
+        [DataMember]
+        public string OperationId { get; set; }
+
+        [DataMember(Name = "database")]
+        public string Database { get; set; }
+
+        [DataMember(Name = "UpdatedAt")]
+        public DateTime UpdatedAt { get; set; }
+
+        [DataMember(Name = "StartedAt")]
+        public DateTime StartedAt { get; set; }
+
+        [DataMember(Name = "syncstate")]
+        public DatabaseSyncState SyncState { get; set; }
+
+        [DataMember(Name = "details")]
+        public string Details { get; set; }
+    }
+
+    public enum DatabaseSyncState
+    {
+        Succeeded,
+        Failed
+    }
+}
