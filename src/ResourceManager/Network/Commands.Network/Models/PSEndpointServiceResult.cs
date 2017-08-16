d9518e9e313c6486dbfab57eae79d1fbd22fd0b8
Evgeny Agafonchikov <evgeny.agafonchikov@akvelon.com>
2017-08-16 14:38:34 +0300

    Added service endpoints cmdlets

---
+++ src/ResourceManager/Network/Commands.Network/Models/PSEndpointServiceResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Microsoft.Azure.Commands.Network.Models
+{
+    public class PSEndpointServiceResult
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public string Type { get; set; }
+    }
+}
