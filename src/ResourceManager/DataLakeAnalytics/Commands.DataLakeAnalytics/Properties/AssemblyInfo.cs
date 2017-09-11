eb3580151b1fe5f76de9f217086db9256a71d17c
markcowl <markcowl@microsoft.com>
2017-09-11 09:45:20 -0700

    Version changes for AzureStack

--- src/ResourceManager/DataLakeAnalytics/Commands.DataLakeAnalytics/Properties/AssemblyInfo.cs
+++ src/ResourceManager/DataLakeAnalytics/Commands.DataLakeAnalytics/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 
 
-[assembly: AssemblyVersion("3.3.1")]
-[assembly: AssemblyFileVersion("3.3.1")]
+[assembly: AssemblyVersion("3.3.2")]
+[assembly: AssemblyFileVersion("3.3.2")]
