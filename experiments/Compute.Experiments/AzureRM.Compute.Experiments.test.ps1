6a2a9cc2537cfce35195e2af7a5260ee14732102
markcowl <markcowl@microsoft.com>
2017-09-15 18:55:46 -0700

    Add simple validation test

---
+++ experiments/Compute.Experiments/AzureRM.Compute.Experiments.test.ps1
@@ -0,0 +1,9 @@
+
+$clientSecret = ConvertTo-SecureString ([Guid]::NewGuid().ToString().Substring(1,10)) -AsPlainText -Force
+$pscredentials = New-Object System.Management.Automation.PSCredential("vmadmin", $clientSecret)
+New-AzVm -Name MyVM -Credential $pscredentials -WhatIf
+$vm = New-AzVm -Name myVm1234 -Credential $pscredentials
+$vm
+
+#Cleanup
+Remove-AzureRmResourceGroup -ResourceId $vm.ResourceGroupId
