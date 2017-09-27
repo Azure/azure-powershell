45db22539fef7ce779284a14fb778658bb606374
Steven Hutchings <sthutchi@microsoft.com>
2017-09-27 13:27:03 -0700

    Added machine learning compute.

---
+++ src/ResourceManager/MachineLearningCompute/Commands.MachineLearningCompute/Cmdlets/MachineLearningComputeCmdletBase.cs
@@ -0,0 +1,82 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Commands.Common.Authentication;
+using Microsoft.Azure.Commands.Common.Authentication.Models;
+using Microsoft.Azure.Commands.ResourceManager.Common;
+using Microsoft.Azure.Management.MachineLearningCompute;
+using Microsoft.Rest.Azure;
+
+namespace Microsoft.Azure.Commands.MachineLearningCompute.Cmdlets
+{
+    public abstract class MachineLearningComputeCmdletBase : AzureRMCmdlet
+    {
+        public const string CmdletSuffix = "AzureRmMlOpCluster";
+
+        public const string ResourceGroupParameterHelpMessage = "The name of the resource group for the operationalization cluster.";
+        public const string NameParameterHelpMessage = "The name of the operationalization cluster.";
+        public const string ClusterObjectParameterHelpMessage = "The operationalization cluster object.";
+        public const string ResourceIdParameterHelpMessage = "The Azure resource id for the operationalization cluster.";
+        public const string ForceParameterHelpMessage = "Do not ask for confirmation.";
+        public const string ClusterParameterHelpMessage = "The operationalization cluster properties.";
+        public const string TagParameterHelpMessage = "The tags for the resource.";
+        public const string LocationParameterHelpMessage = "The operationalization cluster's location.";
+        public const string DescriptionParameterHelpMessage = "The operationalization cluster's description.";
+        public const string ClusterTypeParameterHelpMessage = "The operationalization cluster type.";
+        public const string OrchestratorTypeParameterHelpMessage = "The ACS cluster's orchestrator type.";
+        public const string ClientIdParameterHelpMessage = "The Kubernetes orchestrator service principal id.";
+        public const string SecretParameterHelpMessage = "The Kubernetes orchestrator service principal secret.";
+        public const string MasterCountParameterHelpMessage = "The number of master nodes in the ACS cluster.";
+        public const string AgentCountParameterHelpMessage = "The number of agent nodes in the ACS cluster.";
+        public const string AgentVmSizeParameterHelpMessage = "The VM size of the agent nodes in the ACS cluster.";
+        public const string StorageAccountParameterHelpMessage = "The URI to the storage account to use instead of creating one.";
+        public const string AzureContainerRegistryParameterHelpMessage = "The URI to the azure container registry to use instead of creating one.";
+        public const string ApplicationInsightsParameterHelpMessage = "The URI to the application insights to use instead of creating one.";
+        public const string ETagParameterHelpMessage = "The configuration ETag for updates.";
+        public const string GlobalServiceConfigurationAdditionalPropertiesHelpMessage = "Additional properties for the global service configuration.";
+        public const string SslStatusParameterHelpMessage = "SSL status. Possible values are 'Enabled' and 'Disabled'.";
+        public const string SslCertificateParameterHelpMessage = "The SSL certificate data in PEM format encoded as base64 string.";
+        public const string SslKeyParameterHelpMessage = "The SSL key data in PEM format encoded as base64 string.";
+        public const string SslCNameParameterHelpMessage = "The CName for the SSL certificate.";
+
+        public const string ClusterInputObjectAlias = "Cluster";
+
+        private IMachineLearningComputeManagementClient machineLearningComputeManagementClient;
+
+        public IMachineLearningComputeManagementClient MachineLearningComputeManagementClient
+        {
+            get
+            {
+                return machineLearningComputeManagementClient ??
+                    (machineLearningComputeManagementClient =
+                        AzureSession.Instance.ClientFactory.CreateArmClient<MachineLearningComputeManagementClient>(
+                            DefaultProfile.DefaultContext, Common.Authentication.Abstractions.AzureEnvironment.Endpoint.ResourceManager));
+            }
+        }
+
+        protected void HandleNestedExceptionMessages(CloudException e)
+        {
+            var exceptionDetails = e.Message + ".";
+
+            foreach (var err in e.Body.Details)
+            {
+                exceptionDetails += " " + err.Message + ".";
+            }
+
+            var newException = new CloudException(exceptionDetails);
+      
+            throw newException;
+        }
+    }
+}
---
+++ src/ResourceManager/MachineLearningCompute/Commands.MachineLearningCompute/help/Test-AzureRmMlOpClusterSystemServicesUpdateAvailability.md
@@ -0,0 +1,126 @@
+---
+external help file: Microsoft.Azure.Commands.MachineLearningCompute.dll-Help.xml
+Module Name: AzureRM.MachineLearningCompute
+online version: 
+schema: 2.0.0
+---
+
+# Test-AzureRmMlOpClusterSystemServicesUpdateAvailability
+
+## SYNOPSIS
+Checks if there are updates available for the system services associated with an operationalization cluster.
+
+## SYNTAX
+
+### Test for update availability from cmdlet input parameters.
+```
+Test-AzureRmMlOpClusterSystemServicesUpdateAvailability -ResourceGroupName <String> -Name <String>
+```
+
+### Test for update availability from an OperationalizationCluster instance definition.
+```
+Test-AzureRmMlOpClusterSystemServicesUpdateAvailability -InputObject <PSOperationalizationCluster>
+```
+
+### Test for update availability from an Azure resouce id.
+```
+Test-AzureRmMlOpClusterSystemServicesUpdateAvailability -ResourceId <String>
+```
+
+## DESCRIPTION
+System services receive updates independently from the operationalization cluster. Using this cmdlet will let the user know if Invoke-AzureRmMlOpClusterSystemServicesUpdate.
+
+## EXAMPLES
+
+### Example 1
+```
+PS C:\> Test-AzureRmMlOpClusterSystemServicesUpdateAvailability -ResourceGroupName my-group -Name my-cluster
+```
+
+### Example 2
+```
+PS C:\> Get-AzureRmMlOpCluster | Test-AzureRmMlOpClusterSystemServicesUpdateAvailability
+```
+
+### Example 3
+```
+PS C:\> Find-AzureRmResource -ResourceType Microsoft.MachineLearningCompute/operationalizationClusters | Test-AzureRmMlOpClusterSystemServicesUpdateAvailability
+```
+
+## PARAMETERS
+
+### -InputObject
+The operationalization cluster object.
+
+```yaml
+Type: PSOperationalizationCluster
+Parameter Sets: Test for update availability from an OperationalizationCluster instance definition.
+Aliases: Cluster
+
+Required: True
+Position: Named
+Default value: None
+Accept pipeline input: True (ByValue)
+Accept wildcard characters: False
+```
+
+### -Name
+The name of the operationalization cluster.
+
+```yaml
+Type: String
+Parameter Sets: Test for update availability from cmdlet input parameters.
+Aliases: 
+
+Required: True
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -ResourceGroupName
+The name of the resource group for the operationalization cluster.
+
+```yaml
+Type: String
+Parameter Sets: Test for update availability from cmdlet input parameters.
+Aliases: 
+
+Required: True
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -ResourceId
+The Azure resource id for the operationalization cluster.
+
+```yaml
+Type: String
+Parameter Sets: Test for update availability from an Azure resouce id.
+Aliases: 
+
+Required: True
+Position: Named
+Default value: None
+Accept pipeline input: True (ByPropertyName)
+Accept wildcard characters: False
+```
+
+## INPUTS
+
+### Microsoft.Azure.Commands.MachineLearningCompute.Models.PSOperationalizationCluster
+### System.String
+
+
+## OUTPUTS
+
+### Microsoft.Azure.Management.MachineLearningCompute.Models.CheckSystemServicesUpdatesAvailableResponse
+
+
+## NOTES
+
+## RELATED LINKS
+
