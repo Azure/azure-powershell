01a668f63575c2f64616ed7c96b83f61d493a2df
maddieclayton <maddieclayton1@gmail.com>
2017-09-28 14:20:36 -0700

    Initial work on Scenario Tests

---
+++ src/ResourceManager/Profile/Commands.Profile.Test/ScenarioTests/LocationCompleterTests.cs
@@ -0,0 +1,47 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Commands.ResourceManager.Common;
+using Microsoft.Azure.Commands.ResourceManager.Common.Location;
+using Microsoft.Azure.Commands.Resources.Test.ScenarioTests;
+using Microsoft.Azure.Commands.ScenarioTest;
+using Microsoft.Azure.ServiceManagemenet.Common.Models;
+using Microsoft.WindowsAzure.Commands.ScenarioTest;
+using System.Management.Automation;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.Azure.Commands.Profile.Test.ScenarioTests
+{
+    public class LocationCompleterTests
+    {
+        private XunitTracingInterceptor xunitLogger;
+
+        public LocationCompleterTests(ITestOutputHelper output)
+        {
+            TestExecutionHelpers.SetUpSessionAndProfile();
+            ResourceManagerProfileProvider.InitializeResourceManagerProfile(true);
+
+            xunitLogger = new XunitTracingInterceptor(output);
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void LocationCompleterOutputCorrect()
+        {
+            ScriptBlock scriptBlock = LocationCompleterAttribute.CreateScriptBlock(new string[] { "Microsoft.Batch/operation" });
+            ProfileController.NewInstance.RunPsTest(xunitLogger, "72f988bf-86f1-41af-91ab-2d7cd011db47", string.Format("Test-LocationCompleter -scriptBlock {0}", scriptBlock));
+        }
+    }
+}
