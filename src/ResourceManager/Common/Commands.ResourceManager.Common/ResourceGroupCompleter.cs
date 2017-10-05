6b6c45fab46519f5fb4840a9d1473a8dd62683c2
maddieclayton <maddieclayton1@gmail.com>
2017-10-05 10:06:43 -0700

    add ResourceGroup completer and tests

---
+++ src/ResourceManager/Common/Commands.ResourceManager.Common/ResourceGroupCompleter.cs
@@ -0,0 +1,134 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+namespace Microsoft.Azure.Commands.ResourceManager.Common.TabCompletion
+{
+    using Commands.Common.Authentication;
+    using Commands.Common.Authentication.Abstractions;
+    using Management.Internal.Resources;
+    using Management.Internal.Resources.Models;
+    using Properties;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Management.Automation;
+
+    /// <summary>
+    /// This attribute will allow the user to autocomplete the -ResourceGroup parameter of a cmdlet with valid resource groups
+    /// </summary>
+    public class ResourceGroupCompleterAttribute : ArgumentCompleterAttribute
+    {
+        private static IList<String> _resourceGroupNames = new List<string>();
+        private static readonly object _lock = new object();
+
+        protected static IList<String> ResourceGroupNames
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    _resourceGroupNames = new List<string>();
+                    IAzureContext context = AzureRmProfileProvider.Instance.Profile.DefaultContext;
+                    try
+                    {
+                        var instance = AzureSession.Instance;
+                        var client = instance.ClientFactory.CreateCustomArmClient<ResourceManagementClient>(
+                            context.Environment.GetEndpointAsUri(AzureEnvironment.Endpoint.ResourceManager),
+                            instance.AuthenticationFactory.GetServiceClientCredentials(context, AzureEnvironment.Endpoint.ResourceManager),
+                            instance.ClientFactory.GetCustomHandlers());
+                        //var client = AzureSession.Instance.ClientFactory.CreateArmClient<ResourceManagementClient>(context, AzureEnvironment.Endpoint.ResourceManager);
+                        client.SubscriptionId = context.Subscription.Id;
+                        var resourceGroups = client.ResourceGroups.ListAsync();
+                        if (resourceGroups.Wait(TimeSpan.FromSeconds(5)))
+                        {
+                            if (resourceGroups.Result != null)
+                            {
+                                var resourceGroupList = resourceGroups.Result.ToList();
+                                foreach (ResourceGroup resourceGroup in resourceGroupList)
+                                {
+                                    _resourceGroupNames.Add(resourceGroup.Name);
+                                }
+                            }
+
+                            else
+                            {
+#if DEBUG
+                                throw new Exception("Result from client.ResourceGroups is null");
+#endif
+                            }
+                        }
+
+                        else
+                        {
+#if DEBUG
+                            throw new Exception("client.ResourceGroups call timed out");
+#endif
+                        }
+                    }
+
+                    catch (Exception ex)
+                    {
+#if DEBUG
+                        throw ex;
+#endif
+                    }
+                }
+
+                return _resourceGroupNames;
+            }
+        }
+
+        /// <summary>
+        /// This class will provide a list of resource groups that are available to the user (with default resource group first if it exists). This will then be available to the user to tab through.
+        /// Example: [Parameter(ParameterSetName = ListByNameInTenantParameterSet, ValueFromPipelineByPropertyName = true, Mandatory = false), ResourceGroupCompleter()]
+        /// </summary>
+        /// <param name="resourceTypes"></param>
+        public ResourceGroupCompleterAttribute() : base(CreateScriptBlock())
+        {
+        }
+
+        public static string[] GetResourceGroups()
+        {
+            IAzureContext context = AzureRmProfileProvider.Instance.Profile.DefaultContext;
+            var resourceGroupNamesCopy = ResourceGroupNames;
+            if (context.ExtendedProperties.ContainsKey(Resources.DefaultResourceGroupKey))
+            {
+                return GetResourceGroups(resourceGroupNamesCopy, context.ExtendedProperties[Resources.DefaultResourceGroupKey]);
+            }
+            return GetResourceGroups(resourceGroupNamesCopy, null);
+        }
+
+        public static string[] GetResourceGroups(IList<string> resourceGroupNames, string defaultResourceGroup)
+        {
+            if (defaultResourceGroup != null)
+            {
+                if (resourceGroupNames.Contains(defaultResourceGroup))
+                {
+                    resourceGroupNames.Remove(defaultResourceGroup);
+                    resourceGroupNames.Insert(0, defaultResourceGroup);
+                }
+            }
+            return resourceGroupNames.ToArray();
+        }
+
+        private static ScriptBlock CreateScriptBlock()
+        {
+            string script = "param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)\n" +
+                "$locations = [Microsoft.Azure.Commands.ResourceManager.Common.TabCompletion.ResourceGroupCompleterAttribute]::GetResourceGroups()\n" +
+                "$locations | Where-Object { $_ -Like \"$wordToComplete*\" } | Select-Object -First 50 | ForEach-Object { [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_) }";
+            ScriptBlock scriptBlock = ScriptBlock.Create(script);
+            return scriptBlock;
+        }
+    }
+}
---
+++ src/ResourceManager/Profile/Commands.Profile.Test/ResourcGroupCompleterUnitTests.cs
@@ -0,0 +1,89 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Commands.ResourceManager.Common.TabCompletion;
+using Microsoft.WindowsAzure.Commands.ScenarioTest;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Microsoft.Azure.Commands.Profile.Test
+{
+    public class ResourceGroupCompleterUnitTests
+    {
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void ReturnsEmptyListWhenNoResourceGroupsExist()
+        {
+            IList<string> resourceGroupsReturned = new List<string>();
+
+            Assert.Equal(ResourceGroupCompleterAttribute.GetResourceGroups(resourceGroupsReturned, null), new string[] { });
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void OneResourceGroupNoDefault()
+        {
+            IList<string> resourceGroupsReturned = new List<string>();
+            resourceGroupsReturned.Add("test1");
+
+            Assert.Equal(ResourceGroupCompleterAttribute.GetResourceGroups(resourceGroupsReturned, null), new string[] { "test1" });
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void OneResourceGroupWithDefault()
+        {
+            IList<string> resourceGroupsReturned = new List<string>();
+            resourceGroupsReturned.Add("test1");
+
+            Assert.Equal(ResourceGroupCompleterAttribute.GetResourceGroups(resourceGroupsReturned, "test1"), new string[] { "test1" });
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void OneResourceGroupWithInvalidDefault()
+        {
+            IList<string> resourceGroupsReturned = new List<string>();
+            resourceGroupsReturned.Add("test1");
+
+            Assert.Equal(ResourceGroupCompleterAttribute.GetResourceGroups(resourceGroupsReturned, "invalidDefault"), new string[] { "test1" });
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void MultipleResourceGroupsNoDefault()
+        {
+            IList<string> resourceGroupsReturned = new List<string>();
+            resourceGroupsReturned.Add("test1");
+            resourceGroupsReturned.Add("test2");
+            resourceGroupsReturned.Add("test3");
+            resourceGroupsReturned.Add("test4");
+
+            Assert.Equal(ResourceGroupCompleterAttribute.GetResourceGroups(resourceGroupsReturned, null), new string[] { "test1", "test2", "test3", "test4" });
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void MultipleResourceGroupsWithDefault()
+        {
+            IList<string> resourceGroupsReturned = new List<string>();
+            resourceGroupsReturned.Add("test1");
+            resourceGroupsReturned.Add("test2");
+            resourceGroupsReturned.Add("test3");
+            resourceGroupsReturned.Add("test4");
+
+            Assert.Equal(ResourceGroupCompleterAttribute.GetResourceGroups(resourceGroupsReturned, "test3"), new string[] { "test3", "test1", "test2", "test4" });
+        }
+    }
+}
