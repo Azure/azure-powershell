33f09231061424da74f49a8b08a8cb522618c870
markcowl <markcowl@microsoft.com>
2017-10-08 21:20:17 -0700

    Update versions for release 4.4.1

--- src/ResourceManager/RecoveryServices.SiteRecovery/Commands.RecoveryServices.SiteRecovery.Test/Properties/AssemblyInfo.cs
+++ src/ResourceManager/RecoveryServices.SiteRecovery/Commands.RecoveryServices.SiteRecovery.Test/Properties/AssemblyInfo.cs
@@ -43,6 +43,6 @@ using Xunit;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.1.5")]
-[assembly: AssemblyFileVersion("0.1.5")]
+[assembly: AssemblyVersion("0.1.6")]
+[assembly: AssemblyFileVersion("0.1.6")]
 [assembly: CollectionBehavior(DisableTestParallelization = true)]
