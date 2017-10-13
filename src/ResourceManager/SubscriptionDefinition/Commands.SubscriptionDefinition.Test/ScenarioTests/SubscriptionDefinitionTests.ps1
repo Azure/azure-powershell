69ba8f7c25b8b28019c1c87d7ba34bf09caa34fa
wilcob@microsoft.com <wilcob@microsoft.com>
2017-10-13 14:57:52 -0700

    Initial check-in for SubscriptionDefinition cmdlet support.

---
+++ src/ResourceManager/SubscriptionDefinition/Commands.SubscriptionDefinition.Test/ScenarioTests/SubscriptionDefinitionTests.ps1
@@ -0,0 +1,81 @@
+﻿# ----------------------------------------------------------------------------------
+#
+# Copyright Microsoft Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------------
+
+<#
+.SYNOPSIS
+List subscription definitions
+#>
+function Test-ListSubscriptionDefinitions
+{
+    $definitions = Get-AzureRmSubscriptionDefinition -ManagementGroupId 'ac03d01c-232b-4d97-b869-c979f8669be2'
+
+    Assert-AreEqual 3 $definitions.Count
+	Foreach($def in $definitions)
+	{
+		Assert-NotNull $def.Name
+		Assert-NotNull $def.GroupId
+		Assert-NotNull $def.SubscriptionId
+	}
+}
+
+<#
+.SYNOPSIS
+Get subscription definition in group scope
+#>
+function Test-GetSubscriptionDefinitionInGroupScope
+{
+    $definition = Get-AzureRmSubscriptionDefinition -ManagementGroupId 'ac03d01c-232b-4d97-b869-c979f8669be2' -Name 'mySubDef'
+
+    Assert-NotNull $definition.Name
+	Assert-NotNull $definition.GroupId
+	Assert-NotNull $definition.SubscriptionId
+}
+
+<#
+.SYNOPSIS
+Get subscription definition in subscription scope
+#>
+function Test-GetSubscriptionDefinitionInGroupScope
+{
+    $definition = Get-AzureRmSubscriptionDefinition
+
+    Assert-NotNull $definition.Name
+	Assert-NotNull $definition.GroupId
+	Assert-NotNull $definition.SubscriptionId
+
+    $definition2 = Get-AzureRmSubscription | Get-AzureRmSubscriptionDefinition
+
+    Assert-AreEqual $definition.Name, $definition2.Name
+	Assert-AreEqual $definition.GroupId, $definition2.GroupId
+	Assert-AreEqual $definition.SubscriptionId, $definition2.SubscriptionId
+}
+
+<#
+.SYNOPSIS
+Create subscription definition
+#>
+function Test-NewSubscriptionDefinition
+{
+    $definitions = Get-AzureRmSubscriptionDefinition -ManagementGroup 'ac03d01c-232b-4d97-b869-c979f8669be2'
+    $previousDefinitionCount = $definitions.Count
+
+    $definition = Net-AzureRmSubscriptionDefinition -ManagementGroupId 'ac03d01c-232b-4d97-b869-c979f8669be2' -Name 'myNewSubDef' -OfferType ''
+
+    Assert-NotNull $definition.Name
+	Assert-NotNull $definition.GroupId
+	Assert-NotNull $definition.SubscriptionId
+
+    $definitions = Get-AzureRmSubscriptionDefinition -ManagementGroup 'ac03d01c-232b-4d97-b869-c979f8669be2'
+    Assert-AreEqual $definitions.Count, ($previousDefinitionCount + 1)
+}
---
+++ src/ResourceManager/SubscriptionDefinition/Commands.SubscriptionDefinition/Common/AzureSubscriptionDefinitionCmdletBase.cs
@@ -0,0 +1,57 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Commands.Common.Authentication;
+using Microsoft.Azure.Commands.Common.Authentication.Abstractions;
+using Microsoft.Azure.Commands.ResourceManager.Common;
+using Microsoft.Azure.Commands.SubscriptionDefinition.Models;
+using Microsoft.Azure.Management.ResourceManager;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.Azure.Commands.SubscriptionDefinition.Common
+{
+    /// <summary>
+    /// Base class of Azure Subscription Definition Cmdlet.
+    /// </summary>
+    public abstract class AzureSubscriptionDefinitionCmdletBase : AzureRMCmdlet
+    {
+        private ISubscriptionDefinitionClient _subscriptionDefinitionClient;
+
+        /// <summary>
+        /// Gets or sets the Consumption management client.
+        /// </summary>
+        public ISubscriptionDefinitionClient SubscriptionDefinitionClient
+        {
+            get
+            {
+                return _subscriptionDefinitionClient ??
+                       (_subscriptionDefinitionClient =
+                           AzureSession.Instance.ClientFactory.CreateArmClient<SubscriptionDefinitionClient>(DefaultContext,
+                               AzureEnvironment.Endpoint.ResourceManager));
+            }
+            set { _subscriptionDefinitionClient = value; }
+        }
+
+        protected void WriteSubscriptionDefinitionObject(Microsoft.Azure.Management.ResourceManager.Models.SubscriptionDefinition subscriptionDefinition)
+        {
+            this.WriteObject(new PSSubscriptionDefinition(subscriptionDefinition));
+        }
+
+        protected void WriteSubscriptionDefinitionObjects(IEnumerable<Microsoft.Azure.Management.ResourceManager.Models.SubscriptionDefinition> subscriptionDefinitions)
+        {
+            this.WriteObject(subscriptionDefinitions.Select(x => new PSSubscriptionDefinition(x)), enumerateCollection: true);
+        }
+    }
+}
