10b1cfad80cf563624233b1202b7a96df905e4af
Cormac McCarthy <corm@microsoft.com>
2017-11-01 13:00:00 -0700

    Update WebsitesClient.cs

--- src/ResourceManager/Websites/Commands.Websites/Utilities/WebsitesClient.cs
+++ src/ResourceManager/Websites/Commands.Websites/Utilities/WebsitesClient.cs
@@ -264,7 +264,7 @@ namespace Microsoft.Azure.Commands.WebApps.Utilities
                 WrappedWebsitesClient.WebApps().ListSitePublishingProfileXml(resourceGroupName, webSiteName, options));
             var doc = XDocument.Load(publishingXml, LoadOptions.None);
             if (outputFile != null)
-	    {
+            {
                 doc.Save(outputFile, SaveOptions.OmitDuplicateNamespaces);
             }
 	    return doc.ToString();
