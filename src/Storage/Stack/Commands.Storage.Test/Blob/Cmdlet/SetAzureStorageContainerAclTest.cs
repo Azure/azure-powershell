23f68b2efbdbcd8a2b1f14f8ddebe66f39dfc492
Bala Ganapathy <bganapa@microsoft.com>
2017-11-19 17:04:39 -0800

    Azure.Storage module changes for consuming IStorageContext

--- src/Storage/Stack/Commands.Storage.Test/Blob/Cmdlet/SetAzureStorageContainerAclTest.cs
+++ src/Storage/Stack/Commands.Storage.Test/Blob/Cmdlet/SetAzureStorageContainerAclTest.cs
@@ -15,9 +15,9 @@
 using System;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.WindowsAzure.Commands.Common.Storage.ResourceModel;
 using Microsoft.WindowsAzure.Commands.Storage.Cmdlet;
 using Microsoft.WindowsAzure.Commands.Storage.Common;
-using Microsoft.WindowsAzure.Commands.Storage.Model.ResourceModel;
 using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace Microsoft.WindowsAzure.Commands.Storage.Test.Blob
