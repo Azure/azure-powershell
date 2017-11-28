794284a797df5ff41393faffd2eba803196bd103
maddieclayton <maddieclayton1@gmail.com>
2017-11-28 12:02:31 -0800

    Add LocationCompleter to Network cmdlets

--- src/ResourceManager/Network/Commands.Network/RouteFilter/NewAzureRouteFilterCommand.cs
+++ src/ResourceManager/Network/Commands.Network/RouteFilter/NewAzureRouteFilterCommand.cs
@@ -50,6 +50,7 @@ namespace Microsoft.Azure.Commands.Network
          Mandatory = true,
          ValueFromPipelineByPropertyName = true,
          HelpMessage = "location.")]
+        [LocationCompleter("Microsoft.Network/routeFilters")]
         [ValidateNotNullOrEmpty]
         public string Location { get; set; }
 
