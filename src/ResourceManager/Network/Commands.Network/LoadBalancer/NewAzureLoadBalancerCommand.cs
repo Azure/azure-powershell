321a7d76a0bdcda3c1132824d833d8a388ea1e40
maddieclayton <maddieclayton1@gmail.com>
2017-11-28 12:02:31 -0800

    Add LocationCompleter to Network cmdlets

--- src/ResourceManager/Network/Commands.Network/LoadBalancer/NewAzureLoadBalancerCommand.cs
+++ src/ResourceManager/Network/Commands.Network/LoadBalancer/NewAzureLoadBalancerCommand.cs
@@ -48,6 +48,7 @@ namespace Microsoft.Azure.Commands.Network
          Mandatory = true,
          ValueFromPipelineByPropertyName = true,
          HelpMessage = "location.")]
+        [LocationCompleter("Microsoft.Network/loadBalancers")]
         [ValidateNotNullOrEmpty]
         public virtual string Location { get; set; }
 
