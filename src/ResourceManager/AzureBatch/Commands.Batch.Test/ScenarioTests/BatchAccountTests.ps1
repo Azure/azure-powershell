Azure/azure-powershell

commit 4a128bca2eb9fa3ecbfba9f80af68185d0219bb9

Update ARM and Storage projects with new references (except Resources)

src/ResourceManager/AzureBatch/Commands.Batch/Models/BatchClient.BatchAccounts.cs
@@ -15,7 +15,7 @@
 using Microsoft.Azure.Commands.Batch.Properties;
 using Microsoft.Azure.Management.Batch;
 using Microsoft.Azure.Management.Batch.Models;
-using Microsoft.Azure.Management.Resources;
+using Microsoft.Azure.Management.Internal.Resources;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -25,6 +25,7 @@ using Microsoft.Azure.Batch;
 using Microsoft.Azure.Commands.ResourceManager.Common.Tags;
 using Microsoft.Rest.Azure;
 using CloudException = Hyak.Common.CloudException;
+using Microsoft.Azure.Management.Internal.Resources.Models;
 
 namespace Microsoft.Azure.Commands.Batch.Models
 {
@@ -274,14 +275,12 @@ namespace Microsoft.Azure.Commands.Batch.Models
 
         internal string GetGroupForAccountNoThrow(string accountName)
         {
-            var response = ResourceManagementClient.Resources.List(new Management.Resources.Models.ResourceListParameters()
-            {
-                ResourceType = accountSearch
-            });
+            var response = ResourceManagementClient.Resources.List(new Rest.Azure.OData.ODataQuery<GenericResourceFilter>(
+                r => r.ResourceType == accountName));
 
             string groupName = null;
 
-            foreach (var res in response.Resources)
+            foreach (var res in response)
             {
                 if (res.Name == accountName)
                 {

commit 8faa4051c9d52d26f20102e71ca42b39f4ec3996

Fix Batch account operations bug
  - Operations without a resource group should work by autodetecting the
    resource group.
  - This fixes a bug introduced by Cormac in commit: 4a128bca.

src/ResourceManager/AzureBatch/Commands.Batch.Test/ScenarioTests/BatchAccountTests.ps1
@@ -63,7 +63,12 @@ function Test-BatchAccountEndToEnd
         Assert-AreEqual 1 $updatedAccount.Tags.Count
         Assert-AreEqual $newTagValue $updatedAccount.Tags[$newTagName]
 
-        # Get the account keys
+        # Get the account keys (without resource group)
+        $accountWithKeys = Get-AzureRmBatchAccountKeys -Name $accountName
+        Assert-NotNull $accountWithKeys.PrimaryAccountKey
+        Assert-NotNull $accountWithKeys.SecondaryAccountKey
+
+        # Get the account keys (with resource group)
         $accountWithKeys = Get-AzureRmBatchAccountKeys -Name $accountName -ResourceGroupName $resourceGroup
         Assert-NotNull $accountWithKeys.PrimaryAccountKey
         Assert-NotNull $accountWithKeys.SecondaryAccountKey
@@ -80,8 +85,8 @@ function Test-BatchAccountEndToEnd
         {
             # Delete the account
             Remove-AzureRmBatchAccount -Name $accountName -ResourceGroupName $resourceGroup -Force
-            $errorMessage = "The Resource 'Microsoft.Batch/batchAccounts/$accountName' under resource group '$resourceGroup' was not found."
-            Assert-Throws { Get-AzureRmBatchAccount -Name $accountName -ResourceGroupName $resourceGroup } $errorMessage
+            $errorMessage = "The specified account does not exist."
+            Assert-ThrowsContains { Get-AzureRmBatchAccount -Name $accountName -ResourceGroupName $resourceGroup } $errorMessage
         }
         finally
         {

src/ResourceManager/AzureBatch/Commands.Batch.Test/ScenarioTests/Common.ps1
@@ -52,12 +52,12 @@ function Get-BatchAccountProviderLocation($index)
     {
         $namespace = "Microsoft.Batch"
         $type = "batchAccounts"
-        $r = Get-AzureRmResourceProvider -ProviderNamespace $namespace | where {$_.ResourceTypes[0].ResourceTypeName -eq $type}  
+        $r = Get-AzureRmResourceProvider -ProviderNamespace $namespace | where {$_.ResourceTypes[0].ResourceTypeName -eq $type}
         $location = $r.Locations
   
-        if ($location -eq $null) 
+        if ($location -eq $null)
         {  
-            return "westus"  
+            return "westus"
         } 
         else 
         {  
@@ -67,7 +67,7 @@ function Get-BatchAccountProviderLocation($index)
             }
             else
             {
-                return $location[$index]  
+                return $location[$index]
             }
         }  
     }

src/ResourceManager/AzureBatch/Commands.Batch/Models/BatchClient.BatchAccounts.cs
@@ -285,7 +285,7 @@ namespace Microsoft.Azure.Commands.Batch.Models
         internal string GetGroupForAccountNoThrow(string accountName)
         {
             var response = ResourceManagementClient.Resources.List(new Rest.Azure.OData.ODataQuery<GenericResourceFilter>(
-                r => r.ResourceType == accountName));
+                r => r.ResourceType == accountSearch));
 
             string groupName = null;
 