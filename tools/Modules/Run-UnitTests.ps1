3a45f4165181e4e7e2de04be108513fba417b76e
maddieclayton <maclayto@microsoft.com>
2017-12-04 11:31:16 -0800

    Update Run-UnitTests.ps1

--- tools/Modules/Run-UnitTests.ps1
+++ tools/Modules/Run-UnitTests.ps1
@@ -2,7 +2,7 @@
 
 Import-Module $PSCommandPath\..\..\..\src\Package\Debug\ResourceManager\AzureResourceManager\AzureRM.Profile\AzureRM.Profile.psd1
 Import-Module $PSCommandPath\..\..\..\src\Package\Debug\ResourceManager\AzureResourceManager\AzureRM.Resources\AzureRM.Resources.psd1
-Import-Module $PSCommandPath\TestFx-Tasks.psd1
+Import-Module $PSCommandPath\..\TestFx-Tasks.psd1
 $defaults = [System.IO.Path]::GetDirectoryName($PSCommandPath)
 Set-Location $defaults 
 Invoke-Pester
