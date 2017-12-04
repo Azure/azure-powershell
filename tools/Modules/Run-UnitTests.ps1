c93131b7a3935ae40b6260daec5accdba5e0a9fe
maddieclayton <maddieclayton1@gmail.com>
2017-12-04 09:58:01 -0800

    Fix dependency issues

--- tools/Modules/Run-UnitTests.ps1
+++ tools/Modules/Run-UnitTests.ps1
@@ -1,5 +1,7 @@
 #Requires -Modules Pester
 
+Import-Module $PSCommandPath\..\..\..\src\Package\Debug\ResourceManager\AzureResourceManager\AzureRM.Profile\AzureRM.Profile.psd1
+Import-Module $PSCommandPath\..\..\..\src\Package\Debug\ResourceManager\AzureResourceManager\AzureRM.Resources\AzureRM.Resources.psd1
 Import-Module $PSCommandPath\TestFx-Tasks.psd1
 $defaults = [System.IO.Path]::GetDirectoryName($PSCommandPath)
 Set-Location $defaults 
