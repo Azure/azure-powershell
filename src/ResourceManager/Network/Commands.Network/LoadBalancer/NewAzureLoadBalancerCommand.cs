d016c0d289deaa3e46895f4196a875942c90b810
Sergey Shandar <sergey@live.com.au>
2017-12-05 11:15:47 -0800

    revert NewAzureLoadBalancerCommand

--- src/ResourceManager/Network/Commands.Network/LoadBalancer/NewAzureLoadBalancerCommand.cs
+++ src/ResourceManager/Network/Commands.Network/LoadBalancer/NewAzureLoadBalancerCommand.cs
@@ -13,12 +13,9 @@
 // ----------------------------------------------------------------------------------
 
 using AutoMapper;
-using Microsoft.Azure.Commands.Common.Strategies;
-using Microsoft.Azure.Commands.Common.Strategies.Network;
 using Microsoft.Azure.Commands.Network.Models;
 using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
 using Microsoft.Azure.Commands.ResourceManager.Common.Tags;
-using Microsoft.Azure.Management.Internal.Resources.Models;
 using Microsoft.Azure.Management.Network;
 using System.Collections;
 using System.Collections.Generic;
@@ -31,17 +28,12 @@ namespace Microsoft.Azure.Commands.Network
         OutputType(typeof(PSLoadBalancer))]
     public class NewAzureLoadBalancerCommand : LoadBalancerBaseCmdlet
     {
-        public const string SimpleParameterSet = "SimpleParameterSet";
-
         [Alias("ResourceName")]
         [Parameter(
             Mandatory = true,
             ValueFromPipelineByPropertyName = true,
             HelpMessage = "The resource name.")]
         [ValidateNotNullOrEmpty]
-        [Parameter(
-            ParameterSetName = SimpleParameterSet,
-            Mandatory = true)]
         public virtual string Name { get; set; }
 
         [Parameter(
@@ -50,9 +42,6 @@ namespace Microsoft.Azure.Commands.Network
             HelpMessage = "The resource group name.")]
         [ResourceGroupCompleter]
         [ValidateNotNullOrEmpty]
-        [Parameter(
-            ParameterSetName = SimpleParameterSet,
-            Mandatory = false)]
         public virtual string ResourceGroupName { get; set; }
 
         [Parameter(
@@ -134,26 +123,12 @@ namespace Microsoft.Azure.Commands.Network
                 Name,
                 () =>
                 {
-                    PSLoadBalancer loadBalancer = null;
-                    switch (ParameterSetName)
-                    {
-                        case SimpleParameterSet:
-                            loadBalancer = SimpleParameterSetExecuteCmdlet();
-                            break;
-                        default:
-                            loadBalancer = this.CreateLoadBalancer();
-                            break;
-                    }
-
+                    var loadBalancer = this.CreateLoadBalancer();
                     WriteObject(loadBalancer);
                 },
                 () => present);
         }
-        public PSLoadBalancer SimpleParameterSetExecuteCmdlet()
-        {
-            ResourceGroupName = ResourceGroupName ?? Name;
-            return null;
-        }
+
         private PSLoadBalancer CreateLoadBalancer()
         {
             var loadBalancer = new PSLoadBalancer();
@@ -213,28 +188,6 @@ namespace Microsoft.Azure.Commands.Network
             var getLoadBalancer = this.GetLoadBalancer(this.ResourceGroupName, this.Name);
 
             return getLoadBalancer;
-        }
-    }
-
-    public static class LoadBalancerExtensions
-    {
-        public static MNM.LoadBalancer ToLoadBalancer(this PSLoadBalancer psLb, Hashtable tags)
-        {            
-            // Map to the sdk object
-            var lbModel = NetworkResourceManagerProfile.Mapper.Map<MNM.LoadBalancer>(psLb);
-            lbModel.Tags = TagsConversionHelper.CreateTagDictionary(tags, validate: true);
-
-            return lbModel;
-        }
-
-        public static PSLoadBalancer ToPsLoadBalancer(this MNM.LoadBalancer lb, string resourceGroupName)
-        {
-            var psLoadBalancer = NetworkResourceManagerProfile.Mapper.Map<PSLoadBalancer>(lb);
-            psLoadBalancer.ResourceGroupName = resourceGroupName;
-            psLoadBalancer.Tag =
-                TagsConversionHelper.CreateTagHashtable(lb.Tags);
-
-            return psLoadBalancer;
         }
     }
 }
