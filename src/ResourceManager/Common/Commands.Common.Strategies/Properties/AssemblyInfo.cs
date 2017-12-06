a26d6f105be634858ca9bc35d172d6ab2aaa4d7e
Sergey Shandar <sergey@live.com.au>
2017-12-05 21:51:46 -0800

    XUnit

--- src/ResourceManager/Common/Commands.Common.Strategies/Properties/AssemblyInfo.cs
+++ src/ResourceManager/Common/Commands.Common.Strategies/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Commands.Common.Strategies")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
