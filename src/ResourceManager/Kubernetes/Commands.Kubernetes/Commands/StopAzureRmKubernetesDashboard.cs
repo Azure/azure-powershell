ae9a91d301464cfa60ca8cd54d1a389d8e139a97
David Justice <david@devigned.com>
2017-12-06 11:09:23 -0800

    improve browser pop and tunnel process clean up

---
+++ src/ResourceManager/Kubernetes/Commands.Kubernetes/Commands/StopAzureRmKubernetesDashboard.cs
@@ -0,0 +1,42 @@
+// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System.Linq;
+using System.Management.Automation;
+
+namespace Microsoft.Azure.Commands.Kubernetes
+{
+    [Cmdlet("Stop", KubeNounStr + "Dashboard")]
+    public class StopDashboard : KubeCmdletBase
+    {
+        public override void ExecuteCmdlet()
+        {
+            base.ExecuteCmdlet();
+
+            RunCmdLet(() =>
+            {
+                if (JobRepository.Jobs.FirstOrDefault(j => j.Name == "Kubectl-Tunnel") is KubeTunnelJob exitingJob)
+                {
+                    WriteVerbose(string.Format("Stopping existing Kubectl-Tunnel job with pid {0}.", exitingJob.Pid));
+                    exitingJob.StopJob();
+                    JobRepository.Remove(exitingJob);
+                }
+                else
+                {
+                    WriteVerbose("Did not find job.");
+                }
+            });
+        }
+    }
+}
