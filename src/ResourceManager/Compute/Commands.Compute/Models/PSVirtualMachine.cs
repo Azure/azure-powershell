036b555e7b2afb846e083098ee032db15969e348
Sergey Shandar <sergey@live.com.au>
2017-12-06 10:59:00 -0800

    FQDN => FullyQualifiedDomainName

--- src/ResourceManager/Compute/Commands.Compute/Models/PSVirtualMachine.cs
+++ src/ResourceManager/Compute/Commands.Compute/Models/PSVirtualMachine.cs
@@ -99,6 +99,7 @@ namespace Microsoft.Azure.Commands.Compute.Models
         // Gets or sets the virtual machine zones.
         public IList<string> Zones { get; set; }
 
-        public string Fqdn { get; set; }
+        // Gets or sets the FQDN.
+        public string FullyQualifiedDomainName { get; set; }
     }
 }
