bd8330318150b0628301623296fda97b572f25fd
REDMOND\sepancha <sepancha>
2017-12-09 13:17:46 -0800

    Management Group: Forgot to separate out PSParentGroupInfo.cs

---
+++ src/ResourceManager/ManagementGroups/Commands.ManagementGroups/Models/PSParentGroupInfo.cs
@@ -0,0 +1,25 @@
+﻿namespace Microsoft.Azure.Commands.ManagementGroups.Models
+{
+    using Microsoft.Azure.Management.ManagementGroups.Models;
+
+    public class PSParentGroupInfo
+    {
+        public string ParentId { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public PSParentGroupInfo()
+        {
+            
+        }
+
+        public PSParentGroupInfo(ParentGroupInfo parentGroupInfo)
+        {
+            if (parentGroupInfo != null)
+            {
+                this.ParentId = parentGroupInfo.ParentId;
+                this.DisplayName = parentGroupInfo.DisplayName;
+            }
+        }
+    }
+}
