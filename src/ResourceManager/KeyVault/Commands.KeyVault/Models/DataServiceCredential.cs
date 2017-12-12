64eeb9342262b1b8245f23362f6c20158932425f
tiffanyachen <tifchen@microsoft.com>
2017-12-11 17:16:55 -0800

    Removed check throwing exception for AccessToken authentication

--- src/ResourceManager/KeyVault/Commands.KeyVault/Models/DataServiceCredential.cs
+++ src/ResourceManager/KeyVault/Commands.KeyVault/Models/DataServiceCredential.cs
@@ -78,10 +78,6 @@ namespace Microsoft.Azure.Commands.KeyVault.Models
             if (context.Account == null)
                 throw new ArgumentException(KeyVaultProperties.Resources.ArmAccountNotFound);
 
-            if (context.Account.Type != AzureAccount.AccountType.User &&
-                context.Account.Type != AzureAccount.AccountType.ServicePrincipal)
-                throw new ArgumentException(string.Format(KeyVaultProperties.Resources.UnsupportedAccountType, context.Account.Type));
-
             if (context.Subscription != null && context.Account != null)
                 tenantId = context.Subscription.GetPropertyAsArray(AzureSubscription.Property.Tenants)
                        .Intersect(context.Account.GetPropertyAsArray(AzureAccount.Property.Tenants))
