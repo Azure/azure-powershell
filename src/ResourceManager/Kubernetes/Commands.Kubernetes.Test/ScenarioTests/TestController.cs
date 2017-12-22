c0d40f494488cc698ea9dbf2624c07ae6a63dd0c
David Justice <david@devigned.com>
2017-12-21 19:45:18 -0800

    fixed the test controller

--- src/ResourceManager/Kubernetes/Commands.Kubernetes.Test/ScenarioTests/TestController.cs
+++ src/ResourceManager/Kubernetes/Commands.Kubernetes.Test/ScenarioTests/TestController.cs
@@ -1,16 +1,12 @@
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using Microsoft.Azure.Commands.Common.Authentication;
 using Microsoft.Azure.Commands.Kubernetes.Generated;
 using Microsoft.Azure.Management.Internal.Resources;
-using Microsoft.Azure.Test.HttpRecorder;
-using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
-using TestEnvironmentFactory = Microsoft.Rest.ClientRuntime.Azure.TestFramework.TestEnvironmentFactory;
 using Microsoft.WindowsAzure.Commands.ScenarioTest;
 using Microsoft.WindowsAzure.Commands.Test.Utilities.Common;
+using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
 
 namespace Commands.Kubernetes.Test.ScenarioTests
 {
@@ -29,12 +25,7 @@ namespace Commands.Kubernetes.Test.ScenarioTests
 
         public static TestController NewInstance => new TestController();
 
-        private void SetupManagementClients(MockContext context)
-        {
-            ContainerServiceClient = GetContainerServiceClient(context);
-            ResourceClient = GetResourceManagementClient(context);
-            helper.SetupManagementClients(ContainerServiceClient, ResourceClient);
-        }
+
 
         public void RunPowerShellTest(params string[] scripts)
         {
@@ -44,17 +35,6 @@ namespace Commands.Kubernetes.Test.ScenarioTests
             var callingClassType = sf.GetMethod().ReflectedType.ToString();
             var mockName = st.GetFrame(2).GetMethod().Name;
 
-            var d = new Dictionary<string, string> {{"Microsoft.Resources", null}};
-            var providersToIgnore = new Dictionary<string, string>
-            {
-                {"Microsoft.Azure.Management.Resources.ResourceManagementClient", "2016-02-01"}
-            };
-
-            HttpMockServer.FileSystemUtilsObject = new FileSystemUtils();
-            HttpMockServer.Matcher = new PermissiveRecordMatcherWithApiExclusion(true, d, providersToIgnore);
-            HttpMockServer.RecordsDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "SessionRecords");
-            HttpMockServer.Initialize(callingClassType, mockName);
-            var str = HttpMockServer.FileSystemUtilsObject.GetEnvironmentVariable("AZURE_TEST_MODE");
             using (var context = MockContext.Start(callingClassType, mockName))
             {
                 SetupManagementClients(context);
@@ -67,23 +47,27 @@ namespace Commands.Kubernetes.Test.ScenarioTests
                     "ScenarioTests\\Common.ps1",
                     helper.RMProfileModule,
                     helper.RMResourceModule,
-                    helper.GetRMModulePath(@"AzureRM.Kubernetes.Netcore.psd1"));
+                    helper.GetRMModulePath(@"AzureRM.Kubernetes.psd1"));
 
-                if (scripts != null)
-                {
-                    helper.RunPowerShellTest(scripts);
-                }
+                helper.RunPowerShellTest(scripts);
             }
+        }
+
+        private void SetupManagementClients(MockContext context)
+        {
+            ContainerServiceClient = GetContainerServiceClient(context);
+            ResourceClient = GetResourceManagementClient(context);
+            helper.SetupManagementClients(ContainerServiceClient, ResourceClient);
         }
 
         private ContainerServiceClient GetContainerServiceClient(MockContext context)
         {
-            return context.GetServiceClient<ContainerServiceClient>(TestEnvironmentFactory.GetTestEnvironment());
+            return context.GetServiceClient<ContainerServiceClient>();
         }
 
         private ResourceManagementClient GetResourceManagementClient(MockContext context)
         {
-            return context.GetServiceClient<ResourceManagementClient>(TestEnvironmentFactory.GetTestEnvironment());
+            return context.GetServiceClient<ResourceManagementClient>();
         }
     }
 }
