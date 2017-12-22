7e6371a1ec94b08b3796707b0f8a22a162204787
David Justice <david@devigned.com>
2017-12-21 16:10:01 -0800

    fix output path and drop load.ps1

--- src/ResourceManager/Kubernetes/Commands.Kubernetes/load.ps1
+++
@@ -1,3 +0,0 @@
-Import-Module -Name ./src/Package/Release/ResourceManager/AzureRM.Profile.Netcore/AzureRM.Profile.Netcore.psd1
-Import-Module -Name ./src/Package/Release/ResourceManager/AzureRM.Kubernetes.Netcore/AzureRM.Kubernetes.Netcore.psd1
-Login-AzureRmAccount
