552947caf5cec04d2627b3569a5b66cdc1b19b4f
maddieclayton <maddieclayton1@gmail.com>
2018-01-12 16:25:53 -0800

    DataLakeAnalytics

--- src/ResourceManager/DataLakeAnalytics/Commands.DataLakeAnalytics/Commands/NewAzureRmDataLakeAnalyticsAccount.cs
+++ src/ResourceManager/DataLakeAnalytics/Commands.DataLakeAnalytics/Commands/NewAzureRmDataLakeAnalyticsAccount.cs
@@ -17,6 +17,7 @@ using Microsoft.Azure.Commands.DataLakeAnalytics.Properties;
 using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
 using Microsoft.Azure.Management.DataLake.Analytics.Models;
 using Microsoft.Rest.Azure;
+using System;
 using System.Collections;
 using System.Management.Automation;
 
@@ -50,6 +51,8 @@ namespace Microsoft.Azure.Commands.DataLakeAnalytics
 
         [Parameter(ValueFromPipelineByPropertyName = true, Position = 4, Mandatory = false,
             HelpMessage = "A string,string dictionary of tags associated with this account")]
+        [Obsolete("This property will be removed in favor of -Tag in an upcoming breaking change release.  Please start using the -Tag parameter to avoid breaking scripts.")]
+        [Alias("Tag")]
         [ValidateNotNull]
         public Hashtable Tags { get; set; }
 
