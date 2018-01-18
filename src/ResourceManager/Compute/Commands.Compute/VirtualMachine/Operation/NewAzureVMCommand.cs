c7948d88359f3fcf27b5ed762aa7c6cb9a4af97a
huangpf <phuang@microsoft.com>
2018-01-18 11:59:45 -0800

    Add AzureRM.Compute.ManagedService.psd1 module & ConvertTo-AzureRmVhd script cmdlet

--- src/ResourceManager/Compute/Commands.Compute/VirtualMachine/Operation/NewAzureVMCommand.cs
+++ src/ResourceManager/Compute/Commands.Compute/VirtualMachine/Operation/NewAzureVMCommand.cs
@@ -27,6 +27,8 @@ using Microsoft.Azure.Commands.Compute.Strategies.Compute;
 using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
 using Microsoft.Azure.Management.Compute;
 using Microsoft.Azure.Management.Compute.Models;
+using Microsoft.Azure.Management.Internal.Resources;
+using Microsoft.Azure.Management.Internal.Resources.Models;
 using Microsoft.Azure.Management.Storage;
 using Microsoft.Azure.Management.Storage.Models;
 using Microsoft.Rest.Azure;
@@ -311,6 +313,17 @@ namespace Microsoft.Azure.Commands.Compute
             }
             else
             {
+                var resourceClient =
+                        AzureSession.Instance.ClientFactory.CreateArmClient<ResourceManagementClient>(DefaultProfile.DefaultContext,
+                            AzureEnvironment.Endpoint.ResourceManager);
+                if (!resourceClient.ResourceGroups.CheckExistence(ResourceGroupName))
+                {
+                    var st0 = resourceClient.ResourceGroups.CreateOrUpdate(ResourceGroupName, new ResourceGroup
+                    {
+                        Location = Location,
+                        Name = ResourceGroupName
+                    });
+                }
                 var storageClient =
                         AzureSession.Instance.ClientFactory.CreateArmClient<StorageManagementClient>(DefaultProfile.DefaultContext,
                             AzureEnvironment.Endpoint.ResourceManager);
@@ -348,7 +361,7 @@ namespace Microsoft.Azure.Commands.Compute
                     new Uri(string.Format(
                         "{0}{1}/{2}{3}",
                         storageAccount.PrimaryEndpoints.Blob,
-                        Name.ToLower(),
+                        ResourceGroupName.ToLower(),
                         Name.ToLower(),
                         ".vhd")),
                     out destinationUri);
@@ -480,7 +493,6 @@ namespace Microsoft.Azure.Commands.Compute
 
                     if (!(this.DisableBginfoExtension.IsPresent || IsLinuxOs()))
                     {
-
                         var currentBginfoVersion = GetBginfoExtension();
 
                         if (!string.IsNullOrEmpty(currentBginfoVersion))
