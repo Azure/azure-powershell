0a47edbc0a7e995898efa6054707a920bbbe916e
Siddharth Chatrola <sichatro@microsoft.com>
2018-01-18 15:43:45 -0800

    Fixing storage script for compute tests

--- src/ResourceManager/Common/Commands.ScenarioTests.ResourceManager.Common/AzureRM.Storage.ps1
+++ src/ResourceManager/Common/Commands.ScenarioTests.ResourceManager.Common/AzureRM.Storage.ps1
@@ -132,11 +132,17 @@ function Get-AzureRmStorageAccountKey
     $version = $client.GetType().Assembly.GetName().Version
   }
   PROCESS {
-    if ($version.Major -gt 3)
+    if ($version.Major -gt 5)
     {
         $getTask = $client.StorageAccounts.ListKeysWithHttpMessagesAsync($ResourceGroupName, $name, $null, [System.Threading.CancellationToken]::None)
         $result = $getTask.GetAwaiter().GetResult()
         Write-Output $result.Body.Keys
+    }
+    elseif ($version.Major -gt 3)
+    {
+        $getTask = $client.StorageAccounts.ListKeysWithHttpMessagesAsync($ResourceGroupName, $name, $null, [System.Threading.CancellationToken]::None)
+        $result = $getTask.GetAwaiter().GetResult()
+        Write-Output $result.Body
     }
     else
     {
