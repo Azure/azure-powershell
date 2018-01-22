8a0ad4c4cc2990bb734976d727752de71ce18b46
markcowl <markcowl@microsoft.com>
2018-01-22 00:24:04 -0800

    Completing website addition

--- src/ResourceManager/Websites/Commands.Websites/Strategies/ICmdletAdapter.cs
+++ src/ResourceManager/Websites/Commands.Websites/Strategies/ICmdletAdapter.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.Azure.Commands.Common.Strategies;
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Commands.Common.Strategies;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Microsoft.Azure.Commands.WebApps.Strategies
 {
-    public interface ICmdletAdapter
+    public interface ICmdletAdapter : IShouldProcess
     {
         /// <summary>
         /// Given the target and description of a change, determine if the change should occur
@@ -23,7 +34,7 @@ namespace Microsoft.Azure.Commands.WebApps.Strategies
         /// <param name="query">A textual description of the special circumstances that require special confirmation</param>
         /// <param name="caption">A summary description of the special condition that requires confirmation</param>
         /// <returns>True if the change shoudl proceed, false otherwise</returns>
-        Task<bool> SHouldContinueChangeAsync(string query, string caption);
+        Task<bool> ShouldContinueChangeAsync(string query, string caption);
 
         /// <summary>
         /// Report an error
@@ -31,6 +42,19 @@ namespace Microsoft.Azure.Commands.WebApps.Strategies
         /// <param name="exception">The exception descriging the error</param>
         /// <returns>nothing</returns>
         void WriteExceptionAsync(Exception exception);
+
+        /// <summary>
+        /// Write an object to the ouput stream
+        /// </summary>
+        /// <param name="output">The oject to write</param>
+        void WriteObjectAsync(object output);
+
+        /// <summary>
+        /// Write an object to the outpit stream
+        /// </summary>
+        /// <param name="output">The object to write</param>
+        /// <param name="enumerateCollection">Indicate whether write each element of a collection to the output stream</param>
+        void WriteObjectAsync(object output, bool enumerateCollection);
 
         /// <summary>
         /// Log additional information
@@ -59,53 +83,12 @@ namespace Microsoft.Azure.Commands.WebApps.Strategies
         /// <param name="description">A description of the activity that is starting</param>
         /// <param name="initialStatus">The inbitial status of the activity</param>
         /// <returns>An activity tracker</returns>
-        IActivity StartActivity(string description, string initialStatus, int secondsRemaining);
+        void ReportTaskProgress(ITaskProgress progress);
 
         /// <summary>
         /// Complete all processing
         /// </summary>
         /// <returns>nothing</returns>
         void Complete();
-    }
-
-    public interface IActivity
-    {
-        /// <summary>
-        /// Identifier for the activity
-        /// </summary>
-        int Id { get; }
-
-        /// <summary>
-        /// Summart description of the activity
-        /// </summary>
-        string Description  { get; }
-
-        /// <summary>
-        /// Summary description of the current activity status
-        /// </summary>
-        string StatusDescription { get; }
-
-        /// <summary>
-        /// Complete the given activity
-        /// </summary>
-        /// <returns>A task, which ends when completion is signaled</returns>
-        void CompleteAsync();
-
-        /// <summary>
-        /// Repoprt progress on the activity
-        /// </summary>
-        /// <param name="statusDesscription">A summary description fo the current status</param>
-        /// <param name="secondsRemaining">The number fo secodns until the activity is complete</param>
-        /// <param name="percentComplete">The relative amount of activity complete, where 1 = 1%</param>
-        /// <returns>A Task that ends when completion is signaled</returns>
-        void ReportProgress(string statusDesscription, int secondsRemaining, int percentComplete);
-
-        /// <summary>
-        /// Start a sub-activity of the current activity
-        /// </summary>
-        /// <returns>The child activity</returns>
-        IActivity StartChildActivity();
-
-        void ReportProgress(ITaskProgress progress);
     }
 }
