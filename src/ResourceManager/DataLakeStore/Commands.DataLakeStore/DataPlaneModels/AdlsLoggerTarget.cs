ecc8e6de4e733bc3a3f7f99c64f4c6fe113844f1
Rahul Dutta <rahuldutta90@users.noreply.github.com>
2018-01-31 18:56:37 -0800

    First commit of the target logger

---
+++ src/ResourceManager/DataLakeStore/Commands.DataLakeStore/DataPlaneModels/AdlsLoggerTarget.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Concurrent;
+using System.IO;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace Microsoft.Azure.Commands.DataLakeStore.Models
+{
+    public delegate void WriteMessageDelegate(string message);
+    [Target("AdlsLogger")]
+    public sealed class AdlsLoggerTarget : TargetWithLayout
+    {
+        internal ConcurrentQueue<string> DebugMessageQueue;  
+        public AdlsLoggerTarget()
+        {
+        }
+        protected override void Write(LogEventInfo logEvent)
+        {
+            string logMessage = Layout.Render(logEvent);
+            DebugMessageQueue?.Enqueue(logMessage);
+        }
+    }
+}
