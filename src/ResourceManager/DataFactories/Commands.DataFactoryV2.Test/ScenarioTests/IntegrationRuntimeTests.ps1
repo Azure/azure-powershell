369addcbaab70933382625b2606ab669edf8a073
Vincent He <chinadragon0515@gmail.com>
2018-02-01 16:13:25 -0800

    Add temp nupkg file for build and test, this will be removed by Hermine Vermishyan or Dean Talley once we publish DataFactory SDK to nuget.org

--- src/ResourceManager/DataFactories/Commands.DataFactoryV2.Test/ScenarioTests/IntegrationRuntimeTests.ps1
+++ src/ResourceManager/DataFactories/Commands.DataFactoryV2.Test/ScenarioTests/IntegrationRuntimeTests.ps1
@@ -97,6 +97,7 @@ function Test-SsisAzure-IntegrationRuntime
      
         $irname = "ssis-azure-ir"
         $description = "SSIS-Azure integration runtime"
+        $sasuri = "https://ssisazurefileshare.blob.core.windows.net/privatepreview?st=2018-02-04T05%3A08%3A00Z&se=2020-02-05T05%3A08%3A00Z&sp=rwl&sv=2017-04-17&sr=c&sig=1QxUPy4ClSeDoZM4TOgpxFX7idQWpyoL0jDVg3B3Io8%3D"
    
         $secpasswd = ConvertTo-SecureString $Env:CatalogAdminPassword -AsPlainText -Force
         $mycreds = New-Object System.Management.Automation.PSCredential($Env:CatalogAdminUsername, $secpasswd)
@@ -114,6 +115,7 @@ function Test-SsisAzure-IntegrationRuntime
             -MaxParallelExecutionsPerNode 1 `
             -LicenseType LicenseIncluded `
             -Edition Enterprise `
+            -SetupScriptContainerSasUri $sasuri `
             -Force
 
         $expected = Get-AzureRmDataFactoryV2IntegrationRuntime -ResourceGroupName $rgname `
