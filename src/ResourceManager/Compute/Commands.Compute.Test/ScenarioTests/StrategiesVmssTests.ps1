695d9a1f098240fa0c1466b74e579ecef6a28b99
Sergey Shandar <sergey@live.com.au>
2018-02-05 17:14:58 -0800

    $vmssname

--- src/ResourceManager/Compute/Commands.Compute.Test/ScenarioTests/StrategiesVmssTests.ps1
+++ src/ResourceManager/Compute/Commands.Compute.Test/ScenarioTests/StrategiesVmssTests.ps1
@@ -26,7 +26,7 @@ function Test-SimpleNewVmss
         $username = "admin01"
         $password = "werWER345#%^" | ConvertTo-SecureString -AsPlainText -Force
         $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $password
-		[string]$domainNameLabel = "$name$rgname".tolower();
+		[string]$domainNameLabel = "$vmssname$vmssname".tolower();
 
         # Common
         $x = New-AzureRmVmss -Name $vmssname -Credential $cred -DomainNameLabel $domainNameLabel
