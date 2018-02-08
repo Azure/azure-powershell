60e696fc904128967ca4bf6366767fd8fb1dc2f3
cormacpayne <corm@microsoft.com>
2018-02-08 11:31:04 -0800

    Fix indentation in JSON files, move logging logic

--- src/ResourceManager/Common/Commands.ScenarioTests.ResourceManager.Common/EnvironmentSetupHelper.cs
+++ src/ResourceManager/Common/Commands.ScenarioTests.ResourceManager.Common/EnvironmentSetupHelper.cs
@@ -556,12 +556,13 @@ namespace Microsoft.WindowsAzure.Commands.ScenarioTest
                 catch (Exception psException)
                 {
                     powershell.LogPowerShellException(psException, TracingInterceptor);
+                    powershell.LogPowerShellResults(output, TracingInterceptor);
+                    powershell.Streams.Error.Clear();
                     throw;
                 }
                 finally
                 {
-                    powershell.LogPowerShellResults(output, TracingInterceptor);
-                    powershell.Streams.Error.Clear();
+
                 }
             }
         }
