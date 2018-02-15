68ac74aaf4338b5ee660dc1a88a8d10bec7ddb40
Sergey Shandar <sergey@live.com.au>
2018-02-14 17:41:02 -0800

    minor.

--- src/ResourceManager/Compute/Commands.Compute/Strategies/Network/LoadBalancerStrategy.cs
+++ src/ResourceManager/Compute/Commands.Compute/Strategies/Network/LoadBalancerStrategy.cs
@@ -17,7 +17,6 @@ using Microsoft.Azure.Commands.Compute.Strategies.ResourceManager;
 using Microsoft.Azure.Management.Internal.Network.Version2017_10_01;
 using Microsoft.Azure.Management.Internal.Network.Version2017_10_01.Models;
 using Microsoft.Azure.Management.Internal.Resources.Models;
-using System;
 using System.Collections.Generic;
 
 namespace Microsoft.Azure.Commands.Compute.Strategies.Network
