1c30c57d93fbb0546a77b0faca3cb111242b5514
Sisira Panchagnula <sisirap@microsoft.com>
2018-02-20 16:58:36 -0800

    Re-recording RemoveWebAppTest & adding the changes from pull request #5471 for identity & httpsOnly property

--- src/ResourceManager/Websites/Commands.Websites/Cmdlets/WebApps/SetAzureWebApp.cs
+++ src/ResourceManager/Websites/Commands.Websites/Cmdlets/WebApps/SetAzureWebApp.cs
@@ -97,10 +97,10 @@ namespace Microsoft.Azure.Commands.WebApps.Cmdlets.WebApps
         [Parameter(Mandatory = false, HelpMessage = "Run cmdlet in the background")]
         public SwitchParameter AsJob { get; set; }
         
-        [Parameter(ParameterSetName = ParameterSet1Name, Mandatory = false, HelpMessage = "Enable MSI on an existing azure webapp or functionapp")]
+        [Parameter(ParameterSetName = ParameterSet1Name, Mandatory = false, HelpMessage = "Enable MSI on an existing azure webapp")]
         public SwitchParameter AssignIdentity { get; set; }
 
-        [Parameter(ParameterSetName = ParameterSet1Name, Mandatory = false, HelpMessage = "Enable/disable redirecting all traffic to HTTPS on an existing azure webapp or functionapp")]
+        [Parameter(ParameterSetName = ParameterSet1Name, Mandatory = false, HelpMessage = "Enable/disable redirecting all traffic to HTTPS on an existing azure webapp")]
         public SwitchParameter HttpsOnly { get; set; }
 
         public override void ExecuteCmdlet()
@@ -149,13 +149,13 @@ namespace Microsoft.Azure.Commands.WebApps.Cmdlets.WebApps
                         {
                             Location = location,
                             ServerFarmId = WebApp.ServerFarmId,
-                            Identity = parameters.Contains("AssignIdentity") ? new ManagedServiceIdentity("SystemAssigned", null, null) : WebApp.Identity,
-                            HttpsOnly = parameters.Contains("HttpsOnly")
+                            Identity = AssignIdentity.IsPresent ? new ManagedServiceIdentity("SystemAssigned", null, null) : WebApp.Identity,
+                            HttpsOnly = HttpsOnly.IsPresent
                         };
 
-                        if (parameters.Contains("AssignIdentity"))
+                        Dictionary<string, string> appSettings = WebApp.SiteConfig?.AppSettings?.ToDictionary(x => x.Name, x => x.Value);
+                        if (AssignIdentity.IsPresent)
                         {
-                            Dictionary<string, string> appSettings = WebApp.SiteConfig?.AppSettings?.ToDictionary(x => x.Name, x => x.Value);
 
                             // Add or update the appsettings property
                             appSettings["WEBSITE_DISABLE_MSI"] = (!AssignIdentity).ToString();
@@ -168,6 +168,11 @@ namespace Microsoft.Azure.Commands.WebApps.Cmdlets.WebApps
                                                                             Value = nvp.ConnectionString
                                                                         },
                                                                         StringComparer.OrdinalIgnoreCase));
+                        }
+                        else
+                        {
+                            // disabling the identity property updates the siteconfig only
+                            appSettings["WEBSITE_DISABLE_MSI"] = (!AssignIdentity.IsPresent).ToString();
                         }
                         
                         WebsitesClient.UpdateWebApp(ResourceGroupName, location, Name, null, WebApp.ServerFarmId, site);
