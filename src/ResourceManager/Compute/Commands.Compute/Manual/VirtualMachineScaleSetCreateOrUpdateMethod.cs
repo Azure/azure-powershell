006e1c4f414a212a4cd512f5a47434f942b8d479
Sergey Shandar <sergey@live.com.au>
2018-02-22 17:05:37 -0800

    nameof

--- src/ResourceManager/Compute/Commands.Compute/Manual/VirtualMachineScaleSetCreateOrUpdateMethod.cs
+++ src/ResourceManager/Compute/Commands.Compute/Manual/VirtualMachineScaleSetCreateOrUpdateMethod.cs
@@ -166,7 +166,7 @@ namespace Microsoft.Azure.Commands.Compute.Automation
                 adminPassword: new NetworkCredential(string.Empty, Credential.Password).Password,
                 vmSize: VmSize,
                 instanceCount: InstanceCount,
-                upgradeMode: MyInvocation.BoundParameters.ContainsKey("UpgradePolicyMode")
+                upgradeMode: MyInvocation.BoundParameters.ContainsKey(nameof(UpgradePolicyMode))
                     ? UpgradePolicyMode
                     : (UpgradeMode?)null);
 
