73975a38d41133856caaff25f7ff3b4e0ee69b75
Irina Rogozhkina <irrogozh@microsoft.com>
2018-03-03 22:54:36 -0800

    Rerecorded tests for NetworkWatcher: connectivityCheck, providerList, reachabilityReport

--- src/ResourceManager/Network/Commands.Network.Test/ScenarioTests/NetworkWatcherAPITests.cs
+++ src/ResourceManager/Network/Commands.Network.Test/ScenarioTests/NetworkWatcherAPITests.cs
@@ -75,7 +75,7 @@ namespace Commands.Network.Test.ScenarioTests
             NetworkResourcesController.NewInstance.RunPsTest("Test-FlowLog");
         }
 
-        [Fact(Skip = "There was a bug in API. After fix is deployed, we need to rerecord test.")]
+        [Fact]
         [Trait(Category.AcceptanceType, Category.CheckIn)]
         public void TestConnectivityCheck()
         {
@@ -89,7 +89,7 @@ namespace Commands.Network.Test.ScenarioTests
             NetworkResourcesController.NewInstance.RunPsTest("Test-ReachabilityReport");
         }
 
-        [Fact(Skip = "There was a bug in API. After fix is deployed, we need to rerecord test.")]
+        [Fact]
         [Trait(Category.AcceptanceType, Category.CheckIn)]
         public void TestProvidersList()
         {
