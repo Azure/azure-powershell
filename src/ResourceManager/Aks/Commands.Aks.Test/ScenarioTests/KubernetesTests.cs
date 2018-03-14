d7082276b6fa32b21fe08c97324b3c080aa71cff
maddieclayton <maclayto@microsoft.com>
2018-03-14 14:20:01 -0700

    fix test paths

--- src/ResourceManager/Aks/Commands.Aks.Test/ScenarioTests/KubernetesTests.cs
+++ src/ResourceManager/Aks/Commands.Aks.Test/ScenarioTests/KubernetesTests.cs
@@ -9,6 +9,7 @@ using Microsoft.WindowsAzure.Commands.ScenarioTest;
 using Microsoft.WindowsAzure.Commands.Test.Utilities.Common;
 using Xunit;
 using Xunit.Abstractions;
+using System.Reflection;
 
 namespace Commands.Aks.Test.ScenarioTests
 {
@@ -22,8 +23,9 @@ namespace Commands.Aks.Test.ScenarioTests
             {
                 AzureSession.Instance.DataStore = new MemoryDataStore();
                 var home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-                AzureSession.Instance.DataStore.WriteFile(Path.Combine(home, ".ssh", "id_rsa.pub"), File.ReadAllText("Fixtures/id_rsa.pub"));
-                AzureSession.Instance.DataStore.WriteFile(Path.Combine(home, ".azure", "acsServicePrincipal.json"), "Fixtures/acsServicePrincipal.json");
+                var dir = Path.GetDirectoryName(new Uri(Assembly.GetExecutingAssembly().CodeBase).AbsolutePath);
+                AzureSession.Instance.DataStore.WriteFile(Path.Combine(home, ".ssh", "id_rsa.pub"), File.ReadAllText(dir + "/Fixtures/id_rsa.pub"));
+                AzureSession.Instance.DataStore.WriteFile(Path.Combine(home, ".azure", "acsServicePrincipal.json"), dir + "/Fixtures/acsServicePrincipal.json");
             }
         }
 
