508b54b889868bb13003e03561e52bb1d7fa2c41
markcowl <markcowl@microsoft.com>
2018-03-18 22:37:27 -0700

    Add token caching and retries

---
+++ src/Common/Commands.Common.Authentication/Utilities/HttpClientWithRetry.cs
@@ -0,0 +1,64 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Commands.Common.Authentication.Utilities;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Commands.Common.Authentication
+{
+    public class HttpClientWithRetry
+    {
+        HttpClient _client;
+        Func<IHttpRetryAlgorithm> _retry;
+
+        public HttpClientWithRetry()
+            : this(new HttpClient(), () => HttpRetryAlgorithm.Default)
+        {
+            _client = new HttpClient();
+        }
+
+        public HttpClientWithRetry(TimeSpan interval, int multiplier, int maxTries) :
+            this(new HttpClient(), () => HttpRetryAlgorithm.GetExponentialRetryAlgorithm(interval, multiplier, maxTries))
+        {
+        }
+
+        public HttpClientWithRetry(HttpClient client):
+            this(client, () => HttpRetryAlgorithm.Default)
+        {
+
+        }
+
+        public HttpClientWithRetry(HttpClient client, Func<IHttpRetryAlgorithm> retry)
+        {
+            _client = client;
+            _retry = retry;
+        }
+
+        public async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken token)
+        {
+            HttpResponseMessage response;
+            var retry = _retry();
+            do
+            {
+                await retry.WaitForRetry();
+                response = await _client.SendAsync(request, token);
+            }
+            while (response.IsSuccessStatusCode || retry.ShouldRetry(response));
+            return response;
+        }
+    }
+}
