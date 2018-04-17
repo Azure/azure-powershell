9885237c0d49ba456b9cef0b0ae2bc5794b731b0
Sergey Shandar <sergey.shandar@gmail.com>
2018-04-16 18:12:30 -0700

    AsyncCmdletExtensions is moved to Strategies.

---
+++ src/ResourceManager/Common/Commands.Common.Strategies/AsyncCmdletExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Microsoft.Azure.Commands.Common.Strategies
+{
+    public static class AsyncCmdletExtensions
+    {
+        public static void WriteVerbose(this IAsyncCmdlet cmdlet, string message, params object[] p)
+            => cmdlet.WriteVerbose(string.Format(message, p));
+    }
+}
