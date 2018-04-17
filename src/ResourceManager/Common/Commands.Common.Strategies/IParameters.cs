7e5ba84e6b8b591ace2e4cdbd3bc05c83f6f523b
Sergey Shandar <sergey.shandar@gmail.com>
2018-04-16 17:57:30 -0700

    IParameters is moved to Strategies library.

---
+++ src/ResourceManager/Common/Commands.Common.Strategies/IParameters.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Commands.Common.Strategies
+{
+    public interface IParameters<TModel>
+        where TModel : class
+    {
+        string Location { get; set; }
+
+        Task<ResourceConfig<TModel>> CreateConfigAsync();
+    }
+}
