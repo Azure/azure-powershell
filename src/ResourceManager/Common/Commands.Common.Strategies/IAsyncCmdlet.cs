10c03aff11d5bd838988358fade7e7c0305b3c98
Sergey Shandar <sergey.shandar@gmail.com>
2018-04-16 18:01:19 -0700

    IAsyncCmdlet is moved to Strategies.

---
+++ src/ResourceManager/Common/Commands.Common.Strategies/IAsyncCmdlet.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Commands.Common.Strategies
+{
+    public interface IAsyncCmdlet
+    {
+        void WriteVerbose(string message);
+
+        Task<bool> ShouldProcessAsync(string target, string action);
+
+        void WriteObject(object value);
+
+        void ReportTaskProgress(ITaskProgress taskProgress);
+    }
+}
