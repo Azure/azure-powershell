22b71fba3110f7aa7a0be362c1050140b67765b9
Sergey Shandar <sergey.shandar@gmail.com>
2018-04-17 19:54:26 -0700

    With Strategies.

---
+++ src/ResourceManager/SignalR/Commands.SignalR/Strategies/Client.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.Commands.Common.Authentication;
+using Microsoft.Azure.Commands.Common.Authentication.Abstractions;
+using Microsoft.Azure.Commands.Common.Strategies;
+using Microsoft.Rest;
+
+namespace Microsoft.Azure.Commands.SignalR.Strategies
+{
+    sealed class Client : IClient
+    {
+        public string SubscriptionId { get; }
+
+        IAzureContext Context { get; }
+
+        public Client(IAzureContext context)
+        {
+            Context = context;
+            SubscriptionId = Context.Subscription.Id;
+        }
+
+        public T GetClient<T>()
+            where T : ServiceClient<T>
+            => AzureSession.Instance.ClientFactory.CreateArmClient<T>(
+                Context, AzureEnvironment.Endpoint.ResourceManager);
+    }
+}
