03a6217c6ac9d1f92ca241f175146cf31a0289b3
Pareekshit Singh <pareekshit.singh@microsoft.com>
2018-04-20 14:14:12 -0700

    Fix assembly name in assembluInfo

--- src/Common/Commands.Common.Keyvault/Properties/AssemblyInfo.cs
+++ src/Common/Commands.Common.Keyvault/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Commands.Common.Keyvault")]
+[assembly: AssemblyTitle("Commands.Common.KeyVault")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Commands.Common.Keyvault")]
+[assembly: AssemblyProduct("Commands.Common.KeyVault")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
