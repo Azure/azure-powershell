59fb3faaecaadea960471c7b9c1acbab3341876a
maddieclayton <maclayto@microsoft.com>
2018-04-19 18:03:31 -0700

    fix build

--- src/Common/Commands.Common/Extensions/CmdletExtensions.cs
+++ src/Common/Commands.Common/Extensions/CmdletExtensions.cs
@@ -238,13 +238,13 @@ namespace Microsoft.WindowsAzure.Commands.Utilities.Common
                 return null;
             }
 
-            if (psCmdlet.AsJobSessionState == null)
+            if (psCmdlet.SessionState == null)
             {
                 return path;
             }
 
             path = path.Trim('"', '\'', ' ');
-            var result = psCmdlet.AsJobSessionState.Path.GetResolvedPSPathFromPSPath(path);
+            var result = psCmdlet.SessionState.Path.GetResolvedPSPathFromPSPath(path);
             string fullPath = string.Empty;
 
             if (result != null && result.Count > 0)
@@ -338,7 +338,7 @@ namespace Microsoft.WindowsAzure.Commands.Utilities.Common
                 powershell.AddScript(contents);
                 Collection<T> result = powershell.Invoke<T>();
 
-                if (cmdlet.AsJobSessionState != null)
+                if (cmdlet.SessionState != null)
                 {
                     powershell.Streams.Error.ForEach(e => cmdlet.WriteError(e));
                     powershell.Streams.Verbose.ForEach(r => cmdlet.WriteVerbose(r.Message));
