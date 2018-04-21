6e414677091c3b8883c677419913139893bbd4af
Bala Ganapathy <bganapa@microsoft.com>
2018-04-20 17:24:40 -0700

    Get-AzsSubscription with the subscriptionid instead list and where

--- src/StackAdmin/Azs.Subscriptions.Admin/Module/Azs.Subscriptions.Admin/Generated.PowerShell.Commands/SwaggerPathCommands/Set-AzsUserSubscription.ps1
+++ src/StackAdmin/Azs.Subscriptions.Admin/Module/Azs.Subscriptions.Admin/Generated.PowerShell.Commands/SwaggerPathCommands/Set-AzsUserSubscription.ps1
@@ -176,7 +176,7 @@ function Set-AzsUserSubscription {
                 if ( 'InputObject' -eq $PsCmdlet.ParameterSetName -or 'ResourceId' -eq $PsCmdlet.ParameterSetName -or 'Set' -eq $PsCmdlet.ParameterSetName ) {
 
                     if ( $updatedSubscription -eq $null ) {
-                        $updatedSubscription = Get-AzsUserSubscription | Where-Object { $_.SubscriptionId -eq $SubscriptionId }
+                        $updatedSubscription = Get-AzsUserSubscription -SubscriptionId -eq $SubscriptionId
                     }
 
                     $flattenedParameters = @('TenantId', 'SubscriptionId', 'DisplayName', 'DelegatedProviderSubscriptionId', 'Owner', 'RoutingResourceManagerType', 'ExternalReferenceId', 'State', 'Location', 'OfferId')
