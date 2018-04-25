07b49d0a86ce7e7962bee74627165cfe412e68fc
antoniow <antoniow@microsoft.com>
2018-04-25 16:37:33 -0700

    Implement PSNodeCounts.FormatObject to support printing node state counts on getting PoolNodeCounts

---
+++ src/ResourceManager/AzureBatch/Commands.Batch/Models/PSNodeCounts.FormatObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace Microsoft.Azure.Commands.Batch.Models
+{
+    public partial class PSNodeCounts
+    {
+        private static PropertyInfo[] psNodeCountsPropertyInfos;
+
+        public static string FormatObject(PSNodeCounts psNodeCounts)
+        {
+            var propertyInfos = GetPropertyInfos();
+
+            var propertyStrings = new List<string>();
+            foreach (var propertyInfo in propertyInfos)
+            {
+                var propertyValue = propertyInfo.GetValue(psNodeCounts);
+
+                propertyStrings.Add($"{propertyInfo.Name}: {propertyValue}");
+            }
+
+            return string.Join(",", propertyStrings);
+        }
+
+        private static PropertyInfo[] GetPropertyInfos()
+        {
+            if (psNodeCountsPropertyInfos == null)
+            {
+                psNodeCountsPropertyInfos = typeof(PSNodeCounts).GetProperties(BindingFlags.Public | BindingFlags.GetProperty | BindingFlags.Instance);
+            }
+
+            return psNodeCountsPropertyInfos;
+        }
+    }
+}
