cd71f9effa81264eb38997d1475ed39157c707b2
Jared Moore <jaredmoo@microsoft.com>
2018-05-09 14:34:56 -0700

    Remove commented code

--- src/ResourceManager/Sql/Commands.Sql.Test/ScenarioTests/BlobAuditingTests.cs
+++ src/ResourceManager/Sql/Commands.Sql.Test/ScenarioTests/BlobAuditingTests.cs
@@ -26,13 +26,12 @@ namespace Microsoft.Azure.Commands.Sql.Test.ScenarioTests
         {
             var sqlClient = GetSqlClient(context);
             var sqlLegacyClient = GetLegacySqlClient();
-            //var storageClient = GetStorageClient();
             var storageV2Client = GetStorageV2Client();
             var commonStorageClient = GetCommonStorageClient(context);
             var resourcesClient = GetResourcesClient();
             var newResourcesClient = GetResourcesClient(context);
             var authorizationClient = GetAuthorizationManagementClient();
-            helper.SetupSomeOfManagementClients(sqlClient, sqlLegacyClient, /*storageClient, */storageV2Client, resourcesClient, newResourcesClient, authorizationClient, commonStorageClient);
+            helper.SetupSomeOfManagementClients(sqlClient, sqlLegacyClient, storageV2Client, resourcesClient, newResourcesClient, authorizationClient, commonStorageClient);
         }
 
         public BlobAuditingTests(ITestOutputHelper output) : base(output)
