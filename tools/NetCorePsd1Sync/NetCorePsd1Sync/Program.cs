40540bd655440df0d048426bd72c55ef3c5e2073
MiYanni <miyanni@microsoft.com>
2018-05-09 14:03:23 -0700

    Added AnalysisServices.DataPlane, since it is skipped by our path analysis.

--- tools/NetCorePsd1Sync/NetCorePsd1Sync/Program.cs
+++ tools/NetCorePsd1Sync/NetCorePsd1Sync/Program.cs
@@ -67,6 +67,7 @@ namespace NetCorePsd1Sync
             var netCoreFilePaths = desktopFilePaths.Select(ConvertDesktopToNetCorePath).Where(File.Exists).ToList();
             netCoreFilePaths.Add(Path.Combine(rmPath, @"..\Storage\Azure.Storage.Netcore.psd1"));
             netCoreFilePaths.Add(Path.Combine(rmPath, @"..\..\tools\AzureRM.Netcore\AzureRM.Netcore.psd1"));
+            netCoreFilePaths.Add(Path.Combine(rmPath, @"AnalysisServices\Commands.AnalysisServices.Dataplane\Azure.AnalysisServices.Netcore.psd1"));
             var netCoreHashTables = GetHashtables(netCoreFilePaths);
 
             foreach (var netCoreHashtable in netCoreHashTables)
