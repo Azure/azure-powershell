8c095221347834b3261759325a1a23d437768e4f
v-ajnava <v-ajnava@microsoft.com>
2018-05-08 19:11:43 -0700

    updated packages and Breakingchangeissues.csv

--- src/ResourceManager/EventHub/Commands.EventHub.Test/ScenarioTests/EventHubsController.cs
+++ src/ResourceManager/EventHub/Commands.EventHub.Test/ScenarioTests/EventHubsController.cs
@@ -15,9 +15,9 @@
 using Microsoft.Azure.Commands.Common.Authentication;
 using Microsoft.Azure.Management.Authorization;
 using Microsoft.Azure.Management.Resources;
-using Microsoft.Azure.Management.EventHub;
 using Microsoft.Azure.Subscriptions;
 using Microsoft.Azure.Test.HttpRecorder;
+using Microsoft.Azure.Management.EventHub;
 using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
 using Microsoft.WindowsAzure.Commands.ScenarioTest;
 using System;
