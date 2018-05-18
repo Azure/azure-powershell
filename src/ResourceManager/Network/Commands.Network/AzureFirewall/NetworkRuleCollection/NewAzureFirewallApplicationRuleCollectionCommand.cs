8fcd607248ebf8de3831158b89bd933a6761c098
Stefan Manea <stefan.manea@outlook.com>
2018-05-17 19:02:40 -0700

    Rename every single appearance of SecureGateway with AzureFirewall

---
+++ src/ResourceManager/Network/Commands.Network/AzureFirewall/NetworkRuleCollection/NewAzureFirewallApplicationRuleCollectionCommand.cs
@@ -0,0 +1,62 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using Microsoft.Azure.Commands.Network.Models;
+
+namespace Microsoft.Azure.Commands.Network
+{
+
+    [Cmdlet(VerbsCommon.New, "AzureRmFirewallNetworkRuleCollection", SupportsShouldProcess = true), OutputType(typeof(PSAzureFirewallNetworkRuleCollection))]
+    public class NewAzureFirewallNetworkRuleCollectionCommand : NetworkBaseCmdlet
+    {
+        [Parameter(
+            Mandatory = true,
+            HelpMessage = "The name of the Network Rule Collection")]
+        [ValidateNotNullOrEmpty]
+        public virtual string Name { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            HelpMessage = "The priority of the rule collection")]
+        [ValidateNotNullOrEmpty]
+        public uint Priority { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            HelpMessage = "The list of network rules")]
+        [ValidateNotNullOrEmpty]
+        public List<PSAzureFirewallNetworkRule> Rule { get; set; }
+
+        public override void Execute()
+        {
+            base.Execute();
+
+            if (this.Rule == null || this.Rule.Count == 0)
+            {
+                throw new ArgumentException("At least one application rule should be specified!");
+            }
+
+            var networkRuleCollection = new PSAzureFirewallNetworkRuleCollection
+            {
+                Name = this.Name,
+                Priority = this.Priority,
+                Rules = this.Rule
+            };
+            WriteObject(networkRuleCollection);
+        }
+    }
+}
