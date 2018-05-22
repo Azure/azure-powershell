35890f796d8368299dfbd62ee84aa8d3fdd7b966
REDMOND\chanwan <chanwan@microsoft.com>
2018-05-22 16:13:50 -0700

    Add scenario test cases for budget CRUD operations

---
+++ src/ResourceManager/Consumption/Commands.Consumption.Test/ScenarioTests/BudgetTests.cs
@@ -0,0 +1,103 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Commands.Consumption.Test.ScenarioTests.ScenarioTest;
+using Microsoft.Azure.Commands.ScenarioTest;
+using Microsoft.Azure.ServiceManagemenet.Common.Models;
+using Microsoft.WindowsAzure.Commands.ScenarioTest;
+using Microsoft.WindowsAzure.Commands.Test.Utilities.Common;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.Azure.Commands.Consumption.Test.ScenarioTests
+{
+    public class BudgetTests : RMTestBase
+    {
+        public BudgetTests(ITestOutputHelper output)
+        {
+            XunitTracingInterceptor.AddToContext(new XunitTracingInterceptor(output));
+            TestExecutionHelpers.SetUpSessionAndProfile();
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestNewBudget()
+        {
+            TestController.NewInstance.RunPowerShellTest("Test-NewBudget");
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestNewBudgetAtResourceGroupLevel()
+        {
+            TestController.NewInstance.RunPowerShellTest("Test-NewBudgetAtResourceGroupLevel");
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestGetBudgets()
+        {
+            TestController.NewInstance.RunPowerShellTest("Test-GetBudgets");
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestGetBudgetsAtResourceGroupLevel()
+        {
+            TestController.NewInstance.RunPowerShellTest("Test-GetBudgetsAtResourceGroupLevel");
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestGetBudgetByName()
+        {
+            TestController.NewInstance.RunPowerShellTest("Test-GetBudgetByName");
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestGetBudgetByNameAtResourceGroupLevel()
+        {
+            TestController.NewInstance.RunPowerShellTest("Test-GetBudgetByNameAtResourceGroupLevel");
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestSetBudget()
+        {
+            TestController.NewInstance.RunPowerShellTest("Test-SetBudget");
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestSetBudgetAtResourceGroupLevel()
+        {
+            TestController.NewInstance.RunPowerShellTest("Test-SetBudgetAtResourceGroupLevel");
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestRemoveBudget()
+        {
+            TestController.NewInstance.RunPowerShellTest("Test-RemoveBudget");
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestRemoveBudgetAtResourceGroupLevel()
+        {
+            TestController.NewInstance.RunPowerShellTest("Test-RemoveBudgetAtResourceGroupLevel");
+        }
+    }
+}
---
+++ src/ResourceManager/Consumption/Commands.Consumption.Test/ScenarioTests/BudgetTests.ps1
@@ -0,0 +1,135 @@
+﻿# ----------------------------------------------------------------------------------
+#
+# Copyright Microsoft Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------------
+
+<#
+.SYNOPSIS
+New budget
+#>
+function Test-NewBudget
+{
+    $budget = New-AzureRmConsumptionBudget -Amount 60 -Name PSBudget -Category Cost -StartDate 2018-05-01 -EndDate 2018-11-01 -TimeGrain Monthly
+	Assert-NotNull $budget
+	Assert-AreEqual 60 $budget.Amount
+	Assert-AreEqual PSBudget $budget.Name
+	Assert-AreEqual Cost $budget.Category
+	Assert-AreEqual Monthly $budget.TimeGrain
+}
+
+<#
+.SYNOPSIS
+New budget at resource group level
+#>
+function Test-NewBudgetAtResourceGroupLevel
+{
+    $budget = New-AzureRmConsumptionBudget -ResourceGroupName RGBudgets -Amount 60 -Name PSBudgetRG -Category Cost -StartDate 2018-05-01 -EndDate 2018-11-01 -TimeGrain Monthly 
+	Assert-NotNull $budget
+	Assert-AreEqual 60 $budget.Amount
+	Assert-AreEqual PSBudgetRG $budget.Name
+	Assert-AreEqual Cost $budget.Category
+	Assert-AreEqual Monthly $budget.TimeGrain
+}
+
+<#
+.SYNOPSIS
+Get budgets
+#>
+function Test-GetBudgets
+{
+    $budgets = Get-AzureRmConsumptionBudget 
+	Assert-NotNull $budgets
+}
+
+<#
+.SYNOPSIS
+Get budgets at resource group level
+#>
+function Test-GetBudgetsAtResourceGroupLevel
+{
+    $budgets = Get-AzureRmConsumptionBudget -ResourceGroupName RGBudgets
+	Assert-NotNull $budgets
+}
+
+<#
+.SYNOPSIS
+Get budget by name
+#>
+function Test-GetBudgetByName
+{
+    $budget = Get-AzureRmConsumptionBudget -Name PSBudget
+	Assert-NotNull $budget
+	Assert-AreEqual PSBudget $budget.Name
+	Assert-NotNull $budget.Amount
+	Assert-NotNull $budget.Category
+	Assert-NotNull $budget.TimeGrain
+}
+
+<#
+.SYNOPSIS
+Get budget by name at resource group level
+#>
+function Test-GetBudgetByNameAtResourceGroupLevel
+{
+    $budget = Get-AzureRmConsumptionBudget -ResourceGroupName RGBudgets -Name PSBudgetRG 
+	Assert-NotNull $budget
+	Assert-AreEqual PSBudgetRG $budget.Name
+	Assert-NotNull $budget.Amount
+	Assert-NotNull $budget.Category
+	Assert-NotNull $budget.TimeGrain
+}
+
+<#
+.SYNOPSIS
+Set budget
+#>
+function Test-SetBudget
+{
+    $budget = Set-AzureRmConsumptionBudget -Name PSBudget -Amount 75
+	Assert-NotNull $budget
+	Assert-AreEqual PSBudget $budget.Name
+	Assert-AreEqual 75 $budget.Amount
+	Assert-NotNull $budget.Category
+	Assert-NotNull $budget.TimeGrain
+}
+
+<#
+.SYNOPSIS
+Set budget at resource group level
+#>
+function Test-SetBudgetAtResourceGroupLevel
+{
+    $budget = Set-AzureRmConsumptionBudget -ResourceGroupName RGBudgets -Name PSBudgetRG -Amount 75
+	Assert-NotNull $budget
+	Assert-AreEqual PSBudgetRG $budget.Name
+	Assert-AreEqual 75 $budget.Amount
+	Assert-NotNull $budget.Category
+	Assert-NotNull $budget.TimeGrain
+}
+
+<#
+.SYNOPSIS
+Remove budget
+#>
+function Test-RemoveBudget
+{
+	Remove-AzureRmConsumptionBudget -Name PSBudget
+}
+
+<#
+.SYNOPSIS
+Remove budget at resource group level
+#>
+function Test-RemoveBudgetAtResourceGroupLevel
+{
+	Remove-AzureRmConsumptionBudget -ResourceGroupName RGBudgets -Name PSBudgetRG
+}
