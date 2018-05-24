6d832a0b91118d6bbbe6cf2c42e07b2ff279eb8b
ikanni <ikanni@IKANNIVS2013>
2018-05-23 22:27:32 -0700

    Updated a file to trigger build

--- src/ResourceManager/Automation/Commands.Automation.Test/ScenarioTests/Resources/Test-WorkFlowWithVariousParameters.ps1
+++ src/ResourceManager/Automation/Commands.Automation.Test/ScenarioTests/Resources/Test-WorkFlowWithVariousParameters.ps1
@@ -1,6 +1,6 @@
 workflow Test-WorkFlowWithVariousParameters
 {
-	param([string] $a, [int] $b, [DateTime] $c)
+    param([string] $a, [int] $b, [DateTime] $c)
             "a  is " + $a 
             "b  is " + $b 
             "c  is " + $c 
