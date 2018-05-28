4693f636674c555b3efa1b919689c9221ffba0df
yaakoviyun <yaiyun@microsoft.com>
2018-05-28 11:25:42 +0300

    Added validation the VA policy is defined

--- src/ResourceManager/Sql/Commands.Sql/Properties/Resources.Designer.cs
+++ src/ResourceManager/Sql/Commands.Sql/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Microsoft.Azure.Commands.Sql.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -669,6 +669,15 @@ namespace Microsoft.Azure.Commands.Sql.Properties {
         internal static string RemoveDatabaseDataMaskingRuleWarning {
             get {
                 return ResourceManager.GetString("RemoveDatabaseDataMaskingRuleWarning", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Vulnerability assessment policy is not defined. Please run Set-AzureRmSqlDatabaseVulnerabilityAssessmentPolicy first..
+        /// </summary>
+        internal static string RunSetVulnerabilityAssementPolicy {
+            get {
+                return ResourceManager.GetString("RunSetVulnerabilityAssementPolicy", resourceCulture);
             }
         }
         
