ebda6dc3106810fd7b45e3585b63cb13d6775ed9
yaakoviyun <yaiyun@microsoft.com>
2018-05-29 17:08:28 +0300

    Adding some tests - part 1

---
+++ src/ResourceManager/Sql/Commands.Sql.Test/ScenarioTests/AdvancedThreatProtectionTests.ps1
@@ -0,0 +1,73 @@
+﻿# ----------------------------------------------------------------------------------
+#
+# Copyright Microsoft Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------------
+
+<#
+.SYNOPSIS
+Tests the Advanced Threat Protection Policy cmdlet
+#>
+function Test-AdvancedThreatProtectionPolicyTest
+{
+	# Setup
+	$testSuffix = getAssetName
+	Create-AdvancedThreatProtectionTestEnvironment $testSuffix
+	$params = Get-SqlAdvancedThreatProtectionTestEnvironmentParameters $testSuffix
+
+	try
+	{
+		$atpOffErrorMessage = "Server Advanced Threat Protection is not defined. Please run Set-AzureRmSqlServerAdvancedThreatProtectionPolicy first."
+		$ruleId = "VA2031"
+		$scanId = "myCustomScanId"
+		$baselineResults = @(@("userA", "SELECT"),@("userB", "SELECT"))
+
+
+	}
+	finally
+	{
+		# Cleanup
+		Remove-AdvancedThreatProtectionTestEnvironment $testSuffix
+	}
+}
+
+
+<#
+.SYNOPSIS
+Creates the test environment needed to perform the tests
+#>
+function Create-AdvancedThreatProtectionTestEnvironment ($testSuffix, $location = "West Central US", $serverVersion = "12.0")
+{
+	$params = Get-SqlAdvancedThreatProtectionTestEnvironmentParameters $testSuffix
+	Create-BasicTestEnvironmentWithParams $params $location $serverVersion
+}
+
+<#
+.SYNOPSIS
+Gets the values of the parameters used at the tests
+#>
+function Get-SqlAdvancedThreatProtectionTestEnvironmentParameters ($testSuffix)
+{
+	return @{ rgname = "sql-atp-cmdlet-test-rg" +$testSuffix;
+			  serverName = "sql-atp-cmdlet-server" +$testSuffix;
+			  databaseName = "sql-atp-cmdlet-db" + $testSuffix;
+		}
+}
+
+<#
+.SYNOPSIS
+Removes the test environment that was needed to perform the tests
+#>
+function Remove-AdvancedThreatProtectionTestEnvironment ($testSuffix)
+{
+	$params = Get-SqlAdvancedThreatProtectionTestEnvironmentParameters $testSuffix
+	Remove-AzureRmResourceGroup -Name $params.rgname -Force
+}
