4d46ffccf441f1a69a058d8e7f3546293d3a40ca
REDMOND\juhle <juhle@microsoft.com>
2018-06-18 10:38:48 -0700

    removing unused linese

--- src/ResourceManager/Reservations/Commands.Reservations/Custom/PSReservation.cs
+++ src/ResourceManager/Reservations/Commands.Reservations/Custom/PSReservation.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.Azure.Management.Reservations.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Microsoft.Azure.Commands.Reservations.Models
 {
