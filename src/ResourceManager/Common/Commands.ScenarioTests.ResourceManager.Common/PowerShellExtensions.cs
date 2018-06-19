e0351e79a80ab50b06ac93230b877db437ada9ba
maddieclayton <maclayto@microsoft.com>
2018-06-19 10:11:40 -0700

    move logging to errors only

--- src/ResourceManager/Common/Commands.ScenarioTests.ResourceManager.Common/PowerShellExtensions.cs
+++ src/ResourceManager/Common/Commands.ScenarioTests.ResourceManager.Common/PowerShellExtensions.cs
@@ -146,6 +146,11 @@ namespace Microsoft.WindowsAzure.Commands.ScenarioTest
             Collection<PSObject> output,
             XunitTracingInterceptor xunitLogger)
         {
+            if (output != null)
+            {
+                LogPowerShellStream<PSObject>(xunitLogger, output, "OUTPUT");
+            }
+
             if (xunitLogger != null &&
                 powershell.Commands != null &&
                 powershell.Commands.Commands != null &&
