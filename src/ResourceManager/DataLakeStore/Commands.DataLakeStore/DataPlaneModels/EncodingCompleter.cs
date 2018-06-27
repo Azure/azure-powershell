f25e247ab3a0b47b3d56b7289b69927955d09077
Rahul Dutta <rahuldutta90@users.noreply.github.com>
2018-06-27 14:39:30 -0700

    Create the encodercompleter, still changes need to go

---
+++ src/ResourceManager/DataLakeStore/Commands.DataLakeStore/DataPlaneModels/EncodingCompleter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Commands.DataLakeStore.Models
+{
+    public class EncodingCompleterAttribute :ArgumentCompleterAttribute
+    {
+        public EncodingCompleterAttribute(params string[] resourceTypes) : base(CreateScriptBlock(resourceTypes))
+        {
+        }
+
+        public static string[] FindEncodings(string[] resourceTypes, int timeout)
+        {
+            return FindLocations(resourceTypes);
+        }
+
+        public static ScriptBlock CreateScriptBlock(string[] resourceTypes)
+        {
+            string scriptResourceTypeList = "'" + String.Join("' , '", resourceTypes) + "'";
+            string script = "param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)\n" +
+                            String.Format("$resourceTypes = {0}\n", scriptResourceTypeList) +
+                            "$locations = [Microsoft.Azure.Commands.DataLakeStore.Models.EncodingCompleterAttribute]::FindEncodings($resourceTypes)\n" +
+                            "$locations | Where-Object { $_ -Like \"'$wordToComplete*\" } | Sort-Object | ForEach-Object { [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_) }";
+            ScriptBlock scriptBlock = ScriptBlock.Create(script);
+            return scriptBlock;
+        }
+    }
+}
