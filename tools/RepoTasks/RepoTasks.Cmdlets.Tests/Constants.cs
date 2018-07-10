e20b0b8365fddcfc9930d24b120543945865e47b
MiYanni <miyanni@microsoft.com>
2018-07-10 09:32:16 -0700

    Cleaned out the repo of unused files/projects. Moved over the local packages to the appropriate folder.

--- tools/RepoTasks/RepoTasks.Cmdlets.Tests/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RepoTasks.Cmdlets.Tests
-{
-    class Category
-    {
-        public const string AcceptanceType = "AcceptanceType";
-        public const string CheckIn = "CheckIn";
-    }
-}
