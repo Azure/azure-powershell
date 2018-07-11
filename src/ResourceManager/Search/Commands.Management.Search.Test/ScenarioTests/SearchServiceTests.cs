d5c1e678d1213ee3ce156a3764ad53d42cead30a
felixwa <felixwa@microsoft.com>
2018-07-10 19:16:58 -0700

    The first commit
    The first commit

---
+++ src/ResourceManager/Search/Commands.Management.Search.Test/ScenarioTests/SearchServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Azure.Commands.ScenarioTest;
+using Microsoft.Azure.ServiceManagemenet.Common.Models;
+using Microsoft.WindowsAzure.Commands.ScenarioTest;
+using Microsoft.WindowsAzure.Commands.Test.Utilities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.Azure.Commands.Management.Search.Test.ScenarioTests
+{
+    public class SearchServiceTests : RMTestBase
+    {
+        XunitTracingInterceptor traceInterceptor;
+
+        public SearchServiceTests(ITestOutputHelper output)
+        {
+            this.traceInterceptor = new XunitTracingInterceptor(output);
+            XunitTracingInterceptor.AddToContext(this.traceInterceptor);
+            TestExecutionHelpers.SetUpSessionAndProfile();
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestNewSearchService()
+        {
+            TestController.NewInstance.RunPsTest("Test-NewAzureRmSearchService");
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestGetSearchService()
+        {
+            TestController.NewInstance.RunPsTest("Test-GetAzureRmSearchService");
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestRemoveSearchService()
+        {
+            TestController.NewInstance.RunPsTest("Test-RemoveAzureRmSearchService");
+        }
+    }
+}
