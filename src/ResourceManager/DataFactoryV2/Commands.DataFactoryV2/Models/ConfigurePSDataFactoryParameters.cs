ee44d7b46c89a18c55befc13649ac02d217b4adf
Yaser Alkayale <Yaser.Alkayale@microsoft.com>
2018-07-12 17:04:09 -0700

    Added support for Set-AzureRMDataFactoryV2RepoConfiguration. Project does not build, testing still needed

---
+++ src/ResourceManager/DataFactoryV2/Commands.DataFactoryV2/Models/ConfigurePSDataFactoryParameters.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.Management.DataFactory.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections;
+
+namespace Microsoft.Azure.Commands.DataFactoryV2.Models
+{
+    public class ConfigurePSDataFactoryParameters : DataFactoryParametersBase
+    {
+        public string FactoryResourceId { get; set; }
+        public string LocationId { get; set; }
+        public FactoryRepoConfiguration Repo { get; set; }
+
+        //TODO do we need tags or not.
+        public Hashtable Tags { get; set; }
+    }
+}
