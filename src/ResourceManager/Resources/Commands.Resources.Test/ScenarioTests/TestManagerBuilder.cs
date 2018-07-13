514785c44e6376b5a66d29a56d9a75785ab1730b
vlashch <vlashch@microsoft.com>
2018-07-12 19:10:08 -0700

    travis-ci build fix

--- src/ResourceManager/Resources/Commands.Resources.Test/ScenarioTests/TestManagerBuilder.cs
+++ src/ResourceManager/Resources/Commands.Resources.Test/ScenarioTests/TestManagerBuilder.cs
@@ -1,4 +1,19 @@
-﻿using System.Collections.Generic;
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -10,7 +25,6 @@ using Microsoft.Azure.Commands.TestFw;
 using Microsoft.Azure.Test.HttpRecorder;
 using Microsoft.Rest;
 using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
-using Microsoft.WindowsAzure.Commands.ScenarioTest;
 using Xunit.Abstractions;
 
 namespace Microsoft.Azure.Commands.Resources.Test.ScenarioTests
@@ -43,10 +57,13 @@ namespace Microsoft.Azure.Commands.Resources.Test.ScenarioTests
                 .Build();
 
             var testEnvironment = TestEnvironmentFactory.GetTestEnvironment();
-
-            var credentials = new SubscriptionCloudCredentialsAdapter(
-                testEnvironment.TokenInfo[TokenAudience.Management],
-                testEnvironment.SubscriptionId);
+            var credentials = HttpMockServer.Mode == HttpRecorderMode.Record
+                ? new SubscriptionCloudCredentialsAdapter(
+                    testEnvironment.TokenInfo[TokenAudience.Management],
+                    testEnvironment.SubscriptionId)
+                : new SubscriptionCloudCredentialsAdapter(
+                    new TokenCredentials("foo"),
+                    Guid.Empty.ToString());
 
             HttpClientHelperFactory.Instance = new TestHttpClientHelperFactory(credentials);
         }
