8fc2f67df7af0babfa2d4ba2838ab81d619d3966
Cormac McCarthy <corm@microsoft.com>
2018-07-17 10:45:12 -0700

    Update module versions for .NET Core release

--- tools/NetCorePsd1Sync/NetCorePsd1Sync/Program.cs
+++ tools/NetCorePsd1Sync/NetCorePsd1Sync/Program.cs
@@ -63,9 +63,13 @@ namespace NetCorePsd1Sync
         private static void UpdateModuleVersions(string rmPath, Version newVersion)
         {
             var modulePaths = GetModulePaths(rmPath, true);
+            foreach (var path in modulePaths)
+            {
+                Console.WriteLine(path);
+            }
             var desktopFilePaths = GetDesktopFilePaths(modulePaths);
             var netCoreFilePaths = desktopFilePaths.Select(ConvertDesktopToNetCorePath).Where(File.Exists).ToList();
-            netCoreFilePaths.Add(Path.Combine(rmPath, @"..\Storage\Azure.Storage.Netcore.psd1"));
+            netCoreFilePaths.Add(Path.Combine(rmPath, @"..\Storage\Commands.Storage\Azure.Storage.Netcore.psd1"));
             netCoreFilePaths.Add(Path.Combine(rmPath, @"..\..\tools\AzureRM.Netcore\AzureRM.Netcore.psd1"));
             netCoreFilePaths.Add(Path.Combine(rmPath, @"AnalysisServices\Commands.AnalysisServices.Dataplane\Azure.AnalysisServices.Netcore.psd1"));
             var netCoreHashTables = GetHashtables(netCoreFilePaths);
