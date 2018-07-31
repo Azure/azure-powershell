ef7841de222fcf105f0d313e393a29b397afb6a9
cormacpayne <corm@microsoft.com>
2018-07-31 16:27:11 -0700

    Allow users to provide tenant domain to Connect-AzureRmAccount

--- src/ResourceManager/Profile/Commands.Profile/Models/RMProfileClient.cs
+++ src/ResourceManager/Profile/Commands.Profile/Models/RMProfileClient.cs
@@ -147,6 +147,23 @@ namespace Microsoft.Azure.Commands.ResourceManager.Common
                 // (tenant is present and subscription is not provided)
                 if (!string.IsNullOrEmpty(tenantId))
                 {
+                    Guid tempGuid = Guid.Empty;
+                    if (!Guid.TryParse(tenantId, out tempGuid))
+                    {
+                        var tenant = ListAccountTenants(
+                            account,
+                            environment,
+                            password,
+                            promptBehavior,
+                            promptAction)?.FirstOrDefault();
+                        if (tenant == null)
+                        {
+                            throw new ArgumentNullException(string.Format("Could not find tenant id for provided tenant domain '{0}'.", tenantId));
+                        }
+
+                        tenantId = tenant.Id;
+                    }
+
                     var token = AcquireAccessToken(
                         account,
                         environment,
