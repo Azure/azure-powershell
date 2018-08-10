c0bc4595fcec770077560a1e8cf689c048c3e5d3
markcowl <markcowl@microsoft.com>
2018-08-09 19:25:48 -0700

    Adding necessary target dependencies and cleaning up unnecessary ifdefs

--- src/ResourceManager/Automation/Commands.Automation/Cmdlet/ExportAzureAutomationDscNodeReportContent.cs
+++ src/ResourceManager/Automation/Commands.Automation/Cmdlet/ExportAzureAutomationDscNodeReportContent.cs
@@ -65,9 +65,7 @@ namespace Microsoft.Azure.Commands.Automation.Cmdlet
         /// <summary>
         /// Execute this cmdlet.
         /// </summary>
-#if !NETSTANDARD
         [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
-#endif
         public override void ExecuteCmdlet()
         {
             if (ShouldProcess(ReportId.ToString(), VerbsData.Export))
