fa2fd4efef1c7d4543438da59cee6c4cf0145d57
markcowl <markcowl@microsoft.com>
2018-08-13 16:26:56 -0700

    Respond to review comments

--- src/ResourceManager/ContainerInstance/Commands.ContainerInstance/Commands/NewAzureContainerGroupCommand.cs
+++ src/ResourceManager/ContainerInstance/Commands.ContainerInstance/Commands/NewAzureContainerGroupCommand.cs
@@ -12,7 +12,6 @@
 // limitations under the License.
 // ----------------------------------------------------------------------------------
 
-#if !NETSTANDARD
 using System;
 using System.Collections;
 using System.Collections.ObjectModel;
@@ -150,11 +149,13 @@ namespace Microsoft.Azure.Commands.ContainerInstance
         [ValidateNotNullOrEmpty]
         public int[] Port { get; set; }
 
-        [Parameter(
+ #if !NETSTANDARD
+       [Parameter(
             Mandatory = false,
             HelpMessage = "The command to run in the container.")]
         [ValidateNotNullOrEmpty]
         public string Command { get; set; }
+#endif
 
         [Parameter(
             Mandatory = false,
@@ -201,6 +202,7 @@ namespace Microsoft.Azure.Commands.ContainerInstance
                     AzureFileVolumeAccountKey = ContainerGroupCreationParameters.ConvertToString(this.AzureFileVolumeAccountCredential?.Password),
                     AzureFileVolumeMountPath = this.AzureFileVolumeMountPath
                 };
+#if !NETSTANDARD
 
                 if (!string.IsNullOrWhiteSpace(this.Command))
                 {
@@ -212,7 +214,7 @@ namespace Microsoft.Azure.Commands.ContainerInstance
                     }
                     creationParameter.ContainerCommand = commandTokens.Select(token => token.Content).ToList();
                 }
-
+#endif
                 creationParameter.Validate();
 
                 var psContainerGroup = PSContainerGroup.FromContainerGroup(
@@ -223,5 +225,3 @@ namespace Microsoft.Azure.Commands.ContainerInstance
         }
     }
 }
-
-#endif
