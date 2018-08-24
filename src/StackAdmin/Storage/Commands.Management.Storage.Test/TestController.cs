1bcf7259c7d9c740c858699a04da701b5debb88d
Bala Ganapathy <bganapa@microsoft.com>
2018-08-24 14:15:39 -0700

    AzureRm.Storage module consuming api-version 2016-01-01 and common runtime

---
+++ src/StackAdmin/Storage/Commands.Management.Storage.Test/TestController.cs
@@ -0,0 +1,173 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Azure.Commands.Common.Authentication;
+using Microsoft.Azure.Gallery;
+using Microsoft.Azure.Management.Authorization;
+using Microsoft.Azure.Management.Storage;
+using Microsoft.Azure.Test;
+using Microsoft.Azure.Test.HttpRecorder;
+using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+using Microsoft.WindowsAzure.Commands.ScenarioTest;
+using TestBase = Microsoft.Azure.Test.TestBase;
+using TestUtilities = Microsoft.Azure.Test.TestUtilities;
+
+namespace Microsoft.Azure.Commands.Management.Storage.Test.ScenarioTests
+{
+    public class TestController
+    {
+        private CSMTestEnvironmentFactory csmTestFactory;
+        private EnvironmentSetupHelper helper;
+        
+        public Microsoft.Azure.Management.ResourceManager.ResourceManagementClient LegacyResourceManagementClient { get; private set; }
+
+        public AuthorizationManagementClient AuthorizationManagementClient { get; private set; }
+
+        public StorageManagementClient StorageClient { get; private set; }
+
+        public GalleryClient GalleryClient { get; private set; }
+
+        public static TestController NewInstance 
+        { 
+            get
+            {
+                return new TestController();
+            }
+        }
+
+        public TestController()
+        {
+            helper = new EnvironmentSetupHelper();
+        }
+
+        public void RunPsTest(params string[] scripts)
+        {
+            var callingClassType = TestUtilities.GetCallingClass(2);
+            var mockName = TestUtilities.GetCurrentMethodName(2);
+
+            RunPsTestWorkflow(
+                () => scripts, 
+                // no custom initializer
+                null, 
+                // no custom cleanup 
+                null,
+                callingClassType,
+                mockName);
+        }
+
+        public void RunPsTestWorkflow(
+            Func<string[]> scriptBuilder, 
+            Action<CSMTestEnvironmentFactory> initialize, 
+            Action cleanup,
+            string callingClassType,
+            string mockName)
+        {
+            Dictionary<string, string> d = new Dictionary<string, string>();
+            d.Add("Microsoft.Resources", null);
+            d.Add("Microsoft.Features", null);
+            d.Add("Microsoft.Authorization", null);
+            d.Add("Microsoft.Compute", null);
+            var providersToIgnore = new Dictionary<string, string>();
+            providersToIgnore.Add("Microsoft.Azure.Management.Resources.ResourceManagementClient", "2016-02-01");
+            providersToIgnore.Add("Microsoft.Azure.Management.ResourceManager.ResourceManagementClient", "2016-02-01");
+            providersToIgnore.Add("Microsoft.Azure.Management.Storage.StorageManagementClient", "2016-01-01");
+
+            HttpMockServer.Matcher = new PermissiveRecordMatcherWithApiExclusion(true, d, providersToIgnore);
+
+            using (MockContext context = MockContext.Start(callingClassType, mockName))
+            {
+
+                this.csmTestFactory = new CSMTestEnvironmentFactory();
+
+                if(initialize != null)
+                {
+                    initialize(this.csmTestFactory);
+                }
+
+                SetupManagementClients(context);
+
+                helper.SetupEnvironment(AzureModule.AzureResourceManager);
+                
+                var callingClassName = callingClassType
+                                        .Split(new[] { "." }, StringSplitOptions.RemoveEmptyEntries)
+                                        .Last();
+                helper.SetupModules(AzureModule.AzureResourceManager, 
+                    helper.StackRMProfileModule, 
+                    helper.StackRMResourceModule, 
+                    helper.StackRMStorageDataPlaneModule,
+                    helper.StackRMStorageModule,
+                    "ScenarioTests\\Common.ps1", 
+                    "ScenarioTests\\" + callingClassName + ".ps1");
+
+                try
+                {
+                    if (scriptBuilder != null)
+                    {
+                        var psScripts = scriptBuilder();
+
+                        if (psScripts != null)
+                        {
+                            helper.RunPowerShellTest(psScripts);
+                        }
+                    }
+                }
+                finally
+                {
+                    if(cleanup !=null)
+                    {
+                        cleanup();
+                    }
+                }
+            }
+        }
+
+        private void SetupManagementClients(MockContext context)
+        {
+            LegacyResourceManagementClient = GetLegacyResourceManagementClient(context);
+            StorageClient = GetStorageManagementClient(context);
+            GalleryClient = GetGalleryClient();
+            AuthorizationManagementClient = GetAuthorizationManagementClient();
+
+            helper.SetupManagementClients(
+                LegacyResourceManagementClient,
+                StorageClient,
+                GalleryClient,
+                AuthorizationManagementClient);
+        }
+        
+        private Microsoft.Azure.Management.ResourceManager.ResourceManagementClient GetLegacyResourceManagementClient(MockContext context)
+        {
+            return context.GetServiceClient<Microsoft.Azure.Management.ResourceManager.ResourceManagementClient>(Microsoft.Rest.ClientRuntime.Azure.TestFramework.TestEnvironmentFactory.GetTestEnvironment());
+        }
+
+        private AuthorizationManagementClient GetAuthorizationManagementClient()
+        {
+            return TestBase.GetServiceClient<AuthorizationManagementClient>(this.csmTestFactory);
+        }
+
+        private GalleryClient GetGalleryClient()
+        {
+            return TestBase.GetServiceClient<GalleryClient>(this.csmTestFactory);
+        }
+
+        private StorageManagementClient GetStorageManagementClient(MockContext context)
+        {
+            //return TestBase.GetServiceClient<StorageManagementClient>(this.csmTestFactory);
+            return context.GetServiceClient<StorageManagementClient>(Microsoft.Rest.ClientRuntime.Azure.TestFramework.TestEnvironmentFactory.GetTestEnvironment());
+        }
+    }
+}
