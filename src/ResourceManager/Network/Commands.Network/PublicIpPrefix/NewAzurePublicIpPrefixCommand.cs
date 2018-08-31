cd82507675da90787f94db6ebf40ca880c83ae2c
Anton Evseev <nmbr213@gmail.com>
2018-08-31 22:54:02 +0300

    Addressed PR feedback

--- src/ResourceManager/Network/Commands.Network/PublicIpPrefix/NewAzurePublicIpPrefixCommand.cs
+++ src/ResourceManager/Network/Commands.Network/PublicIpPrefix/NewAzurePublicIpPrefixCommand.cs
@@ -109,7 +109,6 @@ namespace Microsoft.Azure.Commands.Network
         public override void Execute()
         {
             base.Execute();
-            WriteWarning("The output object type of this cmdlet will be modified in a future release.");
             var present = this.IsPublicIpPrefixPresent(this.ResourceGroupName, this.Name);
             ConfirmAction(
                 Force.IsPresent,
