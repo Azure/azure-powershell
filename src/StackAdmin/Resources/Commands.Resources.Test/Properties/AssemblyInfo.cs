3100dacb0af7a3e02843124ef24a5cce39ced5d2
Bala Ganapathy <bganapa@microsoft.com>
2018-08-31 15:59:56 -0700

    removing .net core projects, updating assmebly versions, addressing review comments

--- src/StackAdmin/Resources/Commands.Resources.Test/Properties/AssemblyInfo.cs
+++ src/StackAdmin/Resources/Commands.Resources.Test/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using Xunit;
 
 [assembly: Guid("28fe3fc0-6fdf-4f01-8a82-d7b4a834ddd2")]
 
-[assembly: AssemblyVersion("5.0.0")]
-[assembly: AssemblyFileVersion("5.0.0")]
+[assembly: AssemblyVersion("6.0.2")]
+[assembly: AssemblyFileVersion("6.0.2")]
 [assembly: CollectionBehavior(DisableTestParallelization = true)]
