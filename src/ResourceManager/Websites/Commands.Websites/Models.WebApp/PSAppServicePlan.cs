60c65c5fc88ed1a888e2c9b566b868544a7f7b2f
Sisira Panchagnula <sisirap@microsoft.com>
2018-09-06 14:29:39 -0700

    Adding client Runtime back so that the latest version is always used

--- src/ResourceManager/Websites/Commands.Websites/Models.WebApp/PSAppServicePlan.cs
+++ src/ResourceManager/Websites/Commands.Websites/Models.WebApp/PSAppServicePlan.cs
@@ -1,14 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Azure.Management.WebSites.Models;
+using System;
 
 namespace Microsoft.Azure.Commands.WebApps.Models.WebApp
 {
-    class PSAppServicePlan
+    class PSAppServicePlan: AppServicePlan
     {
+        public PSAppServicePlan(AppServicePlan other): base(
+            location: other.Location,
+            name: other.Name,
+            id: other.Id,
+            kind: other.Kind,
+            type: other.Type,
+            tags: other.Tags,
+            workerTierName: other.WorkerTierName,
+            status: other.Status,
+            subscription: other.Subscription,
+            adminSiteName: other.AdminSiteName,
+            hostingEnvironmentProfile: other.HostingEnvironmentProfile,
+            maximumNumberOfWorkers: other.MaximumNumberOfWorkers,
+            geoRegion: other.GeoRegion,
+            perSiteScaling: other.PerSiteScaling,
+            numberOfSites: other.NumberOfSites,
+            isSpot: other.IsSpot,
+            spotExpirationTime: other.SpotExpirationTime,
+            freeOfferExpirationTime: other.FreeOfferExpirationTime,
+            resourceGroup: other.ResourceGroup,
+            reserved: other.Reserved,
+            isXenon: other.IsXenon,
+            targetWorkerCount: other.TargetWorkerCount,
+            targetWorkerSizeId: other.TargetWorkerSizeId,
+            provisioningState: other.ProvisioningState,
+            sku: other.Sku
+            )
+        {
+
+        }
+
         [Obsolete("This property is deprecated and will be removed in a future releases.")]
-        public string AppServicePlanName { get; set; }
+        public string AppServicePlanName { get { return Name; } }
     }
 }
