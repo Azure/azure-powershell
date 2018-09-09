f411c0e7ed6fce5d50ece2d062b9dc8eaf6c1b6a
Bala Ganapathy <bganapa@microsoft.com>
2018-09-08 22:53:32 -0700

    loading custom classes with using module statements

--- src/StackAdmin/Azs.Compute.Admin/Module/Azs.Compute.Admin/Generated.PowerShell.Commands/CustomObjects/PlatformImageObject.psm1
+++ src/StackAdmin/Azs.Compute.Admin/Module/Azs.Compute.Admin/Generated.PowerShell.Commands/CustomObjects/PlatformImageObject.psm1
@@ -1,4 +1,9 @@
 
+<#
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the MIT License. See License.txt in the project root for license information.
+#>
+
 class PlatformImageObject {
     # Resource properties
     [string]$Id;
