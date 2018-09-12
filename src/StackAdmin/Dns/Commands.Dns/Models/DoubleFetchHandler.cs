0f3b21de198fc1d2dc22d7b0e339f5f1b21feb00
Jeffrey A. Robinson <jeffrey.robinson@microsoft.com>
2018-09-12 13:01:11 -0700

    Handle DNS Double Async Headers

---
+++ src/StackAdmin/Dns/Commands.Dns/Models/DoubleFetchHandler.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.Commands.Dns.Models
+{
+    public class DoubleFetchHandler : DelegatingHandler, ICloneable
+    {
+        public object Clone() {
+            return new DoubleFetchHandler();
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) {
+
+            return base.SendAsync(request, cancellationToken).ContinueWith<HttpResponseMessage>(response =>
+            {
+                var result = response.Result;
+                    if (result.Headers.Contains("Location") && result.Headers.Contains("Azure-AsyncOperation"))
+                    {
+                        result.Headers.Remove("Location");
+                    }
+
+                return result;
+            });
+        }
+
+    }
+}
