8acb9488fd6622118276df3420147bfd8883c397
MiYanni <miyanni@microsoft.com>
2018-09-24 16:44:14 -0700

    MachineLearning tests added to NetStandard.

--- src/ResourceManager/MachineLearning/Commands.MachineLearning.Test/ScenarioTests/BaseTestController.cs
+++ src/ResourceManager/MachineLearning/Commands.MachineLearning.Test/ScenarioTests/BaseTestController.cs
@@ -15,19 +15,11 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using LegacyTest = Microsoft.Azure.Test;
 using Microsoft.Azure.Commands.Common.Authentication;
-using Microsoft.Azure.Commands.ResourceManager.Cmdlets.Components;
-using Microsoft.Azure.Commands.ResourceManager.Cmdlets.Extensions;
-using Microsoft.Azure.Gallery;
-using Microsoft.Azure.Management.Authorization;
 using Microsoft.Azure.Management.Storage;
 using Microsoft.Azure.ServiceManagemenet.Common.Models;
-using Microsoft.Azure.Subscriptions;
-using Microsoft.Azure.Test.Authentication;
 using Microsoft.Azure.Test.HttpRecorder;
 using Microsoft.Rest;
 using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
@@ -38,29 +30,25 @@ namespace Microsoft.Azure.Commands.MachineLearning.Test.ScenarioTests
 {
     public abstract class BaseTestController<T> where T : ServiceClient<T>
     {
-        private readonly EnvironmentSetupHelper helper;
-        private LegacyTest.CSMTestEnvironmentFactory csmTestFactory;
-        private T serviceClient;
-        private ResourceManagementClient resourceManagementClient;
-        private StorageManagementClient storageManagementClient;
+        private readonly EnvironmentSetupHelper _helper;
+        private T _serviceClient;
 
         protected BaseTestController()
         {
-            this.helper = new EnvironmentSetupHelper();
+            _helper = new EnvironmentSetupHelper();
         }
 
         protected abstract T ConstructServiceClient(MockContext context);
 
         public void RunPsTest(XunitTracingInterceptor logger, params string[] scripts)
         {
-            var callingClassType = TestUtilities.GetCallingClass(2);
-            var mockName = TestUtilities.GetCurrentMethodName(2);
-            this.helper.TracingInterceptor = logger;
+            var sf = new StackTrace().GetFrame(1);
+            var callingClassType = sf.GetMethod().ReflectedType?.ToString();
+            var mockName = sf.GetMethod().Name;
+            _helper.TracingInterceptor = logger;
 
-            this.RunPsTestWorkflow(
+            RunPsTestWorkflow(
                 () => scripts,
-                // no custom initializer
-                null,
                 // no custom cleanup
                 null,
                 callingClassType,
@@ -69,7 +57,6 @@ namespace Microsoft.Azure.Commands.MachineLearning.Test.ScenarioTests
 
         public void RunPsTestWorkflow(
             Func<string[]> scriptBuilder,
-            Action<LegacyTest.CSMTestEnvironmentFactory> initialize,
             Action cleanup,
             string callingClassType,
             string mockName)
@@ -89,145 +76,46 @@ namespace Microsoft.Azure.Commands.MachineLearning.Test.ScenarioTests
             HttpMockServer.RecordsDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "SessionRecords");
             using (var context = MockContext.Start(callingClassType, mockName))
             {
-                this.csmTestFactory = new LegacyTest.CSMTestEnvironmentFactory();
-                if (initialize != null)
-                {
-                    initialize(this.csmTestFactory);
-                }
+                SetupManagementClients(context);
+                _helper.SetupEnvironment(AzureModule.AzureResourceManager);
 
-                this.SetupManagementClients(context);
-                helper.SetupEnvironment(AzureModule.AzureResourceManager);
+                var callingClassName = callingClassType.Split(new[] { "." }, StringSplitOptions.RemoveEmptyEntries).Last();
 
-                var callingClassName = callingClassType
-                                      .Split(new[] { "." }, StringSplitOptions.RemoveEmptyEntries)
-                                      .Last();
-
-                helper.SetupModules(AzureModule.AzureResourceManager,
+                _helper.SetupModules(AzureModule.AzureResourceManager,
                    "ScenarioTests\\Common.ps1",
                    "ScenarioTests\\" + callingClassName + ".ps1",
-                   helper.RMProfileModule,
-                   helper.RMResourceModule,
-                   helper.GetRMModulePath(@"AzureRM.MachineLearning.psd1"),
+                   _helper.RMProfileModule,
+                   _helper.RMResourceModule,
+                   _helper.GetRMModulePath(@"AzureRM.MachineLearning.psd1"),
                    "AzureRM.Storage.ps1",
                    "AzureRM.Resources.ps1");
 
                 try
                 {
-                    if (scriptBuilder != null)
+                    var psScripts = scriptBuilder?.Invoke();
+                    if (psScripts != null)
                     {
-                        var psScripts = scriptBuilder();
-
-                        if (psScripts != null)
-                        {
-                            helper.RunPowerShellTest(psScripts);
-                        }
+                        _helper.RunPowerShellTest(psScripts);
                     }
                 }
                 finally
                 {
-                    if (cleanup != null)
-                    {
-                        cleanup();
-                    }
+                    cleanup?.Invoke();
                 }
             }
         }
 
         private void SetupManagementClients(MockContext context)
         {
-            this.serviceClient = this.ConstructServiceClient(context);
+            _serviceClient = ConstructServiceClient(context);
 
-            this.resourceManagementClient = context.GetServiceClient<ResourceManagementClient>(TestEnvironmentFactory.GetTestEnvironment());
-            this.storageManagementClient = LegacyTest.TestBase.GetServiceClient<StorageManagementClient>(this.csmTestFactory);
-            var subscriptionClient = LegacyTest.TestBase.GetServiceClient<SubscriptionClient>(this.csmTestFactory);
-            var authManagementClient = LegacyTest.TestBase.GetServiceClient<AuthorizationManagementClient>(this.csmTestFactory);
-            var gallleryClient = LegacyTest.TestBase.GetServiceClient<GalleryClient>(this.csmTestFactory);
+            var resourceManagementClient = context.GetServiceClient<ResourceManagementClient>(TestEnvironmentFactory.GetTestEnvironment());
+            var storageManagementClient = context.GetServiceClient<StorageManagementClient>(TestEnvironmentFactory.GetTestEnvironment());
 
-            var testEnvironment = this.csmTestFactory.GetTestEnvironment();
-
-            var credentials = new SubscriptionCredentialsAdapter(
-                testEnvironment.AuthorizationContext.TokenCredentials[Microsoft.Azure.Test.TokenAudience.Management],
-                testEnvironment.SubscriptionId);
-
-            HttpClientHelperFactory.Instance = new TestHttpClientHelperFactory(credentials);
-
-            helper.SetupManagementClients(
-                this.resourceManagementClient,
-                subscriptionClient,
-                this.serviceClient,
-                authManagementClient,
-                gallleryClient,
-                this.storageManagementClient);
-        }
-
-        /// <summary>
-        /// The test http client helper factory.
-        /// </summary>
-        private class TestHttpClientHelperFactory : HttpClientHelperFactory
-        {
-            /// <summary>
-            /// The subscription cloud credentials.
-            /// </summary>
-            private readonly SubscriptionCloudCredentials credential;
-
-            /// <summary>
-            /// Initializes a new instance of the <see cref="TestHttpClientHelperFactory"/> class.
-            /// </summary>
-            /// <param name="credentials"></param>
-            public TestHttpClientHelperFactory(SubscriptionCloudCredentials credentials)
-            {
-                this.credential = credentials;
-            }
-
-            /// <summary>
-            /// Creates new instances of the <see cref="HttpClientHelper"/> class.
-            /// </summary>
-            /// <param name="credentials">The credentials.</param>
-            /// <param name="headerValues">The headers.</param>
-            public override HttpClientHelper CreateHttpClientHelper(
-                                                SubscriptionCloudCredentials credentials,
-                                                IEnumerable<ProductInfoHeaderValue> headerValues,
-                                                Dictionary<string, string> cmdletHeaderValues)
-            {
-                return new HttpClientHelperImpl(
-                                credentials: this.credential,
-                                headerValues: headerValues,
-                                cmdletHeaderValues: cmdletHeaderValues);
-            }
-
-            /// <summary>
-            /// An implementation of the <see cref="HttpClientHelper"/> abstract class.
-            /// </summary>
-            private class HttpClientHelperImpl : HttpClientHelper
-            {
-                /// <summary>
-                /// Initializes new instances of the <see cref="HttpClientHelperImpl"/> class.
-                /// </summary>
-                /// <param name="credentials">The credentials.</param>
-                /// <param name="headerValues">The headers.</param>
-                public HttpClientHelperImpl(
-                            SubscriptionCloudCredentials credentials,
-                            IEnumerable<ProductInfoHeaderValue> headerValues,
-                            Dictionary<string, string> cmdletHeaderValues)
-                    : base(
-                        credentials: credentials,
-                        headerValues: headerValues,
-                        cmdletHeaderValues: cmdletHeaderValues)
-                {
-                }
-
-                /// <summary>
-                /// Creates an <see cref="HttpClient"/>
-                /// </summary>
-                /// <param name="primaryHandlers">The handlers that will be added to the top of the chain.</param>
-                public override HttpClient CreateHttpClient(params DelegatingHandler[] primaryHandlers)
-                {
-                    return base.CreateHttpClient(HttpMockServer.CreateInstance()
-                                .AsArray()
-                                .Concat(primaryHandlers)
-                                .ToArray());
-                }
-            }
+            _helper.SetupManagementClients(
+                resourceManagementClient,
+                _serviceClient,
+                storageManagementClient);
         }
     }
 }
