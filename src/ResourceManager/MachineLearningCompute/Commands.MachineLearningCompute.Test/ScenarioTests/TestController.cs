0f128dfeb452dfc6bf4ec0bb17684b0ccac86ad5
MiYanni <miyanni@microsoft.com>
2018-09-24 17:46:02 -0700

    MachineLearningCompute tests running in Az.

--- src/ResourceManager/MachineLearningCompute/Commands.MachineLearningCompute.Test/ScenarioTests/TestController.cs
+++ src/ResourceManager/MachineLearningCompute/Commands.MachineLearningCompute.Test/ScenarioTests/TestController.cs
@@ -1,53 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.IO;
 using Microsoft.Azure.Commands.Common.Authentication;
-using Microsoft.Azure.Management.Resources;
 using Microsoft.Azure.Management.MachineLearningCompute;
 using Microsoft.Azure.ServiceManagemenet.Common.Models;
 using Microsoft.Azure.Test.HttpRecorder;
 using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
 using Microsoft.WindowsAzure.Commands.ScenarioTest;
-using LegacyTest = Microsoft.Azure.Test;
 using TestEnvironmentFactory = Microsoft.Rest.ClientRuntime.Azure.TestFramework.TestEnvironmentFactory;
-using TestUtilities = Microsoft.Rest.ClientRuntime.Azure.TestFramework.TestUtilities;
 using NewResourceManagementClient = Microsoft.Azure.Management.Internal.Resources.ResourceManagementClient;
 
 namespace Microsoft.Azure.Commands.MachineLearningCompute.Test.ScenarioTests
 {
     public class TestController
     {
-        private EnvironmentSetupHelper helper;
-        private LegacyTest.CSMTestEnvironmentFactory csmTestFactory;
-
-        public ResourceManagementClient ResourceManagementClient { get; private set; }
+        private readonly EnvironmentSetupHelper _helper;
         public MachineLearningComputeManagementClient MachineLearningComputeManagementClient { get; private set; }
         public NewResourceManagementClient NewResourceManagementClient { get; private set; }
 
-        public static TestController NewInstance
-        {
-            get
-            {
-                return new TestController();
-            }
-        }
+        public static TestController NewInstance => new TestController();
 
         public TestController()
         {
-            helper = new EnvironmentSetupHelper();
+            _helper = new EnvironmentSetupHelper();
         }
 
         public void RunPsTest(XunitTracingInterceptor logger, params string[] scripts)
         {
-            var callingClassType = TestUtilities.GetCallingClass(2);
-            var mockName = TestUtilities.GetCurrentMethodName(2);
-            this.helper.TracingInterceptor = logger;
+            var sf = new StackTrace().GetFrame(1);
+            var callingClassType = sf.GetMethod().ReflectedType?.ToString();
+            var mockName = sf.GetMethod().Name;
+            _helper.TracingInterceptor = logger;
 
             RunPsTestWorkflow(
                 () => scripts,
-                // no custom initializer
-                null,
                 // no custom cleanup
                 null,
                 callingClassType,
@@ -56,7 +44,6 @@ namespace Microsoft.Azure.Commands.MachineLearningCompute.Test.ScenarioTests
 
         public void RunPsTestWorkflow(
             Func<string[]> scriptBuilder,
-            Action<LegacyTest.CSMTestEnvironmentFactory> initialize,
             Action cleanup,
             string callingClassType,
             string mockName)
@@ -76,58 +63,40 @@ namespace Microsoft.Azure.Commands.MachineLearningCompute.Test.ScenarioTests
 
             using (var context = MockContext.Start(callingClassType, mockName))
             {
-                this.csmTestFactory = new LegacyTest.CSMTestEnvironmentFactory();
-                if (initialize != null)
-                {
-                    initialize(this.csmTestFactory);
-                }
-
-
                 SetupManagementClients(context);
-                helper.SetupEnvironment(AzureModule.AzureResourceManager);
+                _helper.SetupEnvironment(AzureModule.AzureResourceManager);
 
-                var callingClassName = callingClassType
-                                       .Split(new[] { "." }, StringSplitOptions.RemoveEmptyEntries)
-                                       .Last();
+                var callingClassName = callingClassType.Split(new[] { "." }, StringSplitOptions.RemoveEmptyEntries).Last();
 
-                helper.SetupModules(AzureModule.AzureResourceManager,
+                _helper.SetupModules(AzureModule.AzureResourceManager,
                     "ScenarioTests\\" + callingClassName + ".ps1",
-                    helper.RMProfileModule,
-                    helper.GetRMModulePath(@"AzureRM.MachineLearningCompute.psd1"),
+                    _helper.RMProfileModule,
+                    _helper.GetRMModulePath(@"AzureRM.MachineLearningCompute.psd1"),
                     "AzureRM.Resources.ps1");
 
                 try
                 {
-                    if (scriptBuilder != null)
+                    var psScripts = scriptBuilder?.Invoke();
+                    if (psScripts != null)
                     {
-                        var psScripts = scriptBuilder();
-
-                        if (psScripts != null)
-                        {
-                            helper.RunPowerShellTest(psScripts);
-                        }
+                        _helper.RunPowerShellTest(psScripts);
                     }
                 }
                 finally
                 {
-                    if (cleanup != null)
-                    {
-                        cleanup();
-                    }
+                    cleanup?.Invoke();
                 }
             }
         }
 
         private void SetupManagementClients(MockContext context)
         {
-            ResourceManagementClient = LegacyTest.TestBase.GetServiceClient<ResourceManagementClient>(this.csmTestFactory);
             NewResourceManagementClient = context.GetServiceClient<NewResourceManagementClient>(TestEnvironmentFactory.GetTestEnvironment());
             MachineLearningComputeManagementClient = context.GetServiceClient<MachineLearningComputeManagementClient>();
 
-            helper.SetupManagementClients(
-                this.ResourceManagementClient,
-                this.NewResourceManagementClient,
-                this.MachineLearningComputeManagementClient);
+            _helper.SetupManagementClients(
+                NewResourceManagementClient,
+                MachineLearningComputeManagementClient);
         }
     }
 }
