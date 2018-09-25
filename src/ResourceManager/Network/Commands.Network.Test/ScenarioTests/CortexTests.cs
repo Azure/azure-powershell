b9f675d4fdd786d23490185ffa846210a230a6a9
Evgeny Agafonchikov <evgeny.agafonchikov@akvelon.com>
2018-09-25 14:08:53 +0300

    Added test category to run on CI

--- src/ResourceManager/Network/Commands.Network.Test/ScenarioTests/CortexTests.cs
+++ src/ResourceManager/Network/Commands.Network.Test/ScenarioTests/CortexTests.cs
@@ -32,6 +32,7 @@ namespace Commands.Network.Test.ScenarioTests
 
         [Fact]
         [Trait(Category.Owner, Category.brooklynft)]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
         public void TestCortexCRUD()
         {
             //// WARNING - Until Ignite, please use only the Jenkins sub c9cbd920-c00c-427c-852b-8aaf38badaeb to record this test case
