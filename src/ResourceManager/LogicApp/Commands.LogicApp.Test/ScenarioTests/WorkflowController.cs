2a3b88f2b8c6a9cccd2d225443036addf3a7a219
MiYanni <miyanni@microsoft.com>
2018-10-04 13:09:53 -0700

    LogicApp tests running in Az.

--- src/ResourceManager/LogicApp/Commands.LogicApp.Test/ScenarioTests/WorkflowController.cs
+++ src/ResourceManager/LogicApp/Commands.LogicApp.Test/ScenarioTests/WorkflowController.cs
@@ -13,251 +13,125 @@
 // ----------------------------------------------------------------------------------
 
 using Microsoft.Azure.Management.Internal.Resources;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.Azure.Commands.Common.Authentication;
+using Microsoft.Azure.Management.Logic;
+using Microsoft.Azure.Test.HttpRecorder;
+using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+using Microsoft.WindowsAzure.Commands.ScenarioTest;
+using Microsoft.Azure.Management.WebSites.Version2016_09_01;
+using System.IO;
+using Microsoft.Azure.ServiceManagemenet.Common.Models;
 
 namespace Microsoft.Azure.Commands.LogicApp.Test.ScenarioTests
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using Microsoft.Azure.Commands.Common.Authentication;
-    using Microsoft.Azure.Gallery;
-    using Microsoft.Azure.Management.Authorization;
-    using Microsoft.Azure.Management.Logic;
-    using Microsoft.Azure.Subscriptions;
-    using Microsoft.Azure.Test.HttpRecorder;
-    using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
-    using Microsoft.WindowsAzure.Commands.ScenarioTest;
-    using LegacyTest = Microsoft.Azure.Test;
-    using TestEnvironmentFactory = Microsoft.Rest.ClientRuntime.Azure.TestFramework.TestEnvironmentFactory;
-    using TestUtilities = Microsoft.Rest.ClientRuntime.Azure.TestFramework.TestUtilities;
-    using Microsoft.Azure.Management.WebSites.Version2016_09_01;
-    using System.IO;
-    using ServiceManagemenet.Common.Models;
-
-    /// <summary>
-    /// Test controller for the logic app scenario testing
-    /// </summary>
     public class WorkflowController
     {
-        /// <summary>
-        /// CSM test factory
-        /// </summary>
-        private LegacyTest.CSMTestEnvironmentFactory csmTestFactory;
-
-        /// <summary>
-        /// EnvironmentSetupHelper instance
-        /// </summary>
-        private EnvironmentSetupHelper helper;
-
-        /// <summary>
-        /// Authorization Api Version
-        /// </summary>
+        private readonly EnvironmentSetupHelper _helper;
         private const string AuthorizationApiVersion = "2014-07-01-preview";
 
-        /// <summary>
-        /// Gets or sets the ResourceManagementClient
-        /// </summary>
         public ResourceManagementClient ResourceManagementClient { get; private set; }
 
-        /// <summary>
-        /// Gets or sets the SubscriptionClient
-        /// </summary>
-        public SubscriptionClient SubscriptionClient { get; private set; }
-
-        /// <summary>
-        /// Gets or sets the LogicManagementClient
-        /// </summary>
         public LogicManagementClient LogicManagementClient { get; private set; }
 
-        /// <summary>
-        /// Gets or sets the WebSiteManagementClient
-        /// </summary>
         public WebSiteManagementClient WebsiteManagementClient { get; private set; }
 
-        /// <summary>
-        /// Gets or sets the AuthorizationManagementClient
-        /// </summary>
-        public AuthorizationManagementClient AuthorizationManagementClient { get; private set; }
+        public static WorkflowController NewInstance => new WorkflowController();
 
-        /// <summary>
-        /// Gets or sets the GalleryClient
-        /// </summary>
-        public GalleryClient GalleryClient { get; private set; }
-
-        /// <summary>
-        /// Create a new instance of workflow controller.
-        /// </summary>
-        public static WorkflowController NewInstance
-        {
-            get { return new WorkflowController(); }
-        }
-
-        /// <summary>
-        /// Initiliazes the workflow controller
-        /// </summary>
         public WorkflowController()
         {
-            helper = new EnvironmentSetupHelper();
+            _helper = new EnvironmentSetupHelper();
         }
 
-        /// <summary>
-        /// Runs the PowerShell test
-        /// </summary>
-        /// <param name="scripts">script to be executed</param>
         public void RunPowerShellTest(XunitTracingInterceptor logger, params string[] scripts)
         {
-            var callingClassType = TestUtilities.GetCallingClass(2);
-            var mockName = TestUtilities.GetCurrentMethodName(2);
+            var sf = new StackTrace().GetFrame(1);
+            var callingClassType = sf.GetMethod().ReflectedType?.ToString();
+            var mockName = sf.GetMethod().Name;
 
-            helper.TracingInterceptor = logger;
+            _helper.TracingInterceptor = logger;
 
             RunPsTestWorkflow(
                 () => scripts,
-                // no custom initializer
-                null,
                 // no custom cleanup
                 null,
                 callingClassType,
                 mockName);
         }
 
-        /// <summary>
-        /// Runs the PowerShell test under mock undo context based on the test mode setting (Record|Playback)
-        /// </summary>
-        /// <param name="scriptBuilder">Script builder delegate</param>
-        /// <param name="initialize">initialize action</param>
-        /// <param name="cleanup">cleanup action</param>
-        /// <param name="callingClassType">Calling class type</param>
-        /// <param name="mockName">Mock Name</param>
         public void RunPsTestWorkflow(
             Func<string[]> scriptBuilder,
-            Action<LegacyTest.CSMTestEnvironmentFactory> initialize,
             Action cleanup,
             string callingClassType,
             string mockName)
         {
-            Dictionary<string, string> d = new Dictionary<string, string>();
-            d.Add("Microsoft.Authorization", AuthorizationApiVersion);
-            d.Add("Microsoft.Resources", null);
-            d.Add("Microsoft.Features", null);
-            var providersToIgnore = new Dictionary<string, string>();
-            providersToIgnore.Add("Microsoft.Azure.Management.Resources.ResourceManagementClient", "2016-02-01");
+            var d = new Dictionary<string, string>
+            {
+                {"Microsoft.Authorization", AuthorizationApiVersion},
+                {"Microsoft.Resources", null},
+                {"Microsoft.Features", null}
+            };
+            var providersToIgnore = new Dictionary<string, string>
+            {
+                {"Microsoft.Azure.Management.Resources.ResourceManagementClient", "2016-02-01"}
+            };
             HttpMockServer.Matcher = new PermissiveRecordMatcherWithApiExclusion(true, d, providersToIgnore);
 
             HttpMockServer.RecordsDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "SessionRecords");
-            using (MockContext context = MockContext.Start(callingClassType, mockName))
+            using (var context = MockContext.Start(callingClassType, mockName))
             {
-                this.csmTestFactory = new LegacyTest.CSMTestEnvironmentFactory();
-                if (initialize != null)
-                {
-                    initialize(this.csmTestFactory);
-                }
                 SetupManagementClients(context);
-                helper.SetupEnvironment(AzureModule.AzureResourceManager);
+                _helper.SetupEnvironment(AzureModule.AzureResourceManager);
 
-                var callingClassName = callingClassType
-                    .Split(new[] {"."}, StringSplitOptions.RemoveEmptyEntries)
-                    .Last();
-                helper.SetupModules(AzureModule.AzureResourceManager,
+                var callingClassName = callingClassType.Split(new[] {"."}, StringSplitOptions.RemoveEmptyEntries).Last();
+                _helper.SetupModules(AzureModule.AzureResourceManager,
                     "ScenarioTests\\Common.ps1",
                     "ScenarioTests\\" + callingClassName + ".ps1",
-                    helper.RMProfileModule,
-                    helper.GetRMModulePath(@"AzureRM.LogicApp.psd1"),
-                    "ScenarioTests\\AzureRM.Resources.ps1");
+                    _helper.RMProfileModule,
+                    _helper.GetRMModulePath(@"AzureRM.LogicApp.psd1"),
+                    "AzureRM.Resources.ps1");
 
                 try
                 {
-                    if (scriptBuilder != null)
+                    var psScripts = scriptBuilder?.Invoke();
+                    if (psScripts != null)
                     {
-                        var psScripts = scriptBuilder();
-
-                        if (psScripts != null)
-                        {
-                            helper.RunPowerShellTest(psScripts);
-                        }
+                        _helper.RunPowerShellTest(psScripts);
                     }
                 }
                 finally
                 {
-                    if (cleanup != null)
-                    {
-                        cleanup();
-                    }
+                    cleanup?.Invoke();
                 }
             }
         }
 
-        /// <summary>
-        /// Set up mock clients
-        /// </summary>
-        /// <param name="context"></param>
         private void SetupManagementClients(MockContext context)
         {
             ResourceManagementClient = GetResourceManagementClient(context);
-            SubscriptionClient = GetSubscriptionClient();
-            AuthorizationManagementClient = GetAuthorizationManagementClient();
-            GalleryClient = GetGalleryClient();
             LogicManagementClient = GetLogicManagementClient(context);
             WebsiteManagementClient = GetWebsiteManagementClient(context);
-            helper.SetupManagementClients(ResourceManagementClient,
-                SubscriptionClient,
-                AuthorizationManagementClient,
-                GalleryClient,
+            _helper.SetupManagementClients(ResourceManagementClient,
                 LogicManagementClient,
-                WebsiteManagementClient
-                );
+                WebsiteManagementClient);
         }
 
-        /// <summary>
-        /// Create an AuthorizationManagementClient instance based on mode setting
-        /// </summary>
-        /// <returns>AuthorizationManagementClient instance</returns>
-        private AuthorizationManagementClient GetAuthorizationManagementClient()
-        {
-            return LegacyTest.TestBase.GetServiceClient<AuthorizationManagementClient>(this.csmTestFactory);
-        }
-
-        /// <summary>
-        /// Creates a ResourceManagementClient instance
-        /// </summary>
-        /// <returns>ResourceManagementClient instance</returns>
-        private ResourceManagementClient GetResourceManagementClient(MockContext context)
+        private static ResourceManagementClient GetResourceManagementClient(MockContext context)
         {
             return context.GetServiceClient<ResourceManagementClient>(TestEnvironmentFactory.GetTestEnvironment());
         }
 
-        /// <summary>
-        /// Creates LogicManagementClient instance  based on mode setting
-        /// </summary>
-        /// <param name="context">Mock undocontext</param>
-        /// <returns>LogicManagementClient instance</returns>
-        private LogicManagementClient GetLogicManagementClient(MockContext context)
+        private static LogicManagementClient GetLogicManagementClient(MockContext context)
         {
             return context.GetServiceClient<LogicManagementClient>(TestEnvironmentFactory.GetTestEnvironment());
         }
 
-        /// <summary>
-        /// Creates WebSiteManagementClient instance  based on mode setting
-        /// </summary>
-        /// <param name="context">Mock undocontext</param>
-        /// <returns>WebSiteManagementClient instance</returns>
-        private WebSiteManagementClient GetWebsiteManagementClient(MockContext context)
+        private static WebSiteManagementClient GetWebsiteManagementClient(MockContext context)
         {
             return context.GetServiceClient<WebSiteManagementClient>(TestEnvironmentFactory.GetTestEnvironment());
-        }
-
-        private SubscriptionClient GetSubscriptionClient()
-        {
-            return LegacyTest.TestBase.GetServiceClient<SubscriptionClient>(this.csmTestFactory);
-        }
-
-        /// <summary>
-        /// Creates a Gallery Client  based on mode setting
-        /// </summary>
-        /// <returns>GalleryClient instamce</returns>
-        private GalleryClient GetGalleryClient()
-        {
-            return LegacyTest.TestBase.GetServiceClient<GalleryClient>(this.csmTestFactory);
         }
     }
 }
