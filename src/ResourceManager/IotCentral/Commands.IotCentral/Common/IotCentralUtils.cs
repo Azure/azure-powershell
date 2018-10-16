54c8d6dee95dacfe7614fb936669323f03535aa7
cle5eland <clthomps@microsoft.com>
2018-10-16 16:01:46 -0700

    Iot Central PS Mod

---
+++ src/ResourceManager/IotCentral/Commands.IotCentral/Common/IotCentralUtils.cs
@@ -0,0 +1,51 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Commands.IotCentral.Models;
+using Microsoft.Azure.Management.IotCentral.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.Azure.Commands.IotCentral.Common
+{
+    public static class IotCentralUtils
+    {
+        public static T2 ConvertObject<T1, T2>(T1 iotCentralObject)
+        {
+            var serialized = JsonConvert.SerializeObject(iotCentralObject);
+            return JsonConvert.DeserializeObject<T2>(serialized);
+        }
+
+        public static PSIotCentralApp ToPSIotCentralApp(App iotCentralApp)
+        {
+            return ConvertObject<App, PSIotCentralApp>(iotCentralApp);
+        }
+
+        public static IEnumerable<PSIotCentralApp> ToPSIotCentralApps(IEnumerable<App> iotCentralApps)
+        {
+            return ConvertObject<IEnumerable<App>, IEnumerable<PSIotCentralApp>>(iotCentralApps.ToList());
+        }
+
+        public static AppPatch CreateAppPatch(App iotCentralApp)
+        {
+            var copiedIotCenralApp = new AppPatch()
+            {
+                DisplayName = iotCentralApp.DisplayName,
+                Tags = iotCentralApp.Tags
+            };
+            return copiedIotCenralApp;
+        }
+    }
+}
