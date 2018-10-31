4f0dcdc5ae1d88d4e6775073b32a3a9f61d0cdf7
rrama-Microsoft <rrama@microsoft.com>
2018-10-30 18:40:19 -0700

    updated failed test - NamespaceCURD

--- src/ResourceManager/EventHub/Commands.EventHub.Test/ScenarioTests/NamespaceTests.ps1
+++ src/ResourceManager/EventHub/Commands.EventHub.Test/ScenarioTests/NamespaceTests.ps1
@@ -161,12 +161,14 @@ Tests New Parameter for EventHub Namespace Create List Remove operations.
 function NamespaceTests
 {
     # Setup    
-    $location = Get-Location
+    $location = Get-Location	
+	$locationKafka = "westus"
 	$namespaceName = getAssetName "Eventhub-Namespace1-"
 	$namespaceName2 = getAssetName "Eventhub-Namespace2-"
     $resourceGroupName = getAssetName "RGName1-"
 	$secondResourceGroup = getAssetName "RGName2-"
 	$namespaceNameKafka = getAssetName "Eh-NamespaceKafka-"
+
 
     Write-Debug "Create resource group"
     Write-Debug "ResourceGroup name : $resourceGroupName"
