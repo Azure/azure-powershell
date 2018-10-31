0d1deb33ad6cb0863ae392f9c76e903e6fd3acf2
Francisco Gomez Gamino <frangom@microsoft.com>
2018-10-31 13:31:38 -0700

    Fixing CredScan issue--adding SourceControlTests.ps1 to CredScanSuppressions.json.

--- src/ResourceManager/Automation/Commands.Automation.Test/ScenarioTests/SourceControlTests.cs
+++ src/ResourceManager/Automation/Commands.Automation.Test/ScenarioTests/SourceControlTests.cs
@@ -21,36 +21,8 @@ at least one sample runbook to sync. For the current tests, I am using a PowerSh
 After the repos are created, you will need to create a personal access token (PAT),
 which will be used to register the source control. Please note that after the tests are recorded,
 you should revoke access for PAT--this way no one can access to your private repos after the tests
-are recorded.
-
-$testReposInfo = @{
-    VsoGit = @{
-        Name = "AASourceControl-VsoGit"
-        RepoUrl = "https://francisco-gamino.visualstudio.com/_git/VsoGit-SwaggerAndCmdletsTests"
-        Branch = "preview"
-        FolderPath = "Azure/MyRunbooks"
-        SourceType = "VsoGit"
-        PersonalAccessToken = "3qdxa22lutnhezd4atpna74jn3m7wgo6o6kfbwezjfnvgbjhvoca"
-    }
-
-    VsoTfvc =  @{
-        Name = "AASourceControl-VsoTfvc"
-        RepoUrl = "https://francisco-gamino.visualstudio.com/VsoTfvc-SwaggerAndCmdletsTests/_versionControl"
-        FolderPath = "/MyRunbooks"
-        SourceType = "VsoTfvc"
-        PersonalAccessToken = "3qdxa22lutnhezd4atpna74jn3m7wgo6o6kfbwezjfnvgbjhvoca"
-    }
-
-    GitHub = @{
-        Name = "AASourceControl-GitHub"
-        RepoUrl = "https://github.com/Francisco-Gamino/SwaggerAndCmdletsTests.git"
-        Branch = "master"
-        FolderPath = "/"
-        SourceType = "GitHub"
-        PersonalAccessToken = "5fd81166a9ebaebc60da4756f2094a598f1d4c01"
-    }
-}
- */
+are recorded. See example in SourceControlTests.ps1 where $testReposInfo is defined.
+*/
 
 namespace Commands.Automation.Test
 {
