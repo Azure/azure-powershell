a21d21ca34a9b01d949a0a687389f3b149775fc8
maddieclayton <maclayto@microsoft.com>
2018-11-08 14:58:04 -0800

    add secret generation

---
+++ src/ResourceManager/Resources/Commands.Resources/Models.Authorization/PSADCredentialWrapper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Graph.RBAC.Version1_6.ActiveDirectory;
+using System.Security;
+
+namespace Microsoft.Azure.Commands.Resources.Models.Authorization
+{
+    public class PSADCredentialWrapper : PSADCredential
+    {
+        public PSADCredentialWrapper(PSADCredential cred)
+        {
+            StartDate = cred.StartDate;
+            EndDate = cred.EndDate;
+            KeyId = cred.KeyId;
+            Type = cred.Type;
+        }
+
+        public SecureString Secret { get; set; }
+    }
+}
