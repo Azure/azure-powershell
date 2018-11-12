3dde7d05378fc1df1eda2b94c94824cb4a9d3688
maddieclayton <maclayto@microsoft.com>
2018-11-12 13:43:41 -0800

    address comments

--- src/ResourceManager/Resources/Commands.Resources/Models.Authorization/PSADServicePrincipalWrapper.cs
+++ src/ResourceManager/Resources/Commands.Resources/Models.Authorization/PSADServicePrincipalWrapper.cs
@@ -1,4 +1,18 @@
-﻿using Microsoft.Azure.Graph.RBAC.Version1_6.ActiveDirectory;
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Graph.RBAC.Version1_6.ActiveDirectory;
 using System.Security;
 
 namespace Microsoft.Azure.Commands.Resources.Models.Authorization
@@ -7,12 +21,15 @@ namespace Microsoft.Azure.Commands.Resources.Models.Authorization
     {
         public PSADServicePrincipalWrapper(PSADServicePrincipal sp)
         {
-            AdfsId = sp.AdfsId;
-            ApplicationId = sp.ApplicationId;
-            DisplayName = sp.DisplayName;
-            Id = sp.Id;
-            ServicePrincipalNames = sp.ServicePrincipalNames;
-            Type = sp.Type;
+            if (sp != null)
+            {
+                AdfsId = sp.AdfsId;
+                ApplicationId = sp.ApplicationId;
+                DisplayName = sp.DisplayName;
+                Id = sp.Id;
+                ServicePrincipalNames = sp.ServicePrincipalNames;
+                Type = sp.Type;
+            }
         }
         public SecureString Secret { get; set; }
     }
