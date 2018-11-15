f2bd3f03d89f6abca40eae4b656c28cd152dfd47
Wei Wei <weiwei@microsoft.com>
2018-11-15 11:31:47 +0800

    [Storage] Upgrade to SRP SDK 9.0.0 and Get-AzureRmStoreageUsage parameter -Location become madentory

--- src/ResourceManager/Storage/Commands.Management.Storage/StorageAccount/GetAzureStorageUsage.cs
+++ src/ResourceManager/Storage/Commands.Management.Storage/StorageAccount/GetAzureStorageUsage.cs
@@ -27,7 +27,7 @@ namespace Microsoft.Azure.Commands.Management.Storage.StorageAccount
     public class GetAzureStorageUsageCommand : StorageAccountBaseCmdlet
     {
         [Parameter(
-            Mandatory = false,
+            Mandatory = true,
             ValueFromPipelineByPropertyName = true,
             HelpMessage = "Storage Accounts Location.")]
         [LocationCompleter("Microsoft.Storage/storageAccounts")]
@@ -38,16 +38,7 @@ namespace Microsoft.Azure.Commands.Management.Storage.StorageAccount
             base.ExecuteCmdlet();
 
             //Get usage
-            IEnumerable<Usage> usages;
-            if (Location == null)
-            {
-                WriteWarning("Get global storage usage is obsolete, please use get location usage with -Location Parameter instead.");
-                usages = this.StorageClient.Usages.List();
-            }
-            else
-            {
-                usages = this.StorageClient.Usages.ListByLocation(Location);
-            }
+            IEnumerable<Usage> usages = this.StorageClient.Usages.ListByLocation(Location);
 
             //Output usage
             foreach (var usage in usages)
