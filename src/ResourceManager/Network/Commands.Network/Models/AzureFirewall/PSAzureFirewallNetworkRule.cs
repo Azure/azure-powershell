b12b6dc294889b22a2e3c6bed7cfc483a0835317
Anton Evseev <v-anevse@microsoft.com>
2018-12-14 12:01:15 +0300

    Add ICMP protocol to AzureFirewall helper

--- src/ResourceManager/Network/Commands.Network/Models/AzureFirewall/PSAzureFirewallNetworkRule.cs
+++ src/ResourceManager/Network/Commands.Network/Models/AzureFirewall/PSAzureFirewallNetworkRule.cs
@@ -58,7 +58,7 @@ namespace Microsoft.Azure.Commands.Network.Models
 
         public void AddProtocol(string protocolType)
         {
-            (Protocols ?? (Protocols = new List<string>())).Add(AzureFirewallNetworkRuleProtocolHelper.MapUserInputToNetworkProtocol(protocolType));
+            (Protocols ?? (Protocols = new List<string>())).Add(AzureFirewallNetworkRuleProtocolHelper.MapUserInputToNetworkRuleProtocol(protocolType));
         }
     }
 }
