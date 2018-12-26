f530fdcdb78f8935a9d7d0bc74dab6cec8c7f739
Maddie Clayton <maclayto@microsoft.com>
2018-12-26 16:12:02 -0600

    Update Resources.Designer.cs

--- src/Common/Properties/Resources.Designer.cs
+++ src/Common/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Microsoft.WindowsAzure.Commands.Common.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -566,6 +566,15 @@ namespace Microsoft.WindowsAzure.Commands.Common.Properties {
         public static string BreakingChangesAttributesDeclarationMessageWithCmdletName {
             get {
                 return ResourceManager.GetString("BreakingChangesAttributesDeclarationMessageWithCmdletName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to NOTE : Go to {0} for steps to suppress  (and other related information on) the breaking change messages..
+        /// </summary>
+        public static string BreakingChangesAttributesFooterMessage {
+            get {
+                return ResourceManager.GetString("BreakingChangesAttributesFooterMessage", resourceCulture);
             }
         }
         
