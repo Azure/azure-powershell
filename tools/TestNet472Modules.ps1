f9998cdbb2e02f250ae9895a1055860aab94b2a3
MiYanni <miyanni@microsoft.com>
2019-01-07 14:08:43 -0800

    Fixed project paths in TestNet472Modules.ps1 and VersionController.

--- tools/TestNet472Modules.ps1
+++ tools/TestNet472Modules.ps1
@@ -10,19 +10,13 @@ param(
 $dirPath = (Join-Path $PSScriptRoot -ChildPath '..')
 $srcPath = (Join-Path $dirPath -ChildPath 'src')
 $libPath = (Join-Path $srcPath -ChildPath 'lib')
-$rmBasePath = (Join-Path $srcPath -ChildPath 'ResourceManager')
-$storageBasePath = (Join-Path $srcPath -ChildPath 'Storage')
 $testConfig = (Join-Path $libPath -ChildPath 'test.net472.config')
-$testStorageConfig = (Join-Path $libPath -ChildPath 'test.net472.storage.config')
-$rmItems = Get-ChildItem -Recurse -Path $rmBasePath -Include *.Test.dll `
-  | Where {$_.FullName.Contains('bin\Debug\net472')}
-$storageItems = Get-ChildItem -Recurse -Path $storageBasePath -Include *.Test.dll `
+$rmItems = Get-ChildItem -Recurse -Path $srcPath -Include *.Test.dll `
   | Where {$_.FullName.Contains('bin\Debug\net472')}
 
 if ($ModuleFilter)
 {
     $rmItems = $rmItems | Where {$_.FullName.Contains($ModuleFilter)}
-    $storageItems = $storageItems | Where {$_.FullName.Contains($ModuleFilter)}
 }
 
 $rmItems | %{`
@@ -33,14 +27,3 @@ $rmItems | %{`
     -NoNewWindow `
     -ArgumentList $_.FullName, $testConfig, '-trait "AcceptanceType=CheckIn"', '-notrait "Runtype=DesktopOnly"'`
 }
-
-$storageItems | %{`
-  Write-Host "Testing $_.FullName"
-  Start-Process -FilePath $TestExecPath `
-    -Wait `
-    -WorkingDirectory $PSScriptRoot\..\artifacts `
-    -NoNewWindow `
-    -ArgumentList $_.FullName, $testStorageConfig, '-trait "AcceptanceType=CheckIn"'`
-}
-
-
