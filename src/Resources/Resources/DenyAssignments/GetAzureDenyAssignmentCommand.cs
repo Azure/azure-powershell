c18bc6c77fdcde17cba954174189f2375bc9290c
cormacpayne <corm@microsoft.com>
2019-01-08 08:17:46 -0800

    Move remaining DenyAssignment files

---
+++ src/Resources/Resources/DenyAssignments/GetAzureDenyAssignmentCommand.cs
@@ -0,0 +1,188 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Commands.ActiveDirectory;
+using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
+using Microsoft.Azure.Commands.Resources.Models;
+using Microsoft.Azure.Commands.Resources.Models.Authorization;
+using Microsoft.WindowsAzure.Commands.Common;
+using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+
+namespace Microsoft.Azure.Commands.Resources
+{
+    /// <summary>
+    /// Filters deny assignments
+    /// </summary>
+    [Cmdlet("Get", ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "DenyAssignment", DefaultParameterSetName = ParameterSet.Empty), OutputType(typeof(PSDenyAssignment))]
+    public class GetAzureDenyAssignmentCommand : ResourcesBaseCmdlet
+    {
+        private const string DenyAssignmentIdParameterSet = "DenyAssignmentIdParameterSet";
+        private const string DenyAssignmentNameParameterSet = "DenyAssignmentNameParameterSet";
+
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ObjectId,
+            HelpMessage = "The user or group object id.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ResourceGroupWithObjectId,
+            HelpMessage = "The user or group object id.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ResourceWithObjectId,
+            HelpMessage = "The user or group object id.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ScopeWithObjectId,
+            HelpMessage = "The user or group object id.")]
+        [Alias("PrincipalId")]
+        public Guid? ObjectId { get; set; }
+
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ResourceGroupWithSignInName,
+            HelpMessage = "The user SignInName.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ResourceWithSignInName,
+            HelpMessage = "The user SignInName.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ScopeWithSignInName,
+            HelpMessage = "The user SignInName.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.SignInName,
+            HelpMessage = "The user SignInName.")]
+        [ValidateNotNullOrEmpty]
+        [Alias("Email", "UserPrincipalName")]
+        public string SignInName { get; set; }
+
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ResourceGroupWithSPN,
+            HelpMessage = "The app SPN.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ResourceWithSPN,
+            HelpMessage = "The app SPN.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ScopeWithSPN,
+            HelpMessage = "The app SPN.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.SPN,
+            HelpMessage = "The app SPN.")]
+        [ValidateNotNullOrEmpty]
+        [Alias("SPN")]
+        public string ServicePrincipalName { get; set; }
+
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ResourceGroup,
+            HelpMessage = "Resource group to which the deny assignment applies.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.Resource,
+            HelpMessage = "Resource group to which the deny assignment applies.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ResourceGroupWithObjectId,
+            HelpMessage = "Resource group to which the deny assignment applies.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ResourceWithObjectId,
+            HelpMessage = "Resource group to which the deny assignment applies.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ResourceGroupWithSignInName,
+            HelpMessage = "Resource group to which the deny assignment applies.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ResourceWithSignInName,
+            HelpMessage = "Resource group to which the deny assignment applies.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ResourceGroupWithSPN,
+            HelpMessage = "Resource group to which the deny assignment applies.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ResourceWithSPN,
+            HelpMessage = "Resource group to which the deny assignment applies.")]
+        [ResourceGroupCompleter]
+        [ValidateNotNullOrEmpty]
+        public string ResourceGroupName { get; set; }
+
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.Resource,
+            HelpMessage = "Resource name to which the deny assignment applies.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ResourceWithObjectId,
+            HelpMessage = "Resource name to which the deny assignment applies.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ResourceWithSignInName,
+            HelpMessage = "Resource name to which the deny assignment applies.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ResourceWithSPN,
+            HelpMessage = "Resource name to which the deny assignment applies.")]
+        [ValidateNotNullOrEmpty]
+        public string ResourceName { get; set; }
+
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.Resource,
+            HelpMessage = "Resource type to which the deny assignment applies.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ResourceWithObjectId,
+            HelpMessage = "Resource type to which the deny assignment applies.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ResourceWithSignInName,
+            HelpMessage = "Resource type to which the deny assignment applies.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ResourceWithSPN,
+            HelpMessage = "Resource type to which the deny assignment applies.")]
+        [ValidateNotNullOrEmpty]
+        public string ResourceType { get; set; }
+
+        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.Resource,
+            HelpMessage = "Parent resource of the resource to which the deny assignment applies.")]
+        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ResourceWithObjectId,
+            HelpMessage = "Parent resource of the resource to which the deny assignment applies.")]
+        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ResourceWithSignInName,
+            HelpMessage = "Parent resource of the resource to which the deny assignment applies.")]
+        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ResourceWithSPN,
+            HelpMessage = "Parent resource of the resource to which the deny assignment applies.")]
+        [ValidateNotNullOrEmpty]
+        public string ParentResource { get; set; }
+
+        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.Empty,
+            HelpMessage = "Scope of the deny assignment. In the format of relative URI.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.Scope,
+            HelpMessage = "Scope of the deny assignment. In the format of relative URI.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ScopeWithObjectId,
+            HelpMessage = "Scope of the deny assignment. In the format of relative URI.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ScopeWithSignInName,
+            HelpMessage = "Scope of the deny assignment. In the format of relative URI.")]
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = ParameterSet.ScopeWithSPN,
+            HelpMessage = "Scope of the deny assignment. In the format of relative URI.")]
+        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, ParameterSetName = DenyAssignmentIdParameterSet,
+            HelpMessage = "Scope of the deny assignment. In the format of relative URI.")]
+        [Parameter(Mandatory = false, ValueFromPipelineByPropertyName = true, ParameterSetName = DenyAssignmentNameParameterSet,
+            HelpMessage = "Scope of the deny assignment. In the format of relative URI.")]
+        [ValidateNotNullOrEmpty]
+        [ScopeCompleter]
+        public string Scope { get; set; }
+
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = DenyAssignmentIdParameterSet, HelpMessage = "Deny assignment id.")]
+        [ValidateGuidNotEmpty]
+        public Guid Id { get; set; }
+
+        [Parameter(Mandatory = true, ValueFromPipelineByPropertyName = true, ParameterSetName = DenyAssignmentNameParameterSet,
+            HelpMessage = "Name of the deny assignment.")]
+        [ValidateNotNullOrEmpty]
+        public string DenyAssignmentName { get; set; }
+
+        [Parameter(Mandatory = false, ParameterSetName = ParameterSet.ObjectId,
+            HelpMessage = "If specified, returns deny assignments directly assigned to the principal as well as assignments to the principal's groups (transitive). Supported only for User Principals.")]
+        [Parameter(Mandatory = false, ParameterSetName = ParameterSet.SignInName,
+            HelpMessage = "If specified, returns deny assignments directly assigned to the principal as well as assignments to the principal's groups (transitive). Supported only for User Principals.")]
+        public SwitchParameter ExpandPrincipalGroups { get; set; }
+
+        public override void ExecuteCmdlet()
+        {
+            var options = new FilterDenyAssignmentsOptions()
+            {
+                DenyAssignmentId = Id,
+                DenyAssignmentName = DenyAssignmentName,
+                Scope = Scope,
+                ADObjectFilter = new ADObjectFilterOptions
+                {
+                    UPN = SignInName,
+                    SPN = ServicePrincipalName,
+                    Id = ObjectId?.ToString(),
+                },
+                ResourceIdentifier = new ResourceIdentifier()
+                {
+                    ParentResource = ParentResource,
+                    ResourceGroupName = ResourceGroupName,
+                    ResourceName = ResourceName,
+                    ResourceType = ResourceType,
+                    Subscription = string.IsNullOrEmpty(ResourceGroupName) ? null : DefaultProfile.DefaultContext.Subscription.Id.ToString()
+                },
+                ExpandPrincipalGroups = ExpandPrincipalGroups.IsPresent,
+                ExcludeAssignmentsForDeletedPrincipals = false
+            };
+
+            AuthorizationClient.ValidateScope(options.Scope, true);
+
+            List<PSDenyAssignment> denyAssignments = PoliciesClient.FilterDenyAssignments(options, DefaultProfile.DefaultContext.Subscription.Id.ToString());
+
+            WriteObject(denyAssignments, enumerateCollection: true);
+        }
+    }
+}
