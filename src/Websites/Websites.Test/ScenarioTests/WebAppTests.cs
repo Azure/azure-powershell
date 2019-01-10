38201fd2832620b6c2f440289779d6874e74c5ce
Nicholas King <nicking@microsoft.com>
2019-01-10 13:29:00 -0800

    Make Publish-AzWebApp tests live only

--- src/Websites/Websites.Test/ScenarioTests/WebAppTests.cs
+++ src/Websites/Websites.Test/ScenarioTests/WebAppTests.cs
@@ -95,14 +95,14 @@ namespace Microsoft.Azure.Commands.Websites.Test.ScenarioTests
         }
 
         [Fact]
-        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        [Trait(Category.RunType, Category.LiveOnly)]
         public void TestPublishWebAppFromZip()
         {
             WebsitesController.NewInstance.RunPsTest(_logger, "Test-PublishAzureWebAppFromZip");
         }
 
         [Fact]
-        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        [Trait(Category.RunType, Category.LiveOnly)]
         public void TestPublishWebAppFromWar()
         {
             WebsitesController.NewInstance.RunPsTest(_logger, "Test-PublishAzureWebAppFromWar");
