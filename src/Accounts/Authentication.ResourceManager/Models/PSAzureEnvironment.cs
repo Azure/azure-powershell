97cfcb5b8321870c74efcae84608e40b262d9bd3
Maddie Clayton <maclayto@microsoft.com>
2019-01-22 11:50:38 -0800

    Update PSAzureEnvironment.cs

--- src/Accounts/Authentication.ResourceManager/Models/PSAzureEnvironment.cs
+++ src/Accounts/Authentication.ResourceManager/Models/PSAzureEnvironment.cs
@@ -275,6 +275,21 @@ namespace Microsoft.Azure.Commands.Profile.Models
                 this.SetEndpoint(AzureEnvironment.ExtendedEndpoint.AnalysisServicesEndpointSuffix, value);
             }
         }
+        
+        /// <summary>
+        /// Gets or sets the resource Id to use for contacting the analysis services endpoint
+        /// </summary>
+        public string AnalysisServicesEndpointResourceId
+        {
+            get
+            {
+                return this.GetEndpointSuffix(AzureEnvironment.ExtendedEndpoint.AnalysisServicesEndpointResourceId);
+            }
+            set
+            {
+                this.SetEndpoint(AzureEnvironment.ExtendedEndpoint.AnalysisServicesEndpointResourceId, value);
+            }
+        }
 
         public IList<string> VersionProfiles { get; } = new List<string>();
 
