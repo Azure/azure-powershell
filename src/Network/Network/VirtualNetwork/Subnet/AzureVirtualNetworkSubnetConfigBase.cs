94db89f38eaca60a63e6d0b4f75cc6ef4a75c280
Evgeny Agafonchikov <evgeny.agafonchikov@akvelon.com>
2019-02-20 13:50:40 +0300

    Added Update cmdlet for Subnet

--- src/Network/Network/VirtualNetwork/Subnet/AzureVirtualNetworkSubnetConfigBase.cs
+++ src/Network/Network/VirtualNetwork/Subnet/AzureVirtualNetworkSubnetConfigBase.cs
@@ -31,7 +31,7 @@ namespace Microsoft.Azure.Commands.Network
             Mandatory = true,
             HelpMessage = "The address prefixes of the subnet")]
         [ValidateNotNullOrEmpty]
-        public string[] AddressPrefix { get; set; }
+        public virtual string[] AddressPrefix { get; set; }
 
         [Parameter(
             Mandatory = false,
