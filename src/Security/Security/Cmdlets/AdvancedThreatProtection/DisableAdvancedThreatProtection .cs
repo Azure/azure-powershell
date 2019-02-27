42d0eb8409f03ea56dd8edcb6778d94122e5ffc2
Shai Blum <shblum@microsoft.com>
2019-02-27 14:45:06 +0200

    Help files + remove SupportsShouldProcess

--- src/Security/Security/Cmdlets/AdvancedThreatProtection/DisableAdvancedThreatProtection .cs
+++ src/Security/Security/Cmdlets/AdvancedThreatProtection/DisableAdvancedThreatProtection .cs
@@ -19,7 +19,7 @@ using System.Management.Automation;
 
 namespace Microsoft.Azure.Commands.Security.Cmdlets.AdvancedThreatProtection
 {
-    [Cmdlet("DIsable", ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "SecurityAdvancedThreatProtection", DefaultParameterSetName = ParameterSetNames.ResourceId, SupportsShouldProcess = true), OutputType(typeof(PSAdvancedThreatProtection))]
+    [Cmdlet("DIsable", ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "SecurityAdvancedThreatProtection", DefaultParameterSetName = ParameterSetNames.ResourceId), OutputType(typeof(PSAdvancedThreatProtection))]
     public class DisableAdvancedThreatProtection : SecurityCenterCmdletBase
     {
 
