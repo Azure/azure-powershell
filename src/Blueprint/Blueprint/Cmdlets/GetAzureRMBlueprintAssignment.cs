d6b8d26158058d0e62544878df8b3e4bd13099c9
Filiz Topatan <lsefitopata@microsoft.com>
2019-03-01 05:47:35 -0800

    Address PR feedback

--- src/Blueprint/Blueprint/Cmdlets/GetAzureRMBlueprintAssignment.cs
+++ src/Blueprint/Blueprint/Cmdlets/GetAzureRMBlueprintAssignment.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
 using System.Management.Automation;
 using Microsoft.Azure.Commands.Blueprint.Common;
 using Microsoft.Azure.Commands.Blueprint.Models;
@@ -32,7 +46,7 @@ namespace Microsoft.Azure.Commands.Blueprint.Cmdlets
                 switch (ParameterSetName) {
                     case ParameterSetNames.BlueprintAssignmentsBySubscription:
                         foreach (var assignment in BlueprintClient.ListBlueprintAssignments(Utils.GetScopeForSubscription(subscription)))
-                            WriteObject(assignment);
+                            WriteObject(assignment, true);
                         break;
                     case ParameterSetNames.BlueprintAssignmentByName:
                         WriteObject(BlueprintClient.GetBlueprintAssignment(Utils.GetScopeForSubscription(subscription), Name));
