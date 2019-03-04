d602b874637547d825277dee1925df88a1caf0cf
HassanBadir <badeer.h@gmail.com>
2019-03-04 12:15:08 +0200

    change from DetectionType[] to string[]

---
+++ src/Sql/Sql/ThreatDetection/Model/DetectionType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.Commands.Sql.ThreatDetection.Model
+{
+    /// <summary>
+    /// The possible disable alert types
+    /// </summary> 
+    public static class DetectionType
+    {
+        public const string Sql_Injection = "Sql_Injection";
+        public const string Sql_Injection_Vulnerability = "Sql_Injection_Vulnerability";
+        public const string Access_Anomaly = "Access_Anomaly";
+        public const string Data_Exfiltration = "Data_Exfiltration";
+        public const string Unsafe_Action = "Unsafe_Action";
+        public const string None = "None";
+    }
+}
