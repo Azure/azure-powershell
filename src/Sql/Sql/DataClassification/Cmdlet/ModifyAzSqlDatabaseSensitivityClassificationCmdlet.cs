aa1540a49a6953872ff4d4af811cb16a1367d5b3
bashahee <bashahee@users.noreply.github.com>
2019-03-13 18:30:07 +0200

    Add Microsoft Header to files

--- src/Sql/Sql/DataClassification/Cmdlet/ModifyAzSqlDatabaseSensitivityClassificationCmdlet.cs
+++ src/Sql/Sql/DataClassification/Cmdlet/ModifyAzSqlDatabaseSensitivityClassificationCmdlet.cs
@@ -1,14 +1,23 @@
-﻿using Hyak.Common;
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+using Hyak.Common;
 using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
 using Microsoft.Azure.Commands.Sql.Common;
 using Microsoft.Azure.Commands.Sql.Database.Model;
 using Microsoft.Azure.Commands.Sql.DataClassification.Model;
 using Microsoft.Azure.Commands.Sql.DataClassification.Services;
-using System;
-using System.Collections.Generic;
 using System.Management.Automation;
-using System.Net;
-using System.Text;
 
 namespace Microsoft.Azure.Commands.Sql.DataClassification.Cmdlet
 {
