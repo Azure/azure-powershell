15e27a9de464db442febc921e20849e546506ae8
leonard <leonard.francis@netapp.com>
2019-03-14 22:46:46 +0000

    NFSAAS-1707 update from review comments

--- src/NetAppFiles/NetAppFiles.Test/ScenarioTests/AccountTests.cs
+++ src/NetAppFiles/NetAppFiles.Test/ScenarioTests/AccountTests.cs
@@ -40,6 +40,5 @@ namespace Microsoft.Azure.Commands.NetAppFiles.Test.ScenarioTests.ScenarioTest
         {
             TestController.NewInstance.RunPowerShellTest(_logger, "Test-AccountPipelines");
         }
-
     }
 }
