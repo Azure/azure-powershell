b550d5656ab366e2e4c83b656b41268aed8ad8ab
Po-Yu Chuang <pochuang@microsoft.com>
2019-04-01 14:51:04 -0700

    ConnectAzureRmAccount: parameter alias RolloutEnvironment

--- src/Accounts/Accounts/Account/ConnectAzureRmAccount.cs
+++ src/Accounts/Accounts/Account/ConnectAzureRmAccount.cs
@@ -50,7 +50,7 @@ namespace Microsoft.Azure.Commands.Profile
         private IAzureEnvironment _environment = AzureEnvironment.PublicEnvironments[EnvironmentName.AzureCloud];
 
         [Parameter(Mandatory = false, HelpMessage = "Name of the environment containing the account to log into")]
-        [Alias("EnvironmentName")]
+        [Alias("EnvironmentName", "RolloutEnvironment")]
         [ValidateNotNullOrEmpty]
         public string Environment { get; set; }
 
