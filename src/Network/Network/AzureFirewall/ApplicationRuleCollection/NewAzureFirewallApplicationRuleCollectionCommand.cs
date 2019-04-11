0e4dd8037b37cad63966cc8315833e90b460db35
Evgeny Agafonchikov <evgeny.agafonchikov@akvelon.com>
2019-04-11 10:50:25 +0300

    Revert changes for AzureFirewall

--- src/Network/Network/AzureFirewall/ApplicationRuleCollection/NewAzureFirewallApplicationRuleCollectionCommand.cs
+++ src/Network/Network/AzureFirewall/ApplicationRuleCollection/NewAzureFirewallApplicationRuleCollectionCommand.cs
@@ -49,6 +49,7 @@ namespace Microsoft.Azure.Commands.Network
         [ValidateSet(
             MNM.AzureFirewallRCActionType.Allow,
             MNM.AzureFirewallRCActionType.Deny,
+            MNM.AzureFirewallRCActionType.Alert,
             IgnoreCase = false)]
         public string ActionType { get; set; }
 
