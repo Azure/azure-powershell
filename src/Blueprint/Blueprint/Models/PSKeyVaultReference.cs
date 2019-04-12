16283ba1c45ec0af75602fdd56629cca04d0dcfc
Filiz Topatan <lsefitopata@microsoft.com>
2019-04-12 06:44:55 -0700

    Refactor code

--- src/Blueprint/Blueprint/Models/PSKeyVaultReference.cs
+++ src/Blueprint/Blueprint/Models/PSKeyVaultReference.cs
@@ -6,11 +6,6 @@ namespace Microsoft.Azure.Commands.Blueprint.Models
 {
     public class PSKeyVaultReference
     {
-        public string Id { get; private set; }
-
-        public PSKeyVaultReference(string id = null)
-        {
-            Id = id;
-        }
+        public string Id { get; set; }
     }
 }
--- src/Blueprint/Blueprint/Models/PSSecretReferenceParameterValue.cs
+++ src/Blueprint/Blueprint/Models/PSSecretReferenceParameterValue.cs
@@ -7,11 +7,5 @@ namespace Microsoft.Azure.Commands.Blueprint.Models
     public class PSSecretReferenceParameterValue: PSParameterValueBase
     {
         public PSSecretValueReference Reference { get; set; }
-
-        public PSSecretReferenceParameterValue(PSSecretValueReference reference, string description = default(string))
-        {
-            Reference = reference;
-            Description = description;
-        }
     }
 }
