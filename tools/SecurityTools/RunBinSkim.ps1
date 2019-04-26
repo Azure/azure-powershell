7a97c7961b796f9aa051d9a828c0dc7913be4a1b
Michael Yanni <miyanni@microsoft.com>
2019-04-26 13:29:11 -0700

    Removed unused suppressions file. Additional suppression for BinSkim.

--- tools/SecurityTools/RunBinSkim.ps1
+++ tools/SecurityTools/RunBinSkim.ps1
@@ -1,7 +1,7 @@
 $repoRoot = "$PSScriptRoot/../.."
 $binSkim = (Get-Item -Path "$repoRoot/tools/SecurityTools/Microsoft.CodeAnalysis.BinSkim*/tools/*/win-x64/BinSkim.exe" | Select-Object -First 1).FullName
 $dllBaseFolder = "$repoRoot/artifacts/Debug"
-$suppressions = @('Microsoft.Azure.DataLake.Store.dll','Microsoft.WindowsAzure.Storage.DataMovement.PowerShell.dll')
+$suppressions = @('Microsoft.Azure.DataLake.Store.dll','Microsoft.WindowsAzure.Storage.DataMovement.PowerShell.dll','Microsoft.Azure.Storage.DataMovement.dll')
 $resultsPath = "$repoRoot/artifacts/BinSkim-Results.sarif"
 
 # Renaming suppressed files since BinSkim has no file filtering support
