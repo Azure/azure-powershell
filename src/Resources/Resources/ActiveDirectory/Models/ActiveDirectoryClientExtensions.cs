cc1354d22b2d66cbc893478ca5d3ac8e8352c229
Leandro Wajswajn Pereyra <leandrowp@gmail.com>
2019-05-13 22:04:41 -0300

    Update src/Resources/Resources/ActiveDirectory/Models/ActiveDirectoryClientExtensions.cs
    Co-Authored-By: Michael Yanni <MiYanni@microsoft.com>

--- src/Resources/Resources/ActiveDirectory/Models/ActiveDirectoryClientExtensions.cs
+++ src/Resources/Resources/ActiveDirectory/Models/ActiveDirectoryClientExtensions.cs
@@ -112,7 +112,7 @@ namespace Microsoft.Azure.Commands.ActiveDirectory
                 Id = group.ObjectId,
                 SecurityEnabled = group.SecurityEnabled,
                 MailNickname = group.Mail ?? group.AdditionalProperties["mailNickname"].ToString(),
-                Description = (group.AdditionalProperties.ContainsKey("description") && group.AdditionalProperties["description"] != null ? group.AdditionalProperties["description"].ToString() : null)
+                Description = group.AdditionalProperties.ContainsKey("description") ? group.AdditionalProperties["description"]?.ToString() : null
             };
         }
 
