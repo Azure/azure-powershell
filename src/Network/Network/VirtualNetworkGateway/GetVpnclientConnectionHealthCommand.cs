7c8d86eda2a5b3f384b744b3c6441249e94a2152
Ning Wei <ninwe@microsoft.com>
2019-05-16 17:18:14 -0700

    add resource id and inputobj for get vnet gateway vpnclient health cmd

--- src/Network/Network/VirtualNetworkGateway/GetVpnclientConnectionHealthCommand.cs
+++ src/Network/Network/VirtualNetworkGateway/GetVpnclientConnectionHealthCommand.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Azure.Commands.Network.Models;
 using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
+using Microsoft.Azure.Management.Internal.Resources.Utilities.Models;
 using Microsoft.Azure.Management.Network;
+using System;
 using System.Collections.Generic;
 using System.Management.Automation;
 
@@ -13,14 +15,32 @@ namespace Microsoft.Azure.Commands.Network.VirtualNetworkGateway
         [Parameter(
             Mandatory = true,
             ValueFromPipelineByPropertyName = true,
+            ParameterSetName = ParameterSetNames.ByFactoryName,
             HelpMessage = "Virtual network gateway name")]
         [ResourceNameCompleter("Microsoft.Network/virtualNetworkGateways", "ResourceGroupName")]
         [ValidateNotNullOrEmpty]
         public virtual string VirtualNetworkGatewayName { get; set; }
 
+        [Alias("VirtualNetworkGatewayId")]
+        [Parameter(
+            Mandatory = false,
+            ValueFromPipelineByPropertyName = true,
+            ParameterSetName = ParameterSetNames.ByResourceId,
+            HelpMessage = "The resource id of the virtual network gateway object")]
+        public string ResourceId { get; set; }
+
+        [Alias("VirtualNetworkGateway")]
+        [Parameter(
+            Mandatory = true,
+            ValueFromPipeline = true,
+            ParameterSetName = ParameterSetNames.ByFactoryObject,
+            HelpMessage = "The virtual network gateway object to update.")]
+        public PSVirtualNetworkGateway InputObject { get; set; }
+
         [Parameter(
             Mandatory = true,
             ValueFromPipelineByPropertyName = true,
+            ParameterSetName = ParameterSetNames.ByFactoryName,
             HelpMessage = "Virtual network gateway resource group's name")]
         [ResourceGroupCompleter]
         [ValidateNotNullOrEmpty]
@@ -31,9 +51,27 @@ namespace Microsoft.Azure.Commands.Network.VirtualNetworkGateway
 
         public override void Execute()
         {
+            if (ParameterSetName.Equals(ParameterSetNames.ByFactoryObject, StringComparison.OrdinalIgnoreCase))
+            {
+                VirtualNetworkGatewayName = InputObject.Name;
+                ResourceGroupName = InputObject.ResourceGroupName;
+            }
+            else if (ParameterSetName.Equals(ParameterSetNames.ByResourceId, StringComparison.OrdinalIgnoreCase))
+            {
+                var parsedResourceId = new ResourceIdentifier(ResourceId);
+                VirtualNetworkGatewayName = parsedResourceId.ResourceName;
+                ResourceGroupName = parsedResourceId.ResourceGroupName;
+            }
+
             base.Execute();
+
+            if (!IsVirtualNetworkGatewayPresent(ResourceGroupName, VirtualNetworkGatewayName))
+            {
+                throw new ArgumentException(Microsoft.Azure.Commands.Network.Properties.Resources.ResourceNotFound);
+            }
+
             List<PSVpnClientConnectionHealthDetail> vpnclientConnectionHealths = new List<PSVpnClientConnectionHealthDetail>();
-            foreach(var h in this.VirtualNetworkGatewayClient.GetVpnclientConnectionHealth(this.ResourceGroupName, this.VirtualNetworkGatewayName).Value)
+            foreach(var h in VirtualNetworkGatewayClient.GetVpnclientConnectionHealth(ResourceGroupName, VirtualNetworkGatewayName).Value)
             {
                 vpnclientConnectionHealths.Add(NetworkResourceManagerProfile.Mapper.Map<PSVpnClientConnectionHealthDetail>(h));
             }            
