c89bb3095973c45fe71b52fe69651f4f8145aae8
Nick Schonning <nschonni@gmail.com>
2019-07-12 03:46:12 -0400

    typo: pathto -> path to

--- src/DataLakeStore/DataLakeStore/DataPlaneCommands/ExportAzureRmDataLakeStoreChildItemProperties.cs
+++ src/DataLakeStore/DataLakeStore/DataPlaneCommands/ExportAzureRmDataLakeStoreChildItemProperties.cs
@@ -36,7 +36,7 @@ namespace Microsoft.Azure.Commands.DataLakeStore
         [Parameter(ValueFromPipelineByPropertyName = true, Mandatory = false,
             HelpMessage =
                 "Save output to ADL Store, in the same account. The OutputPath parameter should be the full ADL path to output to." + 
-                "Default is to save to a local file. In that case, OutputPath specifies the pathto local file.")]
+                "Default is to save to a local file. In that case, OutputPath specifies the path to local file.")]
         public SwitchParameter SaveToAdl { get; set; }
 
         [Parameter(ValueFromPipelineByPropertyName = true, Mandatory = false,
