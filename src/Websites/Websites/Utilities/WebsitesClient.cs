1d1a62ce7e4c1310979e8082ed1088f07ae36c7a
Vini Soto <18271663+vinisoto@users.noreply.github.com>
2019-07-12 15:13:16 -0700

    Fixing comparison

--- src/Websites/Websites/Utilities/WebsitesClient.cs
+++ src/Websites/Websites/Utilities/WebsitesClient.cs
@@ -323,7 +323,7 @@ namespace Microsoft.Azure.Commands.WebApps.Utilities
                 Version minimumVersion = new Version(6, 1, 0, 0);
 
                 // if there are no compatible versions subsequent to the minimum versions, we don't continue because the command will fail
-                if (compatibleVersions.Where(v => v.CompareTo(minimumVersion) >= 0).Count() > 0)
+                if (compatibleVersions.Where(v => v.CompareTo(minimumVersion) > 0).Count() > 0)
                 {
                     WriteError(Properties.Resources.EnterContainerPSSessionPSCoreVersionNotSupported);
 
