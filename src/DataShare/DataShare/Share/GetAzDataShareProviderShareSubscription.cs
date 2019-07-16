a1cf32d5a807907a6ccfd470ec2c483a39300891
Manasa Kadiri <makadiri@microsoft.com>
2019-07-16 15:34:40 -0700

    fixed tests

--- src/DataShare/DataShare/Share/GetAzDataShareProviderShareSubscription.cs
+++ src/DataShare/DataShare/Share/GetAzDataShareProviderShareSubscription.cs
@@ -104,7 +104,7 @@ namespace Microsoft.Azure.Commands.DataShare.Share
 
         public override void ExecuteCmdlet()
         {
-            if (this.ParameterSetName.Equals(ParameterSetNames.ResourceIdParameterSet))
+            if (this.ParameterSetName.Equals(ParameterSetNames.ProviderShareSubscriptionIdParameterSet))
             {
                 var parsedResourceId = new ResourceIdentifier(this.ResourceId);
                 this.AccountName = parsedResourceId.GetAccountName();
