dd46bbb55a6350c2e9d6bd901e9f2d91864dc228
pi1024e <doremylover123@gmail.com>
2019-07-20 13:31:08 -0400

    Revert "Formatting fixes"
    This reverts commit 6aca296a0310e9ef8b8edf4f85ae8b9d3435a554.

--- src/Security/Security/Cmdlets/Pricings/GetPricings.cs
+++ src/Security/Security/Cmdlets/Pricings/GetPricings.cs
@@ -46,6 +46,7 @@ namespace Microsoft.Azure.Commands.Security.Cmdlets.Pricings
                     break;
                 case ParameterSetNames.ResourceId:
                     pricing = SecurityCenterClient.Pricings.GetWithHttpMessagesAsync(AzureIdUtilities.GetResourceName(ResourceId)).GetAwaiter().GetResult().Body;
+
                     WriteObject(pricing.ConvertToPSType(), enumerateCollection: false);
                     break;
                 default:
