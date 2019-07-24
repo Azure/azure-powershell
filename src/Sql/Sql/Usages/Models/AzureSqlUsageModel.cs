7eabffaa8b2cbf8d89c5e52eabbdafd0f14d4069
John Paul Kee <joke@microsoft.com>
2019-07-24 16:05:16 -0700

    Add instance pool and instance pool usages cmdlets

---
+++ src/Sql/Sql/Usages/Models/AzureSqlUsageModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.Commands.Sql.Usages.Models
+{
+    public class AzureSqlUsageModel
+    {
+        /// <summary>
+        /// Gets or sets the usage id
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the usage current value
+        /// </summary>
+        public int? CurrentValue { get; set; }
+
+        /// <summary>
+        /// Gets or sets the usage limit
+        /// </summary>
+        public int? Limit { get; set; }
+
+        /// <summary>
+        /// Gets or sets the usage requested limit
+        /// </summary>
+        public int? RequestedLimit { get; set; }
+
+        /// <summary>
+        /// Gets or sets the usage unit
+        /// </summary>
+        public string Unit { get; set; }
+
+        /// <summary>
+        /// Gets or sets the usage name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the usage type
+        /// </summary>
+        public string Type { get; set; }
+    }
+}
