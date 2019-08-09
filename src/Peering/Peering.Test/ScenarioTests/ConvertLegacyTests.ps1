4ecf8fa1f3f9739b753635b8d62a5d6902f2278f
Riley McKenna <rimckenn@microsoft.com>
2019-08-09 10:23:36 -0700

    Revert "Removed convert legacy test. Not supported by backend yet."
    This reverts commit fbf1e011ff11adb7c12333a8c1880752c2da8aaa.

---
+++ src/Peering/Peering.Test/ScenarioTests/ConvertLegacyTests.ps1
@@ -0,0 +1,61 @@
+﻿# ----------------------------------------------------------------------------------
+#
+# Copyright Microsoft Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------------
+<#
+.SYNOPSIS
+GetLocationKindExchange 
+#>
+function Test-ConvertLegacyKindExchangeAshburn
+{
+try{
+#must be hard coded asn because they have legacy items.
+	$peerAsn = makePeerAsn 15224;
+	$name = getPeeringVariable "Name" "AS15224_Ashburn_Exchange"
+	$rg = getPeeringVariable "ResourceGroupName" "Building40"
+    $legacy = Get-AzLegacyPeering -Kind Exchange -PeeringLocation Ashburn 
+	Assert-NotNull $legacy
+	Assert-True {$legacy.Count -ge 1}
+	$peering = $legacy | New-AzPeering -ResourceGroupName $rg -Name $name -PeerAsnResourceId $peerAsn.Id
+	}finally {
+			$isRemoved = Remove-AzPeerAsn -Name $peerAsn.Name -Force -PassThru
+		Assert-True {$isRemoved}
+	}
+}
+
+<#
+.SYNOPSIS
+GetLocationKindDirect
+#>
+function Test-ConvertLegacyKindDirectAmsterdamWithNewConnection
+{
+try{
+#must be hard coded asn because they have legacy items.
+	$peerAsn = makePeerAsn 15224
+	$name = getPeeringVariable "Name" "AS15224_Amsterdam_Exchange"
+	$rg = getPeeringVariable "ResourceGroupName" "Building40"
+    $legacy = Get-AzLegacyPeering -Kind Exchange -PeeringLocation Amsterdam 
+	Assert-NotNull $legacy
+	Assert-True {$legacy.Count -ge 1}
+	#has to be hard coded becuase this ip address isnt used.
+	$ipaddress = getPeeringVariable "ipaddress" " 80.249.208.103"
+	$facilityId = 26
+	$maxv4 = maxAdvertisedIpv4
+	$connection = New-AzPeeringExchangeConnectionObject -PeeringDbFacilityId $facilityId -MaxPrefixesAdvertisedIPv4 $maxv4 -PeerSessionIPv4Address $ipaddress
+	$peering = $legacy | New-AzPeering -ResourceGroupName $rg -Name $name -PeerAsnResourceId $peerAsn.Id -ExchangeConnection $connection
+	}
+	finally {
+		$isRemoved = Remove-AzPeerAsn -Name $peerAsn.Name -Force -PassThru
+		Assert-True {$isRemoved}
+	}
+}
+
