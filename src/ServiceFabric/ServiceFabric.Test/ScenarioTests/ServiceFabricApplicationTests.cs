8f6d60ec51192a6798a65eeebd27c043e1d127e8
Alfredo Santamaria Gomez <alsantam@microsoft.com>
2019-08-12 22:05:52 -0700

    add type, type version, app and service tests

---
+++ src/ServiceFabric/ServiceFabric.Test/ScenarioTests/ServiceFabricApplicationTests.cs
@@ -0,0 +1,69 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.IO;
+using Microsoft.Azure.Commands.ServiceFabric.Commands;
+using Microsoft.Azure.Commands.ServiceFabric.Common;
+using Microsoft.Azure.ServiceManagement.Common.Models;
+using Microsoft.WindowsAzure.Commands.Common;
+using Microsoft.WindowsAzure.Commands.ScenarioTest;
+using Microsoft.WindowsAzure.Commands.Test.Utilities.Common;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.Azure.Commands.ServiceFabric.Test.ScenarioTests
+{
+    public class ServiceFabricApplicationTests : RMTestBase
+    {
+        public XunitTracingInterceptor _logger;
+
+        public ServiceFabricApplicationTests(ITestOutputHelper output)
+        {
+            _logger = new XunitTracingInterceptor(output);
+            XunitTracingInterceptor.AddToContext(_logger);
+            
+            ServiceFabricCmdletBase.WriteVerboseIntervalInSec = 0;
+            ServiceFabricCmdletBase.RunningTest = true;
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestAppType()
+        {
+            TestController.NewInstance.RunPsTest(_logger, "Test-AppType");
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestAppTypeVersion()
+        {
+            TestController.NewInstance.RunPsTest(_logger, "Test-AppTypeVersion");
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestApp()
+        {
+            TestController.NewInstance.RunPsTest(_logger, "Test-App");
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestService()
+        {
+            TestController.NewInstance.RunPsTest(_logger, "Test-Service");
+        }
+    }
+}
