36478abc9216fd55363388ffe95308fb6f68f104
Anton Evseev <v-anevse@microsoft.com>
2019-08-15 13:06:59 -0700

    Improve handling of request error exceptions from SDK

--- src/Network/Network/Common/NetworkResourceManagerProfile.cs
+++ src/Network/Network/Common/NetworkResourceManagerProfile.cs
@@ -56,6 +56,12 @@ namespace Microsoft.Azure.Commands.Network
                 cfg.CreateMap<CNM.PSResourceId, MNM.SubResource>();
                 cfg.CreateMap<MNM.SubResource, CNM.PSResourceId>();
 
+                // Map request error exceptions between SDK and PowerShell
+                cfg.CreateMap<MNM.Error, Rest.Azure.CloudError>();
+                cfg.CreateMap<Rest.Azure.CloudError, MNM.Error>();
+                cfg.CreateMap<MNM.ErrorException, Rest.Azure.CloudException>();
+                cfg.CreateMap<Rest.Azure.CloudException, MNM.ErrorException>();
+
                 // Managed Service Identity
                 cfg.CreateMap<CNM.PSManagedServiceIdentity, MNM.ManagedServiceIdentity>();
                 cfg.CreateMap<MNM.ManagedServiceIdentity, CNM.PSManagedServiceIdentity>();
