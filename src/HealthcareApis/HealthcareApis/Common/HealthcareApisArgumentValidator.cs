f63f2cb1e32e7cf90ead9a0e318a16e9c83058b5
Pooja Adhikari <poadhika@microsoft.com>
2019-09-04 10:35:17 -0700

    Requested Updates on PR

--- src/HealthcareApis/HealthcareApis/Common/HealthcareApisArgumentValidator.cs
+++ src/HealthcareApis/HealthcareApis/Common/HealthcareApisArgumentValidator.cs
@@ -1,18 +1,28 @@
-﻿using Microsoft.Azure.Commands.HealthcareApis.Properties;
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Commands.HealthcareApis.Properties;
 using System;
-using System.Collections.Generic;
 using System.Management.Automation;
-using System.Text;
-using System.Text.RegularExpressions;
 
 namespace Microsoft.Azure.PowerShell.Cmdlets.HealthcareApis.Common
 {
     public static class HealthcareApisArgumentValidator
     {
-
-        public static bool ValidateObjectId(string AccessPolicyObjectId)
+        public static bool ValidateObjectId(string accessPolicyObjectId)
         {
-            if (!Guid.TryParse(AccessPolicyObjectId, out _))
+            if (!Guid.TryParse(accessPolicyObjectId, out _))
             {
                 throw new PSArgumentException(Resources.invalidAccessPolicyObjectIdMessage);
             }
