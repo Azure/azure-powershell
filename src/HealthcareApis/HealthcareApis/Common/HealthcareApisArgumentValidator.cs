1d20aa082827b3e74f4a95e9214bb306f9215cb9
Pooja Adhikari <poadhika@microsoft.com>
2019-09-03 17:16:20 -0700

    Enable  updating Tags and added validation message for accesspolicyobject ID

---
+++ src/HealthcareApis/HealthcareApis/Common/HealthcareApisArgumentValidator.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Azure.Commands.HealthcareApis.Properties;
+using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Microsoft.Azure.PowerShell.Cmdlets.HealthcareApis.Common
+{
+    public static class HealthcareApisArgumentValidator
+    {
+
+        public static bool ValidateObjectId(string AccessPolicyObjectId)
+        {
+            if (!Guid.TryParse(AccessPolicyObjectId, out _))
+            {
+                throw new PSArgumentException(Resources.invalidAccessPolicyObjectIdMessage);
+            }
+
+            return true;
+        }
+    }
+}
