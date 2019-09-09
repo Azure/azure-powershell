fbb5efcf7dcc552a57292d12b020d0b483234d96
Zhenyu Zhou <zhezhou@microsoft.com>
2019-09-09 19:12:35 +0800

    Call out breaking change and remove Remove outdated breaking change Add suppression

--- src/HDInsight/HDInsight/ManagementCommands/GrantAzureHDInsightRdpServicesAccessCommand.cs
+++ src/HDInsight/HDInsight/ManagementCommands/GrantAzureHDInsightRdpServicesAccessCommand.cs
@@ -16,11 +16,13 @@ using Microsoft.Azure.Commands.HDInsight.Commands;
 using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
 using Microsoft.Azure.Management.HDInsight.Models;
 using Microsoft.WindowsAzure.Commands.Common;
+using Microsoft.WindowsAzure.Commands.Common.CustomAttributes;
 using System;
 using System.Management.Automation;
 
 namespace Microsoft.Azure.Commands.HDInsight
 {
+    [CmdletDeprecation("3.0.0")]
     [Cmdlet("Grant", ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "HDInsightRdpServicesAccess"),OutputType(typeof(void))]
     public class GrantAzureHDInsightRdpServicesAccessCommand : HDInsightCmdletBase
     {
