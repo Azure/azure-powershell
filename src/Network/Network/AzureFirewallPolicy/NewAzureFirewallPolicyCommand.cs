58ad9442790b456512806917b27886e1943ed175
samankal@microsoft.com <samankal@microsoft.com>
2019-09-23 15:55:00 -0700

    firewall policy progress

---
+++ src/Network/Network/AzureFirewallPolicy/NewAzureFirewallPolicyCommand.cs
@@ -0,0 +1,223 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using Microsoft.Azure.Commands.Network.Models;
+using Microsoft.Azure.Commands.ResourceManager.Common.Tags;
+using Microsoft.Azure.Management.Network;
+using Microsoft.WindowsAzure.Commands.Common.CustomAttributes;
+using MNM = Microsoft.Azure.Management.Network.Models;
+
+namespace Microsoft.Azure.Commands.Network
+{
+    [Cmdlet(VerbsCommon.New, ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "FirewallPolicy", SupportsShouldProcess = true, DefaultParameterSetName = DefaultParameterSet), OutputType(typeof(PSAzureFirewall))]
+    public class NewAzureFirewallPolicyCommand : AzureFirewallPolicyBaseCmdlet
+    {
+        private const string DefaultParameterSet = "Default";
+
+        private PSVirtualNetwork virtualNetwork;
+        private PSPublicIpAddress[] publicIpAddresses;
+
+        [Alias("ResourceName")]
+        [Parameter(
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The resource name.")]
+        [ValidateNotNullOrEmpty]
+        public virtual string Name { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The resource group name.")]
+        [ValidateNotNullOrEmpty]
+        public virtual string ResourceGroupName { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "location.")]
+        [ValidateNotNullOrEmpty]
+        public virtual string Location { get; set; }
+
+        [CmdletParameterBreakingChange(
+            "VirtualNetworkName",
+            deprecateByVersion: "2.0.0",
+            ChangeDescription = "This parameter will be removed in an upcoming breaking change release. After this point the Virtual Network will be provided as an object instead of a string.", 
+            OldWay = "New-AzFirewall -VirtualNetworkName \"vnet-name\"",
+            NewWay = "New-AzFirewall -VirtualNetwork $vnet",
+            OldParamaterType = typeof(string),
+            NewParameterTypeName = nameof(PSVirtualNetwork),
+            ReplaceMentCmdletParameterName = "VirtualNetwork")]
+        [Parameter(
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            ParameterSetName = "OldIpConfigurationParameterValues",
+            HelpMessage = "Virtual Network Name")]
+        [ValidateNotNullOrEmpty]
+        public string VirtualNetworkName { get; set; }
+
+        [CmdletParameterBreakingChange(
+            "PublicIpName",
+            deprecateByVersion: "2.0.0",
+            ChangeDescription = "This parameter will be removed in an upcoming breaking change release. After this point the Public IP Address will be provided as a list of one or more objects instead of a string.",
+            OldWay = "New-AzFirewall -PublicIpName \"public-ip-name\"",
+            NewWay = "New-AzFirewall -PublicIpAddress @($publicip1, $publicip2)",
+            OldParamaterType = typeof(string),
+            NewParameterTypeName = "List<PSPublicIpAddress>",
+            ReplaceMentCmdletParameterName = "PublicIpAddress")]
+        [Parameter(
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            ParameterSetName = "OldIpConfigurationParameterValues",
+            HelpMessage = "Public IP address name. The Public IP must use Standard SKU and must belong to the same resource group as the Firewall.")]
+        [ValidateNotNullOrEmpty]
+        public string PublicIpName { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            ValueFromPipeline = true,
+            ParameterSetName = "IpConfigurationParameterValues",
+            HelpMessage = "Virtual Network")]
+        [ValidateNotNullOrEmpty]
+        public PSVirtualNetwork VirtualNetwork { get; set; }
+
+        [Parameter(
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            ParameterSetName = "IpConfigurationParameterValues",
+            HelpMessage = "One or more Public IP Addresses. The Public IP addresses must use Standard SKU and must belong to the same resource group as the Firewall.")]
+        [ValidateNotNullOrEmpty]
+        public PSPublicIpAddress[] PublicIpAddress { get; set; }
+
+        [Parameter(
+            Mandatory = false,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The list of AzureFirewallApplicationRuleCollections")]
+        public PSAzureFirewallApplicationRuleCollection[] ApplicationRuleCollection { get; set; }
+
+        [Parameter(
+            Mandatory = false,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The list of AzureFirewallNatRuleCollections")]
+        public PSAzureFirewallNatRuleCollection[] NatRuleCollection { get; set; }
+
+        [Parameter(
+            Mandatory = false,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The list of AzureFirewallNetworkRuleCollections")]
+        public PSAzureFirewallNetworkRuleCollection[] NetworkRuleCollection { get; set; }
+
+        [Parameter(
+            Mandatory = false,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The operation mode for Threat Intelligence.")]
+        [ValidateSet(
+            MNM.AzureFirewallThreatIntelMode.Alert,
+            MNM.AzureFirewallThreatIntelMode.Deny,
+            MNM.AzureFirewallThreatIntelMode.Off,
+            IgnoreCase = false)]
+        public string ThreatIntelMode { get; set; }
+
+        [Parameter(
+            Mandatory = false,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "A hashtable which represents resource tags.")]
+        public Hashtable Tag { get; set; }
+
+        [Parameter(
+            Mandatory = false,
+            HelpMessage = "Do not ask for confirmation if you want to overwrite a resource")]
+        public SwitchParameter Force { get; set; }
+
+        [Parameter(
+            Mandatory = false,
+            HelpMessage = "Run cmdlet in the background")]
+        public SwitchParameter AsJob { get; set; }
+
+        [Parameter(
+            Mandatory = false,
+            HelpMessage = "A list of availability zones denoting where the firewall needs to come from.")]
+        public string[] Zone { get; set; }
+
+        public override void Execute()
+        {
+            // Old params provided - Get the virtual network, get the public IP address
+            if (!string.IsNullOrEmpty(VirtualNetworkName))
+            {
+                var vnet = this.VirtualNetworkClient.Get(this.ResourceGroupName, VirtualNetworkName);
+                this.virtualNetwork = NetworkResourceManagerProfile.Mapper.Map<PSVirtualNetwork>(vnet);
+
+                var publicIp = this.PublicIPAddressesClient.Get(this.ResourceGroupName, PublicIpName);
+                this.publicIpAddresses = new PSPublicIpAddress[]
+                {
+                    NetworkResourceManagerProfile.Mapper.Map<PSPublicIpAddress>(publicIp)
+                };
+            }
+            // New params
+            else if (VirtualNetwork != null)
+            {
+                this.virtualNetwork = VirtualNetwork;
+                this.publicIpAddresses = PublicIpAddress;
+            }
+
+            base.Execute();
+
+            var present = this.IsAzureFirewallPresent(this.ResourceGroupName, this.Name);
+            ConfirmAction(
+                Force.IsPresent,
+                string.Format(Properties.Resources.OverwritingResource, Name),
+                Properties.Resources.CreatingResourceMessage,
+                Name,
+                () => WriteObject(this.CreateAzureFirewall()),
+                () => present);
+        }
+
+        private PSAzureFirewall CreateAzureFirewall()
+        {
+            var firewall = new PSAzureFirewall()
+            {
+                Name = this.Name,
+                ResourceGroupName = this.ResourceGroupName,
+                Location = this.Location,
+                ApplicationRuleCollections = this.ApplicationRuleCollection?.ToList(),
+                NatRuleCollections = this.NatRuleCollection?.ToList(),
+                NetworkRuleCollections = this.NetworkRuleCollection?.ToList(),
+                ThreatIntelMode = this.ThreatIntelMode ?? MNM.AzureFirewallThreatIntelMode.Alert
+            };
+
+            if (this.Zone != null)
+            {
+                firewall.Zones = this.Zone?.ToList();
+            }
+
+            if (this.virtualNetwork != null)
+            {
+                firewall.Allocate(this.virtualNetwork, this.publicIpAddresses);
+            }
+
+            // Map to the sdk object
+            var azureFirewallModel = NetworkResourceManagerProfile.Mapper.Map<MNM.AzureFirewall>(firewall);
+            azureFirewallModel.Tags = TagsConversionHelper.CreateTagDictionary(this.Tag, validate: true);
+
+            // Execute the Create AzureFirewall call
+            this.AzureFirewallClient.CreateOrUpdate(this.ResourceGroupName, this.Name, azureFirewallModel);
+            return this.GetAzureFirewall(this.ResourceGroupName, this.Name);
+        }
+    }
+}
---
+++ src/Network/Network/Models/AzureFirewallPolicy/PSAzureFirewallPolicy.cs
@@ -0,0 +1,293 @@
+﻿//
+// Copyright (c) Microsoft.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Microsoft.Azure.Commands.Network.Models
+{
+    public class PSAzureFirewallPolicy : PSTopLevelResource
+    {
+        private const string AzureFirewallSubnetName = "AzureFirewallSubnet";
+        private const string AzureFirewallIpConfigurationName = "AzureFirewallIpConfiguration";
+        
+        public List<PSAzureFirewallPolicyIpConfiguration> IpConfigurations { get; set; }
+
+        public List<PSAzureFirewallPolicyApplicationRuleCollection> ApplicationRuleCollections { get; set; }
+
+        public List<PSAzureFirewallPolicyNatRuleCollection> NatRuleCollections { get; set; }
+
+        public List<PSAzureFirewallPolicyNetworkRuleCollection> NetworkRuleCollections { get; set; }
+
+        public string ThreatIntelMode { get; set; }
+
+        public string ProvisioningState { get; set; }
+
+        public List<string> Zones { get; set; }
+
+        [JsonIgnore]
+        public string IpConfigurationsText
+        {
+            get { return JsonConvert.SerializeObject(IpConfigurations, Formatting.Indented, new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore }); }
+        }
+
+        [JsonIgnore]
+        public string ApplicationRuleCollectionsText
+        {
+            get { return JsonConvert.SerializeObject(ApplicationRuleCollections, Formatting.Indented); }
+        }
+
+        [JsonIgnore]
+        public string NatRuleCollectionsText
+        {
+            get { return JsonConvert.SerializeObject(NatRuleCollections, Formatting.Indented); }
+        }
+
+        [JsonIgnore]
+        public string NetworkRuleCollectionsText
+        {
+            get { return JsonConvert.SerializeObject(NetworkRuleCollections, Formatting.Indented); }
+        }
+
+        #region Ip Configuration Operations
+
+        public void Allocate(PSVirtualNetwork virtualNetwork, PSPublicIpAddress[] publicIpAddresses)
+        {
+            if (virtualNetwork == null)
+            {
+                throw new ArgumentNullException(nameof(virtualNetwork), "Virtual Network cannot be null!");
+            }
+
+            
+            if (publicIpAddresses == null || publicIpAddresses.Count() == 0)
+            {
+                throw new ArgumentNullException(nameof(publicIpAddresses), "Public IP Addresses cannot be null or empty!");
+            }
+
+            PSSubnet firewallSubnet = null;
+            try
+            {
+                firewallSubnet = virtualNetwork.Subnets.Single(subnet => AzureFirewallSubnetName.Equals(subnet.Name));
+            }
+            catch (InvalidOperationException)
+            {
+                throw new ArgumentException($"Virtual Network {virtualNetwork.Name} should contain a Subnet named {AzureFirewallSubnetName}");
+            }
+
+            this.IpConfigurations = new List<PSAzureFirewallPolicyIpConfiguration>();
+
+            for(var i = 0; i < publicIpAddresses.Count(); i++)
+            {
+                this.IpConfigurations.Add(
+                    new PSAzureFirewallPolicyIpConfiguration
+                    {
+                        Name = $"{AzureFirewallIpConfigurationName}{i}",
+                        PublicIpAddress = new PSResourceId { Id = publicIpAddresses[i].Id }
+                    });
+            }
+
+            this.IpConfigurations[0].Subnet = new PSResourceId { Id = firewallSubnet.Id };
+        }
+
+        public void Deallocate()
+        {
+            this.IpConfigurations = new List<PSAzureFirewallPolicyIpConfiguration> ();
+        }
+
+        public void AddPublicIpAddress(PSPublicIpAddress publicIpAddress)
+        {
+            if (publicIpAddress == null)
+            {
+                throw new ArgumentNullException(nameof(publicIpAddress), "Public IP Address cannot be null!");
+            }
+
+            if (this.IpConfigurations.Count > 0)
+            {
+                var conflictingIpConfig = this.IpConfigurations.SingleOrDefault
+                    (ipConfig => string.Equals(ipConfig.PublicIpAddress?.Id, publicIpAddress.Id, System.StringComparison.CurrentCultureIgnoreCase));
+
+                if (conflictingIpConfig != null)
+                {
+                    throw new ArgumentException($"Public IP Address {publicIpAddress.Id} is already attached to firewall {this.Name}");
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException($"Please invoke {nameof(Allocate)} to attach the firewall to a Virtual Network");
+            }
+
+            this.IpConfigurations.Add(
+                new PSAzureFirewallPolicyIpConfiguration
+                {
+                    Name = $"{AzureFirewallIpConfigurationName}{this.IpConfigurations.Count}",
+                    PublicIpAddress = new PSResourceId { Id = publicIpAddress.Id }
+                });
+        }
+
+        public void RemovePublicIpAddress(PSPublicIpAddress publicIpAddress)
+        {
+            if (publicIpAddress == null)
+            {
+                throw new ArgumentNullException(nameof(publicIpAddress), "Public IP Address cannot be null!");
+            }
+
+            var ipConfigToRemove = this.IpConfigurations.SingleOrDefault
+                (ipConfig => string.Equals(ipConfig.PublicIpAddress?.Id, publicIpAddress.Id, System.StringComparison.CurrentCultureIgnoreCase));
+
+            if (ipConfigToRemove == null)
+            {
+                throw new ArgumentException($"Public IP Address {publicIpAddress.Id} is not attached to firewall {this.Name}");
+            }
+
+            if (this.IpConfigurations.Count == 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"WARNING: Removing the last Public IP Address, this will deallocate the firewall. You will have to invoke {nameof(Allocate)} to reallocate it.");
+                Console.ResetColor();
+            }
+
+            this.IpConfigurations.Remove(ipConfigToRemove);
+        }
+
+        #endregion // Ip Configuration Operations
+
+        #region Application Rule Collections Operations
+
+        public void AddApplicationRuleCollection(PSAzureFirewallPolicyApplicationRuleCollection ruleCollection)
+        {
+            this.ApplicationRuleCollections = AddRuleCollection(ruleCollection, this.ApplicationRuleCollections);
+        }
+
+        public PSAzureFirewallPolicyApplicationRuleCollection GetApplicationRuleCollectionByName(string ruleCollectionName)
+        {
+            return GetRuleCollectionByName(ruleCollectionName, this.ApplicationRuleCollections);
+        }
+
+        public PSAzureFirewallPolicyApplicationRuleCollection GetApplicationRuleCollectionByPriority(uint priority)
+        {
+            return this.ApplicationRuleCollections?.FirstOrDefault(rc => rc.Priority == priority);
+        }
+
+        public void RemoveApplicationRuleCollectionByName(string ruleCollectionName)
+        {
+            var ruleCollection = this.GetApplicationRuleCollectionByName(ruleCollectionName);
+            this.ApplicationRuleCollections.Remove(ruleCollection);
+        }
+
+        public void RemoveApplicationRuleCollectionByPriority(uint priority)
+        {
+            var ruleCollection = this.GetApplicationRuleCollectionByPriority(priority);
+            this.ApplicationRuleCollections.Remove(ruleCollection);
+        }
+
+        #endregion // Application Rule Collections Operations
+
+        #region NAT Rule Collections Operations
+
+        public void AddNatRuleCollection(PSAzureFirewallPolicyNatRuleCollection ruleCollection)
+        {
+            this.NatRuleCollections = AddRuleCollection(ruleCollection, this.NatRuleCollections);
+        }
+
+        public PSAzureFirewallPolicyNatRuleCollection GetNatRuleCollectionByName(string ruleCollectionName)
+        {
+            return GetRuleCollectionByName(ruleCollectionName, this.NatRuleCollections);
+        }
+
+        public PSAzureFirewallPolicyNatRuleCollection GetNatRuleCollectionByPriority(uint priority)
+        {
+            return this.NatRuleCollections?.FirstOrDefault(rc => rc.Priority == priority);
+        }
+
+        public void RemoveNatRuleCollectionByName(string ruleCollectionName)
+        {
+            var ruleCollection = this.GetNatRuleCollectionByName(ruleCollectionName);
+            this.NatRuleCollections.Remove(ruleCollection);
+        }
+
+        public void RemoveNatRuleCollectionByPriority(uint priority)
+        {
+            var ruleCollection = this.GetNatRuleCollectionByPriority(priority);
+            this.NatRuleCollections.Remove(ruleCollection);
+        }
+
+        #endregion // NAT Rule Collections Operations
+
+        #region Network Rule Collections Operations
+
+        public void AddNetworkRuleCollection(PSAzureFirewallPolicyNetworkRuleCollection ruleCollection)
+        {
+            this.NetworkRuleCollections = AddRuleCollection(ruleCollection, this.NetworkRuleCollections);
+        }
+
+        public PSAzureFirewallPolicyNetworkRuleCollection GetNetworkRuleCollectionByName(string ruleCollectionName)
+        {
+            return this.GetRuleCollectionByName(ruleCollectionName, this.NetworkRuleCollections);
+        }
+
+        public PSAzureFirewallPolicyNetworkRuleCollection GetNetworkRuleCollectionByPriority(uint priority)
+        {
+            return this.NetworkRuleCollections?.FirstOrDefault(rc => rc.Priority == priority);
+        }
+
+        public void RemoveNetworkRuleCollectionByName(string ruleCollectionName)
+        {
+            var ruleCollection = this.GetNetworkRuleCollectionByName(ruleCollectionName);
+            this.NetworkRuleCollections?.Remove(ruleCollection);
+        }
+
+        public void RemoveNetworkRuleCollectionByPriority(uint priority)
+        {
+            var ruleCollection = this.GetNetworkRuleCollectionByPriority(priority);
+            this.NetworkRuleCollections?.Remove(ruleCollection);
+        }
+
+        #endregion // Application Rule Collections Operations
+
+        #region Private Methods
+
+        private List<BaseRuleCollection> AddRuleCollection<BaseRuleCollection>(BaseRuleCollection ruleCollection, List<BaseRuleCollection> existingRuleCollections) where BaseRuleCollection : PSAzureFirewallPolicyBaseRuleCollection
+        {
+            if (existingRuleCollections == null)
+            {
+                existingRuleCollections = new List<BaseRuleCollection>();
+            }
+
+            existingRuleCollections.Add(ruleCollection);
+            return existingRuleCollections;
+        }
+
+        private BaseRuleCollection GetRuleCollectionByName<BaseRuleCollection> (string ruleCollectionName, List<BaseRuleCollection> ruleCollections) where BaseRuleCollection : PSAzureFirewallPolicyBaseRuleCollection
+        {
+            if (string.IsNullOrEmpty(ruleCollectionName))
+            {
+                throw new ArgumentException($"Rule Collection name cannot be an empty string.");
+            }
+
+            var ruleCollection = ruleCollections?.FirstOrDefault(rc => ruleCollectionName.Equals(rc.Name));
+
+            if (ruleCollection == null)
+            {
+                throw new ArgumentException($"Rule Collection with name {ruleCollectionName} does not exist.");
+            }
+
+            return ruleCollection;
+        }
+
+        #endregion // Private Methods
+    }
+}
