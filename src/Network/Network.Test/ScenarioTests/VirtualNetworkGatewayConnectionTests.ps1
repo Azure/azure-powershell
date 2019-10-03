df3b80878e29afd567911f216faf9bc66562b83b
Khushboo Baheti <khbaheti@microsoft.com>
2019-10-02 18:09:27 -0700

    fixing Tests

--- src/Network/Network.Test/ScenarioTests/VirtualNetworkGatewayConnectionTests.ps1
+++ src/Network/Network.Test/ScenarioTests/VirtualNetworkGatewayConnectionTests.ps1
@@ -612,6 +612,10 @@ function Test-VirtualNetworkGatewayConnectionPacketCapture
            $now=get-date
            $sasurl = New-AzureStorageContainerSASToken -Name $containerName -Context $context -Permission "rwd" -StartTime $now.AddHours(-1) -ExpiryTime $now.AddDays(1) -FullUri
 	  }
+	  else
+	  {
+	  	   $sasurl = "https://storage/test123?sp=racwdl&stvigopKcy"
+	  }
 
       # Create the Virtual Network1
       $subnet1 = New-AzVirtualNetworkSubnetConfig -Name "GatewaySubnet" -AddressPrefix 10.0.0.0/24
