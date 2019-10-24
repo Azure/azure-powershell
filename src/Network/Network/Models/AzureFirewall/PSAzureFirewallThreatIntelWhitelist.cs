1d9184e9602cdcf0846b6bb5ec76f486a9e91d95
Jiejiong Wu <jiejwu@microsoft.com>
2019-10-24 15:29:45 -0700

    create New-AzFirewallThreatIntelWhitelist cmdlet

--- src/Network/Network/Models/AzureFirewall/PSAzureFirewallThreatIntelWhitelist.cs
+++ src/Network/Network/Models/AzureFirewall/PSAzureFirewallThreatIntelWhitelist.cs
@@ -15,11 +15,7 @@
 
 namespace Microsoft.Azure.Commands.Network.Models
 {
-<<<<<<< Updated upstream
-    class PSAzureFirewallThreatIntelWhitelist
-=======
     public class PSAzureFirewallThreatIntelWhitelist
->>>>>>> Stashed changes
     {
         public string FQDNs { get; set; }
 
