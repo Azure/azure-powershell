e583e7728e362e9e10ac71f180ffe86b67255d29
Sudharsan Sripadham <sudharsan_sb@hotmail.com>
2019-10-25 16:30:10 -0700

    Added Validators/Completers

--- src/Network/Network/IpGroup/GetIpGroupCommand.cs
+++ src/Network/Network/IpGroup/GetIpGroupCommand.cs
@@ -34,6 +34,7 @@ namespace Microsoft.Azure.Commands.Network
             HelpMessage = "The resource group name.")]
         [ValidateNotNullOrEmpty]
         [SupportsWildcards]
+        [ResourceNameCompleter("Microsoft.Network/ipGroups", "ResourceGroupName")]
         public virtual string ResourceGroupName { get; set; }
 
         [Alias("ResourceName")]
