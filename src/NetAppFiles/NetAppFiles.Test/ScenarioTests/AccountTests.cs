ed1ca2e05b7f599ea42ffd3104c4a10969f66f5d
Leonard Francis <leonard@greenqloud.com>
2019-10-31 13:36:16 +0000

    ANF-243 update to 2019-07-01 remove record

--- src/NetAppFiles/NetAppFiles.Test/ScenarioTests/AccountTests.cs
+++ src/NetAppFiles/NetAppFiles.Test/ScenarioTests/AccountTests.cs
@@ -31,7 +31,6 @@ namespace Microsoft.Azure.Commands.NetAppFiles.Test.ScenarioTests.ScenarioTest
         [Trait(Category.AcceptanceType, Category.CheckIn)]
         public void TestAccountCrud()
         {
-            GetSessionsDirectoryPath();
             TestController.NewInstance.RunPowerShellTest(_logger, "Test-AccountCrud");
         }
 
@@ -39,7 +38,6 @@ namespace Microsoft.Azure.Commands.NetAppFiles.Test.ScenarioTests.ScenarioTest
         [Trait(Category.AcceptanceType, Category.CheckIn)]
         public void TestAccountActiveDirectory()
         {
-            GetSessionsDirectoryPath();
             TestController.NewInstance.RunPowerShellTest(_logger, "Test-AccountActiveDirectory");
         }
 
@@ -47,15 +45,7 @@ namespace Microsoft.Azure.Commands.NetAppFiles.Test.ScenarioTests.ScenarioTest
         [Trait(Category.AcceptanceType, Category.CheckIn)]
         public void TestAccountPipelines()
         {
-            GetSessionsDirectoryPath();
             TestController.NewInstance.RunPowerShellTest(_logger, "Test-AccountPipelines");
-        }
-
-        private static void GetSessionsDirectoryPath()
-        {
-            string connectionString = "SubscriptionId=8f38cfec-0ecd-413a-892e-2494f77a3b56;ServicePrincipal=c6c4faba-2b22-44d9-80a4-71ff5b71f811;ServicePrincipalSecret={QR[+@avhE+3qiyY1Xf;AADTenant=72f988bf-86f1-41af-91ab-2d7cd011db47;Environment=Prod;HttpRecorderMode=Record;";
-            System.Environment.SetEnvironmentVariable("TEST_CSM_ORGID_AUTHENTICATION", connectionString, System.EnvironmentVariableTarget.Process);
-            System.Environment.SetEnvironmentVariable("AZURE_TEST_MODE", "Record", System.EnvironmentVariableTarget.Process);
         }
     }
 }
