3f4245d9973f6889e324e893e8a36d1fdac9daf3
Mohit Dewan <mdewan@microsoft.com>
2019-12-26 21:30:20 -0500

    cleaning usings

--- src/Authentication.Abstractions/AzureEnvironment.cs
+++ src/Authentication.Abstractions/AzureEnvironment.cs
@@ -20,7 +20,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace Microsoft.Azure.Commands.Common.Authentication.Abstractions
