fd4f9b9467074d2fe504405f3f799263ffd70440
Mohit Dewan <mdewan@microsoft.com>
2019-12-27 07:29:07 -0500

    Removing comments and dead code

--- src/Authentication.Abstractions/AssemblyInfo.cs
+++ src/Authentication.Abstractions/AssemblyInfo.cs
@@ -4,9 +4,3 @@ using System.Runtime.CompilerServices;
 #if !SIGN
     [assembly: InternalsVisibleTo("Authentication.Abstractions.Test")]
 #endif
-namespace Microsoft.Azure.Commands.Common.Authentication.Abstractions
-{
-    class AssemblyInfo
-    {
-    }
-}
