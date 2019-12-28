df47690b583ace8eb91bca76aa869787ce799230
Hossam Fattah <hofattah@microsoft.com>
2019-12-27 17:02:28 -0800

    CMv2 fixes after testing

--- src/Network/Network/NetworkWatcher/ConnectionMonitor/NewAzureNetworkWatcherConnectionMonitorObject.cs
+++ src/Network/Network/NetworkWatcher/ConnectionMonitor/NewAzureNetworkWatcherConnectionMonitorObject.cs
@@ -27,20 +27,18 @@ using MNM = Microsoft.Azure.Management.Network.Models;
 
 namespace Microsoft.Azure.Commands.Network
 {
-    [Cmdlet("New", ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "NetworkWatcherConnectionMonitorObject", SupportsShouldProcess = true, DefaultParameterSetName = "SetByName"), OutputType(typeof(PSNetworkWatcherConnectionMonitorObject))]
-    public class NewAzureNetworkWatcherConnectionMonitorObjectCommand : ConnectionMonitorBaseCmdlet
+    [Cmdlet("New", ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "NetworkWatcherConnectionMonitorObject", SupportsShouldProcess = true), OutputType(typeof(PSNetworkWatcherConnectionMonitorObject))]
+    public class AzureNetworkWatcherConnectionMonitorObjectCommand : ConnectionMonitorBaseCmdlet
     {
         [Parameter(
             Mandatory = true,
             HelpMessage = "The name of network watcher.")]
-        [ResourceNameCompleter("Microsoft.Network/networkWatchers", "ResourceGroupName")]
         [ValidateNotNullOrEmpty]
         public string NetworkWatcherName { get; set; }
 
         [Parameter(
             Mandatory = true,
             HelpMessage = "The name of the network watcher resource group.")]
-        [ResourceGroupCompleter]
         [ValidateNotNullOrEmpty]
         public string ResourceGroupName { get; set; }
 
@@ -89,99 +87,118 @@ namespace Microsoft.Azure.Commands.Network
 
         public bool Validate()
         {
-            foreach (PSNetworkWatcherConnectionMonitorTestGroupObject TestGroup in this.TestGroup)
+
+            if (this.TestGroup != null)
             {
-                // validate mandatory parameters
-                if (string.IsNullOrEmpty(TestGroup.Name) || TestGroup.Sources == null || TestGroup.Destinations == null)
+                foreach (PSNetworkWatcherConnectionMonitorTestGroupObject TestGroup in this.TestGroup)
                 {
-                    throw new ArgumentException("Test group is missing one or more mandatory parameter");
-                }
-
-                // validate Source and Destination Endpoints
-                List<PSNetworkWatcherConnectionMonitorEndpointObject> Endpoints = TestGroup.Sources;
-                Endpoints.Concat(TestGroup.Destinations);
-
-                foreach (PSNetworkWatcherConnectionMonitorEndpointObject Endpoint in Endpoints)
-                {
-                    if (string.IsNullOrEmpty(Endpoint.ResourceId) && string.IsNullOrEmpty(Endpoint.Address) && Endpoint.Filter == null)
+                    // validate mandatory parameters
+                    if (string.IsNullOrEmpty(TestGroup.Name) || TestGroup.Sources == null || TestGroup.Destinations == null)
                     {
-                        throw new ArgumentException("No Endpoint parameter is provided");
+                        throw new ArgumentException("Test group is missing one or more mandatory parameter");
                     }
 
-                    if (string.IsNullOrEmpty(Endpoint.ResourceId) && string.IsNullOrEmpty(Endpoint.Address))
-                    {
-                        throw new ArgumentException("Endpoint ResourceId and Address can not be both empty");
-                    }
+                    // validate Source and Destination Endpoints
+                    List<PSNetworkWatcherConnectionMonitorEndpointObject> Endpoints = TestGroup.Sources;
+                    Endpoints.Concat(TestGroup.Destinations);
 
-                    if (!string.IsNullOrEmpty(Endpoint.ResourceId))
+                    foreach (PSNetworkWatcherConnectionMonitorEndpointObject Endpoint in Endpoints)
                     {
-                        string[] SplittedName = Endpoint.ResourceId.Split('/');
-
-                        // Resource ID must be in this format
-                        // "resourceId": "/subscriptions/96e68903-0a56-4819-9987-8d08ad6a1f99/resourceGroups/MyResourceGroup/providers/Microsoft.Compute/virtualMachines/iraVmTest2"
-                        if (SplittedName.Count() < 9)
+                        if (string.IsNullOrEmpty(Endpoint.ResourceId) && string.IsNullOrEmpty(Endpoint.Address) && Endpoint.Filter == null)
                         {
-                            throw new ArgumentException("Endpoint ResourceId is not in the correct format");
+                            throw new ArgumentException("No Endpoint parameter is provided");
                         }
-                    }
 
-                    if (Endpoint.Filter.Type != null && String.Compare(Endpoint.Filter.Type, "Include", true) != 0)
-                    {
-                        throw new ArgumentException("Only FilterType Include is supported");
-                    }
-                    else if (Endpoint.Filter.Type != null && Endpoint.Filter.Items == null)
-                    {
-                        throw new ArgumentException("Endpoint FilterType defined without FilterAddress");
-                    }
-                    else if (Endpoint.Filter.Type != null && Endpoint.Filter.Items[0] != null && Endpoint.Filter.Items[0].Type != null && String.Compare(Endpoint.Filter.Items[0].Type, "AgentAddress", true) != 0)
-                    {
-                        throw new ArgumentException("Endpoint Filter Items Type is not AgentAddress");
-                    }
-                    else if (Endpoint.Filter.Type != null && Endpoint.Filter.Items[0] != null && Endpoint.Filter.Items[0].Type != null && string.IsNullOrEmpty(Endpoint.Filter.Items[0].Address))
-                    {
-                        throw new ArgumentException("Endpoint Filter Items Address is empty");
-                    }
-
-                    // Endpoint name is optional so if it is not provided, fill it out
-                    if (string.IsNullOrEmpty(Endpoint.Name))
-                    {
-                        string EndpointName = null;
+                        if (string.IsNullOrEmpty(Endpoint.ResourceId) && string.IsNullOrEmpty(Endpoint.Address))
+                        {
+                            throw new ArgumentException("Endpoint ResourceId and Address can not be both empty");
+                        }
 
                         if (!string.IsNullOrEmpty(Endpoint.ResourceId))
                         {
                             string[] SplittedName = Endpoint.ResourceId.Split('/');
-                            // Name is in the form resourceName(ResourceGroupName)
-                            EndpointName = SplittedName[9] + "(" + SplittedName[5] + ")";
+
+                            // Resource ID must be in this format
+                            // "resourceId": "/subscriptions/96e68903-0a56-4819-9987-8d08ad6a1f99/resourceGroups/MyResourceGroup/providers/Microsoft.Compute/virtualMachines/iraVmTest2"
+                            if (SplittedName.Count() < 9)
+                            {
+                                throw new ArgumentException("Endpoint ResourceId is not in the correct format");
+                            }
                         }
-                        else if (!string.IsNullOrEmpty(Endpoint.Address))
+
+                        if (Endpoint.Filter.Type != null && String.Compare(Endpoint.Filter.Type, "Include", true) != 0)
                         {
-                            EndpointName = Endpoint.Address;
+                            throw new ArgumentException("Only FilterType Include is supported");
+                        }
+                        else if (Endpoint.Filter.Type != null && Endpoint.Filter.Items == null)
+                        {
+                            throw new ArgumentException("Endpoint FilterType defined without FilterAddress");
+                        }
+                        else if (Endpoint.Filter.Type != null && Endpoint.Filter.Items[0] != null && Endpoint.Filter.Items[0].Type != null && String.Compare(Endpoint.Filter.Items[0].Type, "AgentAddress", true) != 0)
+                        {
+                            throw new ArgumentException("Endpoint Filter Items Type is not AgentAddress");
+                        }
+                        else if (Endpoint.Filter.Type != null && Endpoint.Filter.Items[0] != null && Endpoint.Filter.Items[0].Type != null && string.IsNullOrEmpty(Endpoint.Filter.Items[0].Address))
+                        {
+                            throw new ArgumentException("Endpoint Filter Items Address is empty");
                         }
 
-                        // assign the new name to the endpoint name
-                        Endpoint.Name = EndpointName;
-                    }
-                }
+                        // Endpoint name is optional so if it is not provided, fill it out
+                        if (string.IsNullOrEmpty(Endpoint.Name))
+                        {
+                            string EndpointName = null;
 
-                foreach (PSNetworkWatcherConnectionMonitorTestConfigurationObject TestConfiguration in TestGroup.TestConfigurations)
-                {
-                    if (TestGroup.TestConfigurations.Any(x => x.Name == TestConfiguration.Name))
+                            if (!string.IsNullOrEmpty(Endpoint.ResourceId))
+                            {
+                                string[] SplittedName = Endpoint.ResourceId.Split('/');
+                                // Name is in the form resourceName(ResourceGroupName)
+                                EndpointName = SplittedName[8] + "(" + SplittedName[4] + ")";
+                            }
+                            else if (!string.IsNullOrEmpty(Endpoint.Address))
+                            {
+                                EndpointName = Endpoint.Address;
+                            }
+
+                            // assign the new name to the endpoint name
+                            Endpoint.Name = EndpointName;
+                        }
+                    }
+
+                    foreach (PSNetworkWatcherConnectionMonitorTestConfigurationObject TestConfiguration in TestGroup.TestConfigurations)
                     {
-                        throw new ArgumentException("Test configuration name is not unique");
+                        if (TestGroup.TestConfigurations.Count(x => x.Name == TestConfiguration.Name) > 2)
+                        {
+                            throw new ArgumentException("Test configuration name is not unique");
+                        }
+
+                        // validate test configuration
+                        if (TestConfiguration.HttpConfiguration == null && TestConfiguration.TcpConfiguration == null && TestConfiguration.IcmpConfiguration == null)
+                        {
+                            throw new ArgumentException("Protocol configuration is not provided.");
+                        }
+
+                        if (TestConfiguration.PreferredIPVersion != null & String.Compare(TestConfiguration.PreferredIPVersion, NetworkBaseCmdlet.IPv4, true) != 0 &&
+                            String.Compare(TestConfiguration.PreferredIPVersion, NetworkBaseCmdlet.IPv6, true) != 0)
+                        {
+                            throw new ArgumentException("IP version is undefined.");
+                        }
                     }
                 }
             }
 
             // validate output
-            foreach (PSNetworkWatcherConnectionMonitorOutputObject Output in this.Output)
+            if (this.Output != null)
             {
-                if (Output.Type == null && Output.WorkspaceSettings == null)
+                foreach (PSNetworkWatcherConnectionMonitorOutputObject Output in this.Output)
                 {
-                    throw new ArgumentException("No Output parameter is provided");
-                }
-                else if (string.IsNullOrEmpty(Output.WorkspaceSettings.WorkspaceResourceId))
-                {
-                    throw new ArgumentException("Output WorkspaceResourceId parameter is empty");
+                    if (Output.Type == null && Output.WorkspaceSettings == null)
+                    {
+                        throw new ArgumentException("No Output parameter is provided");
+                    }
+                    else if (string.IsNullOrEmpty(Output.WorkspaceSettings.WorkspaceResourceId))
+                    {
+                        throw new ArgumentException("Output WorkspaceResourceId parameter is empty");
+                    }
                 }
             }
 
