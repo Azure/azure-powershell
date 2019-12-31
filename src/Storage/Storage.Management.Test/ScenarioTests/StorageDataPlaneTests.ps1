d5d5cc066cf9d467a1fd4daf9c066fa426d42714
Wei Wei <weiwei@microsoft.com>
2019-12-31 16:56:22 +0800

    Fix a test case

--- src/Storage/Storage.Management.Test/ScenarioTests/StorageDataPlaneTests.ps1
+++ src/Storage/Storage.Management.Test/ScenarioTests/StorageDataPlaneTests.ps1
@@ -178,14 +178,14 @@ function Test-Blob
 		# verify set container ACL and Stored Access Policy
 		$accessPolicyName = "policy1"
 		New-AzStorageContainerStoredAccessPolicy -Name $containerName -Context $storageContext -Policy $accessPolicyName -Permission rw
-		$accessPolicy = Get-AzStorageContainerStoredAccessPolicy -Name $containerName -Context $ctx
+		$accessPolicy = Get-AzStorageContainerStoredAccessPolicy -Name $containerName -Context $storageContext
 		Assert-AreNotEqual $null $accessPolicy
         Assert-AreEqual $accessPolicyName $accessPolicy.Policy
 		Set-AzStorageContainerAcl -Name $containerName -Context $storageContext -Permission Blob
-		$accessPolicy = Get-AzStorageContainerStoredAccessPolicy -Name $containerName -Context $ctx
+		$accessPolicy = Get-AzStorageContainerStoredAccessPolicy -Name $containerName -Context $storageContext
 		Assert-AreNotEqual $null $accessPolicy
         Assert-AreEqual $accessPolicyName $accessPolicy.Policy
-		$container = Get-AzStorageContainer -Name $containerName -Context $ctx
+		$container = Get-AzStorageContainer -Name $containerName -Context $storageContext
         Assert-AreEqual 'Blob' $container.Permission.PublicAccess
 
         # Upload local file to Azure Storage Blob.
@@ -721,7 +721,7 @@ function Test-DatalakeGen2
         $ContentMD5 = "i727sP7HigloQDsqadNLHw=="
 
         # Create FileSystem (actually a container)
-        New-AzStorageContainer $filesystemName -Context $storageContext
+        New-AzDatalakeGen2FileSystem $filesystemName -Context $storageContext
 
 		# Create folders
 		$dir1 = New-AzDataLakeGen2Item -Context $storageContext -FileSystem $filesystemName -Path $directoryPath1 -Directory -Permission rwxrwxrwx -Umask ---rwx---  -Property @{"ContentEncoding" = "UDF8"; "CacheControl" = "READ"} -Metadata  @{"tag1" = "value1"; "tag2" = "value2" }
@@ -822,6 +822,9 @@ function Test-DatalakeGen2
 
         # Clean Storage Account
         Get-AzDataLakeGen2ChildItem -Context $storageContext -FileSystem $filesystemName | Remove-AzDataLakeGen2Item -Force
+
+        # remove File system (actually a container)
+        Remove-AzDatalakeGen2FileSystem $filesystemName -Context $storageContext
 
     }
     finally
