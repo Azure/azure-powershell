b1ab996f5b861f341195cef9ccec92acf492a9b0
Rahul Nishant <ranishan@microsoft.com>
2019-12-31 02:02:33 -0800

    AG Listener draft changes

---
+++ src/SqlVirtualMachine/SqlVirtualMachine/AvailabilityGroupListener/Cmdlet/GetAzureAvailabilityGroupListener.cs
@@ -0,0 +1,132 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Text.RegularExpressions;
+using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
+using Microsoft.Azure.Commands.SqlVirtualMachine.Common;
+using Microsoft.Azure.Commands.SqlVirtualMachine.SqlVirtualMachine.Model;
+
+namespace Microsoft.Azure.Commands.SqlVirtualMachine.SqlVirtualMachine.Cmdlet
+{
+    /// <summary>
+    /// This class implements the Get-AzSqlVMGroup cmdlet. It will retrieve the information relative to one or more Availability Group Listener on Azure.
+    /// </summary>
+    [Cmdlet(VerbsCommon.Get, ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "AvailabilityGroupListener", DefaultParameterSetName = ParameterSet.ResourceGroupOnly)]
+    [OutputType(typeof(AzureSqlVMGroupModel))]
+    public class GetAzureAvailabilityGroupListener : AzureAvailabilityGroupListenerCmdletBase
+    {
+        /// <summary>
+        /// Resource group name of the sql virtual machine group, overrided from the base class in order to not be mandatory
+        /// </summary>
+        [Parameter(Mandatory = true,
+            ParameterSetName = ParameterSet.Name,
+            Position = 0,
+            HelpMessage = HelpMessages.ResourceGroupSqlVMGroup)]
+        [Parameter(Mandatory = false,
+            ParameterSetName = ParameterSet.ResourceGroupOnly,
+            Position = 0,
+            HelpMessage = HelpMessages.ResourceGroupSqlVM)]
+        [ResourceGroupCompleter]
+        public new virtual string ResourceGroupName { get; set; }
+
+        /// <summary>
+        /// Name of the sql virtual machine group
+        /// </summary>
+        [Parameter(Mandatory = true,
+            ParameterSetName = ParameterSet.Name,
+            Position = 1,
+            HelpMessage = HelpMessages.NameSqlVMGroup)]
+        [Alias("GroupName")]
+        public new string GroupName { get; set; }
+
+        /// <summary>
+        /// Name of the Availability Group Listener
+        /// </summary>
+        [Parameter(Mandatory = true,
+            ParameterSetName = ParameterSet.Name,
+            Position = 1,
+            HelpMessage = HelpMessages.NameAvailabilityGroupListener)]
+        [Alias("AvailabilityGroupListener")]
+        public new string Name { get; set; }
+
+        /// <summary>
+        /// Resource id of the Availability Group Listener
+        /// </summary>
+        [Parameter(Mandatory = true,
+            ParameterSetName = ParameterSet.ResourceId,
+            ValueFromPipelineByPropertyName = true,
+            Position = 0,
+            HelpMessage = HelpMessages.AvailabilityGroupListenerResourceId)]
+        [Alias("AvailabilityGroupListenerId")]
+        public string ResourceId { get; set; }
+
+        /// <summary>
+        /// Parse the parameters provided as input in order to obtain the name of the resource group and the Availability Group Listener
+        /// </summary>
+        protected override void ParseInput()
+        {
+            if (ParameterSetName == ParameterSet.ResourceId)
+            {
+                if (!ValidateAvailabilityGroupListenerId(ResourceId))
+                    throw new PSArgumentException(
+                string.Format("The Availability Group Listener resource id is not well formatted"),
+                "AvailabilityGroupListener");
+                ResourceGroupName = GetResourceGroupNameFromId(ResourceId);
+                GroupName = GetResourceGroupNameFromId(ResourceId);
+                Name = GetResourceNameFromId(ResourceId);
+            }
+        }
+
+        /// <summary>
+        /// Gets one or more Availability Group Listener.
+        /// </summary>
+        /// <returns></returns>
+        protected override IEnumerable<AzureAvailabilityGroupListenerModel> GetEntity()
+        {
+            ICollection<AzureAvailabilityGroupListenerModel> results = null;
+            if(ShouldGetByName(ResourceGroupName, GroupName, Name))
+            {
+                results = new List<AzureAvailabilityGroupListenerModel>();
+                results.Add(ModelAdapter.GetAvailabilityGroupListener(ResourceGroupName, GroupName, Name));
+            }
+            else
+            {
+                results = ModelAdapter.ListAvailabilityGroupListenerByGroup(ResourceGroupName, GroupName);
+            }
+            return results;
+        }
+
+        /// <summary>
+        /// No user input to apply to model.
+        /// </summary>
+        /// <param name="model">The model to modify</param>
+        /// <returns>The input model</returns>
+        protected override IEnumerable<AzureAvailabilityGroupListenerModel> ApplyUserInputToModel(IEnumerable<AzureAvailabilityGroupListenerModel> model)
+        {
+            return model;
+        }
+
+        /// <summary>
+        /// No changes, thus nothing to persist.
+        /// </summary>
+        /// <param name="entity">The entity retrieved</param>
+        /// <returns>The unchanged entity</returns>
+        protected override IEnumerable<AzureAvailabilityGroupListenerModel> PersistChanges(IEnumerable<AzureAvailabilityGroupListenerModel> entity)
+        {
+            return entity;
+        }
+    }
+}
---
+++ src/SqlVirtualMachine/SqlVirtualMachine/AvailabilityGroupListener/Cmdlet/NewAzureAvailabilityGroupListener.cs
@@ -0,0 +1,120 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Security;
+using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
+using Microsoft.Azure.Commands.ResourceManager.Common.Tags;
+using Microsoft.Azure.Commands.SqlVirtualMachine.Common;
+using Microsoft.Azure.Commands.SqlVirtualMachine.Common.ArgumentCompleters;
+using Microsoft.Azure.Commands.SqlVirtualMachine.SqlVirtualMachine.Model;
+using Microsoft.Azure.Management.SqlVirtualMachine.Models;
+using Microsoft.Rest.Azure;
+using Microsoft.WindowsAzure.Commands.Common;
+
+namespace Microsoft.Azure.Commands.SqlVirtualMachine.SqlVirtualMachine.Cmdlet
+{
+    /// <summary>
+    /// This class implements the New-AzAvailabilityGroupListener cmdlet. It creates a new instance of an Azure Availability Group Listener and returns its information 
+    /// to the powershell user as a AzureAvailabilityGroupListenerModel object.
+    /// </summary>
+    [Cmdlet(VerbsCommon.New, ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "AvailabilityGroupListener", SupportsShouldProcess = true)]
+    [OutputType(typeof(AzureAvailabilityGroupListenerModel))]
+    public class NewAzureAvailabilityGroupListener : AzureAvailabilityGroupListenerUpsertCmdletBase
+    {
+        /// <summary>
+        /// Gets or sets whether or not to run this cmdlet in the background as a job
+        /// </summary>
+        [Parameter(
+            Mandatory = false,
+            HelpMessage = HelpMessages.AsJobHelpMessage)]
+        public SwitchParameter AsJob { get; set; }
+        public string AvailabilityGroupName { get; private set; }
+        public int? Port { get; private set; }
+        public bool? CreateDefaultAvailabilityGroupIfNotExist { get; private set; }
+        public string LoadBalanceResourceId { get; private set; }
+        public PrivateIPAddress PrivateIpAddress { get; private set; }
+        public int? ProbePort { get; private set; }
+        public string PublicIpAddressResourceId { get; private set; }
+        public IList<string> SqlVirtualMachineInstances { get; private set; }
+
+        /// <summary>
+        /// Check to see if Availability Group Listener with the same name already exists in this resource group.
+        /// </summary>
+        /// <returns>Null if the Availability Group Listener doesn't exist.  Otherwise throws exception</returns>
+        protected override IEnumerable<AzureAvailabilityGroupListenerModel> GetEntity()
+        {
+            try
+            {
+                ModelAdapter.GetAvailabilityGroupListener(this.ResourceGroupName, this.GroupName, this.Name);
+            }
+            catch (CloudException)
+            {
+                return null;
+            }
+
+            throw new PSArgumentException(
+                string.Format("An Availability Group Listener with name {0} in resource group {1} already exists. If you want to modify an existing SqlVMGroup you can use" +
+                " Update-AzAvailabilityGroupListener command.", Name, ResourceGroupName),
+                "AvailabilityGroupListener");
+        }
+
+        /// <summary>
+        /// Generates the model from user input.
+        /// </summary>
+        /// <param name="model">This is null since the Availability Group Listener doesn't exist yet</param>
+        /// <returns>The generated model from user input</returns>
+        protected override IEnumerable<AzureAvailabilityGroupListenerModel> ApplyUserInputToModel(IEnumerable<AzureAvailabilityGroupListenerModel> model)
+        {
+            List<AzureAvailabilityGroupListenerModel> newEntity = new List<AzureAvailabilityGroupListenerModel>();
+            var loadBalancerConfiguration = new LoadBalancerConfiguration()
+            {
+                LoadBalancerResourceId = this.LoadBalanceResourceId,
+                PrivateIpAddress = this.PrivateIpAddress,
+                ProbePort = this.ProbePort,
+                PublicIpAddressResourceId = this.PublicIpAddressResourceId,
+                SqlVirtualMachineInstances = this.SqlVirtualMachineInstances
+            };
+            var loadBalancerConfigurations = new List<LoadBalancerConfiguration>();
+            loadBalancerConfigurations.Add(loadBalancerConfiguration);
+
+            newEntity.Add(new AzureAvailabilityGroupListenerModel(ResourceGroupName)
+            {
+                AvailabilityGroupName = this.AvailabilityGroupName,
+                Name = this.Name,
+                Port = this.Port,
+                CreateDefaultAvailabilityGroupIfNotExist = this.CreateDefaultAvailabilityGroupIfNotExist,
+                LoadBalancerConfigurations = loadBalancerConfigurations,
+                Tags = TagsConversionHelper.CreateTagDictionary(Tag, validate: true)
+            });
+            return newEntity;
+        }
+
+        /// <summary>
+        /// Creates the Availability Group Listener
+        /// </summary>
+        /// <param name="entity">The Availability Group Listener to create</param>
+        /// <returns>The created Availability Group Listener</returns>
+        protected override IEnumerable<AzureAvailabilityGroupListenerModel> PersistChanges(IEnumerable<AzureAvailabilityGroupListenerModel> entity)
+        {
+            return new List<AzureAvailabilityGroupListenerModel>()
+            {
+                ModelAdapter.UpsertAvailabilityGroupListener(entity.FirstOrDefault())
+            };
+        }
+    }
+}
