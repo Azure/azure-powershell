4ecc75a154bebf917990ec55ad24404c2c991c64
Kota Sudhakar Reddy <60102891+Kotasudhakarreddy@users.noreply.github.com>
2020-01-31 16:49:59 +0530

    Tasks #10422,#9895,#10482 changes

--- src/Websites/Websites/Cmdlets/AppServicePlans/SetAzureAppServicePlan.cs
+++ src/Websites/Websites/Cmdlets/AppServicePlans/SetAzureAppServicePlan.cs
@@ -18,6 +18,9 @@ using Microsoft.Azure.Management.WebSites.Models;
 using System.Management.Automation;
 using System.Text.RegularExpressions;
 using Microsoft.Azure.Commands.WebApps.Models.WebApp;
+using System.Collections.Generic;
+using System.Collections;
+
 
 namespace Microsoft.Azure.Commands.WebApps.Cmdlets.AppServicePlans
 {
@@ -49,6 +52,9 @@ namespace Microsoft.Azure.Commands.WebApps.Cmdlets.AppServicePlans
         [Parameter(Mandatory = false, HelpMessage = "Run cmdlet in the background")]
         public SwitchParameter AsJob { get; set; }
 
+        [Parameter(ParameterSetName = ParameterSet1Name,Mandatory = false, HelpMessage = "Tags are name/value pairs that enable you to categorize resources")]
+        public Hashtable Tags { get; set; }
+
         public override void ExecuteCmdlet()
         {
             base.ExecuteCmdlet();
@@ -62,6 +68,7 @@ namespace Microsoft.Azure.Commands.WebApps.Cmdlets.AppServicePlans
                     int.TryParse(Regex.Match(AppServicePlan.Sku.Name, @"\d+").Value, out workerSizeAsNumber);
                     AppServicePlan.Sku.Name = string.IsNullOrWhiteSpace(WorkerSize) ? CmdletHelpers.GetSkuName(AppServicePlan.Sku.Tier, workerSizeAsNumber) : CmdletHelpers.GetSkuName(AppServicePlan.Sku.Tier, WorkerSize);
                     AppServicePlan.PerSiteScaling = PerSiteScaling;
+                    AppServicePlan.Tags = (IDictionary<string, string>)CmdletHelpers.ConvertToStringDictionary(Tags);
                     break;
             }
 
--- src/Websites/Websites/Utilities/WebsitesClient.cs
+++ src/Websites/Websites/Utilities/WebsitesClient.cs
@@ -97,11 +97,12 @@ namespace Microsoft.Azure.Commands.WebApps.Utilities
             return CmdletHelpers.CreateHostingEnvironmentProfile(WrappedWebsitesClient.SubscriptionId, resourceGroupName, aseResourceGroupName, aseName);
         }
 
-        public void UpdateWebApp(string resourceGroupName, string location, string webAppName, string slotName, string appServicePlan, Site siteEnvelope =null)
+        public void UpdateWebApp(string resourceGroupName, string location, string webAppName, string slotName, string appServicePlan, Site siteEnvelope =null,string appServicePlanRg=null)
         {
             var webSiteToUpdate = new Site()
             {
-                ServerFarmId = appServicePlan,
+                //Condition added to send full qualified ServerFarmId if ASP and WebApp are under different  RGs.
+                ServerFarmId = (string.IsNullOrEmpty(appServicePlanRg)&&resourceGroupName != appServicePlanRg) ? appServicePlan : siteEnvelope.ServerFarmId,
                 Location = location,
                 Tags = siteEnvelope?.Tags
             };
@@ -112,7 +113,7 @@ namespace Microsoft.Azure.Commands.WebApps.Utilities
             }
 
             // make sure the serverfarm ID is nt overwritten to the old value
-            if (appServicePlan != null)
+            if (appServicePlan != null && (string.IsNullOrEmpty(appServicePlanRg) && resourceGroupName != appServicePlanRg))
             {
                 webSiteToUpdate.ServerFarmId = appServicePlan;
             }
@@ -150,6 +151,7 @@ namespace Microsoft.Azure.Commands.WebApps.Utilities
                 catch (Exception e)
                 {
                     WriteWarning("Could not set custom hostname '{0}'. Details: {1}", hostName, e.ToString());
+                    return;
                 }
             }
 
