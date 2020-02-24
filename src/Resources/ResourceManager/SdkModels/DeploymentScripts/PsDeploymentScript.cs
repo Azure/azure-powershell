0169214f5d2234903974990e69ae0b798c046757
Filiz Topatan <lsefitopata@microsoft.com>
2020-02-24 14:48:21 -0800

    First line scaffolding for deployment scripts cmdlets

---
+++ src/Resources/ResourceManager/SdkModels/DeploymentScripts/PsDeploymentScript.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkModels
+{
+    public class PsDeploymentScript : PsAzureResourceBase
+    {
+        public string Identity { get; set; }
+
+        public string Location { get; set; }
+
+        public string Tags { get; set; }
+
+    }
+}
