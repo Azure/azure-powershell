e8b662efc78263fed347198b0dbafa5956a20c0b
Emily Gu <emgu@microsoft.com>
2020-03-02 12:47:32 -0800

    Preventing unnecessary tests from failing due to server version bump

--- src/Sql/Sql.Test/ScenarioTests/FailoverGroupTests.cs
+++ src/Sql/Sql.Test/ScenarioTests/FailoverGroupTests.cs
@@ -23,6 +23,9 @@ namespace Microsoft.Azure.Commands.Sql.Test.ScenarioTests
     {
         public FailoverGroupTests(ITestOutputHelper output) : base(output)
         {
+            base.resourceTypesToIgnoreApiVersion = new string[] {
+                "Microsoft.Sql/servers"
+            };
         }
 
         [Fact]
