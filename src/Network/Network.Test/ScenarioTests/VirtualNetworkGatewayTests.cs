8ac0287df6de6df7f28b477af2b89ce22d6e1d48
Anton Evseev <v-anevse@microsoft.com>
2020-03-10 16:22:48 +0300

    Update Network SDK & test recordings

--- src/Network/Network.Test/ScenarioTests/VirtualNetworkGatewayTests.cs
+++ src/Network/Network.Test/ScenarioTests/VirtualNetworkGatewayTests.cs
@@ -126,7 +126,7 @@ namespace Commands.Network.Test.ScenarioTests
 
         [Fact]
         [Trait(Category.AcceptanceType, Category.CheckIn)]
-        [Trait(Category.Owner, NrpTeamAlias.brooklynft_subset2)]
+        [Trait(Category.Owner, NrpTeamAlias.brooklynft_subset5)]
         public void TestVirtualNetworKGatewayPacketCapture()
         {
             TestRunner.RunTestScript("Test-VirtualNetworKGatewayPacketCapture");
@@ -134,7 +134,7 @@ namespace Commands.Network.Test.ScenarioTests
 
         [Fact]
         [Trait(Category.AcceptanceType, Category.CheckIn)]
-        [Trait(Category.Owner, NrpTeamAlias.brooklynft_subset2)]
+        [Trait(Category.Owner, NrpTeamAlias.brooklynft_subset5)]
         public void TestDisconnectVirtualNetworkGatewayVpnConnection()
         {
             TestRunner.RunTestScript("Test-DisconnectVNGVpnConnection");
