f96667e13e21becc7e4fad2d50455255ace7344e
dixue@microsoft.com <dixue@microsoft.com>
2020-03-21 22:53:34 +0800

    Replace Sql client using generic rest client

--- src/Network/Network/PrivateLinkService/PrivateEndpointConnection/PrivateEndpointConnectionBaseCmdlet.cs
+++ src/Network/Network/PrivateLinkService/PrivateEndpointConnection/PrivateEndpointConnectionBaseCmdlet.cs
@@ -1,12 +1,20 @@
-﻿using AutoMapper;
-using Microsoft.Azure.Commands.Network.Models;
-using Microsoft.Azure.Commands.ResourceManager.Common.Tags;
-using Microsoft.Azure.Management.Network;
-using System;
-using System.Management.Automation;
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Commands.Network.PrivateLinkService.PrivateLinkServiceProvider;
 using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
-using MNM = Microsoft.Azure.Management.Network.Models;
-using Microsoft.WindowsAzure.Commands.Common.CustomAttributes;
+using System.Management.Automation;
 
 namespace Microsoft.Azure.Commands.Network
 {
@@ -53,22 +61,11 @@ namespace Microsoft.Azure.Commands.Network
           ParameterSetName = "ByResource")]
         public string PrivateLinkResourceType { get; set; }
 
-        protected IPrivateLinkProvider BuildProvider(string resourceType)
+        public string Subscription { get; set; }
+
+        protected IPrivateLinkProvider BuildProvider(string subscription, string privateLinkResourceType)
         {
-            IPrivateLinkProvider provider = null;
-
-            switch (resourceType.ToLower())
-            {
-                case "microsoft.sql/servers":
-                    provider = new SqlProvider(this);
-                    break;
-                case "microsoft.network/privatelinkservices":
-                default:
-                    provider = new NetworkingProvider(this);
-                    break;
-            }
-
-            return provider;
+            return PrivateLinkProviderFactory.CreatePrivateLinkProvder(this, subscription, privateLinkResourceType);
         }
     }
 }
