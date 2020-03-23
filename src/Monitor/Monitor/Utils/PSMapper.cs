bf8c5d5ec1415d669a2374dcb517fe5d6a71a1c8
Yabo Hu <yabhu@microsoft.com>
2020-03-23 10:20:57 +0800

    add mapper

---
+++ src/Monitor/Monitor/Utils/PSMapper.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Microsoft.Azure.Management.Monitor.Models;
+using Microsoft.Azure.Commands.Insights.OutputClasses;
+
+namespace Microsoft.Azure.Commands.Insights.Utils
+{
+    class PSMapper
+    {
+        private static readonly IMapper _instance;
+
+        static PSMapper()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<AzureMonitorPrivateLinkScope, PSMonitorPrivateLinkScope>().ReverseMap();
+            });
+            _instance = config.CreateMapper();
+        }
+
+        public static IMapper Instance { get { return _instance; } }
+    }
+}
