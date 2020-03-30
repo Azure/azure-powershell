b07f455a94ca785f94efd7fc5e50886b69e3d16e
erich-wang <eriwan@microsoft.com>
2020-03-30 15:08:38 +0800

    [AKS] Support creating Windows server container in AKS (#11403)
    * support using windows container to create AKS
    
    * upload to signed AKS sdk
    
    * update onlineversion for help of new cmdlets
    
    * add missing dll in psd1
    
    * disable test parallelization in Aks.Test
    
    * update ContainerService SDK
    
    * fix Start-AzAksDashboard and update help files

---
+++ src/Aks/Aks/Commands/InstallAzureRMAksKubectl.cs
@@ -0,0 +1,169 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.IO;
+using System.Management.Automation;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.Azure.Commands.Aks.Properties;
+using Microsoft.WindowsAzure.Commands.Utilities.Common;
+
+namespace Microsoft.Azure.Commands.Aks.Commands
+{
+    [Cmdlet("Install", ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "AksKubectl", SupportsShouldProcess = true)]
+    [OutputType(typeof(bool))]
+    public class InstallAzureRMAksKubectl : KubeCmdletBase
+    {
+        private const string LatestVersion = "Latest";
+        private const string KubecliString = "kubectl";
+        private const string KubecliExeString = "kubectl.exe";
+        private const string KubecliSiteUrl = "https://storage.googleapis.com/kubernetes-release/release";
+        private const string KubecliSiteUrlMirror = "https://mirror.azure.cn/kubernetes/kubectl";
+        private const string KubecliPathFormat = "/{0}/bin/{1}/amd64/{2}";
+
+        [Parameter(Mandatory = false, HelpMessage = "Path at which to install kubectl. Default to install into ~/.azure-kubectl/")]
+        [ValidateNotNullOrEmpty]
+        public string Destination { get; set; }
+
+        [Parameter(Mandatory = false, HelpMessage = "Version of kubectl to install, e.g. 'v1.17.2'. Default value: Latest")]
+        public string Version { get; set; } = LatestVersion;
+
+        [Parameter(Mandatory = false, HelpMessage = "Download from mirror site : https://mirror.azure.cn/kubernetes/kubectl/")]
+        public SwitchParameter DownloadFromMirror { get; set; }
+
+        [Parameter(Mandatory = false)]
+        public SwitchParameter PassThru { get; set; }
+
+        [Parameter(Mandatory = false, HelpMessage = "Run cmdlet in the background")]
+        public SwitchParameter AsJob { get; set; }
+
+        [Parameter(Mandatory = false, HelpMessage = "Overwrite existing kubectl without prompt")]
+        public SwitchParameter Force { get; set; }
+
+        public override void ExecuteCmdlet()
+        {
+            base.ExecuteCmdlet();
+
+            bool fromMirror = DownloadFromMirror.IsPresent;
+            StringBuilder sourceUrlBuilder = fromMirror ?
+                new StringBuilder(KubecliSiteUrlMirror) :
+                new StringBuilder(KubecliSiteUrl);
+
+            if (!this.IsParameterBound(c => c.Destination))
+            {
+                Destination = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".azure-kubectl");
+            }
+
+            bool isWindows = false;
+            string destFilePath = null; 
+            var webClient = new WebClient();
+
+            if (string.Equals(LatestVersion, Version, StringComparison.InvariantCultureIgnoreCase))
+            {
+                Version = ReadLatestStableVersion(fromMirror, webClient);
+            }
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                destFilePath = Path.Combine(Destination, KubecliExeString);
+                sourceUrlBuilder.AppendFormat(KubecliPathFormat, Version, "windows", KubecliExeString);
+                isWindows = true;
+            }
+            else if(RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                destFilePath = Path.Combine(Destination, KubecliString);
+                sourceUrlBuilder.AppendFormat(KubecliPathFormat, Version, "linux", KubecliString);
+            }
+            else if(RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                if(fromMirror)
+                {
+                    throw new PSInvalidOperationException(Resources.NoKubectlForOsxOnMirror);
+                }
+                destFilePath = Path.Combine(Destination, KubecliString);
+                sourceUrlBuilder.AppendFormat(KubecliPathFormat, Version, "darwin", KubecliString);
+            }
+            else
+            {
+                throw new PSInvalidOperationException(Resources.NotSupportOnThisOs);
+            }
+
+            bool fileExists = File.Exists(destFilePath);
+
+            Action action = () =>
+            {
+                //mkdir if not exist
+                if (!Directory.Exists(Destination))
+                {
+                    WriteVerbose($"Creating directory {Destination}");
+                    Directory.CreateDirectory(Destination);
+                }
+
+                //download to local, use tmp file if already exists
+                var tempFile = fileExists ? destFilePath + ".tmp" : destFilePath;
+                var sourceUrl = sourceUrlBuilder.ToString();
+                WriteVerbose($"Downloading from {sourceUrl} to local : {tempFile}");
+                webClient.DownloadFile(sourceUrl, tempFile);
+
+                if (fileExists)
+                {
+                    WriteVerbose($"Deleting {destFilePath}");
+                    File.Delete(destFilePath);
+                    WriteVerbose($"Moving {tempFile} to {destFilePath}");
+                    File.Move(tempFile, destFilePath);
+                }
+
+                WriteWarning(string.Format(Resources.AddDirectoryToPath,
+                    Destination, isWindows ? KubecliExeString : KubecliString));
+
+                if (PassThru)
+                {
+                    WriteObject(true);
+                }
+                webClient.Dispose();
+            };
+
+            var msg = $"{destFilePath}";
+            if(fileExists)
+            {
+                ConfirmAction(Force.IsPresent,
+                    string.Format(Resources.DoYouWantToOverwriteExistingFile, destFilePath),
+                    Resources.DownloadingKubectlFromWeb,
+                    msg,
+                    action);
+            }
+            else
+            {
+                if(ShouldProcess(Resources.DownloadingKubectlFromWeb, msg))
+                {
+                    RunCmdLet(action);
+                }
+            }
+
+        }
+
+        private string ReadLatestStableVersion(bool useMirror, WebClient client)
+        {
+            var sourceUrl = useMirror ? KubecliSiteUrlMirror : KubecliSiteUrl;
+            var versionFile = sourceUrl + "/stable.txt";
+
+            WriteVerbose($"Get latest stable version for {versionFile}");
+            var content = client.DownloadData(versionFile);
+            var version = Encoding.UTF8.GetString(content);
+            return version.Trim();
+        }
+    }
+}
---
+++ src/Aks/Aks/help/Install-AzAksKubectl.md
@@ -0,0 +1,184 @@
+---
+external help file: Microsoft.Azure.PowerShell.Cmdlets.Aks.dll-Help.xml
+Module Name: Az.Aks
+online version: https://docs.microsoft.com/en-us/powershell/module/az.aks/install-azakskubectl
+schema: 2.0.0
+---
+
+# Install-AzAksKubectl
+
+## SYNOPSIS
+Download and install kubectl, the Kubernetes command-line tool.
+
+## SYNTAX
+
+```
+Install-AzAksKubectl [-Destination <String>] [-Version <String>] [-DownloadFromMirror] [-PassThru] [-AsJob]
+ [-Force] [-DefaultProfile <IAzureContextContainer>] [-WhatIf] [-Confirm] [<CommonParameters>]
+```
+
+## DESCRIPTION
+Download and install kubectl, the Kubernetes command-line tool.
+
+## EXAMPLES
+
+### Download and install latest version of kubectl
+```powershell
+PS C:\> Install-AzAksKubectl -Version latest
+```
+
+## PARAMETERS
+
+### -AsJob
+Run cmdlet in the background
+
+```yaml
+Type: System.Management.Automation.SwitchParameter
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -DefaultProfile
+The credentials, account, tenant, and subscription used for communication with Azure.
+
+```yaml
+Type: Microsoft.Azure.Commands.Common.Authentication.Abstractions.Core.IAzureContextContainer
+Parameter Sets: (All)
+Aliases: AzContext, AzureRmContext, AzureCredential
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -Destination
+Path at which to install kubectl.
+Default to install into ~/.azure-kubectl/
+
+```yaml
+Type: System.String
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -DownloadFromMirror
+Download from mirror site : https://mirror.azure.cn/kubernetes/kubectl/
+
+```yaml
+Type: System.Management.Automation.SwitchParameter
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -Force
+Overwrite existing kubectl without prompt
+
+```yaml
+Type: System.Management.Automation.SwitchParameter
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -PassThru
+{{ Fill PassThru Description }}
+
+```yaml
+Type: System.Management.Automation.SwitchParameter
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -Version
+Version of kubectl to install, e.g.
+'v1.17.2'.
+Default value: Latest
+
+```yaml
+Type: System.String
+Parameter Sets: (All)
+Aliases:
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -Confirm
+Prompts you for confirmation before running the cmdlet.
+
+```yaml
+Type: System.Management.Automation.SwitchParameter
+Parameter Sets: (All)
+Aliases: cf
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### -WhatIf
+Shows what would happen if the cmdlet runs.
+The cmdlet is not run.
+
+```yaml
+Type: System.Management.Automation.SwitchParameter
+Parameter Sets: (All)
+Aliases: wi
+
+Required: False
+Position: Named
+Default value: None
+Accept pipeline input: False
+Accept wildcard characters: False
+```
+
+### CommonParameters
+This cmdlet supports the common parameters: -Debug, -ErrorAction, -ErrorVariable, -InformationAction, -InformationVariable, -OutVariable, -OutBuffer, -PipelineVariable, -Verbose, -WarningAction, and -WarningVariable. For more information, see [about_CommonParameters](http://go.microsoft.com/fwlink/?LinkID=113216).
+
+## INPUTS
+
+### None
+
+## OUTPUTS
+
+### System.Boolean
+
+## NOTES
+
+## RELATED LINKS
