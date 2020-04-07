a1b7daa39c95f53296e540e845d6ee928114d78c
lugoldbe <Lucy.Goldberg@microsoft.com>
2020-04-07 15:50:48 +0300

    add tests for eventgrid and iothub

--- src/Kusto/test/New-AzKustoDataConnection.Tests.ps1
+++ src/Kusto/test/New-AzKustoDataConnection.Tests.ps1
@@ -14,7 +14,7 @@ while(-not $mockingPath) {
 . ($mockingPath | Select-Object -First 1).FullName
 
 Describe 'New-AzKustoDataConnection' {
-    It 'Create' {
+    It 'CreateExpandedEventHub' {
         $subscriptionId = $env.SubscriptionId
         $location = $env.location
         $resourceGroupName = $env.resourceGroupName
@@ -31,6 +31,48 @@ Describe 'New-AzKustoDataConnection' {
         $dataConnectionFullName = "$clusterName/$databaseName/$dataConnectionName"
 
         $dataConnectionCreated = New-AzKustoDataConnection -ResourceGroupName $resourceGroupName -ClusterName $clusterName -DatabaseName $databaseName -DataConnectionName $dataConnectionName -Location $location -Kind $kind -EventHubResourceId $eventHubResourceId -DataFormat $dataFormat -ConsumerGroup '$Default' -Compression "None" -TableName $tableName -MappingRuleName $tableMappingName
-        Validate_DataConnection $dataConnectionCreated $dataConnectionFullName $location $eventHubResourceId $tableName $tableMappingName $dataFormat $kind
+        Validate_EventHubDataConnection $dataConnectionCreated $dataConnectionFullName $location $eventHubResourceId $tableName $tableMappingName $dataFormat $kind
+    }
+
+    It 'CreateExpandedEventGrid' {
+        $subscriptionId = $env.SubscriptionId
+        $location = $env.location
+        $resourceGroupName = $env.resourceGroupName
+        $clusterName = $env.clusterName
+        $databaseName = $env.databaseName
+        $dataConnectionName = $env.dataConnectionName + "g"
+        $eventhubNS= $env.eventhubNSNameForEventGrid
+        $eventhub= $env.eventhubNameForEventGrid
+        $eventHubResourceId = "/subscriptions/$subscriptionId/resourcegroups/$resourceGroupName/providers/Microsoft.EventHub/namespaces/$eventhubNS/eventhubs/$eventhub"
+        $storageAccountName = $env.storageName
+        $storageAccountResourceId = "/subscriptions/$subscriptionId/resourcegroups/$resourceGroupName/providers/Microsoft.Storage/storageAccounts/$storageAccountName"
+        $tableName = $env.tableName
+        $tableMappingName = $env.tableMappingName
+        $dataFormat = $env.dataFormat
+        $kind = "EventGrid"
+        $dataConnectionFullName = "$clusterName/$databaseName/$dataConnectionName"
+
+        $dataConnectionCreated = New-AzKustoDataConnection -ResourceGroupName $resourceGroupName -ClusterName $clusterName -DatabaseName $databaseName -DataConnectionName $dataConnectionName -location $location -Kind $kind -EventHubResourceId $eventHubResourceId -StorageAccountResourceId $storageAccountResourceId -DataFormat $dataFormat -ConsumerGroup '$Default' -TableName $tableName -MappingRuleName $tableMappingName
+        Validate_EventGridDataConnection $dataConnectionCreated $dataConnectionFullName $location $eventHubResourceId $storageAccountResourceId $tableName $tableMappingName $dataFormat $kind
+    }
+
+    It 'CreateExpandedIotHub' {
+        $subscriptionId = $env.SubscriptionId
+        $location = $env.location
+        $resourceGroupName = $env.resourceGroupName
+        $clusterName = $env.clusterName
+        $databaseName = $env.databaseName
+        $dataConnectionName = $env.dataConnectionName + "h"
+        $iothubName = $env.iothubName
+        $iotHubResourceId = "/subscriptions/$subscriptionId/resourcegroups/$resourceGroupName/providers/Microsoft.Devices/IotHubs/$iothubName"
+        $sharedAccessPolicyName = $env.iothubSharedAccessPolicyName
+        $tableName = $env.tableName
+        $tableMappingName = $env.tableMappingName
+        $dataFormat = $env.dataFormat
+        $kind = "IotHub"
+        $dataConnectionFullName = "$clusterName/$databaseName/$dataConnectionName"
+
+        $dataConnectionCreated = New-AzKustoDataConnection -ResourceGroupName $resourceGroupName -ClusterName $clusterName -DatabaseName $databaseName -DataConnectionName $dataConnectionName -location $location -Kind $kind -IotHubResourceId $iotHubResourceId -SharedAccessPolicyName $sharedAccessPolicyName -DataFormat $dataFormat -ConsumerGroup '$Default' -TableName $tableName -MappingRuleName $tableMappingName
+        Validate_IotHubDataConnection $dataConnectionCreated $dataConnectionFullName $location $iotHubResourceId $sharedAccessPolicyName $tableName $tableMappingName $dataFormat $kind
     }
 }
