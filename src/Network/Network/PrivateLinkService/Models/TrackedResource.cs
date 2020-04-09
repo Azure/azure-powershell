ab6e76dbefe443d4add2822966a709339af4a5e2
Dingmeng Xue <56333063+dingmeng-xue@users.noreply.github.com>
2020-04-09 15:03:10 +0800

    Added privatelink support to keyvault, storage, and monitor (#11525)
    * Add storage endpoint
    
    * add storage
    
    * Add private endpoint connection support to keyvault, storage and monitor
    
    * Add private endpoint connection support to keyvault, storage and monitor
    
    * Revise according to review
    
    * Revise according to review

---
+++ src/Network/Network/PrivateLinkService/Models/TrackedResource.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Microsoft.Azure.Commands.Network.PrivateLinkService.Models
+{
+    [Rest.Serialization.JsonTransformation]
+    public partial class TrackedResource : ProxyResource
+    {
+
+        public TrackedResource()
+        {
+            CustomInit();
+        }
+
+        public TrackedResource(string id = default(string), string name = default(string), string type = default(string), IList<PrivateEndpointConnection> privateEndpointConnections = default(IList<PrivateEndpointConnection>))
+            : base(id, name, type)
+        {
+            PrivateEndpointConnections = privateEndpointConnections;
+            CustomInit();
+        }
+
+        partial void CustomInit();
+
+        [JsonProperty(PropertyName = "properties.privateEndpointConnections")]
+        public IList<PrivateEndpointConnection> PrivateEndpointConnections { get; private set; }
+
+    }
+}
