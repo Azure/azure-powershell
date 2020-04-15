a42a4cccb289ec8454060f911c0878a104ea9f2f
Yabo Hu <yabhu@microsoft.com>
2020-04-15 11:14:12 +0800

    add test cases for linked storage account

---
+++ src/ApplicationInsights/ApplicationInsights.Test/ScenarioTests/ApplicationInsightsLinkedStorageAccountTests.ps1
@@ -0,0 +1,70 @@
+# ----------------------------------------------------------------------------------
+#
+# Copyright Microsoft Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------------
+
+<#
+.SYNOPSIS
+Test ApplicationInsightsCRUD
+#>
+function Test-ApplicationInsightsLinkedStorageAccountCRUD
+{
+	# setup
+    $rgname = "azps-test-group-mock"
+    $appName = "azps-test-ai-mock";
+    $loc = Get-ProviderLocation ResourceManagement;
+    $kind = "web";
+    $workspaceName = "azps-test-la-mock";
+
+    $accountName1 = "azpstestaccountamock"
+    $accountName2 = "azpstestaccountbmock"
+
+    try
+    {
+        New-AzResourceGroup -Name $rgname -Location $loc;
+
+        $workspace = New-AzOperationalInsightsWorkspace -ResourceGroupName $rgname -Name $workspaceName -Location $loc
+
+        $app = New-AzApplicationInsights -ResourceGroupName $rgname -Name $appName -Location $loc -Kind $kind -WorkspaceResourceId $workspace.ResourceId
+
+        $account1 = New-AzStorageAccount -ResourceGroupName $rgname -Name $accountName1 -SkuName "Standard_LRS" -Location $loc
+        $account2 = New-AzStorageAccount -ResourceGroupName $rgname -Name $accountName2 -SkuName "Standard_LRS" -Location $loc
+
+        New-ApplicationInsightsLinkedStorageAccount -RecourceGroupName $rgname -Name $appName -LinkedStorageAccountResourceId $account1.Id
+        $linkedAccount = Get-ApplicationInsightsLinkedStorageAccount -RecourceGroupName $rgname -Name $appName
+
+        Assert-NotNull $linkedAccount
+        Assert-AreEqual $account1.Id $linkedAccount.linkedStorageAccount
+        Assert-AreEqual "serviceprofile" $linkedAccount.Name
+
+        Update-ApplicationInsightsLinkedStorageAccount -RecourceGroupName $rgname -Name $appName -LinkedStorageAccountResourceId $account2.Id
+        $linkedAccount = Get-ApplicationInsightsLinkedStorageAccount -RecourceGroupName $rgname -Name $appName
+
+        Assert-NotNull $linkedAccount
+        Assert-AreEqual $account2.Id $linkedAccount.linkedStorageAccount
+
+        Remove-ApplicationInsightsLinkedStorageAccount -RecourceGroupName $rgname -Name $appName
+
+        Remove-AzStorageAccount -ResourceGroupName $rgname -Name $accountName1 -force
+        Remove-AzStorageAccount -ResourceGroupName $rgname -Name $accountName2 -force
+
+        Remove-AzApplicationInsights -ResourceGroupName $rgname -Name $appName
+        Remove-AzOperationalInsightsWorkspace -ResourceGroupName $rgname -Name $workspaceName -force
+
+        Remove-AzResourceGroup -Name $rgname
+	}
+    finally
+    {
+        # Cleanup
+        Clean-ResourceGroup $rgname
+	}
+}
