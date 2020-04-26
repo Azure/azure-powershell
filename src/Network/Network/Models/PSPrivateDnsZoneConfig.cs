4437558533a9ff23e806d4ee921ab2377b888147
dixue@microsoft.com <dixue@microsoft.com>
2020-04-26 09:54:40 +0800

    Add DnsZoneConfig cmdlets

---
+++ src/Network/Network/Models/PSPrivateDnsZoneConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.Commands.Network.Models
+{
+    public partial class PSPrivateDnsZoneConfig
+    {
+        public string Name { get; set; }
+
+        public string PrivateDnsZoneId { get; set; }
+
+        public string ProvisioningState { get; set; }
+    }
+}
---
+++ src/Network/Network/Models/PSPrivateDnsZoneGroup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.Commands.Network.Models
+{
+    public class PSPrivateDnsZoneGroup : PSChildResource
+    {
+        public string ProvisioningState { get; set; }
+
+        public List<PSPrivateDnsZoneConfig> PrivateDnsZoneConfigs { get; set; }
+    }
+}
