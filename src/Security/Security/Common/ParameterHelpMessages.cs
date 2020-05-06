b2c7bc8de4f6a0906f40c2cdfa5c21cd1be81bc4
amitmag-ms1 <62828825+amitmag-ms1@users.noreply.github.com>
2020-05-06 10:16:29 +0300

    Add regulatory compliance cmdlets and tests (#11476)
    * Add regulatory compliance cmdlens
    
    * Fix build errors and PR comments
    
    * Fix alerts tests
    
    * Add examples in help files
    
    * Fix help files
    
    * Add online version for the help files
    
    Co-authored-by: t-ammage <t-ammage@microsoft.com>

--- src/Security/Security/Common/ParameterHelpMessages.cs
+++ src/Security/Security/Common/ParameterHelpMessages.cs
@@ -93,6 +93,14 @@ namespace Microsoft.Azure.Commands.Security.Common
 
         #endregion
 
+
+        #region RegulatoryCompliance
+
+        public const string StandardName = "Standard Name.";
+        public const string ControlName = "Control Name.";
+      
+        #endregion
+
         #region Device Security Groups
 
         public const string ThresholdRules = "Threshold rules.";
