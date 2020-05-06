164d2b42031dd76076b4604feca451a7f02f8616
Francisco Gomez Gamino <frangom@microsoft.com>
2020-05-06 16:48:04 -0700

    Update cmdlets helper functions.

--- src/Functions/custom/Api20190801/Site.cs
+++ src/Functions/custom/Api20190801/Site.cs
@@ -9,6 +9,8 @@ namespace Microsoft.Azure.PowerShell.Cmdlets.Functions.Models.Api20190801
         // Function app settings. These gets set via PowerShell. For more info, please see custom/HelperScripts/HelperFunctions.ps1        
 
         public System.Collections.Hashtable ApplicationSettings { get; set; }
+
+        public System.Collections.Hashtable SiteConfig { get; set; }
         
         public string Runtime { get; set; }
 
