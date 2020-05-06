21aecc373f559c3daaf395bd0576d8f24188545f
Meha Kaushik <mekaushi@microsoft.com>
2020-05-05 22:46:55 -0700

    Renaming references

--- src/CosmosDB/CosmosDB/MongoDB/UpdateAzCosmosDBMongoDBDatabase.cs
+++ src/CosmosDB/CosmosDB/MongoDB/UpdateAzCosmosDBMongoDBDatabase.cs
@@ -48,7 +48,7 @@ namespace Microsoft.Azure.Commands.CosmosDB
 
         [Parameter(Mandatory = true, ParameterSetName = ParentObjectParameterSet, HelpMessage = Constants.AccountObjectHelpMessage)]
         [ValidateNotNull]
-        public PSDatabaseAccount ParentObject { get; set; }
+        public PSDatabaseAccountGetResults ParentObject { get; set; }
 
         [Parameter(Mandatory = true, ParameterSetName = ObjectParameterSet, HelpMessage = Constants.MongoDatabaseObjectHelpMessage)]
         [ValidateNotNull]
