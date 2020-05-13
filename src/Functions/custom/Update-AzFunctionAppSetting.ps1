1d889097565606db633a7c6ce9dd819f5aacee1d
Dingmeng Xue <56333063+dingmeng-xue@users.noreply.github.com>
2020-05-13 13:17:23 +0800

    Add Az.Functions (#11883)
    * Add Az.Functions
    
    * Update json recorded json files

---
+++ src/Functions/custom/Update-AzFunctionAppSetting.ps1
@@ -0,0 +1,161 @@
+function Update-AzFunctionAppSetting {
+    [OutputType([Microsoft.Azure.PowerShell.Cmdlets.Functions.Models.Api20190801.IStringDictionary])]
+    [Microsoft.Azure.PowerShell.Cmdlets.Functions.Description('Adds or updates app settings in a function app.')]
+    [CmdletBinding(DefaultParameterSetName='ByName', SupportsShouldProcess=$true, ConfirmImpact='Medium')]
+    param(
+        [Parameter(ParameterSetName='ByName', Mandatory=$true, HelpMessage='Name of the function app.')]
+        [Microsoft.Azure.PowerShell.Cmdlets.Functions.Category('Path')]
+        [ValidateNotNullOrEmpty()]
+        [System.String]
+        ${Name},
+
+        [Parameter(ParameterSetName='ByName', Mandatory=$true, HelpMessage='Name of the resource group to which the resource belongs.')]
+        [Microsoft.Azure.PowerShell.Cmdlets.Functions.Category('Path')]
+        [ValidateNotNullOrEmpty()]
+        [System.String]
+        ${ResourceGroupName},
+
+        [Parameter(ParameterSetName='ByName', HelpMessage='The Azure subscription ID.')]
+        [Microsoft.Azure.PowerShell.Cmdlets.Functions.Category('Path')]
+        [Microsoft.Azure.PowerShell.Cmdlets.Functions.Runtime.DefaultInfo(Script='(Get-AzContext).Subscription.Id')]
+        [ValidateNotNullOrEmpty()]
+        [System.String]
+        ${SubscriptionId},
+
+        [Parameter(ParameterSetName='ByObjectInput', Mandatory=$true, ValueFromPipeline=$true)]
+        [Microsoft.Azure.PowerShell.Cmdlets.Functions.Models.Api20190801.ISite]
+        [ValidateNotNull()]
+        ${InputObject},
+
+        [Parameter(Mandatory=$true, HelpMessage='Hashtable with keys and values describe the app settings to be added or updated in the function app. For example: @{"myappsetting"="123"}')]
+        [Hashtable]
+        [ValidateNotNullOrEmpty()]
+        ${AppSetting},
+
+        [Parameter(HelpMessage='Forces the cmdlet to update function app setting without prompting for confirmation.')]
+        [System.Management.Automation.SwitchParameter]
+        ${Force},
+
+        [Parameter()]
+        [Alias('AzureRMContext', 'AzureCredential')]
+        [ValidateNotNull()]
+        [Microsoft.Azure.PowerShell.Cmdlets.Functions.Category('Azure')]
+        [System.Management.Automation.PSObject]
+        # The credentials, account, tenant, and subscription used for communication with Azure.
+        ${DefaultProfile},
+
+        [Parameter(DontShow)]
+        [Microsoft.Azure.PowerShell.Cmdlets.Functions.Category('Runtime')]
+        [System.Management.Automation.SwitchParameter]
+        # Wait for .NET debugger to attach
+        ${Break},
+
+        [Parameter(DontShow)]
+        [ValidateNotNull()]
+        [Microsoft.Azure.PowerShell.Cmdlets.Functions.Category('Runtime')]
+        [Microsoft.Azure.PowerShell.Cmdlets.Functions.Runtime.SendAsyncStep[]]
+        # SendAsync Pipeline Steps to be appended to the front of the pipeline
+        ${HttpPipelineAppend},
+
+        [Parameter(DontShow)]
+        [ValidateNotNull()]
+        [Microsoft.Azure.PowerShell.Cmdlets.Functions.Category('Runtime')]
+        [Microsoft.Azure.PowerShell.Cmdlets.Functions.Runtime.SendAsyncStep[]]
+        # SendAsync Pipeline Steps to be prepended to the front of the pipeline
+        ${HttpPipelinePrepend},
+
+        [Parameter(DontShow)]
+        [Microsoft.Azure.PowerShell.Cmdlets.Functions.Category('Runtime')]
+        [System.Uri]
+        # The URI for the proxy server to use
+        ${Proxy},
+
+        [Parameter(DontShow)]
+        [ValidateNotNull()]
+        [Microsoft.Azure.PowerShell.Cmdlets.Functions.Category('Runtime')]
+        [System.Management.Automation.PSCredential]
+        # Credentials for a proxy server to use for the remote call
+        ${ProxyCredential},
+
+        [Parameter(DontShow)]
+        [Microsoft.Azure.PowerShell.Cmdlets.Functions.Category('Runtime')]
+        [System.Management.Automation.SwitchParameter]
+        # Use the default credentials for the proxy
+        ${ProxyUseDefaultCredentials}
+    )
+    process {
+
+        # Remove bound parameters from the dictionary that cannot be process by the intenal cmdlets
+        $paramsToRemove = @(
+            "AppSetting"
+            "Force"
+        )
+        foreach ($paramName in $paramsToRemove)
+        {
+            if ($PSBoundParameters.ContainsKey($paramName))
+            {
+                $PSBoundParameters.Remove($paramName)  | Out-Null
+            }
+        }
+
+        if ($PsCmdlet.ParameterSetName -eq "ByObjectInput")
+        {
+            if ($PSBoundParameters.ContainsKey("InputObject"))
+            {
+                $PSBoundParameters.Remove("InputObject")  | Out-Null
+
+                $Name = $InputObject.Name
+                $ResourceGroupName = $InputObject.ResourceGroupName
+                
+                $PSBoundParameters.Add("Name", $Name)  | Out-Null
+                $PSBoundParameters.Add("ResourceGroupName", $ResourceGroupName)  | Out-Null
+                $PSBoundParameters.Add("SubscriptionId", $InputObject.SubscriptionId)  | Out-Null
+            }
+        }
+
+        if ($AppSetting.Count -eq 0)
+        {
+            return
+        }
+
+        $params = GetParameterKeyValues -PSBoundParametersDictionary $PSBoundParameters `
+                                        -ParameterList @("SubscriptionId", "HttpPipelineAppend", "HttpPipelinePrepend")
+
+        $existingFunctionApp = GetFunctionAppByName -Name $Name -ResourceGroupName $ResourceGroupName @params
+        $currentAppSettings = $existingFunctionApp.ApplicationSettings
+
+        # Add new or replace any existing app settings
+        foreach ($keyName in $AppSetting.Keys)
+        {
+            $currentAppSettings[$keyName] = $AppSetting[$keyName]
+        }
+
+        $newAppSettings = NewAppSettingObject -CurrentAppSetting $currentAppSettings
+        $shouldPromptForConfirmation = ContainsReservedFunctionAppSettingName -AppSettingName $AppSetting.Keys
+
+        $PSBoundParameters.Add("AppSetting", $newAppSettings)  | Out-Null
+
+        if ($PsCmdlet.ShouldProcess($Name, "Updating function app setting"))
+        {
+            if ($shouldPromptForConfirmation)
+            {
+                $message = "You are about to modify app settings that are used to configure your function app '$Name'. "
+                $message += "Doing this could leave your function app in an inconsistent state. Are you sure?"
+
+                if ($Force.IsPresent -or $PsCmdlet.ShouldContinue($message, "Updating function app settings"))
+                {
+                    $settings = Az.Functions.internal\Set-AzWebAppApplicationSetting @PSBoundParameters
+                }
+            }
+            else
+            {
+                $settings = Az.Functions.internal\Set-AzWebAppApplicationSetting @PSBoundParameters
+            }
+
+            if ($settings)
+            {
+                ConvertWebAppApplicationSettingToHashtable -ApplicationSetting $settings
+            }
+        }        
+    }
+}
---
+++ src/Functions/generated/api/Models/Api20190801/KeyVaultReferenceCollectionPropertiesKeyToReferenceStatuses.cs
@@ -0,0 +1,29 @@
+namespace Microsoft.Azure.PowerShell.Cmdlets.Functions.Models.Api20190801
+{
+    using static Microsoft.Azure.PowerShell.Cmdlets.Functions.Runtime.Extensions;
+
+    public partial class KeyVaultReferenceCollectionPropertiesKeyToReferenceStatuses :
+        Microsoft.Azure.PowerShell.Cmdlets.Functions.Models.Api20190801.IKeyVaultReferenceCollectionPropertiesKeyToReferenceStatuses,
+        Microsoft.Azure.PowerShell.Cmdlets.Functions.Models.Api20190801.IKeyVaultReferenceCollectionPropertiesKeyToReferenceStatusesInternal
+    {
+
+        /// <summary>
+        /// Creates an new <see cref="KeyVaultReferenceCollectionPropertiesKeyToReferenceStatuses" /> instance.
+        /// </summary>
+        public KeyVaultReferenceCollectionPropertiesKeyToReferenceStatuses()
+        {
+
+        }
+    }
+    public partial interface IKeyVaultReferenceCollectionPropertiesKeyToReferenceStatuses :
+        Microsoft.Azure.PowerShell.Cmdlets.Functions.Runtime.IJsonSerializable,
+        Microsoft.Azure.PowerShell.Cmdlets.Functions.Runtime.IAssociativeArray<Microsoft.Azure.PowerShell.Cmdlets.Functions.Models.Api20190801.IApiKvReference>
+    {
+
+    }
+    internal partial interface IKeyVaultReferenceCollectionPropertiesKeyToReferenceStatusesInternal
+
+    {
+
+    }
+}
