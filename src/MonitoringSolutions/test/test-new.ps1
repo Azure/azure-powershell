70e5d9063516e3209d51f57217598dd8d6a31992
Yeming Liu <Yeming.Liu@microsoft.com>
2020-06-05 14:32:10 +0800

    test

---
+++ src/MonitoringSolutions/test/test-new.ps1
@@ -0,0 +1,15 @@
+$Name = 'Containers(yeminglog)'
+$PlanName = 'Containers(yeminglog)'
+$Location = 'westus2'
+$ResourceGroupName = 'yemingwestus2'
+$PlanProduct = "OMSGallery/Containers"
+$PlanPublisher = 'Microsoft'
+$WorkspaceResourceId = '/subscriptions/9e223dbe-3399-4e19-88eb-0975f02ac87f/resourcegroups/yemingwestus2/providers/microsoft.operationalinsights/workspaces/yeminglog'
+New-AzMonitorLogAnalyticsSolution -Name $Name -Location $Location -ResourceGroupName $ResourceGroupName -PlanName $PlanName -PlanProduct $PlanProduct -PlanPublisher $PlanPublisher -PlanPromotionCode "" -WorkspaceResourceId $WorkspaceResourceId
+
+#     [-PlanName ""]
+
+#     [-Publisher ""]
+
+#     [-PromotionCode ""]
+
