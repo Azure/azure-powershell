72bf655aabf4ec677de09b2cf3a7851643bfe444
Tejas Shah <tejas.shah@microsoft.com>
2020-06-09 15:43:02 -0700

    StaticAnalysis

--- src/Network/Network.Test/ScenarioTests/AzureFirewallPolicyTests.cs
+++ src/Network/Network.Test/ScenarioTests/AzureFirewallPolicyTests.cs
@@ -26,7 +26,6 @@ namespace Commands.Network.Test.ScenarioTests
         {
         }
 
-        //[Fact(Skip = "Fails with Internal Server Error")]
         [Fact]
         [Trait(Category.AcceptanceType, Category.CheckIn)]
         [Trait(Category.Owner, NrpTeamAlias.azurefirewall)]
