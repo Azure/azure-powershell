4ce2e151fc6f95ea223b2d315442ca9c587e3433
wyunchi-ms <yunwang@microsoft.com>
2020-06-24 17:04:22 +0800

    save

--- src/CostManagement/resources/Create.ps1
+++ src/CostManagement/resources/Create.ps1
@@ -9,13 +9,11 @@ $Aggregation1 = @{ name = 'PreTaxCost'; function='sum'}
 $Aggregation2 = @{ name = 'Cost'; function='sum'}
 $AggregationDict = @{costsum=$aggregation1; cost=$aggregation2}
 
-# New-AzCostManagementExport -Debug -Scope "subscriptions/9e223dbe-3399-4e19-88eb-0975f02ac87f" -Name "TestExport1" `
-#     -ScheduleStatus "Active" -ScheduleRecurrence "Daily" -RecurrencePeriodFrom "2020-06-11T20:00:00Z" `
-#     -RecurrencePeriodTo "2020-06-20T00:00:00Z" -Format "Csv" `
-#     -DestinationResourceId "/subscriptions/9e223dbe-3399-4e19-88eb-0975f02ac87f/resourceGroups/wyunchi-costmanagement/providers/Microsoft.Storage/storageAccounts/wyunchistorageaccount" `
-#     -DestinationContainer "exports" -DestinationRootFolderPath "ad-hoc" -DefinitionType "Usage" -DefinitionTimeframe "MonthToDate" `
-    # -DatasetGranularity "Daily" -ConfigurationColumn @('SubscriptionGuid', 'MeterId', 'InstanceId', 'ResourceGroup', 'PreTaxCost')
-    # -DatasetAggregation $AggregationDict -DatasetGrouping @(@{type='Dimension'; name='SubscriptionName'}; @{type='Tag'; name='Environment'})
-    # -DatasetFilter $QueryFileterAnd -Debug
-Invoke-AzCostManagementUsageQuery -Type AmortizedCost -Scope "subscriptions/9e223dbe-3399-4e19-88eb-0975f02ac87f" -Timeframe 'MonthToDate' -DatasetGranularity 'Daily'
+New-AzCostManagementExport -Debug -Scope "subscriptions/9e223dbe-3399-4e19-88eb-0975f02ac87f" -Name "TestExport7" `
+    -ScheduleStatus "Active" -ScheduleRecurrence "Daily" -RecurrencePeriodFrom "2020-06-25T20:00:00Z" `
+    -RecurrencePeriodTo "2020-06-29T00:00:00Z" -Format "Csv" `
+    -DestinationResourceId "/subscriptions/9e223dbe-3399-4e19-88eb-0975f02ac87f/resourceGroups/wyunchi-costmanagement/providers/Microsoft.Storage/storageAccounts/wyunchistorageaccount" `
+    -DestinationContainer "exports" -DestinationRootFolderPath "ad-hoc" -DefinitionType "Usage" -DefinitionTimeframe "MonthToDate" `
+    -DatasetGranularity "Daily" -DatasetFilter $QueryFileterAnd
+# Invoke-AzCostManagementUsageQuery -Type AmortizedCost -Scope "subscriptions/9e223dbe-3399-4e19-88eb-0975f02ac87f" -Timeframe 'MonthToDate' -DatasetGranularity 'Daily'
     # -DatasetFilter $QueryFilterOr -DatasetGranularity 'Daily'
