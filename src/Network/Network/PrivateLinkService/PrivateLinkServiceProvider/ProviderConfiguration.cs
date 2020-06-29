aadb558fdfe2225944101e2d65bfa29a9e51562a
ankushbindlish2 <34896519+ankushbindlish2@users.noreply.github.com>
2020-06-28 20:17:24 -0700

    Register StorageSync in Network cmdlets (#12251)
    * Update ProviderConfiguration.cs
    
    * Update ProviderConfiguration.cs
    
    * changelog
    
    * Update ProviderConfiguration.cs

--- src/Network/Network/PrivateLinkService/PrivateLinkServiceProvider/ProviderConfiguration.cs
+++ src/Network/Network/PrivateLinkService/PrivateLinkServiceProvider/ProviderConfiguration.cs
@@ -18,6 +18,7 @@ namespace Microsoft.Azure.Commands.Network.PrivateLinkService.PrivateLinkService
             RegisterConfiguration("Microsoft.DBforPostgreSQL/servers", "2018-06-01");
             RegisterConfiguration("Microsoft.Insights/privateLinkScopes", "2019-10-17-preview");
             RegisterConfiguration("Microsoft.Storage/storageAccounts", "2019-06-01", false);
+            RegisterConfiguration("Microsoft.StorageSync/storageSyncServices", "2020-03-01");
             RegisterConfiguration("Microsoft.KeyVault/vaults", "2019-09-01", false);
             RegisterConfiguration("Microsoft.DocumentDB/databaseAccounts", "2019-08-01-preview");
             RegisterConfiguration("Microsoft.CognitiveServices/accounts", "2017-04-18");
@@ -27,14 +28,17 @@ namespace Microsoft.Azure.Commands.Network.PrivateLinkService.PrivateLinkService
             RegisterConfiguration("Microsoft.EventGrid/topics", "2020-04-01-preview");
             RegisterConfiguration("Microsoft.EventGrid/domains", "2020-04-01-preview");
             RegisterConfiguration("Microsoft.Network/applicationgateways", "2020-05-01");
+
         }
 
         private static void RegisterConfiguration(string type, string apiVersion, bool hasConnectionsURI = true)
         {
-            ProviderConfiguration configuration = new ProviderConfiguration();
-            configuration.Type = type;
-            configuration.ApiVersion = apiVersion;
-            configuration.HasConnectionsURI = hasConnectionsURI;
+            ProviderConfiguration configuration = new ProviderConfiguration
+            {
+                Type = type,
+                ApiVersion = apiVersion,
+                HasConnectionsURI = hasConnectionsURI
+            };
             _configurations.Add(type, configuration);
         }
 
