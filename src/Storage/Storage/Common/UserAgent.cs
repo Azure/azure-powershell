6aa258fec55c2cbe5a842c2536a049c840944574
Wei Wei <weiwei@microsoft.com>
2020-07-07 13:01:31 +0800

    [Storage] Support Blobversion with Track2 SDK (#12323)

---
+++ src/Storage/Storage/Common/UserAgent.cs
@@ -0,0 +1,20 @@
+﻿using Azure.Core;
+using Azure.Core.Pipeline;
+
+namespace Microsoft.WindowsAzure.Commands.Storage
+{
+    public class UserAgentPolicy : HttpPipelineSynchronousPolicy
+    {
+        private string _userAgent;
+
+        public UserAgentPolicy(string userAgent)
+        {
+            _userAgent = userAgent;
+        }
+
+        public override void OnSendingRequest(HttpMessage message)
+        {
+            message.Request.Headers.Add("User-Agent", _userAgent);
+        }
+    }
+}
