9ee91bc37431a0b9bda6d9b10c9b7b07005c3f28
Chenlei Yang <t-chenly@microsoft.com>
2020-07-15 16:54:14 +0800

    Add a new method to get default parameter set.

--- tools/Tools.Common/Models/CmdletMetadata.cs
+++ tools/Tools.Common/Models/CmdletMetadata.cs
@@ -275,6 +275,11 @@ namespace Tools.Common.Models
         public string DefaultParameterSetName { get; set; }
 
         /// <summary>
+        /// The default parameter set
+        /// </summary>
+        public ParameterSetMetadata DefaultParameterSet { get { return _parameterSets.Find(p => string.Equals(p.Name, this.DefaultParameterSetName, StringComparison.OrdinalIgnoreCase)); } }
+
+        /// <summary>
         /// The set of output types for the cmdlet
         /// </summary>
         public List<OutputMetadata> OutputTypes { get { return _outputTypes; } }
