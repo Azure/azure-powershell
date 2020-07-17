cd8eeb30e75d5d1fb3f59f9327877638f8865272
Yeming Liu <Yeming.Liu@microsoft.com>
2020-07-17 11:28:08 +0800

    test plan

--- src/AppConfiguration/test/test-plan.ps1
+++ src/AppConfiguration/test/test-plan.ps1
@@ -14,3 +14,13 @@
 #   $result.IdentityType should equal "SystemAssigned, UserAssigned"
 #   $result.IdentityUserAssignedIdentity.Count should > 0
 
+# 2. Update-AzAppConfigurationStore
+# 2.1 Assign a system assigned or user assigned managed identity
+# 2.2 Enable encryption using system-assigned identity (-EncryptionKeyIdentifier https://yemingac.vault.azure.net/keys/encryptionkey/10f3811b78a24208ae5ebbfe18983f6d)
+#   To enable encryption, the managed identity must have access to the key vault (key vault must enable soft delete, purge protection), with key permissions GET WRAP UNWRAP
+#   $result.KeyVaultPropertyKeyIdentifier should equal EncryptionKeyIdentifier
+# 2.3 Enable encryption using user-assigned identity (-EncryptionKeyIdentifier https://yemingac.vault.azure.net/keys/encryptionkey/10f3811b78a24208ae5ebbfe18983f6d -KeyVaultIdentityClientId /subscriptions/9e223dbe-3399-4e19-88eb-0975f02ac87f/resourcegroups/yemingac/providers/Microsoft.ManagedIdentity/userAssignedIdentities/yemingactest)
+# 2.4 Update tags
+# 2.5 Upgrade SKU (free -> Standard)
+# 2.6 Disable managed identity (-IdentityType None)
+
