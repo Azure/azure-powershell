bd9a040734e44280a2420ee6cd0de111075032b6
Yunchi Wang <54880216+wyunchi-ms@users.noreply.github.com>
2020-07-17 13:10:33 +0800

    Update SetAzureExpressRouteCircuitConnectionConfigCommand.cs

--- src/Network/Network/ExpressRouteCircuit/Peering/Connection/SetAzureExpressRouteCircuitConnectionConfigCommand.cs
+++ src/Network/Network/ExpressRouteCircuit/Peering/Connection/SetAzureExpressRouteCircuitConnectionConfigCommand.cs
@@ -79,7 +79,7 @@ namespace Microsoft.Azure.Commands.Network {
 
             if (this.AddressPrefix != null)
             {
-                if (Utils.IsIpv6(this.AddressPrefixType))
+                if (AddressTypeUtils.IsIpv6(this.AddressPrefixType))
                 {
                     if (circuitconnection.IPv6CircuitConnectionConfig != null)
                     {
