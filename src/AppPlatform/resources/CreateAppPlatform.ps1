21e88801d9b4bd529da70e6b0f3c689c9db04f0d
wyunchi-ms <yunwang@microsoft.com>
2020-07-19 22:05:10 +0800

    Update

--- src/AppPlatform/resources/CreateAppPlatform.ps1
+++ src/AppPlatform/resources/CreateAppPlatform.ps1
@@ -1,17 +1,26 @@
-# New-AzResourceGroup -ResourceGroupName wyunchi-app-platform -Location eastus
-New-AzAppPlatformService -ResourceGroupName wyunchi-app-platform -name pwsh-app-platform -Location eastus
+$ResourceGroupName = 'wyunchi-app-platform'
+$ServiceName = 'pwsh-app-platform'
+$GatewayAppName = 'gateway'
+$GatewayJarPath = 'C:\Users\yunwang\source\repos\azure-powershell-generation-db\src\piggymetrics\gateway\target\gateway.jar'
+$AuthAppName = 'auth-service'
+$AuthJarPath = 'C:\Users\yunwang\source\repos\azure-powershell-generation-db\src\piggymetrics\auth-service\target\auth-service.jar'
+$AccountAppName = 'account-service'
+$AccountJarPath = 'C:\Users\yunwang\source\repos\azure-powershell-generation-db\src\piggymetrics\account-service\target\account-service.jar'
 
-New-AzappPlatformApp -ResourceGroupName wyunchi-app-platform -ServiceName pwsh-app-platform -Name gateway -ActiveDeploymentName default
-New-AzappPlatformDeployment -ResourceGroupName wyunchi-app-platform -ServiceName pwsh-app-platform -AppName gateway -DeploymentName default -DeploymentSettingCpu 1 -DeploymentSettingInstanceCount 1 -DeploymentSettingMemoryInGb 1 -SourceRelativePath '<default>'
-Update-azappplatformapp -ResourceGroupName wyunchi-app-platform -ServiceName pwsh-app-platform -AppName gateway -ActiveDeploymentName default -Public
-Deploy-AzAppPlatformApp -ResourceGroupName wyunchi-app-platform -ServiceName pwsh-app-platform -AppName gateway -JarPath C:\Users\yunwang\source\repos\azure-powershell-generation-db\src\piggymetrics\gateway\target\gateway.jar
+New-AzResourceGroup -ResourceGroupName $ResourceGroupName -Location eastus
+New-AzAppPlatformService -ResourceGroupName $ResourceGroupName -name $ServiceName -Location eastus
 
-New-AzappPlatformApp -ResourceGroupName wyunchi-app-platform -ServiceName pwsh-app-platform -Name auth-service -ActiveDeploymentName default
-New-AzappPlatformDeployment -ResourceGroupName wyunchi-app-platform -ServiceName pwsh-app-platform -AppName auth-service -DeploymentName default -DeploymentSettingCpu 1 -DeploymentSettingInstanceCount 1 -DeploymentSettingMemoryInGb 1 -SourceRelativePath '<default>'
-Update-azappplatformapp -ResourceGroupName wyunchi-app-platform -ServiceName pwsh-app-platform -AppName auth-service -ActiveDeploymentName default
-Deploy-AzAppPlatformApp -ResourceGroupName wyunchi-app-platform -ServiceName pwsh-app-platform -AppName auth-service -JarPath C:\Users\yunwang\source\repos\azure-powershell-generation-db\src\piggymetrics\auth-service\target\auth-service.jar
+New-AzappPlatformApp -ResourceGroupName $ResourceGroupName -ServiceName $ServiceName -Name $GatewayAppName -ActiveDeploymentName default
+New-AzappPlatformDeployment -ResourceGroupName $ResourceGroupName -ServiceName $ServiceName -AppName $GatewayAppName -DeploymentName default -DeploymentSettingCpu 1 -DeploymentSettingInstanceCount 1 -DeploymentSettingMemoryInGb 1 -SourceRelativePath '<default>'
+Update-azappplatformapp -ResourceGroupName $ResourceGroupName -ServiceName $ServiceName -AppName $GatewayAppName -ActiveDeploymentName default -Public
+Deploy-AzAppPlatformApp -ResourceGroupName $ResourceGroupName -ServiceName $ServiceName -AppName $GatewayAppName -JarPath $GatewayJarPath
 
-New-AzappPlatformApp -ResourceGroupName wyunchi-app-platform -ServiceName pwsh-app-platform -Name account-service -ActiveDeploymentName default
-New-AzappPlatformDeployment -ResourceGroupName wyunchi-app-platform -ServiceName pwsh-app-platform -AppName account-service -DeploymentName default -DeploymentSettingCpu 1 -DeploymentSettingInstanceCount 1 -DeploymentSettingMemoryInGb 1 -SourceRelativePath '<default>'
-Update-AzappPlatformapp -ResourceGroupName wyunchi-app-platform -ServiceName pwsh-app-platform -AppName account-service -ActiveDeploymentName default
-Deploy-AzAppPlatformApp -ResourceGroupName wyunchi-app-platform -ServiceName pwsh-app-platform -AppName account-service -JarPath C:\Users\yunwang\source\repos\azure-powershell-generation-db\src\piggymetrics\account-service\target\account-service.jar
+New-AzappPlatformApp -ResourceGroupName $ResourceGroupName -ServiceName $ServiceName -Name $AuthAppName -ActiveDeploymentName default
+New-AzappPlatformDeployment -ResourceGroupName $ResourceGroupName -ServiceName $ServiceName -AppName $AuthAppName -DeploymentName default -DeploymentSettingCpu 1 -DeploymentSettingInstanceCount 1 -DeploymentSettingMemoryInGb 1 -SourceRelativePath '<default>'
+Update-azappplatformapp -ResourceGroupName $ResourceGroupName -ServiceName $ServiceName -AppName $AuthAppName -ActiveDeploymentName default
+Deploy-AzAppPlatformApp -ResourceGroupName $ResourceGroupName -ServiceName $ServiceName -AppName $AuthAppName -JarPath $AuthJarPath
+
+New-AzappPlatformApp -ResourceGroupName $ResourceGroupName -ServiceName $ServiceName -Name $AccountAppName -ActiveDeploymentName default
+New-AzappPlatformDeployment -ResourceGroupName $ResourceGroupName -ServiceName $ServiceName -AppName $AccountAppName -DeploymentName default -DeploymentSettingCpu 1 -DeploymentSettingInstanceCount 1 -DeploymentSettingMemoryInGb 1 -SourceRelativePath '<default>'
+Update-AzappPlatformapp -ResourceGroupName $ResourceGroupName -ServiceName $ServiceName -AppName $AccountAppName -ActiveDeploymentName default
+Deploy-AzAppPlatformApp -ResourceGroupName $ResourceGroupName -ServiceName $ServiceName -AppName $AccountAppName -JarPath $AccountJarPath
