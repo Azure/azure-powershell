277cb55043f772a4c7fa0d24e9e7389002969eb5
Shenglong Li <shenglong.li@outlook.com>
2020-07-21 16:27:55 -0700

    Implement spliting tenant and MG level resource IDs

--- src/Resources/Resources.Test/Components/ResourceIdUtilityTests.cs
+++ src/Resources/Resources.Test/Components/ResourceIdUtilityTests.cs
@@ -9,26 +9,64 @@
     {
         [Fact]
         [Trait(Category.AcceptanceType, Category.CheckIn)]
-        public void ParseResourceId_WithSubscriptionLevelId_ReturnsSubscriptionScope()
+        public void SplitResourceId_TenantLevelId_ReturnsTenantScopeAndRelativeId()
         {
-            Guid subscriptionId = Guid.NewGuid();
             Guid roleAssignmentId = Guid.NewGuid();
+            string resourceId = $"/providers/Microsoft.Authorization/roleAssignments/{roleAssignmentId}";
+            
+            (string scope, string relativeResourceId) = ResourceIdUtility.SplitResourceId(resourceId);
 
-            (string scope, string relativeResourceId) = ResourceIdUtility.ParseResourceId(
-                $"/subscriptions/{subscriptionId}/providers/Microsoft.Authorization/roleAssignment/{roleAssignmentId}");
-
-            Assert.Equal($"/subscriptions/{subscriptionId}", scope);
-            Assert.Equal($"Microsoft.Authorization/roleAssignment/{roleAssignmentId}", relativeResourceId);
+            Assert.Equal($"/", scope);
+            Assert.Equal($"Microsoft.Authorization/roleAssignments/{roleAssignmentId}", relativeResourceId);
         }
 
         [Fact]
         [Trait(Category.AcceptanceType, Category.CheckIn)]
-        public void ParseResourceId_WithResourceScopeLevelId_ReturnsResourceGroupScope()
+        public void SplitResourceId_ManagementGroupAsResource_ReturnsTenantScopeAndManagementGroupId()
+        {
+            string resourceId = "/providers/Microsoft.Management/myManagementGroup";
+
+            (string scope, string relativeResourceId) = ResourceIdUtility.SplitResourceId(resourceId);
+
+            Assert.Equal($"/", scope);
+            Assert.Equal($"Microsoft.Management/myManagementGroup", relativeResourceId);
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void SplitResourceId_ManagementGroupLevelId_ReturnsManagementGroupScopeAndRelativeId()
+        {
+            Guid roleAssignmentId = Guid.NewGuid();
+            string resourceId = $"/providers/Microsoft.Management/ManagementGroups/myManagementGroup/providers/Microsoft.Authorization/roleAssignments/{roleAssignmentId}";
+
+            (string scope, string relativeResourceId) = ResourceIdUtility.SplitResourceId(resourceId);
+
+            Assert.Equal($"/providers/Microsoft.Management/ManagementGroups/myManagementGroup", scope);
+            Assert.Equal($"Microsoft.Authorization/roleAssignments/{roleAssignmentId}", relativeResourceId);
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void SplitResourceId_SubscriptionLevelId_ReturnsSubscriptionScopeAndRelativeId()
         {
             Guid subscriptionId = Guid.NewGuid();
+            Guid roleAssignmentId = Guid.NewGuid();
+            string resourceId = $"/subscriptions/{subscriptionId}/providers/Microsoft.Authorization/roleAssignments/{roleAssignmentId}";
 
-            (string scope, string relativeResourceId) = ResourceIdUtility.ParseResourceId(
-                $"/subscriptions/{subscriptionId}/resourceGroups/test-what-if-rg/providers/Microsoft.Sql/servers/TestServer");
+            (string scope, string relativeResourceId) = ResourceIdUtility.SplitResourceId(resourceId);
+
+            Assert.Equal($"/subscriptions/{subscriptionId}", scope);
+            Assert.Equal($"Microsoft.Authorization/roleAssignments/{roleAssignmentId}", relativeResourceId);
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void SplitResourceId_ResourceGroupLevelId_ReturnsResourceGroupScopeAndRelativeId()
+        {
+            Guid subscriptionId = Guid.NewGuid();
+            string resourceId = $"/subscriptions/{subscriptionId}/resourceGroups/test-what-if-rg/providers/Microsoft.Sql/servers/TestServer";
+
+            (string scope, string relativeResourceId) = ResourceIdUtility.SplitResourceId(resourceId);
 
             Assert.Equal($"/subscriptions/{subscriptionId}/resourceGroups/test-what-if-rg", scope);
             Assert.Equal("Microsoft.Sql/servers/TestServer", relativeResourceId);
@@ -36,12 +74,12 @@
 
         [Fact]
         [Trait(Category.AcceptanceType, Category.CheckIn)]
-        public void ParseResourceId_WithResourceGroupAsResource_ReturnsSubscriptionScope()
+        public void SplitResourceId_ResourceGroupAsResource_ReturnsSubscriptionScopeAndResourceGroupId()
         {
             Guid subscriptionId = Guid.NewGuid();
+            string resourceId = $"/subscriptions/{subscriptionId}/resourceGroups/test-what-if-rg";
 
-            (string scope, string relativeResourceId) = ResourceIdUtility.ParseResourceId(
-                $"/subscriptions/{subscriptionId}/resourceGroups/test-what-if-rg");
+            (string scope, string relativeResourceId) = ResourceIdUtility.SplitResourceId(resourceId);
 
             Assert.Equal($"/subscriptions/{subscriptionId}", scope);
             Assert.Equal("resourceGroups/test-what-if-rg", relativeResourceId);
