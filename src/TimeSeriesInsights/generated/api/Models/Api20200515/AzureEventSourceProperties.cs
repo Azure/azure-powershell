e84a545081737612b6270661f01da7fddc4859f1
Yabo Hu <yabhu@microsoft.com>
2020-08-04 16:46:15 +0800

    Timeseriesiinsights api upgrade (#12523)
    * upgrade api version to 2020-05-15
    
    * Update Az.TimeSeriesInsights.psd1

---
+++ src/TimeSeriesInsights/generated/api/Models/Api20200515/AzureEventSourceProperties.cs
@@ -0,0 +1,72 @@
+namespace Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515
+{
+    using static Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Runtime.Extensions;
+
+    /// <summary>Properties of an event source that reads events from an event broker in Azure.</summary>
+    public partial class AzureEventSourceProperties :
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IAzureEventSourceProperties,
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IAzureEventSourcePropertiesInternal,
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Runtime.IValidates
+    {
+        /// <summary>
+        /// Backing field for Inherited model <see cref= "Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEventSourceCommonProperties"
+        /// />
+        /// </summary>
+        private Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEventSourceCommonProperties __eventSourceCommonProperties = new Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.EventSourceCommonProperties();
+
+        /// <summary>Backing field for <see cref="EventSourceResourceId" /> property.</summary>
+        private string _eventSourceResourceId;
+
+        /// <summary>The resource id of the event source in Azure Resource Manager.</summary>
+        [Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Origin(Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.PropertyOrigin.Owned)]
+        public string EventSourceResourceId { get => this._eventSourceResourceId; set => this._eventSourceResourceId = value; }
+
+        /// <summary>
+        /// The event property that will be used as the event source's timestamp. If a value isn't specified for timestampPropertyName,
+        /// or if null or empty-string is specified, the event creation time will be used.
+        /// </summary>
+        [Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Origin(Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.PropertyOrigin.Inherited)]
+        public string TimestampPropertyName { get => ((Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEventSourceCommonPropertiesInternal)__eventSourceCommonProperties).TimestampPropertyName; set => ((Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEventSourceCommonPropertiesInternal)__eventSourceCommonProperties).TimestampPropertyName = value; }
+
+        /// <summary>Creates an new <see cref="AzureEventSourceProperties" /> instance.</summary>
+        public AzureEventSourceProperties()
+        {
+
+        }
+
+        /// <summary>Validates that this object meets the validation criteria.</summary>
+        /// <param name="eventListener">an <see cref="Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Runtime.IEventListener" /> instance that will receive validation
+        /// events.</param>
+        /// <returns>
+        /// A < see cref = "global::System.Threading.Tasks.Task" /> that will be complete when validation is completed.
+        /// </returns>
+        public async global::System.Threading.Tasks.Task Validate(Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Runtime.IEventListener eventListener)
+        {
+            await eventListener.AssertNotNull(nameof(__eventSourceCommonProperties), __eventSourceCommonProperties);
+            await eventListener.AssertObjectIsValid(nameof(__eventSourceCommonProperties), __eventSourceCommonProperties);
+        }
+    }
+    /// Properties of an event source that reads events from an event broker in Azure.
+    public partial interface IAzureEventSourceProperties :
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Runtime.IJsonSerializable,
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEventSourceCommonProperties
+    {
+        /// <summary>The resource id of the event source in Azure Resource Manager.</summary>
+        [Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Runtime.Info(
+        Required = true,
+        ReadOnly = false,
+        Description = @"The resource id of the event source in Azure Resource Manager.",
+        SerializedName = @"eventSourceResourceId",
+        PossibleTypes = new [] { typeof(string) })]
+        string EventSourceResourceId { get; set; }
+
+    }
+    /// Properties of an event source that reads events from an event broker in Azure.
+    internal partial interface IAzureEventSourcePropertiesInternal :
+        Microsoft.Azure.PowerShell.Cmdlets.TimeSeriesInsights.Models.Api20200515.IEventSourceCommonPropertiesInternal
+    {
+        /// <summary>The resource id of the event source in Azure Resource Manager.</summary>
+        string EventSourceResourceId { get; set; }
+
+    }
+}
