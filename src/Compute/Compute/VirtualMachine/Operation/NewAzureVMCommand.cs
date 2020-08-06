05d7ba403a3eb7ecace7f6c1d45dc4b2218b3f0b
Adam Sandor <adsandor@microsoft.com>
2020-08-06 18:13:44 -0400

    Update NewAzureVMCommand.cs

--- src/Compute/Compute/VirtualMachine/Operation/NewAzureVMCommand.cs
+++ src/Compute/Compute/VirtualMachine/Operation/NewAzureVMCommand.cs
@@ -259,6 +259,14 @@ namespace Microsoft.Azure.Commands.Compute
         [Parameter(ParameterSetName = DiskFileParameterSet, Mandatory = false,
             HelpMessage = "EncryptionAtHost property can be used by user in the request to enable or disable the Host Encryption for the virtual machine. This will enable the encryption for all the disks including Resource/Temp disk at host itself.")]
         public SwitchParameter EncryptionAtHost { get; set; } = false;
+        
+        [Parameter(ParameterSetName = SimpleParameterSet, Mandatory = false,
+            HelpMessage = "The resource id of the dedicated host group, on which the customer wants their VM placed using automatic placement.",
+            ValueFromPipelineByPropertyName = true)]
+        [Parameter(ParameterSetName = DiskFileParameterSet, Mandatory = false,
+            HelpMessage = "The resource id of the dedicated host group, on which the customer wants their VM placed using automatic placement.",
+            ValueFromPipelineByPropertyName = true)]
+        public string HostGroupId { get; set; }
 
         public override void ExecuteCmdlet()
         {
