3b0bc35d062645f74dcb9562fbfe54d0c9585d4d
wyunchi-ms <yunwang@microsoft.com>
2020-08-26 17:03:24 +0800

    Update api-version

---
+++ src/CloudService/custom/Serialization/CloudServiceExtensionPropertiesSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.PowerShell.Cmdlets.CloudService.Models.Api20201001Preview
+{
+    public partial class CloudServiceExtensionPropertiesSettings
+    {
+        partial void AfterFromJson(Microsoft.Azure.PowerShell.Cmdlets.CloudService.Runtime.Json.JsonObject json)
+        {
+            // Perform serialization using the appropriate item instantiator
+            Microsoft.Azure.PowerShell.Cmdlets.CloudService.Runtime.JsonSerializable.FromJson(
+                json, 
+                ((Microsoft.Azure.PowerShell.Cmdlets.CloudService.Runtime.IAssociativeArray<Microsoft.Azure.PowerShell.Cmdlets.CloudService.Models.Api20201001Preview.ICloudServiceExtensionPropertiesSettings>)this).AdditionalProperties, 
+                (js) => Microsoft.Azure.PowerShell.Cmdlets.CloudService.Models.Api20201001Preview.CloudServiceExtensionPropertiesSettings.FromJson(js), 
+                null);
+        }
+    }
+}
