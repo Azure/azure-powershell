286b58acccdaeefec9505832a064abd84f4e2ecc
Daniel Orozco <Daniel.Orozco@microsoft.com>
2020-09-01 14:17:12 -0700

    Remove old TODO

--- src/Resources/Resources/RoleAssignments/SetAzureRoleAssignmentCommand.cs
+++ src/Resources/Resources/RoleAssignments/SetAzureRoleAssignmentCommand.cs
@@ -111,7 +111,6 @@ namespace Microsoft.Azure.Commands.Resources
                 throw new ArgumentException("Changing a property other than 'Description', 'Condition' or 'Condition Version' is currently not supported.");
             }
 
-            // TODO check cases when going from null to something or from something to null
             // If ConditionVersion is changed, validate it's in the allowed values
             var oldConditionVersion = double.Parse(InputObject.ConditionVersion ?? "0.0");
             var newConditionVersion = double.Parse(fetchedRole.ConditionVersion ?? "2.0");
