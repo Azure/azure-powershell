9b2d78cfa204bab6fc0cddfec082185dcc47a265
Joshua Van Daalen <emails@joshuavandaalen.com.au>
2020-09-03 14:54:54 +1000

    Update spelling of Proeprty to Property (#12821)
    Co-authored-by: Joshua Van Daalen <admin@joshuavandaalen.com.au>

--- src/Storage/Storage.Management/Blob/GetAzureStorageBlobServiceProperties.cs
+++ src/Storage/Storage.Management/Blob/GetAzureStorageBlobServiceProperties.cs
@@ -27,7 +27,7 @@ namespace Microsoft.Azure.Commands.Management.Storage
     /// <summary>
     /// Modify Azure Storage service properties
     /// </summary>
-    [CmdletOutputBreakingChange(typeof(PSBlobServiceProperties), ChangeDescription = "The deprecated proeprty RestorePolicy.LastEnabledTime will be removed in a future release.")]
+    [CmdletOutputBreakingChange(typeof(PSBlobServiceProperties), ChangeDescription = "The deprecated property RestorePolicy.LastEnabledTime will be removed in a future release.")]
     [Cmdlet("Get", ResourceManager.Common.AzureRMConstants.AzureRMPrefix + StorageBlobServiceProperty, DefaultParameterSetName = AccountNameParameterSet), OutputType(typeof(PSBlobServiceProperties))]
     public class GetAzStorageBlobServicePropertyCommand : StorageBlobBaseCmdlet
     {
