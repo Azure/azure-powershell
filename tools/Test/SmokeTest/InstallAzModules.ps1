565170793492b068aac6562af5ac0715f6a686ba
Beisi Zhou <zhoubeisi@gmail.com>
2020-09-11 16:20:53 +0800

    Get source location from environment variable

--- tools/Test/SmokeTest/InstallAzModules.ps1
+++ tools/Test/SmokeTest/InstallAzModules.ps1
@@ -21,8 +21,7 @@ switch ($gallery) {
     'LocalRepo'
     {
         Write-Host "Registering $gallery Trusted..."
-        $sourceLocation = Join-Path -Path $env:PIPELINE.WORKSPACE -ChildPath "LocalRepo"
-        Register-PSRepository -Name $gallery -SourceLocation $sourceLocation -PackageManagementProvider NuGet -InstallationPolicy Trusted
+        Register-PSRepository -Name $gallery -SourceLocation $env:SourceLocation -PackageManagementProvider NuGet -InstallationPolicy Trusted
         break;
     }
     Default 
