3197e9e769fac34c690ae08a08e992a4b55ec288
Beisi Zhou <zhoubeisi@gmail.com>
2020-09-14 12:50:16 +0800

    Update UpdateAnIndividualModule.ps1

--- tools/Test/SmokeTest/UpdateAnIndividualModule.ps1
+++ tools/Test/SmokeTest/UpdateAnIndividualModule.ps1
@@ -6,30 +6,30 @@ param(
 )
 
 # Get previous version of Az.Compute
-$versions = (find-module Az.Compute -Repository PSGallery -AllVersions).Version | Sort-Object -Descending
-
-if ($versions.Count -ge 2) {
-    # Install previous version of Az.Compute
-    $previousVersion = $versions[1]
-    Write-Host '$previousVersion:', $previousVersion
-    Install-Module -Name Az.Compute -Repository $gallery -RequiredVersion $previousVersion -Scope CurrentUser -AllowClobber -Force
-
-    #Update Az.Compute
-    Update-Module -Name Az.Compute -Scope CurrentUser -Force
-    Write-Host "Installed latest version of Az.Compute"
-
-    # Check Az.Compute
-    Get-AzVM
-        
-    # Check version
-    $azComputeVersion = (Get-Module Az.Compute).Version | Sort-Object -Descending
-    Write-Host "Current version of Az.Compute", $azComputeVersion
-
-    if ($azComputeVersion -ne $versions[0]) {
-        throw "Update Az.Compute failed"
-    }
+if($gallery -eq "LocalRepo"){
+    $versions = (find-module Az.Compute -Repository PSGallery -AllVersions).Version | Sort-Object -Descending
+    $previousVersion = $versions[0]
 }else{
-    Write-Warning "Only one version available for Az.Compute"
-    Write-Host 'Az.Compute versions:', $versions
+    $versions = (find-module Az.Compute -Repository $gallery -AllVersions).Version | Sort-Object -Descending
+    $previousVersion = $versions[1]
+}
+
+# Install previous version of Az.Compute
+$previousVersion = $versions[1]
+Write-Host '$previousVersion:', $previousVersion
+Install-Module -Name Az.Compute -Repository $gallery -RequiredVersion $previousVersion -Scope CurrentUser -AllowClobber -Force
+
+#Update Az.Compute
+Update-Module -Name Az.Compute -Scope CurrentUser -Force
+Write-Host "Installed latest version of Az.Compute"
+
+# Check Az.Compute
+Get-AzVM
+        
+# Check version
+$azComputeVersion = (Get-Module Az.Compute).Version | Sort-Object -Descending
+Write-Host "Current version of Az.Compute", $azComputeVersion
+
+if ([System.Version]$azComputeVersion -le [System.Version]$previousVersion) {
     throw "Update Az.Compute failed"
 }
