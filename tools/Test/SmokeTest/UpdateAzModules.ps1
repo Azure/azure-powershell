42106b5f9149326d00b38b9ebb6019b779296cbc
Beisi Zhou <zhoubeisi@gmail.com>
2020-09-14 12:52:31 +0800

    Update UpdateAzModules.ps1

--- tools/Test/SmokeTest/UpdateAzModules.ps1
+++ tools/Test/SmokeTest/UpdateAzModules.ps1
@@ -5,38 +5,35 @@ param(
     $gallery
 )
 
-# Get previous version of az
-$versions = (find-module Az -Repository PSGallery -AllVersions).Version | Sort-Object -Descending
-
-if ($versions.Count -ge 2) {
-    # Install previous version of Az
-    $previousVersion = $versions[1]
-    Write-Host '$previousVersion:', $previousVersion
-
-    Install-Module -Name Az -Repository $gallery -RequiredVersion $previousVersion -Scope CurrentUser -AllowClobber -Force
-
-    #Update Az
-    Update-Module -Name Az -Scope CurrentUser -Force
-        
-    # Check Az
-    Write-Host "Get latest version of Az"
-    Get-Module -Name Az.* -ListAvailable
-
-    # Check version
-    Import-Module -MinimumVersion '2.6.0' -Name 'Az' -Force -Scope 'Global'
-    $azVersion = (get-module Az).Version
-    Write-Host "Current version of Az", $azVersion
-
-    if ($azVersion -ne $versions[0]) {
-        throw "Update Az failed"
-    }
-        
-    # Reuse connected account and select subscription for test
-    Enable-AzureRmAlias
-    Set-AzContext -Subscription "Azure SDK Powershell Test"
+# Get previous version of Az
+if($gallery -eq "LocalRepo"){
+    $versions = (find-module Az -Repository PSGallery -AllVersions).Version | Sort-Object -Descending
+    $previousVersion = $versions[0]
 }else{
-    Write-Warning "Only one version available for Az"
-    Write-Host 'az versions:', $versions
-    Get-Module PowerShellGet
+    $versions = (find-module Az -Repository $gallery -AllVersions).Version | Sort-Object -Descending
+    $previousVersion = $versions[1]
+}
+
+# Install previous version of Az
+Write-Host '$previousVersion:', $previousVersion
+Install-Module -Name Az -Repository $gallery -RequiredVersion $previousVersion -Scope CurrentUser -AllowClobber -Force
+
+#Update Az
+Update-Module -Name Az -Scope CurrentUser -Force
+        
+# Check Az
+Write-Host "Get latest version of Az"
+Get-Module -Name Az.* -ListAvailable
+
+# Check version
+Import-Module -MinimumVersion '2.6.0' -Name 'Az' -Force -Scope 'Global'
+$azVersion = (get-module Az).Version
+Write-Host "Current version of Az", $azVersion
+
+if ([System.Version]$azVersion -le [System.Version]$previousVersion) {
     throw "Update Az failed"
 }
+        
+# Reuse connected account and select subscription for test
+Enable-AzureRmAlias
+Set-AzContext -Subscription "Azure SDK Powershell Test"
