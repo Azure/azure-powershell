9d753178e5cec3301c065f34d6ea37b80c05d508
Beisi Zhou <zhoubeisi@gmail.com>
2020-09-22 10:49:45 +0800

    Update InstallAnIndividualModule.ps1

--- tools/Test/SmokeTest/InstallAnIndividualModule.ps1
+++ tools/Test/SmokeTest/InstallAnIndividualModule.ps1
@@ -9,6 +9,7 @@ Write-Host "Installing Az.Compute..."
 Install-Module -Name Az.Compute -Repository $gallery -Scope CurrentUser -AllowClobber -Force 
 
 # Load Az.Compute
+Write-Host "Running Get-AzVM to load Az.Compute..."
 Get-AzVM
 
 # Check Az.Compute version
