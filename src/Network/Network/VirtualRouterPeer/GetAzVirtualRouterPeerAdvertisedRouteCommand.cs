a43ea3f8ae8d3e8bcbaf9962e26eccb1959d4497
Litchi Yang <linxyang@microsoft.com>
2020-09-28 16:29:35 -0700

    test

--- src/Network/Network/VirtualRouterPeer/GetAzVirtualRouterPeerAdvertisedRouteCommand.cs
+++ src/Network/Network/VirtualRouterPeer/GetAzVirtualRouterPeerAdvertisedRouteCommand.cs
@@ -6,6 +6,12 @@ using System.Management.Automation;
 using Microsoft.Azure.Management.Network.Models;
 using Microsoft.Azure.Management.Internal.Resources.Utilities.Models;
 using System;
+using Microsoft.Azure.Commands;
+using Newtonsoft.Json;
+using Microsoft.Azure.Commands.Common.Authentication;
+using Microsoft.Azure.Commands.Common.Authentication.Abstractions;
+using Microsoft.Azure.Internal.Common;
+using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Azure.Commands.Network
 {
@@ -50,9 +56,27 @@ namespace Microsoft.Azure.Commands.Network
         [Parameter(Mandatory = false, HelpMessage = "Run cmdlet in the background")]
         public SwitchParameter AsJob { get; set; }
 
+        IAzureContext context;
+        private IAzureRestClient _client;
+        private IAzureRestClient ServiceClient
+        {
+            get
+            {
+                if (_client == null)
+                {
+                    var clientFactory = AzureSession.Instance.ClientFactory;
+                    _client = clientFactory.CreateArmClient<AzureRestClient>(context, AzureEnvironment.Endpoint.ResourceManager);
+                }
+
+                return _client;
+            }
+        }
+
         public override void Execute()
         {
             base.Execute();
+
+            context = DefaultContext;
 
             if (string.Equals(this.ParameterSetName, VirtualRouterPeerParameterSetNames.ByVirtualRouterPeerInputObject, StringComparison.OrdinalIgnoreCase))
             {
@@ -62,8 +86,17 @@ namespace Microsoft.Azure.Commands.Network
                 VirtualRouterName = resourceInfo.ParentResource;
             }
 
+
+            var locationHeader = this.NetworkClient.NetworkManagementClient.VirtualHubBgpConnections.ListAdvertisedRoutesWithHttpMessagesAsync(this.ResourceGroupName, this.VirtualRouterName, this.PeerName).Result.Response.Headers.Location;
+
+            string resourceId = locationHeader.LocalPath;
+            string apiVersion = locationHeader.Query.Substring(13);
+            var reponse = ServiceClient.Operations.GetResourceWithFullResponse(resourceId, apiVersion).Body;
+            dynamic routeServiceRole = JObject.Parse(reponse);
+            List<PeerRoute> peerRouteList = routeServiceRole.RouteServiceRole_IN_0.ToObject<List<PeerRoute>>();
+
             List<PSPeerRoute> advertisedRoutes = new List<PSPeerRoute>();
-            foreach (var route in this.NetworkClient.NetworkManagementClient.VirtualHubBgpConnections.ListAdvertisedRoutes(this.ResourceGroupName, this.VirtualRouterName, this.PeerName).Value)
+            foreach (var route in peerRouteList)
             {
                 advertisedRoutes.Add(NetworkResourceManagerProfile.Mapper.Map<PSPeerRoute>(route));
             }
