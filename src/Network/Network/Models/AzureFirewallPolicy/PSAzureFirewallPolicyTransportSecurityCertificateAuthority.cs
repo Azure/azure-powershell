56c205c011fe8353dde6b7d03b618aceb7ca5c03
Adi Griever <adgrieve@microsoft.com>
2020-10-13 17:54:20 +0300

    add firewall policy new commands

---
+++ src/Network/Network/Models/AzureFirewallPolicy/PSAzureFirewallPolicyTransportSecurityCertificateAuthority.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.Commands.Network.Models
+{
+    public class PSAzureFirewallPolicyTransportSecurityCertificateAuthority
+    {
+        public string Name { get; set; }
+
+        public string KeyVaultSecretId { get; set; }
+    }
+}
