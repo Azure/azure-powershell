746359b0d7d10d827e3a8b24adb0d3c84fed309b
Adi Griever <adgrieve@microsoft.com>
2020-10-14 15:33:10 +0300

    firewall policy premium test

--- src/Network/Network.Test/ScenarioTests/AzureFirewallPolicyTests.cs
+++ src/Network/Network.Test/ScenarioTests/AzureFirewallPolicyTests.cs
@@ -73,5 +73,13 @@ namespace Commands.Network.Test.ScenarioTests
         {
             TestRunner.RunTestScript("Test-AzureFirewallPolicyCRUDWithNatRuleTranslatedFQDN");
         }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        [Trait(Category.Owner, NrpTeamAlias.azurefirewall)]
+        public void TestAzureFirewallPolicyPremiumFeatures()
+        {
+            TestRunner.RunTestScript("Test-AzureFirewallPolicyPremiumFeatures");
+        }
     }
 }
