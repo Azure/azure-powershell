08ed3c6f37589aa043bb99ad11bc15f21ea8d4e1
stuartko <47676853+stuartko@users.noreply.github.com>
2020-10-13 18:20:22 -0700

    Adding cmdlets for Azure Template Specs (Preview) with deployment support (#13090)
    * Initial wrapped models for Template Specs (including versions/artifacts)
    
    * Template Specs: Initial cmdlets for Get/New/Remove. Does not include artifact support
    
    * Initial check-in of the packaging engine for Template Specs
    
    * Template Specs: Adding Export. Adding import from file (with packaging)
    
    * Removing debug console write line.
    
    * Template Specs: Initial Get Cmdlet tests. TODO: Remove temp internal package refs
    
    * Added support for templateSpecs
    
    * Template Specs: Adding basic Template Spec creation cmdlet test
    
    * Template Specs: Adding test for removal cmdlet
    
    * Remove relativePath rewrites from TemplateSpecPackagingEngine
    
    * Adding Set-AzTemplateSpec cmdlet + updating parameter set names
    
    * Template Specs: Adding cmdlet help. Adding Set-AzTemplateSpec to exports
    
    * Fixing help file typo
    
    * Template Specs: Changing -Id to -ResourceId as default id parameter (with -Id as alias)
    
    * Adding basic test for Set-AzTemplateSpec
    
    * Update SDK; Re-record tests; Test for templateSpec deployment
    
    * Fix test
    
    * Dropping model suffix from templateSpec SDK
    
    * Use latest SDK
    
    * Rerecord test
    
    * Change SDK version
    
    * rerecord tests for SDK version change
    
    * Modify output formatting
    
    * Template Specs: Fixing null reference exception
    
    * Template Specs: Adding Id to Version Model
    
    * Fix for https://github.com/Azure/template-specs/issues/9
    
    * Check version in resource ID
    
    * Template Specs: Fix path issue + issue with exporting artifactless templates
    
    * Update obsolete package
    
    * Make -TemplateSpecId work with What-If
    
    * TemplateSpecs: Adding -VersionDescription to New-AzTemplateSpec and updating behavior to 'merge' in Set-AzTemplateSpec if no version description is specified.
    
    * Template Specs: Fixing issue with Template Spec imports where relativePaths starting with a slash were not supported
    
    * Template Specs: Updating New-AzTemplateSpec to prompt on overwrite unless -Force is provided
    
    * Template Specs: Adding more user-friendly formating
    
    * Template Specs: Fixing unpacking bug on Linux. Improving formatting for Artifacts/Versions. Exposing Templates as strings only
    
    * Template Specs: Unifying multi/single version types for PSTemplateSpec. Fixing/updating tests.
    
    * Template Specs: Help file updates for recent return type changes
    
    * Template Specs: Rename TemplateJsonFile to TemplateJson. Warn on TemplateJson use for file paths
    
    * Template Specs: Removing unnecessary localization TODOs (validate preview release first)
    
    * Template Specs: Unused parameter cleanup in Set-AzTemplateSpec
    
    * Template Specs: Include parent template name in exported filename
    
    * Template Specs: Fixing missing comma in psd after merge
    
    * Template Specs: Re-record of deployment test
    
    * Template Specs: Updating changelog for upcoming Az Powershell release
    
    * Template Specs: Removing two unnecessary csproj changes kept during merge.
    
    * Upgrading ResourceManager package for test targets to 3.8.0-preview
    
    * Template Specs: Adding online help URLs for template spec cmdlets
    
    * Template Specs: Adding ShouldProcess support to all applicable cmdlets
    
    * Template Specs: Reduce/fix positional parameters in Set-AzTemplateSpec
    
    * Template Specs: Dropping a positional param
    
    Co-authored-by: Gokul Premraj <gopremra@microsoft.com>
    Co-authored-by: Shenglong Li <shenglong.li@outlook.com>

---
+++ src/Resources/Resources.Test/ScenarioTests/TemplateSpecTests.ps1
@@ -0,0 +1,246 @@
+﻿# ----------------------------------------------------------------------------------
+#
+# Copyright Microsoft Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------------
+
+<#
+.SYNOPSIS
+Returns whether or not the objects are equivalent (matching properties)
+#>
+function Get-AreObjectsEquivalent
+{
+    param(
+        [PSObject] $Lhs,
+        [PSObject] $Rhs
+    )
+
+    if ($Lhs -eq $Rhs) {
+        return $true;
+    }
+
+    $diff = Compare-Object $Lhs $Rhs 
+    return !($diff)
+}
+
+<#
+.SYNOPSIS
+Tests Template Spec GET operations with different parameter sets for PowerShell
+#>
+function Test-GetTemplateSpec
+{
+    # Setup
+    $rgname = Get-ResourceGroupName
+    $rname = Get-ResourceName
+    $rglocation = "West US 2"
+    $subId = (Get-AzContext).Subscription.SubscriptionId
+
+    try
+    {
+        # Prepare our RG and basic template spec (with two versions):
+
+        New-AzResourceGroup -Name $rgname -Location $rglocation
+
+        $sampleTemplateJson = Get-Content -Raw -Path "sampleTemplate.json"
+        $basicCreatedTemplateSpecV1 = New-AzTemplateSpec -ResourceGroupName $rgname -Name $rname -Location $rgLocation -Version "v1" -TemplateJson $sampleTemplateJson
+        $basicCreatedTemplateSpecV2 = New-AzTemplateSpec -ResourceGroupName $rgname -Name $rname -Location $rgLocation -Version "v2" -TemplateJson $sampleTemplateJson
+
+        # Validate we get expected results when getting a specific version (by rg name, template spec name, and version)
+        
+        $returnedByGetOnSpecificVersion = Get-AzTemplateSpec -ResourceGroupName $rgname -Name $rname -Version "v1"
+
+        Assert-NotNull $returnedByGetOnSpecificVersion
+        Assert-True { Get-AreObjectsEquivalent $basicCreatedTemplateSpecV1 $returnedByGetOnSpecificVersion }
+        Assert-NotNull $returnedByGetOnSpecificVersion.Versions
+        Assert-AreEqual $returnedByGetOnSpecificVersion.Versions.Length 1
+ 
+        Assert-AreEqual "v1" $returnedByGetOnSpecificVersion.Versions[0].Name
+
+        # Validate we get expected results when getting a specific version (by id, and version)
+
+        $returnedByGetOnSpecificVersionById = Get-AzTemplateSpec -ResourceId $basicCreatedTemplateSpecV2.Id -Version "v2"
+
+        Assert-NotNull $returnedByGetOnSpecificVersionById
+        Assert-True { Get-AreObjectsEquivalent $basicCreatedTemplateSpecV2 $returnedByGetOnSpecificVersionById }
+        Assert-NotNull $returnedByGetOnSpecificVersionById.Versions
+        Assert-AreEqual $returnedByGetOnSpecificVersionById.Versions.Length 1
+        Assert-AreEqual "v2" $returnedByGetOnSpecificVersionById.Versions[0].Name
+
+        # Now let's get the template spec with all versions
+        
+        $returnedByGetOnTemplateSpecName = Get-AzTemplateSpec -ResourceGroupName $rgname -Name $rname
+
+        Assert-NotNull $returnedByGetOnTemplateSpecName
+        Assert-AreEqual $returnedByGetOnTemplateSpecName.Name $rname
+        Assert-True { $returnedByGetOnTemplateSpecName.Versions -is [system.array] }
+        Assert-AreEqual $returnedByGetOnTemplateSpecName.Versions.Length 2 # Should have 2 versions
+        Assert-True { Get-AreObjectsEquivalent $returnedByGetOnTemplateSpecName.Versions[0] $basicCreatedTemplateSpecV1.Versions[0] }
+        Assert-True { Get-AreObjectsEquivalent $returnedByGetOnTemplateSpecName.Versions[1] $basicCreatedTemplateSpecV2.Versions[0] }
+
+        # Do a list on the resource group
+
+        $returnedByListOnResourceGroup = Get-AzTemplateSpec -ResourceGroupName $rgname
+        Assert-AreNotEqual 0 $returnedByListOnResourceGroup.Length
+        Assert-True { $returnedByListOnResourceGroup.Name.contains($rname) }
+
+        # Do a list on the current subscription
+
+        $returnedByListOnCurrentSub = Get-AzTemplateSpec
+        Assert-AreNotEqual 0 $returnedByListOnCurrentSub.Length
+        Assert-True { $returnedByListOnCurrentSub.Name.contains($rname) }
+    }
+    finally
+    {
+        # Cleanup
+        Clean-ResourceGroup $rgname
+    }
+}
+
+<#
+.SYNOPSIS
+Tests that we can create a basic template spec with New-AzTemplateSpec
+#>
+function Test-CreateTemplateSpec
+{
+    # Setup
+    $rgname = Get-ResourceGroupName
+    $rname = Get-ResourceName
+    $rglocation = "West US 2"
+    $subId = (Get-AzContext).Subscription.SubscriptionId
+
+    try
+    {
+        # Prepare our RG and basic template spec:
+
+        New-AzResourceGroup -Name $rgname -Location $rglocation
+
+        $sampleTemplateJson = Get-Content -Raw -Path "sampleTemplate.json"
+        $basicCreatedTemplateSpecV1 = New-AzTemplateSpec -ResourceGroupName $rgname -Name $rname -Description "My Basic Template Spec" -Location $rgLocation -Version "v1" -TemplateJson $sampleTemplateJson
+
+        # Check to make sure all of the properties match expectations
+
+        Assert-NotNull $basicCreatedTemplateSpecV1
+        Assert-AreEqual $basicCreatedTemplateSpecV1.Versions.Length 1
+        Assert-AreEqual $rname $basicCreatedTemplateSpecV1.Name
+        Assert-AreEqual "My Basic Template Spec" $basicCreatedTemplateSpecV1.Description
+        Assert-AreEqual "$rglocation".Replace(" ", "").ToLowerInvariant() $basicCreatedTemplateSpecV1.Location.Replace(" ", "").ToLowerInvariant()
+        Assert-AreEqual "v1" $basicCreatedTemplateSpecV1.Versions[0].Name
+        
+        # For the ARM template itself we'll do some normalization to ensure a valid comparison:
+        $normalizedSampleTemplateJson = $sampleTemplateJson | ConvertFrom-Json | ConvertTo-Json -Compress
+        $normalizedTemplateJsonInV1 = $basicCreatedTemplateSpecV1.Versions[0].Template.ToString() | ConvertFrom-Json | ConvertTo-Json -Compress
+        
+        Assert-AreEqual $normalizedSampleTemplateJson $normalizedTemplateJsonInV1
+
+        # Make sure we can get the template spec back out:
+        $returnedByGet = Get-AzTemplateSpec -ResourceGroupName $rgname -Name $rname -Version "v1"
+    }
+    finally
+    {
+        # Cleanup
+        Clean-ResourceGroup $rgname
+    }
+}
+
+<#
+.SYNOPSIS
+Tests that we can create/update a basic template spec with Set-AzTemplateSpec
+#>
+function Test-SetTemplateSpec
+{
+    # Setup
+    $rgname = Get-ResourceGroupName
+    $rname = Get-ResourceName
+    $rglocation = "West US 2"
+    $subId = (Get-AzContext).Subscription.SubscriptionId
+
+    try
+    {
+        # Prepare our RG and basic template spec:
+
+        New-AzResourceGroup -Name $rgname -Location $rglocation
+
+        $sampleTemplateJson = Get-Content -Raw -Path "sampleTemplate.json"
+        $basicCreatedTemplateSpecV1 = Set-AzTemplateSpec -ResourceGroupName $rgname -Name $rname -Description "My Basic Template Spec" -Location $rgLocation -Version "v1" -TemplateJson $sampleTemplateJson
+
+        # Check to make sure all of the properties match expectations
+
+        Assert-NotNull $basicCreatedTemplateSpecV1
+        Assert-AreEqual $basicCreatedTemplateSpecV1.Length 1
+        Assert-AreEqual $rname $basicCreatedTemplateSpecV1.Name
+        Assert-AreEqual "My Basic Template Spec" $basicCreatedTemplateSpecV1.Description
+        Assert-AreEqual "$rglocation".Replace(" ", "").ToLowerInvariant() $basicCreatedTemplateSpecV1.Location.Replace(" ", "").ToLowerInvariant()
+        Assert-AreEqual "v1" $basicCreatedTemplateSpecV1.Versions[0].Name
+
+        # For the ARM template itself we'll do some normalization to ensure a valid comparison:
+        $normalizedSampleTemplateJson = $sampleTemplateJson | ConvertFrom-Json | ConvertTo-Json -Compress
+        $normalizedTemplateJsonInV1 = $basicCreatedTemplateSpecV1.Versions[0].Template.ToString() | ConvertFrom-Json | ConvertTo-Json -Compress
+
+        Assert-AreEqual $normalizedSampleTemplateJson $normalizedTemplateJsonInV1
+
+        # Now let's try to update the root template spec by resource id:
+        $updatedTemplateSpec = Set-AzTemplateSpec -ResourceId $basicCreatedTemplateSpecV1.Id -Description "Updated Description"
+        Assert-AreEqual "Updated Description" $updatedTemplateSpec.Description
+
+        # Try updating the version
+        $updatedTemplateSpecv1 = Set-AzTemplateSpec -ResourceId $basicCreatedTemplateSpecV1.Id -Version "v1" -VersionDescription "Updated Version" -TemplateJson $sampleTemplateJson
+        Assert-AreEqual "Updated Version" $updatedTemplateSpecv1.Versions[0].Description
+
+        # Make sure we can get the template spec back out:
+        $returnedByGet = Get-AzTemplateSpec -ResourceGroupName $rgname -Name $rname -Version "v1"
+    }
+    finally
+    {
+        # Cleanup
+        Clean-ResourceGroup $rgname
+    }
+}
+
+<#
+.SYNOPSIS
+Tests 
+#>
+function Test-RemoveTemplateSpec
+{
+    # Setup
+    $rgname = Get-ResourceGroupName
+    $rname = Get-ResourceName
+    $rglocation = "West US 2"
+    $subId = (Get-AzContext).Subscription.SubscriptionId
+
+    try
+    {
+        # Prepare our RG and basic template spec (with two versions):
+
+        New-AzResourceGroup -Name $rgname -Location $rglocation
+
+        $sampleTemplateJson = Get-Content -Raw -Path "sampleTemplate.json"
+        $basicCreatedTemplateSpecV1 = New-AzTemplateSpec -ResourceGroupName $rgname -Name $rname -Location $rgLocation -Version "v1" -TemplateJson $sampleTemplateJson
+        $basicCreatedTemplateSpecV2 = New-AzTemplateSpec -ResourceGroupName $rgname -Name $rname -Location $rgLocation -Version "v2" -TemplateJson $sampleTemplateJson
+
+        # Validate we can remove a single version:
+
+        $singleVersionRemovalResult = Remove-AzTemplateSpec -ResourceGroupName $rgname -Name $rname -Version "v1" -Force
+        Assert-True { $singleVersionRemovalResult }
+        Assert-Throws { Get-AzTemplateSpec -ResourceGroupName $rgname -Name $rname -Version "v1" }
+
+        # Validate we can remove the entire template spec:
+
+        $allVersionsRemovalResult = Remove-AzTemplateSpec -ResourceId $basicCreatedTemplateSpecV1.Id -Force # Note: Id is Id of the root template spec
+        Assert-True { $allVersionsRemovalResult }
+        Assert-Throws { Get-AzTemplateSpec -ResourceGroupName $rgname -Name $rname }
+    }
+    finally
+    {
+        # Cleanup
+        Clean-ResourceGroup $rgname
+    }
+}
