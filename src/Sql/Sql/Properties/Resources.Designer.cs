34467c6ab94e51b08aa229f4f261b3475ed0f10a
lululilliancoding <63571937+lululilliancoding@users.noreply.github.com>
2020-10-14 21:03:34 -0700

    Edit Get & Set ShortTermRetentionPolicy For Adding New Parameter: DiffBackupIntervalInHours (#12562)
    * Draft ShortTermRetentionPolicy Get and Set cmdlets to include new parameter DiffBackupIntervalInHours PR (except record test session).
    
    * set RetentionDays nullable and remove parameters' position requirement.
    
    * scenario test passed.
    
    * Removed .Value to allowe either retentionDays or diffBackupInterval can be set individually.
    Added its related scenario tests and re-record; Added one related example;
    
    * Add Position back to avoid breaking change;
    Add retentionDays nullable changes exception;
    Re-upload records.
    
    * undid position of DiffBackupIntervalInHours
    
    Co-authored-by: Lillian Liu <liulu@microsoft.com>
    Co-authored-by: Yeming Liu <yeliu@microsoft.com>

--- src/Sql/Sql/Properties/Resources.Designer.cs
+++ src/Sql/Sql/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Microsoft.Azure.Commands.Sql.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -1317,6 +1317,15 @@ namespace Microsoft.Azure.Commands.Sql.Properties {
         internal static string SetAdvisorAutoExecuteStatusWarning {
             get {
                 return ResourceManager.GetString("SetAdvisorAutoExecuteStatusWarning", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Parameter &apos;{0}&apos; and &apos;{1}&apos; both being null is not allowed..
+        /// </summary>
+        internal static string SetAzSqlDatabaseBackupShortTermRetentionInvalidParameters {
+            get {
+                return ResourceManager.GetString("SetAzSqlDatabaseBackupShortTermRetentionInvalidParameters", resourceCulture);
             }
         }
         
