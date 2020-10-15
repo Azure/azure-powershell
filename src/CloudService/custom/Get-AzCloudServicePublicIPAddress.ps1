a67954ee8c62cffdf20dd6e01850f56def61e8b7
bberera <bnbiddappa@hotmail.com>
2020-10-15 16:20:07 +0530

    Networking custom cmdlets

---
+++ src/CloudService/custom/Get-AzCloudServicePublicIPAddress.ps1
@@ -0,0 +1,40 @@
+﻿function Get-AzCloudServicePublicIPAddress {
+    param(
+        [string] $SubscriptionId,
+
+        [Parameter(Mandatory=$true, ParameterSetName="CloudServiceName")]
+        [string] $ResourceGroupName,
+        
+        [Parameter(Mandatory=$true, ParameterSetName="CloudServiceName")]
+        [string] $CloudServiceName,
+
+        [Parameter(Mandatory=$true, ParameterSetName="CloudService")]
+        [Microsoft.Azure.PowerShell.Cmdlets.CloudService.Models.Api20201001Preview.CloudService] $CloudService,
+
+        [Parameter(DontShow)]
+        [string] $ApiVersion = "2020-06-01"
+
+    )
+    process {
+
+        if (-not $PSBoundParameters.ContainsKey("SubscriptionId")) 
+        {
+            $SubscriptionId = (Get-AzContext).Subscription.Id
+        }
+
+        if ($PSBoundParameters.ContainsKey("CloudService"))
+        {
+            $elements = $CloudService.Id.Split("/")
+            $SubscriptionId = $elements[2]
+            $ResourceGroupName = $elements[4]
+            $CloudServiceName = $CloudService.Name
+        }
+
+        # Create the URI as per the input
+        $uriToInvoke = "/subscriptions/" + $SubscriptionId + "/resourceGroups/" + $ResourceGroupName + "/providers/Microsoft.Compute/cloudServices/" + $CloudServiceName + "/publicIPAddresses?api-version=" + $ApiVersion
+
+        # Invoke and display the information
+        $result = Az.Accounts\Invoke-AzRestMethod -Method GET -Path $uriToInvoke
+        $result.Content
+    }
+}
--- src/CloudService/custom/Switch-AzCloudService.ps1
+++ src/CloudService/custom/Switch-AzCloudService.ps1
@@ -56,6 +56,7 @@ function Switch-AzCloudService {
     param(
     
         [Parameter(ParameterSetName='Swap')]
+        [Parameter(ParameterSetName='CloudServiceName')]
         [Microsoft.Azure.PowerShell.Cmdlets.CloudService.Category('Path')]
         [Microsoft.Azure.PowerShell.Cmdlets.CloudService.Runtime.DefaultInfo(Script='(Get-AzContext).Subscription.Id')]
         [System.String]
@@ -63,16 +64,21 @@ function Switch-AzCloudService {
         # The subscription ID forms part of the URI for every service call.
         ${SubscriptionId},
     
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory=$true, ParameterSetName="CloudService")]
         [Microsoft.Azure.PowerShell.Cmdlets.CloudService.Category('Path')]
         [Microsoft.Azure.PowerShell.Cmdlets.CloudService.Models.Api20201001Preview.CloudService]
-        ${SourceCloudService},
+        ${CloudService},
     
-        [Parameter(Mandatory)]
+        [Parameter(Mandatory=$true, ParameterSetName="CloudServiceName")]
         [Microsoft.Azure.PowerShell.Cmdlets.CloudService.Category('Path')]
-        [Microsoft.Azure.PowerShell.Cmdlets.CloudService.Models.Api20201001Preview.CloudService]
-        ${TargetCloudService},
-    
+        [System.String]
+        ${ResourceGroupName},
+        
+        [Parameter(Mandatory=$true, ParameterSetName="CloudServiceName")]
+        [Microsoft.Azure.PowerShell.Cmdlets.CloudService.Category('Path')]
+        [System.String] 
+        ${CloudServiceName},
+
         [Parameter()]
         [Alias('AzureRMContext', 'AzureCredential')]
         [ValidateNotNull()]
@@ -140,27 +146,157 @@ function Switch-AzCloudService {
     )
     
     process {
-        if ($SourceCloudService.Location -ne $TargetCloudService.Location) {
-            Throw "Two CloudService should be in the same location."
+        if (-not $PSBoundParameters.ContainsKey("SubscriptionId")) {
+            $SubscriptionId = (Get-AzContext).Subscription.Id
         }
-        $SourcePublicIPAddress = [Microsoft.Azure.PowerShell.Cmdlets.CloudService.Models.Api20200401.PublicIPAddress]::New()
-        $SourcePublicIPAddress.Id = $SourceCloudService.NetworkProfileLoadBalancerConfiguration.FrontendIPConfiguration.PublicIPAddressId
-        $TargetPublicIPAddress = [Microsoft.Azure.PowerShell.Cmdlets.CloudService.Models.Api20200401.PublicIPAddress]::New()
-        $TargetPublicIPAddress.Id = $TargetCloudService.NetworkProfileLoadBalancerConfiguration.FrontendIPConfiguration.PublicIPAddressId
-        if (SourcePublicIPAddress.Id -eq TargetPublicIPAddress.Id) {
-            return $TargetCloudService
+
+        # Fetch the services to ensure that we have the latest information
+        if ($PSBoundParameters.ContainsKey("CloudService"))
+        {
+            $CloudServiceName = $CloudService.Name     
         }
-        $SourceFrontendIPConfiguration = [Microsoft.Azure.PowerShell.Cmdlets.CloudService.Models.Api20200401.FrontendIPConfiguration]::New()
-        $SourceFrontendIPConfiguration.Id = $SourceCloudService.NetworkProfileLoadBalancerConfiguration[0].FrontendIPConfiguration.Id
-        $SourceFrontendIPConfiguration.PublicIPAddress = $TargetPublicIPAddress
-        $TargetFrontendIPConfiguration = [Microsoft.Azure.PowerShell.Cmdlets.CloudService.Models.Api20200401.FrontendIPConfiguration]::New()
-        $TargetFrontendIPConfiguration.Id = $TargetCloudService.NetworkProfileLoadBalancerConfiguration[0].FrontendIPConfiguration.Id
-        $TargetFrontendIPConfiguration.PublicIPAddress = $SourcePublicIPAddress
-        $Null = $PSBoundParameters.Remove("SourceCloudService")
-        $Null = $PSBoundParameters.Remove("TargetCloudService")
-        $PSBoundParameters.Add("Location", $SourceCloudService.Location)
-        $PSBoundParameters.Add("InputObject", $SourcePublicIPAddress)
-        $PSBoundParameters.Add("FrontendIPConfiguration", @($SourceFrontendIPConfiguration, $TargetFrontendIPConfiguration))
-        Az.CloudService.internal\Switch-AzCloudServiceLoadBalancerPublicIPAddress @PSBoundParameters
+
+        $SourceCloudService = Get-AzCloudService -SubscriptionId $SubscriptionId -Name $CloudServiceName -ResourceGroupName $ResourceGroupName
+
+        # Check that both have swappable property set.
+        if ([string]::IsNullOrEmpty($SourceCloudService.SwappableCloudServiceId))
+        {
+            throw "SwappableCloudServiceId is not set on the source cloud service " + $SourceCloudService.Name 
+        }
+
+        # Check that Public IPs counts are correct for source
+        $validSourceIP = ValidateCloudServicePublicIPAddress($SourceCloudService)
+        if ($validSourceIP -eq $false)
+        {
+            throw "Specified source cloud service must have a single public IP address specified in its FrontendIpConfigurations." 
+        }
+  
+        # Parse target cloud service fields
+        $elements = $SourceCloudService.SwappableCloudServiceId.Split("/")
+        $TargetSubscriptionId = $elements[2]
+        $TargetResourceGroupName = $elements[4]
+        $TargetCloudServiceName = $elements[8]
+ 
+        # Fetch the target cloud service 
+        $TargetCloudService = Get-AzCloudService -SubscriptionId $TargetSubscriptionId -Name $TargetCloudServiceName -ResourceGroupName $TargetResourceGroupName
+
+        # Check that Public IPs counts are correct for target
+        $validTargetIP = ValidateCloudServicePublicIPAddress($TargetCloudService)
+        if ($validTargetIP -eq $false)
+        {
+            throw "Specified target cloud service must have a single public IP address specified in its FrontendIpConfigurations." 
+        }
+
+        # Get the LB FrontEndIpConfigs to create the request body
+        $sourceLB = Get-AzLoadBalancer -ResourceGroupName $ResourceGroupName -Name $SourceCloudService.NetworkProfileLoadBalancerConfiguration.Name
+        $validSourceLB = ValidateLoadBalancerFrontEndIPConfiguration($sourceLB)
+        if ($validSourceLB -eq $false)
+        {
+            throw "Source loadbBalancer must have a single value in its FrontendIpConfigurations." 
+        }
+
+        $targetLB = Get-AzLoadBalancer -ResourceGroupName $TargetResourceGroupName -Name $TargetCloudService.NetworkProfileLoadBalancerConfiguration.Name        
+        $validTargetLB = ValidateLoadBalancerFrontEndIPConfiguration($targetLB)
+        if ($validTargetLB -eq $false)
+        {
+            throw "Target loadbBalancer must have a single value in its FrontendIpConfigurations." 
+        }
+
+        # Construct the request body
+        $requestBody = GetVIPSwapRequestBody
+        $requestBody = $requestBody -replace "#LBFE1#", $sourceLB.FrontendIpConfigurations[0].Id
+        $requestBody = $requestBody -replace "#PIP2#", $TargetCloudService.NetworkProfileLoadBalancerConfiguration[0].FrontendIPConfiguration[0].PublicIPAddressId
+        $requestBody = $requestBody -replace "#LBFE2#", $targetLB.FrontendIpConfigurations[0].Id
+        $requestBody = $requestBody -replace "#PIP1#", $SourceCloudService.NetworkProfileLoadBalancerConfiguration[0].FrontendIPConfiguration[0].PublicIPAddressId
+
+        # Set up API URI and Headers
+        $uriToInvoke = "/subscriptions/" + $SubscriptionId + "/providers/Microsoft.Network/locations/" + $SourceCloudService.Location + "/setLoadBalancerFrontendPublicIpAddresses?api-version=" + $ApiVersion
+
+        # Display the information about the VIP swap being made
+        Write-Host "Performing switch cloud service action between" $SourceCloudService.Name "and" $TargetCloudService.Name
+        Write-Host "Request URI :"
+        Write-Host "POST " $uriToInvoke
+        Write-Host "Request Body :"
+        Write-Host $requestBody
+
+        # Invoke the VIP swap API
+        if ($PSCmdlet.ShouldProcess($SourceCloudService.Name,'Switch Cloud Service')) 
+        {
+            $SourcePublicIPAddress = [Microsoft.Azure.PowerShell.Cmdlets.CloudService.Models.Api20200401.PublicIPAddress]::New()
+            $SourcePublicIPAddress.Id = $SourceCloudService.NetworkProfileLoadBalancerConfiguration[0].FrontendIPConfiguration[0].PublicIPAddressId
+            $TargetPublicIPAddress = [Microsoft.Azure.PowerShell.Cmdlets.CloudService.Models.Api20200401.PublicIPAddress]::New()
+            $TargetPublicIPAddress.Id = $TargetCloudService.NetworkProfileLoadBalancerConfiguration[0].FrontendIPConfiguration[0].PublicIPAddressId
+        
+            $SourceFrontendIPConfiguration = [Microsoft.Azure.PowerShell.Cmdlets.CloudService.Models.Api20200401.FrontendIPConfiguration]::New()
+            $SourceFrontendIPConfiguration.Id = $sourceLB.FrontendIpConfigurations[0].Id
+            $SourceFrontendIPConfiguration.PublicIPAddress = $TargetPublicIPAddress
+            $TargetFrontendIPConfiguration = [Microsoft.Azure.PowerShell.Cmdlets.CloudService.Models.Api20200401.FrontendIPConfiguration]::New()
+            $TargetFrontendIPConfiguration.Id = $targetLB.FrontendIpConfigurations[0].Id
+            $TargetFrontendIPConfiguration.PublicIPAddress = $SourcePublicIPAddress
+
+            $Null = $PSBoundParameters.Remove("CloudService")
+            $Null = $PSBoundParameters.Remove("CloudServiceName")
+            $Null = $PSBoundParameters.Remove("ResourceGroupName")
+
+            $PSBoundParameters.Add("Location", $SourceCloudService.Location)
+            $PSBoundParameters.Add("FrontendIPConfiguration", @($SourceFrontendIPConfiguration, $TargetFrontendIPConfiguration))
+            Az.CloudService.internal\Switch-AzCloudServiceLoadBalancerPublicIPAddress @PSBoundParameters
+        }
     }
 }
+
+function ValidateLoadBalancerFrontEndIPConfiguration($lb)
+{
+    if ($lb.FrontendIpConfigurations.Count -eq 1)
+    {   
+        if (-not [string]::IsNullOrEmpty($lb.FrontendIpConfigurations[0].Id))
+        {
+            return $true;
+        }
+    }
+
+    return $false;
+}
+
+function ValidateCloudServicePublicIPAddress($cs)
+{
+    if ($cs.NetworkProfileLoadBalancerConfiguration.Count -eq 1)
+    {
+        if ($cs.NetworkProfileLoadBalancerConfiguration[0].FrontendIPConfiguration.Count -eq 1)
+        {
+            if (-not [string]::IsNullOrEmpty($cs.NetworkProfileLoadBalancerConfiguration[0].FrontendIPConfiguration[0].PublicIPAddressId))
+            {
+                return $true;
+            }
+        }
+    }
+
+    return $false;
+}
+
+function GetVIPSwapRequestBody()
+{
+    return @"
+{
+	"frontendIPConfigurations": [
+		{
+			"id": "#LBFE1#",
+			"properties": {
+				"publicIPAddress": {
+					"id": "#PIP2#"
+				}
+			}
+		},
+		{
+			"id": "#LBFE2#",
+			"properties": {
+				"publicIPAddress": {
+					"id": "#PIP1#"
+				}
+			}
+		}
+	]
+}
+"@
+} 
+
