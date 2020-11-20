1b6c52dd18c5c8d5f6f5d0a88d454bcfde35c1e3
dicolanl <ndicola@microsoft.com>
2020-11-20 03:57:04 +0000

    test v2

--- src/SecurityInsights/SecurityInsights.Test/ScenarioTests/TestController.cs
+++ src/SecurityInsights/SecurityInsights.Test/ScenarioTests/TestController.cs
@@ -12,25 +12,36 @@
 // limitations under the License.
 // ----------------------------------------------------------------------------------
 
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using Microsoft.Azure.Commands.Common.Authentication;
-using Microsoft.Azure.Management.Internal.Resources;
 using Microsoft.Azure.Management.SecurityInsights;
 using Microsoft.Azure.Management.Storage.Version2017_10_01;
 using Microsoft.Azure.Test.HttpRecorder;
 using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
 using Microsoft.WindowsAzure.Commands.ScenarioTest;
 using Microsoft.WindowsAzure.Commands.Test.Utilities.Common;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
+using TestEnvironmentFactory = Microsoft.Rest.ClientRuntime.Azure.TestFramework.TestEnvironmentFactory;
+using Microsoft.Azure.Management.Internal.Resources;
+using Microsoft.Azure.Commands.TestFx;
 
 namespace Microsoft.Azure.Commands.SecurityInsights.Test.ScenarioTests
 {
+    /// <summary>
+    /// Setup for Scenario Tests
+    /// </summary>
     public class TestController : RMTestBase
     {
         private readonly EnvironmentSetupHelper _helper;
+
+        public ResourceManagementClient ResourceManagementClient { get; private set; }
+
+        public SecurityInsightsClient SecurityInsightsClient { get; private set; }
+
+        public StorageManagementClient StorageManagementClient { get; private set; }
 
         public static TestController NewInstance => new TestController();
 
@@ -39,6 +50,23 @@ namespace Microsoft.Azure.Commands.SecurityInsights.Test.ScenarioTests
             _helper = new EnvironmentSetupHelper();
         }
 
+        protected void SetupManagementClients(MockContext context)
+        {
+            ResourceManagementClient = context.GetServiceClient<ResourceManagementClient>(TestEnvironmentFactory.GetTestEnvironment());
+            SecurityInsightsClient = context.GetServiceClient<SecurityInsightsClient>(TestEnvironmentFactory.GetTestEnvironment());
+            StorageManagementClient = context.GetServiceClient<StorageManagementClient>(TestEnvironmentFactory.GetTestEnvironment());
+
+            _helper.SetupManagementClients(
+                ResourceManagementClient,
+                StorageManagementClient,
+                SecurityInsightsClient);
+        }
+
+        /// <summary>
+        /// Methods for invoking PowerShell scripts
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="scripts"></param>
         public void RunPowerShellTest(ServiceManagement.Common.Models.XunitTracingInterceptor logger, params string[] scripts)
         {
             var sf = new StackTrace().GetFrame(1);
@@ -46,10 +74,34 @@ namespace Microsoft.Azure.Commands.SecurityInsights.Test.ScenarioTests
             var mockName = sf.GetMethod().Name;
 
             _helper.TracingInterceptor = logger;
+            RunPsTestWorkflow(
+                () => scripts,
+                // no custom cleanup
+                null,
+                callingClassType,
+                mockName);
+        }
 
-            var providers = new Dictionary<string, string>();
-            var providersToIgnore = new Dictionary<string, string>();
-            HttpMockServer.Matcher = new PermissiveRecordMatcherWithApiExclusion(true, providers, providersToIgnore);
+        private void RunPsTestWorkflow(
+            Func<string[]> scriptBuilder,
+            Action cleanup,
+            string callingClassType,
+            string mockName)
+        {
+
+            var d = new Dictionary<string, string>
+            {
+                {"Microsoft.Resources", null},
+                {"Microsoft.Features", null},
+                {"Microsoft.Authorization", null},
+                {"Microsoft.Compute", null}
+            };
+            var providersToIgnore = new Dictionary<string, string>
+            {
+                {"Microsoft.Azure.Management.Resources.ResourceManagementClient", "2016-02-01"}
+            };
+            HttpMockServer.Matcher = new PermissiveRecordMatcherWithApiExclusion(true, d, providersToIgnore);
+
             HttpMockServer.RecordsDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "SessionRecords");
 
             using (var context = MockContext.Start(callingClassType, mockName))
@@ -58,35 +110,28 @@ namespace Microsoft.Azure.Commands.SecurityInsights.Test.ScenarioTests
 
                 _helper.SetupEnvironment(AzureModule.AzureResourceManager);
 
-                var callingClassName = callingClassType?.Split(new[] { "." }, StringSplitOptions.RemoveEmptyEntries).Last();
-                _helper.SetupModules(
-                    AzureModule.AzureResourceManager,
-                    _helper.RMProfileModule,
-                    _helper.GetRMModulePath(@"AzureRM.LogicApp.psd1"),
-                    _helper.GetRMModulePath(@"AzureRM.Resources.psd1"),
-                    _helper.GetRMModulePath(@"AzureRM.SecurityInsights.psd1"),
+                var callingClassName = callingClassType.Split(new[] { "." }, StringSplitOptions.RemoveEmptyEntries).Last();
+                _helper.SetupModules(AzureModule.AzureResourceManager,
                     "ScenarioTests\\Common.ps1",
                     "ScenarioTests\\" + callingClassName + ".ps1",
-                    "AzureRM.Resources.ps1");
-
-                _helper.RunPowerShellTest(scripts);
+                    _helper.RMProfileModule,
+                    _helper.GetRMModulePath("AzureRM.SecurityInsights.psd1"),
+                    _helper.GetRMModulePath("AzureRM.LogicApp.psd1"),
+                    "AzureRM.Resources.ps1",
+                    "AzureRM.Storage.ps1");
+                try
+                {
+                    var psScripts = scriptBuilder?.Invoke();
+                    if (psScripts != null)
+                    {
+                        _helper.RunPowerShellTest(psScripts);
+                    }
+                }
+                finally
+                {
+                    cleanup?.Invoke();
+                }
             }
-        }
-
-        protected void SetupManagementClients(MockContext context)
-        {
-            var resourcesClient = GetResourcesClient(context);
-            var securityInsightsClient = GetSecurityInsightsClient(context);
-            _helper.SetupManagementClients(securityInsightsClient, resourcesClient);
-        }
-
-        private static SecurityInsightsClient GetSecurityInsightsClient(MockContext context)
-        {
-            return context.GetServiceClient<SecurityInsightsClient>(TestEnvironmentFactory.GetTestEnvironment());
-        }
-        private static ResourceManagementClient GetResourcesClient(MockContext context)
-        {
-            return context.GetServiceClient<ResourceManagementClient>(TestEnvironmentFactory.GetTestEnvironment());
         }
     }
 }
