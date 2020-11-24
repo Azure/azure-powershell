90a07d9c3e14248899dbcae587f37e3028b63ad6
Daeun Yim <daeunyim@microsoft.com>
2020-11-23 21:01:54 -0800

    cred scan issue resolved

--- src/MySql/test/Remove-AzMySqlFlexibleServer.Tests.ps1
+++ src/MySql/test/Remove-AzMySqlFlexibleServer.Tests.ps1
@@ -14,6 +14,7 @@ while(-not $mockingPath) {
 Describe 'Remove-AzMySqlFlexibleServer' {
     It 'Delete' {
         {
+            #[SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine")]
             $password = 'Pasword01!!2020' | ConvertTo-SecureString -AsPlainText -Force
             New-AzMySqlFlexibleServer -ResourceGroupName $env.resourceGroup -Name $env.serverName -AdministratorUserName mysql_test -AdministratorLoginPassword $password 
             Remove-AzMySqlFlexibleServer -ResourceGroupName $env.resourceGroup -Name $env.serverName2
