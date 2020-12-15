c175046a096fb472e23b6613f4310622dd7cf686
Zeshi Luo <zesluo@microsoft.com>
2020-12-15 12:00:17 +0800

    add Getazuresynapsesqldeleteddatabasebackup

---
+++ src/Synapse/Synapse/Commands/ManagementCommands/SqlPool Backup/GetAzureSynapseSqlDeletedDatabaseBackup.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
+using Microsoft.Azure.Commands.Synapse.Common;
+using Microsoft.Azure.Commands.Synapse.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+
+namespace Microsoft.Azure.Commands.Synapse
+{
+    [Cmdlet(VerbsCommon.Get, ResourceManager.Common.AzureRMConstants.AzureRMPrefix + SynapseConstants.SynapsePrefix + SynapseConstants.SqlDeletedDatabaseBackup,
+        DefaultParameterSetName = GetByNameParameterSet)]
+    [OutputType(typeof(PSDeletedDatabaseBackupModel))]
+    public class GetAzureSynapseSqlDeletedDatabaseBackup : SynapseManagementCmdletBase
+    {
+        private const string GetByNameParameterSet = "GetByNameParameterSet";
+
+        [Parameter(Mandatory = true, ParameterSetName = GetByNameParameterSet, HelpMessage = HelpMessages.ResourceGroupName)]
+        [ResourceGroupCompleter]
+        [ValidateNotNullOrEmpty]
+        public string ResourceGroupName { get; set; }
+
+        [Parameter(Mandatory = true, ParameterSetName = GetByNameParameterSet, HelpMessage = HelpMessages.WorkspaceName)]
+        [ResourceNameCompleter(ResourceTypes.Workspace, nameof(ResourceGroupName))]
+        [ValidateNotNullOrEmpty]
+        public string WorkspaceName { get; set; }
+
+        [Parameter(Mandatory = false, ParameterSetName = GetByNameParameterSet, HelpMessage = HelpMessages.SqlPoolName)]
+        [ResourceNameCompleter(
+            ResourceTypes.SqlDeletedDatabaseBackup,
+            nameof(ResourceGroupName),
+            nameof(WorkspaceName))]
+        [ValidateNotNullOrEmpty]
+        public string Name { get; set; }
+
+        [Parameter(Mandatory = false,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The deletion date of the Azure Synaspe SQL Database to retrieve backups for, with millisecond precision (e.g. 2016-02-23T00:21:22.847Z)")]
+        [ValidateNotNullOrEmpty]
+        public DateTime? DeletionDate { get; set; }
+
+        public override void ExecuteCmdlet()
+        {
+            ICollection<PSDeletedDatabaseBackupModel> results;
+
+            if (MyInvocation.BoundParameters.ContainsKey("Name"))
+            {
+                if (MyInvocation.BoundParameters.ContainsKey("DeletionDate"))
+                {
+                    results = new List<PSDeletedDatabaseBackupModel>();
+                    // The Sql pool expects a deleted database ID that consists of the database name and deletion time as a windows file time separated by a comma.
+                    results.Add(this.SynapseAnalyticsClient.GetDeletedDatabaseBackup(this.ResourceGroupName, this.WorkspaceName, this.Name + "," + this.DeletionDate.Value.ToFileTimeUtc().ToString()).ConfigureAwait(true).GetAwaiter().GetResult());
+                }
+                else
+                {
+                    results = this.SynapseAnalyticsClient.ListDeletedDatabaseBackups(this.ResourceGroupName, this.WorkspaceName).ConfigureAwait(true).GetAwaiter().GetResult().Where(backup => backup.DatabaseName == Name).ToList();
+                }
+            }
+            else
+            {
+                results = this.SynapseAnalyticsClient.ListDeletedDatabaseBackups(this.ResourceGroupName, this.WorkspaceName).ConfigureAwait(true).GetAwaiter().GetResult();
+            }
+
+            WriteObject(results, true);
+        }
+    }
+}
---
+++ src/Synapse/Synapse/Commands/ManagementCommands/SqlPool Backup/GetAzureSynapseSqlPoolGeoBackup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
+using Microsoft.Azure.Commands.Synapse.Common;
+using Microsoft.Azure.Commands.Synapse.Models;
+using System.Management.Automation;
+
+namespace Microsoft.Azure.Commands.Synapse
+{
+    [Cmdlet(VerbsCommon.Get, ResourceManager.Common.AzureRMConstants.AzureRMPrefix + SynapseConstants.SynapsePrefix + SynapseConstants.SqlPoolGeoBackup,
+        DefaultParameterSetName = GetByNameParameterSet)]
+    [OutputType(typeof(PSBackupModel))]
+    public class GetAzureSynapseSqlPoolGeoBackup : SynapseManagementCmdletBase
+    {
+        private const string GetByNameParameterSet = "GetByNameParameterSet";
+
+        [Parameter(Mandatory = true, ParameterSetName = GetByNameParameterSet, HelpMessage = HelpMessages.ResourceGroupName)]
+        [ResourceGroupCompleter]
+        [ValidateNotNullOrEmpty]
+        public string ResourceGroupName { get; set; }
+
+        [Parameter(Mandatory = true, ParameterSetName = GetByNameParameterSet, HelpMessage = HelpMessages.WorkspaceName)]
+        [ResourceNameCompleter(ResourceTypes.Workspace, nameof(ResourceGroupName))]
+        [ValidateNotNullOrEmpty]
+        public string WorkspaceName { get; set; }
+
+        [Parameter(Mandatory = false, ParameterSetName = GetByNameParameterSet, HelpMessage = HelpMessages.SqlPoolName)]
+        [ResourceNameCompleter(
+            ResourceTypes.SqlDataBaseGeoBackup,
+            nameof(ResourceGroupName),
+            nameof(WorkspaceName))]
+        [ValidateNotNullOrEmpty]
+        public string Name { get; set; }
+
+        public override async void ExecuteCmdlet()
+        {
+            if (MyInvocation.BoundParameters.ContainsKey("Name") && !WildcardPattern.ContainsWildcardCharacters(Name))
+            {
+                var results = await this.SynapseAnalyticsClient.GetRecoverableManagedDatabase(ResourceGroupName, WorkspaceName, Name);
+                WriteObject(results);
+            }
+            else
+            {
+                var results = this.SynapseAnalyticsClient.ListRecoverableManagedDatabases(this.ResourceGroupName, this.WorkspaceName);
+                WriteObject(results, true);
+            }
+        }
+    }
+}
