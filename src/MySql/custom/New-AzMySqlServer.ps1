8963bdedcefc25706c86bc695aac9e71657dd8fa
LucasYao93 <53558334+LucasYao93@users.noreply.github.com>
2020-12-28 11:19:06 +0800

    [MySql] Write code for assgin value to the paramter MinimalTlsVersion.

--- src/MySql/custom/New-AzMySqlServer.ps1
+++ src/MySql/custom/New-AzMySqlServer.ps1
@@ -184,6 +184,11 @@ function New-AzMySqlServer {
               $Parameter.SslEnforcement = [Microsoft.Azure.PowerShell.Cmdlets.MySql.Support.SslEnforcementEnum]::Enable
           }
 
+          if ($PSBoundParameters.ContainsKey('MinimalTlsVersion')) {
+            $Parameter.MinimalTlsVersion = $PSBoundParameters['MinimalTlsVersion']
+            $null = $PSBoundParameters.Remove('MinimalTlsVersion')
+          }
+
           if ($PSBoundParameters.ContainsKey('BackupRetentionDay')) {
               $Parameter.StorageProfileBackupRetentionDay = $PSBoundParameters['BackupRetentionDay']
               $null = $PSBoundParameters.Remove('BackupRetentionDay')
