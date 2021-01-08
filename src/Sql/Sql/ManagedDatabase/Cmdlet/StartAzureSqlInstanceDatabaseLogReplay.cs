19f72d9b1e0bf35fe1a01aed2ccf13b0d61510ec
Milan Brkic <mibrkic@microsoft.com>
2021-01-08 13:38:16 +0100

    Add AsJob Switch parameter so user can execute as background process as this is a long-running op

--- src/Sql/Sql/ManagedDatabase/Cmdlet/StartAzureSqlInstanceDatabaseLogReplay.cs
+++ src/Sql/Sql/ManagedDatabase/Cmdlet/StartAzureSqlInstanceDatabaseLogReplay.cs
@@ -70,6 +70,12 @@ namespace Microsoft.Azure.Commands.Sql.ManagedDatabase.Cmdlet
         public string Collation { get; set; }
 
         /// <summary>
+        /// Gets or sets whether or not to run this cmdlet in the background as a job
+        /// </summary>
+        [Parameter(Mandatory = false, HelpMessage = "Run cmdlet in the background")]
+        public SwitchParameter AsJob { get; set; }
+
+        /// <summary>
         /// Get the entities from the service
         /// </summary>
         /// <returns>The list of entities</returns>
