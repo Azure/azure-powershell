c088ef65b3697dccd597de047327bb750bd09f2b
Sambit Rath <es15btech11015@iith.ac.in>
2021-01-09 13:45:23 +0530

    Initial commit (#13909)

---
+++ src/DataProtection/custom/Enums/Enums.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Support
+{
+    public enum StorageSettingType
+    {
+        GeoRedundant = 0,
+        LocallyRedundant
+    }
+
+    public enum DataStoreType
+    {
+        ArchiveStore = 0,
+        SnapshotStore,
+        VaultStore
+    }
+
+}
