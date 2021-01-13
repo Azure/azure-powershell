db93fd3bbd8ae7f56a3d31130fec38f7970ca315
Sambit Rath <es15btech11015@iith.ac.in>
2021-01-13 13:58:21 +0530

    Added Policy Cmdlets (#13948)

---
+++ src/DataProtection/custom/Cmdlets/Policy/New-AzDataProtectionPolicyTriggerSchedule.ps1
@@ -0,0 +1,33 @@
+﻿
+function New-AzDataProtectionPolicyTriggerSchedule{
+	[OutputType('System.String[]')]
+    [CmdletBinding(PositionalBinding=$false, SupportsShouldProcess)]
+    [Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Description('Creates new Schedule object')]
+
+    param (
+        [Parameter(Mandatory, HelpMessage='Source Datastore')]
+        [System.DateTime[]]
+        ${ScheduleDays},
+
+        [Parameter(Mandatory, HelpMessage='Source Datastore')]
+        [Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Support.BackupFrequency]
+        ${Frequency}
+    )
+
+    process {
+        $timezone = Get-TimeZone
+        $offset = $timezone.BaseUtcOffset.ToString()
+        $offset = $offset.Substring(0, 5)
+
+        $repeatingTimeIntervals = @()
+
+        foreach($day in $ScheduleDays){
+            $format = $day.ToString("yyyy-MM-ddTHH:mm:ss")
+            $backupFrequency = GetBackupFrequencyString -frequency $Frequency
+            $timeInterval = "R/" + $format + "+" + $offset + "/" + $backupFrequency
+            $repeatingTimeIntervals += $timeInterval
+        }
+        
+        return $repeatingTimeIntervals
+    }
+}
--- src/DataProtection/custom/Enums/Enums.cs
+++ src/DataProtection/custom/Enums/Enums.cs
@@ -15,4 +15,68 @@ namespace Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Support
         VaultStore
     }
 
+    public enum DurationType
+    {
+        Days = 0,
+        Weeks,
+        Months,
+        Years
+    }
+
+    public enum CopyOption
+    {
+        CustomCopyOption = 0,
+        ImmediateCopyOption
+    }
+
+    public enum BackupFrequency
+    {
+        Daily = 0,
+        Weekly
+    }
+
+    public enum AbsoluteTagCriteria
+    {
+        AllBackup = 0,
+        FirstOfDay,
+        FirstOfMonth,
+        FirstOfWeek,
+        FirstOfYear
+    }
+    
+    public enum WeeksOfMonth
+    {
+        First = 0,
+        Second,
+        Third,
+        Fourth,
+        Last
+    }
+
+    public enum MonthsOfYear
+    {
+        January = 0,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+
+    public enum DaysOfWeek
+    {
+        Sunday = 0,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
 }
