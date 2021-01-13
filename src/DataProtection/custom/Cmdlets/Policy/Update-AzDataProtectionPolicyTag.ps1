0ea7fd0a9c8c8d94bfa59946cd777f04157d6688
Sambit Rath <es15btech11015@iith.ac.in>
2021-01-13 13:58:21 +0530

    Added Policy Cmdlets (#13948)

---
+++ src/DataProtection/custom/Cmdlets/Policy/Update-AzDataProtectionPolicyTag.ps1
@@ -0,0 +1,77 @@
+﻿function Update-AzDataProtectionPolicyTag{
+	[OutputType('Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Models.Api202001Alpha.IBackupPolicy')]
+    [CmdletBinding(PositionalBinding=$false, SupportsShouldProcess)]
+    [Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Description('Prepares Datasource object for backup')]
+
+    param(
+        [Parameter(ParameterSetName='updateTag', Mandatory, HelpMessage='Datasource Type')]
+        [Parameter(ParameterSetName='RemoveTag', Mandatory, HelpMessage='Datasource Type')]
+        [Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Models.Api202001Alpha.IBackupPolicy]
+        ${Policy},
+
+        [Parameter(ParameterSetName='updateTag', Mandatory, HelpMessage='Datasource Type')]
+        [Parameter(ParameterSetName='RemoveTag', Mandatory, HelpMessage='Datasource Type')]
+        [System.String]
+        ${Name},
+
+        [Parameter(ParameterSetName='RemoveTag', Mandatory, HelpMessage='Datasource Type')]
+        [System.Management.Automation.SwitchParameter]
+        ${RemoveRule},
+
+        [Parameter(ParameterSetName='updateTag', Mandatory, HelpMessage='Datasource Type')]
+        [Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Models.Api202001Alpha.IScheduleBasedBackupCriteria[]]
+        ${Criteria}
+    )
+
+    process{
+        $parameterSetName = $PsCmdlet.ParameterSetName
+        $backupRuleIndex = -1
+        foreach($index in (0..$Policy.PolicyRule.Length))
+        {
+            if($Policy.PolicyRule[$index].ObjectType -eq "AzureBackupRule")
+            {
+                $backupRuleIndex = $index
+            }
+        }
+
+        if($backupRuleIndex -ne -1)
+        {
+            if($parameterSetName -eq "RemoveTag")
+            {
+                $filteredTags = $Policy.PolicyRule[$backupRuleIndex].Trigger.TaggingCriterion | Where-Object { $_.TagInfoTagName –ne $Name }
+                $Policy.PolicyRule[$backupRuleIndex].Trigger.TaggingCriterion = $filteredTags
+                return $Policy
+            }
+
+            if($parameterSetName -eq "updateTag")
+            {
+                $tagIndex = -1
+                foreach($index in (0..$Policy.PolicyRule[$backupRuleIndex].Trigger.TaggingCriterion.Length))
+                {
+                    if($Policy.PolicyRule[$backupRuleIndex].Trigger.TaggingCriterion[$index].TagInfoTagName -eq $Name)
+                    {
+                        $tagIndex = $index
+                    }
+                }
+
+                if($tagIndex -ne -1)
+                {
+                    $Policy.PolicyRule[$backupRuleIndex].Trigger.TaggingCriterion[$index].Criterion = $Criteria
+                    return $Policy
+                }
+                
+                if($tagIndex -eq -1)
+                {
+                    $tagCriteria = [Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Models.Api202001Alpha.TaggingCriteria]::new()
+                    $tagCriteria.TaggingPriority = GetTaggingPriority -Name $Name
+                    $tagCriteria.Criterion = $Criteria
+                    $tagCriteria.TagInfoTagName = $Name
+
+                    $Policy.PolicyRule[$backupRuleIndex].Trigger.TaggingCriterion += $tagCriteria
+                    return $Policy
+                }
+            }
+        }
+        
+    }
+}
---
+++ src/DataProtection/exports/New-AzDataProtectionPolicyTagCriteria.ps1
@@ -0,0 +1,114 @@
+
+# ----------------------------------------------------------------------------------
+#
+# Copyright Microsoft Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------------
+
+<#
+.Synopsis
+Prepares Datasource object for backup
+.Description
+Prepares Datasource object for backup
+.Example
+PS C:\> {{ Add code here }}
+
+{{ Add output here }}
+.Example
+PS C:\> {{ Add code here }}
+
+{{ Add output here }}
+
+.Outputs
+Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Models.Api202001Alpha.IScheduleBasedBackupCriteria
+.Link
+https://docs.microsoft.com/en-us/powershell/module/az.dataprotection/new-azdataprotectionpolicytagcriteria
+#>
+function New-AzDataProtectionPolicyTagCriteria {
+[OutputType([Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Models.Api202001Alpha.IScheduleBasedBackupCriteria])]
+[CmdletBinding(DefaultParameterSetName='ScheduleCriteria', PositionalBinding=$false, SupportsShouldProcess, ConfirmImpact='Medium')]
+param(
+    [Parameter(ParameterSetName='AbsoluteCriteria', Mandatory)]
+    [Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Category('Body')]
+    [Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Support.AbsoluteTagCriteria]
+    # Datasource Type
+    ${AbsoluteCriteria},
+
+    [Parameter(ParameterSetName='ScheduleCriteria')]
+    [Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Category('Body')]
+    [Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Support.DaysOfWeek[]]
+    # Datasource Type
+    ${DaysOfWeek},
+
+    [Parameter(ParameterSetName='ScheduleCriteria')]
+    [Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Category('Body')]
+    [Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Support.WeeksOfMonth[]]
+    # Datasource Type
+    ${WeeksOfMonth},
+
+    [Parameter(ParameterSetName='ScheduleCriteria')]
+    [Parameter(ParameterSetName='MonthlyCriteria')]
+    [Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Category('Body')]
+    [Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Support.MonthsOfYear[]]
+    # Datasource Type
+    ${MonthsOfYear},
+
+    [Parameter(ParameterSetName='ScheduleCriteria')]
+    [Parameter(ParameterSetName='MonthlyCriteria')]
+    [Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Category('Body')]
+    [System.DateTime[]]
+    # Datasource Type
+    ${ScheduleTimes},
+
+    [Parameter(ParameterSetName='MonthlyCriteria', Mandatory)]
+    [Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Category('Body')]
+    [System.String[]]
+    # Datasource Type
+    ${DaysOfMonth}
+)
+
+begin {
+    try {
+        $outBuffer = $null
+        if ($PSBoundParameters.TryGetValue('OutBuffer', [ref]$outBuffer)) {
+            $PSBoundParameters['OutBuffer'] = 1
+        }
+        $parameterSet = $PSCmdlet.ParameterSetName
+        $mapping = @{
+            AbsoluteCriteria = 'Az.DataProtection.custom\New-AzDataProtectionPolicyTagCriteria';
+            ScheduleCriteria = 'Az.DataProtection.custom\New-AzDataProtectionPolicyTagCriteria';
+            MonthlyCriteria = 'Az.DataProtection.custom\New-AzDataProtectionPolicyTagCriteria';
+        }
+        $wrappedCmd = $ExecutionContext.InvokeCommand.GetCommand(($mapping[$parameterSet]), [System.Management.Automation.CommandTypes]::Cmdlet)
+        $scriptCmd = {& $wrappedCmd @PSBoundParameters}
+        $steppablePipeline = $scriptCmd.GetSteppablePipeline($MyInvocation.CommandOrigin)
+        $steppablePipeline.Begin($PSCmdlet)
+    } catch {
+        throw
+    }
+}
+
+process {
+    try {
+        $steppablePipeline.Process($_)
+    } catch {
+        throw
+    }
+}
+
+end {
+    try {
+        $steppablePipeline.End()
+    } catch {
+        throw
+    }
+}
+}
