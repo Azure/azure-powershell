9b87172998be353c1b49da3035d26e5efd9f8856
Wei Wei <weiwei@microsoft.com>
2021-01-14 17:48:23 +0800

    fix a test failure in Synapse, to avoid storage API version check

--- src/Synapse/Synapse.Test/ScenarioTests/SynapseTestBase.cs
+++ src/Synapse/Synapse.Test/ScenarioTests/SynapseTestBase.cs
@@ -90,7 +90,8 @@ namespace Microsoft.Azure.Commands.Synapse.Test.ScenarioTests
             {
                 {"Microsoft.Resources", null},
                 {"Microsoft.Features", null},
-                {"Microsoft.Authorization", null}
+                {"Microsoft.Authorization", null},
+                {"Microsoft.Storage", null }
             };
             var providersToIgnore = new Dictionary<string, string>
             {
