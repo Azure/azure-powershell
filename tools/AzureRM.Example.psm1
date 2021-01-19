a8ac7933516861b14233bd6681c43e49ae2ea726
Youri <v-lipyo@microsoft.com>
2021-01-19 15:39:51 +0800

    Revert "Create Commit for release 6.13.1"
    This reverts commit 7c4dac501a99dd24168aa17285f22de7f1405641.

--- tools/AzureRM.Example.psm1
+++ tools/AzureRM.Example.psm1
@@ -13,7 +13,7 @@ function Test-DotNet
 {
     try
     {
-        if ((Get-PSDrive 'HKLM' -ErrorAction Ignore) -and (-not (Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\' -ErrorAction Stop | Get-ItemPropertyValue -ErrorAction Stop -Name Release | Where { $_ -ge 461808 })))
+        if (Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\' -ErrorAction Stop | Get-ItemPropertyValue -Name Release | Foreach-Object { $_ -lt 461808 })
         {
             throw ".NET Framework versions lower than 4.7.2 are not supported in Az.  Please upgrade to .NET Framework 4.7.2 or higher."
         }
@@ -24,7 +24,7 @@ function Test-DotNet
     }
 }
 
-if (%ISAZMODULE% -and ($PSEdition -eq 'Desktop'))
+if (%ISAZMODULE%)
 {
     if ($PSVersionTable.PSVersion -lt [Version]'5.1')
     {
@@ -34,25 +34,25 @@ if (%ISAZMODULE% -and ($PSEdition -eq 'Desktop'))
     Test-DotNet
 }
 
-if (Test-Path -Path "$PSScriptRoot\StartupScripts" -ErrorAction Ignore)
+if (Test-Path -Path "$PSScriptRoot\StartupScripts")
 {
     Get-ChildItem "$PSScriptRoot\StartupScripts" -ErrorAction Stop | ForEach-Object {
         . $_.FullName
     }
 }
 
-if (Get-Module %AZORAZURERM%.profile -ErrorAction Ignore)
+if (Get-Module %AZORAZURERM%.profile)
 {
     Write-Warning "%AZORAZURERM%.Profile already loaded. Az and AzureRM module cannot be run side-by-side, please run 'Uninstall-AzureRm' to remove all AzureRm modules from your machine. More information can be found here: https://aka.ms/azps-migration-guide"
     throw "%AZORAZURERM%.Profile already loaded. Az and AzureRM module cannot be run side-by-side, please run 'Uninstall-AzureRm' to remove all AzureRm modules from your machine. More information can be found here: https://aka.ms/azps-migration-guide"
 }
 
 $preloadPath = (Join-Path $PSScriptRoot -ChildPath "PreloadAssemblies")
-if($PSEdition -eq 'Desktop' -and (Test-Path $preloadPath -ErrorAction Ignore))
+if($PSEdition -eq 'Desktop' -and (Test-Path $preloadPath))
 {
     try
     {
-        Get-ChildItem -ErrorAction Stop -Path $preloadPath -Filter "*.dll" | ForEach-Object {
+        Get-ChildItem -Path $preloadPath -Filter "*.dll" | ForEach-Object {
             Add-Type -Path $_.FullName -ErrorAction Ignore | Out-Null
         }
     }
@@ -60,12 +60,12 @@ if($PSEdition -eq 'Desktop' -and (Test-Path $preloadPath -ErrorAction Ignore))
 }
 
 $netCorePath = (Join-Path $PSScriptRoot -ChildPath "NetCoreAssemblies")
-if($PSEdition -eq 'Core' -and (Test-Path $netCorePath -ErrorAction Ignore))
+if($PSEdition -eq 'Core' -and (Test-Path $netCorePath))
 {
     try
     {
         $loadedAssemblies = ([System.AppDomain]::CurrentDomain.GetAssemblies() | %{New-Object -TypeName System.Reflection.AssemblyName -ArgumentList $_.FullName} )
-        Get-ChildItem -ErrorAction Stop -Path $netCorePath -Filter "*.dll" | ForEach-Object {
+        Get-ChildItem -Path $netCorePath -Filter "*.dll" | ForEach-Object {
             $assemblyName = ([System.Reflection.AssemblyName]::GetAssemblyName($_.FullName))
             $matches = ($loadedAssemblies | Where-Object {$_.Name -eq $assemblyName.Name})
             if (-not $matches)
