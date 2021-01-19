f2d4d44c8442da13cb7baecdeb4e92816cd4e68b
Daeun Yim <daeunyim@microsoft.com>
2021-01-19 15:41:52 -0800

    skip test if not live

--- src/MySql/test/Get-AzMySqlFlexibleServerConnect.Tests.ps1
+++ src/MySql/test/Get-AzMySqlFlexibleServerConnect.Tests.ps1
@@ -11,34 +11,38 @@ while(-not $mockingPath) {
 }
 . ($mockingPath | Select-Object -First 1).FullName
 
-Install-Module -Name SimplySQL -Scope CurrentUser -Force
+
 
 Describe 'Get-AzMySqlFlexibleServerConnect' {
     #[SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine")]
     $password = 'Pasword01!!2020' | ConvertTo-SecureString -AsPlainText -Force   
-    It 'Get' {
-        {   
-            Get-AzMySqlFlexibleServerConnect -ResourceGroupName $env.resourceGroup -ServerName $env.flexibleServerName -AdministratorLoginPassword $password
-        } | Should -Not -Throw
-    }
 
-    It 'GetViaIdentity' {
-        {
-            $ID = "/subscriptions/$($env.SubscriptionId)/resourceGroups/$($env.resourceGroup)/providers/Microsoft.DBForMySql/flexibleServers/$($env.flexibleServerName)"
-            Get-AzMySqlFlexibleServerConnect -InputObject $ID -AdministratorLoginPassword $password
-        } | Should -Not -Throw
-    }
+    if ($TestMode -eq 'live' -or $TestMode -eq 'record') {
+        Install-Module -Name SimplySQL -Scope CurrentUser -Force
+        It 'Get' {
+            {   
+                Get-AzMySqlFlexibleServerConnect -ResourceGroupName $env.resourceGroup -ServerName $env.flexibleServerName -AdministratorLoginPassword $password
+            } | Should -Not -Throw
+        }
 
-    It 'GetAndQuery' {
-        {   
-            Get-AzMySqlFlexibleServerConnect -ResourceGroupName $env.resourceGroup -ServerName $env.flexibleServerName -AdministratorLoginPassword $password -Query "CREATE TABLE dbtest (col1 INT)"
-        } | Should -Not -Throw
-    }
+        It 'GetViaIdentity' {
+            {
+                $ID = "/subscriptions/$($env.SubscriptionId)/resourceGroups/$($env.resourceGroup)/providers/Microsoft.DBForMySql/flexibleServers/$($env.flexibleServerName)"
+                Get-AzMySqlFlexibleServerConnect -InputObject $ID -AdministratorLoginPassword $password
+            } | Should -Not -Throw
+        }
 
-    It 'GetViaIdentityAndQuery' {
-        {
-            $ID = "/subscriptions/$($env.SubscriptionId)/resourceGroups/$($env.resourceGroup)/providers/Microsoft.DBForMySql/flexibleServers/$($env.flexibleServerName)"
-            Get-AzMySqlFlexibleServerConnect -InputObject $ID -AdministratorLoginPassword $password -Query "CREATE TABLE dbtest2 (col1 INT)"
-        } | Should -Not -Throw
+        It 'GetAndQuery' {
+            {   
+                Get-AzMySqlFlexibleServerConnect -ResourceGroupName $env.resourceGroup -ServerName $env.flexibleServerName -AdministratorLoginPassword $password -Query "CREATE TABLE dbtest (col1 INT)"
+            } | Should -Not -Throw
+        }
+
+        It 'GetViaIdentityAndQuery' {
+            {
+                $ID = "/subscriptions/$($env.SubscriptionId)/resourceGroups/$($env.resourceGroup)/providers/Microsoft.DBForMySql/flexibleServers/$($env.flexibleServerName)"
+                Get-AzMySqlFlexibleServerConnect -InputObject $ID -AdministratorLoginPassword $password -Query "CREATE TABLE dbtest2 (col1 INT)"
+            } | Should -Not -Throw
+        }
     }
 }
