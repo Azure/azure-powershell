477bb8a11ff792c3e1c0b28e2e0b6a1ec943ca4c
Daeun Yim <daeunyim@microsoft.com>
2021-01-19 16:15:44 -0800

    test only in live

--- src/PostgreSql/test/Get-AzPostgreSqlFlexibleServerConnect.Tests.ps1
+++ src/PostgreSql/test/Get-AzPostgreSqlFlexibleServerConnect.Tests.ps1
@@ -11,32 +11,37 @@ while(-not $mockingPath) {
 }
 . ($mockingPath | Select-Object -First 1).FullName
 
+
 Describe 'Get-AzPostgreSqlFlexibleServerConnect' {
     #[SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine")]
     $password = 'Pa88word!' | ConvertTo-SecureString -AsPlainText -Force   
-    It 'Get' {
-        {   
-            Get-AzPostgreSqlFlexibleServerConnect -ResourceGroupName $env.resourceGroup -ServerName $env.flexibleServerName -AdministratorLoginPassword $password
-        } | Should -Not -Throw
-    }
 
-    It 'GetViaIdentity' {
-        {
-            $ID = "/subscriptions/$($env.SubscriptionId)/resourceGroups/$($env.resourceGroup)/providers/Microsoft.DBForPostgreSql/flexibleServers/$($env.flexibleServerName)"
-            Get-AzPostgreSqlFlexibleServerConnect -InputObject $ID -AdministratorLoginPassword $password
-        } | Should -Not -Throw
-    }
+    If ($TestMode -eq 'live' -or $TestMode -eq 'record') {
+        Install-Module -Name SimplySQL -Scope CurrentUser -Force
+        It 'Get' {
+            {   
+                Get-AzPostgreSqlFlexibleServerConnect -ResourceGroupName $env.resourceGroup -ServerName $env.flexibleServerName -AdministratorLoginPassword $password
+            } | Should -Not -Throw
+        }
 
-    It 'GetAndQuery' {
-        {   
-            Get-AzPostgreSqlFlexibleServerConnect -ResourceGroupName $env.resourceGroup -ServerName $env.flexibleServerName -AdministratorLoginPassword $password -Query "CREATE TABLE dbtest (col1 INT)"
-        } | Should -Not -Throw
-    }
+        It 'GetViaIdentity' {
+            {
+                $ID = "/subscriptions/$($env.SubscriptionId)/resourceGroups/$($env.resourceGroup)/providers/Microsoft.DBForPostgreSql/flexibleServers/$($env.flexibleServerName)"
+                Get-AzPostgreSqlFlexibleServerConnect -InputObject $ID -AdministratorLoginPassword $password
+            } | Should -Not -Throw
+        }
 
-    It 'GetViaIdentityAndQuery' {
-        {
-            $ID = "/subscriptions/$($env.SubscriptionId)/resourceGroups/$($env.resourceGroup)/providers/Microsoft.DBForPostgreSql/flexibleServers/$($env.flexibleServerName)"
-            Get-AzPostgreSqlFlexibleServerConnect -InputObject $ID -AdministratorLoginPassword $password -Query "CREATE TABLE dbtest2 (col1 INT)"
-        } | Should -Not -Throw
+        It 'GetAndQuery' {
+            {   
+                Get-AzPostgreSqlFlexibleServerConnect -ResourceGroupName $env.resourceGroup -ServerName $env.flexibleServerName -AdministratorLoginPassword $password -Query "CREATE TABLE dbtest (col1 INT)"
+            } | Should -Not -Throw
+        }
+
+        It 'GetViaIdentityAndQuery' {
+            {
+                $ID = "/subscriptions/$($env.SubscriptionId)/resourceGroups/$($env.resourceGroup)/providers/Microsoft.DBForPostgreSql/flexibleServers/$($env.flexibleServerName)"
+                Get-AzPostgreSqlFlexibleServerConnect -InputObject $ID -AdministratorLoginPassword $password -Query "CREATE TABLE dbtest2 (col1 INT)"
+            } | Should -Not -Throw
+        }
     }
 }
