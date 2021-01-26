c970660b5da211a23ecd6cda02419ba488a31e34
Yabo Hu <yabhu@microsoft.com>
2021-01-26 17:25:19 +0800

    Vmware (#14020)
    * add security tools configuration for ADO security tools pipeline
    
    * add security tools configuration for ADO security tools pipeline
    
    * add suppression messages for credscan
    
    * remove extra comma
    
    * rename psd1
    
    * rename psd1
    
    * rename folder
    
    * rename folder
    
    * rename
    
    * default confirm for remove-azvmwareprivatecloud
    
    * rename
    
    * update name in how-to.md

---
+++ src/VMware/test/Get-AzVMwarePrivateCloud.Tests.ps1
@@ -0,0 +1,30 @@
+$loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+if (-Not (Test-Path -Path $loadEnvPath)) {
+    $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+}
+. ($loadEnvPath)
+$TestRecordingFile = Join-Path $PSScriptRoot 'Get-AzVMwarePrivateCloud.Recording.json'
+$currentPath = $PSScriptRoot
+while(-not $mockingPath) {
+    $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+    $currentPath = Split-Path -Path $currentPath -Parent
+}
+. ($mockingPath | Select-Object -First 1).FullName
+
+Describe 'Get-AzVMwarePrivateCloud' {
+    It 'List1' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'Get' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'List' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'GetViaIdentity' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+}
