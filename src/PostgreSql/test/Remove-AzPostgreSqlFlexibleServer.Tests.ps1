a593abad2820f02d5a0148e09a84baa8c2c801b7
Daeun Yim <daeunyim@microsoft.com>
2021-02-04 15:57:23 -0800

    update test recordings

--- src/PostgreSql/test/Remove-AzPostgreSqlFlexibleServer.Tests.ps1
+++ src/PostgreSql/test/Remove-AzPostgreSqlFlexibleServer.Tests.ps1
@@ -25,7 +25,7 @@ Describe 'Remove-AzPostgreSqlFlexibleServer' {
 
     It 'DeleteViaIdentity' {
         {
-            $ID = "/subscriptions/$($env.SubscriptionId)/resourceGroups/$($env.resourceGroup)/providers/Microsoft.DBForPostgreSql/flexibleServers/$($env.serverName2)"
+            $ID = "/subscriptions/$($env.SubscriptionId)/resourceGroups/$($env.resourceGroup)/providers/Microsoft.DBforPostgreSQL/flexibleServers/$($env.serverName2)"
             If ($TestMode -eq 'live' -or $TestMode -eq 'record') {
                 #[SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine")]
                 $password = 'Pasword01!!2020' | ConvertTo-SecureString -AsPlainText -Force
