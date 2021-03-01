6774e5f3dcffc9a940dcd1b58424bde6a9dbcf7c
Theodore Chang <thchan@microsoft.com>
2021-03-01 15:15:42 -0500

    Orchestration Mode

--- src/Compute/Compute/Generated/Models/PSVirtualMachineScaleSet.cs
+++ src/Compute/Compute/Generated/Models/PSVirtualMachineScaleSet.cs
@@ -65,6 +65,7 @@ namespace Microsoft.Azure.Commands.Compute.Automation.Models
         public PSExtendedLocation ExtendedLocation { get; set; }
         public IDictionary<string, string> Tags { get; set; }
         public PSVirtualMachineScaleSetVMProfile VirtualMachineProfile { get; set; }
+        public string OrchestrationMode { get; set; }
 
     }
 }
