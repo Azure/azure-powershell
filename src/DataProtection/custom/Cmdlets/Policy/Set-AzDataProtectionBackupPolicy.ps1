d6cb15a59bbe7975bb98ec0f26ada2ab1628a5a3
Sambit Rath <es15btech11015@iith.ac.in>
2021-03-09 14:45:08 +0530

    added validation in policy cmdlet

--- src/DataProtection/custom/Cmdlets/Policy/Set-AzDataProtectionBackupPolicy.ps1
+++ src/DataProtection/custom/Cmdlets/Policy/Set-AzDataProtectionBackupPolicy.ps1
@@ -25,6 +25,50 @@
     process
     {
         # Do Validation
+        $retentionNames = @()
+        $tagNames = @()
+
+        foreach($rule in $Policy.PolicyRule)
+        {
+            if(($rule.ObjectType -eq "AzureRetentionRule") -and ($rule.Name -ne "Default"))
+            {
+                $retentionNames += $rule.Name
+            }
+
+            if($rule.ObjectType -eq "AzureBackupRule")
+            {
+                foreach($criteria in $rule.Trigger.TaggingCriterion)
+                {
+                    if($criteria.TagInfoTagName -ne "Default")
+                    {
+                        $tagNames += $criteria.TagInfoTagName
+                    }
+                }
+            }
+        }
+        if($retentionNames.Length -gt 1) { $retentionNames = $retentionNames | Sort-Object }
+        if($tagNames.Length -gt 1) { $tagNames = $tagNames | Sort-Object }
+
+        $index = 0
+        while($index -lt $retentionNames.Length)
+        {
+            $retentionRuleName = $retentionNames[$index]
+            if($index -eq $tagNames.Length)
+            {
+                throw "Retention Rule " + $retentionRuleName + " has no corresponding tag criteria. Please add tag criteria for " + $retentionRuleName + " retention or remove " + $retentionRuleName + " retention rule from backup policy."
+            }
+            if($retentionRuleName -ne $tagNames[$index])
+            {
+                throw "Retention Rule " + $retentionRuleName + " has no corresponding tag criteria. Please add tag criteria for " + $retentionRuleName + " retention or remove " + $retentionRuleName + " retention rule from backup policy."
+            }
+            if(($index -eq ($retentionNames.Length - 1)) -and ($tagNames.Length -gt $retentionNames.Length))
+            {
+                $tagName = $tagNames[$index + 1]
+                throw "Tag Criteria " + $tagName + " has no corresponding retention rule. Please add retention rule for " + $tagName + " tag criteria or remove " + $tagName + " tag criteria from Azure Backup Rule."
+            }
+            $index += 1
+        }
+
         $policyObject = [Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Models.Api202101.BaseBackupPolicyResource]::new()
         $policyObject.Property = $Policy
 
