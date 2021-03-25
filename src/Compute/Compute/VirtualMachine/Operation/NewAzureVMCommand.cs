9bef4df7e1ac8f1082fb77428fa5a3828c6bde1e
Adam Sandor <adsandor@microsoft.com>
2021-03-25 18:12:11 -0400

    Update NewAzureVMCommand.cs

--- src/Compute/Compute/VirtualMachine/Operation/NewAzureVMCommand.cs
+++ src/Compute/Compute/VirtualMachine/Operation/NewAzureVMCommand.cs
@@ -458,7 +458,7 @@ namespace Microsoft.Azure.Commands.Compute
 
             var parameters = new Parameters(this, client, resourceClient);
 
-            //info of size
+            // Information message if the default Size value is used. 
             if (!this.IsBound(Size))
             {
                 WriteInformation("No Size value has been provided. The VM will be created with the default size Standard_D2s_v3.", new string[] { "PSHOST" });
