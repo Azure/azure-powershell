b60e319cca7064acbdc8aaec4f6b9d701cdf4a17
LucasYao93 <53558334+LucasYao93@users.noreply.github.com>
2021-04-01 17:16:07 +0800

    [StreamAnalytics]Add breaking change message for the cmdlet. (#14650)
    * [StreamAnalytics]Add breaking change message for the cmdlet.
    
    * [StreamAnalytics]Rollback vs studio 16 to 15.
    
    * [StreamAnalytics] updated ChangeLog.md.

--- src/StreamAnalytics/StreamAnalytics/Output/RemoveAzureStreamAnalyticsOutputCommand.cs
+++ src/StreamAnalytics/StreamAnalytics/Output/RemoveAzureStreamAnalyticsOutputCommand.cs
@@ -18,9 +18,11 @@ using System.Globalization;
 using System.Management.Automation;
 using System.Net;
 using System.Security.Permissions;
+using Microsoft.WindowsAzure.Commands.Common.CustomAttributes;
 
 namespace Microsoft.Azure.Commands.StreamAnalytics
 {
+    [GenericBreakingChange("The parameters of Remove-AzStreamAnalyticsOutput will be updated in an upcoming breaking change release.", "2.0.0")]
     [Cmdlet("Remove", ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "StreamAnalyticsOutput", SupportsShouldProcess = true), OutputType(typeof(bool))]
     public class RemoveAzureStreamAnalyticsOutputCommand : StreamAnalyticsResourceProviderBaseCmdlet
     {
