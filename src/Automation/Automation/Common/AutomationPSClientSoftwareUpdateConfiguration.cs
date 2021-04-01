1c7c155af246234f6f6d42bec3c91a1317a7d023
Debashish Mohanta <debashish.ism@gmail.com>
2021-04-01 07:41:19 +0530

    [Automation] Fixed time zone issue while creating an update deployment schedule (#14565)
    * Bug 9534965: New-AzAutomationSoftwareUpdateConfiguration cmdlet is creating schedule by taking the UTC value as the StartTime instead of local time.
    
    * Update ChangeLog.md
    
    Updating ChangeLog.md
    
    Co-authored-by: Debashish Mohanta <demoha@microsoft.com>

--- src/Automation/Automation/Common/AutomationPSClientSoftwareUpdateConfiguration.cs
+++ src/Automation/Automation/Common/AutomationPSClientSoftwareUpdateConfiguration.cs
@@ -81,8 +81,8 @@ namespace Microsoft.Azure.Commands.Automation.Common
                 {
                     ScheduleInfo = new Sdk.ScheduleProperties()
                     {
-                        StartTime = configuration.ScheduleConfiguration.StartTime.ToUniversalTime(),
-                        ExpiryTime = configuration.ScheduleConfiguration.ExpiryTime.ToUniversalTime(),
+                        StartTime = configuration.ScheduleConfiguration.StartTime,
+                        ExpiryTime = configuration.ScheduleConfiguration.ExpiryTime,
                         Frequency = configuration.ScheduleConfiguration.Frequency.ToString(),
                         Interval = configuration.ScheduleConfiguration.Interval,
                         IsEnabled = true,
