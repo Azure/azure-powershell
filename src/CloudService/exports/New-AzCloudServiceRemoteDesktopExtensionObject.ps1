8ed5ccee33fa4d76cba836f5d1978d0c496504f4
Yunchi Wang <54880216+wyunchi-ms@users.noreply.github.com>
2021-04-02 12:49:50 +0800

    Update New-AzCloudServiceRemoteDesktopExtensionObject.ps1

--- src/CloudService/exports/New-AzCloudServiceRemoteDesktopExtensionObject.ps1
+++ src/CloudService/exports/New-AzCloudServiceRemoteDesktopExtensionObject.ps1
@@ -26,7 +26,7 @@ PS C:\> $extension = New-AzCloudServiceRemoteDesktopExtensionObject -Name 'RDPEx
 .Outputs
 Microsoft.Azure.PowerShell.Cmdlets.CloudService.Models.Api20210301.Extension
 .Link
-https://docs.microsoft.com/powershell/module/az.CloudService/new-AzCloudServiceExtensionObject
+https://docs.microsoft.com/powershell/module/az.CloudService/new-azcloudserviceremotedesktopextensionobject
 #>
 function New-AzCloudServiceRemoteDesktopExtensionObject {
 [OutputType([Microsoft.Azure.PowerShell.Cmdlets.CloudService.Models.Api20210301.Extension])]
