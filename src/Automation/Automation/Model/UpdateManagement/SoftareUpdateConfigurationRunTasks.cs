ce314554597a922779f8a7b31f03e49204673cab
Ayush <aysharm@microsoft.com>
2021-04-06 14:50:32 +0530

    avoid breaking change 2

---
+++ src/Automation/Automation/Model/UpdateManagement/SoftareUpdateConfigurationRunTasks.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.Management.Automation.Models
+{
+
+    /// <summary>
+    /// Software update configuration run tasks model. 
+    /// Added temporarily to avoid breaking change
+    /// </summary>
+    public class SoftareUpdateConfigurationRunTasks
+    {
+        /// <summary>
+        /// Initializes a new instance of the
+        /// SoftwareUpdateConfigurationRunTasks class.
+        /// </summary>
+        public SoftareUpdateConfigurationRunTasks()
+        {
+            CustomInit();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the
+        /// SoftwareUpdateConfigurationRunTasks class.
+        /// </summary>
+        /// <param name="preTask">Pre task properties.</param>
+        /// <param name="postTask">Post task properties.</param>
+        public SoftareUpdateConfigurationRunTasks(SoftwareUpdateConfigurationRunTaskProperties preTask = default(SoftwareUpdateConfigurationRunTaskProperties), SoftwareUpdateConfigurationRunTaskProperties postTask = default(SoftwareUpdateConfigurationRunTaskProperties))
+        {
+            PreTask = preTask;
+            PostTask = postTask;
+            CustomInit();
+        }
+
+        /// <summary>
+        /// An initialization method that performs custom operations like setting defaults
+        /// </summary>
+        partial void CustomInit();
+
+        /// <summary>
+        /// Gets or sets pre task properties.
+        /// </summary>
+        public SoftwareUpdateConfigurationRunTaskProperties PreTask { get; set; }
+
+        /// <summary>
+        /// Gets or sets post task properties.
+        /// </summary>
+        public SoftwareUpdateConfigurationRunTaskProperties PostTask { get; set; }
+
+    }
+}
--- src/Automation/Automation/Model/UpdateManagement/SoftwareUpdateRun.cs
+++ src/Automation/Automation/Model/UpdateManagement/SoftwareUpdateRun.cs
@@ -59,50 +59,6 @@ namespace Microsoft.Azure.Commands.Automation.Model.UpdateManagement
 
         public SoftareUpdateConfigurationRunTasks Tasks { get; set; }
 
-        /// <summary>
-        /// Software update configuration run tasks model. 
-        /// Added temporarily to avoid breaking change
-        /// </summary>
-        public partial class SoftareUpdateConfigurationRunTasks
-        {
-            /// <summary>
-            /// Initializes a new instance of the
-            /// SoftwareUpdateConfigurationRunTasks class.
-            /// </summary>
-            public SoftareUpdateConfigurationRunTasks()
-            {
-                CustomInit();
-            }
-
-            /// <summary>
-            /// Initializes a new instance of the
-            /// SoftwareUpdateConfigurationRunTasks class.
-            /// </summary>
-            /// <param name="preTask">Pre task properties.</param>
-            /// <param name="postTask">Post task properties.</param>
-            public SoftareUpdateConfigurationRunTasks(SoftwareUpdateConfigurationRunTaskProperties preTask = default(SoftwareUpdateConfigurationRunTaskProperties), SoftwareUpdateConfigurationRunTaskProperties postTask = default(SoftwareUpdateConfigurationRunTaskProperties))
-            {
-                PreTask = preTask;
-                PostTask = postTask;
-                CustomInit();
-            }
-
-            /// <summary>
-            /// An initialization method that performs custom operations like setting defaults
-            /// </summary>
-            partial void CustomInit();
-
-            /// <summary>
-            /// Gets or sets pre task properties.
-            /// </summary>
-            public SoftwareUpdateConfigurationRunTaskProperties PreTask { get; set; }
-
-            /// <summary>
-            /// Gets or sets post task properties.
-            /// </summary>
-            public SoftwareUpdateConfigurationRunTaskProperties PostTask { get; set; }
-
-        }
 
         // SoftwareUpdateConfigurationRunTasks to SoftareUpdateConfigurationRunTasks
         // Added temporarily to avoid breaking change
