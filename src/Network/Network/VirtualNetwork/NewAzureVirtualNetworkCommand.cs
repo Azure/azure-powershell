89b2b3f356a7d0b610d040b9f1984649b5caea9f
Andrew Sager <asager@microsoft.com>
2021-04-29 16:15:02 -0400

    bulk of network changes - nic,pubip,vnet commands

--- src/Network/Network/VirtualNetwork/NewAzureVirtualNetworkCommand.cs
+++ src/Network/Network/VirtualNetwork/NewAzureVirtualNetworkCommand.cs
@@ -54,6 +54,11 @@ namespace Microsoft.Azure.Commands.Network
         public virtual string Location { get; set; }
 
         [Parameter(
+            Mandatory = false,
+            ValueFromPipelineByPropertyName = true)]
+        public string EdgeZone { get; set; }
+
+        [Parameter(
             Mandatory = true,
             ValueFromPipelineByPropertyName = true,
             HelpMessage = "The address prefixes of the virtual network")]
@@ -133,6 +138,7 @@ namespace Microsoft.Azure.Commands.Network
                 Name = Name,
                 ResourceGroupName = ResourceGroupName,
                 Location = Location,
+                ExtendedLocation = new PSExtendedLocation(EdgeZone),
                 AddressSpace = new PSAddressSpace {AddressPrefixes = AddressPrefix?.ToList()}
             };
 
