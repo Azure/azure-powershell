618b422f1f728ab72770e7714cfb5df261f0edb7
daniel ben-hamoo <dabenham@microsoft.com>
2021-05-13 11:07:03 +0300

    aded support for Get Operations for tenent updatted used nuget to 0.23.0-preview

---
+++ src/OperationalInsights/OperationalInsights/Models/PSOperation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.Management.OperationalInsights.Models;
+
+namespace Microsoft.Azure.Commands.OperationalInsights.Models
+{
+    public class PSOperationsApiVersions
+    {
+        public const string October2020 = "2020-10-01";
+    }
+
+    public class PSOperation
+    {
+        public string Name { get; set; }
+        public string Provider { get; set; }
+        public string Resource { get; set; }
+        public string Operation { get; set; }
+        public string Description { get; set; }
+
+
+        public PSOperation(Operation op)
+        {
+            Name = op.Name;
+            Provider = op.Display.Provider;
+            Resource = op.Display.Resource;
+            Operation = op.Display.Operation;
+            Description = op.Display.Description;
+        }
+
+    }
+}
---
+++ src/OperationalInsights/OperationalInsights/Operations/GetAzureOperationalInsightsOperationsCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
+using Microsoft.Azure.Commands.OperationalInsights.Client;
+//using System.Collections.Generic;
+using System.Management.Automation;
+using Microsoft.Azure.Commands.OperationalInsights.Models;
+
+
+namespace Microsoft.Azure.Commands.OperationalInsights.Operations
+{
+    [Cmdlet("Get", ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "OperationalInsightsOperations", SupportsShouldProcess = true), OutputType(typeof(IList<PSOperation>))]
+    public class GetAzureOperationalInsightsOperationsCommand : OperationalInsightsBaseCmdlet
+    {
+        public override void ExecuteCmdlet()
+        {
+            WriteObject(this.OperationalInsightsClient.GetPSOperations(), true);
+        }
+    }
+}
