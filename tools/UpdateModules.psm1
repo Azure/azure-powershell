6b2cd670e3d1bf6e319d46db8a26b066dab70259
Beisi Zhou <zhoubeisi@gmail.com>
2021-05-18 19:01:01 +0800

    Create AzPreview module (#15034)
    * create AzPreview module
    
    * Update AzPreview.psd1.template
    
    * Update AzPreview.psd1
    
    Co-authored-by: Dingmeng Xue <dixue@microsoft.com>

--- tools/UpdateModules.psm1
+++ tools/UpdateModules.psm1
@@ -437,4 +437,9 @@ function Update-Netcore {
     Write-Host "Updating Netcore module from $modulePath"
     New-ModulePsm1 -ModulePath $modulePath -TemplatePath $script:TemplateLocation -IsNetcore
     Write-Host "Updated Netcore module"
+
+    $modulePath = "$PSScriptRoot\AzPreview"
+    Write-Host "Updating Netcore module from $modulePath"
+    New-ModulePsm1 -ModulePath $modulePath -TemplatePath $script:TemplateLocation -IsNetcore
+    Write-Host "Updated Netcore module"
 }
