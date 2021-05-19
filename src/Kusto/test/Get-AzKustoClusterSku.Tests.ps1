167db3fd0c5a987530e5a0fb9e8df851c0346d8c
astauben <astauben@microsoft.com>
2021-05-19 05:04:57 +0300

    Kusto: 2021-01-01 Client (#14939)
    * update files
    
    * asd
    
    * commit
    
    * 'changes'
    
    * test
    
    * build
    
    * commit
    
    * 32 remaining
    
    * one remaining
    
    * tests are passing
    
    * try1
    
    * Update Invoke-AzKustoDetachClusterFollowerDatabase.Recording.json
    
    * Update Invoke-AzKustoDetachClusterFollowerDatabase.Recording.json
    
    * tests
    
    * tests2
    
    * test
    
    * recordings-get
    
    * test-dc-invoke
    
    * tests
    
    * commit
    
    * fix
    
    * add language
    
    * update dataconnection
    
    * last test
    
    * fix

--- src/Kusto/test/Get-AzKustoClusterSku.Tests.ps1
+++ src/Kusto/test/Get-AzKustoClusterSku.Tests.ps1
@@ -1,20 +1,21 @@
-$loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
-if (-Not (Test-Path -Path $loadEnvPath)) {
-    $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
-}
-. ($loadEnvPath)
-$TestRecordingFile = Join-Path $PSScriptRoot 'Get-AzKustoClusterSku.Recording.json'
-$currentPath = $PSScriptRoot
-while (-not $mockingPath) {
-    $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
-    $currentPath = Split-Path -Path $currentPath -Parent
-}
-. ($mockingPath | Select-Object -First 1).FullName
-
 Describe 'Get-AzKustoClusterSku' {
+    BeforeAll{
+        $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+        if (-Not (Test-Path -Path $loadEnvPath)) {
+            $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+        }
+        . ($loadEnvPath)
+        $TestRecordingFile = Join-Path $PSScriptRoot 'Get-AzKustoClusterSku.Recording.json'
+        $currentPath = $PSScriptRoot
+        while (-not $mockingPath) {
+            $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+            $currentPath = Split-Path -Path $currentPath -Parent
+        }
+        . ($mockingPath | Select-Object -First 1).FullName
+    }
     It 'List' {
         [array]$clusterSku = Get-AzKustoClusterSku
-        $clusterSku.Count | Should -Be 493
+        $clusterSku.Count | Should -Be 530
     }
 
     It 'List1' {
@@ -22,6 +23,6 @@ Describe 'Get-AzKustoClusterSku' {
         $clusterName = $env.clusterName
 
         [array]$clusterSku = Get-AzKustoClusterSku -ResourceGroupName $resourceGroupName -ClusterName $clusterName
-        $clusterSku.Count | Should -Be 23
+        $clusterSku.Count | Should -Be 24
     }
 }
