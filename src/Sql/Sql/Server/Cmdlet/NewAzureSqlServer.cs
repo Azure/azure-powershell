eb271d809189c7e272ddebb75c9791869cb0e4fb
viparek <78561564+viparek@users.noreply.github.com>
2021-06-30 01:48:14 -0700

    [SQL] Bug fix for UMI (#15338)
    * [SQL] Bug fix for UMI
    
    * Add changelog
    
    * UMI fixes for support SMI,UMI
    
    * Add fix for Get-AzSqlInstance
    
    * Added fix for SMI,UMI IdentityType
    
    * Fix for backward compat
    
    * modified help
    
    Co-authored-by: Beisi Zhou <zazbs@qq.com>

--- src/Sql/Sql/Server/Cmdlet/NewAzureSqlServer.cs
+++ src/Sql/Sql/Server/Cmdlet/NewAzureSqlServer.cs
@@ -119,8 +119,8 @@ namespace Microsoft.Azure.Commands.Sql.Server.Cmdlet
         /// Type of identity to be assigned to the server..
         /// </summary>
         [Parameter(Mandatory = false,
-            HelpMessage = "Type of Identity to be used. Possible values are SystemAsssigned, UserAssigned, SystemAssignedUserAssigned and None.")]
-        [PSArgumentCompleter("SystemAssigned", "UserAssigned", "SystemAssignedUserAssigned", "None")]
+            HelpMessage = "Type of Identity to be used. Possible values are SystemAssigned, UserAssigned, 'SystemAssigned,UserAssigned' and None.")]
+        [PSArgumentCompleter("SystemAssigned", "UserAssigned", "\"SystemAssigned,UserAssigned\"", "None")]
         public string IdentityType { get; set; }
 
         /// <summary>
