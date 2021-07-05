508a99e3112d0c42e74d4e2feabb8074277dadad
Erich(Renyong) Wang <eriwan@microsoft.com>
2021-07-05 13:27:16 +0800

    Refine assembly load context logic

--- src/Compute/Compute/PostImportScripts/InitializeAssemblyLoadContext.ps1
+++
@@ -1,7 +0,0 @@
-﻿try {
-    $assemblyLoadContextFolder = [System.IO.Path]::Combine($PSScriptRoot, "..", "ModuleAclAssemblies")
-    [Microsoft.Azure.PowerShell.AuthenticationAssemblyLoadContext.AzAssemblyLoadContextInitializer]::RegisterModuleAssemblyLoadContext("Microsoft.Azure.Commands.Compute.AlcWrapper", $assemblyLoadContextFolder)
-
-  } catch {
-        Write-Warning $_
-  }
