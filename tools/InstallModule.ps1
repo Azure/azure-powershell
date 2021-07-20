2b49deddb16375029840272a41ce23ccc6e22947
Beisi Zhou <bez@microsoft.com>
2021-07-20 17:33:30 +0800

    refine codes

--- tools/InstallModule.ps1
+++ tools/InstallModule.ps1
@@ -8,34 +8,9 @@ param(
   $SourceLocation = $PSScriptRoot
 )
 
-function Register-Gallery {
-  param(
-    [string]
-    [Parameter(Mandatory = $true, Position = 0)]
-    $GalleryName,
-    [string]
-    [Parameter(Mandatory = $false, Position = 1)]
-    $SourceLocation
-  )  
-  switch ($GalleryName) {
-    'PSGallery' {   
-      Write-Host "Setting $GalleryName Trusted..."
-      Set-PSRepository -Name $GalleryName -InstallationPolicy Trusted
-      break;
-    }
-    'TestGallery' { 
-      Write-Host "Registering $GalleryName Trusted..."
-      Register-PSRepository -Name $GalleryName -SourceLocation 'https://www.poshtestgallery.com/api/v2' -PackageManagementProvider NuGet -InstallationPolicy Trusted
-      break;
-    }
-    Default {
-      Write-Host "Registering $GalleryName Trusted..."
-      Register-PSRepository -Name $GalleryName -SourceLocation $SourceLocation -PackageManagementProvider NuGet -InstallationPolicy Trusted    }
-  }
-}
-
 $gallery = [guid]::NewGuid().ToString()
-Register-Gallery -GalleryName $gallery -SourceLocation $SourceLocation
+Write-Host "Registering temporary repository $gallery with InstallationPolicy Trusted..."
+Register-PSRepository -Name $gallery -SourceLocation $SourceLocation -PackageManagementProvider NuGet -InstallationPolicy Trusted    
 
 try {	
 	Write-Host "Installing $ModuleName..."
