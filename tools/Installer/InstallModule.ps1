bddcb3e5471ee0d1ddec1d8d96a7f29340018faa
Beisi Zhou <bez@microsoft.com>
2021-07-21 10:24:06 +0800

    replace tab with space

--- tools/Installer/InstallModule.ps1
+++ tools/Installer/InstallModule.ps1
@@ -12,11 +12,11 @@ $gallery = [guid]::NewGuid().ToString()
 Write-Host "Registering temporary repository $gallery with InstallationPolicy Trusted..."
 Register-PSRepository -Name $gallery -SourceLocation $SourceLocation -PackageManagementProvider NuGet -InstallationPolicy Trusted    
 
-try {	
-	Write-Host "Installing $ModuleName..."
-	Install-Module -Name $ModuleName -Repository $gallery -Scope CurrentUser -AllowClobber -Force 
+try {
+  Write-Host "Installing $ModuleName..."
+  Install-Module -Name $ModuleName -Repository $gallery -Scope CurrentUser -AllowClobber -Force 
 }
 finally {
-	Write-Host "Unregistering gallery $gallery..."
-	Unregister-PSRepository -Name $gallery
+  Write-Host "Unregistering gallery $gallery..."
+  Unregister-PSRepository -Name $gallery
 }
