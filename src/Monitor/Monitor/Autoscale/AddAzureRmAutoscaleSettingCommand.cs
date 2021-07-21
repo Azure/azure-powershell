Azure/azure-powershell

commit a7a2d8c5d4e84381808ed56ce848dff6e99c01be

Fixed bug for `Add-AzAutoscaleSetting` not able to pipe result from `Get-AzAutoScaleSetting` [#13861] (#15353)
* bugfix

* update help markdown

* suppress breakingchange issues

src/Monitor/Monitor/Autoscale/AddAzureRmAutoscaleSettingCommand.cs
@@ -39,7 +39,7 @@ namespace Microsoft.Azure.Commands.Insights.Autoscale
         /// <summary>
         /// Gets or sets the InputObject parameter of the cmdlet
         /// </summary>
-        [Parameter(ParameterSetName = AddAzureRmAutoscaleSettingUpdateParamGroup, Mandatory = true, ValueFromPipelineByPropertyName = true, HelpMessage = "The complete spec of an AutoscaleSetting")]
+        [Parameter(ParameterSetName = AddAzureRmAutoscaleSettingUpdateParamGroup, Mandatory = true, ValueFromPipeline = true, HelpMessage = "The complete spec of an AutoscaleSetting")]
         [ValidateNotNullOrEmpty]
         [Alias("SettingSpec")]
         public PSAutoscaleSetting InputObject { get; set; }
@@ -63,7 +63,6 @@ namespace Microsoft.Azure.Commands.Insights.Autoscale
         /// Gets or set the resource group name
         /// </summary>
         [Parameter(ParameterSetName = AddAzureRmAutoscaleSettingCreateParamGroup, Mandatory = true, ValueFromPipelineByPropertyName = true, HelpMessage = "The resource group name")]
-        [Parameter(ParameterSetName = AddAzureRmAutoscaleSettingUpdateParamGroup, Mandatory = true, ValueFromPipelineByPropertyName = true, HelpMessage = "The resource group name")]
         [ResourceGroupCompleter]
         [ValidateNotNullOrEmpty]
         [Alias("ResourceGroup")]
@@ -90,6 +89,7 @@ namespace Microsoft.Azure.Commands.Insights.Autoscale
         /// </summary>
         [Parameter(ParameterSetName = AddAzureRmAutoscaleSettingCreateParamGroup, Mandatory = true, ValueFromPipelineByPropertyName = true, HelpMessage = "The resource id for the setting")]
         [ValidateNotNullOrEmpty]
+        [Alias("TargetResourceUri")]
         public string TargetResourceId { get; set; }
 
         /// <summary>

commit fb3f22bb6ecc0c59d05f5409f9b2f07eaaf0c2c8

fix breaking changes introduced by #15353 (#15492)
* fix breaking changes

* fix test case

src/Monitor/Monitor/Autoscale/AddAzureRmAutoscaleSettingCommand.cs
@@ -22,6 +22,7 @@ using System.Linq;
 using System.Management.Automation;
 using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
 using Microsoft.Azure.Commands.Insights.TransitionalClasses;
+using Microsoft.Azure.Management.Internal.Resources.Utilities.Models;
 
 namespace Microsoft.Azure.Commands.Insights.Autoscale
 {
@@ -63,6 +64,7 @@ namespace Microsoft.Azure.Commands.Insights.Autoscale
         /// Gets or set the resource group name
         /// </summary>
         [Parameter(ParameterSetName = AddAzureRmAutoscaleSettingCreateParamGroup, Mandatory = true, ValueFromPipelineByPropertyName = true, HelpMessage = "The resource group name")]
+        [Parameter(ParameterSetName = AddAzureRmAutoscaleSettingUpdateParamGroup, Mandatory = false, ValueFromPipelineByPropertyName = true, HelpMessage = "The resource group name")]
         [ResourceGroupCompleter]
         [ValidateNotNullOrEmpty]
         [Alias("ResourceGroup")]
@@ -142,6 +144,7 @@ namespace Microsoft.Azure.Commands.Insights.Autoscale
 
                 this.Location = this.InputObject.Location;
                 this.Name = this.InputObject.Name;
+                this.ResourceGroupName = this.ResourceGroupName ?? new ResourceIdentifier(this.InputObject.Id).ResourceGroupName;
 
                 // The semantics is if AutoscaleProfiles is given it will replace the existing Profiles
                 this.AutoscaleProfile = this.AutoscaleProfile ?? property.Profiles.ToList();