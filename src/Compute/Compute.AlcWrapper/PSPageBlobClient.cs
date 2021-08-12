76e24ffda6d59088331a601fa96a66488a4c129c
Theodore Chang <thchan@microsoft.com>
2021-08-12 18:10:45 -0400

    create PSPageBlobClient.cs in alcWrapper and move content to it

---
+++ src/Compute/Compute.AlcWrapper/PSPageBlobClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Azure.Storage.Blobs.Specialized;
+using System.IO;
+
+namespace Microsoft.Azure.Commands.Compute
+{
+    public class PSPageBlobClient
+    {
+        private PageBlobClient _pageBlobClient;
+
+        internal PSPageBlobClient(PageBlobClient pageblobclient)
+        {
+            _pageBlobClient = pageblobclient;
+        }
+        public PSPageBlobClient(Uri blobUri)
+        {
+            _pageBlobClient = new PageBlobClient(blobUri, null);
+        }
+
+        public Uri Uri { get { return _pageBlobClient.Uri; } }
+
+        public void UploadPages(Stream content, long offset)
+        {
+            _pageBlobClient.UploadPagesAsync(content, offset).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+    }
+}
