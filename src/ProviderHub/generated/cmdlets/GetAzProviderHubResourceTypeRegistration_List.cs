2d9831c7fcdb04ed80f8aaf1ec17a19ebde83293
azurepowershell <azurepowershell@ms.com>
2021-08-16 03:04:44 +0000

    Move ProviderHub to main

--- src/ProviderHub/generated/cmdlets/GetAzProviderHubResourceTypeRegistration_List.cs
+++ src/ProviderHub/generated/cmdlets/GetAzProviderHubResourceTypeRegistration_List.cs
@@ -11,6 +11,7 @@ namespace Microsoft.Azure.PowerShell.Cmdlets.ProviderHub.Cmdlets
     /// <remarks>
     /// [OpenAPI] ListByProviderRegistration=>GET:"/subscriptions/{subscriptionId}/providers/Microsoft.ProviderHub/providerRegistrations/{providerNamespace}/resourcetypeRegistrations"
     /// </remarks>
+    [global::Microsoft.Azure.PowerShell.Cmdlets.ProviderHub.InternalExport]
     [global::System.Management.Automation.Cmdlet(global::System.Management.Automation.VerbsCommon.Get, @"AzProviderHubResourceTypeRegistration_List")]
     [global::System.Management.Automation.OutputType(typeof(Microsoft.Azure.PowerShell.Cmdlets.ProviderHub.Models.Api20201120.IResourceTypeRegistration))]
     [global::Microsoft.Azure.PowerShell.Cmdlets.ProviderHub.Description(@"Gets the list of the resource types for the given provider.")]
@@ -207,7 +208,7 @@ namespace Microsoft.Azure.PowerShell.Cmdlets.ProviderHub.Cmdlets
                     case Microsoft.Azure.PowerShell.Cmdlets.ProviderHub.Runtime.Events.Information:
                     {
                         var data = messageData();
-                        WriteInformation(data, new[] { data.Message });
+                        WriteInformation(data.Message, new string[]{});
                         return ;
                     }
                     case Microsoft.Azure.PowerShell.Cmdlets.ProviderHub.Runtime.Events.Debug:
--- src/ProviderHub/help/Get-AzProviderHubSku.md
+++ src/ProviderHub/help/Get-AzProviderHubSku.md
@@ -65,6 +65,17 @@ testResourceType            Microsoft.ProviderHub/providerRegistrations/skus
 
 Get the resource SKU definition.
 
+### Example 2: Get the nested resource type SKU definition.
+```powershell
+PS C:\> Get-AzProviderHubSku -ProviderNamespace "Microsoft.Contoso" -ResourceType "testResourceType/nestedResourceType" -Sku "default"
+
+Name                                        Type
+----                                        ----
+testResourceType/nestedResourceType         Microsoft.ProviderHub/providerRegistrations/skus
+```
+
+Get the nested resource type SKU definition.
+
 ## PARAMETERS
 
 ### -DefaultProfile
