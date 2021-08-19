d3aee1683416c555b7de85251d92c85b4e669c81
Jinpei Li (Wicresoft North America Ltd) <v-jinpel@microsoft.com>
2021-08-19 16:10:51 +0800

    update

--- src/VMware/test/utils.ps1
+++ src/VMware/test/utils.ps1
@@ -12,14 +12,14 @@ function setupEnv() {
     $env.SubscriptionId = (Get-AzContext).Subscription.Id
     $env.Tenant = (Get-AzContext).Tenant.Id
 
-    $privateCloudName1 = "azps_test_cloud_1" #+ (RandomString -allChars $false -len 3)
-    $privateCloudName2 = "azps_test_cloud_2" #+ (RandomString -allChars $false -len 4)
-    $privateCloudName3 = "azps_test_cloud_3" #+ (RandomString -allChars $false -len 4)
+    $privateCloudName1 = "azps_test_cloud" + (RandomString -allChars $false -len 4)
+    $privateCloudName2 = "azps_test_cloud" + (RandomString -allChars $false -len 4)
+    $privateCloudName3 = "azps_test_cloud" + (RandomString -allChars $false -len 4)
 
-    $rstr1 = "twp4gr" #RandomString -allChars $false -len 6
-    $rstr2 = "wmvla1" #RandomString -allChars $false -len 6
-    $rstr3 = "azslrt" #RandomString -allChars $false -len 6
-    $rstr4 = "ftr3ic" #RandomString -allChars $false -len 6
+    $rstr1 = RandomString -allChars $false -len 6
+    $rstr2 = RandomString -allChars $false -len 6
+    $rstr3 = RandomString -allChars $false -len 6
+    $rstr4 = RandomString -allChars $false -len 6
 
     $env.Add("privateCloudName1", $privateCloudName1)
     $env.Add("privateCloudName2", $privateCloudName2)
@@ -35,28 +35,28 @@ function setupEnv() {
 
     # Create the test group
     write-host "start to create test group"
-    $resourceGroup1 = "azps_test_group_1" #"testgroup" + $env.rstr4
-    $resourceGroup2 = "azps_test_group_2" #"testgroup" + $env.rstr4
-    $resourceGroup3 = "azps_test_group_3" #"testgroup" + $env.rstr4
+    $resourceGroup1 = "testgroup" + $env.rstr4
+    $resourceGroup2 = "testgroup" + $env.rstr4
+    $resourceGroup3 = "testgroup" + $env.rstr4
     $env.Add("resourceGroup1", $resourceGroup1)
     $env.Add("resourceGroup2", $resourceGroup2)
     $env.Add("resourceGroup3", $resourceGroup3)
     
-    #New-AzResourceGroup -Name $env.resourceGroup1 -Location $env.location1
-    #New-AzResourceGroup -Name $env.resourceGroup2 -Location $env.location1
-    #New-AzResourceGroup -Name $env.resourceGroup3 -Location $env.location1
+    New-AzResourceGroup -Name $env.resourceGroup1 -Location $env.location1
+    New-AzResourceGroup -Name $env.resourceGroup2 -Location $env.location1
+    New-AzResourceGroup -Name $env.resourceGroup3 -Location $env.location1
 
-    #New-AzVMwarePrivateCloud -Name $env.privateCloudName1 `
-    #    -ResourceGroupName $env.resourceGroup1 -NetworkBlock 192.168.48.0/22 `
-    #    -Sku av36 -ManagementClusterSize 3 -Location $env.location1 -AcceptEULA
-    #New-AzVMwareExpressRouteAuthorization -Name $env.rstr1 -PrivateCloudName $env.privateCloudName1 `
-    #    -ResourceGroupName $env.resourceGroup1
+    New-AzVMwarePrivateCloud -Name $env.privateCloudName1 `
+        -ResourceGroupName $env.resourceGroup1 -NetworkBlock 192.168.48.0/22 `
+        -Sku av36 -ManagementClusterSize 3 -Location $env.location1 -AcceptEULA
+    New-AzVMwareExpressRouteAuthorization -Name $env.rstr1 -PrivateCloudName $env.privateCloudName1 `
+        -ResourceGroupName $env.resourceGroup1
     
-    #New-AzVMwarePrivateCloud -Name $env.privateCloudName2 `
-    #    -ResourceGroupName $env.resourceGroup2 -NetworkBlock 192.168.48.0/22 `
-    #    -Sku av36 -ManagementClusterSize 3 -Location $env.location1 -AcceptEULA
-    #New-AzVMwareExpressRouteAuthorization -Name $env.rstr1 -PrivateCloudName $env.privateCloudName2 `
-    #    -ResourceGroupName $env.resourceGroup2
+    New-AzVMwarePrivateCloud -Name $env.privateCloudName2 `
+        -ResourceGroupName $env.resourceGroup2 -NetworkBlock 192.168.48.0/22 `
+        -Sku av36 -ManagementClusterSize 3 -Location $env.location1 -AcceptEULA
+    New-AzVMwareExpressRouteAuthorization -Name $env.rstr1 -PrivateCloudName $env.privateCloudName2 `
+        -ResourceGroupName $env.resourceGroup2
 
     New-AzVMwarePrivateCloud -Name $env.privateCloudName3 `
         -ResourceGroupName $env.resourceGroup3 -NetworkBlock 192.168.48.0/22 `
@@ -72,8 +72,8 @@ function setupEnv() {
 }
 function cleanupEnv() {
     # Clean resources you create for testing
-    #Remove-AzResourceGroup -Name $env.resourceGroup1
-    #Remove-AzResourceGroup -Name $env.resourceGroup2
-    #Remove-AzResourceGroup -Name $env.resourceGroup3
+    Remove-AzResourceGroup -Name $env.resourceGroup1
+    Remove-AzResourceGroup -Name $env.resourceGroup2
+    Remove-AzResourceGroup -Name $env.resourceGroup3
 }
 
