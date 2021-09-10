1db68b74630022f9a0303cdc69830d66be120833
Yabo Hu <yabhu@microsoft.com>
2021-09-10 13:55:28 +0800

    Yabo/msgraph (#15850)
    * add app/sp password api to application.yml
    
    * update custom cmdlets for list and remove-application/user

---
+++ src/Resources/MSGraph.Autorest/custom/Get-AzMgServicePrincipal.ps1
@@ -0,0 +1,252 @@
+
+# ----------------------------------------------------------------------------------
+#
+# Copyright Microsoft Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------------
+
+<#
+.Synopsis
+Represents an Azure Active Directory object.
+The directoryObject type is the base type for many other directory entity types.
+.Description
+Represents an Azure Active Directory object.
+The directoryObject type is the base type for many other directory entity types.
+.Example
+PS C:\> {{ Add code here }}
+
+{{ Add output here }}
+.Example
+PS C:\> {{ Add code here }}
+
+{{ Add output here }}
+
+.Outputs
+Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Models.ApiV10.IMicrosoftGraphServicePrincipal
+.Link
+https://docs.microsoft.com/powershell/module/az.resources/get-azmgserviceprincipal
+#>
+function Get-AzMgServicePrincipal {
+[OutputType([Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Models.ApiV10.IMicrosoftGraphServicePrincipal])]
+[CmdletBinding(DefaultParameterSetName='EmptyParameterSet', PositionalBinding=$false)]
+param(
+    [Parameter(ParameterSetName='ObjectIdParameterSet', Mandatory)]
+    [Alias('ServicePrincipalId', 'Id')]
+    [Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Category('Path')]
+    [System.String]
+    # key: id of servicePrincipal
+    ${ObjectId},
+
+    [Parameter(ParameterSetName='SearchStringParameterSet', Mandatory)]
+    [Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Category('Query')]
+    [System.String]
+    # serviceprincipal display name starts with
+    ${DisplayNameStartWith},
+
+    [Parameter(ParameterSetName='DisplayNameParameterSet', Mandatory)]
+    [Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Category('Query')]
+    [System.String]
+    # serviceprincipal display name
+    ${DisplayName},
+
+    [Parameter(ParameterSetName='ApplicationIdParameterSet', Mandatory)]
+    [Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Category('Query')]
+    [System.String]
+    # application id of serviceprincipal
+    ${ApplicationId},
+
+    [Parameter(ParameterSetName='ApplicationObjectParameterSet', Mandatory, ValueFromPipeline)]
+    [Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Category('Query')]
+    [Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Models.ApiV10.IMicrosoftGraphApplication]
+    # application object
+    ${ApplicationObject},
+
+    [Parameter(ParameterSetName='SPNParameterSet', Mandatory)]
+    [Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Category('Query')]
+    [System.String]
+    # serviceprincipal name
+    ${ServicePrincipalName},
+
+    [Parameter()]
+    [AllowEmptyCollection()]
+    [Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Category('Query')]
+    [System.String[]]
+    # Expand related entities
+    ${Expand},
+
+    [Parameter()]
+    [AllowEmptyCollection()]
+    [Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Category('Query')]
+    [System.String[]]
+    # Select properties to be returned
+    ${Select},
+
+    [Parameter(ParameterSetName='EmptyParameterSet')]
+    [Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Category('Query')]
+    [System.Management.Automation.SwitchParameter]
+    # Include count of items
+    ${Count},
+
+    [Parameter(ParameterSetName='EmptyParameterSet')]
+    [Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Category('Query')]
+    [System.String]
+    # Filter items by property values
+    ${Filter},
+
+    [Parameter(ParameterSetName='EmptyParameterSet')]
+    [AllowEmptyCollection()]
+    [Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Category('Query')]
+    [System.String[]]
+    # Order items by property values
+    ${Orderby},
+
+    [Parameter(ParameterSetName='EmptyParameterSet')]
+    [Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Category('Query')]
+    [System.String]
+    # Search items by search phrases
+    ${Search},
+
+    [Parameter()]
+    [Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Category('Header')]
+    [System.String]
+    # Indicates the requested consistency level.
+    # Documentation URL: https://developer.microsoft.com/en-us/office/blogs/microsoft-graph-advanced-queries-for-directory-objects-are-now-generally-available/
+    ${ConsistencyLevel},
+
+    [Parameter()]
+    [Alias('AzureRMContext', 'AzureCredential')]
+    [ValidateNotNull()]
+    [Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Category('Azure')]
+    [System.Management.Automation.PSObject]
+    # The credentials, account, tenant, and subscription used for communication with Azure.
+    ${DefaultProfile},
+
+    [Parameter(DontShow)]
+    [Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Category('Runtime')]
+    [System.Management.Automation.SwitchParameter]
+    # Wait for .NET debugger to attach
+    ${Break},
+
+    [Parameter(DontShow)]
+    [ValidateNotNull()]
+    [Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Category('Runtime')]
+    [Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Runtime.SendAsyncStep[]]
+    # SendAsync Pipeline Steps to be appended to the front of the pipeline
+    ${HttpPipelineAppend},
+
+    [Parameter(DontShow)]
+    [ValidateNotNull()]
+    [Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Category('Runtime')]
+    [Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Runtime.SendAsyncStep[]]
+    # SendAsync Pipeline Steps to be prepended to the front of the pipeline
+    ${HttpPipelinePrepend},
+
+    [Parameter(DontShow)]
+    [Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Category('Runtime')]
+    [System.Uri]
+    # The URI for the proxy server to use
+    ${Proxy},
+
+    [Parameter(DontShow)]
+    [ValidateNotNull()]
+    [Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Category('Runtime')]
+    [System.Management.Automation.PSCredential]
+    # Credentials for a proxy server to use for the remote call
+    ${ProxyCredential},
+
+    [Parameter(DontShow)]
+    [Microsoft.Azure.PowerShell.Cmdlets.Resources.MSGraph.Category('Runtime')]
+    [System.Management.Automation.SwitchParameter]
+    # Use the default credentials for the proxy
+    ${ProxyUseDefaultCredentials}
+)
+
+begin {
+    try {
+        $outBuffer = $null
+        if ($PSBoundParameters.TryGetValue('OutBuffer', [ref]$outBuffer)) {
+            $PSBoundParameters['OutBuffer'] = 1
+        }
+        $parameterSet = $PSCmdlet.ParameterSetName
+
+        $cmdName = 'Az.Resources.MSGraph.private\Get-AzMgServicePrincipal_List'
+        if ($parameterSet -ne "EmptyParameterSet") {
+            if ($parameterSet -eq "ObjectIdParameterSet") {
+                    $cmdletName = 'Az.Resources.MSGraph.private\Get-AzMgServicePrincipal_Get';
+                    $id = $PSBoundParameters['ObjectId']
+                    $null = $PSBoundParameters.Remove('ObjectId')
+                    $PSBoundParameters.Add('Id', $id)
+            } else {
+                switch ($parameterSet) {
+                    "SearchStringParameterSet" {
+                        $key2Remove = "DisplayNameStartWith"
+                        $key2Search = "DisplayName"
+                        break
+                    }
+                    "DisplayNameParameterSet" {
+                        $key2Remove = "DisplayName"
+                        $key2Search = $key2Remove
+                        break
+                    }
+                    "ApplicationObjectParameterSet" {
+                        $obj = $PSBoundParameters["ApplicationObject"]
+                        $null = $PSBoundParameters.Remove("ApplicationObject")
+                        $key2Remove = "ApplicationId"
+                        $key2Search = 'AppId'
+                        $PSBoundParameters[$key2Remove] = $obj.AppId
+                        break;
+                    }
+                    "ApplicationIdParameterSet" {
+                        $key2Remove = "ApplicationId"
+                        $key2Search = 'AppId'
+                        break
+                    }
+                    "SPNParameterSet" {
+                        $key2Remove = "ServicePrincipalName"
+                        $key2Search = 'ServicePrincipalNames'
+                    }
+                    default {
+                        break
+                    }
+                }
+
+                $val = $key2Search + ":" + $PSBoundParameters[$key2Remove]
+                $null = $PSBoundParameters.Remove($key2Remove)
+                $PSBOundParameters["Search"] = $val
+                $PSBoundParameters['ConsistencyLevel'] = 'eventual'
+            }
+        }
+
+        $wrappedCmd = $ExecutionContext.InvokeCommand.GetCommand($cmdName, [System.Management.Automation.CommandTypes]::Cmdlet)
+        $scriptCmd = {& $wrappedCmd @PSBoundParameters}
+        $steppablePipeline = $scriptCmd.GetSteppablePipeline($MyInvocation.CommandOrigin)
+        $steppablePipeline.Begin($PSCmdlet)
+    } catch {
+        throw
+    }
+}
+
+process {
+    try {
+        $steppablePipeline.Process($_)
+    } catch {
+        throw
+    }
+}
+
+end {
+    try {
+        $steppablePipeline.End()
+    } catch {
+        throw
+    }
+}
+}
