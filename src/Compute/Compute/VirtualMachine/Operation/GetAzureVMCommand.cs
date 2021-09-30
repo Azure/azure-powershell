fe620fb8b80bd57e2d4d3a720e1c553e4efc3416
Theodore Chang <thchan@microsoft.com>
2021-09-30 17:44:57 +0100

    remove storage component from get-azvm

--- src/Compute/Compute/VirtualMachine/Operation/GetAzureVMCommand.cs
+++ src/Compute/Compute/VirtualMachine/Operation/GetAzureVMCommand.cs
@@ -100,13 +100,8 @@ namespace Microsoft.Azure.Commands.Compute
 
         public override void ExecuteCmdlet()
         {
-            //TODO: Must be removed after Compute team add real logic
-            AzureStorageService servie = new AzureStorageService();
 
             base.ExecuteCmdlet();
-
-            var azureStorageService = new AzureStorageService();
-            azureStorageService.UseBlobClientOptions();
 
             ExecuteClientAction(() =>
             {
