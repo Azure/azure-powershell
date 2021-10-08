ab4c08200323975436cb764a53b643480943f983
Adam Sandor <adsandor@microsoft.com>
2021-10-08 11:09:23 -0400

    Update VirtualMachineTests.cs

--- src/Compute/Compute.Test/ScenarioTests/VirtualMachineTests.cs
+++ src/Compute/Compute.Test/ScenarioTests/VirtualMachineTests.cs
@@ -437,6 +437,13 @@ namespace Microsoft.Azure.Commands.Compute.Test.ScenarioTests
         {
             TestRunner.RunTestScript("Test-VMwithSSHKey");
         }
+        
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void TestVMUserData()
+        {
+            TestRunner.RunTestScript("Test-VMUserData");
+        }
 
     }
 }
