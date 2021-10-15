c7f507e4b2678dc1ad9e4ac8f374709d935f1f2f
Maoliang Huang <mahuang@microsoft.com>
2021-10-15 10:50:19 -0700

    Fix the colors

--- tools/Az.Tools.Predictor/Az.Tools.Predictor/AzPredictorSurveyHelper.cs
+++ tools/Az.Tools.Predictor/Az.Tools.Predictor/AzPredictorSurveyHelper.cs
@@ -38,7 +38,7 @@ namespace Microsoft.Azure.PowerShell.Tools.AzPredictor
             var promptMessageScript = @"
                 if ([Microsoft.Azure.PowerShell.Tools.AzPredictor.AzPredictorData]::ShowSurveyOnIdle) {
                     [Microsoft.Azure.PowerShell.Tools.AzPredictor.AzPredictorData]::ShowSurveyOnIdle = $False
-                    Write-Host `nSurvey: -ForegroundColor $Host.PrivateData.VerboseBackgroundColor -BackgroundColor $host.PrivateData.VerboseForegroundColor -NoNewLine; Write-Host ' How was your experience using the Az Predictor module?'
+                    Write-Host ''; Write-Host `Survey: -ForegroundColor $Host.PrivateData.VerboseBackgroundColor -BackgroundColor $host.PrivateData.VerboseForegroundColor -NoNewLine; Write-Host ' How was your experience using the Az Predictor module?'
                     Write-Host ''
                     Write-Host 'Run ' -NoNewline; Write-Host Open-AzPredictorSurvey -ForegroundColor $Host.PrivateData.VerboseBackgroundColor -BackgroundColor $host.PrivateData.VerboseForegroundColor -NoNewline; Write-Host ' to give us your feedback.'
                     Write-Host '(press Enter to continue)'
