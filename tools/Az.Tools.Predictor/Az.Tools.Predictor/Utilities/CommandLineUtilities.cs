c71dfdf5f017e453dd1f62b7d0fffc702c2c91f0
Maoliang Huang <mahuang@microsoft.com>
2021-10-20 12:00:01 -0700

    Fix an issue that we get the wrong CommandAst
    - When we get the CommandAst to mask, we get the last found CommandAst which may not be what we're looking for. For example,
      we replace <> with {} in the suggestion list and if that command is executed, we'll get the content inside {} as the
      CommandAst, while we're looking for the whole command line.
    - The change is to get the first CommandAst. This doesn't fix all the issues that we already face but it's a little bit
      progress.

--- tools/Az.Tools.Predictor/Az.Tools.Predictor/Utilities/CommandLineUtilities.cs
+++ tools/Az.Tools.Predictor/Az.Tools.Predictor/Utilities/CommandLineUtilities.cs
@@ -24,12 +24,44 @@ namespace Microsoft.Azure.PowerShell.Tools.AzPredictor.Utilities
     internal static class CommandLineUtilities
     {
         /// <summary>
+        /// Gets the CommandAst for the whole command line.
+        /// </summary>
+        /// <param name="commandLine">The command line to get the CommandAst.</param>
+        /// <returns>The CommandAst.</returns>
+        /// <remarks>This parses the command line and returns the first one it encounters. It doesn't work well in a complex command line, for example: <c>Get-AzContext | Set-AzContext</c> will return <c>Get-AzContext</c>.</remarks>
+        public static CommandAst GetCommandAst(string commandLine)
+        {
+            if (string.IsNullOrWhiteSpace(commandLine))
+            {
+                return null;
+            }
+
+            Ast ast = Parser.ParseInput(commandLine, out _, out _);
+            var commandAst = ast.Find((ast) => ast is CommandAst, searchNestedScriptBlocks: false) as CommandAst;
+            return commandAst;
+        }
+
+        /// <summary>
         /// Masks the user input of any data, like names and locations.
         /// Also alphabetizes the parameters to normalize them before sending
         /// them to the model.
         /// e.g., Get-AzContext -Name Hello -Location 'EastUS' => Get-AzContext -Location *** -Name ***
         /// </summary>
-        /// <param name="cmdAst">The last user input command.</param>
+        /// <param name="commandLine">The command line to mask.</param>
+        public static string MaskCommandLine(string commandLine)
+        {
+            var commandAst = CommandLineUtilities.GetCommandAst(commandLine);
+
+            return CommandLineUtilities.MaskCommandLine(commandAst);
+        }
+
+        /// <summary>
+        /// Masks the user input of any data, like names and locations.
+        /// Also alphabetizes the parameters to normalize them before sending
+        /// them to the model.
+        /// e.g., Get-AzContext -Name Hello -Location 'EastUS' => Get-AzContext -Location *** -Name ***
+        /// </summary>
+        /// <param name="cmdAst">The command to mask.</param>
         public static string MaskCommandLine(CommandAst cmdAst)
         {
             var commandElements = cmdAst?.CommandElements;
