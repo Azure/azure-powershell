b3a42099ab60b1e9ca495f22240a094ce5bcc810
Beisi Zhou <zazbs@qq.com>
2021-10-22 14:09:37 +0800

    Move WebPubSub to main (#16136)
    Co-authored-by: azure-powershell-bot <65331932+azure-powershell-bot@users.noreply.github.com>

---
+++ src/WebPubSub/custom/csharp/UpdateAzWebPubSub_UpdateViaIdentityExpanded.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.PowerShell.Cmdlets.WebPubSub.Models.Api20;
+
+namespace Microsoft.Azure.PowerShell.Cmdlets.WebPubSub.Cmdlets
+{
+    public partial class UpdateAzWebPubSub_UpdateViaIdentityExpanded
+    {
+        partial void overrideOnDefault(HttpResponseMessage responseMessage, Task<IErrorResponse> errorResponseTask, ref Task<bool> returnNow)
+        {
+            this.WriteError(responseMessage, errorResponseTask, ref returnNow);
+        }
+    }
+}
