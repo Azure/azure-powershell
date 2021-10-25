6536822198df8e3e7cb14716f2b3bf1fdc717540
Roger Best <rbest@microsoft.com>
2021-10-25 13:54:06 -0700

    Fix update user and tests for add user quota

--- src/LabServices/test/Add-AzLabServicesUserQuota.Tests.ps1
+++ src/LabServices/test/Add-AzLabServicesUserQuota.Tests.ps1
@@ -19,15 +19,15 @@ $loadVarsPath = Join-Path $PSScriptRoot '\SetVariables.ps1'
 
 Describe 'Add-AzLabServicesUserQuota' {
     It 'Add User Quota Email' {
-        $addTime = New-TimeSpan -Hours 12
-        Add-AzLabServicesUserQuota -SubscriptionId $($ENV:SubscriptionId) -ResourceGroupName $($ENV:ResourceGroupName) -LabName $($ENV:LabName) -Email $($ENV:UserEmail) -UsageQuotaToAddToExisting $addTime | Should -Not -BeNullOrEmpty
-        Get-AzLabServicesUser -SubscriptionId $($ENV:SubscriptionId) -LabName $($ENV:LabName) -ResourceGroupName $($ENV:ResourceGroupName) -UserName $($ENV:UserName) | Select -ExpandProperty additionalUsageQuota | Should -BeExactly $addTime
+        #$addTime = New-TimeSpan -Hours 12
+        Add-AzLabServicesUserQuota -SubscriptionId $($ENV:SubscriptionId) -ResourceGroupName $($ENV:ResourceGroupName) -LabName $($ENV:LabName) -Email $($ENV:UserEmail) -UsageQuotaToAddToExisting "12:00:00" | Should -Not -BeNullOrEmpty
+        Get-AzLabServicesUser -SubscriptionId $($ENV:SubscriptionId) -LabName $($ENV:LabName) -ResourceGroupName $($ENV:ResourceGroupName) -UserName $($ENV:UserName) | Select -ExpandProperty additionalUsageQuota | Should -BeExactly "12:00:00"
     }
 
     It 'Add User Quota Object' {
-        $addTime = New-TimeSpan -Hours 13
+        #$addTime = New-TimeSpan -Hours 13
         $user = Get-AzLabServicesUser -LabName $ENV:LabName -ResourceGroupName $ENV:ResourceGroupName -UserName $ENV:UserName
-        Add-AzLabServicesUserQuota -User $user -UsageQuotaToAddToExisting $addTime | Should -Not -BeNullOrEmpty
-        Get-AzLabServicesUser -LabName $ENV:LabName -ResourceGroupName $ENV:ResourceGroupName -UserName $ENV:UserName | Select -ExpandProperty additionalUsageQuota | Should -BeExactly $(New-TimeSpan -Hours 25)
+        Add-AzLabServicesUserQuota -User $user -UsageQuotaToAddToExisting "13:00:00" | Should -Not -BeNullOrEmpty
+        Get-AzLabServicesUser -LabName $ENV:LabName -ResourceGroupName $ENV:ResourceGroupName -UserName $ENV:UserName | Select -ExpandProperty additionalUsageQuota | Should -BeExactly "1.01:00:00"
     }
 }
