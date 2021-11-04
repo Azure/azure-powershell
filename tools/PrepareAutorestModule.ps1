907aface638df4caaf29c7b5bb0d692f445e9c70
Xiaogang <xidi@microsoft.com>
2021-11-04 11:28:14 +0800

    [Do not merge] test by xiaogang

--- tools/PrepareAutorestModule.ps1
+++ tools/PrepareAutorestModule.ps1
@@ -41,7 +41,7 @@ foreach ($file in $ChangedFiles)
     }
     else
     {
-        $NUll = $ModuleSet.Add($ALL_MODULE)
+        $NUll = $ModuleSet.Add("KubernetesConfiguration")
     }
 }
 if ($ModuleSet.Contains($ALL_MODULE))
