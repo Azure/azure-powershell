5efb8ee69b7139b90d472457ce5cef58771cd023
Serena Zhang <sezh@microsoft.com>
2021-11-10 00:34:12 -0800

    Upgrade monitor version for Sql and Synapse modules

--- src/Synapse/Synapse.Test/ScenarioTests/SynapseTestBase.cs
+++ src/Synapse/Synapse.Test/ScenarioTests/SynapseTestBase.cs
@@ -103,7 +103,8 @@ namespace Microsoft.Azure.Commands.Synapse.Test.ScenarioTests
                 {"Microsoft.Resources", null},
                 {"Microsoft.Features", null},
                 {"Microsoft.Authorization", null},
-                {"Microsoft.EventHub", null}
+                {"Microsoft.EventHub", null},
+                {"Microsoft.Ingishts", null}
             };
             var providersToIgnore = new Dictionary<string, string>
             {
