d41a8964fb86aa5f1792d1f0480b68e0b8604445
lijinpei2008 <31384087+lijinpei2008@users.noreply.github.com>
2021-11-12 15:03:39 +0800

    [ConnectedKubernetes] Update connected kubernetes api version 2021-10-01 (#16303)
    * update connected kubernetes api version 2021-10-01
    
    * update
    
    * create test case
    
    * create test case
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * re-name cmdlet
    
    * update
    
    * correct some issue
    
    * update

--- src/ConnectedKubernetes/test/Get-AzConnectedKubernetesUserCredential.Tests.ps1
+++ src/ConnectedKubernetes/test/Get-AzConnectedKubernetesUserCredential.Tests.ps1
@@ -1,11 +1,11 @@
-if(($null -eq $TestName) -or ($TestName -contains 'Get-AzConnectedKubernetes'))
+if(($null -eq $TestName) -or ($TestName -contains 'Get-AzConnectedKubernetesUserCredential'))
 {
   $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
   if (-Not (Test-Path -Path $loadEnvPath)) {
       $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
   }
   . ($loadEnvPath)
-  $TestRecordingFile = Join-Path $PSScriptRoot 'Get-AzConnectedKubernetes.Recording.json'
+  $TestRecordingFile = Join-Path $PSScriptRoot 'Get-AzConnectedKubernetesUserCredential.Recording.json'
   $currentPath = $PSScriptRoot
   while(-not $mockingPath) {
       $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
@@ -14,20 +14,12 @@ if(($null -eq $TestName) -or ($TestName -contains 'Get-AzConnectedKubernetes'))
   . ($mockingPath | Select-Object -First 1).FullName
 }
 
-Describe 'Get-AzConnectedKubernetes' {
-    It 'List1' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
-    }
-
-    It 'Get' -skip {
+Describe 'Get-AzConnectedKubernetesUserCredential' {
+    It 'ListExpanded' -skip {
         { throw [System.NotImplementedException] } | Should -Not -Throw
     }
 
     It 'List' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
-    }
-
-    It 'GetViaIdentity' -skip {
         { throw [System.NotImplementedException] } | Should -Not -Throw
     }
 }
