c1d8769bf40ab79e9438248ada31d2e02612b514
Yan jun Gao <v-yanjungao@microsoft.com>
2021-11-26 12:16:12 +0800

    update psmodel sync up with sdk

--- src/Synapse/Synapse/Models/DataPlaneModels/ManagedPrivateEndpoints/PSManagedPrivateEndpointProperties.cs
+++ src/Synapse/Synapse/Models/DataPlaneModels/ManagedPrivateEndpoints/PSManagedPrivateEndpointProperties.cs
@@ -13,6 +13,7 @@
 // ----------------------------------------------------------------------------------
 
 using Azure.Analytics.Synapse.ManagedPrivateEndpoints.Models;
+using System.Collections.Generic;
 
 namespace Microsoft.Azure.Commands.Synapse.Models
 {
@@ -20,21 +21,54 @@ namespace Microsoft.Azure.Commands.Synapse.Models
     {
         public PSManagedPrivateEndpointProperties(ManagedPrivateEndpointProperties properties)
         {
+            this.Name = properties?.Name;
             this.PrivateLinkResourceId = properties?.PrivateLinkResourceId;
             this.GroupId = properties?.GroupId;
             this.ProvisioningState = properties?.ProvisioningState;
             this.IsReserved = properties?.IsReserved;
             this.ConnectionState = properties?.ConnectionState != null? new PSManagedPrivateEndpointConnectionState(properties?.ConnectionState) : null;
-        }      
+            this.Fqdns = properties?.Fqdns;
+            this.IsCompliant = properties?.IsCompliant;
+        }
 
+        /// <summary>
+        /// The name of managed private endpoint.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The ARM resource ID of the resource to which the managed private endpoint is created.
+        /// </summary>
         public string PrivateLinkResourceId { get; set; }
 
+        /// <summary>
+        /// The groupId to which the managed private endpoint is created.
+        /// </summary>
         public string GroupId { get; set; }
 
+        /// <summary>
+        /// The managed private endpoint provisioning state.
+        /// </summary>
         public string ProvisioningState { get; }
 
+        /// <summary>
+        /// The managed private endpoint connection state.
+        /// </summary>
         public PSManagedPrivateEndpointConnectionState ConnectionState { get; set; }
 
+        /// <summary>
+        /// Denotes whether the managed private endpoint is reserved.
+        /// </summary>
         public bool? IsReserved { get; }
+
+        /// <summary>
+        /// List of fully qualified domain names.
+        /// </summary>
+        public IList<string> Fqdns { get; }
+
+        /// <summary>
+        /// Denotes whether the managed private endpoint is compliant.
+        /// </summary>
+        public bool? IsCompliant { get; set; }
     }
 }
