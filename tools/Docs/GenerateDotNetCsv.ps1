221566e2b8288455717f8fd289e3339771353171
bganapa <bganapa@microsoft.com>
2021-12-06 17:24:39 -0800

    Copying the target CopyAboutTopics target from main

---
+++ tools/Docs/GenerateDotNetCsv.ps1
@@ -0,0 +1,40 @@
+# ----------------------------------------------------------------------------------
+#
+# Copyright Microsoft Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------------
+<#
+.SYNOPSIS Generates az-ps-latest.csv as the feed of online csharp reference publishing according to feed psd1.
+#>
+Param(
+    [Parameter(Mandatory = $true)]
+    [string]$FeedPsd1FullPath,
+    [Parameter(Mandatory = $false)]
+    [string]$CustomSource = "https://www.powershellgallery.com/api/v2/"
+)
+
+$feedDir = (Get-Item $FeedPsd1FullPath).Directory
+$feedName = (Get-Item $FeedPsd1FullPath).Name
+Import-LocalizedData -BindingVariable ModuleMetadata -BaseDirectory $feedDir -FileName $feedName
+$modules = $ModuleMetadata.RequiredModules
+
+$dotnetCsv = New-Item -Path "$PSScriptRoot\" -Name "az-ps-latest.csv" -ItemType "file" -Force
+$dotnetCsvContent = ""
+for ($index = 0; $index -lt $modules.Count; $index++){
+    $moduleName = $modules[$index].ModuleName
+    $moduleVersion = [string]::IsNullOrEmpty($modules[$index].RequiredVersion) ? $modules[$index].ModuleVersion : $modules[$index].RequiredVersion
+    $dotnetCsvContent += "pac$index,[ps=true;customSource=$CustomSource]$moduleName,$moduleVersion`n"
+}
+Set-Content -Path $dotnetCsv.FullName -Value $dotnetCsvContent -Encoding UTF8
+
+
+
+
