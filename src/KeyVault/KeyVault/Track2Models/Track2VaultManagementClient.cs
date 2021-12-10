bc2ad0f1aadc143ba12a1f4d88ded815b84f2b6a
Beisi Zhou <bez@microsoft.com>
2021-12-10 17:30:10 +0800

    wip

---
+++ src/KeyVault/KeyVault/Track2Models/Track2VaultManagementClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.Commands.KeyVault.Track2Models
+{
+    public class Track2VaultManagementClient
+    {
+        private Track2VaultCollection VaultCollection => _vaultCollection ?? (_vaultCollection = new Track2VaultCollection());
+        private Track2VaultCollection _vaultCollection;
+
+        private Track2ManagedHsmCollection ManagedHsmCollection => _managedHsmCollection ?? (_managedHsmCollection = new Track2ManagedHsmCollection());
+        private Track2ManagedHsmCollection _managedHsmCollection;
+
+        public void test()
+        {
+            
+        }
+    }
+}
