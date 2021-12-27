dc31b0468ac7bbea25ca08d80aa3aed0a54332ef
Yunchi Wang <54880216+wyunchi-ms@users.noreply.github.com>
2021-12-27 18:02:35 +0800

    Update PrepareAutorestModule.ps1

--- tools/PrepareAutorestModule.ps1
+++ tools/PrepareAutorestModule.ps1
@@ -69,7 +69,7 @@ Add-Content -Path .git/info/sparse-checkout -Value "tools/"
 git pull origin main
 Move-Item -Path "$TmpFolder\src\Accounts" -Destination "$TmpFolder\Accounts" -Force
 Copy-Item "$TmpFolder\Accounts" "$PSScriptRoot\..\src" -Recurse -Force
-Remove-Item -Path "$TmpFolder\src" -Recursemove -Force
+Remove-Item -Path "$TmpFolder\src" -Recurse -Force
 Move-Item -Path "$TmpFolder\tools\Common*.targets" -Destination "$PSScriptRoot\..\tools" -Force
 Remove-Item -Path "$TmpFolder\tools" -Recurse -Force
 Install-Module Az.Accounts -Repository PSGallery -Force
