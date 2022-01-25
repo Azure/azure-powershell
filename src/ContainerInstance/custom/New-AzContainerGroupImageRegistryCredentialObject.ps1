b1915ef1f4019e0b0757d330a1d0f7c7f6d5e8be
wyunchi-ms <yunwang@microsoft.com>
2022-01-25 13:55:07 +0800

    Remove useless field in customization code.

--- src/ContainerInstance/custom/New-AzContainerGroupImageRegistryCredentialObject.ps1
+++ src/ContainerInstance/custom/New-AzContainerGroupImageRegistryCredentialObject.ps1
@@ -18,9 +18,6 @@
 Create a in-memory object for ImageRegistryCredential
 .Description
 Create a in-memory object for ImageRegistryCredential
-
-.Outputs
-Microsoft.Azure.PowerShell.Cmdlets.ContainerInstance.Models.Api20210901.ImageRegistryCredential
 .Link
 https://docs.microsoft.com/powershell/module/az.ContainerInstance/new-AzContainerGroupImageRegistryCredentialObject
 #>
