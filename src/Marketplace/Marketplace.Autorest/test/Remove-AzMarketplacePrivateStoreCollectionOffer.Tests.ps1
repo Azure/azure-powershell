cfd1dd26a6689744fd431895f02e631a21a37bdb
ilavi19 <92529277+ilavi19@users.noreply.github.com>
2022-01-27 07:28:40 +0200

    New AzMarketplace Cmdlets (#16817)
    * marketplace commands
    
    marketplace commands
    
    * example and docs
    
    example and docs
    
    * changed verbs types
    
    * change parameter names to singular  shorten mapping to subscription command name
    
    * Update .gitignore
    
    * Delete test-module.ps1
    
    * Delete run-module.ps1
    
    * Delete pack-module.ps1
    
    * Delete generate-help.ps1
    
    * Delete export-surface.ps1
    
    * Delete src/Marketplace/generated directory
    
    * Delete src/Marketplace/exports directory
    
    * Delete src/Marketplace/internal directory
    
    * Delete src/Marketplace/tools/Resources directory
    
    * Delete Az.Marketplace.csproj
    
    * Delete create-model-cmdlets.ps1
    
    * Delete check-dependencies.ps1
    
    * Delete build-module.ps1
    
    * Delete MSSharedLibKey.snk
    
    * Delete Az.Marketplace.format.ps1xml
    
    * Delete Az.Marketplace.nuspec
    
    * Delete Az.Marketplace.psm1
    
    * Update .gitignore
    
    * Delete Az.Marketplace.custom.psm1
    
    * remove Cmdlets files that the name were changed
    
    * fix copy offer docs file
    
    * only copy offer
    
    * update copy offer
    
    * Update Copy-AzMarketplacePrivateStoreCollectionOffer.md
    
    * fix examples in docs file -closed blocks
    
    * fix examples file code blocks
    
    * add breaking changes
    
    * changed folder structure and removed breaking changes while changing Cmdlts names
    
    * change get privateStore to V1
    
    * remove tabs from readme
    
    * re-recorded tests
    
    * Update path
    
    Co-authored-by: Yunchi Wang <54880216+wyunchi-ms@users.noreply.github.com>
    Co-authored-by: wyunchi-ms <yunwang@microsoft.com>

---
+++ src/Marketplace/Marketplace.Autorest/test/Remove-AzMarketplacePrivateStoreCollectionOffer.Tests.ps1
@@ -0,0 +1,26 @@
+if(($null -eq $TestName) -or ($TestName -contains 'Remove-AzMarketplacePrivateStoreCollectionOffer'))
+{
+  $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+  if (-Not (Test-Path -Path $loadEnvPath)) {
+      $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+  }
+  . ($loadEnvPath)
+  $TestRecordingFile = Join-Path $PSScriptRoot 'Remove-AzMarketplacePrivateStoreCollectionOffer.Recording.json'
+  $currentPath = $PSScriptRoot
+  while(-not $mockingPath) {
+      $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+      $currentPath = Split-Path -Path $currentPath -Parent
+  }
+  . ($mockingPath | Select-Object -First 1).FullName
+}
+
+Describe 'Remove-AzMarketplacePrivateStoreCollectionOffer' {
+    It 'Delete'  {
+       Remove-AzMarketplacePrivateStoreCollectionOffer -PrivateStoreId a260d38c-96cf-492d-a340-404d0c4b3ad6 -CollectionId fdb889a1-cf3e-49f0-95b8-2bb012fa01f1 -OfferId aumatics.azure_managedservices
+
+	try{Get-AzMarketplacePrivateStoreCollectionOffer -PrivateStoreId a260d38c-96cf-492d-a340-404d0c4b3ad6 -CollectionId fdb889a1-cf3e-49f0-95b8-2bb012fa01f1 -OfferId aumatics.azure_managedservices} catch {$Error[0] | Should -Match "aumatics.azure_managedservices does not exist"}
+
+    }
+
+    
+}
