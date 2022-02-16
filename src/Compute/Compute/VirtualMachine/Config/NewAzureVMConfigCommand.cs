32039937cb2d8df66c2035d9b21209f5544a8c3e
Adam Sandor <adsandor@microsoft.com>
2022-02-16 17:06:42 -0500

    cleanup comments

--- src/Compute/Compute/VirtualMachine/Config/NewAzureVMConfigCommand.cs
+++ src/Compute/Compute/VirtualMachine/Config/NewAzureVMConfigCommand.cs
@@ -162,14 +162,12 @@ namespace Microsoft.Azure.Commands.Compute
         public SwitchParameter HibernationEnabled { get; set; }
 
         [Parameter(
-            //ParameterSetName = DiskFileParameterSet,
             Mandatory = false,
             ValueFromPipelineByPropertyName = true,
             HelpMessage = "Specifies the number of vCPUs available for the VM. When this property is not specified in the request body the default behavior is to set it to the value of vCPUs available for that VM size exposed in api response of [List all available virtual machine sizes in a region](https://docs.microsoft.com/en-us/rest/api/compute/resource-skus/list).")]
         public int vCPUsAvailable { get; set; }
 
         [Parameter(
-            //ParameterSetName = DiskFileParameterSet,
             Mandatory = false,
             ValueFromPipelineByPropertyName = true,
             HelpMessage = "Specifies the vCPU to physical core ratio. <br><br> When this property is not specified in the request body the default behavior is set to the value of vCPUsPerCore for the VM Size exposed in api response of [List all available virtual machine sizes in a region](https://docs.microsoft.com/en-us/rest/api/compute/resource-skus/list) <br><br> Setting this property to 1 also means that hyper-threading is disabled.")]
