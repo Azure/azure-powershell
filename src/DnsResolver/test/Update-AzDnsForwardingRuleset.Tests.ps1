3f06e3f725245f721f707e9b12b6a8e11d3d4e41
arpit-gagneja <gagneja.arpit@gmail.com>
2022-02-17 19:15:05 -0800

    Added patch tests for resources (#17165)
    * Added tests for PATCH DNS resolver and inbound endpoint
    
    * Added test for patch outbound endpoint
    
    * Added tests for patch DNS forwarding ruleset
    
    * Added test for patch forwarding rule
    
    * Added tests for patch virtual network link
    
    * Added tests for get DNS resolver
    
    * Added tests for remove DNS resolver
    
    Co-authored-by: Arpit Gagneja <argagnej@microsoft.com>

--- src/DnsResolver/test/Update-AzDnsForwardingRuleset.Tests.ps1
+++ src/DnsResolver/test/Update-AzDnsForwardingRuleset.Tests.ps1
@@ -1,3 +1,10 @@
+."$PSScriptRoot\testDataGenerator.ps1"
+."$PSScriptRoot\virtualNetworkClient.ps1"
+."$PSScriptRoot\dnsForwardingRulesetAssertions.ps1"
+."$PSScriptRoot\Constants.ps1"
+
+Add-AssertionOperator -Name 'BeSuccessfullyCreatedDnsForwardingRuleset' -Test $Function:BeSuccessfullyCreatedDnsForwardingRuleset
+
 $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
 if (-Not (Test-Path -Path $loadEnvPath)) {
     $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
@@ -12,11 +19,31 @@ while(-not $mockingPath) {
 . ($mockingPath | Select-Object -First 1).FullName
 
 Describe 'Update-AzDnsForwardingRuleset' {
-    It 'UpdateExpanded' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
-    }
+    It 'Update DNS forwarding ruleset by adding tag, expect DNS forwarding ruleset updated' {
+        # ARRANGE
+        $dnsResolverName = "psdnsresolvername54";
+        $outboundEndpointName = "psoutboundendpointname54";
+        $dnsForwardingRulesetName = "psdnsforwardingrulesetname54";
+        $virtualNetworkName = "psvirtualnetworkname54";
 
-    It 'UpdateViaIdentityExpanded' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+        if ($TestMode -eq "Record")
+        {
+            $virtualNetwork = CreateVirtualNetwork -SubscriptionId $SUBSCRIPTION_ID -ResourceGroupName $RESOURCE_GROUP_NAME -VirtualNetworkName $virtualNetworkName;
+            $subnet = CreateSubnet -SubscriptionId $SUBSCRIPTION_ID -ResourceGroupName $RESOURCE_GROUP_NAME -VirtualNetworkName $virtualNetworkName;
+        }
+
+        New-AzDnsResolver -Name $dnsResolverName -ResourceGroupName $RESOURCE_GROUP_NAME -VirtualNetworkId $virtualNetwork.Id -Location $LOCATION
+        $outboundEndpoint = New-AzDnsResolverOutboundEndpoint -DnsResolverName $dnsResolverName -Name $outboundEndpointName -ResourceGroupName $RESOURCE_GROUP_NAME -SubnetId $subnet.Id -Location $LOCATION
+
+        New-AzDnsForwardingRuleset -Name $dnsForwardingRulesetName -ResourceGroupName $RESOURCE_GROUP_NAME -Location $LOCATION -DnsResolverOutboundEndpoint  @{id = $outboundEndpoint.Id;}
+        
+        $tag  = GetRandomHashtable -size 5
+
+        # ACT
+        $updatedDnsForwardingRuleset = Update-AzDnsForwardingRuleset -Name $dnsForwardingRulesetName -ResourceGroupName $RESOURCE_GROUP_NAME -Tag $tag
+
+        # ASSERT
+        $updatedDnsForwardingRuleset | Should -BeSuccessfullyCreatedDnsForwardingRuleset
+        $updatedDnsForwardingRuleset.Tag.Count | Should -Be $tag.Count
     }
 }
