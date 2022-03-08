5ebb8922b5dea29bdbcedd28c3963fcb5e257c31
Santosh Kulkarni <66682828+kr-santosh@users.noreply.github.com>
2022-03-08 17:01:09 +0530

    Revert "Updated Mongo session records"
    This reverts commit 37dcc0ee8b896f01831591d6aac9951008acca7c.

--- src/CosmosDB/CosmosDB.Test/ScenarioTests/TestController.cs
+++ src/CosmosDB/CosmosDB.Test/ScenarioTests/TestController.cs
@@ -28,6 +28,7 @@ using Microsoft.WindowsAzure.Commands.ScenarioTest;
 using Microsoft.WindowsAzure.Commands.Test.Utilities.Common;
 using TestEnvironmentFactory = Microsoft.Rest.ClientRuntime.Azure.TestFramework.TestEnvironmentFactory;
 using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Commands.Common.MSGraph.Version1_0;
 using Microsoft.Azure.Commands.Common.KeyVault.Version2016_10_1;
 
 namespace Microsoft.Azure.Commands.CosmosDB.Test.ScenarioTests.ScenarioTest
