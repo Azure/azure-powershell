6a3ac2f0ed0811e2f98f9733e888d91626dfe8b3
Santosh Kulkarni <66682828+kr-santosh@users.noreply.github.com>
2022-03-09 14:43:18 +0530

    Revert "updates"
    This reverts commit 6bdf39df167bd99ed66167931868cc15478ea015.

--- src/CosmosDB/CosmosDB.Test/ScenarioTests/TestController.cs
+++ src/CosmosDB/CosmosDB.Test/ScenarioTests/TestController.cs
@@ -28,6 +28,7 @@ using Microsoft.WindowsAzure.Commands.ScenarioTest;
 using Microsoft.WindowsAzure.Commands.Test.Utilities.Common;
 using TestEnvironmentFactory = Microsoft.Rest.ClientRuntime.Azure.TestFramework.TestEnvironmentFactory;
 using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Commands.Common.MSGraph.Version1_0;
 using Microsoft.Azure.Commands.Common.KeyVault.Version2016_10_1;
 
 namespace Microsoft.Azure.Commands.CosmosDB.Test.ScenarioTests.ScenarioTest
@@ -168,7 +169,6 @@ namespace Microsoft.Azure.Commands.CosmosDB.Test.ScenarioTests.ScenarioTest
         {
             return context.GetServiceClient<KeyVaultManagementClient>(TestEnvironmentFactory.GetTestEnvironment());
         }
-
         private CosmosDBTestHelper GetTestHelper()
         {
             return new CosmosDBTestHelper(this.KeyVaultManagementClient, this.KeyVaultClient);
