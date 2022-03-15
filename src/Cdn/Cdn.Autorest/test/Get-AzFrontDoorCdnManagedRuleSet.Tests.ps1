3d114177460a31e466181336e1744c0b2d2d6e46
Chenglong Liu <chengll@microsoft.com>
2022-03-15 13:10:57 +0800

    Update

--- src/Cdn/Cdn.Autorest/test/Get-AzFrontDoorCdnManagedRuleSet.Tests.ps1
+++ src/Cdn/Cdn.Autorest/test/Get-AzFrontDoorCdnManagedRuleSet.Tests.ps1
@@ -1,11 +1,11 @@
-if(($null -eq $TestName) -or ($TestName -contains 'Get-AzCdnManagedRuleSet'))
+if(($null -eq $TestName) -or ($TestName -contains 'Get-AzFrontDoorCdnManagedRuleSet'))
 {
   $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
   if (-Not (Test-Path -Path $loadEnvPath)) {
       $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
   }
   . ($loadEnvPath)
-  $TestRecordingFile = Join-Path $PSScriptRoot 'Get-AzCdnManagedRuleSet.Recording.json'
+  $TestRecordingFile = Join-Path $PSScriptRoot 'Get-AzFrontDoorCdnManagedRuleSet.Recording.json'
   $currentPath = $PSScriptRoot
   while(-not $mockingPath) {
       $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
@@ -14,7 +14,7 @@ if(($null -eq $TestName) -or ($TestName -contains 'Get-AzCdnManagedRuleSet'))
   . ($mockingPath | Select-Object -First 1).FullName
 }
 
-Describe 'Get-AzCdnManagedRuleSet' {
+Describe 'Get-AzFrontDoorCdnManagedRuleSet' {
     It 'List' -skip {
         { throw [System.NotImplementedException] } | Should -Not -Throw
     }
--- src/Cdn/Cdn.Autorest/test/New-AzFrontDoorCdnOrigin.Tests.ps1
+++ src/Cdn/Cdn.Autorest/test/New-AzFrontDoorCdnOrigin.Tests.ps1
@@ -15,7 +15,39 @@ if(($null -eq $TestName) -or ($TestName -contains 'New-AzFrontDoorCdnOrigin'))
 }
 
 Describe 'New-AzFrontDoorCdnOrigin' {
-    It 'CreateExpanded' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+    It 'CreateExpanded' {
+        $ResourceGroupName = 'testps-rg-' + (RandomString -allChars $false -len 6)
+        try
+        {
+            Write-Host -ForegroundColor Green "Create test group $($ResourceGroupName)"
+            New-AzResourceGroup -Name $ResourceGroupName -Location $env.location
+
+            $frontDoorCdnProfileName = 'fdp-' + (RandomString -allChars $false -len 6);
+            Write-Host -ForegroundColor Green "Use frontDoorCdnProfileName : $($frontDoorCdnProfileName)"
+
+            $profileSku = "Standard_AzureFrontDoor";
+            New-AzFrontDoorCdnProfile -SkuName $profileSku -Name $frontDoorCdnProfileName -ResourceGroupName $ResourceGroupName -Location Global
+
+            $originGroupName = 'org' + (RandomString -allChars $false -len 6);
+            New-AzFrontDoorCdnOriginGroup -OriginGroupName $originGroupName -ProfileName $frontDoorCdnProfileName -ResourceGroupName $ResourceGroupName `
+            -LoadBalancingSettingSampleSize 5 `
+            -LoadBalancingSettingSuccessfulSamplesRequired 4 `
+            -LoadBalancingSettingAdditionalLatencyInMillisecond 200 `
+            -HealthProbeSettingProbeIntervalInSecond 1 `
+            -HealthProbeSettingProbePath "/" `
+            -HealthProbeSettingProbeProtocol $([Microsoft.Azure.PowerShell.Cmdlets.Cdn.Support.ProbeProtocol]::Https) `
+            -HealthProbeSettingProbeRequestType $([Microsoft.Azure.PowerShell.Cmdlets.Cdn.Support.HealthProbeRequestType]::Get) `
+
+            Get-AzFrontDoorCdnOriginGroup -ResourceGroupName $ResourceGroupName -ProfileName $frontDoorCdnProfileName -OriginGroupName $originGroupName
+
+            $hostName = "en.wikipedia.org";
+            $originName = 'ori' + (RandomString -allChars $false -len 6);
+            New-AzFrontDoorCdnOrigin -ResourceGroupName $ResourceGroupName -ProfileName $frontDoorCdnProfileName -OriginGroupName $originGroupName `
+            -OriginName $originName -OriginHostHeader $hostName -HostName $hostName `
+            -HttpPort 80 -HttpsPort 443 -Priority 1 -Weight 1000
+        } Finally
+        {
+            Remove-AzResourceGroup -Name $ResourceGroupName -NoWait
+        }
     }
 }
