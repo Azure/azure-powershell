c35dc1f06baf0c61cc772b68dbfa826ccdc1e4fd
Dingmeng Xue <dixue@microsoft.com>
2022-03-17 09:57:12 +0800

    Upgrade Azure.Core to 1.22.0 (#17502)
    * Upgrade Azure.Core to 1.22.0
    
    * Update doc
    
    * update doc
    
    * update doc
    
    * update

--- src/Accounts/Authentication/Utilities/CustomAssemblyResolver.cs
+++ src/Accounts/Authentication/Utilities/CustomAssemblyResolver.cs
@@ -24,8 +24,8 @@ namespace Microsoft.Azure.Commands.Profile.Utilities
         private static IDictionary<string, Version> NetFxPreloadAssemblies =
             new Dictionary<string, Version>(StringComparer.InvariantCultureIgnoreCase)
             {
-                {"Azure.Core", new Version("1.21.0.0")},
-                {"Microsoft.Bcl.AsyncInterfaces", new Version("1.0.0.0")},
+                {"Azure.Core", new Version("1.22.0.0")},
+                {"Microsoft.Bcl.AsyncInterfaces", new Version("1.1.1.0")},
                 {"Microsoft.Identity.Client", new Version("4.30.1.0") },
                 {"Microsoft.Identity.Client.Extensions.Msal", new Version("2.18.4.0") },
                 {"Microsoft.IdentityModel.Clients.ActiveDirectory", new Version("3.19.2.6005")},
@@ -46,7 +46,7 @@ namespace Microsoft.Azure.Commands.Profile.Utilities
                 {"System.Security.Principal.Windows", new Version("4.1.1.0")},
                 {"System.ServiceModel.Primitives", new Version("4.7.0.0")}, //used by Compute
                 {"System.Text.Encodings.Web", new Version("4.0.5.1")},
-                {"System.Text.Json", new Version("4.0.0.0")},
+                {"System.Text.Json", new Version("4.0.1.2")},
                 {"System.Threading.Tasks.Extensions", new Version("4.2.0.1")},
                 {"System.Xml.ReaderWriter", new Version("4.1.0.0")}
             };
