2db4d968a8dbbcca55ca5d58668a04b42dcdc249
yanfa317 <53584318+yanfa317@users.noreply.github.com>
2022-03-18 13:13:35 -0700

    Fix help doc

--- src/Network/Network/NetworkManager/NetworkGroup/NetworkManagerStaticMember/NewAzNetworkManagerStaticMemberCommand.cs
+++ src/Network/Network/NetworkManager/NetworkGroup/NetworkManagerStaticMember/NewAzNetworkManagerStaticMemberCommand.cs
@@ -63,7 +63,7 @@ namespace Microsoft.Azure.Commands.Network
         public virtual string ResourceGroupName { get; set; }
 
         [Parameter(
-           Mandatory = false,
+           Mandatory = true,
            ValueFromPipelineByPropertyName = true,
            HelpMessage = "Resource Id.")]
         public string ResourceId { get; set; }
