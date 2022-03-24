8efa0f96148d79a08ab04ac3a5eb35b06e077b6d
Yeming Liu <11371776+isra-fel@users.noreply.github.com>
2022-03-24 16:38:40 +0800

    Modified code from Microsot.Extensions.Configuration

---
+++ src/Accounts/Authentication/Config/Internal/ConfigurationProvider.cs
@@ -0,0 +1,97 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Commands.Common.Authentication.Config.Internal.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.Azure.Commands.Common.Authentication.Config.Internal
+{
+    /// <summary>
+    /// Base helper class for implementing an <see cref="IConfigurationProvider"/>
+    /// </summary>
+    internal abstract class ConfigurationProvider : IConfigurationProvider
+    {
+        public string Id { get; }
+
+        /// <summary>
+        /// Initializes a new <see cref="IConfigurationProvider"/>
+        /// </summary>
+        protected ConfigurationProvider(string id)
+        {
+            Id = id;
+            Data = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// The configuration key value pairs for this provider.
+        /// </summary>
+        protected IDictionary<string, string> Data { get; set; }
+
+        /// <summary>
+        /// Attempts to find a value with the given key, returns true if one is found, false otherwise.
+        /// </summary>
+        /// <param name="key">The key to lookup.</param>
+        /// <param name="value">The value found at key if one is found.</param>
+        /// <returns>True if key has a value, false otherwise.</returns>
+        public virtual bool TryGet(string key, out string value)
+            => Data.TryGetValue(key, out value);
+
+        /// <summary>
+        /// Sets a value for a given key.
+        /// </summary>
+        /// <param name="key">The configuration key to set.</param>
+        /// <param name="value">The value to set.</param>
+        public virtual void Set(string key, string value)
+            => Data[key] = value;
+
+        /// <summary>
+        /// Loads (or reloads) the data for this provider.
+        /// </summary>
+        public virtual void Load()
+        { }
+
+        /// <summary>
+        /// Returns the list of keys that this provider has.
+        /// </summary>
+        /// <param name="earlierKeys">The earlier keys that other providers contain.</param>
+        /// <param name="parentPath">The path for the parent IConfiguration.</param>
+        /// <returns>The list of keys for this provider.</returns>
+        public virtual IEnumerable<string> GetChildKeys(
+            IEnumerable<string> earlierKeys,
+            string parentPath)
+        {
+            string prefix = parentPath == null ? string.Empty : parentPath + ConfigurationPath.KeyDelimiter;
+
+            return Data
+                .Where(kv => kv.Key.StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
+                .Select(kv => Segment(kv.Key, prefix.Length))
+                .Concat(earlierKeys)
+                .OrderBy(k => k, ConfigurationKeyComparer.Instance);
+        }
+
+        private static string Segment(string key, int prefixLength)
+        {
+            int indexOf = key.IndexOf(ConfigurationPath.KeyDelimiter, prefixLength, StringComparison.OrdinalIgnoreCase);
+            return indexOf < 0 ? key.Substring(prefixLength) : key.Substring(prefixLength, indexOf - prefixLength);
+        }
+
+        /// <summary>
+        /// Generates a string representing this provider name and relevant details.
+        /// </summary>
+        /// <returns> The configuration name. </returns>
+        public override string ToString() => $"{GetType().Name}";
+    }
+}
---
+++ src/Accounts/Authentication/Config/Internal/Interfaces/IConfigurationBuilder.cs
@@ -0,0 +1,49 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace Microsoft.Azure.Commands.Common.Authentication.Config.Internal.Interfaces
+{
+    /// <summary>
+    /// Represents a type used to build application configuration.
+    /// </summary>
+    internal interface IConfigurationBuilder
+    {
+        /// <summary>
+        /// Gets a key/value collection that can be used to share data between the <see cref="IConfigurationBuilder"/>
+        /// and the registered <see cref="IConfigurationSource"/>s.
+        /// </summary>
+        IDictionary<string, object> Properties { get; }
+
+        /// <summary>
+        /// Gets the sources used to obtain configuration values
+        /// </summary>
+        IList<IConfigurationSource> Sources { get; }
+
+        /// <summary>
+        /// Adds a new configuration source.
+        /// </summary>
+        /// <param name="source">The configuration source to add.</param>
+        /// <returns>The same <see cref="IConfigurationBuilder"/>.</returns>
+        IConfigurationBuilder Add(string id, IConfigurationSource source);
+
+        /// <summary>
+        /// Builds an <see cref="IConfiguration"/> with keys and values from the set of sources registered in
+        /// <see cref="Sources"/>.
+        /// </summary>
+        /// <returns>An <see cref="IConfigurationRoot"/> with keys and values from the registered sources.</returns>
+        IConfigurationRoot Build();
+    }
+}
