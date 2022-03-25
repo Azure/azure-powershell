4769a1a3eacb6c6661ab57c65511d7cff7b29bc7
Kacie Kang <jikang@microsoft.com>
2022-03-24 18:50:59 -0700

    add using

--- src/ApiManagement/ApiManagement.ServiceManagement/Commands/SetAzureApiManagementOperation.cs
+++ src/ApiManagement/ApiManagement.ServiceManagement/Commands/SetAzureApiManagementOperation.cs
@@ -95,6 +95,8 @@ namespace Microsoft.Azure.Commands.ApiManagement.ServiceManagement.Commands
             ValueFromPipelineByPropertyName = true,
             Mandatory = false,
             HelpMessage = "Operation request details. This parameter is optional.")]
+        public const String ChangeDesc = "Change Request.Representations.Sample to Request.Representations.Example";
+        [CmdletParameterBreakingChange("Request.Representations.Sample", ChangeDescription = ChangeDesc)]
         public PsApiManagementRequest Request { get; set; }
         
         public const String ChangeDesc2 = "Change Responses.Representations.Sample to Responses.Representations.Example";
@@ -103,6 +105,8 @@ namespace Microsoft.Azure.Commands.ApiManagement.ServiceManagement.Commands
             ValueFromPipelineByPropertyName = true,
             Mandatory = false,
             HelpMessage = "Array of possible operation responses. This parameter is optional.")]
+        public const String ChangeDesc2 = "Change Responses.Representations.Sample to Responses.Representations.Example";
+        [CmdletParameterBreakingChange("Responses.Representations.Sample", ChangeDescription = ChangeDesc2)]
         public PsApiManagementResponse[] Responses { get; set; }
 
         [Parameter(
