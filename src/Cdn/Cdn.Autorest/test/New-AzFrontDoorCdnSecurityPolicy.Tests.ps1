a16215f94ccfbb3c1694f14236e82906b0ecd81b
Chenglong Liu <chengll@microsoft.com>
2022-03-28 14:02:39 +0800

    Update

--- src/Cdn/Cdn.Autorest/test/New-AzFrontDoorCdnSecurityPolicy.Tests.ps1
+++ src/Cdn/Cdn.Autorest/test/New-AzFrontDoorCdnSecurityPolicy.Tests.ps1
@@ -15,7 +15,25 @@ if(($null -eq $TestName) -or ($TestName -contains 'New-AzFrontDoorCdnSecurityPol
 }
 
 Describe 'New-AzFrontDoorCdnSecurityPolicy' {
-    It 'CreateExpanded' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+    It 'CreateExpanded'  {
+        $ResourceGroupName = 'testps-rg-' + (RandomString -allChars $false -len 6)
+        try
+        {
+            Write-Host -ForegroundColor Green "Create test group $($ResourceGroupName)"
+            New-AzResourceGroup -Name $ResourceGroupName -Location $env.location
+
+            $frontDoorCdnProfileName = 'fdp-' + (RandomString -allChars $false -len 6);
+            Write-Host -ForegroundColor Green "Use frontDoorCdnProfileName : $($frontDoorCdnProfileName)"
+
+            $profileSku = "Standard_AzureFrontDoor";
+            $frontDoorCdnProfile = New-AzFrontDoorCdnProfile -SkuName $profileSku -Name $frontDoorCdnProfileName -ResourceGroupName $ResourceGroupName -Location Global
+
+            $policyName = "pol-" + (RandomString -allChars $false -len 6);
+            Write-Host -ForegroundColor Green "Use policyName : $($policyName)"
+            New-AzFrontDoorCdnSecurityPolicy -Name $policyName -ProfileName $frontDoorCdnProfile -ResourceGroupName $ResourceGroupName
+        } Finally
+        {
+            #Remove-AzResourceGroup -Name $ResourceGroupName -NoWait
+        }
     }
 }
