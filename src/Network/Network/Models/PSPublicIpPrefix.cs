d17c2f42dfb675fe5e1abb926c6bf2bb0007f688
Weiheng Li <weihl@microsoft.com>
2022-04-06 14:24:41 -0700

    fix

--- src/Network/Network/Models/PSPublicIpPrefix.cs
+++ src/Network/Network/Models/PSPublicIpPrefix.cs
@@ -46,6 +46,12 @@ namespace Microsoft.Azure.Commands.Network.Models
         }
 
         [JsonIgnore]
+        public string CustomIpPrefixText
+        {
+            get { return JsonConvert.SerializeObject(new PSResourceId() { Id = CustomIpPrefix.Id }, Formatting.Indented, new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore }); }
+        }
+
+        [JsonIgnore]
         public string IpTagsText
         {
             get { return JsonConvert.SerializeObject(IpTags, Formatting.Indented, new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore }); }
