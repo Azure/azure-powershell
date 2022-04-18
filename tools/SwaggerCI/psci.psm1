c4acd7e692587b886b9c16f5f0c6d9e1f55dc452
Xiaogang <xidi@microsoft.com>
2022-04-18 22:38:55 +0800

    Use write-warning when CI validation fails for a RP

--- tools/SwaggerCI/psci.psm1
+++ tools/SwaggerCI/psci.psm1
@@ -56,7 +56,7 @@ function Invoke-SwaggerCI {
             }
             $packages += $package
         } catch {
-            Write-Error "Azure PowerShell CI validation failed for Az.$modulePath"
+            Write-Warning "Azure PowerShell CI validation failed for Az.$modulePath"
             $package = @{
                 packageName = "Az.$modulePath"
                 path = @("swaggerci/$modulePath")
