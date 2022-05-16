316fe08181d8ed01e040d15bd0f22ae7ba54eae4
Yunchi Wang <54880216+wyunchi-ms@users.noreply.github.com>
2022-05-16 15:16:14 +0800

    First preview release for module Az.ServiceLinker (#18166)
    * Move ServiceLinker to main
    
    * Add some reference
    
    * Add some reference
    
    * Add static analysis exceptions
    
    Co-authored-by: azure-powershell-bot <65331932+azure-powershell-bot@users.noreply.github.com>

---
+++ src/ServiceLinker/test/New-AzServiceLinkerSystemAssignedIdentityAuthInfoObject.Tests.ps1
@@ -0,0 +1,26 @@
+if(($null -eq $TestName) -or ($TestName -contains 'New-AzServiceLinkerSystemAssignedIdentityAuthInfoObject'))
+{
+  $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+  if (-Not (Test-Path -Path $loadEnvPath)) {
+      $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+  }
+  . ($loadEnvPath)
+  $TestRecordingFile = Join-Path $PSScriptRoot 'New-AzServiceLinkerSystemAssignedIdentityAuthInfoObject.Recording.json'
+  $currentPath = $PSScriptRoot
+  while(-not $mockingPath) {
+      $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+      $currentPath = Split-Path -Path $currentPath -Parent
+  }
+  . ($mockingPath | Select-Object -First 1).FullName
+}
+
+Describe 'New-AzServiceLinkerSystemAssignedIdentityAuthInfoObject' {
+    It '__AllParameterSets' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'New SystemMI authInfo' {
+        $authInfo = New-AzServiceLinkerSystemAssignedIdentityAuthInfoObject
+        $authInfo.AuthType | Should -Be "systemAssignedIdentity"
+    }
+}
