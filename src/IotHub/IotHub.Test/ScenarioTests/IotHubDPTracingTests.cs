72e31d9a93b2219553b20367291b3da1771a7e6c
Ziyue Zheng <v-ziyzhe@microsoft.com>
2022-05-17 16:42:52 +0800

    Add IotHubTestRunner to replace IotHubController

--- src/IotHub/IotHub.Test/ScenarioTests/IotHubDPTracingTests.cs
+++ src/IotHub/IotHub.Test/ScenarioTests/IotHubDPTracingTests.cs
@@ -12,29 +12,23 @@
 // limitations under the License.
 // ----------------------------------------------------------------------------------
 
-using Microsoft.Azure.ServiceManagement.Common.Models;
 using Microsoft.WindowsAzure.Commands.ScenarioTest;
-using Microsoft.WindowsAzure.Commands.Test.Utilities.Common;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Microsoft.Azure.Commands.IotHub.Test.ScenarioTests
 {
-    public class IotHubDPTracingTests : RMTestBase
+    public class IotHubDPTracingTests : IotHubTestRunner
     {
-        public XunitTracingInterceptor _logger;
-
-        public IotHubDPTracingTests(ITestOutputHelper output)
+        public IotHubDPTracingTests(ITestOutputHelper output) : base(output)
         {
-            _logger = new XunitTracingInterceptor(output);
-            XunitTracingInterceptor.AddToContext(_logger);
         }
 
         [Fact]
         [Trait(Category.AcceptanceType, Category.LiveOnly)]
         public void TestAzureIotHubTracing()
         {
-            IotHubController.NewInstance.RunPsTest(_logger, "Test-AzureRmIotHubTracing");
+            TestRunner.RunTestScript("Test-AzureRmIotHubTracing");
         }
     }
 }
