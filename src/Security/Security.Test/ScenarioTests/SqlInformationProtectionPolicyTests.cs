79fe9561544092511b034620540f0456b8df5af1
amar-sagare <45947937+amar-sagare@users.noreply.github.com>
2022-05-19 05:55:24 -0700

    revert non cosmosdb changes

--- src/Security/Security.Test/ScenarioTests/SqlInformationProtectionPolicyTests.cs
+++ src/Security/Security.Test/ScenarioTests/SqlInformationProtectionPolicyTests.cs
@@ -12,6 +12,8 @@
 // limitations under the License.
 // ----------------------------------------------------------------------------------
 
+using Microsoft.Azure.Commands.ScenarioTest;
+using Microsoft.Azure.ServiceManagement.Common.Models;
 using Microsoft.WindowsAzure.Commands.ScenarioTest;
 using Xunit;
 
