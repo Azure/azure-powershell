60420cfe669bf08afcdb6c339b0156e00ed6340b
MoChilia <chenshiyingcn@163.com>
2022-05-31 15:05:25 +0800

    fix import-module

--- tools/ScriptAnalyzer/AnalyzeRules/CommandName.psm1
+++ tools/ScriptAnalyzer/AnalyzeRules/CommandName.psm1
@@ -5,7 +5,7 @@
     File: CommandName.psm1
 #>
 
-Import-Module (Get-ChildItem -Path ./Debug).FullName -Scope Global
+Import-Module (Get-ChildItem -Path ./artifacts/Debug).FullName -Scope Global
 
 enum RuleNames {
     Invalid_Cmdlet
