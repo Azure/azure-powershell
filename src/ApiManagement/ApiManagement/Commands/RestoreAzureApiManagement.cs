6bc3905b82a7942cf05ab79531e5542e14661262
MoChilia <chenshiyingcn@163.com>
2022-06-01 07:53:16 +0800

    test for error md and correct cs

--- src/ApiManagement/ApiManagement/Commands/RestoreAzureApiManagement.cs
+++ src/ApiManagement/ApiManagement/Commands/RestoreAzureApiManagement.cs
@@ -64,7 +64,7 @@ namespace Microsoft.Azure.Commands.ApiManagement.Commands
         public string SourceBlobName { get; set; }
 
         [Parameter(
-            Mandatory = True,
+            Mandatory = false,
             HelpMessage = "The type of access to be used for the storage account. The default value is AccessKey.")]
         [PSArgumentCompleter(SdkModels.AccessType.AccessKey, SdkModels.AccessType.SystemAssignedManagedIdentity, SdkModels.AccessType.UserAssignedManagedIdentity)]
         public string AccessType { get; set; }
@@ -126,4 +126,4 @@ namespace Microsoft.Azure.Commands.ApiManagement.Commands
             }
         }
     }
-}
+}  
