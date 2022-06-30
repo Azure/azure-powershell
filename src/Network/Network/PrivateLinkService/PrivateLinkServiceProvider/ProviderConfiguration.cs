b3e9df8797917b82c6fa4220a38efe7758a4f38f
anagg929 <59664801+anagg929@users.noreply.github.com>
2022-06-30 14:34:52 -0700

    Update ProviderConfiguration.cs

--- src/Network/Network/PrivateLinkService/PrivateLinkServiceProvider/ProviderConfiguration.cs
+++ src/Network/Network/PrivateLinkService/PrivateLinkServiceProvider/ProviderConfiguration.cs
@@ -43,6 +43,7 @@ namespace Microsoft.Azure.Commands.Network.PrivateLinkService.PrivateLinkService
             RegisterConfiguration("Microsoft.HealthcareApis/services", "2020-03-30", false, true);
             RegisterConfiguration("Microsoft.HDInsight/clusters", "2018-06-01-preview", true, true);
             RegisterConfiguration("Microsoft.HybridCompute/privateLinkScopes", "2021-05-20",true, true);
+            RegisterConfiguration("Microsoft.KubernetesConfiguration/privateLinkScopes", "2022-04-02-preview", true, true);
             RegisterConfiguration("Microsoft.Insights/privateLinkScopes", "2019-10-17-preview", true, true);
             RegisterConfiguration("Microsoft.KeyVault/vaults", "2018-02-14", false, false);
             RegisterConfiguration("Microsoft.Keyvault/managedHSMs", "2021-06-01-preview", true, false);
