6a9fcc22ae40ff8fff07e75d1036a51955603944
Adam Sandor <adsandor@microsoft.com>
2022-06-30 00:16:32 -0400

    test stop running

--- src/Compute/Compute.Test/ScenarioTests/VirtualMachineTests.cs
+++ src/Compute/Compute.Test/ScenarioTests/VirtualMachineTests.cs
@@ -509,7 +509,7 @@ namespace Microsoft.Azure.Commands.Compute.Test.ScenarioTests
         }
 
         [Fact]
-        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        [Trait(Category.AcceptanceType, Category.LiveOnly)]
         public void TestConfidentialVMSetAzVmOsDiskDESId()
         {
             TestRunner.RunTestScript("Test-ConfidentialVMSetAzVmOsDiskDESId");
