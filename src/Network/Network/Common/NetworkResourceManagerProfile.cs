3b56c9319113b8e43e2628e22d9d2ca8719fc9f9
dtuCloud <tudan0103@gmail.com>
2022-06-30 21:04:24 -0700

    Revert "Add mapping rule on VirtualNetworkEncryption  (#18731)" (#18847)
    This reverts commit 76400a9a57930bfb85b9b785f36d6e8b15a50cae.
    
    Co-authored-by: Dan Tu <dantu@microsoft.com>

--- src/Network/Network/Common/NetworkResourceManagerProfile.cs
+++ src/Network/Network/Common/NetworkResourceManagerProfile.cs
@@ -264,10 +264,6 @@ namespace Microsoft.Azure.Commands.Network
                 cfg.CreateMap<MNM.BgpServiceCommunity, CNM.PSBgpServiceCommunity>();
                 cfg.CreateMap<MNM.BGPCommunity, CNM.PSBgpCommunity>();
 
-                // Virtual Network Encryption
-                cfg.CreateMap<CNM.PSVirtualNetworkEncryption, MNM.VirtualNetworkEncryption>();
-                cfg.CreateMap<MNM.VirtualNetworkEncryption, CNM.PSVirtualNetworkEncryption>();
-
                 // Subnet
                 // CNM to MNM
                 cfg.CreateMap<CNM.PSDhcpOptions, MNM.DhcpOptions>();
