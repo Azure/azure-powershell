516540d1d98ba0c95ac299dd0b9dbd98f8a8ce7c
gaoyp830 <51546332+gaoyp830@users.noreply.github.com>
2022-07-08 16:59:25 -0700

    Update

--- src/Reservations/test/Merge-AzReservation.Tests.ps1
+++ src/Reservations/test/Merge-AzReservation.Tests.ps1
@@ -14,67 +14,40 @@ if(($null -eq $TestName) -or ($TestName -contains 'Merge-AzReservation'))
   . ($mockingPath | Select-Object -First 1).FullName
 }
 
-$reservationId1 = "/providers/Microsoft.Capacity/reservationOrders/c615c897-aaaa-4123-8527-c42cc0da41e0/reservations/7ab831c6-046a-4938-9ace-4a236e409d88"
-$reservationId2 = "/providers/Microsoft.Capacity/reservationOrders/c615c897-aaaa-4123-8527-c42cc0da41e0/reservations/7ab831c6-046a-4938-9ace-4a236e409d88"
+$reservationId1 = "/providers/Microsoft.Capacity/reservationOrders/30000000-aaaa-bbbb-cccc-200000000013/reservations/10000000-aaaa-bbbb-cccc-200000000007"
+$reservationId2 = "/providers/Microsoft.Capacity/reservationOrders/30000000-aaaa-bbbb-cccc-200000000013/reservations/50000000-aaaa-bbbb-cccc-200000000007"
 
 function ExecuteTestCases([object]$response) {
-    $response | Should -Not -Be $null
-    $response.Count | Should -Be 3
-    $childQuantityTotal = $response[0].Quantity + $response[1].Quantity
-    $childQuantityTotal | Should -Be $response[2].Quantity
-    $response[0].ProvisioningState | Should -Be "Cancelled"
-    $response[1].ProvisioningState | Should -Be "Cancelled"
-    $response[2].ProvisioningState | Should -Be "Succeeded"
-    $response[0].ExtendedStatusInfo | Should -Not -Be $null
-    $response[0].ExtendedStatusInfo.StatusCode | Should -Be "Merged"  
-    $response[0].ExtendedStatusInfo | Should -Not -Be $null
-    $response[0].ExtendedStatusInfo.StatusCode | Should -Be "Merged"  
-    $response[1].ExtendedStatusInfo | Should -Not -Be $null
-    $response[1].ExtendedStatusInfo.StatusCode | Should -Be "Merged"
-    $response[0].SplitProperties | Should -Not -Be $null
-    $response[0].SplitProperties.SplitSource | Should -Not -Be $null
-    $response[0].MergeProperties | Should -Not -Be $null
-    $response[0].MergeProperties.MergeDestination | Should -Not -Be $null
-    $response[1].SplitProperties | Should -Not -Be $null
-    $response[1].SplitProperties.SplitSource | Should -Not -Be $null
-    $response[1].MergeProperties | Should -Not -Be $null
-    $response[1].MergeProperties.MergeDestination | Should -Not -Be $null
-    $response[2].MergeProperties | Should -Not -Be $null
-    $response[2].MergeProperties.MergeSources | Should -Not -Be $null
-    $response[2].MergeProperties.MergeSources.Count | Should -Contain $reservationId1
-    $response[2].MergeProperties.MergeSources.Count | Should -Contain $reservationId2
+
 }
 
 Describe 'Merge-AzReservation' {
-    It 'MergeExpanded' {
-        $reservationIds = @($reservationId1, $reservationId2)
-        $res = Merge-AzReservation -ReservationOrderId "30000000-aaaa-bbbb-cccc-200000000013" -ReservationId $reservationIds
-    }
-
     It 'Merge' {
         $reservationIds = @($reservationId1, $reservationId2)
-        $mergeRequest = @{
-            Source = $reservationIds
-        }
-        $res = Merge-AzReservation -ReservationOrderId "30000000-aaaa-bbbb-cccc-200000000013" -Body $mergeRequest
-    }
-
-    It 'MergeViaIdentityExpanded' {
-        $param = @{
-            ReservationOrderId = "30000000-aaaa-bbbb-cccc-200000000013"
-        }
-        $reservationIds = @($reservationId1, $reservationId2)
-        $mergeRequest = @{
-            Source = $reservationIds
-        }
-        $res = Merge-AzReservation -InputObject $param -Body $mergeRequest
-    }
-
-    It 'MergeViaIdentity' {
-        $param = @{
-            ReservationOrderId = "30000000-aaaa-bbbb-cccc-200000000013"
-        }
-        $reservationIds = @($reservationId1, $reservationId2)
-        $res = Merge-AzReservation -InputObject $param -ReservationId $reservationIds
+        $response = Merge-AzReservation -ReservationOrderId "30000000-aaaa-bbbb-cccc-200000000013" -ReservationId $reservationIds
+        $response | Should -Not -Be $null
+        $response.Count | Should -Be 3
+        $childQuantityTotal = $response[0].Quantity + $response[1].Quantity
+        $childQuantityTotal | Should -Be $response[2].Quantity
+        $response[0].ProvisioningState | Should -Be "Cancelled"
+        $response[1].ProvisioningState | Should -Be "Cancelled"
+        $response[2].ProvisioningState | Should -Be "Succeeded"
+        $response[0].ExtendedStatusInfo | Should -Not -Be $null
+        $response[0].ExtendedStatusInfo.StatusCode | Should -Be "Merged"  
+        $response[1].ExtendedStatusInfo | Should -Not -Be $null
+        $response[1].ExtendedStatusInfo.StatusCode | Should -Be "Merged"
+        $response[0].SplitProperties | Should -Not -Be $null
+        $response[0].SplitProperties.SplitSource | Should -Not -Be $null
+        $response[0].MergeProperties | Should -Not -Be $null
+        $response[0].MergeProperties.MergeDestination | Should -Not -Be $null
+        $response[1].SplitProperties | Should -Not -Be $null
+        $response[1].SplitProperties.SplitSource | Should -Not -Be $null
+        $response[1].MergeProperties | Should -Not -Be $null
+        $response[1].MergeProperties.MergeDestination | Should -Not -Be $null
+        $response[2].MergeProperties | Should -Not -Be $null
+        $response[2].MergeProperties.MergeSources | Should -Not -Be $null
+        $response[2].MergeProperties.MergeSources.Count | Should -BeGreaterThan 0
+        $response[2].MergeProperties.MergeSources | Should -Contain $reservationId1
+        $response[2].MergeProperties.MergeSources | Should -Contain $reservationId2
     }
 }
--- src/Reservations/test/Update-AzReservation.Tests.ps1
+++ src/Reservations/test/Update-AzReservation.Tests.ps1
@@ -21,26 +21,26 @@ function GetNewName([string]$oldName) {
 
 Describe 'Update-AzReservation' {
     It 'UpdateExpanded' {
-        $reservation = Get-AzReservation -ReservationOrderId "10000000-aaaa-bbbb-cccc-200000000007" -ReservationId "30000000-aaaa-bbbb-cccc-200000000018"
+        $reservation = Get-AzReservation -ReservationOrderId "30000000-aaaa-bbbb-cccc-200000000013" -ReservationId "10000000-aaaa-bbbb-cccc-200000000007"
         $newName = GetNewName($reservation.DisplayName)
-        $res = Update-AzReservation -ReservationOrderId "10000000-aaaa-bbbb-cccc-200000000007" -ReservationId "30000000-aaaa-bbbb-cccc-200000000018" -Name $newName
+        $res = Update-AzReservation -ReservationOrderId "30000000-aaaa-bbbb-cccc-200000000013" -ReservationId "10000000-aaaa-bbbb-cccc-200000000007" -Name $newName
 
         $res.DisplayName | Should -Not -Be reservation.DisplayName
     }
 
     It 'Update' {
-        $reservation = Get-AzReservation -ReservationOrderId "10000000-aaaa-bbbb-cccc-200000000007" -ReservationId "30000000-aaaa-bbbb-cccc-200000000018"
+        $reservation = Get-AzReservation -ReservationOrderId "30000000-aaaa-bbbb-cccc-200000000013" -ReservationId "10000000-aaaa-bbbb-cccc-200000000007"
         $newName = GetNewName($reservation.DisplayName)
         $newRi = @{Name = $newName}
-        $res = Update-AzReservation -ReservationOrderId "10000000-aaaa-bbbb-cccc-200000000007" -ReservationId "30000000-aaaa-bbbb-cccc-200000000018" -Reservation $newRi
+        $res = Update-AzReservation -ReservationOrderId "30000000-aaaa-bbbb-cccc-200000000013" -ReservationId "10000000-aaaa-bbbb-cccc-200000000007" -Reservation $newRi
 
         $res.DisplayName | Should -Not -Be reservation.DisplayName
     }
 
     It 'UpdateViaIdentityExpanded' {
         $input = @{
-            ReservationId = "30000000-aaaa-bbbb-cccc-200000000018"
-            ReservationOrderId = "10000000-aaaa-bbbb-cccc-200000000007"
+            ReservationId = "10000000-aaaa-bbbb-cccc-200000000007"
+            ReservationOrderId = "30000000-aaaa-bbbb-cccc-200000000013"
         }
         $reservation = Get-AzReservation -InputObject $input
         $newName = GetNewName($reservation.DisplayName)
@@ -51,8 +51,8 @@ Describe 'Update-AzReservation' {
 
     It 'UpdateViaIdentity' {
         $input = @{
-            ReservationId = "30000000-aaaa-bbbb-cccc-200000000018"
-            ReservationOrderId = "10000000-aaaa-bbbb-cccc-200000000007"
+            ReservationId = "10000000-aaaa-bbbb-cccc-200000000007"
+            ReservationOrderId = "30000000-aaaa-bbbb-cccc-200000000013"
         }
         $reservation = Get-AzReservation -InputObject $input
         $newName = GetNewName($reservation.DisplayName)
@@ -60,5 +60,29 @@ Describe 'Update-AzReservation' {
         $res = Update-AzReservation -InputObject $input -Reservation $newRi
 
         $res.DisplayName | Should -Not -Be reservation.DisplayName
+    }
+
+    It 'UpdateAppliedScope' {
+        #Update AppliedScopeType to be 'Shared'
+        $res = Update-AzReservation -ReservationOrderId "30000000-aaaa-bbbb-cccc-200000000013" -ReservationId "10000000-aaaa-bbbb-cccc-200000000007" -AppliedScopeType "Shared"
+        $reservation = Get-AzReservation -ReservationOrderId "30000000-aaaa-bbbb-cccc-200000000013" -ReservationId "10000000-aaaa-bbbb-cccc-200000000007"
+        $res.AppliedScopeType | Should -Be "Shared"
+        $res.AppliedScopes | Should -Be $null
+
+        #Update AppliedScopeType to be 'Single'
+        $res = Update-AzReservation -ReservationOrderId "30000000-aaaa-bbbb-cccc-200000000013" -ReservationId "10000000-aaaa-bbbb-cccc-200000000007" -AppliedScopeType "Single" -AppliedScope "/subscriptions/30000000-aaaa-bbbb-cccc-200000000018"
+        $reservation = Get-AzReservation -ReservationOrderId "30000000-aaaa-bbbb-cccc-200000000013" -ReservationId "10000000-aaaa-bbbb-cccc-200000000007"
+        $res.AppliedScopeType | Should -Be "Single"
+        $res.AppliedScopes | Should -Not -Be $null
+        $res.AppliedScopes.Count | Should -Be 1
+        $res.AppliedScopes[0] | Should -Be "/subscriptions/30000000-aaaa-bbbb-cccc-200000000018"
+
+        #Update AppliedScopeType to be 'Single' with a resource group
+        $res = Update-AzReservation -ReservationOrderId "30000000-aaaa-bbbb-cccc-200000000013" -ReservationId "10000000-aaaa-bbbb-cccc-200000000007" -AppliedScopeType "Single" -AppliedScope "/subscriptions/30000000-aaaa-bbbb-cccc-200000000018/resourcegroups/testrg"
+        $reservation = Get-AzReservation -ReservationOrderId "30000000-aaaa-bbbb-cccc-200000000013" -ReservationId "10000000-aaaa-bbbb-cccc-200000000007"
+        $res.AppliedScopeType | Should -Be "Single"
+        $res.AppliedScopes | Should -Not -Be $null
+        $res.AppliedScopes.Count | Should -Be 1
+        $res.AppliedScopes[0] | Should -Be "/subscriptions/30000000-aaaa-bbbb-cccc-200000000018/resourcegroups/testrg"
     }
 }
