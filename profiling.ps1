67827cc0558dd8a261620d2238b95626be34c9ef
Yeming Liu <11371776+isra-fel@users.noreply.github.com>
2022-07-11 14:41:15 +0000

    fuzzysharp

--- profiling.ps1
+++ profiling.ps1
@@ -1,27 +1,37 @@
 $ErrorActionPreference = 'SilentlyContinue'
 $WarningPreference = 'SilentlyContinue'
 
+function Get-RandomString {
+    $l = 3..12 | Get-Random
+    return -join (97..122 | Get-Random -Count $l | ForEach-Object {[char]$_})
+}
+
+function Get-RandomCommand {
+    $i = 0..3 | Get-Random
+    $verb = @('Get', 'Set', 'New', 'Remove')[$i]
+    return "$verb-Az$(Get-RandomString)"
+}
+
 # test 1: how long to run
-for ($i = 0; $i -ne 2000; $i++) {
-    Get-AzKeyVaultCat
-}
+$commands = 1..200 | ForEach-Object { Get-RandomCommand }
 
-ipmo ./artifacts/Debug/Az.Accounts/Az.Accounts.psd1
+$commands | ForEach-Object { Invoke-Expression $_ }
 
-for ($i = 0; $i -ne 2000; $i++) {
-    Get-AzKeyVaultCat
-}
+import-module ./artifacts/Debug/Az.Accounts/Az.Accounts.psd1
+
+$commands | ForEach-Object { Invoke-Expression $_ }
+
+[Microsoft.Azure.Commands.Profile.Utilities.CommandNotFoundHelper]::EnableFuzzyString = $false
+
+$commands | ForEach-Object { Invoke-Expression $_ }
 
 
-# Test 2: how long initialization takes
-# Get-AzKeyVault
-# Get-AzKeyVault
-# Get-AzKeyVault
-# Get-AzKeyVault
-# Get-AzKeyVault
-# ipmo ./artifacts/Debug/Az.Accounts/Az.Accounts.psd1
-# Get-AzKeyVault
-# Get-AzKeyVault
-# Get-AzKeyVault
-# Get-AzKeyVault
-# Get-AzKeyVault
+# # Test 2: how long initialization takes
+# New-AzKeyVaultStorageAccount
+# New-AzKeyVaultKey
+# Import-Module ./artifacts/Debug/Az.Accounts/Az.Accounts.psd1
+# New-AzKeyVaultStorageAccount
+# New-AzKeyVaultKey
+# [Microsoft.Azure.Commands.Profile.Utilities.CommandNotFoundHelper]::EnableFuzzyString = $false
+# New-AzKeyVaultStorageAccount
+# New-AzKeyVaultKey
--- src/Accounts/Accounts/Utilities/CommandNotFoundHelper.cs
+++ src/Accounts/Accounts/Utilities/CommandNotFoundHelper.cs
@@ -20,6 +20,8 @@ using System.Management.Automation;
 using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
+using FuzzySharp.SimilarityRatio;
+using FuzzySharp.SimilarityRatio.Scorer.StrategySensitive;
 using Newtonsoft.Json;
 
 namespace Microsoft.Azure.Commands.Profile.Utilities
@@ -29,6 +31,8 @@ namespace Microsoft.Azure.Commands.Profile.Utilities
     /// </summary>
     public static class CommandNotFoundHelper
     {
+        public static bool EnableFuzzyString = true;
+
         private static CommandInvocationIntrinsics Cii = null;
         private static Regex AzOrAzureRMRegex = new Regex(@"^\w+-az\w+$", RegexOptions.IgnoreCase);
         private static Regex AzureRMRegex = new Regex(@"^\w+-azurerm\w+$", RegexOptions.IgnoreCase);
@@ -109,30 +113,29 @@ namespace Microsoft.Azure.Commands.Profile.Utilities
             {
                 if (IsAzureRMCommand(args.CommandName))
                 {
-                    WriteWarning($@"The command {args.CommandName} is in the AzureRM PowerShell module, which is outdated. See todo fwlink https://docs.microsoft.com/en-us/powershell/azure/migrate-from-azurerm-to-az?view=azps-8.1.0 for instructions to migrate to Az.");
+                    WriteWarning($@"[todo] The command {args.CommandName} is in the AzureRM PowerShell module, which is outdated. See [todo] fwlink https://docs.microsoft.com/en-us/powershell/azure/migrate-from-azurerm-to-az?view=azps-8.1.0 for instructions to migrate to Az.");
                 }
                 else if (TryGetMigrationGuide(args.CommandName, out MigrationDetails details))
                 {
                     // todo: fill in the migration part in CommandMappings.json
-                    WriteWarning($@"The command {args.CommandName} has been deprecated and replaced by {details.Replacement} since {details.Since}. Please refer to the migration guide [todo]");
+                    WriteWarning($@"[todo] The command {args.CommandName} has been deprecated and replaced by {details.Replacement} since {details.Since}. Please refer to the migration guide [todo]");
                 }
                 else if (TryGetModuleOfCommand(args.CommandName, out string moduleName))
                 {
-                    WriteWarning($@"The term {args.CommandName} is a cmdlet belongs to Azure PowerShell module {moduleName} and it is not installed.
+                    WriteWarning($@"[todo] The term {args.CommandName} is a cmdlet belongs to Azure PowerShell module {moduleName} and it is not installed.
 Run 'Install-Module {moduleName}' to install it.");
                 }
-                else if (TryGetFuzzyStringSuggestions(args.CommandName, out IEnumerable<string> suggestions))
+                else if (EnableFuzzyString && TryGetFuzzyStringSuggestions(args.CommandName, out IEnumerable<string> suggestions))
                 {
-                    // todo: introduce FuzzySharp
-                    WriteWarning(FormatFuzzyStringSuggestions(suggestions));
+                    WriteWarning(FormatFuzzyStringSuggestions(args.CommandName, suggestions));
                 }
             }
             args.StopSearch = false;
         }
 
-        private static string FormatFuzzyStringSuggestions(IEnumerable<string> suggestions)
+        private static string FormatFuzzyStringSuggestions(string commandName, IEnumerable<string> suggestions)
         {
-            StringBuilder sb = new StringBuilder("todo ... The most similar commands are:");
+            StringBuilder sb = new StringBuilder($"[todo] {commandName} is not found. The most similar Azure PowerShell commands are:");
             sb.Append(Environment.NewLine);
             foreach (var suggestion in suggestions)
             {
@@ -167,8 +170,20 @@ Run 'Install-Module {moduleName}' to install it.");
 
         private static bool TryGetFuzzyStringSuggestions(string commandName, out IEnumerable<string> suggestions)
         {
-            var allCommandsInLowercase = LazyCommandLowercaseToNormal.Value.Keys;
-            var suggestionsInLowercase = new string[] { "new-azbastion", "get-azbastion" };
+            // todo: refactor
+            var commandNamesInLowercase = LazyCommandLowercaseToNormal.Value.Keys;
+            var suggestionsInLowercase = FuzzySharp.Process.ExtractTop(
+                commandName.ToLowerInvariant(),
+                commandNamesInLowercase,
+                processor: s => s, // ExtractTop by default do ToLowercase to every candidate. It's inefficient so we override it and cache a lowercase version of command names array.
+                scorer: ScorerCache.Get<DefaultRatioScorer>(),
+                cutoff: 80,
+                limit: 3)
+                .Select(x =>
+                {
+                    // Console.WriteLine(x.ToString());
+                    return x.Value;
+                });
             suggestions = suggestionsInLowercase.Select(x => LazyCommandLowercaseToNormal.Value[x]);
             return suggestions.Any();
         }
