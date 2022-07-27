a24e1f098a87827fc0096c737075c863d8eed3d7
Shiying Chen <72982571+MoChilia@users.noreply.github.com>
2022-07-27 10:17:16 +0800

    Update utils.ps1

--- tools/StaticAnalysis/ExampleAnalyzer/utils.ps1
+++ tools/StaticAnalysis/ExampleAnalyzer/utils.ps1
@@ -12,7 +12,6 @@
                     Get-RecordsNotInAllowList
                     Measure-SectionMissingAndOutputScript
                     Get-ScriptAnalyzerResult
-                    Redo-ImportModule
 #>
 $DebugPreference = 'Continue'
 
@@ -458,7 +457,6 @@ function Get-ScriptAnalyzerResult {
         $analysisResults = Invoke-ScriptAnalyzer -Path $ScriptPath -CustomRulePath $RulePath -IncludeDefaultRules:$IncludeDefaultRules.IsPresent
     }
     $results = @()
-    # $analysisResults | Out-File res.txt -Append
     foreach($analysisResult in $analysisResults){
         if($analysisResult.Severity -eq "ParseError"){
             $Severity = 1
@@ -506,30 +504,4 @@ function Get-ScriptAnalyzerResult {
     $results = Get-NonExceptionRecord $results
 
     return $results
-}
-
-<#
-    .SYNOPSIS
-    Retry import-module
-#>
-function Redo-ImportModule {
-    param (
-        [string]$CommandName,
-        [string]$moduleName
-    )
-    $modulePath = "$PSScriptRoot\..\..\..\artifacts\Debug\$moduleName\$moduleName.psd1"
-    if(!(Test-Path $modulePath)){
-        Write-Debug "Cannot find path $modulePath."
-        return $false
-    }
-    Get-Item $modulePath | Import-Module -Global
-    $GetCommandName = Get-Command -ListImported | Where-Object {$_.Name -eq $CommandName}
-    if ($null -eq $GetCommandName) {
-        Write-Debug "$CommandName is still invalid"
-        return $false
-    }
-    else{
-        Write-Debug "Succeed by retrying import-module"
-        return $true
-    }
 }
