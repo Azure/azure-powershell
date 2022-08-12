dac726ae5926d863b30432363c3f948c73f1adfc
Ab0907 <109592962+Ab0907@users.noreply.github.com>
2022-08-12 16:37:08 +0530

    Update PowershellJsonConverter.cs

--- src/Automation/Automation/Common/PowershellJsonConverter.cs
+++ src/Automation/Automation/Common/PowershellJsonConverter.cs
@@ -1,4 +1,4 @@
-﻿// ----------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------
 //
 // Copyright Microsoft Corporation
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,6 +14,7 @@
 
 using Microsoft.Azure.Commands.Automation.Properties;
 using Microsoft.WindowsAzure.Commands.Utilities.Common;
+using Microsoft.Azure.Commands.Common.Compute.Version2016_04_preview.Models;
 using System;
 using System.Collections;
 using System.Collections.ObjectModel;
@@ -23,6 +24,7 @@ using System.Text;
 
 namespace Microsoft.Azure.Commands.Automation.Common
 {
+    
     public static class PowerShellJsonConverter
     {
         public static string Serialize(object inputObject)
@@ -45,7 +47,8 @@ namespace Microsoft.Azure.Commands.Automation.Common
 
             return result[0].ToString();
         }
-  public static PSObject Deserialize(string json)
+
+        public static PSObject Deserialize(string json)
         {
             if (string.IsNullOrEmpty(json))
             {
@@ -55,51 +58,45 @@ namespace Microsoft.Azure.Commands.Automation.Common
             Hashtable parameters = new Hashtable();
             int PSVersion = 5;
             Collection<PSObject> result=null;
-            bool JsonParseStatus = false;
             PSVersion = Int32.Parse(AzurePSCmdlet.PowerShellVersion[0].ToString());
             parameters.Add(Constants.PsCommandParamInputObject, json);
             if (PSVersion > 6)
             {
                 try
                 {
-                    result = PowerShellJsonConverter.InvokeScript(Constants.PsCommandConvertFromJson, parameters);
-                    JsonParseStatus = true;
-                }
-                catch (Exception)
-                {
-
-                }
-                if(!JsonParseStatus)
-                {
-                    return json;
-                }
-                else
-                {
+                    result = PowerShellJsonConverter.InvokeScript(Constants.PsCommandConvertFromJson, parameters,true);
                     if (result.Count != 1)
                     {
                         return null;
                     }
                     return result[0];
                 }
+                catch (Exception)
+                {
+                    return json;
+                }
                 
             }
             else
             {
-                result = PowerShellJsonConverter.InvokeScript(Constants.PsCommandConvertFromJson, parameters);
-
-
-                if (result.Count != 1)
+                try
                 {
-                    return null;
+                    result = PowerShellJsonConverter.InvokeScript(Constants.PsCommandConvertFromJson, parameters);
+                    if (result.Count != 1)
+                    {
+                        return null;
+                    }
+                    return result[0];
                 }
-
-                //count == 1. return the first psobject
-                return result[0];
+                catch (Exception)
+                {
+                    return json;
+                }
             }
 
         }
 
-        private static Collection<PSObject> InvokeScript(string scriptName, Hashtable parameters)
+        private static Collection<PSObject> InvokeScript(string scriptName, Hashtable parameters, bool addNoEnumerateSwitchToParameters=false)
         {
             using (var powerShell = System.Management.Automation.PowerShell.Create(RunspaceMode.CurrentRunspace))
             {
@@ -107,6 +104,10 @@ namespace Microsoft.Azure.Commands.Automation.Common
                 foreach (DictionaryEntry parameter in parameters)
                 {
                     powerShell.AddParameter(parameter.Key.ToString(), parameter.Value);
+                }
+                if(addNoEnumerateSwitchToParameters)
+                {
+                    powerShell.AddParameter("NoEnumerate");
                 }
                 var result = powerShell.Invoke();
                 //Error handling
@@ -126,3 +127,4 @@ namespace Microsoft.Azure.Commands.Automation.Common
         }
     }
 }
+
