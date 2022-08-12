f6ba51d1f0b4b508b8dab678c124c2979c412ed6
jago2136 <42753636+jago2136@users.noreply.github.com>
2022-08-11 19:20:53 -0700

    Merge Azure Virtual Network Manager Cmdlets (#19023)
    * add initial network manager files
    
    * adding siugnature issues and itemgroup to test csproj file
    
    * adding siugnature issues and itemgroup to test csproj file
    
    * add all tests and bug fixes
    
    * add help docs
    
    * bux fix for param autocomplete and update changelog.md
    
    * update help files and changelog to address comments
    
    * resolve PR review suggestions
    
    * resolve PR review comments
    
    * fix review comments involving input object, list->array object types, and various param renames
    
    * Update src/Network/Network/ChangeLog.md
    
    Co-authored-by: Beisi Zhou <zhoubeisi@gmail.com>
    
    * fix markdown validation error
    
    * fix markdown errors reported by PR check
    
    * updating markdown files to fix validation errors
    
    * updating markdown files to fix validation errors
    
    * add cross-tenant scopes property to network manager
    
    * add valid enum values to subscriptions and push most recent test records
    
    * update help docs and set commands
    
    * fix help docs online version link
    
    Co-authored-by: Jared Gorthy <jaredgorthy@microsoft.com>
    Co-authored-by: Beisi Zhou <zhoubeisi@gmail.com>

---
+++ src/Network/Network/NetworkManager/NetworkManagerActiveConfiguration/GetAzNetworkManagerActiveConnectivityConfigurationCommand.cs
@@ -0,0 +1,79 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Commands.Network.Models;
+using Microsoft.Azure.Management.Network;
+using System.Collections.Generic;
+using System.Management.Automation;
+using Microsoft.Azure.Management.Network.Models;
+using Microsoft.Rest.Azure;
+using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
+using Microsoft.Azure.Commands.Network.Models.NetworkManager;
+using System.Linq;
+
+namespace Microsoft.Azure.Commands.Network
+{
+    [Cmdlet("Get", ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "NetworkManagerActiveConnectivityConfiguration"), OutputType(typeof(PSNetworkManagerActiveConnectivityConfigurationResult))]
+    public class GetAzNetworkManagerActiveConnectivityConfigurationCommand : NetworkManagerBaseCmdlet
+    {
+        [Parameter(
+           Mandatory = true,
+           ValueFromPipelineByPropertyName = true,
+           HelpMessage = "The network manager name.")]
+        [ValidateNotNullOrEmpty]
+        [ResourceNameCompleter("Microsoft.Network/networkManagers", "ResourceGroupName")]
+        [SupportsWildcards]
+        public virtual string NetworkManagerName { get; set; }
+
+        [Parameter(
+           Mandatory = true,
+           ValueFromPipelineByPropertyName = true,
+           HelpMessage = "The resource group name.")]
+        [ResourceGroupCompleter]
+        [ValidateNotNullOrEmpty]
+        [SupportsWildcards]
+        public virtual string ResourceGroupName { get; set; }
+
+        [Parameter(
+           Mandatory = false,
+           ValueFromPipelineByPropertyName = true,
+           HelpMessage = "List of regions.")]
+        [AllowEmptyCollection]
+        public string[] Region { get; set; }
+
+        [Parameter(
+           Mandatory = false,
+           ValueFromPipelineByPropertyName = true,
+           HelpMessage = "SkipToken.")]
+        public string SkipToken { get; set; }
+
+        public override void Execute()
+        {
+            base.Execute();
+            var parameter = new ActiveConfigurationParameter();
+            if(this.Region != null)
+            {
+                parameter.Regions = this.Region.ToList();
+            }
+            if(!string.IsNullOrEmpty(this.SkipToken))
+            {
+                parameter.SkipToken = this.SkipToken;
+            }
+                
+            var networkManagerActiveConnectivityConfiguration = this.NetworkClient.NetworkManagementClient.ListActiveConnectivityConfigurations(parameter, this.ResourceGroupName, this.NetworkManagerName);
+            var psActiveConnectivityConfiguration = NetworkResourceManagerProfile.Mapper.Map<PSNetworkManagerActiveConnectivityConfigurationResult>(networkManagerActiveConnectivityConfiguration);
+            WriteObject(psActiveConnectivityConfiguration);
+        }
+    }
+}
