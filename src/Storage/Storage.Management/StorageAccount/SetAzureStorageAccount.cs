494c50068deb03f92ec961ce8215e404f5124cc2
Wei Wei <weiwei@microsoft.com>
2022-08-15 17:33:35 +0800

    Update src/Storage/Storage.Management/StorageAccount/SetAzureStorageAccount.cs
    Co-authored-by: Yeming Liu <11371776+isra-fel@users.noreply.github.com>

--- src/Storage/Storage.Management/StorageAccount/SetAzureStorageAccount.cs
+++ src/Storage/Storage.Management/StorageAccount/SetAzureStorageAccount.cs
@@ -293,7 +293,7 @@ namespace Microsoft.Azure.Commands.Management.Storage
         {
             get
             {
-                return enableAzureActiveDirectoryKerberosForFile.Value;
+                return enableAzureActiveDirectoryKerberosForFile.HasValue ? enableAzureActiveDirectoryKerberosForFile.Value : false;
             }
             set
             {
