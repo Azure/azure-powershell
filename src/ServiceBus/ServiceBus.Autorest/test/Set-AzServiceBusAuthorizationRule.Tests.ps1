cf6dbf34963dcf31248cffb86294d94b84067b2b
unknown <davadhani@microsoft.com>
2022-09-15 20:42:52 +0530

    Fixes and Test additions

---
+++ src/ServiceBus/ServiceBus.Autorest/test/Set-AzServiceBusAuthorizationRule.Tests.ps1
@@ -0,0 +1,55 @@
+if(($null -eq $TestName) -or ($TestName -contains 'Set-AzServiceBusAuthorizationRule'))
+{
+  $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+  if (-Not (Test-Path -Path $loadEnvPath)) {
+      $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+  }
+  . ($loadEnvPath)
+  $TestRecordingFile = Join-Path $PSScriptRoot 'Set-AzServiceBusAuthorizationRule.Recording.json'
+  $currentPath = $PSScriptRoot
+  while(-not $mockingPath) {
+      $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+      $currentPath = Split-Path -Path $currentPath -Parent
+  }
+  . ($mockingPath | Select-Object -First 1).FullName
+}
+
+Describe 'Set-AzServiceBusAuthorizationRule' {
+    It 'SetExpandedNamespace' {
+        $authRule = Set-AzServiceBusAuthorizationRule -ResourceGroupName $env.resourceGroup -NamespaceName $env.namespace -Name namespaceAuthRule1 -Rights @("Send","Listen","Manage")
+        $authRule.Name | Should -Be "namespaceAuthRule1"
+        $authRule.ResourceGroupName | Should -Be $env.resourceGroup
+        $authRule.Rights.Count | Should -Be 3
+    }
+
+    It 'SetExpandedQueue' {
+        $authRule = Set-AzServiceBusAuthorizationRule -ResourceGroupName $env.resourceGroup -NamespaceName $env.namespace -QueueName queue1 -Name queueAuthRule1 -Rights @("Send")
+        $authRule.Name | Should -Be "queueAuthRule1"
+        $authRule.ResourceGroupName | Should -Be $env.resourceGroup
+        $authRule.Rights.Count | Should -Be 1
+    }
+
+    It 'SetExpandedTopic' {
+        $authRule = Set-AzServiceBusAuthorizationRule -ResourceGroupName $env.resourceGroup -NamespaceName $env.namespace -TopicName topic1 -Name topicAuthRule2 -Rights @("Listen")
+        $authRule.Name | Should -Be "topicAuthRule2"
+        $authRule.ResourceGroupName | Should -Be $env.resourceGroup
+        $authRule.Rights.Count | Should -Be 1
+    }
+
+    It 'SetViaIdentityExpanded' {
+        $authRule = Get-AzServiceBusAuthorizationRule -ResourceGroupName $env.resourceGroup -NamespaceName $env.namespace -Name namespaceAuthRule3 | Set-AzServiceBusAuthorizationRule -Rights @('Send')
+        $authRule.Name | Should -Be "namespaceAuthRule3"
+        $authRule.ResourceGroupName | Should -Be $env.resourceGroup
+        $authRule.Rights.Count | Should -Be 1
+
+        $authRule = Get-AzServiceBusAuthorizationRule -ResourceGroupName $env.resourceGroup -NamespaceName $env.namespace -QueueName queue1 -Name queueAuthRule2 | Set-AzServiceBusAuthorizationRule -Rights @('Send','Listen','Manage')
+        $authRule.Name | Should -Be "queueAuthRule2"
+        $authRule.ResourceGroupName | Should -Be $env.resourceGroup
+        $authRule.Rights.Count | Should -Be 3
+
+        $authRule = Get-AzServiceBusAuthorizationRule -ResourceGroupName $env.resourceGroup -NamespaceName $env.namespace -QueueName queue1 -Name topicAuthRule2 | Set-AzServiceBusAuthorizationRule -Rights @('Send','Listen','Manage')
+        $authRule.Name | Should -Be "topicAuthRule2"
+        $authRule.ResourceGroupName | Should -Be $env.resourceGroup
+        $authRule.Rights.Count | Should -Be 3
+    }
+}
