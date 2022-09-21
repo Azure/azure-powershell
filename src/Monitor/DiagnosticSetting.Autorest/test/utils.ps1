1024a200b5cd11a9a4295f55d28acba10fdac2ba
VeryEarly <yabhu@microsoft.com>
2022-09-21 15:36:48 +0800

    add test for diagnostic setting

--- src/Monitor/DiagnosticSetting.Autorest/test/utils.ps1
+++ src/Monitor/DiagnosticSetting.Autorest/test/utils.ps1
@@ -18,7 +18,28 @@ function setupEnv() {
     # as default. You could change them if needed.
     $env.SubscriptionId = (Get-AzContext).Subscription.Id
     $env.Tenant = (Get-AzContext).Tenant.Id
+    $resourceGroupName = 'diagnostic-setting-group' + (RandomString -allChars $false -len 6)
+    write-host "start to create test group $resourceGroupName"
+    New-AzResourceGroup -Name $resourceGroupName -Location eastus
+    $null = $env.Add("resourceGroupName", $resourceGroupName)
+
     # For any resources you created for test, you should add it to $env here.
+
+    $vnetName = 'test-vnet' + (RandomString -allChars $false -len 6)
+    write-host "start to create virtual network $vnetName"
+    New-AzVirtualNetwork -Name $vnetName -ResourceGroupName $resourceGroupName -AddressPrefix "10.0.1.0/24" -Location eastus
+    $vnetId = (Get-AzVirtualNetwork -Name $vnetName -ResourceGroupName $resourceGroupName).Id
+    $null = $env.Add("vnetId", $vnetId)
+
+    $workspaceName = 'test-workspace' + (RandomString -allChars $false -len 6)
+    write-host "start to create log analytics workspace $workspaceName"
+    New-AzOperationalInsightsWorkspace -ResourceGroupName $resourceGroupName -Name $workspaceName -Location eastus
+    $workspaceId = (Get-AzOperationalInsightsWorkspace -ResourceGroupName $resourceGroupName -Name $workspaceName).ResourceId
+    $null = $env.Add("workspaceId", $workspaceId)
+
+    $diagnosticSettingName = 'test-diagnostic-setting' + (RandomString -allChars $false -len 6)
+    $null = $env.Add("diagnosticSettingName", $diagnosticSettingName)
+
     $envFile = 'env.json'
     if ($TestMode -eq 'live') {
         $envFile = 'localEnv.json'
