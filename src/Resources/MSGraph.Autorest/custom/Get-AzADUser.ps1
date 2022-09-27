8ad6de6347876ee337079305bc8199cb6e121a6f
Yabo Hu <yabhu@microsoft.com>
2022-09-27 09:20:19 +0800

    [MSGraph] add detail for -filter (#19615)
    * add detail for -filter
    
    * fix examples

--- src/Resources/MSGraph.Autorest/custom/Get-AzADUser.ps1
+++ src/Resources/MSGraph.Autorest/custom/Get-AzADUser.ps1
@@ -98,7 +98,7 @@ function Get-AzADUser {
     
         [Parameter(ParameterSetName='List')]
         [System.String]
-        # Filter items by property values
+        # Filter items by property values, for more detail about filter query please see: https://learn.microsoft.com/en-us/graph/filter-query-parameter
         ${Filter},
     
         [Parameter(ParameterSetName='List')]
