8010dca5b8028afe685eeade1848dcbe4e6fea66
Adam Sandor <adsandor@microsoft.com>
2022-09-28 14:18:41 -0400

    storage version update

--- src/Accounts/AuthenticationAssemblyLoadContext/AzAssemblyLoadContextInitializer.cs
+++ src/Accounts/AuthenticationAssemblyLoadContext/AzAssemblyLoadContextInitializer.cs
@@ -32,7 +32,7 @@ namespace Microsoft.Azure.PowerShell.AuthenticationAssemblyLoadContext
             var azSharedAssemblies = new Dictionary<string, Version>()
             {
                 {"Azure.Core", new Version("1.25.0.0")},
-                {"Azure.Identity", new Version("1.6.1.0")},
+                {"Azure.Identity", new Version("1.6.0.0")},
                 {"Microsoft.Bcl.AsyncInterfaces", new Version("1.1.1.0")},
                 {"Microsoft.Identity.Client", new Version("4.39.0.0") },
                 {"Microsoft.Identity.Client.Extensions.Msal", new Version("2.19.3.0") },
