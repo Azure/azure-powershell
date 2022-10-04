9fb1e0af16a47e91a121ea9d60ce9722bc4be563
Vivian Thiebaut <vthiebaut@microsoft.com>
2022-10-04 14:50:16 -0400

    Simplify Config VM tests. Re-record http requests.

--- src/Ssh/Ssh.Test/ScenarioTests/GetAzSshConfigTests.ps1
+++ src/Ssh/Ssh.Test/ScenarioTests/GetAzSshConfigTests.ps1
@@ -84,9 +84,10 @@ function Test-GetVmConfig
 
     try 
     {
-        New-AzVM -ResourceGroupName $ResourceGroupName -Name $VmName -Location "eastus" -Image UbuntuLTS -Credential $cred
+        $vm = New-AzVM -ResourceGroupName $ResourceGroupName -Name $VmName -Location "eastus" -Image UbuntuLTS -Credential $cred
         Remove-Item ./config -ErrorAction Ignore
 
+        Assert-NotNull $vm
         $configEntry = Export-AzSshConfig -ResourceGroupName $ResourceGroupName -Name $VmName -ConfigFilePath ./config -LocalUser $username
 
         Assert-NotNull $configEntry
@@ -102,52 +103,6 @@ function Test-GetVmConfig
 
         $configFileContent = (Get-Content -Path ./config)
         Assert-AreEqual "Host $ResourceGroupName-$VmName-$username" $configFileContent[1]
-
-        #Create a new entry with a key, and append to config
-        Remove-Item ./tempprivkey -ErrorAction Ignore
-        New-Item -path . -name tempprivkey -ItemType file
-        $privkeypath = (Resolve-Path ./tempprivkey).Path
-        $configEntryKeyAuth = Export-AzSshConfig -ResourceGroupName $ResourceGroupName -Name $VmName -ConfigFilePath ./config -LocalUser $username -PrivateKeyFile ./tempprivkey -Port 35000
-
-        Assert-NotNull $configEntryKeyAuth
-        Assert-AreEqual $configEntryKeyAuth.Host "$ResourceGroupName-$VmName"
-        Assert-AreEqual $configEntryKeyAuth.User $username
-        Assert-AreEqual $configEntryKeyAuth.ResourceType "Microsoft.Compute/virtualMachines"
-        Assert-AreEqual $configEntryKeyAuth.LoginType "LocalUser"
-        Assert-AreEqual $configEntryKeyAuth.IdentityFile $privkeypath
-        Assert-AreEqual $configEntryKeyAuth.Port "35000"
-        Assert-Null $configEntryKeyAuth.CertificateFile
-        Assert-Null $configEntryKeyAuth.ProxyCommand
-        Assert-NotNull $configEntryKeyAuth.HostName
-
-        $len = $configFileContent.length
-        $configFileContent = (Get-Content -Path ./config)
-
-        Assert-AreEqual "Host $ResourceGroupName-$VmName-$username" $configFileContent[$len + 1]
-        Assert-AreEqual "IdentityFile `"$privkeypath`"" $configFileContent[$len + 4].Trim()
-
-        #Create a new entry with certificate, and overwrite Config
-        Remove-Item ./tempsshcert -ErrorAction Ignore
-        New-Item -path . -name tempsshcert -ItemType file
-        $certpath = (Resolve-Path ./tempsshcert).Path
-        $configEntryCertAuth = Export-AzSshConfig -ResourceGroupName $ResourceGroupName -Name $VmName -ConfigFilePath ./config -LocalUser $username -PrivateKeyFile ./tempprivkey -CertificateFile ./tempsshcert -Overwrite
-        $configFileContent = (Get-Content -Path ./config)
-
-        Assert-NotNull $configEntryCertAuth
-        Assert-AreEqual $configEntryCertAuth.Host "$ResourceGroupName-$VmName"
-        Assert-AreEqual $configEntryCertAuth.User $username
-        Assert-AreEqual $configEntryCertAuth.ResourceType "Microsoft.Compute/virtualMachines"
-        Assert-AreEqual $configEntryCertAuth.LoginType "LocalUser"
-        Assert-AreEqual $configEntryCertAuth.IdentityFile $privkeypath
-        Assert-AreEqual $configEntryCertAuth.CertificateFile $certpath
-        Assert-Null $configEntryCertAuth.Port
-        Assert-Null $configEntryCertAuth.ProxyCommand
-        Assert-NotNull $configEntryCertAuth.HostName
-        Assert-AreEqual $configFileContent.length 12
-
-        Assert-AreEqual "Host $ResourceGroupName-$VmName-$username" $configFileContent[1]
-        Assert-AreEqual "CertificateFile `"$certpath`"" $configFileContent[4].Trim()
-        Assert-AreEqual "IdentityFile `"$privkeypath`"" $configFileContent[5].Trim()
         
     }
     finally {
