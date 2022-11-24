46f05ffce1dd92abd012ef33dce0209230dcecdf
Lucas Yao <53558334+LucasYao93@users.noreply.github.com>
2022-11-24 14:07:00 +0800

    [module-cdn/generation-helpurl]replace docs help url. (#20115)
    * [Generation]replace docs help url.
    
    * Update SignatureIssues.csv

--- src/Cdn/custom/Update-AzCdnProfile.ps1
+++ src/Cdn/custom/Update-AzCdnProfile.ps1
@@ -53,7 +53,7 @@ INPUTOBJECT <ICdnIdentity>: Identity Parameter
   [SecurityPolicyName <String>]: Name of the security policy under the profile.
   [SubscriptionId <String>]: Azure Subscription ID.
 .Link
-https://docs.microsoft.com/powershell/module/az.cdn/update-azcdnprofile
+https://learn.microsoft.com/powershell/module/az.cdn/update-azcdnprofile
 #>
 function Update-AzCdnProfile {
     [OutputType([Microsoft.Azure.PowerShell.Cmdlets.Cdn.Models.Api20210601.IProfile])]
