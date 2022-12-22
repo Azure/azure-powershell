30d739b437acb11ee4fe787e90a8a303a381b7c8
Austin Tolani <austintolani@microsoft.com>
2022-12-22 14:13:30 -0800

    add preferredDataArchiveAuthMethod param to import/export

--- src/RedisCache/RedisCache/Models/RedisCacheClient.cs
+++ src/RedisCache/RedisCache/Models/RedisCacheClient.cs
@@ -280,10 +280,11 @@ namespace Microsoft.Azure.Commands.RedisCache
             );
         }
 
-        public void ImportToCache(string resourceGroupName, string cacheName, string[] blobUrisWithSasTokens, string format)
+        public void ImportToCache(string resourceGroupName, string cacheName, string[] blobUrisWithSasTokens, string format, string preferredDataArchiveAuthMethod)
         {
             ImportRDBParameters parameters = new ImportRDBParameters();
             parameters.Files = blobUrisWithSasTokens;
+            parameters.PreferredDataArchiveAuthMethod = preferredDataArchiveAuthMethod;
             if (!string.IsNullOrWhiteSpace(format))
             {
                 parameters.Format = format;
@@ -291,11 +292,12 @@ namespace Microsoft.Azure.Commands.RedisCache
             _client.Redis.ImportData(resourceGroupName: resourceGroupName, name: cacheName, parameters: parameters);
         }
 
-        public void ExportToCache(string resourceGroupName, string cacheName, string containerUrisWithSasTokens, string prefix, string format)
+        public void ExportToCache(string resourceGroupName, string cacheName, string containerUrisWithSasTokens, string prefix, string format, string preferredDataArchiveAuthMethod)
         {
             ExportRDBParameters parameters = new ExportRDBParameters();
             parameters.Container = containerUrisWithSasTokens;
             parameters.Prefix = prefix;
+            parameters.PreferredDataArchiveAuthMethod = preferredDataArchiveAuthMethod;
             if (!string.IsNullOrWhiteSpace(format))
             {
                 parameters.Format = format;
