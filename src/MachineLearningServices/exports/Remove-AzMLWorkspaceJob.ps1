89534dbb328df1efb5b1a0fbd429fb6fc94b9ec5
Lucas Yao <53558334+LucasYao93@users.noreply.github.com>
2023-01-03 16:33:54 +0800

    Update domain name of online docs
    Co-authored-by: dingmeng-xue <dixue@microsoft.com>

--- src/MachineLearningServices/exports/Remove-AzMLWorkspaceJob.ps1
+++ src/MachineLearningServices/exports/Remove-AzMLWorkspaceJob.ps1
@@ -48,7 +48,7 @@ INPUTOBJECT <IMachineLearningServicesIdentity>: Identity Parameter
   [Version <String>]: Version identifier. This is case-sensitive.
   [WorkspaceName <String>]: Name of Azure Machine Learning workspace.
 .Link
-https://docs.microsoft.com/powershell/module/az.machinelearningservices/remove-azmlworkspacejob
+https://learn.microsoft.com/powershell/module/az.machinelearningservices/remove-azmlworkspacejob
 #>
 function Remove-AzMLWorkspaceJob {
 [OutputType([System.Boolean])]
