d03cdc36718fb127a2a391bb1b56c4237ebe6702
ArmaanMcleod <armaan_mcleod@outlook.com>
2023-01-08 20:17:34 +1100

    Updated cmdlet and resource manager sdk client

---
+++ src/Resources/ResourceManager/SdkModels/Tags/TagOperators.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkModels
+{
+    public enum TagOperators
+    {
+        Any = 0,
+        All
+    }
+}
---
+++ src/Resources/ResourceManager/SdkModels/Tags/TagSettings.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Azure.Commands.ResourceManager.Common.Tags;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkModels
+{
+    public class TagSettings
+    {
+        public IDictionary<string, string> Tags { get; set; }
+
+        public TagOperators FilterOperator { get; set; }
+
+        public TagSettings() { }
+
+        public TagSettings(Hashtable tags, TagOperators filterOperator)
+        {
+            Tags = TagsConversionHelper.CreateTagDictionary(tags, validate: true);
+            FilterOperator = filterOperator;
+        }
+
+        public bool HasTags => Tags != null && Tags.Count > 0;
+    }
+}
