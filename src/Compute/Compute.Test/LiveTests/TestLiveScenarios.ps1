f7666138258227acb029426d9a6574cdb562f682
Alex-wdy <dayong_w@126.com>
2023-01-29 18:52:44 +0800

    update scenario test vm

--- src/Compute/Compute.Test/LiveTests/TestLiveScenarios.ps1
+++ src/Compute/Compute.Test/LiveTests/TestLiveScenarios.ps1
@@ -4,66 +4,21 @@ Invoke-LiveTestScenario -Name "Creates a virtual machine." -Description "Test cr
 
     $rgName = $rg.ResourceGroupName
     $name = New-LiveTestResourceName
-    $NICName = New-LiveTestResourceName
 
-    ## VM Account
-    # Credentials for Local Admin account you created in the sysprepped (generalized) vhd image
-    $VMLocalAdminUser = "LocalAdminUser"
-    $VMLocalAdminSecurePassword = ConvertTo-SecureString "Password" -AsPlainText -Force
-    ## Azure Account
-    $LocationName = "westus"
-    # This a Premium_LRS storage account.
-    # It is required in order to run a client VM with efficiency and high performance.
-    # $StorageAccount = "Mydisk"
-    $StorageAccount = New-LiveTestResourceName
-
-    ## VM
-    # $OSDiskName = "MyClient"
-    # $ComputerName = "MyClientVM"
-    $OSDiskName = New-LiveTestResourceName
-    $ComputerName = New-LiveTestResourceName
-    ##$OSDiskUri = "https://Mydisk.blob.core.windows.net/disks/MyOSDisk.vhd"
-    ##$SourceImageUri = "https://Mydisk.blob.core.windows.net/vhds/MyOSImage.vhd"
-
-    # Modern hardware environment with fast disk, high IOPs performance.
-    # Required to run a client VM with efficiency and performance
-    $VMSize = "Standard_DS3"
-    $OSDiskCaching = "ReadWrite"
-    $OSCreateOption = "FromImage"
-
-    ## Networking
-    # $DNSNameLabel = "mydnsname" # mydnsname.westus.cloudapp.azure.com
-    # $NetworkName = "MyNet"
-    # $NICName = "MyNIC"
-    # $PublicIPAddressName = "MyPIP"
-    # $SubnetName = "MySubnet"
-
-    $DNSNameLabel = New-LiveTestResourceName
-    $NetworkName = New-LiveTestResourceName
-    $NICName = New-LiveTestResourceName
-    $PublicIPAddressName = New-LiveTestResourceName
-    $SubnetName = New-LiveTestResourceName
-    $SubnetAddressPrefix = "10.0.0.0/24"
-    $VnetAddressPrefix = "10.0.0.0/16"
-
-    $SingleSubnet = New-AzVirtualNetworkSubnetConfig -Name $SubnetName -AddressPrefix $SubnetAddressPrefix
-    $Vnet = New-AzVirtualNetwork -Name $NetworkName -ResourceGroupName $rgName -Location $LocationName -AddressPrefix $VnetAddressPrefix -Subnet $SingleSubnet
-    $PIP = New-AzPublicIpAddress -Name $PublicIPAddressName -DomainNameLabel $DNSNameLabel -ResourceGroupName $rgName -Location $LocationName -AllocationMethod Dynamic
-    $NIC = New-AzNetworkInterface -Name $NICName -ResourceGroupName $rgName -Location $LocationName -SubnetId $Vnet.Subnets[0].Id -PublicIpAddressId $PIP.Id
-
+    $VMLocalAdminUser = New-LiveTestResourceName;
+    $VMLocalAdminSecurePassword = ConvertTo-SecureString "Password" -AsPlainText -Force;
+    $LocationName = "eastus";
+    $domainNameLabel = New-LiveTestResourceName;
     $Credential = New-Object System.Management.Automation.PSCredential ($VMLocalAdminUser, $VMLocalAdminSecurePassword);
+    $text = New-LiveTestResourceName;
+    $bytes = [System.Text.Encoding]::Unicode.GetBytes($text);
+    $userData = [Convert]::ToBase64String($bytes);
 
-    $VirtualMachine = New-AzVMConfig -VMName $name -VMSize $VMSize
-    $VirtualMachine = Set-AzVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $ComputerName -Credential $Credential -ProvisionVMAgent -EnableAutoUpdate
-    $VirtualMachine = Add-AzVMNetworkInterface -VM $VirtualMachine -Id $NIC.Id
-    ##$VirtualMachine = Set-AzVMOSDisk -VM $VirtualMachine -Name $OSDiskName -VhdUri $OSDiskUri -SourceImageUri $SourceImageUri -Caching $OSDiskCaching -CreateOption $OSCreateOption -Windows
-    $VirtualMachine = Set-AzVMOSDisk -VM $VirtualMachine -Name $OSDiskName -Caching $OSDiskCaching -CreateOption $OSCreateOption -Windows
-
-    $actual =  New-AzVM -ResourceGroupName $rgName -Location $LocationName -VM $VirtualMachine -Verbose
+    $actual =  New-AzVM -ResourceGroupName $rgName -Name $name -Credential $Credential -DomainNameLabel $domainNameLabel -UserData $userData;
 
     Assert-AreEqual $name $actual.Name
-    Assert-AreEqual $NICName $actual.NICName
-    Assert-AreEqual $SubnetName $actual.NICName
+    Assert-AreEqual $domainNameLabel $actual.DomainNameLabel
+    Assert-AreEqual $userData $actual.UserData
 }
 
 Invoke-LiveTestScenario -Name "Removes a virtual machine from Azure" -Description "Test removes a virtual machine from Azure."  -ScenarioScript `
@@ -72,60 +27,16 @@ Invoke-LiveTestScenario -Name "Removes a virtual machine from Azure" -Descriptio
 
     $rgName = $rg.ResourceGroupName
     $name = New-LiveTestResourceName
-    ## VM Account
-    # Credentials for Local Admin account you created in the sysprepped (generalized) vhd image
-    $VMLocalAdminUser = "LocalAdminUser"
-    $VMLocalAdminSecurePassword = ConvertTo-SecureString "Password" -AsPlainText -Force
-    ## Azure Account
-    $LocationName = "westus"
-    # This a Premium_LRS storage account.
-    # It is required in order to run a client VM with efficiency and high performance.
-    # $StorageAccount = "Mydisk"
-    $StorageAccount = New-LiveTestResourceName
-
-    ## VM
-    # $OSDiskName = "MyClient"
-    # $ComputerName = "MyClientVM"
-    $OSDiskName = New-LiveTestResourceName
-    $ComputerName = New-LiveTestResourceName
-    ##$OSDiskUri = "https://Mydisk.blob.core.windows.net/disks/MyOSDisk.vhd"
-    ##$SourceImageUri = "https://Mydisk.blob.core.windows.net/vhds/MyOSImage.vhd"
-
-    # Modern hardware environment with fast disk, high IOPs performance.
-    # Required to run a client VM with efficiency and performance
-    $VMSize = "Standard_DS3"
-    $OSDiskCaching = "ReadWrite"
-    $OSCreateOption = "FromImage"
-
-    ## Networking
-    # $DNSNameLabel = "mydnsname" # mydnsname.westus.cloudapp.azure.com
-    # $NetworkName = "MyNet"
-    # $NICName = "MyNIC"
-    # $PublicIPAddressName = "MyPIP"
-    # $SubnetName = "MySubnet"
-
-    $DNSNameLabel = New-LiveTestResourceName 
-    $NetworkName = New-LiveTestResourceName
-    $NICName = New-LiveTestResourceName
-    $PublicIPAddressName = New-LiveTestResourceName
-    $SubnetName = New-LiveTestResourceName
-    $SubnetAddressPrefix = "10.0.0.0/24"
-    $VnetAddressPrefix = "10.0.0.0/16"
-
-    $SingleSubnet = New-AzVirtualNetworkSubnetConfig -Name $SubnetName -AddressPrefix $SubnetAddressPrefix
-    $Vnet = New-AzVirtualNetwork -Name $NetworkName -ResourceGroupName $rgName -Location $LocationName -AddressPrefix $VnetAddressPrefix -Subnet $SingleSubnet
-    $PIP = New-AzPublicIpAddress -Name $PublicIPAddressName -DomainNameLabel $DNSNameLabel -ResourceGroupName $rgName -Location $LocationName -AllocationMethod Dynamic
-    $NIC = New-AzNetworkInterface -Name $NICName -ResourceGroupName $rgName -Location $LocationName -SubnetId $Vnet.Subnets[0].Id -PublicIpAddressId $PIP.Id
-
+    $VMLocalAdminUser = New-LiveTestResourceName;
+    $VMLocalAdminSecurePassword = ConvertTo-SecureString "Password" -AsPlainText -Force;
+    $LocationName = "eastus";
+    $domainNameLabel = New-LiveTestResourceName;
     $Credential = New-Object System.Management.Automation.PSCredential ($VMLocalAdminUser, $VMLocalAdminSecurePassword);
+    $text = New-LiveTestResourceName;
+    $bytes = [System.Text.Encoding]::Unicode.GetBytes($text);
+    $userData = [Convert]::ToBase64String($bytes);
 
-    $VirtualMachine = New-AzVMConfig -VMName $name -VMSize $VMSize
-    $VirtualMachine = Set-AzVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $ComputerName -Credential $Credential -ProvisionVMAgent -EnableAutoUpdate
-    $VirtualMachine = Add-AzVMNetworkInterface -VM $VirtualMachine -Id $NIC.Id
-    ##$VirtualMachine = Set-AzVMOSDisk -VM $VirtualMachine -Name $OSDiskName -VhdUri $OSDiskUri -SourceImageUri $SourceImageUri -Caching $OSDiskCaching -CreateOption $OSCreateOption -Windows
-    $VirtualMachine = Set-AzVMOSDisk -VM $VirtualMachine -Name $OSDiskName -Caching $OSDiskCaching -CreateOption $OSCreateOption -Windows
-
-    New-AzVM -ResourceGroupName $rgName -Location $LocationName -VM $VirtualMachine -Verbose
+    New-AzVM -ResourceGroupName $rgName -Name $name -Credential $Credential -DomainNameLabel $domainNameLabel -UserData $userData;
     Remove-AzVM -ResourceGroupName $rgName -Name $name -Force
 
     $removedVM = Get-AzVM -ResourceGroupName $rgName -Name $name
