5186f6c458eeabae6e6c1e8cbd1d6837b03e123f
Alex-wdy <dayong_w@126.com>
2023-01-30 23:45:43 +0800

    update location Storage live test

---
+++ src/Storage/Storage.Management.Test/LiveTests/TestLiveScenarios.ps1
@@ -0,0 +1,40 @@
+Invoke-LiveTestScenario -Name "Creates a Storage account" -Description "Test create storage account" -ScenarioScript `
+{
+    param ($rg)
+
+    $rgName = $rg.ResourceGroupName
+    $name = New-LiveTestResourceName
+    $location = "westus"
+
+    $sa = Get-AzStorageAccount -ResourceGroupName $rgName -Name $name  
+    if ($null -eq $sa) {
+        $actual = New-AzStorageAccount  -ResourceGroupName $rgName -Name $name  -Location $location -SkuName Standard_GRS 
+    }
+
+    Assert-AreEqual $name $actual.Name
+    Assert-AreEqual $rgName $actual.ResourceGroupName
+    Assert-AreEqual $vaultLocation $actual.Location
+    Assert-AreEqual "Standard_GRS" $actual.SkuName
+    Assert-AreEqual $false $actual.EnabledForDeployment
+    Assert-True { $actual.AllowBlobPublicAccess } "By default AllowBlobPublicAccess should be true"
+    Assert-Null $actual.AllowSharedKeyAccess "By default AllowSharedKeyAccess should be null"
+    Assert-False { $actual.EnableHttpsTrafficOnly } "By default EnableHttpsTrafficOnly should be false"
+}
+
+Invoke-LiveTestScenario -Name "Removes a Storage account" -Description "Test removes a Storage account from Azure." -ScenarioScript `
+{
+    param ($rg)
+
+    $rgName = $rg.ResourceGroupName
+    $name = New-LiveTestResourceName
+    $location = "westus"
+
+    New-AzStorageAccount -ResourceGroupName $rgname -Name $name -Location $location -SkuName Standard_GRS 
+    Remove-AzStorageAccount -ResourceGroupName $rgname -Name $name -Force
+
+    $removedAccount = Get-AzStorageAccount -ResourceGroupName $rgName -Name $name
+    Assert-Null $removedAccount
+
+
+    Remove-AzStorageAccount -ResourceGroupName $rgName -Name $name -Force
+}
