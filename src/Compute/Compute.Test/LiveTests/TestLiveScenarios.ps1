d0daaaa2ed3f1871c4258cca1d641d91c341d181
Alex <108926104+Alex-wdy@users.noreply.github.com>
2023-01-31 18:29:23 +0800

    Shared/livetest： Create livetest, storage and VM (#20678)
    * add scenario test vm
    
    * add scenario test Storage
    
    * update scenario test Storage
    
    * update scenario test vm
    
    * update scenario test Storage
    
    * update scenario test vm
    
    * update scenario test vm
    
    * update scenario test vm
    
    * update scenario test vm
    
    * update scenario test storage
    
    * update scenario test vm, update the variable
    
    * Shortened the random resource name to 15 in total
    
    * update scenario test vm
    
    * update scenario test vm
    
    * update scenario test vm
    
    * update scenario test vm
    
    * update location Storage live test
    
    * update location Storage live test
    
    * delete scenario test Storage.Test
    
    * msg
    
    * update location Storage live test
    
    * update location Storage live test
    
    * update location Storage live test
    
    ---------
    
    Co-authored-by: Vincent Dai <23257217+vidai-msft@users.noreply.github.com>

---
+++ src/Compute/Compute.Test/LiveTests/TestLiveScenarios.ps1
@@ -0,0 +1,45 @@
+Invoke-LiveTestScenario -Name "Creates a virtual machine." -Description "Test create new VM" -ScenarioScript `
+{
+    param ($rg)
+
+    $rgName = $rg.ResourceGroupName
+    $name = New-LiveTestResourceName
+
+    $VMLocalAdminUser = New-LiveTestResourceName;
+    $VMLocalAdminSecurePassword = ConvertTo-SecureString "Aalexwdy5#" -AsPlainText -Force;
+    $LocationName = "eastus";
+    $domainNameLabel = New-LiveTestResourceName;
+    $Credential = New-Object System.Management.Automation.PSCredential ($VMLocalAdminUser, $VMLocalAdminSecurePassword);
+    $text = New-LiveTestResourceName;
+    $bytes = [System.Text.Encoding]::Unicode.GetBytes($text);
+    $userData = [Convert]::ToBase64String($bytes);
+
+    $actual =  New-AzVM -ResourceGroupName $rgName -Name $name -Credential $Credential -DomainNameLabel $domainNameLabel -UserData $userData;
+
+    Assert-AreEqual $name $actual.Name
+    # Assert-AreEqual "Succeeded" Label $actual.ProvisioningState
+    # Assert-AreEqual $userData $actual.UserData
+}
+
+Invoke-LiveTestScenario -Name "Removes a virtual machine from Azure" -Description "Test removes a virtual machine from Azure."  -ScenarioScript `
+{
+    param ($rg)
+
+    $rgName = $rg.ResourceGroupName
+    $name = New-LiveTestResourceName
+
+    $VMLocalAdminUser = New-LiveTestResourceName;
+    $VMLocalAdminSecurePassword = ConvertTo-SecureString "Aalexwdy5#" -AsPlainText -Force;
+    $LocationName = "eastus";
+    $domainNameLabel = New-LiveTestResourceName;
+    $Credential = New-Object System.Management.Automation.PSCredential ($VMLocalAdminUser, $VMLocalAdminSecurePassword);
+    $text = New-LiveTestResourceName;
+    $bytes = [System.Text.Encoding]::Unicode.GetBytes($text);
+    $userData = [Convert]::ToBase64String($bytes);
+
+    New-AzVM -ResourceGroupName $rgName -Name $name -Credential $Credential -DomainNameLabel $domainNameLabel -UserData $userData;
+    Remove-AzVM -ResourceGroupName $rgName -Name $name -Force 
+
+    $removedVM = Get-AzVM -ResourceGroupName $rgName -Name $name -ErrorAction SilentlyContinue
+    Assert-Null $removedVM
+}
