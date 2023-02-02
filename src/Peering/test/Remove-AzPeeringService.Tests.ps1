eb9c793d1e8bed57edf345395f8d226520e63bf9
lijinpei2008 <31384087+lijinpei2008@users.noreply.github.com>
2023-02-02 10:57:08 +0800

    Create peering (#20826)

---
+++ src/Peering/test/Remove-AzPeeringService.Tests.ps1
@@ -0,0 +1,23 @@
+if(($null -eq $TestName) -or ($TestName -contains 'Remove-AzPeeringService'))
+{
+  $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+  if (-Not (Test-Path -Path $loadEnvPath)) {
+      $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+  }
+  . ($loadEnvPath)
+  $TestRecordingFile = Join-Path $PSScriptRoot 'Remove-AzPeeringService.Recording.json'
+  $currentPath = $PSScriptRoot
+  while(-not $mockingPath) {
+      $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+      $currentPath = Split-Path -Path $currentPath -Parent
+  }
+  . ($mockingPath | Select-Object -First 1).FullName
+}
+
+Describe 'Remove-AzPeeringService' {
+    It 'Delete' {
+        {
+            Remove-AzPeeringService -Name TestPeeringService -ResourceGroupName DemoRG
+        } | Should -Not -Throw
+    }
+}
