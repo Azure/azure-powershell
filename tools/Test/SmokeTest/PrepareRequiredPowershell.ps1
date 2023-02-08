381a41f6c1e39f4c274dcad5d766330a4d141a73
Beisi Zhou <bez@microsoft.com>
2023-02-08 16:09:20 +0800

    dir

--- tools/Test/SmokeTest/PrepareRequiredPowershell.ps1
+++ tools/Test/SmokeTest/PrepareRequiredPowershell.ps1
@@ -47,8 +47,8 @@ function Install-PowerShell {
 
   if($requiredPsVersion -match "7.2.*" -and $AgentOS -eq "Darwin"){
     Write-Host $AgentOS
-    Get-ChildItem $HOME/work/1/s/ -Recurse
-    copy-item $PSHOME/Microsoft.ApplicationInsights.dll  $HOME/work/1/s/.dotnet/tools/.store/powershell/7.2.9/tools/net6.0/any/unix
+    Get-ChildItem -Filter 7.2.9 -Directory -Recurse -Recurse
+    copy-item $PSHOME/Microsoft.ApplicationInsights.dll  $PSHOMEwork/1/s/.dotnet/tools/.store/powershell/7.2.9/tools/net6.0/any/unix
   }
 
   # Update PowershellGet to the latest one
