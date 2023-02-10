22e860b0331bef90a71148ca8d1118d7551d8cc5
Beisi Zhou <zhoubeisi@gmail.com>
2023-02-10 15:42:18 +0800

    Update PrepareRequiredPowershell.ps1

--- tools/Test/SmokeTest/PrepareRequiredPowershell.ps1
+++ tools/Test/SmokeTest/PrepareRequiredPowershell.ps1
@@ -47,7 +47,7 @@ function Install-PowerShell {
 
   if($requiredPsVersion -match "7.2.*" -and $AgentOS -eq "Darwin"){
       Write-Host "________________________________________________"
-     (Get-ChildItem $PowerShellPath/dotnet -Recurse).FullName  
+     (Get-ChildItem $Home -Filter powershell -Recurse).FullName  
      Write-Host "________________________________________________"
 
     copy-item $PSHOME/Microsoft.ApplicationInsights.dll  $PowerShellPath/dotnet/tools/.store/powershell/7.2.9/tools/net6.0/any/unix
