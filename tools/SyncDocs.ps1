d172d89f868bc0fe478d97fb89c6a7d8cc8c0589
NoriZC <110961157+NoriZC@users.noreply.github.com>
2023-02-17 10:56:52 +0800

    Update tools/SyncDocs.ps1
    Co-authored-by: Yunchi Wang <54880216+wyunchi-ms@users.noreply.github.com>

--- tools/SyncDocs.ps1
+++ tools/SyncDocs.ps1
@@ -28,8 +28,8 @@ foreach ($SyncPath in $Config.SyncPath)
 
 $SyncFile = Split-Path $SyncPath -Leaf
 
-git config --global user.email "azurepowershell@ms.com"
-git config --global user.name "azurepowershell"
+git config user.email "65331932+azure-powershell-bot@users.noreply.github.com"
+git config user.name "azure-powershell-bot"
 
 cd $WorkSpace
 git clone $RepoCloneLink
