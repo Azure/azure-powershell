2539be86031e3ea172abfe35fa01644a6d1af19a
azurepowershell <azurepowershell@ms.com>
2023-02-21 09:40:42 +0000

    Sync tools folder from main branch to generation branch

--- tools/ExecuteCIStep.ps1
+++ tools/ExecuteCIStep.ps1
@@ -50,6 +50,9 @@ Param(
     [Switch]
     $StaticAnalysisUX,
 
+    [Switch]
+    $StaticAnalysisCmdletDiff,
+
     [String]
     $RepoArtifacts='artifacts',
 
@@ -239,6 +242,7 @@ If ($StaticAnalysis)
     .("$PSScriptRoot/ExecuteCIStep.ps1") -StaticAnalysisSignature @Parameters
     .("$PSScriptRoot/ExecuteCIStep.ps1") -StaticAnalysisHelp @Parameters
     .("$PSScriptRoot/ExecuteCIStep.ps1") -StaticAnalysisUX @Parameters
+    .("$PSScriptRoot/ExecuteCIStep.ps1") -StaticAnalysisCmdletDiff @Parameters
     Return
 }
 
@@ -328,6 +332,24 @@ If ($StaticAnalysisUX)
     {
         Write-Host "Running static analysis for UX metadata..."
         dotnet $RepoArtifacts/StaticAnalysis/StaticAnalysis.Netcore.dll -p $RepoArtifacts/$Configuration -r $StaticAnalysisOutputDirectory --analyzers ux -u -m $UXModuleList
+    }
+    Return
+}
+
+If ($StaticAnalysisCmdletDiff)
+{
+    If ($PSBoundParameters.ContainsKey("TargetModule"))
+    {
+        $CmdletDiffModuleList = $TargetModule
+    }
+    Else
+    {
+        $CmdletDiffModuleList = Join-String -Separator ';' -InputObject $CIPlan.'cmdlet-diff'
+    }
+    If ("" -Ne $CmdletDiffModuleList)
+    {
+        Write-Host "Running static analysis for cmdlet diff..."
+        dotnet $RepoArtifacts/StaticAnalysis/StaticAnalysis.Netcore.dll -p $RepoArtifacts/$Configuration -r $StaticAnalysisOutputDirectory --analyzers cmdlet-diff -u -m $CmdletDiffModuleList
     }
     Return
 }
