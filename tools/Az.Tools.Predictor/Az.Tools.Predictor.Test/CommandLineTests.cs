a52e210e921846115fb323c44b0dea73f0fe3981
kceiw <mahuang@microsoft.com>
2023-02-27 21:47:28 -0800

    Mask the whole command in telemetry when it's not supported (#21038)
    * Mask the whole command when we don't recognize it.
    
    * Rename the property field names.

--- tools/Az.Tools.Predictor/Az.Tools.Predictor.Test/CommandLineTests.cs
+++ tools/Az.Tools.Predictor/Az.Tools.Predictor.Test/CommandLineTests.cs
@@ -59,17 +59,20 @@ namespace Microsoft.Azure.PowerShell.Tools.AzPredictor.Test
         [InlineData("Get-AzResourceGroup -Location:*** -Name ***", "Get-AzResourceGroup -Name Test -Location:WestUS2")]
         // skip the positional parameter
         [InlineData("Get-AzResourceGroup -Location:***", "Get-AzResourceGroup Test -Location:WestUS2")]
+        [InlineData("Get-AzResourceGroup", "Get-AzResourceGroup Test WestUS2")]
+        // Take the last command in a pipe
+        [InlineData("Set-AzContext", "$context | Set-AzContext")]
+        // Take the command at the right of assignment
+        [InlineData("Get-AzRoleAssignment -ObjectId ***", "$a=Get-AzRoleAssignment -ObjectId xxx")]
+        // Take the command inside parentheses
+        [InlineData("Get-AzRoleAssignment -ObjectId ***", "Remove-AzRoleAssignment -RoleDefinitionId (Get-AzRoleAssignment -ObjectId xxx")]
         [InlineData("git", "git status")]
         [InlineData("customCommand -Parameter ***", "customCommand -Parameter Value")]
         [InlineData("customCommand", "customCommand /Parameter Value")]
         [InlineData("customCommand", "customCommand --Parameter Value")]
         public void VerifyMaskCommandLine(string expected, string input)
         {
-            var asts = Parser.ParseInput(input, out _, out _);
-            var allNestedAsts = asts?.FindAll((ast) => ast is CommandAst, true);
-            var commandAst = allNestedAsts?.LastOrDefault() as CommandAst;
-
-            string maskedCommandLine = CommandLineUtilities.MaskCommandLine(commandAst);
+            string maskedCommandLine = CommandLineUtilities.MaskCommandLine(input);
 
             Assert.Equal(expected, maskedCommandLine);
         }
@@ -82,7 +85,7 @@ namespace Microsoft.Azure.PowerShell.Tools.AzPredictor.Test
         [InlineData("Set-AzContext -Subscription 'xxxx-xxxx-xxxx-xxxx' -Tenant {String}", null)]
         [InlineData("Set-AzContext -Subscription $subscription -Tenant [[String]]", null)]
         [InlineData("Set-AzContext -Subscription 'xxxx-xxxx-xxxx-xxxx' -Tenant TenantName", null)]
-        [InlineData("Get-AzContext | Set-AzContext", "Get-AzContext")]
+        [InlineData("Get-AzContext | Set-AzContext", "Set-AzContext")]
         public void VerifyGetCommandAst(string input, string expected)
         {
             if (expected == null)
