a7e20dcd9eeed6fda7cc491747b55bb9be6c082d
michaelshikh <michaelshikh@microsoft.com>
2023-03-15 19:16:24 +0200

    sku capacity 1

--- src/Kusto/test/Update-AzKustoCluster.Tests.ps1
+++ src/Kusto/test/Update-AzKustoCluster.Tests.ps1
@@ -29,6 +29,6 @@ Describe 'Update-AzKustoCluster' {
     It 'UpdateViaIdentityExpanded' {
         $clusterGetItem = Get-AzKustoCluster -ResourceGroupName $env.resourceGroupName -Name $env.kustoClusterName
         $updatedCluster = Update-AzKustoCluster -InputObject $clusterGetItem -SkuName $env.kustoSkuName -SkuTier $env.kustoClusterTier
-        Validate_Cluster $updatedCluster $env.kustoClusterName $env.location "Running" "Succeeded" "Microsoft.Kusto/Clusters" $env.kustoSkuName $env.kustoClusterTier 2
+        Validate_Cluster $updatedCluster $env.kustoClusterName $env.location "Running" "Succeeded" "Microsoft.Kusto/Clusters" $env.kustoSkuName $env.kustoClusterTier 1
     }
 }
