f08c452803931ddee846a90bda7e61502156e094
Suhani Shrivastava <suhani1396@gmail.com>
2023-03-17 15:22:18 +0530

    Adding recording and test for create monitor
    Adding recording and test for create monitor

--- src/Workloads/test/New-AzWorkloadsMonitor.Tests.ps1
+++ src/Workloads/test/New-AzWorkloadsMonitor.Tests.ps1
@@ -15,11 +15,19 @@ if(($null -eq $TestName) -or ($TestName -contains 'New-AzWorkloadsMonitor'))
 }
 
 Describe 'New-AzWorkloadsMonitor' {
-    It 'CreateExpanded' -skip{
-        $monitor = New-AzWorkloadsMonitor -ResourceGroupName PowerShell-CLI-TestRG -Name powershellmonitor09 -Location eastus2euap `
-        -AppLocation eastus -ManagedResourceGroupName powershellmonitor09-mrg `
-        -MonitorSubnet "/subscriptions/49d64d54-e966-4c46-a868-1999802b762c/resourceGroups/PowerShell-CLI-TestRG/providers/Microsoft.Network/virtualNetworks/lucas-workloads-vnet/subnets/subnet03" `
+    It 'CreateMonitorExpanded' {
+        $monitor = New-AzWorkloadsMonitor -ResourceGroupName $env.MonitorRg -Name $env.CreateMonitorName -Location eastus2euap `
+        -AppLocation eastus -ManagedResourceGroupName mrg-170313 `
+        -MonitorSubnet $env.MonitorSubnet `
         -RoutingPreference 'RouteAll' -ZoneRedundancyPreference Disabled
         $monitor.ProvisioningState | Should -Be "Succeeded"
+    }
+
+    It 'CreateHaMonitorExpanded' {
+        $monitorHa = New-AzWorkloadsMonitor -ResourceGroupName $env.MonitorRg -Name $env.CreateHaMonitorName -Location eastus2euap `
+        -AppLocation eastus -ManagedResourceGroupName mrg-1703133 `
+        -MonitorSubnet $env.HaMonitorSubnet `
+        -RoutingPreference 'RouteAll' -ZoneRedundancyPreference Disabled
+        $monitorHa.ProvisioningState | Should -Be "Succeeded"
     }
 }
