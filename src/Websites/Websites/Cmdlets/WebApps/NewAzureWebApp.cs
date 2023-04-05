9c41dfd9ade374780167038d2bf91d03904acf29
Kota Sudhakar Reddy (Accenture) <v-kotred@microsoft.com>
2023-04-05 13:01:59 +0530

    [AppService]: fix #21399 - [New-AzWebApp] -Tag parameter not available in all ParameterSets

--- src/Websites/Websites/Cmdlets/WebApps/NewAzureWebApp.cs
+++ src/Websites/Websites/Cmdlets/WebApps/NewAzureWebApp.cs
@@ -137,7 +137,7 @@ namespace Microsoft.Azure.Commands.WebApps.Cmdlets.WebApps
         [Parameter(Mandatory = false, HelpMessage = "Path to the GitHub repository containign the web application to deploy.", ParameterSetName = SimpleParameterSet)]
         public string GitRepositoryPath { get; set; }
 
-        [Parameter(Mandatory = false, HelpMessage = "Tags are name/value pairs that enable you to categorize resources", ParameterSetName = SimpleParameterSet)]
+        [Parameter(Mandatory = false, HelpMessage = "Tags are name/value pairs that enable you to categorize resources")]
         public Hashtable Tag { get; set; }
 
         protected override void ProcessRecord()
@@ -236,7 +236,7 @@ namespace Microsoft.Azure.Commands.WebApps.Cmdlets.WebApps
 
             try
             {
-                WriteObject(new PSSite(WebsitesClient.CreateWebApp(ResourceGroupName, Name, null, Location, AppServicePlan, cloningInfo, AseName, AseResourceGroupName)));
+                WriteObject(new PSSite(WebsitesClient.CreateWebApp(ResourceGroupName, Name, null, Location, AppServicePlan, cloningInfo, AseName, AseResourceGroupName, (IDictionary<string, string>)CmdletHelpers.ConvertToStringDictionary(Tag))));
             }
             catch (Exception e)
             {
