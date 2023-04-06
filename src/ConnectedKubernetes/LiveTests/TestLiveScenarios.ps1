f2972069bd945de7d871469e0514050d27cc6f09
Nanxiang Liu <nanxiangliu@microsoft.com>
2023-04-06 19:06:20 +0800

    add test for connectedk8s

---
+++ src/ConnectedKubernetes/LiveTests/TestLiveScenarios.ps1
@@ -0,0 +1,26 @@
+Invoke-LiveTestScenario -Name "Test ConnectedKubernetes" -Description "Test New-AzConnectedKubernetes" -ScenarioScript `
+{
+    param ($rg)
+    $rgName = $rg.ResourceGroupName
+    $ckName = New-LiveTestResourceName
+    kind create cluster --name $ckName
+    $ckLocation = "westus"
+    $actual = New-AzConnectedKubernetes -ClusterName $ckName -ResourceGroupName $rgName -Location $ckLocation
+    Assert-AreEqual $ckName $actual.Name
+    Assert-AreEqual $ckLocation $actual.Location
+
+    $actual = Get-AzConnectedKubernetes -ResourceGroupName $rgName
+    Assert-AreEqual 1 $actual.Count
+
+    $actual = Get-AzConnectedKubernetes -ResourceGroupName $rgName -ClusterName $ckName
+    Assert-AreEqual $ckName $actual.Name
+
+    $tag = @{'key'='1'}
+    $null = Update-AzConnectedKubernetes -ClusterName $ckName -ResourceGroupName $rgName -Tag $tag
+    $actual = Get-AzConnectedKubernetes -ResourceGroupName $rgName -ClusterName $ckName
+    Assert-AreEqual $actual.Tag["key"] "1"
+
+    Remove-AzConnectedKubernetes -ResourceGroupName $rgName -Name $ckName
+    $GetServiceList = Get-AzConnectedKubernetes -ResourceGroupName $rgName
+    Assert-False { $GetServiceList.Name -contains $rgName}
+}
