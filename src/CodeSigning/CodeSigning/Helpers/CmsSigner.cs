967d37d8414076c4dbfed7f8bf11bad29183f949
Dawn Wang <dawang@microsoft.com>
2023-04-05 21:24:02 -0700

    fixed retry and added metadata json file supports

--- src/CodeSigning/CodeSigning/Helpers/CmsSigner.cs
+++ src/CodeSigning/CodeSigning/Helpers/CmsSigner.cs
@@ -20,51 +20,64 @@ namespace Microsoft.Azure.Commands.CodeSigning.Helpers
             string endpointUrl, string unsignedCIFilePath, string signedCIFilePath, string timeStamperUrl)
         {
             int retry = 5;
-            while (retry-- > 0)
+
+            while (retry > 0)
             {
-                var context = new AzCodeSignContext(tokenCred, accountName, certProfile, endpointUrl);
-
-                var cert = context.InitializeChainAsync().Result;
-                RSA rsa = new RSAAzCodeSign(context);
-
-                var cipolicy = File.ReadAllBytes(unsignedCIFilePath);
-                var cmscontent = new ContentInfo(new Oid("1.3.6.1.4.1.311.79.1"), cipolicy);
-                var cms = new SignedCms(cmscontent, false);
-
-                var signer = new System.Security.Cryptography.Pkcs.CmsSigner(SubjectIdentifierType.IssuerAndSerialNumber, cert, rsa);
-                cms.ComputeSignature(signer);
-
-                cms.CheckSignature(true);
-                //Console.WriteLine(Util.BytesToHex(cms.Encode(), " ", 16));
-
-                var signedData = cms.Encode();
-
-                if (!string.IsNullOrWhiteSpace(timeStamperUrl))
+                try
                 {
-                    var timestampingUri = new Uri("http://www.microsoft.com");
-                    try
+                    var context = new AzCodeSignContext(tokenCred, accountName, certProfile, endpointUrl);
+
+                    var cert = context.InitializeChainAsync().Result;
+                    RSA rsa = new RSAAzCodeSign(context);
+
+                    var cipolicy = File.ReadAllBytes(unsignedCIFilePath);
+                    var cmscontent = new ContentInfo(new Oid("1.3.6.1.4.1.311.79.1"), cipolicy);
+                    var cms = new SignedCms(cmscontent, false);
+
+                    var signer = new System.Security.Cryptography.Pkcs.CmsSigner(SubjectIdentifierType.IssuerAndSerialNumber, cert, rsa);
+                    cms.ComputeSignature(signer);
+
+                    cms.CheckSignature(true);
+                    //Console.WriteLine(Util.BytesToHex(cms.Encode(), " ", 16));
+
+                    var signedData = cms.Encode();
+
+                    if (!string.IsNullOrWhiteSpace(timeStamperUrl))
                     {
-                        timestampingUri = new Uri(timeStamperUrl);
-
-                        var signedAndTimestampedFullFileContents = TimeStampingHelper.Rfc3161Timestamp(
-                            input: signedData,
-                            timestampServerUrl: timestampingUri.ToString());
-
-                        if (signedAndTimestampedFullFileContents == null)
+                        var timestampingUri = new Uri("http://www.microsoft.com");
+                        try
                         {
-                            throw new Exception("Timestamping failed. ");
-                        }
+                            timestampingUri = new Uri(timeStamperUrl);
 
-                        File.WriteAllBytes(signedCIFilePath, signedAndTimestampedFullFileContents); ;
+                            var signedAndTimestampedFullFileContents = TimeStampingHelper.Rfc3161Timestamp(
+                                input: signedData,
+                                timestampServerUrl: timestampingUri.ToString());
+
+                            if (signedAndTimestampedFullFileContents == null)
+                            {
+                                throw new Exception("Timestamping failed. ");
+                            }
+
+                            File.WriteAllBytes(signedCIFilePath, signedAndTimestampedFullFileContents); ;
+                        }
+                        catch
+                        {
+                            throw new Exception("Input TimeStamperUrl is not valid Uri. Please check.");
+                        }
                     }
-                    catch
+                    else
                     {
-                        throw new Exception("Input TimeStamperUrl is not valid Uri. Please check.");
+                        File.WriteAllBytes(signedCIFilePath, signedData);
                     }
+                    return;
                 }
-                else
+                catch(Exception ex)
                 {
-                    File.WriteAllBytes(signedCIFilePath, signedData);
+                    retry--;
+                    if (retry == 0 || ex.Message == "Input TimeStamperUrl is not valid Uri. Please check.")
+                    { 
+                        throw ex;
+                    }
                 }
             }
         }
