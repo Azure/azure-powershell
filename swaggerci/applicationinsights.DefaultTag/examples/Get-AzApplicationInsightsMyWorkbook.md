8c8998c41bc6e1fbaff6c07ba3979c15d9449acc
SDKAuto <sdkautomation@microsoft.com>
2023-04-20 05:50:22 +0000

    CodeGen from PR 23639 in Azure/azure-rest-api-specs Update readme.python.md (#23639)
    * Update readme.python.md
    
    * Update readme.python.md

---
+++ swaggerci/applicationinsights.DefaultTag/examples/Get-AzApplicationInsightsMyWorkbook.md
@@ -0,0 +1,22 @@
+### Example 1: {{ Add title here }}
+```powershell
+{{ Add code here }}
+```
+
+```output
+{{ Add output here }}
+```
+
+{{ Add description here }}
+
+### Example 2: {{ Add title here }}
+```powershell
+{{ Add code here }}
+```
+
+```output
+{{ Add output here }}
+```
+
+{{ Add description here }}
+
---
+++ swaggerci/applicationinsights.DefaultTag/test/Add-AzApplicationInsightsFavorite.Tests.ps1
@@ -0,0 +1,33 @@
+if(($null -eq $TestName) -or ($TestName -contains 'Add-AzApplicationInsightsFavorite'))
+{
+  $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+  if (-Not (Test-Path -Path $loadEnvPath)) {
+      $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+  }
+  . ($loadEnvPath)
+  $TestRecordingFile = Join-Path $PSScriptRoot 'Add-AzApplicationInsightsFavorite.Recording.json'
+  $currentPath = $PSScriptRoot
+  while(-not $mockingPath) {
+      $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+      $currentPath = Split-Path -Path $currentPath -Parent
+  }
+  . ($mockingPath | Select-Object -First 1).FullName
+}
+
+Describe 'Add-AzApplicationInsightsFavorite' {
+    It 'AddExpanded' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'Add' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'AddViaIdentityExpanded' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'AddViaIdentity' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+}
