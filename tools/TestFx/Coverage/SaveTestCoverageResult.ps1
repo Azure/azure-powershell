39f0c117e7b145a8cb6adcdf47a19ada4d0a1acd
Vincent Dai <23257217+vidai-msft@users.noreply.github.com>
2023-04-26 20:19:40 +0800

    Fixed Kusto util arguments issue and removed the unnecessary parameters from the list

--- tools/TestFx/Coverage/SaveTestCoverageResult.ps1
+++ tools/TestFx/Coverage/SaveTestCoverageResult.ps1
@@ -5,55 +5,39 @@ param (
 
     [Parameter(Mandatory, Position = 1)]
     [ValidateNotNullOrEmpty()]
-    [string] $BuildId,
+    [guid] $KustoServicePrincipalTenantId,
 
     [Parameter(Mandatory, Position = 2)]
     [ValidateNotNullOrEmpty()]
-    [guid] $KustoServicePrincipalTenantId,
+    [guid] $KustoServicePrincipalId,
 
     [Parameter(Mandatory, Position = 3)]
     [ValidateNotNullOrEmpty()]
-    [guid] $KustoServicePrincipalId,
+    [string] $KustoServicePrincipalSecret,
 
     [Parameter(Mandatory, Position = 4)]
     [ValidateNotNullOrEmpty()]
-    [string] $KustoServicePrincipalSecret,
+    [string] $ClusterName,
 
     [Parameter(Mandatory, Position = 5)]
     [ValidateNotNullOrEmpty()]
-    [string] $ClusterName,
-
-    [Parameter(Mandatory, Position = 6)]
-    [ValidateNotNullOrEmpty()]
-    [string] $ClusterRegion,
-
-    [Parameter(Mandatory, Position = 7)]
-    [ValidateNotNullOrEmpty()]
-    [string] $DatabaseName,
-
-    [Parameter(Mandatory, Position = 8)]
-    [ValidateNotNullOrEmpty()]
-    [string] $TableName,
-
-    [Parameter(Mandatory, Position = 9)]
-    [ValidateNotNull()]
-    [ValidateScript({ Test-Path -LiteralPath $_ -PathType Container })]
-    [string] $DataLocation
+    [string] $ClusterRegion
 )
 
 $script:AzPSCommonParameters = @("-Break", "-Confirm", "-Debug", "-DefaultProfile", "-ErrorAction", "-ErrorVariable", "-HttpPipelineAppend", "-HttpPipelinePrepend", "-InformationAction", "-InformationVariable",
     "-OutBuffer", "-OutVariable", "-PassThru", "-PipelineVariable", "-Proxy", "-ProxyCredential", "-ProxyUseDefaultCredentials", "-Verbose", "-WarningAction", "-WarningVariable", "-WhatIf")
 
-$cvgDir = Join-Path -Path $DataLocation -ChildPath "TestCoverageAnalysis" | Join-Path -ChildPath "Raw"
+$cvgDir = Join-Path -Path ${env:TESTCOVERAGELOCATION} -ChildPath "TestCoverageAnalysis" | Join-Path -ChildPath "Raw"
 if (Test-Path -LiteralPath $cvgDir -PathType Container) {
-    Import-Module (Join-Path -Path ($PSScriptRoot | Split-Path) -ChildPath "Utilities" | Join-Path -ChildPath "KustoUtility.psd1") -Force
+    $kustoUtil = Join-Path -Path ($PSScriptRoot | Split-Path) -ChildPath "Utilities" | Join-Path -ChildPath "KustoUtility.psd1"
+    Import-Module $kustoUtil -ArgumentList $KustoServicePrincipalTenantId, $KustoServicePrincipalId, $KustoServicePrincipalSecret, $ClusterName, $ClusterRegion -Force
 
     $cvgRawCsv = Get-ChildItem -Path $cvgDir -Filter "*.csv" -File | Select-Object -ExpandProperty FullName
     $cvgRawCsv | ForEach-Object {
         (Import-Csv -Path $_) |
         Select-Object `
         @{ Name = "Source"; Expression = { $Source } }, `
-        @{ Name = "BuildId"; Expression = { $BuildId } }, `
+        @{ Name = "BuildId"; Expression = { ${env:BUILD_BUILDID} } }, `
         @{ Name = "Module"; Expression = { $_.Module } }, `
         @{ Name = "CommandName"; Expression = { $_.CommandName } }, `
         @{ Name = "TotalCommands"; Expression = { $_.TotalCommands } }, `
@@ -69,15 +53,7 @@ if (Test-Path -LiteralPath $cvgDir -PathType Container) {
         Export-Csv -Path $_ -Encoding utf8 -NoTypeInformation -Force
     }
 
-    Import-KustoDataFromCsv `
-        -ServicePrincipalTenantId $KustoServicePrincipalTenantId `
-        -ServicePrincipalId $KustoServicePrincipalId `
-        -ServicePrincipalSecret $KustoServicePrincipalSecret `
-        -ClusterName $ClusterName `
-        -ClusterRegion $ClusterRegion `
-        -DatabaseName $DatabaseName `
-        -TableName $TableName `
-        -CsvFile $cvgRawCsv
+    Import-KustoDataFromCsv -DatabaseName ${env:TESTCOVERAGEDATABASENAME} -TableName ${env:TESTCOVERAGETABLENAME} -CsvFile $cvgRawCsv
 }
 else {
     Write-Host "##[warning]No test coverage data was found." -ForegroundColor Yellow
