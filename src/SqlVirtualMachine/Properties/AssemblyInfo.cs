e75a1456e526725c297d192e08987e5b35414c3c
YanaXu <sheshouxy@gmail.com>
2023-05-08 16:15:18 +0800

    prepare for merging code to main branch

--- src/SqlVirtualMachine/Properties/AssemblyInfo.cs
+++ src/SqlVirtualMachine/Properties/AssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
 [assembly: Guid("75a22dbf-0558-441c-8bb3-125816be65f3")]
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyVersion("1.1.1")]
+[assembly: AssemblyFileVersion("1.1.1")]
