00e4d4d780fe36aa99a1191eb27f332b7d1afbba
Srinikhil Naravamakula <nanarava@microsoft.com>
2023-05-10 15:44:14 -0500

    CosmosDB: Adds changes to handle enabling partition merge on CosmosDB account.

--- src/CosmosDB/CosmosDB.Test/ScenarioTests/AccountTests.ps1
+++ src/CosmosDB/CosmosDB.Test/ScenarioTests/AccountTests.ps1
@@ -72,10 +72,6 @@ function Test-AccountRelatedCmdlets
   Assert-AreEqual $updatedCosmosDBAccount.NetworkAclBypassResourceIds.Count 1
   Assert-AreEqual $updatedCosmosDBAccount.BackupPolicy.BackupIntervalInMinutes 480
   Assert-AreEqual $updatedCosmosDBAccount.BackupPolicy.BackupRetentionIntervalInHours 16
-  Assert-AreEqual $updatedCosmosDBAccount.EnablePartitionMerge 0
-
-  $updatedCosmosDBAccount = Update-AzCosmosDBAccount -ResourceGroupName $rgName -Name $cosmosDBAccountName -EnableParitionMerge 1
-  Assert-AreEqual $updatedCosmosDBAccount.EnablePartitionMerge 1
 
   $updatedCosmosDBAccount = Update-AzCosmosDBAccount -ResourceGroupName $rgName -Name $cosmosDBAccountName -EnableParitionMerge 0
   Assert-AreEqual $updatedCosmosDBAccount.EnablePartitionMerge 0
