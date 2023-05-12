d5595c302753d941997fa83c52817efe3e80163d
wyunchi-ms <yunwang@microsoft.com>
2023-05-12 10:27:40 +0800

    update

--- tools/CleanupBuild.ps1
+++ tools/CleanupBuild.ps1
@@ -79,7 +79,9 @@ foreach($RMPath in $resourceManagerPaths)
             # netcoreapp, can't be in RequiredAssemblies, but we need to pack it
             "Microsoft.Azure.PowerShell.AuthenticationAssemblyLoadContext.dll",
             # customized AutoMapper
-            "Microsoft.Azure.PowerShell.AutoMapper.dll"
+            "Microsoft.Azure.PowerShell.AutoMapper.dll",
+            "System.Security.Cryptography.Pkcs.dll",
+            "System.Formats.Asn1.dll"
         )
 
         # NestedModule Assemblies may have a folder path, just getting the dll name alone
