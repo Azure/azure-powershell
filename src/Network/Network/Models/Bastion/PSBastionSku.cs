931da0abaae43de1449590fd4a5f4b12dbb0bd82
Abhishek Shah <shabhis@microsoft.com>
2023-06-05 18:59:47 -0700

    Optimize for sku and features

--- src/Network/Network/Models/Bastion/PSBastionSku.cs
+++ src/Network/Network/Models/Bastion/PSBastionSku.cs
@@ -1,6 +1,9 @@
 ﻿namespace Microsoft.Azure.Commands.Network.Models.Bastion
 {
     using Microsoft.WindowsAzure.Commands.Common.Attributes;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using MNM = Management.Network.Models;
 
     public class PSBastionSku
@@ -8,14 +11,39 @@
         [Ps1Xml(Target = ViewControl.List)]
         public string Name { get; set; }
 
+        public const string Basic = "Basic";
+        public const string Standard = "Standard";
+
         public PSBastionSku(string skuName = null)
         {
-            if (string.IsNullOrEmpty(skuName) || string.IsNullOrWhiteSpace(skuName))
+            if (string.IsNullOrWhiteSpace(skuName))
             {
-                this.Name = MNM.BastionHostSkuName.Basic;
+                this.Name = MNM.BastionHostSkuName.Standard;
             }
 
-            this.Name = skuName;
+            this.Name = GetSkuTier().FirstOrDefault(sku => sku.Equals(skuName, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public static IEnumerable<string> GetSkuTier()
+        {
+            yield return Basic;
+            yield return Standard;
+        }
+
+        public static bool TryGetSkuTier(string skuTier, out string skuTierValue)
+        {
+            skuTierValue = null;
+            if (string.Equals(skuTier, Basic, StringComparison.OrdinalIgnoreCase))
+            {
+                skuTierValue = Basic;
+                return true;
+            }
+            if (string.Equals(skuTier, Standard, StringComparison.OrdinalIgnoreCase))
+            {
+                skuTierValue = Standard;
+                return true;
+            }
+            return false;
         }
     }
 }
