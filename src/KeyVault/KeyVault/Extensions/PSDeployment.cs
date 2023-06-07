d27cb88abcbac7a874e693c96baeed3cc2073f49
NoriZC <110961157+NoriZC@users.noreply.github.com>
2023-06-07 20:50:40 +0800

    Private preview for arm template (#21959) (#22014)
    * Refresh the kv arm template branch to publish a private preview (#21959)
    
    * First commit of arm template implement
    
    * With Proress bar.
    
    * Fake progress bar for deployment validation
    
    * unexpected modifications
    
    * Add progress bar for validation and strating creation.
    
    * Revert "unexpected modifications"
    
    This reverts commit 2ab0409d2be09e171bbd6c959770aaa3eb00df75.
    
    * Polish progress bar timespan.
    
    * Set softdelete days as 90 and fix network property in whatif
    
    * Handle errors better.
    
    * Fix whatif network.
    
    * Add assembly in psd1 file
    
    * add -FailOnExist, api version, polish progress bar
    
    * Add progress bar for whatif. Add rotated bar.
    
    * NamingStrategy = new CamelCaseNamingStrategy()
    
    * Resolve enablePergeProtection and SKU issue
    
    * Resolve unthrown error issue
    
    * comment unpassed case
    
    * comment unpassed case
    
    * comment RBC unpassed case
    
    * comment RBC unpassed case
    
    * record unpassed case in kv
    
    * compute test case (#22013)
    
    * First commit of arm template implement
    
    * With Proress bar.
    
    * Fake progress bar for deployment validation
    
    * unexpected modifications
    
    * Add progress bar for validation and strating creation.
    
    * Revert "unexpected modifications"
    
    This reverts commit 2ab0409d2be09e171bbd6c959770aaa3eb00df75.
    
    * Polish progress bar timespan.
    
    * Set softdelete days as 90 and fix network property in whatif
    
    * Handle errors better.
    
    * Fix whatif network.
    
    * Add assembly in psd1 file
    
    * add -FailOnExist, api version, polish progress bar
    
    * Add progress bar for whatif. Add rotated bar.
    
    * NamingStrategy = new CamelCaseNamingStrategy()
    
    * Resolve enablePergeProtection and SKU issue
    
    * Resolve unthrown error issue
    
    * comment unpassed case
    
    * comment unpassed case
    
    * comment RBC unpassed case
    
    * comment RBC unpassed case
    
    * record unpassed case in kv
    
    * try to commit after removing secrets
    
    * try to commit after removing secrets

---
+++ src/KeyVault/KeyVault/Extensions/PSDeployment.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.PowerShell.Cmdlets.KeyVault.Helpers.Resources.Models;
+
+namespace Microsoft.Azure.Commands.KeyVault.Extensions
+{
+    public class PSDeployment : PSDeploymentObject
+    {
+        public string Id { get; set; }
+
+        public string Location { get; set; }
+
+        public string ManagementGroupId { get; set; }
+
+        public string ResourceGroupName { get; set; }
+
+        public OnErrorDeploymentExtended OnErrorDeployment { get; set; }
+    }
+}
---
+++ src/KeyVault/KeyVault/Extensions/PSResourceGroup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace Microsoft.Azure.Commands.KeyVault.Extensions
+{
+    public class PSResourceGroup
+    {
+        public string ResourceGroupName { get; set; }
+
+        public string Location { get; set; }
+
+        public string ProvisioningState { get; set; }
+
+        public Hashtable Tags { get; set; }
+
+        public string TagsTable
+        {
+            get { return ResourcesExtensions.ConstructTagsTable(Tags); }
+        }
+
+        public string ResourceId { get; set; }
+
+        public string ManagedBy { get; set; }
+    }
+}
