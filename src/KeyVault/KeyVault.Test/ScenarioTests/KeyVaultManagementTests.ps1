8be38d9457df45b37bc07e496c4b8029b6850ab2
Nori Zhang <norizhang@microsoft.com>
2023-06-07 11:17:47 +0800

    comment RBC unpassed case

--- src/KeyVault/KeyVault.Test/ScenarioTests/KeyVaultManagementTests.ps1
+++ src/KeyVault/KeyVault.Test/ScenarioTests/KeyVaultManagementTests.ps1
@@ -827,8 +827,8 @@ function Test-UpdateKeyVault {
         # Assert-True { $vault.EnableRbacAuthorization } "5. EnableRbacAuthorization should be true"
 
         #Set EnableRbacAuthorization false
-        $vault = $vault | Update-AzKeyVault -EnableRbacAuthorization $false
-        Assert-False { $vault.EnableRbacAuthorization } "6. EnableRbacAuthorization should be false"
+        # $vault = $vault | Update-AzKeyVault -EnableRbacAuthorization $false
+        # Assert-False { $vault.EnableRbacAuthorization } "6. EnableRbacAuthorization should be false"
 
         # Update Tags
         $vault = $vault | Update-AzKeyVault -Tag @{key = "value"}
