db5c4b3d9b22d6683de44ecf5cfee707ef19715e
Azure PowerShell <65331932+azure-powershell-bot@users.noreply.github.com>
2023-06-25 10:06:44 +0800

    Sync tools folder from main branch to generation branch (#22142)
    Co-authored-by: azurepowershell <azurepowershell@ms.com>

--- tools/Az.Tools.Predictor/Az.Tools.Predictor/Telemetry/AzPredictorTelemetryClient.cs
+++ tools/Az.Tools.Predictor/Az.Tools.Predictor/Telemetry/AzPredictorTelemetryClient.cs
@@ -41,6 +41,11 @@ namespace Microsoft.Azure.PowerShell.Tools.AzPredictor.Telemetry
         /// </remarks>
         internal const int MaxPropertyValueSizeWithBuffer = MaxAppInsightPropertyValueSize - 100;
 
+        /// <summary>
+        /// The value used to concatenate the string together.
+        /// </summary>
+        internal const char _StringValueConcatenator = '\n';
+
         /// <inheritdoc/>
         public string RequestId { get; set; } = Guid.NewGuid().ToString();
 
@@ -77,6 +82,18 @@ namespace Microsoft.Azure.PowerShell.Tools.AzPredictor.Telemetry
         /// </remarks>
         protected AggregatedTelemetryData CachedAggregatedTelemetryData { get; private set; } = new();
 
+        /// <summary>
+        /// A set of user inputs that will be sent in the telemetry.
+        /// </summary>
+        /// <remarks>
+        /// This is the same value (after de-deuplication) of the user input field in the prediction property.
+        /// It's added as the property so that we can tell that if any issue is from the user inputs or the found examples
+        /// from the service. Though the user input is also in the prediction property, there is still too much data to go
+        /// through. We still need the user input in prediction to help with query.
+        ///
+        /// It's a class level field so that we don't always need to create a new instance for sending each telemetry.
+        /// </remarks>
+        private HashSet<string> _uniqueUserInput = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
         private bool _isDisposed;
 
         /// <summary>
@@ -174,16 +191,6 @@ namespace Microsoft.Azure.PowerShell.Tools.AzPredictor.Telemetry
         }
 
         /// <inheritdoc/>
-        public void OnParseCommandLineFailure(CommandLineParsingTelemetryData telemetryData)
-        {
-            PostTelemetryData(telemetryData);
-
-#if TELEMETRY_TRACE && DEBUG
-            System.Diagnostics.Trace.WriteLine("Recording CommandLineParsing");
-#endif
-        }
-
-        /// <inheritdoc/>
         public virtual void OnGeneralException(GeneralExceptionTelemetryData telemetryData)
         {
             PostTelemetryData(telemetryData);
@@ -227,9 +234,6 @@ namespace Microsoft.Azure.PowerShell.Tools.AzPredictor.Telemetry
                 case ParameterMapTelemetryData parameterMap:
                     ProcessTelemetryData(parameterMap);
                     break;
-                case CommandLineParsingTelemetryData commandLineParsing:
-                    ProcessTelemetryData(commandLineParsing);
-                    break;
                 case GeneralExceptionTelemetryData exception:
                     ProcessTelemetryData(exception);
                     break;
@@ -252,7 +256,19 @@ namespace Microsoft.Azure.PowerShell.Tools.AzPredictor.Telemetry
                 properties.Add(RequestPredictionTelemetryData.PropertyNameHttpRequestSent, aggregatedData.HasSentHttpRequest.Value.ToString(CultureInfo.InvariantCulture));
             }
 
+            if (aggregatedData.PredictorSummary != null)
+            {
+                properties.Add(RequestPredictionTelemetryData.PropertyNameReceivedCommandCount, aggregatedData.PredictorSummary.ReceivedCommandCount.ToString(CultureInfo.InvariantCulture));
+                properties.Add(RequestPredictionTelemetryData.PropertyNameValidCommandCount, aggregatedData.PredictorSummary.ValidCommandCount.ToString(CultureInfo.InvariantCulture));
+                if (aggregatedData.PredictorSummary.Errors?.Any() == true)
+                {
+                    properties.Add(RequestPredictionTelemetryData.PropertyNameCommandLineParsingError, string.Join(_StringValueConcatenator, aggregatedData.PredictorSummary.Errors));
+                }
+            }
+
             var suggestions = new List<Dictionary<string, object>>();
+            _uniqueUserInput.Clear();
+
             for (var i = 0; i < aggregatedData.SuggestionSessions.Count; ++i)
             {
                 var suggestionSession = aggregatedData.SuggestionSessions[i];
@@ -276,6 +292,7 @@ namespace Microsoft.Azure.PowerShell.Tools.AzPredictor.Telemetry
                 if (suggestionSession.UserInput != null)
                 {
                     toAddSuggestion.Add(GetSuggestionTelemetryData.PropertyNameUserInput, suggestionSession.UserInput);
+                    _uniqueUserInput.Add(suggestionSession.UserInput);
                 }
 
                 // Add the display suggestion data
@@ -307,6 +324,7 @@ namespace Microsoft.Azure.PowerShell.Tools.AzPredictor.Telemetry
             if (suggestions.Count > 0)
             {
                 properties.Add(GetSuggestionTelemetryData.PropertyNamePrediction, JsonSerializer.Serialize(suggestions, JsonUtilities.TelemetrySerializerOptions));
+                properties.Add(GetSuggestionTelemetryData.PropertyNameUserInput, string.Join(_StringValueConcatenator, _uniqueUserInput));
             }
 
             if (aggregatedData.CommandLine != null)
@@ -419,6 +437,7 @@ namespace Microsoft.Azure.PowerShell.Tools.AzPredictor.Telemetry
 
             CachedAggregatedTelemetryData.UpdateFromTelemetryData(telemetryData);
             CachedAggregatedTelemetryData.HasSentHttpRequest = telemetryData.HasSentHttpRequest;
+            CachedAggregatedTelemetryData.PredictorSummary = telemetryData.PredictorSummary;
 
             if (telemetryData.Exception != null)
             {
@@ -568,22 +587,6 @@ namespace Microsoft.Azure.PowerShell.Tools.AzPredictor.Telemetry
             if (telemetryData.Exception != null)
             {
                 SendException("An error occurred when loading parameter map.", telemetryData, telemetryData.Exception);
-            }
-        }
-
-        /// <summary>
-        /// Processes the telemetry with the command line parsing information.
-        /// </summary>
-        private void ProcessTelemetryData(CommandLineParsingTelemetryData telemetryData)
-        {
-            if (telemetryData.Exception != null)
-            {
-                var properties = new Dictionary<string,string>()
-                {
-                    { "Command", telemetryData.Command },
-                };
-
-                SendException("An error occurred when parsing command line.", telemetryData, telemetryData.Exception, properties);
             }
         }
 
