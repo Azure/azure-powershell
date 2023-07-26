277b1f24d87a3be4e69754d6507daef345cc04fc
derek <xidi@microsoft.com>
2023-07-26 14:30:47 +0800

    Use autorest.powershell to generate management SDK for cosmosDB

--- src/CosmosDB/CosmosDB/MongoDB/RemoveAzCosmosDBMongoDBRoleDefinition.cs
+++ src/CosmosDB/CosmosDB/MongoDB/RemoveAzCosmosDBMongoDBRoleDefinition.cs
@@ -69,7 +69,7 @@ namespace Microsoft.Azure.Commands.CosmosDB
 
             if (ShouldProcess(Id, "Deleting CosmosDB MongoDB User Definition"))
             {
-                CosmosDBManagementClient.MongoDBResources.DeleteMongoRoleDefinitionWithHttpMessagesAsync(MongoRoleHelper.ParseToMongoDbRoleDefinitionId(Id), ResourceGroupName, AccountName).GetAwaiter().GetResult();
+                CosmosDBManagementClient.MongoDbResources.DeleteMongoRoleDefinitionWithHttpMessagesAsync(MongoRoleHelper.ParseToMongoDbRoleDefinitionId(Id), ResourceGroupName, AccountName).GetAwaiter().GetResult();
 
                 if (PassThru.IsPresent)
                 {
