62f7cb01b1ebc4b17e1be57b0bc7704a4e944781
Beisi Zhou <bez@microsoft.com>
2023-08-08 17:18:11 +0800

    add live test

---
+++ src/KeyVault/KeyVault.Test/LiveTests/ManagedHsmDataPlaneLiveTests/TestSetting.ps1
@@ -0,0 +1,13 @@
+Invoke-LiveTestScenario -Name "Get and update key vault setting in a MSHM" -Description "Get and update a key vault setting in a MSHM" -ScenarioScript `
+{
+    param ($rg)
+
+    $rgName = $rg.ResourceGroupName
+    $hsmName = "bezmhsm" + (New-LiveTestRandomName -Option AllNumbers)
+    $hsmLocation = 'eastus2euap'
+    $adminId = "2f153a9e-5be9-4f43-abd2-04561777c8b0" #(Get-AzADUser -StartsWith Beisi).Id
+    $hsmObject = New-AzKeyVaultManagedHsm -HsmName $hsmName -ResourceGroupName $rgName -Location $hsmLocation -Administrator $adminId
+    $setting = $hsmObject | Get-AzKeyVaultSetting -Name "AllowKeyManagementOperationsThroughARM"
+    $updatedSetting= $setting | Update-AzKeyVaultSetting -Value true -PassThru
+    Assert-AreEqual $updatedSetting.Value "true"
+}
