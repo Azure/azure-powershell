06aa5127f5e25e777431da8829f93490347c4f15
Beisi Zhou <bez@microsoft.com>
2023-08-09 15:45:39 +0800

    fix

--- src/KeyVault/KeyVault.Test/LiveTests/ManagedHsmDataPlaneLiveTests/TestSetting.ps1
+++ src/KeyVault/KeyVault.Test/LiveTests/ManagedHsmDataPlaneLiveTests/TestSetting.ps1
@@ -5,11 +5,11 @@ Invoke-LiveTestScenario -Name "Get and update key vault setting in a MSHM" -Desc
     $rgName = $rg.ResourceGroupName
     $hsmName = "bezmhsm" + (New-LiveTestRandomName -Option AllNumbers)
     $hsmLocation = 'eastus2euap'
-    $adminId = "2f153a9e-5be9-4f43-abd2-04561777c8b0" #(Get-AzADUser -StartsWith Beisi).Id
+    $adminId = (Get-AzADUser -StartsWith Beisi).Id
     $hsmObject = New-AzKeyVaultManagedHsm -HsmName $hsmName -ResourceGroupName $rgName -Location $hsmLocation -Administrator $adminId
-    Start-Sleep 600
+    Start-Sleep 1800
     New-AzKeyVaultRoleAssignment -HsmName $hsmName -RoleDefinitionName "Managed HSM Crypto User" -ObjectId $adminId
-    Export-AzKeyVaultSecurityDomain -Certificates "$PSScriptRoot\sd1.cer", "$PSScriptRoot\sd2.cer", "$PSScriptRoot\sd3.cer" -Quorum 2 -OutputPath ./sd.ps.json -Name $hsmName
+    Export-AzKeyVaultSecurityDomain -Certificates "$PSScriptRoot\sd1.cer", "$PSScriptRoot\sd2.cer", "$PSScriptRoot\sd3.cer" -Quorum 2 -OutputPath $PSScriptRoot/sd.ps.json -Name $hsmName
     $setting = $hsmObject | Get-AzKeyVaultSetting -Name "AllowKeyManagementOperationsThroughARM"
     $updatedSetting= $setting | Update-AzKeyVaultSetting -Value true -PassThru
     Assert-AreEqual $updatedSetting.Value "true"
