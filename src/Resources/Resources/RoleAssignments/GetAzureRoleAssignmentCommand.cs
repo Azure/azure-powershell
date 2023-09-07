ba9cc8ed354ecb25de6275454a4d15fb851c820d
NoriZC <110961157+NoriZC@users.noreply.github.com>
2023-09-07 09:19:11 +0800

    [Az.Resources] Fix unexplicable error message when subscription and scope are both null. (#22730)
    * Fix unexplicate error message when subscription and scope are both null
    
    * Delete unnecessary namespace
    
    * Delete unnecessary namespace
    
    * Update ChangeLog
    
    * Resolve comments
    
    * Resolve Comments

--- src/Resources/Resources/RoleAssignments/GetAzureRoleAssignmentCommand.cs
+++ src/Resources/Resources/RoleAssignments/GetAzureRoleAssignmentCommand.cs
@@ -21,6 +21,7 @@ using Microsoft.WindowsAzure.Commands.Common;
 using System;
 using System.Collections.Generic;
 using System.Management.Automation;
+using ProjectResources = Microsoft.Azure.Commands.Resources.Properties.Resources;
 
 namespace Microsoft.Azure.Commands.Resources
 {
@@ -244,6 +245,11 @@ namespace Microsoft.Azure.Commands.Resources
                 ExpandPrincipalGroups = ExpandPrincipalGroups.IsPresent,
                 IncludeClassicAdministrators = IncludeClassicAdministrators.IsPresent,
             };
+
+            if (options.Scope == null && options.ResourceIdentifier.Subscription == null)
+            {
+                WriteTerminatingError(ProjectResources.ScopeAndSubscriptionNeitherProvided);
+            }
 
             AuthorizationClient.ValidateScope(options.Scope, true);
 
