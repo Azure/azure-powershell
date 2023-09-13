19514f127dfe82a5982585a1714c1b4240728074
Yifan Zhang <yifanzhang@microsoft.com>
2023-09-13 14:45:33 +0800

    Add warning messages for Queue breaking change

--- src/Storage/Storage/Queue/Cmdlet/SetAzureStorageQueueStoredAccessPolicy.cs
+++ src/Storage/Storage/Queue/Cmdlet/SetAzureStorageQueueStoredAccessPolicy.cs
@@ -22,7 +22,10 @@ namespace Microsoft.WindowsAzure.Commands.Storage.Queue.Cmdlet
     using System.Globalization;
     using System.Management.Automation;
     using System.Security.Permissions;
+    using Microsoft.WindowsAzure.Commands.Common.CustomAttributes;
 
+    [GenericBreakingChangeWithVersion("Permissions in the ouput access policy will be changed to a string like \"raup\" in a future release.",
+        "11.0.0", "6.0.0")]
     [Cmdlet("Set", Azure.Commands.ResourceManager.Common.AzureRMConstants.AzurePrefix + "StorageQueueStoredAccessPolicy", SupportsShouldProcess = true), OutputType(typeof(String))]
     public class SetAzureStorageQueueStoredAccessPolicyCommand : StorageQueueBaseCmdlet
     {
