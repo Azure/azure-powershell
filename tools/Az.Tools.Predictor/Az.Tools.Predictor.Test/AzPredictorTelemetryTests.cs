0617f39deef2888a0c743a23a3e18ed7031cb737
Maoliang Huang <mahuang@microsoft.com>
2023-10-02 14:46:17 -0700

    Use regex to check if that's a valid az command

--- tools/Az.Tools.Predictor/Az.Tools.Predictor.Test/AzPredictorTelemetryTests.cs
+++ tools/Az.Tools.Predictor/Az.Tools.Predictor.Test/AzPredictorTelemetryTests.cs
@@ -624,6 +624,10 @@ namespace Microsoft.Azure.PowerShell.Tools.AzPredictor.Test
         [InlineData("start_of_snippet", @"New-AzureStorageContext-StorageAccountName ""xxx"" -StorageAccountKey ""xxx""")]
         [InlineData("start_of_snippet", @"sig=xxxxxxxxxxx/Sensor-Azure")]
         [InlineData("start_of_snippet", @"sig=Signature/Test-Sensor-Azu")]
+        [InlineData("start_of_snippet", "New-AzStorage\"key=value\"")]
+        [InlineData("start_of_snippet", "New-Az\\\"key=value\\\"")]
+        [InlineData("start_of_snippet", "git")]
+        [InlineData("start_of_snippet", "Get-Key -Key abcd")]
         public void VerifyUserInputInGetSuggestionEvent(string expectedUserInput, string input)
         {
             var expectedTelemetryCount = 1;
