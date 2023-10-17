2ead3f670e4b4e8cefecc07c03d402c68a5079ee
Nori Zhang <norizhang@microsoft.com>
2023-10-17 16:53:25 +0800

    test modify gen code

--- src/Security/Security.Management.Sdk/Generated/AdaptiveApplicationControlsOperations.cs
+++ src/Security/Security.Management.Sdk/Generated/AdaptiveApplicationControlsOperations.cs
@@ -7,7 +7,7 @@
 // Changes may cause incorrect behavior and will be lost if the code is
 // regenerated.
 // </auto-generated>
-
+// unexpected change
 namespace Microsoft.Azure.Management.Security
 {
     using Microsoft.Rest;
