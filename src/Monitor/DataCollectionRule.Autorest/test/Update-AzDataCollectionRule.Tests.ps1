159d843c2d8d0d9a49dfcad97f54aa47aca4e698
JoyerJin <116236375+JoyerJin@users.noreply.github.com>
2023-10-31 10:33:46 +0800

    [Monitor] data collection rule migrates to autorest (#22879)
    * Generate Monitor Data Collection Rule by autorest v4
    
    * write examples and test cases following docs
    
    * fix signature issues and add model cmdlets' examples
    
    * fix AMCS breaking change issue

---
+++ src/Monitor/DataCollectionRule.Autorest/test/Update-AzDataCollectionRule.Tests.ps1
@@ -0,0 +1,27 @@
+if(($null -eq $TestName) -or ($TestName -contains 'Update-AzDataCollectionRule'))
+{
+  $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+  if (-Not (Test-Path -Path $loadEnvPath)) {
+      $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+  }
+  . ($loadEnvPath)
+  $TestRecordingFile = Join-Path $PSScriptRoot 'Update-AzDataCollectionRule.Recording.json'
+  $currentPath = $PSScriptRoot
+  while(-not $mockingPath) {
+      $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+      $currentPath = Split-Path -Path $currentPath -Parent
+  }
+  . ($mockingPath | Select-Object -First 1).FullName
+}
+
+Describe 'Update-AzDataCollectionRule' {
+    It 'UpdateExpanded' {
+        {
+            Update-AzDataCollectionRule -Name $env.testCollectionRule1 -ResourceGroupName $env.resourceGroup -Tag @{"123"="abc"}
+        } | Should -Not -Throw
+    }
+
+    It 'UpdateViaIdentityExpanded' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+}
