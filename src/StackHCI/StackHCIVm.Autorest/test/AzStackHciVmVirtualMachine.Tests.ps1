ceccc533025d790053c8e012064aa1feb4fe8086
Harika Vedati <havedati@microsoft.com>
2023-11-06 21:57:33 -0800

    fixing vm recording

--- src/StackHCI/StackHCIVm.Autorest/test/AzStackHciVmVirtualMachine.Tests.ps1
+++ src/StackHCI/StackHCIVm.Autorest/test/AzStackHciVmVirtualMachine.Tests.ps1
@@ -17,7 +17,7 @@ if(($null -eq $TestName) -or ($TestName -contains 'AzStackHCIVmVirtualMachine'))
 Describe 'AzStackHCIVmVirtualMachine' {
     It 'Create Virtual Machine  '  {
         
-        New-AzStackHciVMVirtualMachine -Name manualvmtest2 -OsType Linux  -ImageName "bugbashimage" -VmSize "Standard_K8S_v1"  -ComputerName "manualvmtest2" -ResourceGroupName "AltaylSnClus-rg" -CustomLocationId "/subscriptions/0709bd7a-8383-4e1d-98c8-f81d1b3443fc/resourceGroups/AltaylSnClus-rg/providers/Microsoft.ExtendedLocation/customLocations/myResourceBridge-cl"  -Location "eastus" -ProvisionVMAgent:$false -ProvisionVMConfigAgent:$false -SubscriptionId $env.subscriptionId | Select-Object -Property ProvisioningState  | Should -BeExactly "@{ProvisioningState=Succeeded}"
+        New-AzStackHciVMVirtualMachine -Name manualvmtest2 -OsType Linux  -ImageId "/subscriptions/0709bd7a-8383-4e1d-98c8-f81d1b3443fc/resourceGroups/AltaylSnClus-rg/providers/Microsoft.AzureStackHCI/galleryImages/bugbashimage" -VmSize "Standard_K8S_v1"  -ComputerName "manualvmtest2" -ResourceGroupName "AltaylSnClus-rg" -CustomLocationId "/subscriptions/0709bd7a-8383-4e1d-98c8-f81d1b3443fc/resourceGroups/AltaylSnClus-rg/providers/Microsoft.ExtendedLocation/customLocations/myResourceBridge-cl"  -Location "eastus" -ProvisionVMAgent:$false -ProvisionVMConfigAgent:$false -SubscriptionId $env.subscriptionId | Select-Object -Property ProvisioningState  | Should -BeExactly "@{ProvisioningState=Succeeded}"
     
     }
 
