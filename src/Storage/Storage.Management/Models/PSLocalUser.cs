8bfe281e3fd63b464853ffae30c12c0f7d23b9f2
Nori Zhang <norizhang@microsoft.com>
2023-11-17 15:10:30 +0800

    Trigger Storage CI

--- src/Storage/Storage.Management/Models/PSLocalUser.cs
+++ src/Storage/Storage.Management/Models/PSLocalUser.cs
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // ----------------------------------------------------------------------------------
-
+// trigger CI
 using Microsoft.Azure.Management.Storage.Models;
 using Microsoft.WindowsAzure.Commands.Common.Attributes;
 using System.Collections.Generic;
