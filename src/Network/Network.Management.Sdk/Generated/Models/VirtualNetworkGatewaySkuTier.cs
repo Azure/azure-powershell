17d93e6b9068d31971884fa7648514d274d68255
Xiaogang Ding <xidi@microsoft.com>
2023-11-23 19:53:19 +0800

    Fixed some minor issues for Az.network

--- src/Network/Network.Management.Sdk/Generated/Models/VirtualNetworkGatewaySkuTier.cs
+++ src/Network/Network.Management.Sdk/Generated/Models/VirtualNetworkGatewaySkuTier.cs
@@ -16,8 +16,8 @@ namespace Microsoft.Azure.Management.Network.Models
     /// </summary>
     public static class VirtualNetworkGatewaySkuTier
     {
-      public const string Basic = "Basic";
-          public const string HighPerformance = "HighPerformance";
+        public const string Basic = "Basic";
+        public const string HighPerformance = "HighPerformance";
         public const string Standard = "Standard";
         public const string UltraPerformance = "UltraPerformance";
         public const string VpnGw1 = "VpnGw1";
