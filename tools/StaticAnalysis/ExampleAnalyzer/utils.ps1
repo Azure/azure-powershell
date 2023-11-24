e99326a62d5c9b69fc1dd2562d4626cc42a1629e
Nori Zhang <norizhang@microsoft.com>
2023-11-24 18:26:38 +0800

    unexpected gen sdk suppress

--- tools/StaticAnalysis/ExampleAnalyzer/utils.ps1
+++ tools/StaticAnalysis/ExampleAnalyzer/utils.ps1
@@ -574,6 +574,17 @@ function Get-NonExceptionRecord{
                 }
             }
         }
+        
+        $exceptionPath = Join-Path -Path $exceptionPaths -ChildPath (EnsureModuleName($record.Module)) -AdditionalChildPath "GeneratedSdkIssues.csv"
+        if(Test-Path -Path $exceptionPath){
+            $exceptionContents = Import-Csv -Path $exceptionPath
+            foreach($exceptionContent in $exceptionContents) {
+                if($exceptionContent.Module -eq $record.Module -and $exceptionContent.Sdk -eq $record.Sdk -and $exceptionContent.Severity -eq $record.Severity -and $exceptionContent.ProblemId -eq $record.ProblemId -and $exceptionContent.Description -eq $record.Description){
+                    $needAdd = $false
+                    break
+                }
+            }
+        }
         if($needAdd){
             $errors += $record
         }
