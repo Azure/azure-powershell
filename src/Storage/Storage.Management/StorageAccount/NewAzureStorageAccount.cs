6ed153e40b2fe94ffcf6a7510b9f971c402315f7
Yabo Hu <yabhu@microsoft.com>
2023-11-28 17:23:59 +0800

    keep storage message for future version

--- src/Storage/Storage.Management/StorageAccount/NewAzureStorageAccount.cs
+++ src/Storage/Storage.Management/StorageAccount/NewAzureStorageAccount.cs
@@ -26,6 +26,12 @@ using StorageModels = Microsoft.Azure.Management.Storage.Models;
 
 namespace Microsoft.Azure.Commands.Management.Storage
 {
+    [GenericBreakingChangeWithVersion("Default value of AllowBlobPublicAccess and AllowCrossTenantReplication settings on storage account will be changed to False in the future release. \n" +
+        "When AllowBlobPublicAccess is False on a storage account, container ACLs cannot be configured to allow anonymous access to blobs within the storage account. \n" +
+        "When AllowCrossTenantReplication is False on a storage account, cross AAD tenant object replication is not allowed when setting up Object Replication policies. Target version is for reference only, it might be changed by service plan.",
+        "11.2.0", "6.2.0",
+        OldWay = "AllowBlobPublicAccess and AllowCrossTenantReplication are set to True by defult.", 
+        NewWay = "AllowBlobPublicAccess and AllowCrossTenantReplication are set to False by default.")]
     [Cmdlet("New", ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "StorageAccount", DefaultParameterSetName = AzureActiveDirectoryDomainServicesForFileParameterSet), OutputType(typeof(PSStorageAccount))]
     public class NewAzureStorageAccountCommand : StorageAccountBaseCmdlet
     {
