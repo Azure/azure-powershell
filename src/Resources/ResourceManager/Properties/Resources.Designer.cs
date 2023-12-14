2476133d9177b365ee5668efc3906fcf2aec82b3
Stephen Weatherford (MSFT) <StephenWeatherford@users.noreply.github.com>
2023-12-13 20:46:22 -0800

    Add -WithSource to bicep publish (#23317)
    * Add -WithSource to Publish-AzBicepModule
    
    * Revert unintended changes
    
    * Add back missing tests and fix merge error
    
    * Fix test
    
    * Remove quotes around version
    
    * Update src/Resources/Resources/ChangeLog.md
    
    ---------
    
    Co-authored-by: Stephen Weatherford <Stephen.Weatherford.com>
    Co-authored-by: Beisi Zhou <zhoubeisi@gmail.com>

--- src/Resources/ResourceManager/Properties/Resources.Designer.cs
+++ src/Resources/ResourceManager/Properties/Resources.Designer.cs
@@ -79,7 +79,7 @@ namespace Microsoft.Azure.Commands.ResourceManager.Cmdlets.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Please use bicep &apos;{0}&apos; or higher verison..
+        ///   Looks up a localized string similar to Please use bicep &apos;{0}&apos; or higher..
         /// </summary>
         internal static string BicepVersionRequirement {
             get {
