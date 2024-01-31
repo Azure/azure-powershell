cebc8e268758b2c2df59310e765455da57c49953
Yunchi Wang <54880216+wyunchi-ms@users.noreply.github.com>
2024-01-31 14:46:48 +0800

    Test

--- tools/NewOutputTypeIndex.ps1
+++ tools/NewOutputTypeIndex.ps1
@@ -26,41 +26,43 @@ Register-PSRepository -Name $ReleaseRepository -SourceLocation "$PSScriptRoot/..
 Install-Module -Scope CurrentUser -Name Az -Repository $ReleaseRepository -Force -AllowClobber
 
 $ModuleMetadata.RequiredModules | ForEach-Object {
-        $ModuleName = $_.ModuleName
-        $RequiredVersion = $_.RequiredVersion
-        $srcFile = $ModuleManifestFile | Where-Object {$_.Name -eq "$ModuleName.psd1"}
-        Import-LocalizedData -BindingVariable srcMetadata -BaseDirectory $srcFile.DirectoryName -FileName $srcFile.Name
-        $DestinationModulePath = [System.IO.Path]::Combine($ModulePath, $ModuleName, $RequiredVersion)
-        $psd1Path = Join-Path -Path $DestinationModulePath -ChildPath "$ModuleName.psd1"
-        Write-Host $Psd1Path
-        if (-not (Test-Path $Psd1Path))
+    $ModuleName = $_.ModuleName
+    $RequiredVersion = $_.RequiredVersion
+    $srcFile = $ModuleManifestFile | Where-Object {$_.Name -eq "$ModuleName.psd1"}
+    Import-LocalizedData -BindingVariable srcMetadata -BaseDirectory $srcFile.DirectoryName -FileName $srcFile.Name
+    $DestinationModulePath = [System.IO.Path]::Combine($ModulePath, $ModuleName, $RequiredVersion)
+    $psd1Path = Join-Path -Path $DestinationModulePath -ChildPath "$ModuleName.psd1"
+    Write-Host $Psd1Path
+    if (-not (Test-Path $Psd1Path))
+    {
+        Write-Warning "Module $ModuleName not found in $DestinationModulePath"
+    }
+    # Import-Module $Psd1Path -Force
+    Import-Module $ModuleName
+    $Module = Get-Module $ModuleName
+    foreach ($Cmdlet in $Module.ExportedCmdlets.Values) {
+        $OutputAttributeList = $Cmdlet.ImplementingType.GetTypeInfo().GetCustomAttributes([System.Management.Automation.OutputTypeAttribute], $true)
+        foreach ($OutputAttribute in $OutputAttributeList)
         {
-            Write-Warning "Module $ModuleName not found in $DestinationModulePath"
-        }
-        Import-Module $Psd1Path -Force
-        $Module = Get-Module $ModuleName
-        foreach ($Cmdlet in $Module.ExportedCmdlets.Values) {
-            $OutputAttributeList = $Cmdlet.ImplementingType.GetTypeInfo().GetCustomAttributes([System.Management.Automation.OutputTypeAttribute], $true)
-            foreach ($OutputAttribute in $OutputAttributeList)
+            foreach ($OutputType in $OutputAttribute.Type)
             {
-                foreach ($OutputType in $OutputAttribute.Type)
-                {
-                    $outputTypes.Add($OutputType.Name) | Out-Null
-                }
+                $outputTypes.Add($OutputType.Name) | Out-Null
             }
-            foreach ($Parameter in $Cmdlet.Parameters.Values){
-                    if ($Parameter.Attributes.TypeId.FullName -contains "System.Management.Automation.ParameterAttribute") {
-                        if ($Parameter.ParameterType.FullName -like "*System.Nullable*``[``[*")
-                        {
-                            $outputTypes.Add(($Parameter.ParameterType.BaseType.FullName -replace "[][]", "")) | Out-Null
-                        }
-                        elseif ($Parameter.ParameterType.FullName -notlike "*``[``[*")
-                        {
-                            $outputTypes.Add(($Parameter.ParameterType.FullName -replace "[][]", "")) | Out-Null
-                        }
+        }
+        foreach ($Parameter in $Cmdlet.Parameters.Values){
+                if ($Parameter.Attributes.TypeId.FullName -contains "System.Management.Automation.ParameterAttribute") {
+                    if ($Parameter.ParameterType.FullName -like "*System.Nullable*``[``[*")
+                    {
+                        $outputTypes.Add(($Parameter.ParameterType.BaseType.FullName -replace "[][]", "")) | Out-Null
                     }
-            }
+                    elseif ($Parameter.ParameterType.FullName -notlike "*``[``[*")
+                    {
+                        $outputTypes.Add(($Parameter.ParameterType.FullName -replace "[][]", "")) | Out-Null
+                    }
+                }
         }
     }
+}
+gmo -l
 $json = ConvertTo-Json $outputTypes
 $json | Out-File "$OutputFile"
