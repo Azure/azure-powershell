ea9ce118ef6f7f316a4fd445ea88e6e77e43e973
nanxiangLiu <nanxiangliu@microsoft.com>
2024-02-02 10:47:41 +0800

    fix script

--- tools/Docs/OutputTypeIndex.ps1
+++ tools/Docs/OutputTypeIndex.ps1
@@ -50,33 +50,35 @@ $ModuleMetadata.RequiredModules | ForEach-Object {
     }
     $srcFile = $ModuleManifestFile | Where-Object {$_.Name -eq "$ModuleName.psd1"}
     Import-LocalizedData -BindingVariable srcMetadata -BaseDirectory $srcFile.DirectoryName -FileName $srcFile.Name
+    $containsPsd1 = $srcMetadata.NestedModules | Where-Object { $_ -like "*.dll" }
     $DestinationModulePath = [System.IO.Path]::Combine($ModulePath, $ModuleName, $Version)
     $psd1Path = Join-Path -Path $DestinationModulePath -ChildPath "$ModuleName.psd1"
-    Write-Host $Psd1Path
-    if (-not (Test-Path $Psd1Path))
-    {
-        Write-Warning "Module $ModuleName not found in $DestinationModulePath"
-    }
-    Import-Module $ModuleName
-    $Module = Get-Module $ModuleName
-    foreach ($Cmdlet in $Module.ExportedCmdlets.Values) {
-        $OutputAttributeList = $Cmdlet.ImplementingType.GetTypeInfo().GetCustomAttributes([System.Management.Automation.OutputTypeAttribute], $true)
-        foreach ($OutputAttribute in $OutputAttributeList)
-        {
-            foreach ($OutputType in $OutputAttribute.Type)
-            {
-                $outputTypes.Add($OutputType.Name) | Out-Null
-            }
-        }
-        foreach ($Parameter in $Cmdlet.Parameters.Values){
-            if ($Parameter.Attributes.TypeId.FullName -contains "System.Management.Automation.ParameterAttribute") {
-                if ($Parameter.ParameterType.FullName -like "*System.Nullable*``[``[*")
-                {
-                    $outputTypes.Add(($Parameter.ParameterType.BaseType.FullName -replace "[][]", "")) | Out-Null
-                }
-                elseif ($Parameter.ParameterType.FullName -notlike "*``[``[*")
-                {
-                    $outputTypes.Add(($Parameter.ParameterType.FullName -replace "[][]", "")) | Out-Null
+    if (($containsPsd1.count -gt 0) -and (Test-Path $psd1Path)){
+        Import-Module $Psd1Path -Force
+        $Module = Get-Module $ModuleName
+        foreach ($ModuleInfo in $Module.NestedModules){
+            if ($srcMetadata.NestedModules -contains $ModuleInfo.Name+".dll") {
+                foreach ($Cmdlet in $ModuleInfo.ExportedCmdlets.Values) {
+                    $OutputAttributeList = $Cmdlet.ImplementingType.GetTypeInfo().GetCustomAttributes([System.Management.Automation.OutputTypeAttribute], $true)
+                    foreach ($OutputAttribute in $OutputAttributeList)
+                    {
+                        foreach ($OutputType in $OutputAttribute.Type)
+                        {
+                            $outputTypes.Add($OutputType.Name) | Out-Null
+                        }
+                    }
+                    foreach ($Parameter in $Cmdlet.Parameters.Values){
+                            if ($Parameter.Attributes.TypeId.FullName -contains "System.Management.Automation.ParameterAttribute") {
+                                if ($Parameter.ParameterType.FullName -like "*System.Nullable*``[``[*")
+                                {
+                                    $outputTypes.Add(($Parameter.ParameterType.BaseType.FullName -replace "[][]", "")) | Out-Null
+                                }
+                                elseif ($Parameter.ParameterType.FullName -notlike "*``[``[*")
+                                {
+                                    $outputTypes.Add(($Parameter.ParameterType.FullName -replace "[][]", "")) | Out-Null
+                                }
+                            }
+                    }
                 }
             }
         }
