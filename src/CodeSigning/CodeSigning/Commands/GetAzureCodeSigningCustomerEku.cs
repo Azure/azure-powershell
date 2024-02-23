11c52bbc38c8a0b835b40e6056e719b4201f6b9b
Yunchi Wang <54880216+wyunchi-ms@users.noreply.github.com>
2024-02-23 10:43:35 +0800

    Revert "Revert "az.codesigning initial version (#23908)" (#24211)"
    This reverts commit 573fb936b65eeee1a7818f51b649c7ab5c354f32.

---
+++ src/CodeSigning/CodeSigning/Commands/GetAzureCodeSigningCustomerEku.cs
@@ -0,0 +1,90 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Commands.CodeSigning.Models;
+using System;
+using System.Management.Automation;
+
+namespace Microsoft.Azure.Commands.CodeSigning
+{
+    [Cmdlet("Get", ResourceManager.Common.AzureRMConstants.AzurePrefix + "CodeSigningCustomerEku", DefaultParameterSetName = ByAccountProfileNameParameterSet)]
+    [OutputType(typeof(string[]))]
+    public class GetAzureCodeSigningCustomerEku : CodeSigningCmdletBase
+    {
+        #region Parameter Set Names
+
+        private const string ByAccountProfileNameParameterSet = "ByAccountProfileNameParameterSet";
+        private const string ByMetadataFileParameterSet = "ByMetadataFileParameterSet";
+
+        #endregion
+
+        #region Input Parameter Definitions
+
+        /// <summary>
+        /// Account Profile name
+        /// </summary>
+        [Parameter(Mandatory = true,
+            Position = 0,
+            ParameterSetName = ByAccountProfileNameParameterSet,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The account name of Azure CodeSigning.")]
+        [ValidateNotNullOrEmpty]
+        public string AccountName { get; set; }
+
+        [Parameter(Mandatory = true,
+            Position = 1,
+            ParameterSetName = ByAccountProfileNameParameterSet,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The certificate profile name of Azure CodeSigning account.")]
+        [ValidateNotNullOrEmpty()]
+        public string ProfileName { get; set; }
+
+        [Parameter(Mandatory = true,
+            Position = 2,
+            ParameterSetName = ByAccountProfileNameParameterSet,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The endpoint url used to submit request to Azure CodeSigning.")]
+        public string EndpointUrl { get; set; }
+
+
+        /// <summary>
+        /// Metadata File Path
+        /// </summary>
+        [Parameter(Mandatory = true,
+            Position = 0,
+            ParameterSetName = ByMetadataFileParameterSet,
+             ValueFromPipelineByPropertyName = true,
+            HelpMessage = "Metadata File path. Cmdlet constructs the FQDN of an account profile based on the Metadata File and currently selected environment.")]
+        [ValidateNotNullOrEmpty]
+        public string MetadataFilePath { get; set; }
+
+        #endregion
+
+        public override void ExecuteCmdlet()
+        {
+            string[] eku = Array.Empty<string>();
+
+            if (!string.IsNullOrEmpty(AccountName))
+            {
+                eku = CodeSigningServiceClient.GetCodeSigningEku(AccountName, ProfileName, EndpointUrl);
+            }
+            else if (!string.IsNullOrEmpty(MetadataFilePath))
+            {
+                eku = CodeSigningServiceClient.GetCodeSigningEku(MetadataFilePath);
+            }
+
+            WriteObject(eku);
+        }
+    }
+}
