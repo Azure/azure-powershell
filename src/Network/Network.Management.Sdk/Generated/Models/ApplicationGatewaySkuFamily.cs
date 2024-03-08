8f90d571819697325c20f07832920f24c9b63319
tracyMicro <127259533+tracyMicro@users.noreply.github.com>
2024-03-08 13:43:32 -0800

    add sku family to powershell

---
+++ src/Network/Network.Management.Sdk/Generated/Models/ApplicationGatewaySkuFamily.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.Management.Network.Generated.Models
+{
+    /// <summary>
+    /// Defines values for ApplicationGatewaySkuFamily.
+    /// </summary>
+    public static class ApplicationGatewaySkuFamily
+    {
+        public const string Generation1 = "Generation_1";
+        public const string Generation2 = "Generation_2";
+    }
+}
