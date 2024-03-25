b5935647f2105282dc2429ace1bbe161bee7abe9
Azure PowerShell <65331932+azure-powershell-bot@users.noreply.github.com>
2024-03-25 21:22:53 +0800

    Migrate Fleet from generation to main (#24497)
    * Move Fleet to main
    
    * Update ChangeLog.md
    
    ---------
    
    Co-authored-by: Vincent Dai <23257217+vidai-msft@users.noreply.github.com>

--- src/Fleet/Fleet.Autorest/exports/New-AzFleet.ps1
+++ src/Fleet/Fleet.Autorest/exports/New-AzFleet.ps1
@@ -77,7 +77,6 @@ param(
     [Microsoft.Azure.PowerShell.Cmdlets.Fleet.Category('Path')]
     [Microsoft.Azure.PowerShell.Cmdlets.Fleet.Models.IFleetIdentity]
     # Identity Parameter
-    # To construct, see NOTES section for INPUTOBJECT properties and create a hash table.
     ${InputObject},
 
     [Parameter()]
@@ -230,7 +229,13 @@ begin {
             CreateViaJsonString = 'Az.Fleet.private\New-AzFleet_CreateViaJsonString';
         }
         if (('CreateExpanded', 'CreateViaJsonFilePath', 'CreateViaJsonString') -contains $parameterSet -and -not $PSBoundParameters.ContainsKey('SubscriptionId') ) {
-            $PSBoundParameters['SubscriptionId'] = (Get-AzContext).Subscription.Id
+            $testPlayback = $false
+            $PSBoundParameters['HttpPipelinePrepend'] | Foreach-Object { $testPlayback = $testPlayback -or ('Microsoft.Azure.PowerShell.Cmdlets.Fleet.Runtime.PipelineMock' -eq $_.Target.GetType().FullName -and 'Playback' -eq $_.Target.Mode) }
+            if ($testPlayback) {
+                $PSBoundParameters['SubscriptionId'] = . (Join-Path $PSScriptRoot '..' 'utils' 'Get-SubscriptionIdTestSafe.ps1')
+            } else {
+                $PSBoundParameters['SubscriptionId'] = (Get-AzContext).Subscription.Id
+            }
         }
         $cmdInfo = Get-Command -Name $mapping[$parameterSet]
         [Microsoft.Azure.PowerShell.Cmdlets.Fleet.Runtime.MessageAttributeHelper]::ProcessCustomAttributesAtRuntime($cmdInfo, $MyInvocation, $parameterSet, $PSCmdlet)
--- src/Fleet/Fleet.Autorest/help/Update-AzFleetUpdateStrategy.md
+++ src/Fleet/Fleet.Autorest/help/Update-AzFleetUpdateStrategy.md
@@ -140,7 +140,6 @@ Accept wildcard characters: False
 
 ### -FleetInputObject
 Identity Parameter
-To construct, see NOTES section for FLEETINPUTOBJECT properties and create a hash table.
 
 ```yaml
 Type: Microsoft.Azure.PowerShell.Cmdlets.Fleet.Models.IFleetIdentity
@@ -201,7 +200,6 @@ Accept wildcard characters: False
 
 ### -InputObject
 Identity Parameter
-To construct, see NOTES section for INPUTOBJECT properties and create a hash table.
 
 ```yaml
 Type: Microsoft.Azure.PowerShell.Cmdlets.Fleet.Models.IFleetIdentity
@@ -264,7 +262,6 @@ Accept wildcard characters: False
 ### -StrategyStage
 The list of stages that compose this update run.
 Min size: 1.
-To construct, see NOTES section for STRATEGYSTAGE properties and create a hash table.
 
 ```yaml
 Type: Microsoft.Azure.PowerShell.Cmdlets.Fleet.Models.IUpdateStage[]
