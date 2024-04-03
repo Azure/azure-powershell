aa053e9d4f37f3db73728523d2b6ded8d4d3fe07
Chris Stackhouse <cstack@microsoft.com>
2024-04-03 01:01:11 -0700

    Correct location handling, fix tests and consistency (#24573)
    * Update to autorest 4.x
    Update to Azure Policy 2023-04-01
    Move to non-versioned interface types
    Complete full test suite for existing cmdlets
    Few bug fixes
    Add completer for Location parameter
    Fix record/playback, remove -LiveOnly tags
    Make test names repeatable to work with record/playback
    Implement resource group functions that became inaccessible
    Fix test failures due to default parameter injection
    
    * Clean up some transforms in README
    Remove unsupported (for now) parameters
    Streamline common test code
    Move common test code to utils.ps1
    Add serialization of test variables to env.json
    
    * Remove extra parameters from Get-AzPolicyAssignment
    Rerecord the tests
    
    * Fix $testFilesFolder calculation to work on macOS/linux.
    
    * Hide generated parameter sets for New-*
    Correct definitions, validation, and tests for New-AzPolicyAssignment parameters
    Re-record all tests
    
    * Replace Get-AzContext with Utils\Get-SubscriptionIdTestSafe
    Move to autorest 4.0.690
    Update managed identity properties to match generation changes
    Remove several parameter transforms no longer needed
    Rerecord all tests
    91 tests still failing on -Playback with the same signature
    
    * Remove location completer from exports/docs/examples/help
    
    * Improve location completer tests
    
    * Remove transforms for Scope and Id parameters no longer needed
    Update cmdlets to use Scope and Id directly
    Rerecord all tests
    All tests pass in -Record mode
    Failures in -Playback mode unchanged
    
    * Change location completer to lazy load (vs. module load)
    Preserve "extra" input parameters in Update-* calls to Get-* in order to support -Playback test mode
    Tag test files that don't support -Playback mode as LiveOnly
    Rerecord all recording files
    
    * Minor updates for consistency
    Rerecord all tests
    Move 4 test files back to LiveOnly
    
    * Updates for consistency and to fix tests
    Make changes to ensure consistent and valid handling of $location
    Rerecord tests
    
    * Fix bad merge
    Rerecord two test files
    
    * Re-record tests

--- src/Resources/Policy.Autorest/custom/Get-AzPolicyExemption.ps1
+++ src/Resources/Policy.Autorest/custom/Get-AzPolicyExemption.ps1
@@ -207,7 +207,7 @@ process {
             switch ($resolved.ScopeType) {
                 'mgName' {
                     if ($IncludeDescendent) {
-                        throw 'The -IncludeDescendent switch is not supported for management group scopes.'
+                        throw 'The IncludeDescendent switch is not supported for management group scopes.'
                     }
 
                     $calledParameterSet = 'List3'
