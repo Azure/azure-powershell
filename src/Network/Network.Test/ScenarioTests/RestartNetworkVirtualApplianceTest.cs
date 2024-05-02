3bab3399dc5c5bb7bb2d0967439ca949f0e5118a
Arsheen Khatib <akhatib@microsoft.com>
2024-05-01 17:52:40 -0700

    Adding the tests

---
+++ src/Network/Network.Test/ScenarioTests/RestartNetworkVirtualApplianceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Azure.Commands.Network.Test.ScenarioTests;
+using Microsoft.WindowsAzure.Commands.ScenarioTest;
+using Xunit;
+
+namespace Commands.Network.Test.ScenarioTests
+{
+    public class RestartNetworkVirtualApplianceTest : NetworkTestRunner
+    {
+        public RestartNetworkVirtualApplianceTest(Xunit.Abstractions.ITestOutputHelper output)
+            : base(output)
+        {
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        [Trait(Category.Owner, NrpTeamAlias.nvadev)]
+        public void TestNetworkVirtualApplianceRestart()
+        {
+            TestRunner.RunTestScript(string.Format("Test-NetworkVirtualApplianceRestart"));
+        }
+
+    }
+}
