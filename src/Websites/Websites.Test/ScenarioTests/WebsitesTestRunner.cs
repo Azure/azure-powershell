7390c367911a3a81192535d24b9dfb9c3e7068d4
Vincent Dai <23257217+vidai-msft@users.noreply.github.com>
2024-05-09 16:45:56 +0800

    Update WebsitesTestRunner.cs

--- src/Websites/Websites.Test/ScenarioTests/WebsitesTestRunner.cs
+++ src/Websites/Websites.Test/ScenarioTests/WebsitesTestRunner.cs
@@ -37,7 +37,6 @@ namespace Microsoft.Azure.Commands.Websites.Test.ScenarioTests
                 .WithNewRmModules(helper => new[]
                 {
                     helper.RMProfileModule,
-                    helper.RMResourceModule,
                     helper.GetRMModulePath("Az.Websites.psd1"),
                     helper.GetRMModulePath("Az.Network.psd1"),
                 })
