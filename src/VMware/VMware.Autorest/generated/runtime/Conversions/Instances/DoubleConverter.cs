89d541402112c53010c2c09e40885a4e202f346f
Ana Arias Rosales <anaarias@microsoft.com>
2024-05-10 14:47:56 -0400

    add files

---
+++ src/VMware/VMware.Autorest/generated/runtime/Conversions/Instances/DoubleConverter.cs
@@ -0,0 +1,13 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+﻿namespace Microsoft.Azure.PowerShell.Cmdlets.VMware.Runtime.Json
+{
+    public sealed class DoubleConverter : JsonConverter<double>
+    {
+        internal override JsonNode ToJson(double value) => new JsonNumber(value);
+
+        internal override double FromJson(JsonNode node) => (double)node;
+    }
+}
