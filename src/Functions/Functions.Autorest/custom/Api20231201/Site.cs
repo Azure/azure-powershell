b6e37efc6a48c7452c64d6a22fc5bbeea5c00734
Francisco Gamino <Francisco-Gamino@users.noreply.github.com>
2024-06-24 00:33:47 -0700

    [Functions] Upgrade to Microsoft.Web API version 2023-12-01 and add support for creating function apps on container apps (#25348)
    * Upgrade to API version 2023-12-01
    
    * Add support to create function apps ACA
    
    * Use ManagedEnvironmentId to get the subscription id
    
    * Update DefaultCentauriImage and update error messages
    
    * Update Stacks and tab completers initialization logic
    
    * Update logic to retrieve managed environment
    
    * Add function app ACA test cases
    
    * Update docs
    
    * Add GetRandomStringValue function
    
    * Fix typo in app name
    
    * Add new test recordings
    
    * Update string comparison logic
    
    * Update stack's definition
    
    * Update examples
    
    * Rename MinReplicas to ScaleMinReplica and MaxReplicas to ScaleMaxReplica
    
    * Update functions ACA test cases
    
    * Update docs with new param names

--- src/Functions/Functions.Autorest/custom/Api20231201/Site.cs
+++ src/Functions/Functions.Autorest/custom/Api20231201/Site.cs
@@ -1,8 +1,8 @@
-namespace Microsoft.Azure.PowerShell.Cmdlets.Functions.Models.Api20190801
+namespace Microsoft.Azure.PowerShell.Cmdlets.Functions.Models.Api20231201
 {
     public partial class Site :
-        Microsoft.Azure.PowerShell.Cmdlets.Functions.Models.Api20190801.ISite,
-        Microsoft.Azure.PowerShell.Cmdlets.Functions.Models.Api20190801.ISiteInternal,
+        Microsoft.Azure.PowerShell.Cmdlets.Functions.Models.Api20231201.ISite,
+        Microsoft.Azure.PowerShell.Cmdlets.Functions.Models.Api20231201.ISiteInternal,
         Microsoft.Azure.PowerShell.Cmdlets.Functions.Runtime.IValidates
     {
         	    
