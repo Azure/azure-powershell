2db7acf99b1818036d7cb86850dcd6326e961d5d
Kanchan Joshi <30471843+koderjoker@users.noreply.github.com>
2024-07-10 18:37:18 +0530

    Azure RedisCache New Api v2024.03.01 (#25497)
    * Generate new api version code
    
    * Update cmdlets
    
    * Update scenario tests
    
    * Add test recordings
    
    * Update Changelog
    
    * Update help docs
    
    * Update ChangeLog.md
    
    ---------
    
    Co-authored-by: Kanchan Joshi <kanchanjoshi@microsoft.com>
    Co-authored-by: Yabo Hu <yabhu@microsoft.com>

--- src/RedisCache/RedisCache.Management.Sdk/Generated/Models/RedisResource.cs
+++ src/RedisCache/RedisCache.Management.Sdk/Generated/Models/RedisResource.cs
@@ -64,8 +64,9 @@ namespace Microsoft.Azure.Management.RedisCache.Models
         /// &#39;Succeeded&#39;, &#39;Unlinking&#39;, &#39;Unprovisioning&#39;, &#39;Updating&#39;, &#39;ConfiguringAAD&#39;</param>
 
         /// <param name="redisConfiguration">All Redis Settings. Few possible keys:
-        /// rdb-backup-enabled,rdb-storage-connection-string,rdb-backup-frequency,maxmemory-delta,maxmemory-policy,notify-keyspace-events,maxmemory-samples,slowlog-log-slower-than,slowlog-max-len,list-max-ziplist-entries,list-max-ziplist-value,hash-max-ziplist-entries,hash-max-ziplist-value,set-max-intset-entries,zset-max-ziplist-entries,zset-max-ziplist-value
-        /// etc.
+        /// rdb-backup-enabled,rdb-storage-connection-string,rdb-backup-frequency,maxmemory-delta,
+        /// maxmemory-policy,notify-keyspace-events, aof-backup-enabled,
+        /// aof-storage-connection-string-0, aof-storage-connection-string-1 etc.
         /// </param>
 
         /// <param name="redisVersion">Redis version. This should be in the form &#39;major[.minor]&#39; (only &#39;major&#39; is
@@ -99,6 +100,10 @@ namespace Microsoft.Azure.Management.RedisCache.Models
         /// &#39;Enabled&#39;
         /// Possible values include: &#39;Enabled&#39;, &#39;Disabled&#39;</param>
 
+        /// <param name="disableAccessKeyAuthentication">Authentication to Redis through access keys is disabled when set as true.
+        /// Default value is false.
+        /// </param>
+
         /// <param name="subnetId">The full resource ID of a subnet in a virtual network to deploy the Redis
         /// cache in. Example format:
         /// /subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/Microsoft.{Network|ClassicNetwork}/VirtualNetworks/vnet1/subnets/subnet1
@@ -130,7 +135,7 @@ namespace Microsoft.Azure.Management.RedisCache.Models
         /// <param name="privateEndpointConnections">List of private endpoint connection associated with the specified redis
         /// cache
         /// </param>
-        public RedisResource(string location, Sku sku, string id = default(string), string name = default(string), string type = default(string), System.Collections.Generic.IDictionary<string, string> tags = default(System.Collections.Generic.IDictionary<string, string>), System.Collections.Generic.IList<string> zones = default(System.Collections.Generic.IList<string>), ManagedServiceIdentity identity = default(ManagedServiceIdentity), string updateChannel = default(string), string provisioningState = default(string), RedisCommonPropertiesRedisConfiguration redisConfiguration = default(RedisCommonPropertiesRedisConfiguration), string redisVersion = default(string), bool? enableNonSslPort = default(bool?), int? replicasPerMaster = default(int?), int? replicasPerPrimary = default(int?), System.Collections.Generic.IDictionary<string, string> tenantSettings = default(System.Collections.Generic.IDictionary<string, string>), int? shardCount = default(int?), string minimumTlsVersion = default(string), string publicNetworkAccess = default(string), string subnetId = default(string), string staticIP = default(string), string hostName = default(string), int? port = default(int?), int? sslPort = default(int?), RedisAccessKeys accessKeys = default(RedisAccessKeys), System.Collections.Generic.IList<RedisLinkedServer> linkedServers = default(System.Collections.Generic.IList<RedisLinkedServer>), System.Collections.Generic.IList<RedisInstanceDetails> instances = default(System.Collections.Generic.IList<RedisInstanceDetails>), System.Collections.Generic.IList<PrivateEndpointConnection> privateEndpointConnections = default(System.Collections.Generic.IList<PrivateEndpointConnection>))
+        public RedisResource(string location, Sku sku, string id = default(string), string name = default(string), string type = default(string), System.Collections.Generic.IDictionary<string, string> tags = default(System.Collections.Generic.IDictionary<string, string>), System.Collections.Generic.IList<string> zones = default(System.Collections.Generic.IList<string>), ManagedServiceIdentity identity = default(ManagedServiceIdentity), string updateChannel = default(string), string provisioningState = default(string), RedisCommonPropertiesRedisConfiguration redisConfiguration = default(RedisCommonPropertiesRedisConfiguration), string redisVersion = default(string), bool? enableNonSslPort = default(bool?), int? replicasPerMaster = default(int?), int? replicasPerPrimary = default(int?), System.Collections.Generic.IDictionary<string, string> tenantSettings = default(System.Collections.Generic.IDictionary<string, string>), int? shardCount = default(int?), string minimumTlsVersion = default(string), string publicNetworkAccess = default(string), bool? disableAccessKeyAuthentication = default(bool?), string subnetId = default(string), string staticIP = default(string), string hostName = default(string), int? port = default(int?), int? sslPort = default(int?), RedisAccessKeys accessKeys = default(RedisAccessKeys), System.Collections.Generic.IList<RedisLinkedServer> linkedServers = default(System.Collections.Generic.IList<RedisLinkedServer>), System.Collections.Generic.IList<RedisInstanceDetails> instances = default(System.Collections.Generic.IList<RedisInstanceDetails>), System.Collections.Generic.IList<PrivateEndpointConnection> privateEndpointConnections = default(System.Collections.Generic.IList<PrivateEndpointConnection>))
 
         : base(location, id, name, type, tags)
         {
@@ -148,6 +153,7 @@ namespace Microsoft.Azure.Management.RedisCache.Models
             this.ShardCount = shardCount;
             this.MinimumTlsVersion = minimumTlsVersion;
             this.PublicNetworkAccess = publicNetworkAccess;
+            this.DisableAccessKeyAuthentication = disableAccessKeyAuthentication;
             this.SubnetId = subnetId;
             this.StaticIP = staticIP;
             this.HostName = hostName;
@@ -202,8 +208,9 @@ namespace Microsoft.Azure.Management.RedisCache.Models
 
         /// <summary>
         /// Gets or sets all Redis Settings. Few possible keys:
-        /// rdb-backup-enabled,rdb-storage-connection-string,rdb-backup-frequency,maxmemory-delta,maxmemory-policy,notify-keyspace-events,maxmemory-samples,slowlog-log-slower-than,slowlog-max-len,list-max-ziplist-entries,list-max-ziplist-value,hash-max-ziplist-entries,hash-max-ziplist-value,set-max-intset-entries,zset-max-ziplist-entries,zset-max-ziplist-value
-        /// etc.
+        /// rdb-backup-enabled,rdb-storage-connection-string,rdb-backup-frequency,maxmemory-delta,
+        /// maxmemory-policy,notify-keyspace-events, aof-backup-enabled,
+        /// aof-storage-connection-string-0, aof-storage-connection-string-1 etc.
         /// </summary>
         [Newtonsoft.Json.JsonProperty(PropertyName = "properties.redisConfiguration")]
         public RedisCommonPropertiesRedisConfiguration RedisConfiguration {get; set; }
@@ -263,6 +270,13 @@ namespace Microsoft.Azure.Management.RedisCache.Models
         /// </summary>
         [Newtonsoft.Json.JsonProperty(PropertyName = "properties.publicNetworkAccess")]
         public string PublicNetworkAccess {get; set; }
+
+        /// <summary>
+        /// Gets or sets authentication to Redis through access keys is disabled when
+        /// set as true. Default value is false.
+        /// </summary>
+        [Newtonsoft.Json.JsonProperty(PropertyName = "properties.disableAccessKeyAuthentication")]
+        public bool? DisableAccessKeyAuthentication {get; set; }
 
         /// <summary>
         /// Gets or sets the full resource ID of a subnet in a virtual network to
