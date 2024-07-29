75f1667c00d2aa52c797d412fa7697a410512cab
Yabo Hu <yabhu@microsoft.com>
2024-07-29 10:27:06 +0800

    cherry-pick 475411bf4001904b293bb80580ac35947c85ac6e from generation

--- src/Resources/Policy.Autorest/test/Backcompat/PolicyDefinitionCRUDAtSubscription.Tests.ps1
+++ src/Resources/Policy.Autorest/test/Backcompat/PolicyDefinitionCRUDAtSubscription.Tests.ps1
@@ -1,7 +1,7 @@
 # setup the Pester environment for policy backcompat tests
 . (Join-Path $PSScriptRoot 'Common.ps1') 'Backcompat-PolicyDefinitionCRUDAtSubscription'
 
-Describe 'Backcompat-PolicyDefinitionCRUDAtSubscription' {
+Describe 'Backcompat-PolicyDefinitionCRUDAtSubscription' -Tag 'LiveOnly' {
 
     BeforeAll {
         # setup
