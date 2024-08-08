fa7a02982fffe86e70a5ce8330fa3694f1ffff1b
Lei jin <leijin@microsoft.com>
2024-08-08 09:38:35 +0800

    Rename folder

--- tools/AuthIntegrationTest/TestServicePrincipalAuthFlow.ps1
+++ tools/AuthIntegrationTest/TestServicePrincipalAuthFlow.ps1
@@ -1,5 +1,22 @@
+# ----------------------------------------------------------------------------------
+#
+# Copyright Microsoft Corporation
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------------
 [CmdletBinding()]
 param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $TenantId,
+
     [Parameter(ParameterSetName = 'CertificateFile', Mandatory = $true)]
     [Switch]
     $UseCertificateFile,
@@ -74,9 +91,7 @@ $importAzAccountsTo = "Import-Module Az.Accounts -RequiredVersion $accountsTo`n"
 $getToken = "Get-AzAccessToken`n"
 $getAzAcountVersion = "Get-Module -Name Az.Accounts`n"
 
-$baseCommand = @"
-."$PSScriptRoot/ConnectAzAccountLiveTest.ps1" -ServicePrincipalName "AzAccountsTest" -CredentialPrefix "AzAccountsTest"
-"@
+$baseCommand = ".'$PSScriptRoot/ServicePrincipalAuthFlow.ps1' -ServicePrincipalName 'AzAccountsTest' -CredentialPrefix 'AzAccountsTest' -Tenant ${Tenant}"
 
 $smokeTest = {
     try {
