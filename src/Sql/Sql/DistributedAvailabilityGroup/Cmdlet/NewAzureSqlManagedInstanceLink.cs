8838bcb86db172eaad0c3bb5ddcb66caf9e91984
jovancevic123 <jojovancevic@microsoft.com>
2024-09-03 11:58:22 +0200

    Style fix

--- src/Sql/Sql/DistributedAvailabilityGroup/Cmdlet/NewAzureSqlManagedInstanceLink.cs
+++ src/Sql/Sql/DistributedAvailabilityGroup/Cmdlet/NewAzureSqlManagedInstanceLink.cs
@@ -84,7 +84,7 @@ namespace Microsoft.Azure.Commands.Sql.ManagedInstanceHybridLink.Cmdlet
         [Parameter(Mandatory = true, ParameterSetName = CreateByNameParameterSet, HelpMessage = "Database names in the distributed availability group.")]
         [Parameter(Mandatory = true, ParameterSetName = CreateByParentObjectParameterSet, HelpMessage = "Database names in the distributed availability group.")]
         [ValidateNotNullOrEmpty]
-        public string[] Databases { get; set; }
+        public List<string> Databases { get; set; }
 
         /// <summary>
         /// Gets or sets the source endpoint
