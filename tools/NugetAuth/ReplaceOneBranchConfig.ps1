a2c3b5684c4f46b7493957ee669c41fcd6ed563f
Yabo Hu <yabhu@microsoft.com>
2024-09-25 10:07:59 +0800

    Delete tools/NugetAuth/ReplaceOneBranchConfig.ps1

--- tools/NugetAuth/ReplaceOneBranchConfig.ps1
+++
@@ -1,4 +0,0 @@
-$oneBranchConfigPath = Join-Path $PSScriptRoot 'OneBranchNuget.Config'
-$devConfigPath = Join-Path ($PSScriptRoot | Split-path -Parent | Split-path -Parent) 'Nuget.Config'
-
-Get-Content $oneBranchConfigPath -Raw | Set-Content $devConfigPath -Force
