1af1a73b87dc9c295c6a05e0086dd93798e8cba3
V V S N L Prasanna K <v-vprasannak@microsoft.com>
2024-09-26 10:57:40 +0530

    Email Services - data plane - inline attachment while sending an email.

--- src/Communication/EmailServicedata.Autorest/test/Send-AzEmailServicedataEmail.Tests.ps1
+++ src/Communication/EmailServicedata.Autorest/test/Send-AzEmailServicedataEmail.Tests.ps1
@@ -22,10 +22,20 @@ Describe 'Send-AzEmailServicedataEmail' {
                 DisplayName = "ContosoUser"
             }
         )
+        $fileBytes1 = [System.IO.File]::ReadAllBytes("test\inline-attachment.jpg")
+        $emailAttachment = @(
+	        @{
+		        ContentInBase64 = $fileBytes1
+		        ContentType = "png"
+		        Name = "inline-attachment.png"
+		        contentId = "inline-attachment"
+	        }
+        )
         $message = @{
 	        ContentSubject = "Test Email"
 	        RecipientTo = @($emailRecipientTo)  # Array of email address objects
 	        SenderAddress = $env.senderAddress
+            ContentHtml = "<html><head><title>Exciting offer!</title></head><body><img src='cid:inline-attachment' alt='kitten'/><h1>This exciting offer was created especially for you, our most loyal customer.</h1></body></html>"
 	        ContentPlainText = "This is the first email from ACS - HTML"	
         }
         $SendEmailResult = Send-AzEmailServicedataEmail -Message $message -endpoint $env.endPoint
