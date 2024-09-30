9c87888171ca51084576d04f7285c34c898c94d1
Wei Wei <weiwei@microsoft.com>
2024-09-30 09:17:46 +0000

    [Storage] Add live test to cover File/Table, and cover StorageContext generate in different ways (#26069)
    * [Storage] Add live test to cover File/Table, and cover StorageContext generate in different ways
    
    * remove change log

--- src/Storage/LiveTests/Storage.Management/QueueTests.ps1
+++ src/Storage/LiveTests/Storage.Management/QueueTests.ps1
@@ -7,8 +7,9 @@ Invoke-LiveTestScenario -Name "Queue basics" -Description "Test queue basic oper
     $queueName = New-LiveTestResourceName
     $queueName2 = New-LiveTestResourceName
     $location = $rg.Location
-    $account = New-AzStorageAccount -ResourceGroupName $rgName -Name $storageAccountName -Location $location -SkuName Standard_GRS 
+    $account = New-AzStorageAccount -ResourceGroupName $rgName -Name $storageAccountName -Location $location -SkuName Standard_GRS -AllowSharedKeyAccess $true -Tag @{"Az.Sec.DisableAllowSharedKeyAccess::Skip" = "For Powershell test."}
     $ctx = $account.Context 
+    $ctx1 = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey (Get-AzStorageAccountKey -ResourceGroupName $rgName -Name $storageAccountName)[0].Value
 
     $q = New-AzStorageQueue -Name $queueName -Context $ctx 
     Assert-AreEqual $queueName $q.Name 
@@ -17,8 +18,8 @@ Invoke-LiveTestScenario -Name "Queue basics" -Description "Test queue basic oper
     Assert-AreEqual 0 $q.ApproximateMessageCount
     Assert-AreEqual 0 $q.QueueProperties.ApproximateMessagesCount
 
-    $q = New-AzStorageQueue -Name $queueName2 -Context $ctx 
-    $qs = Get-AzStorageQueue -Context $ctx 
+    $q = New-AzStorageQueue -Name $queueName2 -Context $ctx1 
+    $qs = Get-AzStorageQueue -Context $ctx1 
     Assert-AreEqual 2 $qs.Count
 
     $sas = New-AzStorageAccountSASToken -Service Queue -ResourceType Container,Object,Service -Permission rwdl -ExpiryTime 3000-01-01 -Context $ctx 
@@ -26,7 +27,7 @@ Invoke-LiveTestScenario -Name "Queue basics" -Description "Test queue basic oper
     $qs = Get-AzStorageQueue -Context $ctxaccountsas 
     Assert-AreEqual 2 $qs.Count
 
-    $sas = New-AzStorageQueueSASToken -Name $queueName -Context $ctx -Permission ruap 
+    $sas = New-AzStorageQueueSASToken -Name $queueName -Context $ctx1 -Permission ruap 
     $sasctx = New-AzStorageContext -StorageAccountName $storageAccountName -SasToken $sas 
     $q = Get-AzStorageQueue -Name $queueName -Context $sasctx 
     Assert-AreEqual $queueName $q.Name
@@ -53,10 +54,10 @@ Invoke-LiveTestScenario -Name "Queue basics" -Description "Test queue basic oper
     Assert-Null $p.StartTime
     Assert-AreEqual "rau" $p.Permissions
 
-    $p = New-AzStorageQueueStoredAccessPolicy -Queue $queueName -Policy $policyName2 -Permission ruap -Context $ctx
+    $p = New-AzStorageQueueStoredAccessPolicy -Queue $queueName -Policy $policyName2 -Permission ruap -Context $ctx1
     $p = Get-AzStorageQueueStoredAccessPolicy -Queue $queueName -Context $ctx 
     Assert-AreEqual 2 $p.Count
-    $p = Get-AzStorageQueueStoredAccessPolicy -Queue $queueName -Policy $policyName1 -Context $ctx
+    $p = Get-AzStorageQueueStoredAccessPolicy -Queue $queueName -Policy $policyName1 -Context $ctx1
     Assert-AreEqual $policyName1 $p.Policy
 
     Remove-AzStorageQueueStoredAccessPolicy -Queue $queueName -Policy $policyName1 -Context $ctx
