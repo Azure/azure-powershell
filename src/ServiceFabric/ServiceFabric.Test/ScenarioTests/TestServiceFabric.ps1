51c36e4543e77c57fa2195e584376c8d41688e4f
Mwesigwa Guma (from Dev Box) <mwesigwaguma@microsoft.com>
2024-10-23 04:48:32 -0400

    test

--- src/ServiceFabric/ServiceFabric.Test/ScenarioTests/TestServiceFabric.ps1
+++ src/ServiceFabric/ServiceFabric.Test/ScenarioTests/TestServiceFabric.ps1
@@ -41,6 +41,7 @@ function Test-UpdateAzureRmServiceFabricReliability
 	Assert-AreEqual $reliabilityLevel $clusters[0].ReliabilityLevel
 }
 
+
 function Test-AddAzureRmServiceFabricClientCertificate
 {
 	$clusterName = "sfmcaddcert"
