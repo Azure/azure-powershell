030a6c2f444091a746d5241eec17a7a648679741
Vincent Dai <23257217+vidai-msft@users.noreply.github.com>
2024-10-23 03:26:23 -0700

    Refactor test script and use Start-TestSleep instead of Start-Sleep to avoid waiting during playback (#26443)

--- src/EventHub/EventHub.Autorest/test/Set-AzEventHubGeoDRConfigurationFailOver.Tests.ps1
+++ src/EventHub/EventHub.Autorest/test/Set-AzEventHubGeoDRConfigurationFailOver.Tests.ps1
@@ -1,28 +1,25 @@
-if(($null -eq $TestName) -or ($TestName -contains 'Set-AzEventHubGeoDRConfigurationFailOver'))
-{
-  $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
-  if (-Not (Test-Path -Path $loadEnvPath)) {
-      $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
-  }
-  . ($loadEnvPath)
-  $TestRecordingFile = Join-Path $PSScriptRoot 'Set-AzEventHubGeoDRConfigurationFailOver.Recording.json'
-  $currentPath = $PSScriptRoot
-  while(-not $mockingPath) {
-      $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
-      $currentPath = Split-Path -Path $currentPath -Parent
-  }
-  . ($mockingPath | Select-Object -First 1).FullName
+if (($null -eq $TestName) -or ($TestName -contains 'Set-AzEventHubGeoDRConfigurationFailOver')) {
+    $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+    if (-Not (Test-Path -Path $loadEnvPath)) {
+        $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+    }
+    . ($loadEnvPath)
+    $TestRecordingFile = Join-Path $PSScriptRoot 'Set-AzEventHubGeoDRConfigurationFailOver.Recording.json'
+    $currentPath = $PSScriptRoot
+    while (-not $mockingPath) {
+        $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+        $currentPath = Split-Path -Path $currentPath -Parent
+    }
+    . ($mockingPath | Select-Object -First 1).FullName
 }
 
 Describe 'Set-AzEventHubGeoDRConfigurationFailOver' {
     It 'Fail' -skip:$($env.secondaryLocation -eq '') {
         Set-AzEventHubGeoDRConfigurationFailOver -ResourceGroupName $env.resourceGroup -NamespaceName $env.secondaryNamespace -Name $env.alias
-        
-        while($drConfig.ProvisioningState -ne "Succeeded"){
+
+        while ($drConfig.ProvisioningState -ne "Succeeded") {
             $drConfig = Get-AzEventHubGeoDRConfiguration -Name $env.alias -ResourceGroupName $env.resourceGroup -NamespaceName $env.secondaryNamespace
-            if ($TestMode -ne 'playback') {
-                Start-Sleep 10
-            }
+            Start-TestSleep 10
         }
 
         $drConfig.Name | Should -Be $env.alias
@@ -31,29 +28,23 @@ Describe 'Set-AzEventHubGeoDRConfigurationFailOver' {
         $drConfig.Role | Should -Be "PrimaryNotReplicating"
 
         $drConfig = Remove-AzEventHubGeoDRConfiguration -Name $env.alias -ResourceGroupName $env.resourceGroup -NamespaceName $env.secondaryNamespace
-        if ($TestMode -ne 'playback') {
-            Start-Sleep 180
-        }
+        Start-TestSleep 180
         $drConfig = New-AzEventHubGeoDRConfiguration -Name $env.alias -ResourceGroupName $env.resourceGroup -NamespaceName $env.primaryNamespace -PartnerNamespace $env.secondaryNamespaceResourceId
-        
-        while($drConfig.ProvisioningState -ne "Succeeded"){
+
+        while ($drConfig.ProvisioningState -ne "Succeeded") {
             $drConfig = Get-AzEventHubGeoDRConfiguration -Name $env.alias -ResourceGroupName $env.resourceGroup -NamespaceName $env.primaryNamespace
-            if ($TestMode -ne 'playback') {
-                Start-Sleep 10
-            }
+            Start-TestSleep 10
         }
     }
     It 'FailViaIdentity' -skip:$($env.secondaryLocation -eq '') {
         $drConfig = Get-AzEventHubGeoDRConfiguration -Name $env.alias -ResourceGroupName $env.resourceGroup -NamespaceName $env.secondaryNamespace
-        
+
         Set-AzEventHubGeoDRConfigurationFailOver -InputObject $drConfig
-        
+
         do {
             $drConfig = Get-AzEventHubGeoDRConfiguration -Name $env.alias -ResourceGroupName $env.resourceGroup -NamespaceName $env.secondaryNamespace
-            if ($TestMode -ne 'playback') {
-                Start-Sleep 10
-            }
-        } while($drConfig.ProvisioningState -ne "Succeeded")
+            Start-TestSleep 10
+        } while ($drConfig.ProvisioningState -ne "Succeeded")
 
         $drConfig.Name | Should -Be $env.alias
         $drConfig.ResourceGroupName | Should -Be $env.resourceGroup
@@ -61,10 +52,8 @@ Describe 'Set-AzEventHubGeoDRConfigurationFailOver' {
         $drConfig.Role | Should -Be "PrimaryNotReplicating"
 
         $drConfig = Remove-AzEventHubGeoDRConfiguration -Name $env.alias -ResourceGroupName $env.resourceGroup -NamespaceName $env.secondaryNamespace
-        
-        if ($TestMode -ne 'playback') {
-            Start-Sleep 180
-        }
+
+        Start-TestSleep 180
 
         { Get-AzEventHubGeoDRConfiguration -Name $env.alias -ResourceGroupName $env.resourceGroup -NamespaceName $env.primaryNamespace -ErrorAction Stop } | Should -Throw
     }
