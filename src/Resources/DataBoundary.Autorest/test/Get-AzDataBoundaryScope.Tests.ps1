8cb4bf562ccaba8efd749e3f62e67ee0864373e4
NanxiangLiu <33285578+Nickcandy@users.noreply.github.com>
2024-10-26 16:28:25 +0800

    Generation (#26490)
    * intial cmdlets
    
    * tests
    
    * examples
    
    * testing
    
    * updated readme
    
    * fixing build
    
    * remove update files
    
    * removing update command
    
    * examples edit
    
    * naming
    
    * formatting
    
    * fix error
    
    * examples fix
    
    * description
    
    ---------
    
    Co-authored-by: Subhashni Kadhiresan <skadhiresan@microsoft.com>
    Co-authored-by: skadhiresan <165337276+skadhiresan@users.noreply.github.com>

---
+++ src/Resources/DataBoundary.Autorest/test/Get-AzDataBoundaryScope.Tests.ps1
@@ -0,0 +1,26 @@
+if(($null -eq $TestName) -or ($TestName -contains 'Get-AzDataBoundaryScope'))
+{
+  $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+  if (-Not (Test-Path -Path $loadEnvPath)) {
+      $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+  }
+  . ($loadEnvPath)
+  $TestRecordingFile = Join-Path $PSScriptRoot 'Get-AzDataBoundaryScope.Recording.json'
+  $currentPath = $PSScriptRoot
+  while(-not $mockingPath) {
+      $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+      $currentPath = Split-Path -Path $currentPath -Parent
+  }
+  . ($mockingPath | Select-Object -First 1).FullName
+}
+
+Describe 'Get-AzDataBoundaryScope' {
+    It 'Get' {
+        { 
+            $scope = "/subscriptions/" + $env.SubscriptionId
+            $boundaryData = Get-AzDataBoundaryScope -Scope $scope
+            $boundaryData.DataBoundary | Should -Be "EU"
+            $boundaryData.ProvisioningState | Should -Be "Succeeded"
+        } | Should -Not -Throw
+    }
+}
