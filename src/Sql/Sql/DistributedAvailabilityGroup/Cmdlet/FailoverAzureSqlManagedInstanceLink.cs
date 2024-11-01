dbc077fc5e5bfacdd72b466196ef211d5e6d5f6d
jovancevic123 <62032416+jovancevic123@users.noreply.github.com>
2024-11-01 15:41:26 +0100

    [SQL] Distributed Availability Groups cmdlet update to version 2023-08-01 API (#26224)
    * Adding autorest generated classes.
    
    * Output of commands changed. Create command updated.
    
    * Handling errors the proper way to see the error message instead of generic one.
    
    * Adding failover functionality.
    
    * Adding help files.
    
    * Removing replication mode parameter, it was added by mistake
    
    * Bug fix in failover cmdlet
    
    * Calling GET cmdlet after failover, because REST doesn't get the link after the operation is succeeded.
    
    * Testing done.
    
    * Adding LiveOnly to tests.
    
    * Altering box first test.
    
    * Changing output to list of strings. Adding handling error for planned failover type.
    
    * Removing breaking change announce.
    
    * Updating help files.
    
    * Style fix
    
    * Handling type errors for fixed strings
    
    * Comment fixes.
    
    * Authoring help files.
    
    * Updating help files, changelog.md and parameter descriptions.
    
    * Build fix.
    
    * Changing param Databases to Database because of PS rules, adding ChangeLog.
    
    * Breaking change issue csv added.
    
    ---------
    
    Co-authored-by: jovancevic123 <jojovancevic@microsoft.com>

--- src/Sql/Sql/DistributedAvailabilityGroup/Cmdlet/FailoverAzureSqlManagedInstanceLink.cs
+++ src/Sql/Sql/DistributedAvailabilityGroup/Cmdlet/FailoverAzureSqlManagedInstanceLink.cs
@@ -14,6 +14,7 @@
 
 using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
 using Microsoft.Azure.Commands.Sql.ManagedInstance.Model;
+using Microsoft.Azure.Commands.Sql.ManagedInstanceHybridLink.Cmdlet;
 using Microsoft.Azure.Commands.Sql.ManagedInstanceHybridLink.Model;
 using Microsoft.Azure.Management.Internal.Resources.Utilities.Models;
 using Microsoft.WindowsAzure.Commands.Utilities.Common;
@@ -23,27 +24,25 @@ using System.Globalization;
 using System.Linq;
 using System.Management.Automation;
 
-namespace Microsoft.Azure.Commands.Sql.ManagedInstanceHybridLink.Cmdlet
+namespace Microsoft.Azure.Commands.Sql.DistributedAvailabilityGroup.Cmdlet
 {
     /// <summary>
-    /// Cmdlet to update Managed Instance Link
+    /// Cmdlet to failover Managed Instance Link
     /// </summary>
-    [Cmdlet("Update", ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "SqlInstanceLink",
-        DefaultParameterSetName = UpdateByNameParameterSet, SupportsShouldProcess = true),
+    [Cmdlet("Start", ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "SqlInstanceLinkFailover",
+        DefaultParameterSetName = FailoverByNameParameterSet, SupportsShouldProcess = true),
         OutputType(typeof(AzureSqlManagedInstanceLinkModel))]
-    [Alias(VerbsCommon.Set + "-" + ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "SqlInstanceLink")]
-
-    public class UpdateAzureSqlManagedInstanceLink : AzureSqlManagedInstanceLinkCmdletBase
+    public class FailoverAzureSqlManagedInstanceLink : AzureSqlManagedInstanceLinkCmdletBase
     {
-        private const string UpdateByNameParameterSet = "UpdateByNameParameterSet";
-        private const string UpdateByParentObjectParameterSet = "UpdateByParentObjectParameterSet";
-        private const string UpdateByInputObjectParameterSet = "UpdateByInputObjectParameterSet";
-        private const string UpdateByResourceIdParameterSet = "UpdateByResourceIdParameterSet";
+        private const string FailoverByNameParameterSet = "FailoverByNameParameterSet";
+        private const string FailoverByParentObjectParameterSet = "FailoverByParentObjectParameterSet";
+        private const string FailoverByInputObjectParameterSet = "FailoverByInputObjectParameterSet";
+        private const string FailoverByResourceIdParameterSet = "FailoverByResourceIdParameterSet";
 
         /// <summary>
         /// Gets or sets the name of the resource group to use.
         /// </summary>
-        [Parameter(Mandatory = true, ParameterSetName = UpdateByNameParameterSet, Position = 0, HelpMessage = "Name of the resource group.")]
+        [Parameter(Mandatory = true, ParameterSetName = FailoverByNameParameterSet, Position = 0, HelpMessage = "Name of the resource group.")]
         [ResourceGroupCompleter]
         [ValidateNotNullOrEmpty]
         public override string ResourceGroupName { get; set; }
@@ -51,7 +50,7 @@ namespace Microsoft.Azure.Commands.Sql.ManagedInstanceHybridLink.Cmdlet
         /// <summary>
         /// Gets or sets the name of target managed instance
         /// </summary>
-        [Parameter(Mandatory = true, ParameterSetName = UpdateByNameParameterSet, Position = 1, HelpMessage = "Name of the managed instance.")]
+        [Parameter(Mandatory = true, ParameterSetName = FailoverByNameParameterSet, Position = 1, HelpMessage = "Name of the managed instance.")]
         [ResourceNameCompleter("Microsoft.Sql/managedInstances", "ResourceGroupName")]
         [ValidateNotNullOrEmpty]
         public string InstanceName { get; set; }
@@ -59,44 +58,51 @@ namespace Microsoft.Azure.Commands.Sql.ManagedInstanceHybridLink.Cmdlet
         /// <summary>
         /// Gets or sets the link name
         /// </summary>
-        [Parameter(Mandatory = true, ParameterSetName = UpdateByNameParameterSet, Position = 2, HelpMessage = "Managed Instance link name.")]
-        [Parameter(Mandatory = true, ParameterSetName = UpdateByParentObjectParameterSet, Position = 1, HelpMessage = "Managed Instance link name.")]
+        [Parameter(Mandatory = true, ParameterSetName = FailoverByNameParameterSet, Position = 2, HelpMessage = "Managed Instance link name.")]
+        [Parameter(Mandatory = true, ParameterSetName = FailoverByParentObjectParameterSet, Position = 1, HelpMessage = "Managed Instance link name.")]
+        [Parameter(Mandatory = true, ParameterSetName = FailoverByInputObjectParameterSet, Position = 1, HelpMessage = "Managed Instance link name.")]
         [ResourceNameCompleter("Microsoft.Sql/managedInstances/distributedAvailabilityGroups", nameof(ResourceGroupName), nameof(InstanceName))]
         [ValidateNotNullOrEmpty]
         [Alias("LinkName")]
         public string Name { get; set; }
 
         /// <summary>
-        /// Gets or sets the replication mode
+        /// Gets or sets the link failover type - Can be ForcedAllowDataLoss or Planned.
         /// </summary>
-        [Parameter(Mandatory = true, ParameterSetName = UpdateByNameParameterSet, Position = 3, HelpMessage = "Replication mode of a Managed Instance link. Parameter will be ignored during link creation.")]
-        [Parameter(Mandatory = true, ParameterSetName = UpdateByParentObjectParameterSet, Position = 2, HelpMessage = "Replication mode of a Managed Instance link. Parameter will be ignored during link creation.")]
-        [Parameter(Mandatory = false, ParameterSetName = UpdateByInputObjectParameterSet, Position = 1, HelpMessage = "Replication mode of a Managed Instance link. Parameter will be ignored during link creation.")]
-        [Parameter(Mandatory = true, ParameterSetName = UpdateByResourceIdParameterSet, Position = 1, HelpMessage = "Replication mode of a Managed Instance link. Parameter will be ignored during link creation.")]
-        [PSArgumentCompleter("Sync", "Async")]
+        [Parameter(Mandatory = true, ParameterSetName = FailoverByNameParameterSet, HelpMessage = "The failover type, can be ForcedAllowDataLoss or Planned.")]
+        [Parameter(Mandatory = true, ParameterSetName = FailoverByParentObjectParameterSet, HelpMessage = "The failover type, can be ForcedAllowDataLoss or Planned.")]
+        [Parameter(Mandatory = true, ParameterSetName = FailoverByInputObjectParameterSet, HelpMessage = "The failover type, can be ForcedAllowDataLoss or Planned.")]
+        [Parameter(Mandatory = true, ParameterSetName = FailoverByResourceIdParameterSet, HelpMessage = "The failover type, can be ForcedAllowDataLoss or Planned.")]
         [ValidateNotNullOrEmpty]
-        public string ReplicationMode { get; set; }
+        [PSArgumentCompleter("Planned", "ForcedAllowDataLoss")]
+        public string FailoverType { get; set; }
 
         /// <summary>
         /// Gets or sets the instance link resource Id
         /// </summary>
-        [Parameter(Mandatory = true, ParameterSetName = UpdateByResourceIdParameterSet, ValueFromPipelineByPropertyName = true, Position = 0, HelpMessage = "The instance link resource ID.")]
+        [Parameter(Mandatory = true, ParameterSetName = FailoverByResourceIdParameterSet, ValueFromPipelineByPropertyName = true, Position = 0, HelpMessage = "The instance link resource ID.")]
         [ValidateNotNullOrEmpty]
         public string ResourceId { get; set; }
 
         /// <summary>
         /// Gets or sets the instance Object
         /// </summary>
-        [Parameter(Mandatory = true, ParameterSetName = UpdateByParentObjectParameterSet, ValueFromPipeline = true, Position = 0, HelpMessage = "Instance input object.")]
+        [Parameter(Mandatory = true, ParameterSetName = FailoverByParentObjectParameterSet, ValueFromPipeline = true, Position = 0, HelpMessage = "Instance input object.")]
         [ValidateNotNullOrEmpty]
         public AzureSqlManagedInstanceModel InstanceObject { get; set; }
 
         /// <summary>
         /// Gets or set the input mi link object
         /// </summary>
-        [Parameter(Mandatory = true, ValueFromPipeline = true, ParameterSetName = UpdateByInputObjectParameterSet, Position = 0, HelpMessage = "Instance link input object.")]
+        [Parameter(Mandatory = true, ValueFromPipeline = true, ParameterSetName = FailoverByInputObjectParameterSet, Position = 0, HelpMessage = "Instance link input object.")]
         [ValidateNotNull]
         public AzureSqlManagedInstanceLinkModel InputObject { get; set; }
+
+        /// <summary>
+        /// Defines whether it is ok to skip the requesting of failover managed instance link confirmation
+        /// </summary>
+        [Parameter(HelpMessage = "Skip confirmation message for performing the action")]
+        public SwitchParameter Force { get; set; }
 
         /// <summary>
         /// Get the entities from the service
@@ -111,22 +117,22 @@ namespace Microsoft.Azure.Commands.Sql.ManagedInstanceHybridLink.Cmdlet
         {
             switch (ParameterSetName)
             {
-                case UpdateByNameParameterSet:
+                case FailoverByNameParameterSet:
                     // default case, we're getting RG, MI and MiLink names directly from args
                     break;
-                case UpdateByParentObjectParameterSet:
+                case FailoverByParentObjectParameterSet:
                     // we need to extract RG and MI name from the Instance object, MiLink name received directly from arg
                     ResourceGroupName = InstanceObject.ResourceGroupName;
                     InstanceName = InstanceObject.ManagedInstanceName;
                     break;
-                case UpdateByInputObjectParameterSet:
+                case FailoverByInputObjectParameterSet:
                     // we need to extract RG, MI and MiLink name directly from the MiLink object but replication mode can be either from the object or from arg
                     ResourceGroupName = InputObject.ResourceGroupName;
                     InstanceName = InputObject.InstanceName;
                     Name = InputObject.Name;
-                    ReplicationMode = this.IsParameterBound(c => c.ReplicationMode) ? ReplicationMode : InputObject.ReplicationMode;
+                    FailoverType = this.IsParameterBound(c => c.FailoverType) ? FailoverType : InputObject.ReplicationMode;
                     break;
-                case UpdateByResourceIdParameterSet:
+                case FailoverByResourceIdParameterSet:
                     // we need to derive RG, MI and MiLink name from resource id
                     var resourceInfo = new ResourceIdentifier(ResourceId);
                     ResourceGroupName = resourceInfo.ResourceGroupName;
@@ -143,7 +149,40 @@ namespace Microsoft.Azure.Commands.Sql.ManagedInstanceHybridLink.Cmdlet
                 string.Format(CultureInfo.InvariantCulture, Properties.Resources.SetAzureSqlInstanceLinkWarning, ResourceGroupName, InstanceName, Name),
                 Properties.Resources.ShouldProcessCaption))
             {
-                base.ExecuteCmdlet();
+                // message prompt requiring the customer to explicitly confirm the delete operation
+                if (!FailoverType.Equals("ForcedAllowDataLoss") || Force || (FailoverType.Equals("ForcedAllowDataLoss") &&
+                    ShouldContinue("Executing forced failover may result in loss of data which has not yet been replicated to the secondary instance. Do you wish to proceed?",
+                    Properties.Resources.ShouldProcessCaption)))
+                {
+                    // base.ExecuteCmdlet();
+                    ModelAdapter = InitModelAdapter();
+                    IEnumerable<AzureSqlManagedInstanceLinkModel> model = this.GetEntity();
+                    IEnumerable<AzureSqlManagedInstanceLinkModel> updatedModel = ApplyUserInputToModel(model);
+                    IEnumerable<AzureSqlManagedInstanceLinkModel> responseModel = default(IEnumerable<AzureSqlManagedInstanceLinkModel>);
+
+                    ConfirmAction(GetConfirmActionProcessMessage(), GetResourceId(updatedModel), () =>
+                    {
+                        responseModel = PersistChanges(updatedModel);
+                    });
+
+                    var link = ModelAdapter.GetManagedInstanceLink(ResourceGroupName, InstanceName, Name);
+                    IEnumerable<AzureSqlManagedInstanceLinkModel> response = new List<AzureSqlManagedInstanceLinkModel> { link };
+
+                    if (link != null && responseModel != null)
+                    {
+                        if (WriteResult())
+                        {
+                            WriteObject(TransformModelToOutputObject(response), true);
+                        }
+                    }
+                    else
+                    {
+                        if (WriteResult())
+                        {
+                            WriteObject(TransformModelToOutputObject(updatedModel));
+                        }
+                    }
+                }
             }
         }
 
@@ -155,21 +194,21 @@ namespace Microsoft.Azure.Commands.Sql.ManagedInstanceHybridLink.Cmdlet
         protected override IEnumerable<AzureSqlManagedInstanceLinkModel> ApplyUserInputToModel(IEnumerable<AzureSqlManagedInstanceLinkModel> model)
         {
             List<AzureSqlManagedInstanceLinkModel> newEntity = new List<AzureSqlManagedInstanceLinkModel> { };
-            var updatedModel = model.First();
-            updatedModel.ReplicationMode = ReplicationMode;
-            newEntity.Add(updatedModel);
+            var failoverModel = model.First();
+            failoverModel.FailoverMode = FailoverType;
+            newEntity.Add(failoverModel);
             return newEntity;
         }
 
         /// <summary>
-        /// Create the new Failover Group
+        /// Failovers managed instance link
         /// </summary>
         /// <param name="entity">The output of apply user input to model</param>
         /// <returns>The input entity</returns>
         protected override IEnumerable<AzureSqlManagedInstanceLinkModel> PersistChanges(IEnumerable<AzureSqlManagedInstanceLinkModel> entity)
         {
             return new List<AzureSqlManagedInstanceLinkModel>() {
-                ModelAdapter.UpdateManagedInstanceLink(entity.First())
+                ModelAdapter.FailoverManagedInstanceLink(entity.First())
             };
         }
     }
