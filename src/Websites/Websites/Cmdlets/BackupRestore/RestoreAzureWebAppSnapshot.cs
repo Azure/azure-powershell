165be0ac191711eea32e89aa2856c3ce81b3221e
Ashok Kumar Karasala <106590066+vkarasala@users.noreply.github.com>
2024-11-14 12:23:38 +0530

    [AppServices] RestoreAzureWebAppSnapshot fetch the data from Microsoft.Web RP similar to other commands

--- src/Websites/Websites/Cmdlets/BackupRestore/RestoreAzureWebAppSnapshot.cs
+++ src/Websites/Websites/Cmdlets/BackupRestore/RestoreAzureWebAppSnapshot.cs
@@ -51,11 +51,32 @@ namespace Microsoft.Azure.Commands.WebApps.Cmdlets.BackupRestore
         public override void ExecuteCmdlet()
         {
             base.ExecuteCmdlet();
-            var sourceApp = ResourcesClient.GetAppResource(InputObject.Name, InputObject.Slot);
+            var sourceAppLocation = string.Empty;
+            var sourceAppArmResourceId = string.Empty;
+
+            // Try to get the source app location and resource ID from Microsoft.Web RP
+            try
+            {
+                var app = new PSSite(WebsitesClient.GetWebApp(ResourceGroupName, Name, Slot));
+                sourceAppLocation = app.Location;
+                sourceAppArmResourceId = app.Id;
+            }
+            catch (Exception)
+            {
+            }
+
+            // Fall back code to fetch the source app location and resource ID from ARM cache, Useful with disaster recovery scenaior's when Microsoft.Web RP is not accessible
+            if (string.IsNullOrEmpty(sourceAppLocation) || string.IsNullOrEmpty(sourceAppArmResourceId))
+            {
+                var sourceApp = ResourcesClient.GetAppResource(InputObject.Name, InputObject.Slot);
+                sourceAppLocation = sourceApp?.Location;
+                sourceAppArmResourceId = sourceApp?.Id;
+            }
+
             SnapshotRecoverySource source = new SnapshotRecoverySource()
             {
-                Location = sourceApp?.Location,
-                Id = sourceApp?.Id
+                Location = sourceAppLocation,
+                Id = sourceAppArmResourceId
             };
             SnapshotRestoreRequest recoveryReq = new SnapshotRestoreRequest()
             {
