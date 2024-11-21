9123a99fa2ead3b54dd5ff6431363b0740cd24fb
Tate Smalligan <161764628+tsmallig33@users.noreply.github.com>
2024-11-20 23:24:16 -0600

    Add Diagnostics and Warnings to Deployments WhatIf/Validate cmdlets (#26732)
    * Add diagnostics to what if output
    
    * Update ChangeLog.md
    
    * Add unsupportedReason to diagnostics
    
    * Add Diagnostics to TestDeployments
    
    * Remove comments
    
    * Update Test cmdlets to match WhatIf output
    
    * Update with Environment.NewLine
    
    * Update src/Resources/Resources/ChangeLog.md
    
    ---------
    
    Co-authored-by: Tate Smalligan <tasmalligan@microsoft.com>
    Co-authored-by: Yabo Hu <yabhu@microsoft.com>

--- src/Resources/ResourceManager/SdkClient/NewResourceManagerSdkClient.cs
+++ src/Resources/ResourceManager/SdkClient/NewResourceManagerSdkClient.cs
@@ -491,9 +491,9 @@ namespace Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkClient
                 switch (validationResult)
                 {
                     case DeploymentExtended deploymentExtended:
-                        return new TemplateValidationInfo(deploymentExtended.Properties?.Providers?.ToList() ?? new List<Provider>(), new List<ErrorDetail>());
+                        return new TemplateValidationInfo(deploymentExtended.Properties?.Providers?.ToList() ?? new List<Provider>(), new List<ErrorDetail>(), deploymentExtended.Properties?.Diagnostics?.ToList() ?? new List<DeploymentDiagnosticsDefinition>());
                     case DeploymentValidationError deploymentValidationError:
-                        return new TemplateValidationInfo(new List<Provider>(), new List<ErrorDetail>(deploymentValidationError.Error.AsArray()));
+                        return new TemplateValidationInfo(new List<Provider>(), new List<ErrorDetail>(deploymentValidationError.Error.AsArray()), new List<DeploymentDiagnosticsDefinition>());
                     default:
                         throw new InvalidOperationException($"Received unexpected type {validationResult.GetType()}");
                 }
@@ -501,7 +501,7 @@ namespace Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkClient
             catch (Exception ex)
             {
                 var error = HandleError(ex).FirstOrDefault();
-                return new TemplateValidationInfo(new List<Provider>(), error.AsArray().ToList());
+                return new TemplateValidationInfo(new List<Provider>(), error.AsArray().ToList(), new List<DeploymentDiagnosticsDefinition>());
             }
         }
 
@@ -1716,8 +1716,8 @@ namespace Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkClient
         /// Validates a given deployment.
         /// </summary>
         /// <param name="parameters">The deployment create options</param>
-        /// <returns>The validation errors if there's any, or empty list otherwise.</returns>
-        public virtual List<PSResourceManagerError> ValidateDeployment(PSDeploymentCmdletParameters parameters)
+        /// <returns>The validation info</returns>
+        public virtual TemplateValidationInfo ValidateDeployment(PSDeploymentCmdletParameters parameters)
         {
             if (parameters.DeploymentName == null)
             {
@@ -1731,7 +1731,7 @@ namespace Microsoft.Azure.Commands.ResourceManager.Cmdlets.SdkClient
             {
                 WriteVerbose(ProjectResources.TemplateValid);
             }
-            return validationInfo.Errors.Select(e => e.ToPSResourceManagerError()).ToList();
+            return validationInfo;
         }
 
         public string GetDeploymentErrorMessagesWithOperationId(DeploymentOperationErrorInfo errorInfo, string deploymentName = null, string correlationId = null)
