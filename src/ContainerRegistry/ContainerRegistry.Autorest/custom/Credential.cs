3e5fdfa46a17d48a9a2e4d53203be407eb7d5278
YanaXu <sheshouxy@gmail.com>
2024-12-12 17:41:26 +0800

    Update password from String to SecureString

--- src/ContainerRegistry/ContainerRegistry.Autorest/custom/Credential.cs
+++ src/ContainerRegistry/ContainerRegistry.Autorest/custom/Credential.cs
@@ -10,12 +10,12 @@ namespace Microsoft.Azure.PowerShell.Cmdlets.ContainerRegistry.Models.Api202301P
         public PSContainerRegistryCredential(RegistryListCredentialsResult credentials)
         {
             Username = credentials?.Username;
-            Password = credentials?.Password?.Length > 0 ? credentials.Password[0]?.Value : string.Empty;
-            Password2 = credentials?.Password?.Length > 1 ? credentials.Password[1]?.Value : string.Empty;
+            Password = credentials?.Password?.Length > 0 ? credentials.Password[0]?.Value.ToSecureString() : string.Empty.ToSecureString();
+            Password2 = credentials?.Password?.Length > 1 ? credentials.Password[1]?.Value.ToSecureString() : string.Empty.ToSecureString();
         }
 
         public string Username { get; set; }
-        public string Password { get; set; }
-        public string Password2 { get; set; }
+        public System.Security.SecureString Password { get ; set; }
+        public System.Security.SecureString Password2 { get ; set; }
     }
 }
