632d5afe18f91e384982691b91b4a6b78f7e9a0d
Yabo Hu <yabhu@microsoft.com>
2024-12-31 13:10:24 +0800

    Test whether resourcemanager track1 sdk can be removed from testfx

--- tools/TestFx/PageExtensions.cs
+++ tools/TestFx/PageExtensions.cs
@@ -12,7 +12,6 @@
 // limitations under the License.
 // ----------------------------------------------------------------------------------
 
-using Microsoft.Azure.Management.ResourceManager.Models;
 using System.Collections.Generic;
 using System.Reflection;
 
