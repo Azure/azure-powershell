c39c87fee29c72d29c69b51475c6d5785bf5452c
Janiel Martell Colon <janielm@microsoft.com>
2025-02-07 13:05:17 -0800

    add tests

--- src/TrustedSigning/TrustedSigning.Autorest/test/New-AzTrustedSigningCertificateProfile.Tests.ps1
+++ src/TrustedSigning/TrustedSigning.Autorest/test/New-AzTrustedSigningCertificateProfile.Tests.ps1
@@ -1,29 +1,30 @@
-if(($null -eq $TestName) -or ($TestName -contains 'New-AzTrustedSigningCertificateProfile'))
-{
-  $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
-  if (-Not (Test-Path -Path $loadEnvPath)) {
-      $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
-  }
-  . ($loadEnvPath)
-  $TestRecordingFile = Join-Path $PSScriptRoot 'New-AzTrustedSigningCertificateProfile.Recording.json'
-  $currentPath = $PSScriptRoot
-  while(-not $mockingPath) {
-      $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
-      $currentPath = Split-Path -Path $currentPath -Parent
-  }
-  . ($mockingPath | Select-Object -First 1).FullName
+if (($null -eq $TestName) -or ($TestName -contains 'New-AzTrustedSigningCertificateProfile')) {
+    $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+    if (-Not (Test-Path -Path $loadEnvPath)) {
+        $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+    }
+    . ($loadEnvPath)
+    $TestRecordingFile = Join-Path $PSScriptRoot 'New-AzTrustedSigningCertificateProfile.Recording.json'
+    $currentPath = $PSScriptRoot
+    while (-not $mockingPath) {
+        $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+        $currentPath = Split-Path -Path $currentPath -Parent
+    }
+    . ($mockingPath | Select-Object -First 1).FullName
 }
 
 Describe 'New-AzTrustedSigningCertificateProfile' {
-    It 'CreateExpanded' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
-    }
-
-    It 'CreateViaJsonFilePath' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
-    }
-
-    It 'CreateViaJsonString' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+    It 'CreateExpanded' {
+        $accountName = $profileName = 'azpstestprofile'
+        
+        New-AzTrustedSigningAccount -ResourceGroupName $env.rg -AccountName $accountName -Location $env.location -SkuName 'Basic'
+        
+        $sut = New-AzTrustedSigningCertificateProfile -AccountName $accountName -ResourceGroupName $env.rg -ProfileName $profileName `
+            -IdentityValidationId $env.IdentityValidationId -ProfileType:PublicTrustTest
+		
+        $sut | Should -Not -BeNullOrEmpty
+        $sut.Name | Should -Be $profileName
+        $sut.ProfileType | Should -Be 'PublicTrustTest'
+        $sut.IdentityValidationId | Should -Be $env.IdentityValidationId
     }
 }
--- src/TrustedSigning/TrustedSigning.Autorest/test/Update-AzTrustedSigningAccount.Tests.ps1
+++ src/TrustedSigning/TrustedSigning.Autorest/test/Update-AzTrustedSigningAccount.Tests.ps1
@@ -1,33 +1,31 @@
-if(($null -eq $TestName) -or ($TestName -contains 'Update-AzTrustedSigningAccount'))
-{
-  $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
-  if (-Not (Test-Path -Path $loadEnvPath)) {
-      $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
-  }
-  . ($loadEnvPath)
-  $TestRecordingFile = Join-Path $PSScriptRoot 'Update-AzTrustedSigningAccount.Recording.json'
-  $currentPath = $PSScriptRoot
-  while(-not $mockingPath) {
-      $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
-      $currentPath = Split-Path -Path $currentPath -Parent
-  }
-  . ($mockingPath | Select-Object -First 1).FullName
+if (($null -eq $TestName) -or ($TestName -contains 'Update-AzTrustedSigningAccount')) {
+    $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+    if (-Not (Test-Path -Path $loadEnvPath)) {
+        $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+    }
+    . ($loadEnvPath)
+    $TestRecordingFile = Join-Path $PSScriptRoot 'Update-AzTrustedSigningAccount.Recording.json'
+    $currentPath = $PSScriptRoot
+    while (-not $mockingPath) {
+        $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+        $currentPath = Split-Path -Path $currentPath -Parent
+    }
+    . ($mockingPath | Select-Object -First 1).FullName
 }
 
 Describe 'Update-AzTrustedSigningAccount' {
-    It 'UpdateExpanded' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
-    }
+    It 'UpdateExpanded' {
+        $accountName = 'azpstestaccountupdate'
+        New-AzTrustedSigningAccount -ResourceGroupName $env.rg -AccountName $accountName -Location $env.location -SkuName 'Basic'
 
-    It 'UpdateViaJsonString' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
-    }
-
-    It 'UpdateViaJsonFilePath' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
-    }
-
-    It 'UpdateViaIdentityExpanded' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+        # update doesn't return the new account object
+        Update-AzTrustedSigningAccount -ResourceGroupName $env.rg -AccountName $accountName -SkuName 'Premium' 
+        $sut = Get-AzTrustedSigningAccount -ResourceGroupName $env.rg -AccountName $accountName
+        
+        $sut | Should -Not -BeNullOrEmpty
+        $sut.Name | Should -Be $accountName
+        $sut.Location | Should -Be $env.location
+        $sut.SkuName | Should -Not -Be 'Basic'
+        $sut.SkuName | Should -Be 'Premium'
     }
 }
