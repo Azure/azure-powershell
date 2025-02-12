98b758fdb434e31a19bbcf70e70ed2c98280b167
Nori Zhang <norizhang@microsoft.com>
2025-02-12 14:07:49 +0800

    update test case

--- src/Network/Network.Test/ScenarioTests/NetworkVirtualApplianceBootDiagnosticsTest.ps1
+++ src/Network/Network.Test/ScenarioTests/NetworkVirtualApplianceBootDiagnosticsTest.ps1
@@ -18,69 +18,59 @@ Test getting the boot diagnostic logs for an existing NetworkVirtualAppliance VM
 #>
 function Test-NetworkVirtualApplianceBootDiagnostics
 {
-    # Resource details
-    $rgname = Get-ResourceGroupName
+    $rgname = "nashoktest"
     $location = "australiaeast"
-    $nvaname = Get-ResourceName
-    $wanname = Get-ResourceName
-    $hubname = Get-ResourceName
+    $nvaname = "chkptbd1031"
+    $wanname = "sctestvwan"
+    $hubname = "sctesthub"
     $resourceTypeParent = "Microsoft.Network/networkVirtualAppliance"
-    # NVA sku details
     $vendor = "checkpoint"
     $scaleunit = 2
     $version = 'latest'
     $asn = 64512
     $prefix = "10.1.0.0/16"
-    # Storage account details where logs will be copied
     $storetype = 'Standard_GRS'
     $containerName = "testcontainer"
-    $serialConsoleLogsblobName = "example.txt"
-    $consoleScreenshotblobName = "screenshot.png"
-    $storeName = "nvabootdiagstorage";
+    $blobName = "example.txt"
+    $storeName = "stonashoktest";
     try{
-        $resourceGroup = New-AzResourceGroup -Name $rgname -Location $location
-        $sku = New-AzVirtualApplianceSkuProperty -VendorName $vendor -BundledScaleUnit $scaleunit -MarketPlaceVersion $version
-        Assert-NotNull $sku
+        # Retrieve the Resource Group
+        $resourceGroup = Get-AzResourceGroup -Name $rgname
+        Assert-NotNull $resourceGroup
 
-        $wan = New-AzVirtualWan -ResourceGroupName $rgname -Name $wanname -Location $location
-        $hub = New-AzVirtualHub -ResourceGroupName $rgname -Name $hubname -Location $location -VirtualWan $wan -AddressPrefix $prefix
+        # Retrieve the Virtual WAN
+        $virtualWan = Get-AzVirtualWan -ResourceGroupName $rgname -Name $wanname
+        Assert-NotNull $virtualWan
 
-        # Wait for Virtual Hub Routing State to become Provisioned or Failed
-        while ($hub.RoutingState -eq "Provisioning")
-        {
-            Start-TestSleep -Seconds 30
-            $hub = Get-AzVirtualHub -ResourceGroupName $rgName -Name $hubname
-        }
-        Assert-AreEqual $hub.RoutingState "Provisioned"
+        # Retrieve the Virtual Hub within the Virtual WAN
+        $virtualHub = Get-AzVirtualHub -ResourceGroupName $rgname -Name $hubname
+        Assert-NotNull $virtualHub
 
-        $nva = New-AzNetworkVirtualAppliance -ResourceGroupName $rgname -Name $nvaname -Location $location -VirtualApplianceAsn $asn -VirtualHubId $hub.Id -Sku $sku -CloudInitConfiguration "echo hi" 
+        # Retrieve the Network Virtual Appliance (NVA) within the Virtual WAN
+        $nva = Get-AzNetworkVirtualAppliance -ResourceGroupName $rgname -Name $nvaname
         Assert-NotNull $nva
 
-        # Create a new storage account
-        New-AzStorageAccount -ResourceGroupName $rgname -Name $storeName -Location $location -Type $storetype
-        $key = Get-AzStorageAccountKey -ResourceGroupName $rgname -Name $storeName
-        $context = New-AzStorageContext -StorageAccountName $storeName -StorageAccountKey $key[0].Value
-        # Create container within the storage
-        New-AzStorageContainer -Name $containerName -Context $context
-        $container = Get-AzStorageContainer -Name $containerName -Context $context
-        # Upload an empty blob for storing serial console logs 
-        $serialLogsBlob = Set-AzStorageBlobContent -Container $containerName -Blob $serialConsoleLogsblobName -Context $context
-        # Upload an empty blob for storing console screen shot 
-        $consoleScreenshotblob = Set-AzStorageBlobContent -Container $containerName -Blob $consoleScreenshotblobName -Context $context
+        # Retrieve the Storage Account
+        $storageAccount = Get-AzStorageAccount -ResourceGroupName $rgname -Name $storeName
+        Assert-NotNull $storageAccount
 
-        #Generate a sas url for both the blobs created above
-        $serialConsoleLogsSasUrl = New-AzStorageBlobSASToken -Container $containerName -Blob $serialConsoleLogsblobName -Context $context -Permission "rw" -ExpiryTime ([System.DateTime]::UtcNow).AddDays(1) -StartTime ([System.DateTime]::UtcNow).AddHours(-1) -FullUri
-        Assert-NotNull $serialConsoleLogsSasUrl
-        $consoleScreenshotSasUrl = New-AzStorageBlobSASToken -Container $containerName -Blob $consoleScreenshotblobName -Context $context -Permission "rw" -ExpiryTime ([System.DateTime]::UtcNow).AddDays(1) -StartTime ([System.DateTime]::UtcNow).AddHours(-1) -FullUri
-        Assert-NotNull $consoleScreenshotSasUrl
+        # Get the storage account context
+        $storageAccountContext = $storageAccount.Context
+        Assert-NotNull $storageAccountContext
 
-        # Call PS cmdlet to retrieve boot diagnostic logs for this NVA for VM instance 0
-        $nvabootdiagnostics = Get-AzNetworkVirtualApplianceBootDiagnostics -ResourceGroupName $rgname -Name $nvaname -InstanceId 0 -SerialConsoleStorageSasUrl $serialConsoleLogsSasUrl -ConsoleScreenshotStorageSasUrl $consoleScreenshotSasUrl
+        # Generate SAS token for the existing Blob
+        #$sasToken = New-AzStorageBlobSASToken -Container $containerName -Blob $blobName -Context $storageAccountContext -Permission "rw" -ExpiryTime ([System.DateTime]::UtcNow).AddDays(1) -StartTime ([System.DateTime]::UtcNow).AddHours(-1) -FullUri
+        $sasToken = "https://stonashoktest.blob.core.windows.net/testcontainer/example.txt?sp=rw&st=2025-02-12T05:01:16Z&se=2025-02-14T13:01:16Z&skoid=7f5b7efd-fa3f-4fbd-9430-8cf0f0ca6822&sktid=72f988bf-86f1-41af-91ab-2d7cd011db47&skt=2025-02-12T05:01:16Z&ske=2025-02-14T13:01:16Z&sks=b&skv=2022-11-02&spr=https&sv=2022-11-02&sr=b&sig=hunV37f5Mc%2By4Cln6PfzUZzvMFm%2B4eMfGQIWZ6H70ZI%3D"
+        Assert-NotNull $sasToken
+        Write-Host "SAS uri for the blob '$blobName' is: $sasToken"
+
+        $sasUrlScreenShot = "https://stonashoktest.blob.core.windows.net/testcontainer/pstestscreenshot.png?sp=rw&st=2025-02-12T05:02:57Z&se=2025-02-12T13:02:57Z&skoid=7f5b7efd-fa3f-4fbd-9430-8cf0f0ca6822&sktid=72f988bf-86f1-41af-91ab-2d7cd011db47&skt=2025-02-12T05:02:57Z&ske=2025-02-12T13:02:57Z&sks=b&skv=2022-11-02&spr=https&sv=2022-11-02&sr=b&sig=dVMyalbtEwn6ekXs6p4VN6Kmo%2FmklmkbN3Vk6RhEF%2FY%3D"
+
+        $nvabootdiagnostics = Get-AzNetworkVirtualApplianceBootDiagnostics -ResourceGroupName $rgname -Name $nvaname -InstanceId 0 -SerialConsoleStorageSasUrl $sasToken -ConsoleScreenshotStorageSasUrl $sasUrlScreenShot
         Assert-AreEqual $nvabootdiagnostics.Status "Succeeded"
    	} 
     finally{
         # Clean up.
-        Clean-ResourceGroup $rgname
 	} 
 
 }
