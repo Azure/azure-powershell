ad0b030227b49ce49f7ae1bbdb5e10f7e973a055
Nandita Ashok (from Dev Box) <nanditaashok@microsoft.com>
2025-02-20 13:57:28 -0800

    remove unused directives

--- src/Network/Network/NetworkVirtualAppliance/GetNetworkVirtualApplianceBootDiagnosticsCommand.cs
+++ src/Network/Network/NetworkVirtualAppliance/GetNetworkVirtualApplianceBootDiagnosticsCommand.cs
@@ -23,8 +23,6 @@ using System.Collections.Generic;
 using System.Management.Automation;
 using System.Security;
 using Microsoft.WindowsAzure.Commands.Common;
-using Microsoft.WindowsAzure.Commands.Utilities.Common;
-using Microsoft.WindowsAzure.Commands.Common.CustomAttributes;
 
 namespace Microsoft.Azure.Commands.Network
 {
