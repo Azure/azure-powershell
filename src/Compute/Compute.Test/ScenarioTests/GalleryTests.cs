c9959ff802fd5d1f9dafd3c5e31cac5bcbc7512c
haider agha <haagha@microsoft.com>
2025-02-24 14:19:07 -0500

    first pass

--- src/Compute/Compute.Test/ScenarioTests/GalleryTests.cs
+++ src/Compute/Compute.Test/ScenarioTests/GalleryTests.cs
@@ -74,30 +74,9 @@ namespace Microsoft.Azure.Commands.Compute.Test.ScenarioTests
 
         [Fact]
         [Trait(Category.AcceptanceType, Category.CheckIn)]
-        public void testgennewazgallery()
-        {
-            TestRunner.RunTestScript("TestGen-newazgallery");
-        }
-
-        [Fact]
-        [Trait(Category.AcceptanceType, Category.CheckIn)]
-        public void testgenupdateazgallery()
-        {
-            TestRunner.RunTestScript("TestGen-updateazgallery");
-        }
-
-        [Fact]
-        [Trait(Category.AcceptanceType, Category.CheckIn)]
-        public void testgennewazgalleryimageversion()
-        {
-            TestRunner.RunTestScript("TestGen-newazgalleryimageversion");
-        }
-
-        [Fact]
-        [Trait(Category.AcceptanceType, Category.CheckIn)]
         public void testgenupdateazgalleryimageversion()
         {
-            TestRunner.RunTestScript("TestGen-updateazgalleryimageversion");
+            TestRunner.RunTestScript("TestGen-BlockDeletionBeforeEndOfLife");
         }
-                    }
+    }
 }
