a129a80f65112f1aaa2f63b4b845c983b043c69f
Yabo Hu <yabhu@microsoft.com>
2025-04-08 22:06:02 +0800

    Update AccountTests.cs

--- src/NetAppFiles/NetAppFiles.Test/ScenarioTests/AccountTests.cs
+++ src/NetAppFiles/NetAppFiles.Test/ScenarioTests/AccountTests.cs
@@ -46,7 +46,7 @@ namespace Microsoft.Azure.Commands.NetAppFiles.Test.ScenarioTests.ScenarioTest
 
         [Fact]
         //[Fact(Skip = "Doesn't work at the moment due to service side issue, un-skip when it is working again")]
-        [Trait(Category.AcceptanceType, Category.CheckIn)]        
+        [Trait(Category.AcceptanceType, Category.LiveOnly)]        
         public void TestAccountCMK()
         {
             TestRunner.RunTestScript("Test-AccountCMK");
