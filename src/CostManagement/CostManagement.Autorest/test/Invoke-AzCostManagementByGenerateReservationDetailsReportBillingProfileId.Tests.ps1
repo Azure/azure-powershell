58ffe1ba797aed3b261a817b06f72110d8397783
Jinpei Li <v-jinpel@microsoft.com>
2025-04-14 18:39:45 +0800

    Az.CostManagement update generation tool version: autorest.powershell v3->v4

---
+++ src/CostManagement/CostManagement.Autorest/test/Invoke-AzCostManagementByGenerateReservationDetailsReportBillingProfileId.Tests.ps1
@@ -0,0 +1,29 @@
+if(($null -eq $TestName) -or ($TestName -contains 'Invoke-AzCostManagementByGenerateReservationDetailsReportBillingProfileId'))
+{
+  $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+  if (-Not (Test-Path -Path $loadEnvPath)) {
+      $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+  }
+  . ($loadEnvPath)
+  $TestRecordingFile = Join-Path $PSScriptRoot 'Invoke-AzCostManagementByGenerateReservationDetailsReportBillingProfileId.Recording.json'
+  $currentPath = $PSScriptRoot
+  while(-not $mockingPath) {
+      $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+      $currentPath = Split-Path -Path $currentPath -Parent
+  }
+  . ($mockingPath | Select-Object -First 1).FullName
+}
+
+Describe 'Invoke-AzCostManagementByGenerateReservationDetailsReportBillingProfileId' {
+    It 'By' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'ByViaIdentityBillingAccount' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'ByViaIdentity' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+}
