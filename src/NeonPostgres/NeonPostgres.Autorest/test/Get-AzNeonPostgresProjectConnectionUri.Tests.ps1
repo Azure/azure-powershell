ce1e95f3f3b8b65671a1a599cb33e8cc7fc01021
Srinivas Alluri <sralluri@microsoft.com>
2025-04-28 17:09:43 +0530

    added neon postgres project, branches, databases commands

---
+++ src/NeonPostgres/NeonPostgres.Autorest/test/Get-AzNeonPostgresProjectConnectionUri.Tests.ps1
@@ -0,0 +1,49 @@
+if(($null -eq $TestName) -or ($TestName -contains 'Get-AzNeonPostgresProjectConnectionUri'))
+{
+  $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+  if (-Not (Test-Path -Path $loadEnvPath)) {
+      $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+  }
+  . ($loadEnvPath)
+  $TestRecordingFile = Join-Path $PSScriptRoot 'Get-AzNeonPostgresProjectConnectionUri.Recording.json'
+  $currentPath = $PSScriptRoot
+  while(-not $mockingPath) {
+      $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+      $currentPath = Split-Path -Path $currentPath -Parent
+  }
+  . ($mockingPath | Select-Object -First 1).FullName
+}
+
+Describe 'Get-AzNeonPostgresProjectConnectionUri' {
+    It 'GetExpanded' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'GetViaJsonString' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'GetViaJsonFilePath' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'Get' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'GetViaIdentityOrganizationExpanded' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'GetViaIdentityOrganization' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'GetViaIdentityExpanded' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    It 'GetViaIdentity' -skip {
+        { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+}
--- src/NeonPostgres/NeonPostgres/help/Az.NeonPostgres.md
+++ src/NeonPostgres/NeonPostgres/help/Az.NeonPostgres.md
@@ -11,15 +11,48 @@ Locale: en-US
 Microsoft Azure PowerShell: NeonPostgres cmdlets
 
 ## Az.NeonPostgres Cmdlets
+### [Get-AzNeonPostgresBranch](Get-AzNeonPostgresBranch.md)
+Get a Branch
+
+### [Get-AzNeonPostgresNeonDatabase](Get-AzNeonPostgresNeonDatabase.md)
+List NeonDatabase resources by Branch
+
+### [Get-AzNeonPostgresNeonRole](Get-AzNeonPostgresNeonRole.md)
+List NeonRole resources by Branch
+
 ### [Get-AzNeonPostgresOrganization](Get-AzNeonPostgresOrganization.md)
 Get a OrganizationResource
+
+### [Get-AzNeonPostgresProject](Get-AzNeonPostgresProject.md)
+Get a Project
+
+### [Get-AzNeonPostgresProjectConnectionUri](Get-AzNeonPostgresProjectConnectionUri.md)
+Action to retrieve the connection URI for the Neon Database.
+
+### [New-AzNeonPostgresBranch](New-AzNeonPostgresBranch.md)
+create a Branch
 
 ### [New-AzNeonPostgresOrganization](New-AzNeonPostgresOrganization.md)
 create a OrganizationResource
 
+### [New-AzNeonPostgresProject](New-AzNeonPostgresProject.md)
+create a Project
+
+### [Remove-AzNeonPostgresBranch](Remove-AzNeonPostgresBranch.md)
+Delete a Branch
+
 ### [Remove-AzNeonPostgresOrganization](Remove-AzNeonPostgresOrganization.md)
 Delete a OrganizationResource
 
+### [Remove-AzNeonPostgresProject](Remove-AzNeonPostgresProject.md)
+Delete a Project
+
+### [Update-AzNeonPostgresBranch](Update-AzNeonPostgresBranch.md)
+update a Branch
+
 ### [Update-AzNeonPostgresOrganization](Update-AzNeonPostgresOrganization.md)
 update a OrganizationResource
+
+### [Update-AzNeonPostgresProject](Update-AzNeonPostgresProject.md)
+update a Project
 
