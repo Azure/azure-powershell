eb2994376bf02377831176c808d8c9062a8b7ad6
Yabo Hu <yabhu@microsoft.com>
2025-05-02 15:07:51 +0800

    finally works

--- tools/Mcp/src/index.ts
+++ tools/Mcp/src/index.ts
@@ -1,30 +1,77 @@
-import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
-import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
-import { exec } from 'child_process';
-import { z } from "zod";
-import { generateByAutorest } from "./services/toolServices.js";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js"
+import { z, ZodRawShape } from "zod";
+import { generateByAutorest, toolServices } from "./services/toolServices.js";
+import { CodegenServer } from "./models/CodegenServer.js";
+import specs from "./specs/Specs.json" assert { type: "json" };
+import { toolParameterSchema, toolSchema } from "./types.js";
 
 // Create server instance
-const server = new McpServer({
-    name: "codegen",
-    version: "1.0.0",
-    capabilities: {
-      resources: {},
-      tools: {},
-    },
-});
+// const server = new McpServer({
+//     name: "codegen",
+//     version: "1.0.0",
+//     capabilities: {
+//       resources: {},
+//       tools: {},
+//     },
+// });
 
-server.tool(
-    "generate-autorest",
-    "Generate code using autorest",
-    {workingDirectory: z.string().describe("absolute path of the README.md where the code will be generated")},
-    ({workingDirectory}) => generateByAutorest(workingDirectory),
-);
+// const toolSchemas = specs.tools as toolSchema[];
+// for (const schema of toolSchemas) {
+//     const parameter = createToolParameterfromSchema(schema.parameters);
+//     const callBack = toolServices<typeof parameter>(schema.callbackName);
+//     server.tool(
+//         schema.name,
+//         schema.description,
+//         parameter,
+//         (parameter) => callBack(parameter)
+//     );
+// }
+// function createToolParameterfromSchema(schemas: toolParameterSchema[]){
+//     const parameter: {[k: string]: z.ZodTypeAny} = {}; 
+//     for (const schema of schemas) {
+//         switch (schema.type) {
+//             case "string":
+//                 parameter[schema.name] = z.string().describe(schema.description);
+//                 break;
+//             case "number":
+//                 parameter[schema.name] = z.number().describe(schema.description);
+//                 break;
+//             case "boolean": 
+//             parameter[schema.name] = z.boolean().describe(schema.description);
+//                 break;
+//             case "array":
+//                 parameter[schema.name] = z.array(z.string()).describe(schema.description);
+//                 break;
+//             // object parameter not supported yet    
+//             // case "object":
+//             //     parameter[schema.name] = z.object({}).describe(input.description); // Placeholder for object type
+//             //     break;
+//             default:
+//                 throw new Error(`Unsupported parameter type: ${schema.type}`);
+//         }
+//     }
+//     return parameter;
+// }
+
+// const param = {
+//     workingDirectory: z.string().describe("absolute path of the README.md where the code will be generated"),
+// } as ZodRawShape;
+
+// server.tool(
+//     "generate-autorest",
+//     "Generate code using autorest",
+//     param,
+//     (param) => generateByAutorest(param),
+// );
+
+const server = CodegenServer.getInstance();
 
 async function main() {
+    server.init();
     const transport = new StdioServerTransport();
     await server.connect(transport);
-    console.log("Codegen MCP Server running on stdio");
+    console.log(`Codegen MCP Server running on stdio at ${new Date()}`);
 }
 
 main().catch((error) => {
--- tools/Mcp/src/models/CodegenServer.ts
+++ tools/Mcp/src/models/CodegenServer.ts
@@ -1,5 +1,9 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+import { toolParameterSchema, toolSchema } from "../types.js";
+import specs from "../specs/Specs.json" assert { type: "json" };
+import { toolServices } from "../services/toolServices.js";
 
 export class CodegenServer {
     private static _instance: CodegenServer;
@@ -16,8 +20,8 @@ export class CodegenServer {
         });
     }
 
-    private init(): void {
-        
+    init(): void {
+        this.initTools();
     }
 
     public static getInstance(): CodegenServer {
@@ -29,5 +33,47 @@ export class CodegenServer {
 
     public async connect(transport: StdioServerTransport): Promise<void> {
         await this._mcp.connect(transport);
+    }
+
+
+    initTools() {
+        const toolSchemas = specs.tools as toolSchema[];
+        for (const schema of toolSchemas) {
+            const parameter = this.createToolParameterfromSchema(schema.parameters);
+            const callBack = toolServices<typeof parameter>(schema.callbackName);
+            this._mcp.tool(
+                schema.name,
+                schema.description,
+                parameter,
+                (parameter) => callBack(parameter)
+            );
+        }
+    }
+
+    createToolParameterfromSchema(schemas: toolParameterSchema[]){
+        const parameter: {[k: string]: z.ZodTypeAny} = {}; 
+        for (const schema of schemas) {
+            switch (schema.type) {
+                case "string":
+                    parameter[schema.name] = z.string().describe(schema.description);
+                    break;
+                case "number":
+                    parameter[schema.name] = z.number().describe(schema.description);
+                    break;
+                case "boolean": 
+                parameter[schema.name] = z.boolean().describe(schema.description);
+                    break;
+                case "array":
+                    parameter[schema.name] = z.array(z.string()).describe(schema.description);
+                    break;
+                // object parameter not supported yet    
+                // case "object":
+                //     parameter[schema.name] = z.object({}).describe(input.description); // Placeholder for object type
+                //     break;
+                default:
+                    throw new Error(`Unsupported parameter type: ${schema.type}`);
+            }
+        }
+        return parameter;
     }
 }
