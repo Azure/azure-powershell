019934cd6f17e7dc813a6bd044191ff93d36abae
Qi Pan <43341456+Pan-Qi@users.noreply.github.com>
2025-05-06 16:17:21 +1000

    test

---
+++ .azure-pipelines/PipelineSteps/BatchGeneration/test-modules.ps1
@@ -0,0 +1,58 @@
+param (
+    [string]$MatrixKey,
+    [string]$TestEnvName,
+    [string]$RepoRoot
+)
+
+$utilFilePath = Join-Path $RepoRoot '.azure-pipelines' 'PipelineSteps' 'BatchGeneration' 'util.psm1'
+Import-Module $utilFilePath -Force
+$subModuleGroup = Get-Targets -RepoRoot $RepoRoot -TargetsOutputFileName "test$($TestEnvName)Targets.json" -MatrixKey $MatrixKey
+
+if ($TestEnvName -eq 'Linux') {
+    $accountsModulePath = Join-Path $RepoRoot 'artifacts' 'Debug' "Az.Accounts" "Az.Accounts.psd1"
+    Import-Module $accountsModulePath -Force
+}
+
+$results = @()  
+
+foreach ($subModule in $subModuleGroup) {
+    $startTime = Get-Date
+    $moduleName, $subModuleName = $subModule -split '/'
+    $result = @{
+        Module = $moduleName
+        SubModule = $subModuleName
+        Status = "Success"
+        DurationSeconds = 0
+        Error = ""
+    }
+
+    try {
+        Write-Host "Testing sub module: $subModule"
+        $subModulePath = Join-Path $RepoRoot 'artifacts' 'Debug' "Az.$ModuleName" $subModuleName
+        Push-Location $subModulePath
+
+        & ".\test-module.ps1"  
+
+        if ($LASTEXITCODE -ne 0) {
+            Write-Warning "❌ Test failed: $subModule (exit code $LASTEXITCODE)"
+            $result.Status = "Failed"
+            $result.Error = "Test failed with exit code $LASTEXITCODE"
+        }
+    }
+    catch {
+        Write-Warning "Failed to test module: $module"
+        Write-Warning "Error message: $($_.Exception.Message)"
+        $result.Status = "Failed"
+        $result.Error = $_.Exception.Message
+    }
+    finally {
+        $endTime = Get-Date
+        $result.DurationSeconds = ($endTime - $startTime).TotalSeconds
+        $results += $result
+        Pop-Location
+    }
+}
+
+$artifactRoot = Join-Path $RepoRoot 'artifacts'
+$reportPath = Join-Path $artifactRoot "Test$($TestEnvName)Report-$MatrixKey.json"
+$results | ConvertTo-Json -Depth 3 | Out-File -FilePath $reportPath -Encoding utf8
