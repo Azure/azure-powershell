4eceb26bc9487ca6cd56275f59359dddda068976
Faizan Uddin Fahad Khan <faikh@microsoft.com>
2025-05-14 18:22:44 +0530

    add update tests

--- src/AzureDataTransfer/AzureDataTransfer.Autorest/test/Update-AzDataTransferFlow.Tests.ps1
+++ src/AzureDataTransfer/AzureDataTransfer.Autorest/test/Update-AzDataTransferFlow.Tests.ps1
@@ -15,8 +15,24 @@ if(($null -eq $TestName) -or ($TestName -contains 'Update-AzDataTransferFlow'))
 }
 
 Describe 'Update-AzDataTransferFlow' {
-    It 'UpdateExpanded' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+    It 'UpdateTagsForExistingFlow' {
+        {
+            # Update tags for an existing flow
+            Update-AzDataTransferFlow -ResourceGroupName $env:ResourceGroupName -ConnectionName $env:ConnectionName -Name $env:FlowName -Tag @{Environment="Production"; Department="IT"} -Confirm:$false | Should -BeNullOrEmpty
+
+            # Verify the tags are updated
+            $updatedFlow = Get-AzDataTransferFlow -ResourceGroupName $env:ResourceGroupName -ConnectionName $env:ConnectionName -Name $env:FlowName
+            $updatedFlow | Should -Not -BeNullOrEmpty
+            $updatedFlow.Tags.Environment | Should -Be "Production"
+            $updatedFlow.Tags.Department | Should -Be "IT"
+        } | Should -Not -Throw
+    }
+
+    It 'UpdateTagsForNonExistentFlow' {
+        {
+            # Attempt to update tags for a non-existent flow
+            Update-AzDataTransferFlow -ResourceGroupName $env:ResourceGroupName -ConnectionName $env:ConnectionName -Name "NonExistentFlow" -Tag @{Environment="Production"; Department="IT"} -Confirm:$false
+        } | Should -Throw
     }
 
     It 'UpdateViaIdentityConnectionExpanded' -skip {
