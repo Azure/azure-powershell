50748d3d351350787e0573642e10d5aaec712da7
Yukun Li (Beyondsoft) <v-yukunli@microsoft.com>
2025-05-20 09:54:17 +0800

    update test

--- src/HDInsight/HDInsight.Test/ScenarioTests/HDInsightClusterTests.ps1
+++ src/HDInsight/HDInsight.Test/ScenarioTests/HDInsightClusterTests.ps1
@@ -24,22 +24,22 @@ function Test-CreateClusterWithWasbAndMSI{
 		$storageAccountKey=Get-AzStorageAccountKey -ResourceGroupName "yukundemo1" -Name "yk01wasb666666666666"
 		$storageAccountKey=$storageAccountKey[0].Value
 		$clusterParams = @{
-			ClusterType						= $params.clusterType
-			ClusterSizeInNodes				= $params.clusterSizeInNodes
-			ResourceGroupName				= $params.resourceGroupName
-			ClusterName						= $params.clusterName
-			HttpCredential					= $params.httpCredential
-			SshCredential					= $params.sshCredential
-			Location						= $params.location
-			MinSupportedTlsVersion			= $params.minSupportedTlsVersion
-			VirtualNetworkId				= $params.virtualNetworkId
-			SubnetName						= "default"
-			Version							= "5.1"
-			StorageAccountType				= "AzureStorage"
-			StorageContainer				= $params.clusterName
-			StorageAccountKey				= $storageAccountKey
-			StorageAccountResourceId		= "/subscriptions/964c10bb-8a6c-43bc-83d3-6b318c6c7305/resourceGroups/yukundemo1/providers/Microsoft.Storage/storageAccounts/yk01wasb666666666666"
-			StorageAccountManagedIdentity	= "/subscriptions/964c10bb-8a6c-43bc-83d3-6b318c6c7305/resourceGroups/yukundemo1/providers/Microsoft.ManagedIdentity/userAssignedIdentities/yk-test-msi"
+			ClusterType                     = $params.clusterType
+			ClusterSizeInNodes              = $params.clusterSizeInNodes
+			ResourceGroupName               = $params.resourceGroupName
+			ClusterName                     = $params.clusterName
+			HttpCredential                  = $params.httpCredential
+			SshCredential                   = $params.sshCredential
+			Location                        = $params.location
+			MinSupportedTlsVersion          = $params.minSupportedTlsVersion
+			VirtualNetworkId                = $params.virtualNetworkId
+			SubnetName                      = "default"
+			Version                         = "5.1"
+			StorageAccountType              = "AzureStorage"
+			StorageContainer                = $params.clusterName
+			StorageAccountKey               = $storageAccountKey
+			StorageAccountResourceId        = "/subscriptions/964c10bb-8a6c-43bc-83d3-6b318c6c7305/resourceGroups/yukundemo1/providers/Microsoft.Storage/storageAccounts/yk01wasb666666666666"
+			StorageAccountManagedIdentity   = "/subscriptions/964c10bb-8a6c-43bc-83d3-6b318c6c7305/resourceGroups/yukundemo1/providers/Microsoft.ManagedIdentity/userAssignedIdentities/yk-test-msi"
         }
 		# test create cluster
 		$cluster = New-AzHDInsightCluster @clusterParams
@@ -67,20 +67,20 @@ function Test-CreateEntraCluster{
 		 $user1 = New-AzHDInsightClusterGatewayEntraUserInfo -ObjectId "9cd85b81-c262-44da-bc33-96c3b8c182d3" -DisplayName "Yukun Li (Beyondsoft)"  -Upn "v-yukunli@microsoft"
 		
 		 $clusterParams = @{
-			ClusterType						= $params.clusterType
-			ClusterSizeInNodes				= $params.clusterSizeInNodes
-			ResourceGroupName				= $params.resourceGroupName
-			ClusterName						= $params.clusterName
-			SshCredential					= $params.sshCredential
-			Location						= $params.location
-			MinSupportedTlsVersion			= $params.minSupportedTlsVersion
-			VirtualNetworkId				= $params.virtualNetworkId
-			SubnetName						= "default"
-			Version							= "5.1"
-			StorageContainer				= $params.clusterName
-			StorageAccountKey				= $params.storageAccountKey
-			StorageAccountResourceId		= $params.storageAccountResourceId
-			RestAuthEntraUsers				= $user1
+			ClusterType                     = $params.clusterType
+			ClusterSizeInNodes              = $params.clusterSizeInNodes
+			ResourceGroupName               = $params.resourceGroupName
+			ClusterName                     = $params.clusterName
+			SshCredential                   = $params.sshCredential
+			Location                        = $params.location
+			MinSupportedTlsVersion          = $params.minSupportedTlsVersion
+			VirtualNetworkId                = $params.virtualNetworkId
+			SubnetName                      = "default"
+			Version                         = "5.1"
+			StorageContainer                = $params.clusterName
+			StorageAccountKey               = $params.storageAccountKey
+			StorageAccountResourceId        = $params.storageAccountResourceId
+			RestAuthEntraUsers              = $user1
         }
 		$resultCluster = New-AzHDInsightCluster @clusterParams
 		Assert-NotNull $resultCluster
@@ -109,15 +109,15 @@ function Test-UpdateAndGetEntraUserInfo{
 		$user2 = New-AzHDInsightClusterGatewayEntraUserInfo -ObjectId "745addd7-cb2c-4023-903f-cf9d71b4385f" -DisplayName "Yukun Li (Beyondsoft)"  -Upn "v-yukunli_microsoft.com#EXT#@securehadooprc.onmicrosoft.com"
 		
 		$resultEntraUsers = Get-AzHDInsightClusterGatewayEntraUserInfo -ResourceGroupName $resourceGroupName -ClusterName $clusterName
-		Assert-AreEqual $user1.ObjectId		$resultEntraUsers[0].ObjectId
-		Assert-AreEqual $user1.DisplayName	$resultEntraUsers[0].DisplayName
-		Assert-AreEqual $user1.Upn			$resultEntraUsers[0].Upn
+		Assert-AreEqual $user1.ObjectId      $resultEntraUsers[0].ObjectId
+		Assert-AreEqual $user1.DisplayName   $resultEntraUsers[0].DisplayName
+		Assert-AreEqual $user1.Upn           $resultEntraUsers[0].Upn
 
 		Set-AzHDInsightGatewaySettings -ResourceGroupName $resourceGroupName -ClusterName $clusterName -RestAuthEntraUsers $user2
 		$resultEntraUsers = Get-AzHDInsightClusterGatewayEntraUserInfo -ResourceGroupName $resourceGroupName -ClusterName $clusterName
-		Assert-AreEqual $user2.ObjectId		$resultEntraUsers[1].ObjectId
-		Assert-AreEqual $user2.DisplayName	$resultEntraUsers[1].DisplayName
-		Assert-AreEqual $user2.Upn			$resultEntraUsers[1].Upn
+		Assert-AreEqual $user2.ObjectId      $resultEntraUsers[1].ObjectId
+		Assert-AreEqual $user2.DisplayName   $resultEntraUsers[1].DisplayName
+		Assert-AreEqual $user2.Upn           $resultEntraUsers[1].Upn
 		
 	}
 
