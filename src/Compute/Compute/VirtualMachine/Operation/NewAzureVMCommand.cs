f35bb14d5738b6898f52450b90c1044203980d91
Theodore Chang <thchan@microsoft.com>
2025-05-26 22:43:00 -0400

    Default VM size update breaking change message  (#27763)
    Co-authored-by: Yeming Liu <11371776+isra-fel@users.noreply.github.com>

--- src/Compute/Compute/VirtualMachine/Operation/NewAzureVMCommand.cs
+++ src/Compute/Compute/VirtualMachine/Operation/NewAzureVMCommand.cs
@@ -59,6 +59,7 @@ using System.Text.RegularExpressions;
 
 namespace Microsoft.Azure.Commands.Compute
 {
+    [GenericBreakingChangeWithVersion("The default VM size will change from 'Standard_D2s_v3' to 'Standard_D2s_v5'.", "15.0.0", "11.0.0", "Nov 2025")]
     [Cmdlet("New", ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "VM", SupportsShouldProcess = true, DefaultParameterSetName = "SimpleParameterSet")]
     [OutputType(typeof(PSAzureOperationResponse), typeof(PSVirtualMachine))]
     public class NewAzureVMCommand : VirtualMachineBaseCmdlet
