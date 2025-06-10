b1905fddff2aed135dee64d10937cae81f3193bd
raghumetta <raghunadh.metta@oracle.com>
2025-06-10 15:31:04 -0500

    INDIGO-18978 - Rename Base File for Exascale powershell testing

--- src/Oracle/Oracle.Autorest/test/AzOracleExadbVMCluster.Tests.ps1
+++ src/Oracle/Oracle.Autorest/test/AzOracleExadbVMCluster.Tests.ps1
@@ -1,11 +1,11 @@
-if(($null -eq $TestName) -or ($TestName -contains 'OracleExadbVMCluster'))
+if(($null -eq $TestName) -or ($TestName -contains 'AzOracleExadbVMCluster'))
 {
   $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
   if (-Not (Test-Path -Path $loadEnvPath)) {
       $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
   }
   . ($loadEnvPath)
-  $TestRecordingFile = Join-Path $PSScriptRoot 'OracleExadbVMCluster.Recording.json'
+  $TestRecordingFile = Join-Path $PSScriptRoot 'AzOracleExadbVMCluster.Recording.json'
   $currentPath = $PSScriptRoot
   while(-not $mockingPath) {
       $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
@@ -14,7 +14,7 @@ if(($null -eq $TestName) -or ($TestName -contains 'OracleExadbVMCluster'))
   . ($mockingPath | Select-Object -First 1).FullName
 }
 
-Describe 'OracleExadbVMCluster' {
+Describe 'AzOracleExadbVMCluster' {
     It 'CreateOracleExadbVMCluster' {
         {
             $sshPublicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDKJkePl4prXTs6cZ77AS9kGs5TO1EdfDdQZAtD7cfBVJ8X4wN+aOvLhk+u74D3qXad2OdQ/ij5q+xVzoXLXNBIZFQjB8JqWpgvOrOCAakFGc0OatJhSVlmJKW7JboQcUu7AzABfu+Ciso1QQTqlc2+awoZzPhfP9sgDMN6zI15Q9wSuxERor8oMSc78NW652wMzl97zO+bYdO9vIjBu27/WYZN/OpFJ0Ss4AzW/V9r2h6FFCkG+GXzhZArk3NeEstCSO2bjv3vO40+M0vfRD2jQrOSKhaLolk+crLGamaclY0YYCVB23rk6gCimWbVuvpHn+x1QSvN2d19xAmrIsHdTv/1lCEJetMA96pBq/jbljPwVKPFfVkyC8Ivt5rkbYizmUlYAbDMksGMUR4ncjScY7o/S0JKs14HihOnCoSGVXhH1dDgc8AsI+Ujs+GGR4U8IXJGEpZmhdnLa6mDymvr1tLWdQaI2y5FuWxsy4diKjEsPxCrnqfxlZxFBbQ29AU= generated-by-azure"
