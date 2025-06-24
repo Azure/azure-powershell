3a55fd35a2f44d879917c4479e5097b9612a684a
yoavmalichi_microsoft <yoavmalichi@microsoft.com>
2025-06-24 17:37:46 +0300

    adding double slashes for copyright alignment

--- src/Network/Network/Models/PSApplicationGatewayFirewallPolicyManagedRuleSet.cs
+++ src/Network/Network/Models/PSApplicationGatewayFirewallPolicyManagedRuleSet.cs
@@ -1,4 +1,5 @@
-﻿// Copyright (c) Microsoft.  All rights reserved.
+﻿//
+// Copyright (c) Microsoft.  All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
