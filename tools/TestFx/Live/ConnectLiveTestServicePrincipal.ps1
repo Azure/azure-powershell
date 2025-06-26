e4e729ab8afb5e15260bc27de29336cb2596c7d2
Vincent Dai <23257217+vidai-msft@users.noreply.github.com>
2025-06-26 15:58:13 +0800

    Fix live test macos issue.

--- tools/TestFx/Live/ConnectLiveTestServicePrincipal.ps1
+++ tools/TestFx/Live/ConnectLiveTestServicePrincipal.ps1
@@ -19,8 +19,14 @@ param (
 $PreErrorActionPreference = $ErrorActionPreference
 $ErrorActionPreference = "Stop"
 
+Get-AzContextAutosaveSetting
+
+Write-Host "##[section]Connecting to Azure with Service Principal ID: $ServicePrincipalId in Subscription: $SubscriptionId and Tenant: $TenantId."
+
 $servicePrincipalSecureSecret = ConvertTo-SecureString -String $ServicePrincipalSecret -AsPlainText -Force
 $servicePrincipalCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ServicePrincipalId, $servicePrincipalSecureSecret
 Connect-AzAccount -SubscriptionId $SubscriptionId -TenantId $TenantId -Credential $servicePrincipalCredential -ServicePrincipal
+
+Write-Host "Connected to Azure with Service Principal ID: $ServicePrincipalId in Subscription: $SubscriptionId and Tenant: $TenantId."
 
 $ErrorActionPreference = $PreErrorActionPreference
