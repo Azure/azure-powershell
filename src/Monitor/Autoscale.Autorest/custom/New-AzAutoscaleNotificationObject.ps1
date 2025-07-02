e50d96bd684c99ed933441ba92b765dd7f0db05b
JoyerJin <116236375+JoyerJin@users.noreply.github.com>
2025-07-02 14:47:33 +0800

    add breaking change message for Autoscale submodule under Monitor module

--- src/Monitor/Autoscale.Autorest/custom/New-AzAutoscaleNotificationObject.ps1
+++ src/Monitor/Autoscale.Autorest/custom/New-AzAutoscaleNotificationObject.ps1
@@ -26,6 +26,7 @@ Microsoft.Azure.PowerShell.Cmdlets.Monitor.Autoscale.Models.Api20221001.Autoscal
 https://learn.microsoft.com/powershell/module/Az.Monitor/new-AzAutoscaleNotificationObject
 #>
 function New-AzAutoscaleNotificationObject {
+    [Microsoft.Azure.PowerShell.Cmdlets.Monitor.Autoscale.Runtime.CmdletBreakingChange("15.0.0", "7.0.0", "2025/11/03", ChangeDescription="The type of property 'Webhook' and 'EmailCustomEmail' will be changed from array to List.")]
     [OutputType('Microsoft.Azure.PowerShell.Cmdlets.Monitor.Autoscale.Models.Api20221001.AutoscaleNotification')]
     [CmdletBinding(PositionalBinding=$false)]
     Param(
