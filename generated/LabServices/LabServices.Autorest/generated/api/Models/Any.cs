60f606fa84f94c4e187d35f6ff9e0648bff2af0a
Azure PowerShell <65331932+azure-powershell-bot@users.noreply.github.com>
2025-07-07 18:32:16 +1000

    [skip ci] Archive a1a12bfd3f966a6d2fa47136a4dde4f9c905e97a (#28144)

--- generated/LabServices/LabServices.Autorest/generated/api/Models/Any.cs
+++ generated/LabServices/LabServices.Autorest/generated/api/Models/Any.cs
@@ -7,7 +7,7 @@ namespace Microsoft.Azure.PowerShell.Cmdlets.LabServices.Models
 {
     using static Microsoft.Azure.PowerShell.Cmdlets.LabServices.Runtime.Extensions;
 
-    /// <summary>Any object</summary>
+    /// <summary>Anything</summary>
     public partial class Any :
         Microsoft.Azure.PowerShell.Cmdlets.LabServices.Models.IAny,
         Microsoft.Azure.PowerShell.Cmdlets.LabServices.Models.IAnyInternal
@@ -19,13 +19,13 @@ namespace Microsoft.Azure.PowerShell.Cmdlets.LabServices.Models
 
         }
     }
-    /// Any object
+    /// Anything
     public partial interface IAny :
         Microsoft.Azure.PowerShell.Cmdlets.LabServices.Runtime.IJsonSerializable
     {
 
     }
-    /// Any object
+    /// Anything
     internal partial interface IAnyInternal
 
     {
