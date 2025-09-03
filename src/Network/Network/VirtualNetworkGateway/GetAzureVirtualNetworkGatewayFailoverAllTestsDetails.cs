410e290b70d041dd0576cd6e4a954dd5baba4b8d
SHUBHIKA JAIN <SHUJAI@microsoft.com>
2025-09-03 22:54:59 +0530

    corrected output for all apis

--- src/Network/Network/VirtualNetworkGateway/GetAzureVirtualNetworkGatewayFailoverAllTestsDetails.cs
+++ src/Network/Network/VirtualNetworkGateway/GetAzureVirtualNetworkGatewayFailoverAllTestsDetails.cs
@@ -7,23 +7,27 @@
 // ----------------------------------------------------------------------------------
 
 using Microsoft.Azure.Commands.Network.Models;
-using Microsoft.Azure.Management.Network;
+using Microsoft.Rest.Azure;
+using Microsoft.Azure.Commands.ResourceManager.Common.Tags;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System;
 using System.Management.Automation;
-using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
 using System.Collections.Generic;
-using CNM = Microsoft.Azure.Commands.Network.Models;
+using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
+
 
 namespace Microsoft.Azure.Commands.Network.VirtualNetworkGateway
 {
     [Cmdlet(VerbsCommon.Get, ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "VirtualNetworkGatewayFailoverAllTestsDetails", DefaultParameterSetName = "ByName"), OutputType(typeof(List<PSExpressRouteFailoverTestDetails>))]
     public class GetAzureVirtualNetworkGatewayFailoverAllTestsDetails : NetworkBaseCmdlet
     {
-        private const string ByName = "ByName";
+       private const string GetByNameParameterSet = "GetByNameParameterSet";
 
         [Parameter(
             Mandatory = true,
             HelpMessage = "The resource group name of the virtual network gateway.",
-            ParameterSetName = ByName)]
+            ParameterSetName = GetByNameParameterSet)]
         [ResourceGroupCompleter]
         [ValidateNotNullOrEmpty]
         public string ResourceGroupName { get; set; }
@@ -31,36 +35,108 @@ namespace Microsoft.Azure.Commands.Network.VirtualNetworkGateway
         [Parameter(
             Mandatory = true,
             HelpMessage = "The name of the virtual network gateway.",
-            ParameterSetName = ByName)]
+            ParameterSetName = GetByNameParameterSet)]
         [ValidateNotNullOrEmpty]
         public string VirtualNetworkGatewayName { get; set; }
 
         [Parameter(
             Mandatory = true,
-            HelpMessage = "The type of failover test. Example: SingleSiteFailover, MultiSiteFailover, All",
-            ParameterSetName = ByName)]
+            HelpMessage = "The type of failover test.",
+            ParameterSetName = GetByNameParameterSet)]
         [ValidateNotNullOrEmpty]
         public string Type { get; set; }
 
         [Parameter(
             Mandatory = true,
-            HelpMessage = "Fetch only the latest test for each peering location.",
-            ParameterSetName = ByName)]
+            HelpMessage = "Fetch only the latest tests for each peering location.",
+            ParameterSetName = GetByNameParameterSet)]
         public bool FetchLatest { get; set; }
 
         public override void Execute()
         {
             base.Execute();
 
-            var response = NetworkClient.NetworkManagementClient.VirtualNetworkGateways.GetFailoverAllTestDetails(
-                ResourceGroupName,
-                VirtualNetworkGatewayName,
-                Type,
-                FetchLatest
-            );
+            // Start the operation to get failover test details
+            var response = NetworkClient.NetworkManagementClient.VirtualNetworkGateways
+                .GetFailoverAllTestDetailsWithHttpMessagesAsync(
+                    resourceGroupName: ResourceGroupName,
+                    virtualNetworkGatewayName: VirtualNetworkGatewayName,
+                    type: Type,
+                    fetchLatest: FetchLatest)
+                .GetAwaiter().GetResult();
 
-            var psResult = NetworkResourceManagerProfile.Mapper.Map<List<PSExpressRouteFailoverTestDetails>>(response);
-            WriteObject(psResult, enumerateCollection: true);
+            // If the response status is 202 (Accepted), poll for the result
+            if (response.Response.StatusCode == System.Net.HttpStatusCode.Accepted)
+            {
+                var locationUrl = response.Response.Headers.Location?.ToString();
+                if (!string.IsNullOrEmpty(locationUrl))
+                {
+                    WriteVerbose("Operation accepted. Polling the Location URL until completion...");
+                    var testDetails = PollAndParse(locationUrl);
+                    var fullJson = JsonConvert.SerializeObject(new PSExpressRouteFailoverTestResponse { Value = testDetails }, Formatting.Indented);
+                    WriteObject(fullJson);
+
+                }
+                else
+                {
+                    throw new InvalidOperationException("Location header is missing in 202 Accepted response.");
+                }
+            }
+            else if (response.Response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                // If the response status is 200 (OK), directly deserialize the response
+                var testDetails = DeserializeJsonResponse(response.Response);
+                var fullJson = JsonConvert.SerializeObject(new PSExpressRouteFailoverTestResponse { Value = testDetails }, Formatting.Indented);
+                WriteObject(fullJson);
+            }
+            else
+            {
+                throw new InvalidOperationException($"Unexpected response status: {response.Response.StatusCode}");
+            }
+        }
+
+        // Wrapper class for the response containing a list of test details
+        public class PSExpressRouteFailoverTestResponse
+        {
+            [JsonProperty("value")]
+            public List<PSExpressRouteFailoverTestDetails> Value { get; set; }
+        }
+
+        // Deserialize the JSON response into the wrapper class
+        private List<PSExpressRouteFailoverTestDetails> DeserializeJsonResponse(HttpResponseMessage responseMessage)
+        {
+            var json = responseMessage.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            var response = JsonConvert.DeserializeObject<PSExpressRouteFailoverTestResponse>(json);
+            return response.Value;
+        }
+
+        // Poll until the operation completes and the status changes to 200 OK
+        private List<PSExpressRouteFailoverTestDetails> PollAndParse(string locationUrl)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                while (true)
+                {
+                    System.Threading.Thread.Sleep(5000); // Wait 5 seconds between polls
+
+                    var pollResponse = httpClient.GetAsync(locationUrl).GetAwaiter().GetResult();
+
+                    if (pollResponse.StatusCode == System.Net.HttpStatusCode.Accepted)
+                    {
+                        continue; // keep polling
+                    }
+                    else if (pollResponse.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        var json = pollResponse.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                        var response = JsonConvert.DeserializeObject<PSExpressRouteFailoverTestResponse>(json);
+                        return response.Value;
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException($"Polling failed. Status code: {pollResponse.StatusCode}");
+                    }
+                }
+            }
         }
     }
 }
--- src/Network/Network/VirtualNetworkGateway/GetAzureVirtualNetworkGatewayRouteSetsInformation.cs
+++ src/Network/Network/VirtualNetworkGateway/GetAzureVirtualNetworkGatewayRouteSetsInformation.cs
@@ -1,36 +1,20 @@
-// ---------------------------------------------------------------------------------- 
-//
-// Copyright Microsoft Corporation
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// ----------------------------------------------------------------------------------
-
 using Microsoft.Azure.Commands.ResourceManager.Common.Tags;
 using Microsoft.Rest.Azure;
 using Microsoft.Azure.Commands.Network.Models;
 using Microsoft.Azure.Management.Internal.Resources.Utilities.Models;
 using Microsoft.Azure.Management.Network;
 using Microsoft.Azure.Management.Network.Models;
-using System;
-using System.Collections.Generic;
 using System.Management.Automation;
 using CNM = Microsoft.Azure.Commands.Network.Models;
 using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
-using System.Linq;
-using Microsoft.WindowsAzure.Commands.Utilities.Common;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System;
 
-namespace Microsoft.Azure.Commands.Network
+namespace Microsoft.Azure.Commands.Network.VirtualNetworkGateway
 {
-    [Cmdlet(VerbsCommon.Get, ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "VirtualNetworkGatewayRouteSetsInformation", DefaultParameterSetName = "GetByNameParameterSet"), OutputType(typeof(PSGatewayRouteSetsInformation))]
-    public partial class GetAzureVirtualNetworkGatewayRouteSetsInformation : NetworkBaseCmdlet
+    [Cmdlet(VerbsCommon.Get, ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "VirtualNetworkGatewayRoutesInformation", DefaultParameterSetName = "GetByNameParameterSet"), OutputType(typeof(PSGatewayRouteSetsInformation))]
+    public class GetAzureVirtualNetworkGatewayRoutesInformationCommand : NetworkBaseCmdlet
     {
         private const string GetByNameParameterSet = "GetByNameParameterSet";
 
@@ -39,6 +23,7 @@ namespace Microsoft.Azure.Commands.Network
             HelpMessage = "The resource group name of the virtual network gateway.",
             ParameterSetName = GetByNameParameterSet)]
         [ResourceGroupCompleter]
+        [ValidateNotNullOrEmpty]
         public string ResourceGroupName { get; set; }
 
         [Parameter(
@@ -46,19 +31,87 @@ namespace Microsoft.Azure.Commands.Network
             HelpMessage = "The name of the virtual network gateway.",
             ParameterSetName = GetByNameParameterSet)]
         [ValidateNotNullOrEmpty]
-        public string GatewayName { get; set; }
+        public string VirtualNetworkGatewayName { get; set; }
+
+        [Parameter(
+            Mandatory = false,
+            HelpMessage = "Attempt to recalculate the Route Sets Information for the gateway.")]
+        public bool AttemptRefresh { get; set; } = false;
 
         public override void Execute()
         {
             base.Execute();
 
-            var response = this.NetworkClient.NetworkManagementClient.VirtualNetworkGateways
-                .GetRoutesInformationWithHttpMessagesAsync(this.ResourceGroupName, this.GatewayName)
-                .GetAwaiter()
-                .GetResult();
+            var response = NetworkClient.NetworkManagementClient.VirtualNetworkGateways
+                .GetRoutesInformationWithHttpMessagesAsync(
+                    resourceGroupName: ResourceGroupName,
+                    virtualNetworkGatewayName: VirtualNetworkGatewayName,
+                    attemptRefresh: AttemptRefresh)
+                .GetAwaiter().GetResult();
 
-            var psRouteSets = NetworkResourceManagerProfile.Mapper.Map<PSGatewayRouteSetsInformation>(response.Body);
-            WriteObject(psRouteSets, true);
+            GatewayRouteSetsInformation routeInfo = null;
+
+            if (response.Response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                routeInfo = DeserializeJsonResponse(response.Response);
+            }
+            else if (response.Response.StatusCode == System.Net.HttpStatusCode.Accepted)
+            {
+                var locationUrl = response.Response.Headers.Location?.ToString();
+                if (!string.IsNullOrEmpty(locationUrl))
+                {
+                    WriteVerbose("Operation accepted. Polling the Location URL until completion...");
+                    routeInfo = PollAndParse(locationUrl);
+                }
+                else
+                {
+                    throw new InvalidOperationException("Location header is missing in 202 Accepted response.");
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException($"Unexpected response status: {response.Response.StatusCode}");
+            }
+
+            if (routeInfo != null)
+            {
+                var formattedJson = JsonConvert.SerializeObject(routeInfo, Formatting.Indented);
+                WriteObject(formattedJson);
+            }
+        }
+
+        private GatewayRouteSetsInformation DeserializeJsonResponse(HttpResponseMessage responseMessage)
+        {
+            var json = responseMessage.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            json = json.Replace(" PM UTC", " PM");
+            return JsonConvert.DeserializeObject<GatewayRouteSetsInformation>(json);
+        }
+
+        private GatewayRouteSetsInformation PollAndParse(string locationUrl)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                while (true)
+                {
+                    System.Threading.Thread.Sleep(5000); // Wait 5 seconds between polls
+
+                    var pollResponse = httpClient.GetAsync(locationUrl).GetAwaiter().GetResult();
+
+                    if (pollResponse.StatusCode == System.Net.HttpStatusCode.Accepted)
+                    {
+                        continue; // keep polling
+                    }
+                    else if (pollResponse.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        var json = pollResponse.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                        return JsonConvert.DeserializeObject<GatewayRouteSetsInformation>(json);
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException($"Polling failed. Status code: {pollResponse.StatusCode}");
+                    }
+                }
+            }
         }
     }
 }
