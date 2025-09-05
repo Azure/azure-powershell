5b4577569e459670a96955cef5a02c8c55fac4fd
Adam Myatt <adammyatt@hotmail.com>
2025-09-05 09:25:00 -0400

    Update DistributedHAAvSetDiffTransRgShare.Tests.ps1
    Fix typo of Enviornment to Environment

--- src/Workloads/SapVirtualInstance.Autorest/test/DistributedHAAvSetDiffTransRgShare.Tests.ps1
+++ src/Workloads/SapVirtualInstance.Autorest/test/DistributedHAAvSetDiffTransRgShare.Tests.ps1
@@ -18,7 +18,7 @@ Describe 'DistributedHAAvSetDiffTransRgShare' {
     It 'CreateDistributedHAAvSetDiffTransRgShare' {
         $MsiIdentityName = @{ $env.IdentityName = @{}}
         $CreateDistributedHAAvSetDiffTransRgShareConfig = Join-Path $PSScriptRoot $env.CreateDistributedHAAvSetDiffTransRgShareConfigPath
-        $CreateDistributedHAAvSetDiffTransRgShareResponse = New-AzWorkloadsSapVirtualInstance -SubscriptionId $env.WaaSSubscriptionId -Name $env.CreateDistributedHAAvSetDiffTransRgShareSID -ResourceGroupName $env.ResourceGroupCreateSVI -Environment $env.EnviornmentNonProd -Location $env.Location -SapProduct $env.SapProduct -Configuration $CreateDistributedHAAvSetDiffTransRgShareConfig -IdentityType $env.IdentityType -UserAssignedIdentity $MsiIdentityName
+        $CreateDistributedHAAvSetDiffTransRgShareResponse = New-AzWorkloadsSapVirtualInstance -SubscriptionId $env.WaaSSubscriptionId -Name $env.CreateDistributedHAAvSetDiffTransRgShareSID -ResourceGroupName $env.ResourceGroupCreateSVI -Environment $env.EnvironmentNonProd -Location $env.Location -SapProduct $env.SapProduct -Configuration $CreateDistributedHAAvSetDiffTransRgShareConfig -IdentityType $env.IdentityType -UserAssignedIdentity $MsiIdentityName
         $CreateDistributedHAAvSetDiffTransRgShareResponse.provisioningState | Should -Be $env.ProvisioningStateSucceeded
         ($CreateDistributedHAAvSetDiffTransRgShareResponse.Configuration | ConvertFrom-Json).infrastructureConfiguration.storageConfiguration.transportFileShareConfiguration.configurationType | Should -Be $env.MountTransShareConfigType
     }
@@ -30,7 +30,7 @@ Describe 'DistributedHAAvSetDiffTransRgShare' {
     It 'CreateDistributedHAAvSetDiffTransRgShareAlias' {
         $MsiIdentityName = @{ $env.IdentityName = @{}}
         $CreateDistributedHAAvSetDiffTransRgShareAliasConfig = Join-Path $PSScriptRoot $env.CreateDistributedHAAvSetDiffTransRgShareConfigPath
-        $CreateDistributedHAAvSetDiffTransRgShareAliasResponse = New-AzVIS -SubscriptionId $env.WaaSSubscriptionId -Name $env.CreateDistributedHAAvSetDiffTransRgShareSID -ResourceGroupName $env.ResourceGroupCreateSVI -Environment $env.EnviornmentNonProd -Location $env.Location -SapProduct $env.SapProduct -Configuration $CreateDistributedHAAvSetDiffTransRgShareAliasConfig -IdentityType $env.IdentityType -UserAssignedIdentity $MsiIdentityName
+        $CreateDistributedHAAvSetDiffTransRgShareAliasResponse = New-AzVIS -SubscriptionId $env.WaaSSubscriptionId -Name $env.CreateDistributedHAAvSetDiffTransRgShareSID -ResourceGroupName $env.ResourceGroupCreateSVI -Environment $env.EnvironmentNonProd -Location $env.Location -SapProduct $env.SapProduct -Configuration $CreateDistributedHAAvSetDiffTransRgShareAliasConfig -IdentityType $env.IdentityType -UserAssignedIdentity $MsiIdentityName
         $CreateDistributedHAAvSetDiffTransRgShareAliasResponse.provisioningState | Should -Be $env.ProvisioningStateSucceeded
         ($CreateDistributedHAAvSetDiffTransRgShareAliasResponse.Configuration | ConvertFrom-Json).infrastructureConfiguration.storageConfiguration.transportFileShareConfiguration.configurationType | Should -Be $env.MountTransShareConfigType
     }
