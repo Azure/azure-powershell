89058c6f99da9e12103ef0407cb60cc323f297e5
Yabo Hu <yabhu@microsoft.com>
2025-09-16 13:59:22 +0800

    [Eng] Fix dotnet test not working (#28563)

--- .azure-pipelines/PipelineSteps/test-steps.ps1
+++ .azure-pipelines/PipelineSteps/test-steps.ps1
@@ -36,10 +36,9 @@ $buildArgs = "/p:Configuration=$Configuration;TestFramework=$TestFramework"
 
 if ($IsLinux) {
     # Applying various parallelism/heap restriction behaviors to prevent OOM issues on Ubuntu 24.04
-    Write-Host -ForegroundColor Yellow "Detected Linux agent – applying memory tuning for tests"
+    Write-Host -ForegroundColor Yellow "Detected Linux agent - applying memory tuning for tests"
     $env:DOTNET_gcServer = "0"
     $env:DOTNET_gcHeapCount = "2"
-    $buildArgs += ";RunConfiguration.MaxCpuCount=2"
 }
 
 dotnet msbuild $buildProjPath /t:Test $buildArgs
