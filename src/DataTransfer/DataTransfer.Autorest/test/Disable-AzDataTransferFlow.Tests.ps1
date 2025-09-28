5b4230c13d9b8c77a5cd2cd0d5dd2af53272cd90
Faizan Uddin Fahad Khan <faikh@microsoft.com>
2025-09-28 21:05:32 +0530

    Record some moar tests

--- src/DataTransfer/DataTransfer.Autorest/test/Disable-AzDataTransferFlow.Tests.ps1
+++ src/DataTransfer/DataTransfer.Autorest/test/Disable-AzDataTransferFlow.Tests.ps1
@@ -14,11 +14,43 @@ if(($null -eq $TestName) -or ($TestName -contains 'Disable-AzDataTransferFlow'))
   . ($mockingPath | Select-Object -First 1).FullName
 }
 
+$recvFlowToDisable = "test-flow-to-disable-1"
+$sendFlowToDisable = "test-flow-to-disable-2"
+
 Describe 'Disable-AzDataTransferFlow' {
+    $rcvFlowParams = @{
+        ResourceGroupName     = $env.ResourceGroupName
+        ConnectionName        = $env.ConnectionLinked
+        Name                  = $recvFlowToDisable
+        Location              = $env.Location
+        FlowType              = "Mission"
+        DataType              = "Blob"
+        StorageAccountName    = $env.StorageAccountName
+        StorageContainerName  = $env.StorageContainerName
+    }
+
+    $createdRcvFlow =  New-AzDataTransferFlow @rcvFlowParams
+
+    $sendFlowParams = @{
+        ResourceGroupName     = $env.ResourceGroupName
+        ConnectionName        = $env.ConnectionLinkedSend
+        Name                  = $sendFlowToDisable
+        Location              = $env.Location
+        FlowType              = "Mission"
+        DataType              = "Blob"
+        StorageAccountName    = $env.StorageAccountName
+        StorageContainerName  = $env.StorageContainerName
+    }
+    $createdSendFlow =  New-AzDataTransferFlow @sendFlowParams
+
+    Invoke-AzDataTransferLinkPendingFlow -ResourceGroupName $env.ResourceGroupName -ConnectionName $env.ConnectionLinked -PendingFlowId $createdSendFlow.Id -FlowName $createdRcvFlow.Name -Confirm:$false
+
+    Enable-AzDataTransferFlow -ResourceGroupName $env.ResourceGroupName -ConnectionName $env.ConnectionLinkedSend -Name $sendFlowToDisable -Confirm:$false
+
     It 'Disable' {
         {
             # Disable the flow
-            $disabledFlow = Disable-AzDataTransferFlow -ResourceGroupName $env.ResourceGroupName -ConnectionName $env.ConnectionLinkedSend -Name $env.SendFlow -Confirm:$false
+            $disabledFlow = Disable-AzDataTransferFlow -ResourceGroupName $env.ResourceGroupName -ConnectionName $env.ConnectionLinked -Name $recvFlowToDisable -Confirm:$false
 
             # Verify the flow is disabled
             $disabledFlow.Status | Should -Be "Disabled"
@@ -28,7 +60,7 @@ Describe 'Disable-AzDataTransferFlow' {
     It 'Disable when already disabled' {
         {
             # Attempt to disable the flow again
-            $disabledFlow = Disable-AzDataTransferFlow -ResourceGroupName $env.ResourceGroupName -ConnectionName $env.ConnectionLinkedSend -Name $env.DisabledFlow -Confirm:$false
+            $disabledFlow = Disable-AzDataTransferFlow -ResourceGroupName $env.ResourceGroupName -ConnectionName $env.ConnectionLinked -Name $recvFlowToDisable -Confirm:$false
 
             # Verify the flow is still disabled
             $disabledFlow.Status | Should -Be "Disabled"
@@ -38,7 +70,7 @@ Describe 'Disable-AzDataTransferFlow' {
     It 'Disable AsJob' {
         {
             # Disable the flow as a background job
-            $job = Disable-AzDataTransferFlow -ResourceGroupName $env.ResourceGroupName -ConnectionName $env.ConnectionLinkedSend -Name $env.SendFlow -AsJob -Confirm:$false
+            $job = Disable-AzDataTransferFlow -ResourceGroupName $env.ResourceGroupName -ConnectionName $env.ConnectionLinkedSend -Name $sendFlowToDisable -AsJob -Confirm:$false
     
             # Verify the job is created
             $job | Should -Not -BeNullOrEmpty
@@ -49,7 +81,8 @@ Describe 'Disable-AzDataTransferFlow' {
             ($job.State -eq "Completed") | Should -Be $true
     
             # Verify the flow is disabled after the job completes
-            $disabledFlow = Get-AzDataTransferFlow -ResourceGroupName $env.ResourceGroupName -ConnectionName $env.ConnectionLinkedSend -Name $env.SendFlow
+            $disabledFlow = Get-AzDataTransferFlow -ResourceGroupName $env.ResourceGroupName -ConnectionName $env.ConnectionLinkedSend -Name $sendFlowToDisable
+            $disabledFlow | Should -Not -BeNullOrEmpty
             $disabledFlow.Status | Should -Be "Disabled"
         } | Should -Not -Throw
     }
@@ -60,5 +93,9 @@ Describe 'Disable-AzDataTransferFlow' {
 
     It 'DisableViaIdentity' -skip {
         { throw [System.NotImplementedException] } | Should -Not -Throw
+    }
+
+    AfterAll {
+        # Remove-AzDataTransferFlow -ConnectionName $env.ConnectionLinked -ResourceGroupName $env.ResourceGroupName -Name $flowToDisable -Confirm:$false
     }
 }
--- src/DataTransfer/DataTransfer.Autorest/test/Enable-AzDataTransferFlow.Tests.ps1
+++ src/DataTransfer/DataTransfer.Autorest/test/Enable-AzDataTransferFlow.Tests.ps1
@@ -14,11 +14,44 @@ if(($null -eq $TestName) -or ($TestName -contains 'Enable-AzDataTransferFlow'))
   . ($mockingPath | Select-Object -First 1).FullName
 }
 
+$recvFlowToEnable = "test-flow-to-enable-1"
+$sendFlowToEnable = "test-flow-to-enable-2"
+
 Describe 'Enable-AzDataTransferFlow' {
+    $rcvFlowParams = @{
+        ResourceGroupName     = $env.ResourceGroupName
+        ConnectionName        = $env.ConnectionLinked
+        Name                  = $recvFlowToEnable
+        Location              = $env.Location
+        FlowType              = "Mission"
+        DataType              = "Blob"
+        StorageAccountName    = $env.StorageAccountName
+        StorageContainerName  = $env.StorageContainerName
+    }
+
+    $createdRcvFlow =  New-AzDataTransferFlow @rcvFlowParams
+
+    $sendFlowParams = @{
+        ResourceGroupName     = $env.ResourceGroupName
+        ConnectionName        = $env.ConnectionLinkedSend
+        Name                  = $sendFlowToEnable
+        Location              = $env.Location
+        FlowType              = "Mission"
+        DataType              = "Blob"
+        StorageAccountName    = $env.StorageAccountName
+        StorageContainerName  = $env.StorageContainerName
+    }
+    $createdSendFlow =  New-AzDataTransferFlow @sendFlowParams
+
+    Invoke-AzDataTransferLinkPendingFlow -ResourceGroupName $env.ResourceGroupName -ConnectionName $env.ConnectionLinked -PendingFlowId $createdSendFlow.Id -FlowName $createdRcvFlow.Name -Confirm:$false
+
+    Disable-AzDataTransferFlow -ResourceGroupName $env.ResourceGroupName -ConnectionName $env.ConnectionLinked -Name $recvFlowToEnable -Confirm:$false
+    Disable-AzDataTransferFlow -ResourceGroupName $env.ResourceGroupName -ConnectionName $env.ConnectionLinkedSend -Name $sendFlowToEnable -Confirm:$false
+
     It 'Enable' {
         {
             # Enable the flow
-            $enabledFlow = Enable-AzDataTransferFlow -ResourceGroupName $env.ResourceGroupName -ConnectionName $env.ConnectionLinked -Name $env.RecvFlow -Confirm:$false
+            $enabledFlow = Enable-AzDataTransferFlow -ResourceGroupName $env.ResourceGroupName -ConnectionName $env.ConnectionLinked -Name $recvFlowToEnable -Confirm:$false
 
             # Verify the flow is enabled
             $enabledFlow.Status | Should -Be "Enabled"
@@ -28,7 +61,7 @@ Describe 'Enable-AzDataTransferFlow' {
     It 'Enable when already enabled' {
         {
             # Attempt to enable the flow again
-            $enabledFlow = Enable-AzDataTransferFlow -ResourceGroupName $env.ResourceGroupName -ConnectionName $env.ConnectionLinked -Name $env.EnabledFlow -Confirm:$false
+            $enabledFlow = Enable-AzDataTransferFlow -ResourceGroupName $env.ResourceGroupName -ConnectionName $env.ConnectionLinked -Name $recvFlowToEnable -Confirm:$false
 
             # Verify the flow is still enabled
             $enabledFlow.Status | Should -Be "Enabled"
@@ -38,7 +71,7 @@ Describe 'Enable-AzDataTransferFlow' {
     It 'Enable AsJob' {
         {
             # Enable the flow as a background job
-            $job = Enable-AzDataTransferFlow -ResourceGroupName $env.ResourceGroupName -ConnectionName $env.ConnectionLinked -Name $env.RecvFlow -AsJob -Confirm:$false
+            $job = Enable-AzDataTransferFlow -ResourceGroupName $env.ResourceGroupName -ConnectionName $env.ConnectionLinkedSend -Name $sendFlowToEnable -AsJob -Confirm:$false
     
             # Verify the job is created
             $job | Should -Not -BeNullOrEmpty
@@ -49,7 +82,7 @@ Describe 'Enable-AzDataTransferFlow' {
             ($job.State -eq "Completed") | Should -Be $true
     
             # Verify the flow is enabled after the job completes
-            $enabledFlow = Get-AzDataTransferFlow -ResourceGroupName $env.ResourceGroupName -ConnectionName $env.ConnectionLinked -Name $env.RecvFlow
+            $enabledFlow = Get-AzDataTransferFlow -ResourceGroupName $env.ResourceGroupName -ConnectionName $env.ConnectionLinkedSend -Name $sendFlowToEnable
             $enabledFlow.Status | Should -Be "Enabled"
         } | Should -Not -Throw
     }
