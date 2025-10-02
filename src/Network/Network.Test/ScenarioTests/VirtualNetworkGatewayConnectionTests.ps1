99176e7e3c27a8bc7d16ee66206d5b8e0012eb16
Brandon Villa <brandonvilla@microsoft.com>
2025-10-02 13:52:16 -0700

    revert unrelated change

--- src/Network/Network.Test/ScenarioTests/VirtualNetworkGatewayConnectionTests.ps1
+++ src/Network/Network.Test/ScenarioTests/VirtualNetworkGatewayConnectionTests.ps1
@@ -113,7 +113,7 @@ function Test-VirtualNetworkGatewayConnectionWithBgpCRUD
       $subnet = Get-AzVirtualNetworkSubnetConfig -Name "GatewaySubnet" -VirtualNetwork $vnet
 
       # Create the publicip
-      $publicip = New-AzPublicIpAddress -ResourceGroupName $rgname -name $publicIpName -location $location -AllocationMethod Static -DomainNameLabel $domainNameLabel    
+      $publicip = New-AzPublicIpAddress -ResourceGroupName $rgname -name $publicIpName -location $location -AllocationMethod Dynamic -DomainNameLabel $domainNameLabel    
 
       # Create VirtualNetworkGateway
       $vnetIpConfig = New-AzVirtualNetworkGatewayIpConfig -Name $vnetGatewayConfigName -PublicIpAddress $publicip -Subnet $subnet
