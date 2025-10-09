da6abf41970ecd66587250cbe7e0a32aceed0533
mihret kidane <mihret.kidane@oracle.com>
2025-10-09 17:18:14 -0500

    NA

--- src/Oracle/Oracle.Autorest/test/New-AzOracleNetworkAnchor.Tests.ps1
+++ src/Oracle/Oracle.Autorest/test/New-AzOracleNetworkAnchor.Tests.ps1
@@ -22,36 +22,26 @@ Describe 'New-AzOracleNetworkAnchor' {
     $rgName   = 'PowerShellTestRg'
     $location = 'eastus'
     $name     = 'OFake_PowerShellTestNetworkAnchor'
-    $raName   = '/subscriptions/fd42b73d-5f28-4a23-ae7c-ca08c625fe07/resourceGroups/IAD-AZ/providers/Oracle.Database/resourceAnchors/RAIADAZ01'
+    $raName   = '/subscriptions/3b3aa069-da96-41b6-b5aa-6f20dd9db826/resourceGroups/PowerShellTestRgMihr/providers/Oracle.Database/resourceAnchors/Create'
 
-    $hasCmd = Get-Command -Name New-AzOracleNetworkAnchor -ErrorAction SilentlyContinue
-
-    It 'Warmup' {
-        # Ensure at least one real HTTP call flows so the recorder writes the file
-        Get-AzOracleGiVersion -Location 'eastus' | Out-Null
-    }
+    # $hasCmd = Get-Command -Name New-AzOracleNetworkAnchor -ErrorAction SilentlyContinue
+    # $isRecord   = ($TestMode -eq 'record' -or $env:AZURE_TEST_MODE -eq 'Record')
+    # $isPlayback = ($TestMode -eq 'playback' -or $env:AZURE_TEST_MODE -eq 'Playback')
 
     It 'Create' {
         {
-            if ($hasCmd -and $env:AZURE_TEST_MODE -ne 'Record') {
-                # Create via flattened parameters (no JsonString)
-                $created = New-AzOracleNetworkAnchor `
+                
+           $created = New-AzOracleNetworkAnchor `
                     -Name $name `
                     -ResourceGroupName $rgName `
                     -Location $location `
                     -SubscriptionId $env.SubscriptionId `
-                    -ResourceAnchorId $raName  `
+                    -ResourceAnchorId $raName `
                     -Zone "1" `
-                    -SubnetId "/subscriptions/fd42b73d-5f28-4a23-ae7c-ca08c625fe07/resourceGroups/0322yumfeiTest/providers/Microsoft.Network/virtualNetworks/0607yumfeiTest/subnets/delegated" `
-                
-
-                # Basic assertions only (RU: держим тест максимально лёгким)
+                    -SubnetId "/subscriptions/3b3aa069-da96-41b6-b5aa-6f20dd9db826/resourceGroups/IAD-AZ/providers/Microsoft.Network/virtualNetworks/VNIADAZ01/subnets/delegated"
                 $created | Should -Not -BeNullOrEmpty
-                $created.Name | Should -Be $name
-            } else {
-                # In Record/Playback or when cmdlet is unavailable, keep passing while Warmup generates the recording
-                $true | Should -Be $true
-            }
+            $created.Name | Should -Be $name
+        
         } | Should -Not -Throw
     }
 }
