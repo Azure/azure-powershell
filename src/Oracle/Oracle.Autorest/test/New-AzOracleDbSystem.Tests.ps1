d4b29696a5bd6beac4bef3ffc7b0f66b7b879d4f
Mikita Artsemyeu <mikita.artsemyeu@oracle.com>
2025-10-10 13:24:46 -0500

    new dbsystem creation and update

--- src/Oracle/Oracle.Autorest/test/New-AzOracleDbSystem.Tests.ps1
+++ src/Oracle/Oracle.Autorest/test/New-AzOracleDbSystem.Tests.ps1
@@ -23,33 +23,26 @@ if(($null -eq $TestName) -or ($TestName -contains 'New-AzOracleDbSystem'))
 }
 
 Describe 'New-AzOracleDbSystem' {
-    $expected = [pscustomobject]@{
-        name = "PowershellSdk"
-        location = "eastus"
-        properties = @{
-            networkAnchorId = "/subscriptions/049e5678-fbb1-4861-93f3-7528bd0779fd/resourceGroups/basedb-rg929-ti-iad52/providers/Oracle.Database/networkAnchors/basedb-na9293-ti-iad52"
-            resourceAnchorId = "/subscriptions/049e5678-fbb1-4861-93f3-7528bd0779fd/resourcegroups/basedb-rg929-ti-iad52/providers/oracle.database/resourceanchors/basedb-ra929-ti-iad52"
-        }
-    }
+    # Inputs (keep consistent with the recording)
+    $dbsName  = 'PowershellSdk'
+    $location = 'eastus'
+    $subId    = if ($env:SubscriptionId) { $env:SubscriptionId } else { '049e5678-fbb1-4861-93f3-7528bd0779fd' }
+    $rgName   = if ($env:resourceGroup)  { $env:resourceGroup }  else { 'basedb-rg929-ti-iad52' }
+    $zone     = '3'
 
-    $subId     = if ($env:SubscriptionId) { $env:SubscriptionId } else { "049e5678-fbb1-4861-93f3-7528bd0779fd" }
-    $rgName    = if ($env:resourceGroup)  { $env:resourceGroup }  else { "basedb-rg929-ti-iad52" }
-    $location  = $expected.location
-    $zone      = "3"
-    $netAnchor = $expected.properties.networkAnchorId
-    $resAnchor = $expected.properties.resourceAnchorId
+    $netAnchor = '/subscriptions/049e5678-fbb1-4861-93f3-7528bd0779fd/resourceGroups/basedb-rg929-ti-iad52/providers/Oracle.Database/networkAnchors/basedb-na9293-ti-iad52'
+    $resAnchor = '/subscriptions/049e5678-fbb1-4861-93f3-7528bd0779fd/resourcegroups/basedb-rg929-ti-iad52/providers/oracle.database/resourceanchors/basedb-ra929-ti-iad52'
 
     $sshKey = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC924t51mimqfTUclp0f9QFTUH8yx6J5p+dIEvl0mNDxBXZ+CgcUHjJ74OUMGj4017KDlqGs1/jEDeshZlkuk3NRACyjkmUWqMG4xYAvNlszRYh6X0nq+acjK1Xu3Ez/nYe/mQo4O3BpR1DnCb67xZYCmHDc7u58lqxNF1+fgeuoqaIgJWGE3ykq/RyFd+RtfOvLnOdYTIKkt/LPgsYP30tbFjRtu7sGQcLzuE/3rr33+OfNAeALtk9SwlBz43RVzJJZYly5lIZFH91nTJx90u3xd5BAOC+d6fXZh4vN9GdT7PQUiBdlULFjVkYJkJrqj0gq9LeRsJYx0LVQTZuc0YiqYmCWx6pXhc4ye5psnCsJml4rwCEk68567SuvxELAahOZW0M1aS9Lc12bYTm/71u35CHevcUwyQk+Ejizmq2xRK5g9Ez1fb6imifgJ3Ll/B7U7dbsux3D4zLrkoDNisr2XekP3qr8nwe0r/Ppgyi8jNr5lMPkSLHpSCMEBDKuxE= xuezh@xuezh-mac'
     [SecureString]$adminPwd = ConvertTo-SecureString -String 'testAdminPassword123-_#' -AsPlainText -Force
 
     $hasCmd = Get-Command -Name New-AzOracleDbSystem -ErrorAction SilentlyContinue
-    $isPlayback = ($TestMode -eq 'playback' -or $env:AZURE_TEST_MODE -eq 'Playback')
 
-    It 'Create minimal' {
+    It 'Create (CreateBasedb)' {
         {
-            if ($hasCmd -and -not $isPlayback) {
+            if ($hasCmd) {
                 $created = New-AzOracleDbSystem `
-                    -Name $expected.name `
+                    -Name $dbsName `
                     -ResourceGroupName $rgName `
                     -SubscriptionId $subId `
                     -Location $location `
--- src/Oracle/Oracle.Autorest/test/Update-AzOracleDbSystem.Tests.ps1
+++ src/Oracle/Oracle.Autorest/test/Update-AzOracleDbSystem.Tests.ps1
@@ -1,63 +1,34 @@
+# Minimal playback test for Update-AzOracleDbSystem (tags-only)
+
 if(($null -eq $TestName) -or ($TestName -contains 'Update-AzOracleDbSystem'))
 {
-  $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
-  if (-Not (Test-Path -Path $loadEnvPath)) {
-      $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
-  }
-  . ($loadEnvPath)
-  $TestRecordingFile = Join-Path $PSScriptRoot 'Update-AzOracleDbSystem.Recording.json'
-  # Ensure Az.Oracle module (which defines PipelineMock) is loaded before mocking
-  $modulePsd1 = Join-Path $PSScriptRoot '..\Az.Oracle.psd1'
-  if (Test-Path -Path $modulePsd1) { Import-Module $modulePsd1 -Force }
-  $currentPath = $PSScriptRoot
-  while(-not $mockingPath) {
-      $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
-      $currentPath = Split-Path -Path $currentPath -Parent
-  }
-  . ($mockingPath | Select-Object -First 1).FullName
+    $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+    if (-Not (Test-Path -Path $loadEnvPath)) { $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1' }
+    . ($loadEnvPath)
+
+    $TestRecordingFile = Join-Path $PSScriptRoot 'Update-AzOracleDbSystem.Recording.json'
+    $currentPath = $PSScriptRoot
+    while(-not $mockingPath) {
+        $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+        $currentPath = Split-Path -Path $currentPath -Parent
+    }
+    . ($mockingPath | Select-Object -First 1).FullName
 }
 
 Describe 'Update-AzOracleDbSystem' {
-    $hasCmd = Get-Command -Name Update-AzOracleDbSystem -ErrorAction SilentlyContinue
-    $isRecord   = ($TestMode -eq 'record' -or $env:AZURE_TEST_MODE -eq 'Record')
-    $isPlayback = ($TestMode -eq 'playback' -or $env:AZURE_TEST_MODE -eq 'Playback')
-    $rgName = $env.resourceGroup
-    if (-not $rgName -or [string]::IsNullOrWhiteSpace($rgName)) { $rgName = 'PowerShellTestRg' }
+    # Use a var name that won't collide with Pester's $Name
+    $dbsName = 'PowershellSdk'
+    $rgName  = if ($env:resourceGroup)  { $env:resourceGroup }  else { 'basedb-rg929-ti-iad52' }
+    $subId   = if ($env:SubscriptionId) { $env:SubscriptionId } else { '049e5678-fbb1-4861-93f3-7528bd0779fd' }
 
-    It 'Update' {
+    It 'Update tags' {
         {
-            if ($hasCmd -and -not ($isRecord -or $isPlayback)) {
-                # Discover an existing DbSystem to update
-                $item = $null
-                if ($rgName) {
-                    $list = Get-AzOracleDbSystem -ResourceGroupName $rgName
-                    if ($list -and $list.value) { $list = $list.value }
-                    if ($list) { $item = $list | Select-Object -First 1 }
-                }
-                if (-not $item) {
-                    $list = Get-AzOracleDbSystem
-                    if ($list -and $list.value) { $list = $list.value }
-                    if ($list) { $item = $list | Select-Object -First 1 }
-                }
-                $item | Should -Not -BeNullOrEmpty
+            $tags = @{ updatedBy = 'Pester'; purpose = 'sdk-test' }
+            Update-AzOracleDbSystem -Name $dbsName -ResourceGroupName $rgName -SubscriptionId $subId -Tag $tags | Out-Null
 
-                $id        = $item.Id
-                $subId     = (($id -split '/subscriptions/')[1] -split '/')[0]
-                $rgFromId  = (($id -split '/resourceGroups/')[1] -split '/')[0]
-                $nameFromId= (($id -split '/dbSystems/')[1] -split '/')[0]
-
-                # Use typed parameters (avoid JsonString/JsonFilePath)
-                $updated = Update-AzOracleDbSystem `
-                    -Name $nameFromId `
-                    -ResourceGroupName $rgFromId `
-                    -SubscriptionId $subId
-
-                $updated | Should -Not -BeNullOrEmpty
-                $updated.Name | Should -Be $nameFromId
-            } else {
-                # In Record/Playback or when cmdlet is unavailable, keep passing
-                $true | Should -Be $true
-            }
+            $db = Get-AzOracleDbSystem -Name $dbsName -ResourceGroupName $rgName -SubscriptionId $subId
+            $db.Tag.Get_Item('updatedBy') | Should -Be 'Pester'
+            $db.Tag.Get_Item('purpose')   | Should -Be 'sdk-test'
         } | Should -Not -Throw
     }
 }
