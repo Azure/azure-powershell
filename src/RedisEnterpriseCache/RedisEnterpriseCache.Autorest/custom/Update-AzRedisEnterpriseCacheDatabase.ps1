52d9ef4ae46f0a064e532dd30d011d1be50ea061
JoyerJin <116236375+JoyerJin@users.noreply.github.com>
2025-10-16 13:26:50 +0800

    update release date

--- src/RedisEnterpriseCache/RedisEnterpriseCache.Autorest/custom/Update-AzRedisEnterpriseCacheDatabase.ps1
+++ src/RedisEnterpriseCache/RedisEnterpriseCache.Autorest/custom/Update-AzRedisEnterpriseCacheDatabase.ps1
@@ -60,7 +60,7 @@ https://learn.microsoft.com/powershell/module/az.redisenterprisecache/update-azr
 #>
 function Update-AzRedisEnterpriseCacheDatabase {
     [Microsoft.Azure.PowerShell.Cmdlets.RedisEnterpriseCache.Runtime.PreviewMessage("**********************************************************************************************`n
-    * This cmdlet will undergo a breaking change in Az v15.0.0, to be released on November 19th 2025. *`n
+    * This cmdlet will undergo a breaking change in Az v15.0.0, to be released in May 2026. *`n
     * At least one change applies to this cmdlet.                                                     *`n
     * See all possible breaking changes at https://go.microsoft.com/fwlink/?linkid=2333486            *`n
     ***************************************************************************************************")]
