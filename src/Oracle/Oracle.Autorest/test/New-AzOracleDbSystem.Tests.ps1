fa48facf764ad2673a38502af3401354c0e53e4b
mihret kidane <mihret.kidane@oracle.com>
2025-10-24 10:50:54 -0500

    remove sec finding

--- src/Oracle/Oracle.Autorest/test/New-AzOracleDbSystem.Tests.ps1
+++ src/Oracle/Oracle.Autorest/test/New-AzOracleDbSystem.Tests.ps1
@@ -32,7 +32,7 @@ Describe 'New-AzOracleDbSystem' {
     # Inputs (keep consistent with the recording)
 
     $sshKey = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC924t51mimqfTUclp0f9QFTUH8yx6J5p+dIEvl0mNDxBXZ+CgcUHjJ74OUMGj4017KDlqGs1/jEDeshZlkuk3NRACyjkmUWqMG4xYAvNlszRYh6X0nq+acjK1Xu3Ez/nYe/mQo4O3BpR1DnCb67xZYCmHDc7u58lqxNF1+fgeuoqaIgJWGE3ykq/RyFd+RtfOvLnOdYTIKkt/LPgsYP30tbFjRtu7sGQcLzuE/3rr33+OfNAeALtk9SwlBz43RVzJJZYly5lIZFH91nTJx90u3xd5BAOC+d6fXZh4vN9GdT7PQUiBdlULFjVkYJkJrqj0gq9LeRsJYx0LVQTZuc0YiqYmCWx6pXhc4ye5psnCsJml4rwCEk68567SuvxELAahOZW0M1aS9Lc12bYTm/71u35CHevcUwyQk+Ejizmq2xRK5g9Ez1fb6imifgJ3Ll/B7U7dbsux3D4zLrkoDNisr2XekP3qr8nwe0r/Ppgyi8jNr5lMPkSLHpSCMEBDKuxE= xuezh@xuezh-mac'
-    [SecureString]$adminPwd = ConvertTo-SecureString -String 'testAdminPassword123-_#' -AsPlainText -Force
+    [SecureString]$adminPwd = ConvertTo-SecureString -String 'password' -AsPlainText -Force
 
     $hasCmd = Get-Command -Name New-AzOracleDbSystem -ErrorAction SilentlyContinue
 
