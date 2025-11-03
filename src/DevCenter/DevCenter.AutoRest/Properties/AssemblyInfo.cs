d8ad50d9ffd26fbe70a057232f3c6ced22b3b21a
Amanda Lim <amlim@microsoft.com>
2025-11-03 15:58:40 -0800

    Fix PR comment

--- src/DevCenter/DevCenter.AutoRest/Properties/AssemblyInfo.cs
+++ src/DevCenter/DevCenter.AutoRest/Properties/AssemblyInfo.cs
@@ -24,3 +24,6 @@ using System.Runtime.InteropServices;
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.2")]
 [assembly: System.Runtime.InteropServices.ComVisibleAttribute(false)]
 [assembly: System.CLSCompliantAttribute(false)]
+
+
+
