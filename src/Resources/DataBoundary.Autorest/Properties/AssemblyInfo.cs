d12255c3f7f487992886ece4df8f1987ceea1f81
Qi Pan <43341456+Pan-Qi@users.noreply.github.com>
2026-01-22 23:29:31 +1100

    Fix Resources module console close when no login issue (#29086)

--- src/Resources/DataBoundary.Autorest/Properties/AssemblyInfo.cs
+++ src/Resources/DataBoundary.Autorest/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Microsoft")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © Microsoft")]
 [assembly: System.Reflection.AssemblyProductAttribute("Microsoft Azure PowerShell")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Microsoft Azure PowerShell - Authorization")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Microsoft Azure PowerShell - DataBoundary")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("9.0.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("9.0.0")]
 [assembly: System.Runtime.InteropServices.ComVisibleAttribute(false)]
