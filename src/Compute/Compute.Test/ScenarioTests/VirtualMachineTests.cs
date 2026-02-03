68535326fdf412b0c9817673a32282480c422cc3
Haider Agha <Haider.Agha@microsoft.com>
2026-02-03 11:46:50 -0500

    removing duplicate test

--- src/Compute/Compute.Test/ScenarioTests/VirtualMachineTests.cs
+++ src/Compute/Compute.Test/ScenarioTests/VirtualMachineTests.cs
@@ -703,12 +703,5 @@ namespace Microsoft.Azure.Commands.Compute.Test.ScenarioTests
         {
             TestRunner.RunTestScript("Test-VirtualMachineGalleryApplicationFlags");
         }
-
-        [Fact]
-        [Trait(Category.AcceptanceType, Category.CheckIn)]
-        public void TestVirtualMachineScheduledEventsPolicy()
-        {
-            TestRunner.RunTestScript("Test-VirtualMachineScheduledEventsPolicy 'eastus2euap'");
-        }
     }
 }
