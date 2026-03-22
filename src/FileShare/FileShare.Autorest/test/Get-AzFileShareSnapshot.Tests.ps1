646c3e2b55992de1dbc6b240e204ef5b8284b05e
Ankush Bindlish <34896519+ankushbindlish2@users.noreply.github.com>
2026-03-22 16:43:21 -0700

    Az.FileShares - Add Tests for fileshares (#29256)

--- src/FileShare/FileShare.Autorest/test/Get-AzFileShareSnapshot.Tests.ps1
+++ src/FileShare/FileShare.Autorest/test/Get-AzFileShareSnapshot.Tests.ps1
@@ -15,19 +15,89 @@ if(($null -eq $TestName) -or ($TestName -contains 'Get-AzFileShareSnapshot'))
 }
 
 Describe 'Get-AzFileShareSnapshot' {
-    It 'List' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+    It 'List' {
+        {
+            $config = Get-AzFileShareSnapshot -ResourceGroupName $env.resourceGroup -ResourceName $env.fileShareName01
+            $config.Count | Should -BeGreaterOrEqual 0
+        } | Should -Not -Throw
     }
 
-    It 'GetViaIdentityFileShare' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+    It 'Get' {
+        {
+            # First create a snapshot to retrieve
+            $snapshotName = "snapshot-get-test"
+            $snapshot = New-AzFileShareSnapshot -ResourceGroupName $env.resourceGroup `
+                                               -ResourceName $env.fileShareName01 `
+                                               -Name $snapshotName `
+                                               -Metadata @{"purpose" = "testing"; "environment" = "test"}
+            $snapshot | Should -Not -BeNullOrEmpty
+            
+            # Get the specific snapshot by name
+            $retrieved = Get-AzFileShareSnapshot -ResourceGroupName $env.resourceGroup `
+                                                 -ResourceName $env.fileShareName01 `
+                                                 -Name $snapshotName
+            $retrieved | Should -Not -BeNullOrEmpty
+            $retrieved.Name | Should -Be $snapshotName
+            
+            # Clean up
+            Remove-AzFileShareSnapshot -ResourceGroupName $env.resourceGroup `
+                                      -ResourceName $env.fileShareName01 `
+                                      -Name $snapshotName
+        } | Should -Not -Throw
     }
 
-    It 'Get' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+    It 'GetViaIdentity' {
+        {
+            # First create a snapshot
+            $snapshotName = "snapshot-identity-test"
+            $snapshot = New-AzFileShareSnapshot -ResourceGroupName $env.resourceGroup `
+                                               -ResourceName $env.fileShareName01 `
+                                               -Name $snapshotName `
+                                               -Metadata @{"purpose" = "testing"; "environment" = "test"}
+            $snapshot | Should -Not -BeNullOrEmpty
+            
+            # Get via identity
+            $inputObj = @{
+                SubscriptionId = $env.SubscriptionId
+                ResourceGroupName = $env.resourceGroup
+                ResourceName = $env.fileShareName01
+                Name = $snapshotName
+            }
+            $identity = [Microsoft.Azure.PowerShell.Cmdlets.FileShare.Models.FileShareIdentity]$inputObj
+            $retrieved = Get-AzFileShareSnapshot -InputObject $identity
+            $retrieved | Should -Not -BeNullOrEmpty
+            $retrieved.Name | Should -Be $snapshotName
+            
+            # Clean up
+            Remove-AzFileShareSnapshot -InputObject $identity
+        } | Should -Not -Throw
     }
 
-    It 'GetViaIdentity' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+    It 'GetViaIdentityFileShare' {
+        {
+            # First create a snapshot
+            $snapshotName = "snapshot-fileshare-test"
+            $snapshot = New-AzFileShareSnapshot -ResourceGroupName $env.resourceGroup `
+                                               -ResourceName $env.fileShareName01 `
+                                               -Name $snapshotName `
+                                               -Metadata @{"purpose" = "testing"; "environment" = "test"}
+            $snapshot | Should -Not -BeNullOrEmpty
+            
+            # Get via file share identity (provides file share, then specify snapshot name)
+            $fileShareInputObj = @{
+                SubscriptionId = $env.SubscriptionId
+                ResourceGroupName = $env.resourceGroup
+                ResourceName = $env.fileShareName01
+            }
+            $fileShareIdentity = [Microsoft.Azure.PowerShell.Cmdlets.FileShare.Models.FileShareIdentity]$fileShareInputObj
+            $retrieved = Get-AzFileShareSnapshot -FileShareInputObject $fileShareIdentity -Name $snapshotName
+            $retrieved | Should -Not -BeNullOrEmpty
+            $retrieved.Name | Should -Be $snapshotName
+            
+            # Clean up
+            Remove-AzFileShareSnapshot -ResourceGroupName $env.resourceGroup `
+                                      -ResourceName $env.fileShareName01 `
+                                      -Name $snapshotName
+        } | Should -Not -Throw
     }
 }
