b71988fcd9191a34568b6b9b559bb542eba41fdf
JunKai-v <v-juphang@microsoft.com>
2026-03-27 15:41:29 +0800

    fix No Azure login detected

--- src/CloudService/CloudService.Autorest/custom/New-AzCloudServiceExtensionObject.ps1
+++ src/CloudService/CloudService.Autorest/custom/New-AzCloudServiceExtensionObject.ps1
@@ -20,6 +20,7 @@ Create a in-memory object for Extension
 Create a in-memory object for Extension
 #>
 function New-AzCloudServiceExtensionObject {
+    [Microsoft.Azure.PowerShell.Cmdlets.CloudService.ModelCmdletAttribute()]
     [OutputType('Microsoft.Azure.PowerShell.Cmdlets.CloudService.Models.Extension')]
     [Microsoft.Azure.PowerShell.Cmdlets.CloudService.Runtime.OutputBreakingChangeAttribute("Microsoft.Azure.PowerShell.Cmdlets.CloudService.Models.Extension", "16.0.0", "9.0.0", "May 2026", ReplacementCmdletOutputType = "Microsoft.Azure.PowerShell.Cmdlets.CloudService.Models.Extension", DeprecatedOutputProperties = ("RolesAppliedTo System.String[]"), NewOutputProperties = ("RolesAppliedTo System.Collections.Generic.List`1[System.String]"))]
     [CmdletBinding(PositionalBinding=$false)]
