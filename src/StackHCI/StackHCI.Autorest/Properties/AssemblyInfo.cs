6245671360ac11dbab927d0e24bff2e5eefe4e28
JiaSeng-v <v-jiasenglaw@microsoft.com>
2026-03-30 08:11:43 +0800

    rebuild

--- src/StackHCI/StackHCI.Autorest/Properties/AssemblyInfo.cs
+++ src/StackHCI/StackHCI.Autorest/Properties/AssemblyInfo.cs
@@ -24,3 +24,4 @@ using System.Runtime.InteropServices;
 [assembly: System.Reflection.AssemblyVersionAttribute("2.6.6")]
 [assembly: System.Runtime.InteropServices.ComVisibleAttribute(false)]
 [assembly: System.CLSCompliantAttribute(false)]
+
