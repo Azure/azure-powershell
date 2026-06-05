eb9b5b929d87ceb852fb8b7933ff835c9930d970
samudra <sabaner@microsoft.com>
2026-06-04 18:37:05 -0700

    Tests for Get-VMMSS and Remove-Fleet

--- src/ComputeFleet/ComputeFleet.Autorest/test/Get-AzComputeFleetVirtualMachineScaleSet.Tests.ps1
+++ src/ComputeFleet/ComputeFleet.Autorest/test/Get-AzComputeFleetVirtualMachineScaleSet.Tests.ps1
@@ -15,7 +15,53 @@ if(($null -eq $TestName) -or ($TestName -contains 'Get-AzComputeFleetVirtualMach
 }
 
 Describe 'Get-AzComputeFleetVirtualMachineScaleSet' {
-    It 'List' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+    BeforeAll {
+        $resourceGroupName = "fleet-vmss-list-test-rg"
+        $vnetName = "vnet"
+        $nsgName = "nsg"
+        $vmNamePrefix = "fleetvm"
+        $managedFleetName = "managed-fleet-vmss"
+
+        $result = New-TestResourceGroup -ResourceGroupName $resourceGroupName `
+            -Location $env.Location -VNetName $vnetName -NsgName $nsgName
+        $subnetId = $result.SubnetId
+        $nsgId = $result.NsgId
+
+        $vmProfile = New-TestVmProfile -SubnetId $subnetId -NsgId $nsgId -VmNamePrefix $vmNamePrefix
+
+        $vmSize1 = [Microsoft.Azure.PowerShell.Cmdlets.ComputeFleet.Models.VMSizeProfile]::new()
+        $vmSize1.Name = "Standard_D2s_v3"
+
+        New-AzComputeFleet -Name $managedFleetName `
+            -ResourceGroupName $resourceGroupName `
+            -SubscriptionId $env.SubscriptionId `
+            -Location $env.Location `
+            -ComputeProfileBaseVirtualMachineProfile $vmProfile `
+            -ComputeProfileComputeApiVersion "2024-03-01" `
+            -VMSizesProfile @($vmSize1) `
+            -SpotPriorityProfileCapacity 2 `
+            -SpotPriorityProfileAllocationStrategy "PriceCapacityOptimized" `
+            -SpotPriorityProfileEvictionPolicy "Delete" `
+            -RegularPriorityProfileCapacity 2 `
+            -RegularPriorityProfileMinCapacity 1 `
+            -RegularPriorityProfileAllocationStrategy "LowestPrice" `
+            -Tag @{ environment = "test" }
+
+        # Wait for VMSS resources to be provisioned
+        Start-Sleep -Seconds 60
+    }
+
+    It 'List' {
+        {
+            $vmssList = Get-AzComputeFleetVirtualMachineScaleSet -Name $managedFleetName `
+                -ResourceGroupName $resourceGroupName `
+                -SubscriptionId $env.SubscriptionId
+
+            $vmssList.Count | Should -BeGreaterOrEqual 2
+        } | Should -Not -Throw
+    }
+
+    AfterAll {
+        Remove-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue -Confirm:$false
     }
 }
--- src/ComputeFleet/ComputeFleet.Autorest/test/Remove-AzComputeFleet.Tests.ps1
+++ src/ComputeFleet/ComputeFleet.Autorest/test/Remove-AzComputeFleet.Tests.ps1
@@ -15,11 +15,86 @@ if(($null -eq $TestName) -or ($TestName -contains 'Remove-AzComputeFleet'))
 }
 
 Describe 'Remove-AzComputeFleet' {
-    It 'Delete' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+
+    BeforeAll {
+        $resourceGroupName = "fleet-remove-test-rg"
+        $vnetName = "vnet"
+        $nsgName = "nsg"
+        $vmNamePrefix = "fleetvm"
+        $deleteFleetName = "delete-fleet"
+        $deleteViaIdentityFleetName = "delete-via-identity-fleet"
+
+        $result = New-TestResourceGroup -ResourceGroupName $resourceGroupName `
+            -Location $env.Location -VNetName $vnetName -NsgName $nsgName
+        $subnetId = $result.SubnetId
+        $nsgId = $result.NsgId
     }
 
-    It 'DeleteViaIdentity' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+    It 'Delete' {
+        {
+            $vmProfile = New-TestVmProfile -SubnetId $subnetId -NsgId $nsgId
+            $vmSize = [Microsoft.Azure.PowerShell.Cmdlets.ComputeFleet.Models.VMSizeProfile]::new()
+            $vmSize.Name = "Standard_D2s_v3"
+
+            $fleet = New-AzComputeFleet -Name $deleteFleetName `
+                -ResourceGroupName $resourceGroupName `
+                -SubscriptionId $env.SubscriptionId `
+                -Location $env.Location `
+                -ComputeProfileBaseVirtualMachineProfile $vmProfile `
+                -ComputeProfileComputeApiVersion "2024-03-01" `
+                -VMSizesProfile @($vmSize) `
+                -RegularPriorityProfileCapacity 1 `
+                -RegularPriorityProfileMinCapacity 1 `
+                -RegularPriorityProfileAllocationStrategy "LowestPrice" `
+                -Mode "Launch" `
+                -VMNamePrefix $vmNamePrefix `
+                -Tag @{ environment = "test" }
+
+            $fleet.Name | Should -Be $deleteFleetName
+            $fleet.ProvisioningState | Should -Be "Succeeded"
+
+            Remove-AzComputeFleet -Name $deleteFleetName `
+                -ResourceGroupName $resourceGroupName `
+                -SubscriptionId $env.SubscriptionId
+
+            { Get-AzComputeFleet -Name $deleteFleetName `
+                -ResourceGroupName $resourceGroupName `
+                -SubscriptionId $env.SubscriptionId } | Should -Throw
+        } | Should -Not -Throw
+    }
+
+    It 'DeleteViaIdentity' {
+        {
+            $vmProfile = New-TestVmProfile -SubnetId $subnetId -NsgId $nsgId
+            $vmSize = [Microsoft.Azure.PowerShell.Cmdlets.ComputeFleet.Models.VMSizeProfile]::new()
+            $vmSize.Name = "Standard_D2s_v3"
+
+            $fleet = New-AzComputeFleet -Name $deleteViaIdentityFleetName `
+                -ResourceGroupName $resourceGroupName `
+                -SubscriptionId $env.SubscriptionId `
+                -Location $env.Location `
+                -ComputeProfileBaseVirtualMachineProfile $vmProfile `
+                -ComputeProfileComputeApiVersion "2024-03-01" `
+                -VMSizesProfile @($vmSize) `
+                -RegularPriorityProfileCapacity 1 `
+                -RegularPriorityProfileMinCapacity 1 `
+                -RegularPriorityProfileAllocationStrategy "LowestPrice" `
+                -Mode "Launch" `
+                -VMNamePrefix $vmNamePrefix `
+                -Tag @{ environment = "test" }
+
+            $fleet.Name | Should -Be $deleteViaIdentityFleetName
+            $fleet.ProvisioningState | Should -Be "Succeeded"
+
+            Remove-AzComputeFleet -InputObject $fleet
+
+            { Get-AzComputeFleet -Name $deleteViaIdentityFleetName `
+                -ResourceGroupName $resourceGroupName `
+                -SubscriptionId $env.SubscriptionId } | Should -Throw
+        } | Should -Not -Throw
+    }
+
+    AfterAll {
+        Remove-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue -Confirm:$false
     }
 }
