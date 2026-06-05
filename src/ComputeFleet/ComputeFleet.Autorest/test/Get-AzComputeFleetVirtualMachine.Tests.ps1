6c96bc8b1af30211e35ea0ee5d37bcc0dc702222
samudra <sabaner@microsoft.com>
2026-06-04 18:07:17 -0700

    UT for Get-AzComputeFleetVirtualMachine.Test

--- src/ComputeFleet/ComputeFleet.Autorest/test/Get-AzComputeFleetVirtualMachine.Tests.ps1
+++ src/ComputeFleet/ComputeFleet.Autorest/test/Get-AzComputeFleetVirtualMachine.Tests.ps1
@@ -15,7 +15,52 @@ if(($null -eq $TestName) -or ($TestName -contains 'Get-AzComputeFleetVirtualMach
 }
 
 Describe 'Get-AzComputeFleetVirtualMachine' {
-    It 'List' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+    BeforeAll {
+        $resourceGroupName = "fleet-vm-list-test-rg"
+        $vnetName = "vnet"
+        $nsgName = "nsg"
+        $vmNamePrefix = "fleetvm"
+        $launchFleetName = "launch-fleet-vm"
+
+        $result = New-TestResourceGroup -ResourceGroupName $resourceGroupName `
+            -Location $env.Location -VNetName $vnetName -NsgName $nsgName
+        $subnetId = $result.SubnetId
+        $nsgId = $result.NsgId
+
+        $vmProfile = New-TestVmProfile -SubnetId $subnetId -NsgId $nsgId -VmNamePrefix $vmNamePrefix
+
+        $vmSize1 = [Microsoft.Azure.PowerShell.Cmdlets.ComputeFleet.Models.VMSizeProfile]::new()
+        $vmSize1.Name = "Standard_D2s_v3"
+
+        New-AzComputeFleet -Name $launchFleetName `
+            -ResourceGroupName $resourceGroupName `
+            -SubscriptionId $env.SubscriptionId `
+            -Location $env.Location `
+            -ComputeProfileBaseVirtualMachineProfile $vmProfile `
+            -ComputeProfileComputeApiVersion "2024-03-01" `
+            -VMSizesProfile @($vmSize1) `
+            -RegularPriorityProfileCapacity 2 `
+            -RegularPriorityProfileMinCapacity 1 `
+            -RegularPriorityProfileAllocationStrategy "LowestPrice" `
+            -Mode "Launch" `
+            -VMNamePrefix $vmNamePrefix `
+            -Tag @{ environment = "test" }
+
+        # Wait for VMs to be provisioned
+        Start-Sleep -Seconds 60
+    }
+
+    It 'List' {
+        {
+            $vmList = Get-AzComputeFleetVirtualMachine -Name $launchFleetName `
+                -ResourceGroupName $resourceGroupName `
+                -SubscriptionId $env.SubscriptionId
+
+            $vmList.Count | Should -BeGreaterOrEqual 1
+        } | Should -Not -Throw
+    }
+
+    AfterAll {
+        Remove-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue -Confirm:$false
     }
 }
