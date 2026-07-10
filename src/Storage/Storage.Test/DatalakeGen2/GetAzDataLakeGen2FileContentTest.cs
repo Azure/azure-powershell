5d109f3310f61aeeeafc02772db217c5bca5c784
Luis Roca <lroca@microsoft.com>
2026-07-10 11:29:31 -0400

    Adding unit tests to catch regressions on detecting path traversals

---
+++ src/Storage/Storage.Test/DatalakeGen2/GetAzDataLakeGen2FileContentTest.cs
@@ -0,0 +1,103 @@
+// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.WindowsAzure.Commands.Storage.Blob.Cmdlet;
+
+namespace Microsoft.WindowsAzure.Commands.Storage.Test.Blob.Cmdlet
+{
+    /// <summary>
+    /// Unit test for get azure data lake gen2 file content cmdlet
+    /// </summary>
+    [TestClass]
+    public class GetAzDataLakeGen2FileContentTest : StorageBlobTestBase
+    {
+        private GetAzDataLakeGen2ItemContentCommand command = null;
+
+        private string destinationDirectory = null;
+
+        [TestInitialize]
+        public void InitCommand()
+        {
+            command = new GetAzDataLakeGen2ItemContentCommand(BlobMock)
+            {
+                CommandRuntime = MockCmdRunTime
+            };
+            CurrentBlobCmd = command;
+
+            destinationDirectory = Path.Combine(Path.GetTempPath(), "GetDataLakeContentTraversalTest_" + Guid.NewGuid().ToString("N"));
+            Directory.CreateDirectory(destinationDirectory);
+        }
+
+        [TestCleanup]
+        public void CleanCommand()
+        {
+            if (destinationDirectory != null && Directory.Exists(destinationDirectory))
+            {
+                Directory.Delete(destinationDirectory, true);
+            }
+            destinationDirectory = null;
+            command = null;
+        }
+
+        /// <summary>
+        /// An item name that resolves outside the destination directory must be rejected.
+        /// </summary>
+        [DataTestMethod]
+        [DataRow("../evil.dll")]
+        [DataRow("../../evil.dll")]
+        [DataRow("..\\..\\evil.dll")]
+        [DataRow("sub/../../evil.dll")]
+        [DataRow("sub/sub2/../../../evil.dll")]
+        public void GetFullReceiveFilePathBlocksPathTraversal(string itemName)
+        {
+            AssertThrows<ArgumentException>(
+                () => command.GetFullReceiveFilePath(destinationDirectory, itemName),
+                string.Format(Resources.DownloadDestinationPathTraversal, itemName, destinationDirectory));
+        }
+
+        /// <summary>
+        /// Legitimate item names (including nested virtual directories) must resolve
+        /// to a path inside the destination directory.
+        /// </summary>
+        [TestMethod]
+        public void GetFullReceiveFilePathAllowsInDirectoryPaths()
+        {
+            Assert.AreEqual(
+                Path.Combine(destinationDirectory, "file.txt"),
+                command.GetFullReceiveFilePath(destinationDirectory, "file.txt"));
+
+            Assert.AreEqual(
+                Path.Combine(destinationDirectory, "sub", "file.txt"),
+                command.GetFullReceiveFilePath(destinationDirectory, "sub/file.txt"));
+
+            Assert.AreEqual(
+                Path.Combine(destinationDirectory, "a", "b", "c", "file.txt"),
+                command.GetFullReceiveFilePath(destinationDirectory, "a/b/c/file.txt"));
+        }
+
+        /// <summary>
+        /// An item name with a ".." segment that still lands inside the destination
+        /// directory must be allowed (not over-blocked).
+        /// </summary>
+        [TestMethod]
+        public void GetFullReceiveFilePathAllowsInDirectoryTraversal()
+        {
+            string result = command.GetFullReceiveFilePath(destinationDirectory, "sub/../file.txt");
+            Assert.AreEqual(Path.Combine(destinationDirectory, "file.txt"), Path.GetFullPath(result));
+        }
+    }
+}
