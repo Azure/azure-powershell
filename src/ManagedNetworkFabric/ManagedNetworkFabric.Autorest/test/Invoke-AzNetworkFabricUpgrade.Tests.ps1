7f82091563b8f4adc22900474b94b442c7d2efc0
Joselyn Narvaez <jnarvaez@microsoft.com>
2026-07-13 08:47:10 -0500

    update tests

--- src/ManagedNetworkFabric/ManagedNetworkFabric.Autorest/test/Invoke-AzNetworkFabricUpgrade.Tests.ps1
+++ src/ManagedNetworkFabric/ManagedNetworkFabric.Autorest/test/Invoke-AzNetworkFabricUpgrade.Tests.ps1
@@ -1,22 +1,23 @@
-if(($null -eq $TestName) -or ($TestName -contains 'Invoke-AzNetworkFabricUpgrade'))
-{
-  $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
-  if (-Not (Test-Path -Path $loadEnvPath)) {
-      $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
-  }
-  . ($loadEnvPath)
-  $TestRecordingFile = Join-Path $PSScriptRoot 'Invoke-AzNetworkFabricUpgrade.Recording.json'
-  $currentPath = $PSScriptRoot
-  while(-not $mockingPath) {
-      $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
-      $currentPath = Split-Path -Path $currentPath -Parent
-  }
-  . ($mockingPath | Select-Object -First 1).FullName
+if (($null -eq $TestName) -or ($TestName -contains 'Invoke-AzNetworkFabricUpgrade')) {
+    $loadEnvPath = Join-Path $PSScriptRoot 'loadEnv.ps1'
+    if (-Not (Test-Path -Path $loadEnvPath)) {
+        $loadEnvPath = Join-Path $PSScriptRoot '..\loadEnv.ps1'
+    }
+    . ($loadEnvPath)
+    $TestRecordingFile = Join-Path $PSScriptRoot 'Invoke-AzNetworkFabricUpgrade.Recording.json'
+    $currentPath = $PSScriptRoot
+    while (-not $mockingPath) {
+        $mockingPath = Get-ChildItem -Path $currentPath -Recurse -Include 'HttpPipelineMocking.ps1' -File
+        $currentPath = Split-Path -Path $currentPath -Parent
+    }
+    . ($mockingPath | Select-Object -First 1).FullName
 }
 
 Describe 'Invoke-AzNetworkFabricUpgrade' {
-    It 'UpgradeExpanded' -skip {
-        { throw [System.NotImplementedException] } | Should -Not -Throw
+    It 'UpgradeExpanded' {
+        {
+            Invoke-AzNetworkFabricUpgrade -Name $global:config.fabric.name -ResourceGroupName $global:config.common.resourceGroupName -SubscriptionId $global:config.common.subscriptionId
+        } | Should -Not -Throw
     }
 
     It 'UpgradeViaJsonString' -skip {
