97536e91773a4af286e94298087c4faf36464836
piyushvermaa <112806418+piyushvermaa@users.noreply.github.com>
2026-07-24 12:36:08 +0530

    Scrub PII from Datadog test files and recordings

--- src/Datadog/Datadog.Autorest/test/Initialize-AzDatadogSaaSOperationGroupResource.Tests.ps1
+++ src/Datadog/Datadog.Autorest/test/Initialize-AzDatadogSaaSOperationGroupResource.Tests.ps1
@@ -17,25 +17,25 @@ if(($null -eq $TestName) -or ($TestName -contains 'Initialize-AzDatadogSaaSOpera
 Describe 'Initialize-AzDatadogSaaSOperationGroupResource' {
     It 'ActivateExpanded' {
         $saaSResourceId = "/subscriptions/$($env.SubscriptionId)/resourceGroups/$($env.resourceGroup)/providers/Microsoft.SaaS/resources/$($env.monitorName01)SaaS"
-        { Initialize-AzDatadogSaaSOperationGroupResource -SubscriptionId $env.SubscriptionId -SaaSResourceId $saaSResourceId -UserInfoName 'piyushverma' -UserInfoEmailAddress 'vermapiyush@microsoft.com' -DatadogOrganizationPropertyName 'testorg' -DatadogOrganizationPropertyId 'org123' -WhatIf } | Should -Not -Throw
+        { Initialize-AzDatadogSaaSOperationGroupResource -SubscriptionId $env.SubscriptionId -SaaSResourceId $saaSResourceId -UserInfoName 'azureuser' -UserInfoEmailAddress 'user@example.com' -DatadogOrganizationPropertyName 'testorg' -DatadogOrganizationPropertyId 'org123' -WhatIf } | Should -Not -Throw
     }
 
     It 'Activate' {
         $saaSResourceId = "/subscriptions/$($env.SubscriptionId)/resourceGroups/$($env.resourceGroup)/providers/Microsoft.SaaS/resources/$($env.monitorName01)SaaS"
-        { Initialize-AzDatadogSaaSOperationGroupResource -SubscriptionId $env.SubscriptionId -Body @{ saaSResourceId = $saaSResourceId; userInfo = @{ name = 'piyushverma'; emailAddress = 'vermapiyush@microsoft.com'; phoneNumber = '11111111111' }; datadogOrganizationProperties = @{ name = 'testorg'; id = 'org123' } } -WhatIf } | Should -Not -Throw
+        { Initialize-AzDatadogSaaSOperationGroupResource -SubscriptionId $env.SubscriptionId -Body @{ saaSResourceId = $saaSResourceId; userInfo = @{ name = 'azureuser'; emailAddress = 'user@example.com'; phoneNumber = '11111111111' }; datadogOrganizationProperties = @{ name = 'testorg'; id = 'org123' } } -WhatIf } | Should -Not -Throw
     }
 
     It 'ActivateViaJsonFilePath' {
         $saaSResourceId = "/subscriptions/$($env.SubscriptionId)/resourceGroups/$($env.resourceGroup)/providers/Microsoft.SaaS/resources/$($env.monitorName01)SaaS"
         $tempFile = New-TemporaryFile
-        @{ saaSResourceId = $saaSResourceId; userInfo = @{ name = 'piyushverma'; emailAddress = 'vermapiyush@microsoft.com'; phoneNumber = '11111111111' }; datadogOrganizationProperties = @{ name = 'testorg'; id = 'org123' } } | ConvertTo-Json -Depth 4 | Set-Content -Path $tempFile.FullName
+        @{ saaSResourceId = $saaSResourceId; userInfo = @{ name = 'azureuser'; emailAddress = 'user@example.com'; phoneNumber = '11111111111' }; datadogOrganizationProperties = @{ name = 'testorg'; id = 'org123' } } | ConvertTo-Json -Depth 4 | Set-Content -Path $tempFile.FullName
         { Initialize-AzDatadogSaaSOperationGroupResource -JsonFilePath $tempFile.FullName -WhatIf } | Should -Not -Throw
         Remove-Item -Path $tempFile.FullName -Force
     }
 
     It 'ActivateViaJsonString' {
         $saaSResourceId = "/subscriptions/$($env.SubscriptionId)/resourceGroups/$($env.resourceGroup)/providers/Microsoft.SaaS/resources/$($env.monitorName01)SaaS"
-        $jsonString = @{ saaSResourceId = $saaSResourceId; userInfo = @{ name = 'piyushverma'; emailAddress = 'vermapiyush@microsoft.com'; phoneNumber = '11111111111' }; datadogOrganizationProperties = @{ name = 'testorg'; id = 'org123' } } | ConvertTo-Json -Depth 4
+        $jsonString = @{ saaSResourceId = $saaSResourceId; userInfo = @{ name = 'azureuser'; emailAddress = 'user@example.com'; phoneNumber = '11111111111' }; datadogOrganizationProperties = @{ name = 'testorg'; id = 'org123' } } | ConvertTo-Json -Depth 4
         { Initialize-AzDatadogSaaSOperationGroupResource -JsonString $jsonString -WhatIf } | Should -Not -Throw
     }
 }
